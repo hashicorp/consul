fd831ade43d368c07074c71570c294bcaaf21211
Armon Dadgar <armon.dadgar@gmail.com>
2013-12-12 11:48:22 -0800

    Remove unused raft endpoints

--- consul/raft_endpoint.go
+++ consul/raft_endpoint.go
@@ -9,21 +9,6 @@ type Raft struct {
 	server *Server
 }
 
-func (r *Raft) Apply(args []byte, reply *struct{}) error {
-	future := r.server.raft.Apply(args, 0)
-	return future.Error()
-}
-
-func (r *Raft) AddPeer(args string, reply *struct{}) error {
-	peer, err := net.ResolveTCPAddr("tcp", args)
-	if err != nil {
-		r.server.logger.Printf("[ERR] Failed to parse peer: %v", err)
-		return err
-	}
-	future := r.server.raft.AddPeer(peer)
-	return future.Error()
-}
-
 func (r *Raft) RemovePeer(args string, reply *struct{}) error {
 	peer, err := net.ResolveTCPAddr("tcp", args)
 	if err != nil {
