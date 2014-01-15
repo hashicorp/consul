554ebdfc9afe286652995ea942eb0641a07aee76
Armon Dadgar <armon.dadgar@gmail.com>
2014-01-15 12:34:17 -1000

    Methods for random staggering

--- command/agent/util.go
+++ command/agent/util.go
@@ -2,6 +2,7 @@ package agent
 
 import (
 	"math"
+	"math/rand"
 	"time"
 )
 
@@ -22,4 +23,9 @@ func aeScale(interval time.Duration, n int) time.Duration {
 
 	multiplier := math.Ceil(math.Log2(float64(n))-math.Log2(aeScaleThreshold)) + 1.0
 	return time.Duration(multiplier) * interval
+}
+
+// Returns a random stagger interval between 0 and the duration
+func randomStagger(intv time.Duration) time.Duration {
+	return time.Duration(uint64(rand.Int63()) % uint64(intv))
 }
--- command/agent/util_test.go
+++ command/agent/util_test.go
@@ -20,3 +20,13 @@ func TestAEScale(t *testing.T) {
 		t.Fatalf("Bad: %v", v)
 	}
 }
+
+func TestRandomStagger(t *testing.T) {
+	intv := time.Minute
+	for i := 0; i < 10; i++ {
+		stagger := randomStagger(intv)
+		if stagger < 0 || stagger >= intv {
+			t.Fatalf("Bad: %v", stagger)
+		}
+	}
+}
