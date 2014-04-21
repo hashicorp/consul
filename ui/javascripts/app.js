62e5ea194147f2b37e9fb935a82d4e1402ad063b
Jack Pearkes <jackpearkes@gmail.com>
2014-04-21 16:36:50 -0400

    ui: add base ember

---
+++ ui/javascripts/app.js
@@ -0,0 +1,11 @@
+App = Ember.Application.create();
+
+App.Router.map(function() {
+  // put your routes here
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return ['red', 'yellow', 'blue'];
+  }
+});
