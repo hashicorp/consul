9d6b63c4070e8910c6fa67a5fe409cdd7ecd1d07
Jack Pearkes <jackpearkes@gmail.com>
2014-04-25 13:49:36 -0400

    ui: refactor file structure

---
+++ javascripts/app/controllers.js
@@ -0,0 +1,15 @@
+//
+// path: /
+//
+// The index is for choosing datacenters.
+//
+App.IndexController = Ember.Controller.extend({
+});
+
+//
+// path: /:dc
+//
+App.DcController = Ember.Controller.extend({
+})
+
+
