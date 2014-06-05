9114abaf115b40a4e64916eb29b6d075b4cf75dc
Jack Pearkes <jackpearkes@gmail.com>
2014-06-05 13:24:03 -0400

    ui: show and hide list view on mobile

--- javascripts/app/controllers.js
+++ javascripts/app/controllers.js
@@ -1,3 +1,9 @@
+App.ApplicationController = Ember.ObjectController.extend({
+  updateCurrentPath: function() {
+    App.set('currentPath', this.get('currentPath'));
+  }.observes('currentPath')
+});
+
 App.DcController = Ember.Controller.extend({
   // Whether or not the dropdown menu can be seen
   isDropdownVisible: false,
@@ -230,13 +236,19 @@ App.KvEditController = KvBaseController.extend({
 });
 
 ItemBaseController = Ember.ArrayController.extend({
-  needs: ["dc"],
+  needs: ["dc", "application"],
   queryParams: ["filter", "status", "condensed"],
   dc: Ember.computed.alias("controllers.dc"),
   condensed: true,
   filter: "", // default
   status: "any status", // default
   statuses: ["any status", "passing", "failing"],
+
+  isShowingItem: function() {
+    var currentPath = this.get('controllers.application.currentPath');
+    console.log(currentPath);
+    return (currentPath === "dc.nodes.show" || currentPath === "dc.services.show");
+  }.property('controllers.application.currentPath'),
 
   filteredContent: function() {
     var filter = this.get('filter');
@@ -269,7 +281,6 @@ ItemBaseController = Ember.ArrayController.extend({
 App.NodesController = ItemBaseController.extend({
   items: Ember.computed.alias("nodes"),
 });
-
 
 App.ServicesController = ItemBaseController.extend({
   items: Ember.computed.alias("services"),
