87373787e27f446b6300ac6b4a760eb864b80a1a
Armon Dadgar <armon.dadgar@gmail.com>
2014-06-09 21:34:19 -0700

    agent: Fixing merge of CheckUpdateInterval

--- command/agent/config.go
+++ command/agent/config.go
@@ -553,7 +553,7 @@ func MergeConfig(a, b *Config) *Config {
 	if b.DNSConfig.MaxStale != 0 {
 		result.DNSConfig.MaxStale = b.DNSConfig.MaxStale
 	}
-	if b.CheckUpdateIntervalRaw != "" {
+	if b.CheckUpdateIntervalRaw != "" || b.CheckUpdateInterval != 0 {
 		result.CheckUpdateInterval = b.CheckUpdateInterval
 	}
 
