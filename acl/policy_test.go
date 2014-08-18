05900f35c2720cf23f6ebbb20fc0f7e9319ab2b1
Armon Dadgar <armon.dadgar@gmail.com>
2014-08-18 14:54:52 -0700

    acl: Test parsing JSON

--- acl/policy_test.go
+++ acl/policy_test.go
@@ -50,3 +50,51 @@ key "foo/bar/baz" {
 		t.Fatalf("bad: %#v %#v", out, exp)
 	}
 }
+
+func TestParse_JSON(t *testing.T) {
+	inp := `{
+	"key": {
+		"": {
+			"policy": "read"
+		},
+		"foo/": {
+			"policy": "write"
+		},
+		"foo/bar/": {
+			"policy": "read"
+		},
+		"foo/bar/baz": {
+			"policy": "deny"
+		}
+	}
+}`
+	exp := &Policy{
+		Keys: []*KeyPolicy{
+			&KeyPolicy{
+				Prefix: "",
+				Policy: KeyPolicyRead,
+			},
+			&KeyPolicy{
+				Prefix: "foo/",
+				Policy: KeyPolicyWrite,
+			},
+			&KeyPolicy{
+				Prefix: "foo/bar/",
+				Policy: KeyPolicyRead,
+			},
+			&KeyPolicy{
+				Prefix: "foo/bar/baz",
+				Policy: KeyPolicyDeny,
+			},
+		},
+	}
+
+	out, err := Parse(inp)
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+
+	if !reflect.DeepEqual(out, exp) {
+		t.Fatalf("bad: %#v %#v", out, exp)
+	}
+}
