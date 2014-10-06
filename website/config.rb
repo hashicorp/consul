13ca50102c812ccd2540a07c83a19915d2dabdab
Seth Vargo <sethvargo@gmail.com>
2014-10-06 19:13:22 -0400

    Move sidebar helper inline

--- website/config.rb
+++ website/config.rb
@@ -17,4 +17,15 @@ configure :build do
   activate :asset_hash
   activate :minify_html
   activate :minify_javascript
+helpers do
+  # This helps by setting the "active" class for sidebar nav elements
+  # if the YAML frontmatter matches the expected value.
+  def sidebar_current(expected)
+    current = current_page.data.sidebar_current || ""
+    if current.start_with?(expected)
+      return " class=\"active\""
+    else
+      return ""
+    end
+  end
 end
