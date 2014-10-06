fcd86a47216c81046995e2f081f7755d2e49f920
Seth Vargo <sethvargo@gmail.com>
2014-10-06 19:15:01 -0400

    Use middleman-hashicorp :)

--- website/config.rb
+++ website/config.rb
@@ -2,21 +2,17 @@
 # Configure Middleman
 #-------------------------------------------------------------------------
 
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
+activate :hashicorp do |h|
+  h.version      = '0.4.0'
+  h.bintray_repo = 'mitchellh/consul'
+  h.bintray_user = 'mitchellh'
+  h.bintray_key  = ENV['BINTRAY_API_KEY']
 
-# Use the RedCarpet Markdown engine
-set :markdown_engine, :redcarpet
-set :markdown,
-    :fenced_code_blocks => true,
-    :with_toc_data => true
+  h.bintray_exclude_proc = Proc.new do |os, filename|
+    os == 'web'
+  end
+end
 
-# Build-specific configuration
-configure :build do
-  activate :asset_hash
-  activate :minify_html
-  activate :minify_javascript
 helpers do
   # This helps by setting the "active" class for sidebar nav elements
   # if the YAML frontmatter matches the expected value.
