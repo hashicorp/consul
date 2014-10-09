93f17736fd52d1e2dbc4d74bbff786021e24ce4c
Armon Dadgar <armon.dadgar@gmail.com>
2014-10-09 11:33:25 -0700

    Ensure FSM also does not use UUIDs

--- scripts/verify_no_uuid.sh
+++ scripts/verify_no_uuid.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-set -e
+
 grep generateUUID consul/state_store.go
 RESULT=$?
+if [ $RESULT -eq 0 ]; then
+    exit 1
+fi
 
+grep generateUUID consul/fsm.go
+RESULT=$?
 if [ $RESULT -eq 0 ]; then
     exit 1
 fi
