b9977338fda7de59b29a523acb22acbad2f0e103
Ryan Uber <ru@ryanuber.com>
2014-10-13 22:38:12 -0700

    agent: Filter messages logged to syslog. Fixes #272

---
+++ command/agent/syslog_test.go
@@ -0,0 +1,35 @@
+package agent
+
+import (
+	"testing"
+
+	"github.com/hashicorp/go-syslog"
+	"github.com/hashicorp/logutils"
+)
+
+func TestSyslogFilter(t *testing.T) {
+	l, err := gsyslog.NewLogger(gsyslog.LOG_NOTICE, "LOCAL0", "consul")
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	filt := LevelFilter()
+	filt.MinLevel = logutils.LogLevel("INFO")
+
+	s := &SyslogWrapper{l, filt}
+	n, err := s.Write([]byte("[INFO] test"))
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	if n == 0 {
+		t.Fatalf("should have logged")
+	}
+
+	n, err = s.Write([]byte("[DEBUG] test"))
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	if n != 0 {
+		t.Fatalf("should not have logged")
+	}
+}
