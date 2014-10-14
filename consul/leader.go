e39d2ee675af48ef195b4142e494cc03a45e353f
Armon Dadgar <armon.dadgar@gmail.com>
2014-10-14 11:02:26 -0700

    consul: Reap left members ignoring state. Fixes #371

--- consul/leader.go
+++ consul/leader.go
@@ -197,7 +197,7 @@ func (s *Server) reconcile() (err error) {
 // a "reap" event to cause the node to be cleaned up.
 func (s *Server) reconcileReaped(known map[string]struct{}) error {
 	state := s.fsm.State()
-	_, critical := state.ChecksInState(structs.HealthCritical)
+	_, critical := state.ChecksInState(structs.HealthAny)
 	for _, check := range critical {
 		// Ignore any non serf checks
 		if check.CheckID != SerfCheckID {
