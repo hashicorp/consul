c283754381fa59054ae08f74e575d62a2385721b
Ryan Uber <ru@ryanuber.com>
2014-11-19 16:45:49 -0800

    Rebase against upstream

--- command/agent/command_test.go
+++ command/agent/command_test.go
@@ -1,16 +1,10 @@
 package agent
 
 import (
-<<<<<<< HEAD
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
-	"strings"
-=======
-	"github.com/mitchellh/cli"
->>>>>>> agent: -encrypt appends to keyring if one exists
 	"testing"
 
 	"github.com/hashicorp/consul/testutil"
@@ -167,34 +161,5 @@ func TestRetryJoinWanFail(t *testing.T) {
 
 	if code := cmd.Run(args); code == 0 {
 		t.Fatalf("bad: %d", code)
-	}
-}
-
-func TestArgConflict(t *testing.T) {
-	ui := new(cli.MockUi)
-	c := &Command{Ui: ui}
-
-	dir, err := ioutil.TempDir("", "agent")
-	if err != nil {
-		t.Fatalf("err: %s", err)
-	}
-
-	key := "HS5lJ+XuTlYKWaeGYyG+/A=="
-
-	fileLAN := filepath.Join(dir, SerfLANKeyring)
-	if err := testutil.InitKeyring(fileLAN, key); err != nil {
-		t.Fatalf("err: %s", err)
-	}
-
-	args := []string{
-		"-encrypt=" + key,
-		"-data-dir=" + dir,
-	}
-	code := c.Run(args)
-	if code != 1 {
-		t.Fatalf("bad: %d. %#v", code, ui.ErrorWriter.String())
-	}
-	if !strings.Contains(ui.ErrorWriter.String(), "keyring files exist") {
-		t.Fatalf("bad: %#v", ui.ErrorWriter.String())
 	}
 }
--- consul/client.go
+++ consul/client.go
@@ -206,6 +206,11 @@ func (c *Client) UserEvent(name string, payload []byte) error {
 	return c.serf.UserEvent(userEventName(name), payload, false)
 }
 
+// KeyManagerLAN returns the LAN Serf keyring manager
+func (c *Client) KeyManagerLAN() *serf.KeyManager {
+	return c.serf.KeyManager()
+}
+
 // Encrypted determines if gossip is encrypted
 func (c *Client) Encrypted() bool {
 	return c.serf.EncryptionEnabled()
