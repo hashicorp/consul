462a03c1abb35defc024d90b915001b74f686953
buddhamagnet <buddhamagnet@gmail.com>
2015-07-08 23:22:12 +0100

    switch to more idiomatic var syntax

--- version.go
+++ version.go
@@ -1,8 +1,10 @@
 package main
 
 // The git commit that was compiled. This will be filled in by the compiler.
-var GitCommit string
-var GitDescribe string
+var (
+	GitCommit   string
+	GitDescribe string
+)
 
 // The main version number that is being run at the moment.
 const Version = "0.5.2"
