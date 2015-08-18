96785edd9a6eca327567262b63966030371fea9f
Shawn Cook <cooksha@yahoo-inc.com>
2015-08-18 14:03:48 -0700

    Add EnableTagDrift logic to command/agent/local.go

--- command/agent/local.go
+++ command/agent/local.go
@@ -379,7 +379,12 @@ func (l *localState) setSyncState() error {
 		}
 
 		// If our definition is different, we need to update it
-		l.logger.Printf("[ERR] services: %v", service)
+		if existing.EnableTagDrift {
+			l.logger.Printf("[DEBUG] Tag drift enabled.")
+			existing.Tags = service.Tags
+		} else {
+			l.logger.Printf("[DEBUG] Tag drift disabled.")
+		}
 		equal := reflect.DeepEqual(existing, service)
 		l.serviceStatus[id] = syncStatus{inSync: equal}
 	}
