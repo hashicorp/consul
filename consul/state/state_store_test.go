0085512eb83adff043b185181a1ce4856129274a
Ryan Uber <ru@ryanuber.com>
2015-08-22 13:29:44 -0700

    consul/state: more tests for EnsureNode/GetNode

--- consul/state/state_store_test.go
+++ consul/state/state_store_test.go
@@ -18,8 +18,13 @@ func testStateStore(t *testing.T) *StateStore {
 	return s
 }
 
-func TestStateStore_EnsureNode(t *testing.T) {
+func TestStateStore_EnsureNode_GetNode(t *testing.T) {
 	s := testStateStore(t)
+
+	// Fetching a non-existent node returns nil
+	if node, err := s.GetNode("node1"); node != nil || err != nil {
+		t.Fatalf("expected (nil, nil), got: (%#v, %#v)", node, err)
+	}
 
 	// Create a node registration request
 	in := &structs.Node{
@@ -63,6 +68,18 @@ func TestStateStore_EnsureNode(t *testing.T) {
 	// Node and indexes were updated
 	if out.CreateIndex != 1 || out.ModifyIndex != 2 || out.Address != "1.1.1.2" {
 		t.Fatalf("bad: %#v", out)
+	}
+
+	// Node upsert is idempotent
+	if err := s.EnsureNode(2, in); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	out, err = s.GetNode("node1")
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	if out.Address != "1.1.1.2" || out.CreateIndex != 1 || out.ModifyIndex != 2 {
+		t.Fatalf("node was modified: %#v", out)
 	}
 }
 
