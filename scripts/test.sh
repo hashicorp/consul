9e980e4ad0f4770eda97e65e2af5a03fcd976d6c
John Sullivan <josullivan@cimpress.com>
2015-11-06 14:20:26 -0500

    Remove unintentionally committed code that invokes the 'godebug' debugger when running tests.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
-export PATH=${PATH}:${GOPATH}/src/github.com/mailgun/godebug
-
 # Create a temp dir and clean it up on exit
 TEMPDIR=`mktemp -d -t consul-test.XXX`
 trap "rm -rf $TEMPDIR" EXIT HUP INT QUIT TERM
 
 # Build the Consul binary for the API tests
 echo "--> Building consul"
-godebug build -o $TEMPDIR/consul || exit 1
+go build -o $TEMPDIR/consul || exit 1
 
 # Run the tests
 echo "--> Running tests"
-go list ./... | PATH=$TEMPDIR:$PATH xargs -n1 godebug test
+go list ./... | PATH=$TEMPDIR:$PATH xargs -n1 go test
