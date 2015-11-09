6593f945eb3f96ebe3ac8701862dae6d45c8a358
captainill <jonthomas0@gmail.com>
2015-11-09 13:48:29 -0800

    whitespace

--- website/source/assets/javascripts/_app/homepage.js
+++ website/source/assets/javascripts/_app/homepage.js
@@ -17,26 +17,26 @@ var APP = APP || {};
           $hero: $('#jumbotron'),
           $collapse: $('.navbar-collapse')
         }
-        
+
         this.addEventListeners();
 
       },
-
+      
       addEventListeners: function(){
         var _this = this;
 
         if(APP.Utils.isMobile)
-          return;
-        
+        return;
+
         _this.ui.$doc.scroll(function() {
 
           //if collapseable menu is open dont do parrallax. It looks wonky. Bootstrap conflict
           if( _this.ui.$collapse.hasClass('in'))
-              return;
+          return;
 
           var top = _this.ui.$doc.scrollTop(),
-              speedAdj = (top*0.8),
-              speedAdjOffset = speedAdj - top;
+          speedAdj = (top*0.8),
+          speedAdjOffset = speedAdj - top;
 
           _this.ui.$hero.css('webkitTransform', 'translate(0, '+ speedAdj +'px)');
           _this.ui.$hero.find('.container').css('webkitTransform', 'translate(0, '+  speedAdjOffset +'px)');
@@ -46,4 +46,3 @@ var APP = APP || {};
   }());
 
 }(jQuery, this));
-
