be205b8ed0dd147378aa3bced4ed774393b80543
Sean Chittenden <sean@chittenden.org>
2016-02-01 22:48:59 -0800

    Chase case change in the function name now that GenerateUUID is an exported function.

--- scripts/windows/verify_no_uuid.bat
+++ scripts/windows/verify_no_uuid.bat
@@ -5,10 +5,10 @@ setlocal
 if not exist %1\consul\state\state_store.go exit /B 1
 if not exist %1\consul\fsm.go exit /B 1
 
-findstr /R generateUUID %1\consul\state\state_store.go 1>nul
+findstr /R GenerateUUID %1\consul\state\state_store.go 1>nul
 if not %ERRORLEVEL% EQU 1 exit /B 1
 
-findstr generateUUID %1\consul\fsm.go 1>nul
+findstr GenerateUUID %1\consul\fsm.go 1>nul
 if not %ERRORLEVEL% EQU 1 exit /B 1
 
 exit /B 0
