afdeb2f1fc189c5a9e6440c27c1918e7b09c2cdc
James Phillips <james@hashicorp.com>
2016-02-16 11:45:29 -0800

    Adds support for EnableTagOverride to the API client.

--- api/agent.go
+++ api/agent.go
@@ -18,11 +18,12 @@ type AgentCheck struct {
 
 // AgentService represents a service known to the agent
 type AgentService struct {
-	ID      string
-	Service string
-	Tags    []string
-	Port    int
-	Address string
+	ID                string
+	Service           string
+	Tags              []string
+	Port              int
+	Address           string
+	EnableTagOverride bool
 }
 
 // AgentMember represents a cluster member known to the agent
@@ -42,13 +43,14 @@ type AgentMember struct {
 
 // AgentServiceRegistration is used to register a new service
 type AgentServiceRegistration struct {
-	ID      string   `json:",omitempty"`
-	Name    string   `json:",omitempty"`
-	Tags    []string `json:",omitempty"`
-	Port    int      `json:",omitempty"`
-	Address string   `json:",omitempty"`
-	Check   *AgentServiceCheck
-	Checks  AgentServiceChecks
+	ID                string   `json:",omitempty"`
+	Name              string   `json:",omitempty"`
+	Tags              []string `json:",omitempty"`
+	Port              int      `json:",omitempty"`
+	Address           string   `json:",omitempty"`
+	EnableTagOverride bool     `json:",omitempty"`
+	Check             *AgentServiceCheck
+	Checks            AgentServiceChecks
 }
 
 // AgentCheckRegistration is used to register a new check
