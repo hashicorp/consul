075d1b628fd6d9fdd5f98e64e4e1dff2caa7fb03
Sean Chittenden <sean@chittenden.org>
2016-02-18 15:44:32 -0800

    Commit miss re: consuls variable rename

--- consul/client_test.go
+++ consul/client_test.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/hashicorp/consul/consul/structs"
 	"github.com/hashicorp/consul/testutil"
-	"github.com/hashicorp/net-rpc-msgpackrpc"
+	"github.com/hashicorp/consul/vendor/github.com/hashicorp/net-rpc-msgpackrpc"
 	"github.com/hashicorp/serf/serf"
 )
 
