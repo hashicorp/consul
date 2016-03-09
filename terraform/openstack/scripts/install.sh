b79774ff969a3f3fc40d15821b1676178aeae7bc
Tyler Tidman <tyler.tidman@draak.ca>
2016-03-09 11:03:31 -0500

    Use more portable shebangs for bash scripts

--- terraform/openstack/scripts/install.sh
+++ terraform/openstack/scripts/install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
 # Read the address to join from the file we provisioned
--- terraform/openstack/scripts/server.sh
+++ terraform/openstack/scripts/server.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
 # Read from the file we created
