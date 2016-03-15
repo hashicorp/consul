56de005c8a20f028a386a0a72f8a8e48985d86f5
Shane O'Grady <shane@ogrady.ie>
2016-03-15 14:57:26 -0300

    Add RPC port to iptables config
    From https://www.consul.io/docs/agent/options.html#ports

--- terraform/shared/scripts/ip_tables.sh
+++ terraform/shared/scripts/ip_tables.sh
@@ -4,6 +4,7 @@ set -e
 sudo iptables -I INPUT -s 0/0 -p tcp --dport 8300 -j ACCEPT
 sudo iptables -I INPUT -s 0/0 -p tcp --dport 8301 -j ACCEPT
 sudo iptables -I INPUT -s 0/0 -p tcp --dport 8302 -j ACCEPT
+sudo iptables -I INPUT -s 0/0 -p tcp --dport 8400 -j ACCEPT
 
 if [ -d /etc/sysconfig ]; then
   sudo iptables-save | sudo tee /etc/sysconfig/iptables
