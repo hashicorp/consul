6b667cc3404b9f1e92cf88e7668949e717da132c
Miek Gieben <miek@miek.nl>
2016-03-22 11:13:12 +0000

    tests

--- middleware/etcd/path_test.go
+++ middleware/etcd/path_test.go
@@ -3,11 +3,11 @@ package etcd
 import "testing"
 
 func TestPath(t *testing.T) {
-	for path := range []string{"mydns", "skydns"} {
-		e := Etcd{PathPrefix: "mydns"}
+	for _, path := range []string{"mydns", "skydns"} {
+		e := Etcd{PathPrefix: path}
 		result := e.Path("service.staging.skydns.local.")
-		if result != "/"+path+"/"+"/local/skydns/staging/service" {
-			t.Errorf("Failure to get domain's path with prefix: %s", path)
+		if result != "/"+path+"/local/skydns/staging/service" {
+			t.Errorf("Failure to get domain's path with prefix: %s", result)
 		}
 	}
 }
