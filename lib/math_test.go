4584e70636a18702c618be5a615ef6ad5ebf9501
Sean Chittenden <sean@chittenden.org>
2016-03-30 11:47:37 -0700

    Add lib.AbsInt() helper function

--- lib/math_test.go
+++ lib/math_test.go
@@ -6,24 +6,33 @@ import (
 	"github.com/hashicorp/consul/lib"
 )
 
+func TestMathAbsInt(t *testing.T) {
+	tests := [][3]int{{1, 1}, {-1, 1}, {0, 0}}
+	for _, test := range tests {
+		if test[1] != lib.AbsInt(test[0]) {
+			t.Fatalf("expected %d, got %d", test[1], test[0])
+		}
+	}
+}
+
 func TestMathMaxInt(t *testing.T) {
 	tests := [][3]int{{1, 2, 2}, {-1, 1, 1}, {2, 0, 2}}
-	for i, _ := range tests {
-		expected := tests[i][2]
-		actual := lib.MaxInt(tests[i][0], tests[i][1])
+	for _, test := range tests {
+		expected := test[2]
+		actual := lib.MaxInt(test[0], test[1])
 		if expected != actual {
-			t.Fatalf("in iteration %d expected %d, got %d", i, expected, actual)
+			t.Fatalf("expected %d, got %d", expected, actual)
 		}
 	}
 }
 
 func TestMathMinInt(t *testing.T) {
 	tests := [][3]int{{1, 2, 1}, {-1, 1, -1}, {2, 0, 0}}
-	for i, _ := range tests {
-		expected := tests[i][2]
-		actual := lib.MinInt(tests[i][0], tests[i][1])
+	for _, test := range tests {
+		expected := test[2]
+		actual := lib.MinInt(test[0], test[1])
 		if expected != actual {
-			t.Fatalf("in iteration %d expected %d, got %d", i, expected, actual)
+			t.Fatalf("expected %d, got %d", expected, actual)
 		}
 	}
 }
