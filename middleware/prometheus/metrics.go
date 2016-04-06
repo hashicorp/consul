ad9ddbbbd5b2fdffa622a9d2b5420127223b4e86
Miek Gieben <miek@miek.nl>
2016-04-06 11:34:24 +0000

    smaller time buckets

--- middleware/prometheus/metrics.go
+++ middleware/prometheus/metrics.go
@@ -62,6 +62,7 @@ func define(subsystem string) {
 		Namespace: namespace,
 		Subsystem: subsystem,
 		Name:      "request_duration_seconds",
+		Buckets:   append([]float64{0, 0001, 0.0005, 0.001, 0.0025}, DefBuckets...),
 		Help:      "Histogram of the time (in seconds) each request took.",
 	}, []string{"zone", "qtype"})
 
