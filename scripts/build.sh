e96eedd9e2efe65c837518617a09a919c9ff6788
James Phillips <james@hashicorp.com>
2016-07-19 15:19:32 -0700

    Switches to the short form of the SHA for the build info.

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,7 +15,7 @@ DIR="$( cd -P "$( dirname "$SOURCE" )/.." && pwd )"
 cd "$DIR"
 
 # Get the git commit
-GIT_COMMIT="$(git rev-parse HEAD)"
+GIT_COMMIT="$(git rev-parse --short HEAD)"
 GIT_DIRTY="$(test -n "`git status --porcelain`" && echo "+CHANGES" || true)"
 GIT_DESCRIBE="$(git describe --tags --always)"
 
