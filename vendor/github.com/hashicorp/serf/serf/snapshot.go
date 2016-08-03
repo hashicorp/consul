2ff8c0231daffe1b174b649b0b7420592028980d
kunitake <koichi@kunitake.org>
2016-08-03 11:53:02 +0900

    Added: Trying to recovery from error state like disk-full.

--- vendor/github.com/hashicorp/serf/serf/snapshot.go
+++ vendor/github.com/hashicorp/serf/serf/snapshot.go
@@ -320,7 +320,14 @@ func (s *Snapshotter) appendLine(l string) error {
 
 	n, err := s.buffered.WriteString(l)
 	if err != nil {
-		return err
+		// Try to recovery from error state like disk-full.
+		s.buffered.Reset(s.fh)
+
+		if err := s.buffered.Flush(); err != nil {
+			return err
+		} else {
+			s.lastFlush = time.Now()
+		}
 	}
 
 	// Check if we should flush
