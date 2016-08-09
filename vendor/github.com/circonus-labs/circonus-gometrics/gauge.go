6cb32b9f1f24b39297fdb6dc6afb000c16ce274c
James Phillips <slackpad@gmail.com>
2016-08-09 16:34:48 -0700

    Updates circonus-labs/circonus-gometrics and circonus-labs/circonusllhist.

--- vendor/github.com/circonus-labs/circonus-gometrics/gauge.go
+++ vendor/github.com/circonus-labs/circonus-gometrics/gauge.go
@@ -5,16 +5,20 @@ package circonusgometrics
 // Use a gauge to track metrics which increase and decrease (e.g., amount of
 // free memory).
 
+import (
+	"fmt"
+)
+
 // Gauge sets a gauge to a value
-func (m *CirconusMetrics) Gauge(metric string, val int64) {
+func (m *CirconusMetrics) Gauge(metric string, val interface{}) {
 	m.SetGauge(metric, val)
 }
 
 // SetGauge sets a gauge to a value
-func (m *CirconusMetrics) SetGauge(metric string, val int64) {
+func (m *CirconusMetrics) SetGauge(metric string, val interface{}) {
 	m.gm.Lock()
 	defer m.gm.Unlock()
-	m.gauges[metric] = val
+	m.gauges[metric] = m.gaugeValString(val)
 }
 
 // RemoveGauge removes a gauge
@@ -36,4 +40,38 @@ func (m *CirconusMetrics) RemoveGaugeFunc(metric string) {
 	m.gfm.Lock()
 	defer m.gfm.Unlock()
 	delete(m.gaugeFuncs, metric)
+}
+
+// gaugeValString converts an interface value (of a supported type) to a string
+func (m *CirconusMetrics) gaugeValString(val interface{}) string {
+	vs := ""
+	switch v := val.(type) {
+	default:
+		// ignore it, unsupported type
+	case int:
+		vs = fmt.Sprintf("%d", v)
+	case int8:
+		vs = fmt.Sprintf("%d", v)
+	case int16:
+		vs = fmt.Sprintf("%d", v)
+	case int32:
+		vs = fmt.Sprintf("%d", v)
+	case int64:
+		vs = fmt.Sprintf("%d", v)
+	case uint:
+		vs = fmt.Sprintf("%d", v)
+	case uint8:
+		vs = fmt.Sprintf("%d", v)
+	case uint16:
+		vs = fmt.Sprintf("%d", v)
+	case uint32:
+		vs = fmt.Sprintf("%d", v)
+	case uint64:
+		vs = fmt.Sprintf("%d", v)
+	case float32:
+		vs = fmt.Sprintf("%f", v)
+	case float64:
+		vs = fmt.Sprintf("%f", v)
+	}
+	return vs
 }
