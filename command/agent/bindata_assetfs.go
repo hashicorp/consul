282a4b8a3f70bd3b79ddab5ab589209623e24031
James Phillips <slackpad@gmail.com>
2016-08-09 19:03:10 -0700

    Updates bindata after vendor update.

--- command/agent/bindata_assetfs.go
+++ command/agent/bindata_assetfs.go
@@ -401,8 +401,11 @@ func _filePath(dir, name string) string {
 }
 
 func assetFS() *assetfs.AssetFS {
+	assetInfo := func(path string) (os.FileInfo, error) {
+		return os.Stat(path)
+	}
 	for k := range _bintree.Children {
-		return &assetfs.AssetFS{Asset: Asset, AssetDir: AssetDir, AssetInfo: AssetInfo, Prefix: k}
+		return &assetfs.AssetFS{Asset: Asset, AssetDir: AssetDir, AssetInfo: assetInfo, Prefix: k}
 	}
 	panic("unreachable")
 }
