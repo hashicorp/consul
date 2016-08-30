0558b9c44b35b22c21d37f68c01525b6cc6df990
James Phillips <slackpad@gmail.com>
2016-08-30 00:10:46 -0700

    Fixes a stale comment.

--- api/operator.go
+++ api/operator.go
@@ -58,7 +58,7 @@ func (op *Operator) RaftRemovePeerByAddress(address raft.ServerAddress, q *Write
 	r.setWriteOptions(q)
 
 	// TODO (slackpad) Currently we made address a query parameter. Once
-	// IDs are in place this will be DELETE /v1/raft-peer/<id>.
+	// IDs are in place this will be DELETE /v1/operator/raft/peer/<id>.
 	r.params.Set("address", string(address))
 
 	_, resp, err := requireOK(op.c.doRequest(r))
