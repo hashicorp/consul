99cd11c2d4a32d67d43e821f461e4f408161125e
Miek Gieben <miek@miek.nl>
2016-10-08 18:06:28 +0100

    Add secondary test

---
+++ middleware/secondary/setup_test.go
@@ -0,0 +1,41 @@
+package secondary
+
+import (
+	"testing"
+
+	"github.com/mholt/caddy"
+)
+
+// TODO(miek): this only check the syntax.
+func TestSecondaryParse(t *testing.T) {
+	tests := []struct {
+		inputFileRules string
+		shouldErr      bool
+		transferFrom   string
+	}{
+		{
+			`secondary`,
+			false, // TODO(miek): should actually be true, because without transfer lines this does not make sense
+			"",
+		},
+		{
+			`secondary {
+				transfer from 127.0.0.1
+				transfer to 127.0.0.1
+			}`,
+			false,
+			"",
+		},
+	}
+
+	for i, test := range tests {
+		c := caddy.NewTestController("secondary", test.inputFileRules)
+		_, err := secondaryParse(c)
+
+		if err == nil && test.shouldErr {
+			t.Fatalf("Test %d expected errors, but got no error", i)
+		} else if err != nil && !test.shouldErr {
+			t.Fatalf("Test %d expected no errors, but got '%v'", i, err)
+		}
+	}
+}
