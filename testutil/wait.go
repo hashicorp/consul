1f903547b54e8ad180c4cdeff2344f6054e6bf10
James Phillips <slackpad@gmail.com>
2017-01-26 09:15:29 -0800

    Bumps the wait even more.

--- testutil/wait.go
+++ testutil/wait.go
@@ -11,7 +11,7 @@ type testFn func() (bool, error)
 type errorFn func(error)
 
 func WaitForResult(test testFn, error errorFn) {
-	retries := 300
+	retries := 400
 
 	for retries > 0 {
 		time.Sleep(100 * time.Millisecond)
