06dbeefaad33d5546bc1d811aad52d8a00846113
Seth Vargo <sethvargo@gmail.com>
2017-03-30 18:35:47 -0400

    Move to new branding
    This JUST moves over to the new branding. Once we get this in, I can
    work on making the sidebar links and splitting out the API into its own
    section.

--- website/config.rb
+++ website/config.rb
@@ -11,6 +11,33 @@ activate :hashicorp do |h|
 end
 
 helpers do
+  # Get the title for the page.
+  #
+  # @param [Middleman::Page] page
+  #
+  # @return [String]
+  def title_for(page)
+    if page && page.data.page_title
+      return "#{page.data.page_title} - Consul by HashiCorp"
+    end
+
+     "Consul by HashiCorp"
+   end
+
+  # Get the description for the page
+  #
+  # @param [Middleman::Page] page
+  #
+  # @return [String]
+  def description_for(page)
+    description = (page.data.description || "")
+      .gsub('"', '')
+      .gsub(/\n+/, ' ')
+      .squeeze(' ')
+
+    return escape_html(description)
+  end
+
   # This helps by setting the "active" class for sidebar nav elements
   # if the YAML frontmatter matches the expected value.
   def sidebar_current(expected)
@@ -22,27 +49,47 @@ helpers do
     end
   end
 
-  # Get the title for the page.
-  #
-  # @param [Middleman::Page] page
-  #
+  # Returns the id for this page.
   # @return [String]
-  def title_for(page)
-    if page && page.data.page_title
-      return "#{page.data.page_title} - Consul by HashiCorp"
+  def body_id_for(page)
+    if !(name = page.data.sidebar_current).blank?
+      return "page-#{name.strip}"
     end
-
-    "Consul by HashiCorp"
+    if page.url == "/" || page.url == "/index.html"
+      return "page-home"
+    end
+    if !(title = page.data.page_title).blank?
+      return title
+        .downcase
+        .gsub('"', '')
+        .gsub(/[^\w]+/, '-')
+        .gsub(/_+/, '-')
+        .squeeze('-')
+        .squeeze(' ')
+    end
+    return ""
   end
 
-  # Get the description for the page
-  #
-  # @param [Middleman::Page] page
-  #
+  # Returns the list of classes for this page.
   # @return [String]
-  def description_for(page)
-    description = page.data.description || ""
-    description = description.gsub(/\n+/, " ")
-    return escape_html(description)
+  def body_classes_for(page)
+    classes = []
+
+    if !(layout = page.data.layout).blank?
+      classes << "layout-#{page.data.layout}"
+    end
+
+    if !(title = page.data.page_title).blank?
+      title = title
+        .downcase
+        .gsub('"', '')
+        .gsub(/[^\w]+/, '-')
+        .gsub(/_+/, '-')
+        .squeeze('-')
+        .squeeze(' ')
+      classes << "page-#{title}"
+    end
+
+    return classes.join(" ")
   end
 end
