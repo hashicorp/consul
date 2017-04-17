238c99b1363cc8f29ba2292d2f68e27267020fd8
James Phillips <slackpad@gmail.com>
2017-04-17 11:14:48 -0700

    Bumps download link to 0.8.1.

--- website/config.rb
+++ website/config.rb
@@ -2,7 +2,7 @@ set :base_url, "https://www.consul.io/"
 
 activate :hashicorp do |h|
   h.name        = "consul"
-  h.version     = "0.8.0"
+  h.version     = "0.8.1"
   h.github_slug = "hashicorp/consul"
 end
 
