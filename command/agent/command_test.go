37c180a9207149c2f4583ca4b569d068728657a0
Frank Schroeder <frank.schroeder@gmail.com>
2017-04-26 17:42:04 -0700

    test: Speedup Retry*Join tests

--- command/agent/command_test.go
+++ command/agent/command_test.go
@@ -277,6 +277,7 @@ func TestRetryJoinFail(t *testing.T) {
 		"-data-dir", tmpDir,
 		"-retry-join", serfAddr,
 		"-retry-max", "1",
+		"-retry-interval", "10ms",
 	}
 
 	if code := cmd.Run(args); code == 0 {
@@ -308,6 +309,7 @@ func TestRetryJoinWanFail(t *testing.T) {
 		"-data-dir", tmpDir,
 		"-retry-join-wan", serfAddr,
 		"-retry-max-wan", "1",
+		"-retry-interval-wan", "10ms",
 	}
 
 	if code := cmd.Run(args); code == 0 {
