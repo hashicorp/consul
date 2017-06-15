5d99ee80ca2af8152a832b9a55088afa509aa55b
Kyle Havlovitz <kylehav@gmail.com>
2017-06-15 15:24:04 -0700

    Add an option to disable keyring file (#3145)
    Also disables keyring file in dev mode.

--- vendor/github.com/hashicorp/serf/serf/internal_query.go
+++ vendor/github.com/hashicorp/serf/serf/internal_query.go
@@ -192,10 +192,12 @@ func (s *serfQueries) handleInstallKey(q *Query) {
 		goto SEND
 	}
 
-	if err := s.serf.writeKeyringFile(); err != nil {
-		response.Message = err.Error()
-		s.logger.Printf("[ERR] serf: Failed to write keyring file: %s", err)
-		goto SEND
+	if s.serf.config.KeyringFile != "" {
+		if err := s.serf.writeKeyringFile(); err != nil {
+			response.Message = err.Error()
+			s.logger.Printf("[ERR] serf: Failed to write keyring file: %s", err)
+			goto SEND
+		}
 	}
 
 	response.Result = true
