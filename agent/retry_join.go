aef552bbadbaf5bd9bc681a165e9d4ef603953f1
Frank Schroeder <frank.schroeder@gmail.com>
2017-06-16 23:16:51 +0200

    agent: use generic discover function for retry join

--- agent/retry_join.go
+++ agent/retry_join.go
@@ -2,94 +2,89 @@ package agent
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
-	"github.com/hashicorp/consul/discover/aws"
-	"github.com/hashicorp/consul/discover/azure"
-	"github.com/hashicorp/consul/discover/gce"
+	"github.com/hashicorp/consul/discover"
 )
 
 // RetryJoin is used to handle retrying a join until it succeeds or all
 // retries are exhausted.
 func (a *Agent) retryJoin() {
 	cfg := a.config
-
-	ec2Enabled := cfg.RetryJoinEC2.TagKey != "" && cfg.RetryJoinEC2.TagValue != ""
-	gceEnabled := cfg.RetryJoinGCE.TagValue != ""
-	azureEnabled := cfg.RetryJoinAzure.TagName != "" && cfg.RetryJoinAzure.TagValue != ""
-
-	if len(cfg.RetryJoin) == 0 && !ec2Enabled && !gceEnabled && !azureEnabled {
-		return
-	}
+	awscfg := cfg.RetryJoinEC2
+	azurecfg := cfg.RetryJoinAzure
+	gcecfg := cfg.RetryJoinGCE
 
 	a.logger.Printf("[INFO] agent: Joining cluster...")
-	attempt := 0
+	attempts := cfg.RetryMaxAttempts
 	for {
-		var servers []string
-		var err error
+		var args []string
 		switch {
-		case ec2Enabled:
-			c := cfg.RetryJoinEC2
-			awscfg := &aws.Config{
-				Region:          c.Region,
-				TagKey:          c.TagKey,
-				TagValue:        c.TagValue,
-				AccessKeyID:     c.AccessKeyID,
-				SecretAccessKey: c.SecretAccessKey,
+		case awscfg.TagKey != "" && awscfg.TagValue != "":
+			args = []string{
+				"provider=aws",
+				"region=" + awscfg.Region,
+				"tag_key=" + awscfg.TagKey,
+				"tag_value=" + awscfg.TagValue,
+				"access_key_id=" + awscfg.AccessKeyID,
+				"secret_access_key=" + awscfg.SecretAccessKey,
 			}
-			servers, err = aws.Discover(awscfg, a.logger)
-			if err != nil {
-				a.logger.Printf("[ERR] agent: Unable to query EC2 instances: %s", err)
-			}
-			a.logger.Printf("[INFO] agent: Discovered %d servers from EC2", len(servers))
 
-		case gceEnabled:
-			c := cfg.RetryJoinGCE
-			gcecfg := &gce.Config{
-				ProjectName:     c.ProjectName,
-				ZonePattern:     c.ZonePattern,
-				TagValue:        c.TagValue,
-				CredentialsFile: c.CredentialsFile,
+		case gcecfg.TagValue != "":
+			args = []string{
+				"provider=gce",
+				"project_name=" + gcecfg.ProjectName,
+				"zone_pattern=" + gcecfg.ZonePattern,
+				"tag_value=" + gcecfg.TagValue,
+				"credentials_file=" + gcecfg.CredentialsFile,
 			}
-			servers, err = gce.Discover(gcecfg, a.logger)
-			if err != nil {
-				a.logger.Printf("[ERR] agent: Unable to query GCE instances: %s", err)
+
+		case azurecfg.TagName != "" && azurecfg.TagValue != "":
+			args = []string{
+				"provider=azure",
+				"tag_name=" + azurecfg.TagName,
+				"tag_value=" + azurecfg.TagValue,
+				"tenant_id=" + azurecfg.TenantID,
+				"client_id=" + azurecfg.ClientID,
+				"subscription_id=" + azurecfg.SubscriptionID,
+				"secret_access_key=" + azurecfg.SecretAccessKey,
 			}
-			a.logger.Printf("[INFO] agent: Discovered %d servers from GCE", len(servers))
-		case azureEnabled:
-			c := cfg.RetryJoinAzure
-			azurecfg := &azure.Config{
-				TagName:         c.TagName,
-				TagValue:        c.TagValue,
-				SubscriptionID:  c.SubscriptionID,
-				TenantID:        c.TenantID,
-				ClientID:        c.ClientID,
-				SecretAccessKey: c.SecretAccessKey,
-			}
-			servers, err = azure.Discover(azurecfg, a.logger)
-			if err != nil {
-				a.logger.Printf("[ERR] agent: Unable to query Azure instances: %s", err)
-			}
-			a.logger.Printf("[INFO] agent: Discovered %d servers from Azure", len(servers))
 		}
+
+		// do not retry join
+		if len(cfg.RetryJoin) == 0 && len(args) == 0 {
+			return
+		}
+
+		var n int
+		var err error
+		var servers []string
+
+		discovered, err := discover.Discover(strings.Join(args, " "), a.logger)
+		if err != nil {
+			goto Retry
+		}
+		servers = discovered
 
 		servers = append(servers, cfg.RetryJoin...)
 		if len(servers) == 0 {
 			err = fmt.Errorf("No servers to join")
-		} else {
-			n, err := a.JoinLAN(servers)
-			if err == nil {
-				a.logger.Printf("[INFO] agent: Join completed. Synced with %d initial agents", n)
-				return
-			}
+			goto Retry
 		}
 
-		attempt++
-		if cfg.RetryMaxAttempts > 0 && attempt > cfg.RetryMaxAttempts {
-			a.retryJoinCh <- fmt.Errorf("agent: max join retry exhausted, exiting")
+		n, err = a.JoinLAN(servers)
+		if err == nil {
+			a.logger.Printf("[INFO] agent: Join completed. Synced with %d initial agents", n)
 			return
 		}
 
+	Retry:
+		attempts--
+		if attempts <= 0 {
+			a.retryJoinCh <- fmt.Errorf("agent: max join retry exhausted, exiting")
+			return
+		}
 		a.logger.Printf("[WARN] agent: Join failed: %v, retrying in %v", err, cfg.RetryInterval)
 		time.Sleep(cfg.RetryInterval)
 	}
