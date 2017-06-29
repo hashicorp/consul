37bd20e37ab6db61e08d876cca0621e36a38e41b
Frank Schroeder <frank.schroeder@gmail.com>
2017-06-29 15:43:50 +0200

    agent: fix failing test from localState decoupling
    Would be nice to know what changed though.

--- api/catalog_test.go
+++ api/catalog_test.go
@@ -49,7 +49,7 @@ func TestAPI_CatalogNodes(t *testing.T) {
 					"wan": "127.0.0.1",
 				},
 				Meta:        map[string]string{},
-				CreateIndex: meta.LastIndex - 1,
+				CreateIndex: meta.LastIndex,
 				ModifyIndex: meta.LastIndex,
 			},
 		}
