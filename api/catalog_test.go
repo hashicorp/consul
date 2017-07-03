2ab4002ec1d180a6017978c0ff6253b4b9a8ccd2
Frank Schroeder <frank.schroeder@gmail.com>
2017-07-03 09:30:46 +0200

    api: fix TestAPI_CatalogNodes test

--- api/catalog_test.go
+++ api/catalog_test.go
@@ -49,7 +49,7 @@ func TestAPI_CatalogNodes(t *testing.T) {
 					"wan": "127.0.0.1",
 				},
 				Meta:        map[string]string{},
-				CreateIndex: meta.LastIndex,
+				CreateIndex: meta.LastIndex - 1,
 				ModifyIndex: meta.LastIndex,
 			},
 		}
