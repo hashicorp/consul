454b3a2a618f973600df72b55d80dca99d2f2ab8
Preetha Appan <preetha@hashicorp.com>
2017-08-04 10:36:41 -0500

    Pick up raft library change that fsyncs snapshot files correctly

--- vendor/github.com/hashicorp/raft/file_snapshot.go
+++ vendor/github.com/hashicorp/raft/file_snapshot.go
@@ -37,10 +37,11 @@ type snapMetaSlice []*fileSnapshotMeta
 
 // FileSnapshotSink implements SnapshotSink with a file.
 type FileSnapshotSink struct {
-	store  *FileSnapshotStore
-	logger *log.Logger
-	dir    string
-	meta   fileSnapshotMeta
+	store     *FileSnapshotStore
+	logger    *log.Logger
+	dir       string
+	parentDir string
+	meta      fileSnapshotMeta
 
 	stateFile *os.File
 	stateHash hash.Hash64
@@ -158,9 +159,10 @@ func (f *FileSnapshotStore) Create(version SnapshotVersion, index, term uint64,
 
 	// Create the sink
 	sink := &FileSnapshotSink{
-		store:  f,
-		logger: f.logger,
-		dir:    path,
+		store:     f,
+		logger:    f.logger,
+		dir:       path,
+		parentDir: f.path,
 		meta: fileSnapshotMeta{
 			SnapshotMeta: SnapshotMeta{
 				Version:            version,
@@ -404,6 +406,19 @@ func (s *FileSnapshotSink) Close() error {
 		return err
 	}
 
+	// fsync the parent directory, to sync directory edits to disk
+	parentFH, err := os.Open(s.parentDir)
+	defer parentFH.Close()
+	if err != nil {
+		s.logger.Printf("[ERR] snapshot: Failed to open snapshot parent directory %v, error: %v", s.parentDir, err)
+		return err
+	}
+
+	if err = parentFH.Sync(); err != nil {
+		s.logger.Printf("[ERR] snapshot: Failed syncing parent directory %v, error: %v", s.parentDir, err)
+		return err
+	}
+
 	// Reap any old snapshots
 	if err := s.store.ReapSnapshots(); err != nil {
 		return err
@@ -434,6 +449,11 @@ func (s *FileSnapshotSink) Cancel() error {
 func (s *FileSnapshotSink) finalize() error {
 	// Flush any remaining data
 	if err := s.buffered.Flush(); err != nil {
+		return err
+	}
+
+	// Sync to force fsync to disk
+	if err := s.stateFile.Sync(); err != nil {
 		return err
 	}
 
@@ -468,13 +488,21 @@ func (s *FileSnapshotSink) writeMeta() error {
 
 	// Buffer the file IO
 	buffered := bufio.NewWriter(fh)
-	defer buffered.Flush()
 
 	// Write out as JSON
 	enc := json.NewEncoder(buffered)
 	if err := enc.Encode(&s.meta); err != nil {
 		return err
 	}
+
+	if err = buffered.Flush(); err != nil {
+		return err
+	}
+
+	if err = fh.Sync(); err != nil {
+		return err
+	}
+
 	return nil
 }
 
