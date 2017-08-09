e1231bce4d2ed12f8ceb7bd73e27bcc72ea3b5b2
James Phillips <slackpad@gmail.com>
2017-08-08 22:47:24 -0700

    Bumps download version to 0.9.1.

--- website/config.rb
+++ website/config.rb
@@ -2,7 +2,7 @@ set :base_url, "https://www.consul.io/"
 
 activate :hashicorp do |h|
   h.name        = "consul"
-  h.version     = "0.9.0"
+  h.version     = "0.9.1"
   h.github_slug = "hashicorp/consul"
 end
 
