f8261330f003c60567983e0d7bd513735885c116
Kyle Havlovitz <kylehav@gmail.com>
2017-09-01 10:18:43 -0700

    Remove duplicate error message

--- command/segment_stub.go
+++ command/segment_stub.go
@@ -2,17 +2,14 @@
 
 package command
 
-import (
-	consulapi "github.com/hashicorp/consul/api"
-	"fmt"
-)
+import consulapi "github.com/hashicorp/consul/api"
 
 // getSegmentMembers returns an empty list since network segments are not
 // supported in OSS Consul.
 func getSegmentMembers(client *consulapi.Client) ([]*consulapi.AgentMember, error) {
 	members, err := client.Agent().MembersOpts(consulapi.MembersOpts{})
 	if err != nil {
-		return nil, fmt.Errorf("Error retrieving members: %s", err)
+		return nil, err
 	}
 
 	return members, nil
