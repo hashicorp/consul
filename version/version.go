8bc8c25cfb029a849678aa003d1ccb682476f675
James Phillips <slackpad@gmail.com>
2017-09-25 13:47:45 -0700

    Tweaks some versions around 1.0.0.

--- version/version.go
+++ version/version.go
@@ -15,7 +15,7 @@ var (
 	//
 	// Version must conform to the format expected by github.com/hashicorp/go-version
 	// for tests to work.
-	Version = "0.9.4"
+	Version = "1.0.0"
 
 	// A pre-release marker for the version. If this is "" (empty string)
 	// then it means that it is a final release. Otherwise, this is a pre-release
