9362cbcbc29531d35fba6ecd9d03154eae9c2620
Frank Schroeder <frank.schroeder@gmail.com>
2017-09-25 03:12:30 +0200

    Add support to discover public v4 and v6 addresses on AWS (#3471)
    Update github.com/hashicorp/go-discover/provider/aws to support the
    'addr_type' option which allows detection of private_v4, public_v4 and
    public_v6 addresses.
    
    Fixes #3471

--- vendor/github.com/hashicorp/go-discover/provider/aws/aws_discover.go
+++ vendor/github.com/hashicorp/go-discover/provider/aws/aws_discover.go
@@ -23,6 +23,7 @@ func (p *Provider) Help() string {
     region:            The AWS region. Default to region of instance.
     tag_key:           The tag key to filter on
     tag_value:         The tag value to filter on
+    addr_type:         "private_v4", "public_v4" or "public_v6". Defaults to "private_v4".
     access_key_id:     The AWS access key to use
     secret_access_key: The AWS secret access key to use
 
@@ -43,10 +44,21 @@ func (p *Provider) Addrs(args map[string]string, l *log.Logger) ([]string, error
 	region := args["region"]
 	tagKey := args["tag_key"]
 	tagValue := args["tag_value"]
+	addrType := args["addr_type"]
 	accessKey := args["access_key_id"]
 	secretKey := args["secret_access_key"]
 
-	log.Printf("[DEBUG] discover-aws: Using region=%s tag_key=%s tag_value=%s", region, tagKey, tagValue)
+	if addrType != "private_v4" && addrType != "public_v4" && addrType != "public_v6" {
+		l.Printf("[INFO] discover-aws: Address type %s is not supported. Valid values are {private_v4,public_v4,public_v6}. Falling back to 'private_v4'", addrType)
+		addrType = "private_v4"
+	}
+
+	if addrType == "" {
+		l.Printf("[DEBUG] discover-aws: Address type not provided. Using 'private_v4'")
+		addrType = "private_v4"
+	}
+
+	log.Printf("[DEBUG] discover-aws: Using region=%s tag_key=%s tag_value=%s addr_type=%s", region, tagKey, tagValue, addrType)
 	if accessKey == "" && secretKey == "" {
 		log.Printf("[DEBUG] discover-aws: No static credentials")
 		log.Printf("[DEBUG] discover-aws: Using environment variables, shared credentials or instance role")
@@ -89,6 +101,10 @@ func (p *Provider) Addrs(args map[string]string, l *log.Logger) ([]string, error
 				Name:   aws.String("tag:" + tagKey),
 				Values: []*string{aws.String(tagValue)},
 			},
+			&ec2.Filter{
+				Name:   aws.String("instance-state-name"),
+				Values: []*string{aws.String("running")},
+			},
 		},
 	})
 	if err != nil {
@@ -103,14 +119,42 @@ func (p *Provider) Addrs(args map[string]string, l *log.Logger) ([]string, error
 			id := *inst.InstanceId
 			l.Printf("[DEBUG] discover-aws: Found instance %s", id)
 
-			// Terminated instances don't have the PrivateIpAddress field
-			if inst.PrivateIpAddress == nil {
-				l.Printf("[DEBUG] discover-aws: Instance %s has no private ip", id)
-				continue
-			}
+			switch addrType {
+			case "public_v6":
+				l.Printf("[DEBUG] discover-aws: Instance %s has %d network interfaces", id, len(inst.NetworkInterfaces))
 
-			l.Printf("[INFO] discover-aws: Instance %s has private ip %s", id, *inst.PrivateIpAddress)
-			addrs = append(addrs, *inst.PrivateIpAddress)
+				for _, networkinterface := range inst.NetworkInterfaces {
+					l.Printf("[DEBUG] discover-aws: Checking NetworInterfaceId %s on Instance %s", *networkinterface.NetworkInterfaceId, id)
+					// Check if instance got any ipv6
+					if networkinterface.Ipv6Addresses == nil {
+						l.Printf("[DEBUG] discover-aws: Instance %s has no IPv6 on NetworkInterfaceId %s", id, *networkinterface.NetworkInterfaceId)
+						continue
+					}
+					for _, ipv6address := range networkinterface.Ipv6Addresses {
+						l.Printf("[INFO] discover-aws: Instance %s has IPv6 %s on NetworkInterfaceId %s", id, *ipv6address.Ipv6Address, *networkinterface.NetworkInterfaceId)
+						addrs = append(addrs, *ipv6address.Ipv6Address)
+					}
+				}
+
+			case "public_v4":
+				if inst.PublicIpAddress == nil {
+					l.Printf("[DEBUG] discover-aws: Instance %s has no public IPv4", id)
+					continue
+				}
+
+				l.Printf("[INFO] discover-aws: Instance %s has public ip %s", id, *inst.PublicIpAddress)
+				addrs = append(addrs, *inst.PublicIpAddress)
+
+			default:
+				// EC2-Classic don't have the PrivateIpAddress field
+				if inst.PrivateIpAddress == nil {
+					l.Printf("[DEBUG] discover-aws: Instance %s has no private ip", id)
+					continue
+				}
+
+				l.Printf("[INFO] discover-aws: Instance %s has private ip %s", id, *inst.PrivateIpAddress)
+				addrs = append(addrs, *inst.PrivateIpAddress)
+			}
 		}
 	}
 
