12c2d71aee51a911611336196882900c4f6b0d41
James Phillips <slackpad@gmail.com>
2017-10-04 16:08:26 -0700

    Adds a clarifying comment.

--- agent/util_windows.go
+++ agent/util_windows.go
@@ -9,7 +9,7 @@ import (
 	"syscall"
 )
 
-// ExecScript returns a command to execute a script
+// ExecScript returns a command to execute a script through a shell.
 func ExecScript(script string) (*exec.Cmd, error) {
 	shell := "cmd"
 	if other := os.Getenv("SHELL"); other != "" {
