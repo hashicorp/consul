6780964eb8e45885d70993439e4d72ec06b45d4d
Frank Schroeder <frank.schroeder@gmail.com>
2017-10-11 10:16:21 +0200

    config: remove redundant code

--- agent/config/segments_oss_test.go
+++ agent/config/segments_oss_test.go
@@ -3,7 +3,6 @@
 package config
 
 import (
-	"net"
 	"os"
 	"testing"
 
@@ -23,9 +22,6 @@ func TestSegments(t *testing.T) {
 			json: []string{`{ "server": true, "segment": "a" }`},
 			hcl:  []string{` server = true segment = "a" `},
 			err:  `Network segments are not supported in this version of Consul`,
-			privatev4: func() ([]*net.IPAddr, error) {
-				return []*net.IPAddr{ipAddr("10.0.0.1")}, nil
-			},
 		},
 		{
 			desc: "segment port must be set",
@@ -44,9 +40,6 @@ func TestSegments(t *testing.T) {
 			json: []string{`{ "segments":[{ "name":"x", "port": 123 }] }`},
 			hcl:  []string{`segments = [{ name = "x" port = 123 }]`},
 			err:  `Network segments are not supported in this version of Consul`,
-			privatev4: func() ([]*net.IPAddr, error) {
-				return []*net.IPAddr{ipAddr("10.0.0.1")}, nil
-			},
 		},
 	}
 
