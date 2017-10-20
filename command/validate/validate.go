ecee15b4668e33762d36850b74e4d462bb39b917
James Phillips <slackpad@gmail.com>
2017-10-19 18:59:05 -0700

    Updates documentation for consul validate.
    This makes it clear that you need to pass the full configuration,
    and that the command won't work with config fragments.
    
    Closes #3591

--- command/validate/validate.go
+++ command/validate/validate.go
@@ -67,11 +67,15 @@ const synopsis = "Validate config files/directories"
 const help = `
 Usage: consul validate [options] FILE_OR_DIRECTORY...
 
-  Performs a basic sanity test on Consul configuration files. For each file
-  or directory given, the validate command will attempt to parse the
-  contents just as the "consul agent" command would, and catch any errors.
+  Performs a thorough sanity test on Consul configuration files. For each file
+  or directory given, the validate command will attempt to parse the contents
+  just as the "consul agent" command would, and catch any errors.
+
   This is useful to do a test of the configuration only, without actually
-  starting the agent.
+  starting the agent. This performs all of the validation the agent would, so
+  this should be given the complete set of configuration files that are going
+  to be loaded by the agent. This command cannot operate on partial
+  configuration fragments since those won't pass the full agent validation.
 
   Returns 0 if the configuration is valid, or 1 if there are problems.
 `
