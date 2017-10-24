d57ef823f9892d9c9e821f21571f04be1ba4057a
Frank Schroeder <frank.schroeder@gmail.com>
2017-10-24 14:39:26 +0200

    watch: convert TestChecksWatch_State to use channels

--- watch/funcs_test.go
+++ watch/funcs_test.go
@@ -346,28 +346,30 @@ func TestChecksWatch_State(t *testing.T) {
 	a := agent.NewTestAgent(t.Name(), ``)
 	defer a.Shutdown()
 
+	invoke := make(chan error)
 	plan := mustParse(t, `{"type":"checks", "state":"warning"}`)
-	invoke := 0
 	plan.Handler = func(idx uint64, raw interface{}) {
-		if invoke == 0 {
-			if raw == nil {
-				return
-			}
-			v, ok := raw.([]*consulapi.HealthCheck)
-			if len(v) == 0 {
-				return
-			}
-			if !ok || v[0].CheckID != "foobar" {
-				t.Fatalf("Bad: %#v", raw)
-			}
-			invoke++
+		if raw == nil {
+			return // ignore
 		}
+		v, ok := raw.([]*consulapi.HealthCheck)
+		if !ok || len(v) == 0 {
+			return // ignore
+		}
+		if v[0].CheckID != "foobar" || v[0].Status != "warning" {
+			invoke <- errBadContent
+			return
+		}
+		invoke <- nil
 	}
 
+	var wg sync.WaitGroup
+	wg.Add(1)
 	go func() {
-		time.Sleep(20 * time.Millisecond)
-
+		defer wg.Done()
 		catalog := a.Client().Catalog()
+
+		time.Sleep(20 * time.Millisecond)
 		reg := &consulapi.CatalogRegistration{
 			Node:       "foobar",
 			Address:    "1.1.1.1",
@@ -379,27 +381,25 @@ func TestChecksWatch_State(t *testing.T) {
 				Status:  consulapi.HealthWarning,
 			},
 		}
-		catalog.Register(reg, nil)
-
-		time.Sleep(20 * time.Millisecond)
-		plan.Stop()
-
-		dereg := &consulapi.CatalogDeregistration{
-			Node:       "foobar",
-			Address:    "1.1.1.1",
-			Datacenter: "dc1",
+		if _, err := catalog.Register(reg, nil); err != nil {
+			t.Fatalf("err: %v", err)
 		}
-		catalog.Deregister(dereg, nil)
 	}()
 
-	err := plan.Run(a.HTTPAddr())
-	if err != nil {
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		if err := plan.Run(a.HTTPAddr()); err != nil {
+			t.Fatalf("err: %v", err)
+		}
+	}()
+
+	if err := <-invoke; err != nil {
 		t.Fatalf("err: %v", err)
 	}
 
-	if invoke == 0 {
-		t.Fatalf("bad: %v", invoke)
-	}
+	plan.Stop()
+	wg.Wait()
 }
 
 func TestChecksWatch_Service(t *testing.T) {
