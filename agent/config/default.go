2892f91d0bfb95d2e52e6fda8f6859ab527aef9d
James Phillips <slackpad@gmail.com>
2017-12-13 10:31:45 -0800

    Copies the autopilot settings from the runtime config.
    Fixes #3730

--- agent/config/default.go
+++ agent/config/default.go
@@ -54,6 +54,12 @@ func DefaultSource() Source {
 		syslog_facility = "LOCAL0"
 		tls_min_version = "tls10"
 
+		autopilot = {
+			cleanup_dead_servers = true
+			last_contact_threshold = "200ms"
+			max_trailing_logs = 250
+			server_stabilization_time = "10s"
+		}
 		dns_config = {
 			allow_stale = true
 			udp_answer_limit = 3
