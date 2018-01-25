ea95a2003a710b9057d7500bdca38f6394896294
Miek Gieben <miek@miek.nl>
2018-01-25 19:45:35 +0000

    Release 1.0.5

--- coremain/version.go
+++ coremain/version.go
@@ -1,9 +1,7 @@
 package coremain
 
 const (
-	coreName = "CoreDNS"
-	// CoreVersion is the current version of CoreDNS.
+	coreName    = "CoreDNS"
 	CoreVersion = "1.0.5"
-
-	serverType = "dns"
+	serverType  = "dns"
 )
