f80cebaef340ffa35513bbc1c49a88647247192b
Kyle Havlovitz <kylehav@gmail.com>
2018-02-07 12:50:12 -0800

    Bump download version to 1.0.5

--- website/config.rb
+++ website/config.rb
@@ -2,7 +2,7 @@ set :base_url, "https://www.consul.io/"
 
 activate :hashicorp do |h|
   h.name        = "consul"
-  h.version     = "1.0.3"
+  h.version     = "1.0.5"
   h.github_slug = "hashicorp/consul"
 end
 
