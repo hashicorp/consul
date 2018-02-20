a8d374510466e44e519adf48e3340c28d38ac9e5
Pierre Souchay <p.souchay@criteo.com>
2018-02-20 23:57:28 +0100

    Fixed comments for function maxIndexForService

--- agent/consul/state/catalog.go
+++ agent/consul/state/catalog.go
@@ -760,8 +760,9 @@ func (s *Store) ServicesByNodeMeta(ws memdb.WatchSet, filters map[string]string)
 }
 
 // maxIndexForService return the maximum Raft Index for a service
-// If the index is not set for the service, it will return the max
-// Raft Index of "nodes", "services"
+// If the index is not set for the service, it will return:
+// - maxIndex(nodes, services) if checks if false
+// - maxIndex(nodes, services, checks) if checks if false
 func maxIndexForService(tx *memdb.Txn, serviceName string, checks bool) (uint64, error) {
 	transaction, err := tx.First("index", "id", serviceIndexName(serviceName))
 	if err == nil {
