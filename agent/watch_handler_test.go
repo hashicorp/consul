b6667616585064f0bf587d372c884e78d52d5092
Paul Banks <banks@banksco.de>
2018-02-22 14:06:06 +0000

    Fix test running in non-bash shells

--- agent/watch_handler_test.go
+++ agent/watch_handler_test.go
@@ -15,7 +15,7 @@ func TestMakeWatchHandler(t *testing.T) {
 	t.Parallel()
 	defer os.Remove("handler_out")
 	defer os.Remove("handler_index_out")
-	script := "echo $CONSUL_INDEX >> handler_index_out && cat >> handler_out"
+	script := "bash -c 'echo $CONSUL_INDEX >> handler_index_out && cat >> handler_out'"
 	handler := makeWatchHandler(os.Stderr, script)
 	handler(100, []string{"foo", "bar", "baz"})
 	raw, err := ioutil.ReadFile("handler_out")
