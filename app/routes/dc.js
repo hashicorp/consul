84ec93adfb8bddbc14e042bcd1f8e19e1a1644b3
John Cowen <jcowen@hashicorp.com>
2018-03-02 11:07:28 +0000

    Back out of workflow for the minute, concentrate on one thing at a time

--- app/routes/dc.js
+++ app/routes/dc.js
@@ -1,20 +1,18 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { hash } from 'rsvp';
 
 export default Route.extend({
-  workflow: service('workflow'),
   repo: service('dc'),
   nodeRepo: service('nodes'),
   model: function(params) {
     const repo = this.get('repo');
     const nodeRepo = this.get('nodeRepo');
-    return this.get('workflow').execute(() => {
-      return {
-        item: params.dc,
-        items: repo.findAll(),
-        nodes: nodeRepo.findAllByDatacenter(params.dc),
-        coordinates: nodeRepo.findAllCoordinatesByDatacenter(params.dc),
-      };
+    return hash({
+      item: params.dc,
+      items: repo.findAll(),
+      nodes: nodeRepo.findAllByDatacenter(params.dc),
+      coordinates: nodeRepo.findAllCoordinatesByDatacenter(params.dc),
     });
   },
   setupController: function(controller, model) {
