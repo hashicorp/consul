d40d8b70ea1952f456f6353ef673c287eebbae17
John Cowen <jcowen@hashicorp.com>
2018-03-06 10:47:29 +0000

    Delete files I'm not using right now

--- app/services/ajax/dc.js
+++
@@ -1,8 +0,0 @@
-import Service from '@ember/service';
-
-import get from 'consul-ui/utils/request/get';
-export default Service.extend({
-  findAll: function() {
-    return get('/v1/catalog/datacenters');
-  },
-});
