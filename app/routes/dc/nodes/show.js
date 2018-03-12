da5d54fd512dd870520ffdf2ee2539b0e0adfd09
John Cowen <jcowen@hashicorp.com>
2018-03-12 10:41:36 +0000

    Make sure sessions are shown

--- app/routes/dc/nodes/show.js
+++ app/routes/dc/nodes/show.js
@@ -28,9 +28,11 @@ export default Route.extend({
       // Load the sessions for the node
       // jc: This was in afterModel, I think the only for which was
       // that the model needed resolving first to get to Node
-      return assign(model, {
-        sessions: sessionRepo.findByNode(model.model.get('Node'), model.dc),
-      });
+      return hash(
+        assign({}, model, {
+          sessions: sessionRepo.findByNode(model.model.get('Node'), model.dc),
+        })
+      );
     });
   },
   setupController: function(controller, model) {
