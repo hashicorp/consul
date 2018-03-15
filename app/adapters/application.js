c32ecb184543e2616412ab427e2f092d89926c04
John Cowen <jcowen@hashicorp.com>
2018-03-15 18:08:36 +0000

    Hoist createURL

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,9 @@
 import Adapter from 'ember-data/adapters/rest';
 import { inject as service } from '@ember/service';
 import { assign } from '@ember/polyfills';
+const createURL = function(encoded, raw, encode = encodeURIComponent) {
+  return encoded.concat(raw.map(encode)).join('/');
+};
 export default Adapter.extend({
   namespace: 'v1',
   repo: service('settings'),
@@ -8,6 +11,6 @@ export default Adapter.extend({
     return assign({}, this.get('repo').findHeaders(), this._super(...arguments));
   },
   appendURL: function(path, parts = []) {
-    return [this.buildURL(), path].concat(parts.map(encodeURIComponent)).join('/');
+    return createURL([this.buildURL(), path], parts);
   },
 });
