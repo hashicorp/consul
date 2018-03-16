caa483d62459ce10937bf8b3fdba3b7827048ab4
John Cowen <jcowen@hashicorp.com>
2018-03-16 14:22:43 +0000

    Take out some comments

--- app/serializers/service.js
+++ app/serializers/service.js
@@ -1,26 +1,4 @@
 import Serializer from './application';
 export default Serializer.extend({
   primaryKey: 'Id',
-  // normalizeQueryResponse: function(store, primaryModelClass, payload, id, requestType) {
-  //   return this._super(
-  //     store,
-  //     primaryModelClass,
-  //     {
-  //       [primaryModelClass.modelName]: payload,
-  //     },
-  //     id,
-  //     requestType
-  //   );
-  // },
-  // normalizeQueryRecordResponse: function(store, primaryModelClass, payload, id, requestType) {
-  //   return this._super(
-  //     store,
-  //     primaryModelClass,
-  //     {
-  //       [primaryModelClass.modelName]: payload,
-  //     },
-  //     id,
-  //     requestType
-  //   );
-  // },
 });
