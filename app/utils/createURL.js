f11e520bbe95931f8a68336c99cfdeeb2696cb2c
John Cowen <jcowen@hashicorp.com>
2018-03-19 14:01:20 +0000

    Encode query string values

--- app/utils/createURL.js
+++ app/utils/createURL.js
@@ -4,7 +4,7 @@ export default function(encoded, raw, query = {}, encode = encodeURIComponent) {
     Object.keys(query)
       .map(function(key, i, arr) {
         if (query[key] != null) {
-          return `${key}=${query[key]}`;
+          return `${key}=${encode(query[key])}`;
         }
         return key;
       })
