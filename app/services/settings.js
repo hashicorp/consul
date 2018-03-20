d21097c7731acae3ad6ab4e6056ebb5f87820110
John Cowen <jcowen@hashicorp.com>
2018-03-20 15:46:22 +0000

    Move settings to use the same service/route API as the rest of the app

--- app/services/settings.js
+++ app/services/settings.js
@@ -1,16 +1,22 @@
 import Service from '@ember/service';
+import Promise from 'rsvp';
 
 export default Service.extend({
   // TODO: change name
+  storage: window.localStorage,
   findHeaders: function() {
+    // TODO: if possible this should be a promise
     return {
-      'X-Consul-Token': this.get('token'),
+      'X-Consul-Token': this.get('storage').getItem('token'),
     };
   },
-  get: function(key) {
-    return window.localStorage.getItem(key);
+  findAll: function(key) {
+    return Promise.resolve({ token: this.get('storage').getItem('token') });
   },
-  set: function(key, value) {
-    return window.localStorage.setItem(key, value);
+  persist: function(obj) {
+    return Promise.resolve(this.get('storage').setItem('token', obj.token));
+  },
+  delete: function(obj) {
+    return Promise.resolve(this.get('storage').removeItem('token'));
   },
 });
