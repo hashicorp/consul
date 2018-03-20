a765184889213fe1b0e7af24cc5a6e6ee853a83b
John Cowen <jcowen@hashicorp.com>
2018-03-20 11:40:57 +0000

    Breaking test for DATACENTER_KEY usage

--- tests/unit/adapters/kv-test.js
+++ tests/unit/adapters/kv-test.js
@@ -6,7 +6,21 @@ module('Unit | Adapter | kv', function(hooks) {
 
   // Replace this with your real tests.
   test('it exists', function(assert) {
-    let adapter = this.owner.lookup('adapter:kv');
+    const adapter = this.owner.lookup('adapter:kv');
     assert.ok(adapter);
+  });
+  test('handleResponse returns a Kv-like object when the request is a createRecord', function(assert) {
+    const adapter = this.owner.lookup('adapter:kv');
+    // unflake, this is also going through _super and
+    // using urlForCreateRecord and makeAttrable so not the unit
+    const expected = 'key/name';
+    const actual = adapter.handleResponse(200, {}, true, {
+      method: 'GET',
+      url: `/v1/kv/${expected}`,
+    });
+    assert.deepEqual(actual, {
+      Key: expected,
+      Datacenter: '',
+    });
   });
 });
