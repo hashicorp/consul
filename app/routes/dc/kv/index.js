0fdc5608912baf8d0774370371d5d2ba890f7b0f
John Cowen <jcowen@hashicorp.com>
2018-03-21 13:06:32 +0000

    Move away from using `reopen` to using Mixins

--- app/routes/dc/kv/index.js
+++ app/routes/dc/kv/index.js
@@ -1,6 +1,7 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({
+import WithKeyUtils from 'consul-ui/mixins/with-key-utils';
+export default Route.extend(WithKeyUtils, {
   beforeModel: function() {
     this.transitionTo('dc.kv.show', this.rootKey);
   },
