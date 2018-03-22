e3c9d5f433750d1e8a220d23f1b13e6dcfcf0242
John Cowen <jcowen@hashicorp.com>
2018-03-22 16:27:46 +0000

    Start stripping back the HTML to semantics

--- app/components/filtered-list.js
+++ app/components/filtered-list.js
@@ -2,6 +2,7 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 
 export default Component.extend({
+  // tagName: 'tbody',
   filter: '',
   filtered: computed('filter', 'items.@each', function() {
     const filter = this.get('filter');
---
+++ tests/integration/components/datacenter-list-test.js
@@ -0,0 +1,34 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('datacenter-list', 'Integration | Component | datacenter list', {
+  integration: true,
+});
+
+test('it renders', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{datacenter-list}}`);
+
+  assert.equal(
+    this.$()
+      .text()
+      .trim(),
+    ''
+  );
+
+  // Template block usage:
+  this.render(hbs`
+    {{#datacenter-list}}
+      template block text
+    {{/datacenter-list}}
+  `);
+
+  assert.equal(
+    this.$()
+      .text()
+      .trim(),
+    'template block text'
+  );
+});
