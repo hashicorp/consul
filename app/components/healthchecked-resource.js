efebb8028e620da8ce21540f2b0919019c397872
John Cowen <jcowen@hashicorp.com>
2018-04-06 15:30:59 +0100

    Stick with `checks` instead of healthy/unhealthy

--- app/components/healthchecked-resource.js
+++ app/components/healthchecked-resource.js
@@ -5,6 +5,14 @@ export default Component.extend({
   classNames: ['healthchecked-resource'],
   attributeBindings: ['style'],
   style: style('gridRowEnd'),
+  unhealthy: computed.filter(`checks.@each.Status`, function(item) {
+    const status = get(item, 'Status');
+    return status === 'critical' || status === 'warning';
+  }),
+  healthy: computed.filter(`checks.@each.Status`, function(item) {
+    const status = get(item, 'Status');
+    return status === 'passing';
+  }),
   gridRowEnd: computed('UnhealthyChecks', function() {
     return {
       gridRow: `auto / span ${4 + (get(this, 'unhealthy.length') || 0)}`
