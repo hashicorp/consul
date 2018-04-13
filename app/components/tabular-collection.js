dd97e54c24795cba22fc2eedbd19f7c45a943c75
John Cowen <jcowen@hashicorp.com>
2018-04-13 15:29:11 +0100

    Add fix for ember-collection

--- app/components/tabular-collection.js
+++ app/components/tabular-collection.js
@@ -1,5 +1,16 @@
 import Component from 'ember-collection/components/ember-collection';
+import needsRevalidate from 'ember-collection/utils/needs-revalidate';
 
 export default Component.extend({
   tagName: '',
+  _needsRevalidate: function() {
+    if (this.isDestroyed || this.isDestroying) {
+      return;
+    }
+    if (this._isGlimmer2()) {
+      this.rerender();
+    } else {
+      needsRevalidate(this);
+    }
+  },
 });
