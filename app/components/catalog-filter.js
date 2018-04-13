486630cc0c72b0f52312115e70eb475feb07adcd
John Cowen <jcowen@hashicorp.com>
2018-04-13 10:15:42 +0100

    Start adding test attributes

--- app/components/catalog-filter.js
+++ app/components/catalog-filter.js
@@ -3,5 +3,6 @@ import Component from '@ember/component';
 export default Component.extend({
   tagName: 'form',
   classNames: ['filter-bar'],
-  onchange: function(){}
+  'data-test-catalog-filter': true,
+  onchange: function() {},
 });
--- app/mixins/with-health-filtering.js
+++ app/mixins/with-health-filtering.js
@@ -11,14 +11,20 @@ const countStatus = function(items, status) {
   const key = `Checks${ucfirst(status)}`;
   return items.reduce(function(prev, item, i, arr) {
     const num = get(item, key);
-    return prev + (typeof num !== 'undefined' ? num : get(item, 'Checks').filter(
-        function(item) {
-          return item.Status === status;
-        }
-      ).length) || 0;
+    return (
+      prev +
+        (typeof num !== 'undefined'
+          ? num
+          : get(item, 'Checks').filter(function(item) {
+              return item.Status === status;
+            }).length) || 0
+    );
   }, 0);
 };
 export default Mixin.create(WithFiltering, {
+  filters: {
+    status: '',
+  },
   healthFilters: computed('items', function() {
     const items = get(this, 'items');
     return ['', 'passing', 'warning', 'critical'].map(function(item) {
