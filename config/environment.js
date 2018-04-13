aab2f66ce910592c98660d821e46b5bedca092cc
John Cowen <jcowen@hashicorp.com>
2018-04-13 15:28:36 +0100

    Add rest/spread babel plugin, remove mirage for now

--- config/environment.js
+++ config/environment.js
@@ -33,9 +33,9 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
-    ENV['ember-cli-mirage'] = {
-      enabled: false,
-    };
+    // ENV['ember-cli-mirage'] = {
+    //   enabled: false,
+    // };
   }
 
   if (environment === 'test') {
