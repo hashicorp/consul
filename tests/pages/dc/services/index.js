447a04beb8cfb1cf57e13c7a08b166917345e8b8
John Cowen <jcowen@hashicorp.com>
2018-04-13 14:20:32 +0100

    Add a page.. page object

--- tests/pages/dc/services/index.js
+++ tests/pages/dc/services/index.js
@@ -1,5 +1,6 @@
 import { create, visitable, collection, text } from 'ember-cli-page-object';
 
+import page from 'consul-ui/tests/pages/components/page';
 import filter from 'consul-ui/tests/pages/components/catalog-filter';
 
 export default create({
@@ -8,6 +9,7 @@ export default create({
     name: text('a'),
   }),
   dcs: collection('[data-test-datacenter-picker]'),
+  navigation: page.navigation,
 
   filter: filter,
 });
