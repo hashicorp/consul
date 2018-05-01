1723f58f049759f7851d23cf225f3bde1f47a7c7
John Cowen <jcowen@hashicorp.com>
2018-05-01 11:53:09 +0100

    Find a way to reliably set the class of html from the view

--- ui-v2/app/components/app-view.js
+++ ui-v2/app/components/app-view.js
@@ -1,6 +1,16 @@
 import Component from '@ember/component';
 import SlotsMixin from 'ember-block-slots';
-
+import { get } from '@ember/object';
+const $html = document.documentElement;
+const templatize = function(arr = []) {
+  return arr.map(item => `template-${item}`);
+};
 export default Component.extend(SlotsMixin, {
   classNames: ['app-view'],
+  didInsertElement: function() {
+    $html.classList.add(...templatize(get(this, 'class').split(' ')));
+  },
+  didDestroyElement: function() {
+    $html.classList.remove(...templatize(get(this, 'class').split(' ')));
+  },
 });
