292ea381403a7cc8f5ec0844795ad342cf00bfba
John Cowen <jcowen@hashicorp.com>
2018-05-04 14:40:24 +0100

    Unit test some more utils

--- ui-v2/app/utils/promisedTimeout.js
+++ ui-v2/app/utils/promisedTimeout.js
@@ -1,5 +1,8 @@
 export default function(P = Promise, timeout = setTimeout) {
+  // var interval;
   return function(milliseconds, cb) {
+    // clearInterval(interval);
+    // const cb = typeof _cb !== 'function' ? (i) => { clearInterval(interval);interval = i; } : _cb;
     return new P((resolve, reject) => {
       cb(
         timeout(function() {
