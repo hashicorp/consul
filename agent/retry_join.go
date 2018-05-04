accb85a6a9388d8145d1640bdd3a74322e495b36
Seth Vargo <seth@sethvargo.com>
2018-05-04 12:11:12 -0700

    Use new discover and useragent libs

--- agent/retry_join.go
+++ agent/retry_join.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/hashicorp/consul/lib"
 	discover "github.com/hashicorp/go-discover"
 )
 
@@ -67,7 +68,11 @@ func (r *retryJoiner) retryJoin() error {
 		return nil
 	}
 
-	disco := discover.Discover{}
+	disco, err := discover.New(discover.WithUserAgent(lib.UserAgent()))
+	if err != nil {
+		return err
+	}
+
 	r.logger.Printf("[INFO] agent: Retry join %s is supported for: %s", r.cluster, strings.Join(disco.Names(), " "))
 	r.logger.Printf("[INFO] agent: Joining %s cluster...", r.cluster)
 	attempt := 0
