53fbe2b1119d8d12938e547d7bce21d2eea30589
Matt Keeler <mjkeeler7@gmail.com>
2018-05-31 17:20:16 -0400

    Update unit tests to reflect change to func signature

--- watch/plan_test.go
+++ watch/plan_test.go
@@ -47,7 +47,7 @@ func TestRun_Stop(t *testing.T) {
 
 	errCh := make(chan error, 1)
 	go func() {
-		errCh <- plan.Run("127.0.0.1:8500")
+		errCh <- plan.Run("127.0.0.1:8500", nil)
 	}()
 
 	select {
