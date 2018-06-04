e62139f76ac6eed540ce173b1e438dc197dd4a63
John Cowen <jcowen@hashicorp.com>
2018-06-04 15:47:51 +0100

    Add a couple more unit tests for the utils folder

--- ui-v2/tests/unit/utils/get-component-factory-test.js
+++ ui-v2/tests/unit/utils/get-component-factory-test.js
@@ -3,8 +3,46 @@ import { module, test } from 'qunit';
 
 module('Unit | Utility | get component factory');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = getComponentFactory({ lookup: function() {} });
-  assert.ok(result);
+test("it uses lookup to locate the instance of the component based on the DOM element's id", function(assert) {
+  const expected = 'name';
+  let getComponent = getComponentFactory({
+    lookup: function() {
+      return { id: expected };
+    },
+  });
+  assert.equal(typeof getComponent, 'function', 'returns a function');
+  const actual = getComponent({
+    getAttribute: function(name) {
+      return 'id';
+    },
+  });
+  assert.equal(actual, expected, 'performs a lookup based on the id');
+});
+test("it returns null if it can't find it", function(assert) {
+  const expected = null;
+  let getComponent = getComponentFactory({
+    lookup: function() {
+      return { id: '' };
+    },
+  });
+  const actual = getComponent({
+    getAttribute: function(name) {
+      return 'non-existent';
+    },
+  });
+  assert.equal(actual, expected);
+});
+test('it returns null if there is no id', function(assert) {
+  const expected = null;
+  let getComponent = getComponentFactory({
+    lookup: function() {
+      return { id: '' };
+    },
+  });
+  const actual = getComponent({
+    getAttribute: function(name) {
+      return;
+    },
+  });
+  assert.equal(actual, expected);
 });
--- ui-v2/tests/unit/utils/qsa-factory-test.js
+++ ui-v2/tests/unit/utils/qsa-factory-test.js
@@ -3,8 +3,32 @@ import { module, test } from 'qunit';
 
 module('Unit | Utility | qsa factory');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = qsaFactory();
-  assert.ok(result);
+test('querySelectorAll is called on `document` when called with document', function(assert) {
+  assert.expect(2);
+  const expected = 'html';
+  const $$ = qsaFactory({
+    querySelectorAll: function(sel) {
+      assert.equal(sel, expected);
+      return true;
+    },
+  });
+  assert.ok($$(expected));
+});
+test('querySelectorAll is called on `context` when called with context', function(assert) {
+  assert.expect(2);
+  const expected = 'html';
+  const context = {
+    querySelectorAll: function(sel) {
+      assert.equal(sel, expected);
+      return true;
+    },
+  };
+  const $$ = qsaFactory({
+    // this should never be called
+    querySelectorAll: function(sel) {
+      assert.equal(sel, expected);
+      return false;
+    },
+  });
+  assert.ok($$(expected, context));
 });
