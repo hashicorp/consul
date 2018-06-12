ee3a32d77197e2682ad736d9a1a117b49a26beb7
John Cowen <jcowen@hashicorp.com>
2018-06-12 14:09:47 +0100

    More detailed error messages for duplicate intentions

--- ui-v2/app/mixins/intention/with-actions.js
+++ ui-v2/app/mixins/intention/with-actions.js
@@ -2,6 +2,7 @@ import Mixin from '@ember/object/mixin';
 import { get } from '@ember/object';
 import WithFeedback from 'consul-ui/mixins/with-feedback';
 
+import { INTERNAL_SERVER_ERROR as HTTP_INTERNAL_SERVER_ERROR } from 'consul-ui/utils/http/status';
 export default Mixin.create(WithFeedback, {
   actions: {
     create: function(item) {
@@ -14,7 +15,17 @@ export default Mixin.create(WithFeedback, {
             });
         },
         `Your intention has been added.`,
-        `There was an error adding your intention.`
+        function(e) {
+          if (e.errors && e.errors[0]) {
+            const error = e.errors[0];
+            if (parseInt(error.status) === HTTP_INTERNAL_SERVER_ERROR) {
+              if (error.detail.indexOf('duplicate intention found:') === 0) {
+                return `An intention already exists for this Source-Destination pair. Please enter a different combination of Services, or search the intentions to edit an existing intention.`;
+              }
+            }
+          }
+          return `There was an error adding your intention.`;
+        }
       );
     },
     update: function(item) {
--- ui-v2/app/services/feedback.js
+++ ui-v2/app/services/feedback.js
@@ -1,35 +1,43 @@
 import Service, { inject as service } from '@ember/service';
 import { get, set } from '@ember/object';
+import callableType from 'consul-ui/utils/callable-type';
 
 export default Service.extend({
   notify: service('flashMessages'),
   logger: service('logger'),
   execute: function(handle, success, error, controller) {
     set(controller, 'isLoading', true);
+    const displaySuccess = callableType(success);
+    const displayError = callableType(error);
     const notify = get(this, 'notify');
-    return handle()
-      .then(() => {
-        notify.add({
-          type: 'success',
-          message: success,
-        });
-      })
-      .catch(e => {
-        get(this, 'logger').execute(e);
-        if (e.name === 'TransitionAborted') {
+    return (
+      handle()
+        //TODO: pass this through to display success..
+        .then(() => {
           notify.add({
             type: 'success',
-            message: success,
+            // here..
+            message: displaySuccess(),
           });
-        } else {
-          notify.add({
-            type: 'error',
-            message: error,
-          });
-        }
-      })
-      .finally(function() {
-        set(controller, 'isLoading', false);
-      });
+        })
+        .catch(e => {
+          get(this, 'logger').execute(e);
+          if (e.name === 'TransitionAborted') {
+            notify.add({
+              type: 'success',
+              // and here
+              message: displaySuccess(),
+            });
+          } else {
+            notify.add({
+              type: 'error',
+              message: displayError(e),
+            });
+          }
+        })
+        .finally(function() {
+          set(controller, 'isLoading', false);
+        })
+    );
   },
 });
