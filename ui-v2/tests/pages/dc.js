2e293f269766f16b5193e85e4002c8c9de050fbd
John Cowen <jcowen@hashicorp.com>
2018-06-21 12:44:32 +0100

    Add a beforeModel hook at dc/index to auto transition to services

--- ui-v2/tests/pages/dc.js
+++ ui-v2/tests/pages/dc.js
@@ -1,7 +1,7 @@
 import { create, visitable, attribute, collection, clickable } from 'ember-cli-page-object';
 
 export default create({
-  visit: visitable('/:dc/services/'),
+  visit: visitable('/:dc/'),
   dcs: collection('[data-test-datacenter-picker]'),
   showDatacenters: clickable('[data-test-datacenter-selected]'),
   selectedDc: attribute('data-test-datacenter-selected', '[data-test-datacenter-selected]'),
