79edfad571c04d0a3b99655545defa006d3dbe90
RJ Spiker <spiker.rj@gmail.com>
2018-06-26 15:20:18 -0600

    website: update home hero js to work better with turbolinks

--- website/source/assets/javascripts/consul-connect/home-hero.js
+++ website/source/assets/javascripts/consul-connect/home-hero.js
@@ -4,11 +4,11 @@ var qsa = document.querySelectorAll.bind(document)
 var $$wrappers = qsa('#home-hero .videos > div') // all the wrappers for the videos
 var $$videos = qsa('#home-hero video') // all the videos
 var $$videoControls = qsa('#home-hero .controls > div') // carousel controllers
-var currentIndex = 0 // currently playing video
+var currentIndex = 1 // currently playing video
 var playbackRate = 2 // video playback speed
 
 // initiate a video change
-function initialiateVideoChange(index) {
+function initiateVideoChange(index) {
   var wrapper = $$wrappers[currentIndex]
   var nextWrapper = $$wrappers[index]
 
@@ -68,27 +68,42 @@ function playVideo(index, wrapper) {
   currentIndex = index
 }
 
-// loop through videos to set up options/listeners
-for (var i = 0; i < $$videos.length; i++) {
-  // set video default speed
-  $$videos[i].playbackRate = playbackRate
+function initiateVideos() {
+  // remove 'active' from wrappers which may be 
+  // there on page load because of turbolinks
+  for (var i = 0; i < $$wrappers.length; i++) {
+    $$wrappers[i].classList.remove('active')
+  }
 
-  // listen for video ending, then go to the next video
-  $$videos[i].addEventListener('ended', function() {
-    var nextIndex = currentIndex + 1
-    initialiateVideoChange(nextIndex < $$videos.length ? nextIndex : 0)
-  })
+  // loop through videos to set up options/listeners
+  for (var i = 0; i < $$videos.length; i++) {
+    // set video default speed
+    $$videos[i].playbackRate = playbackRate
+
+    // listen for video ending, then go to the next video
+    $$videos[i].addEventListener('ended', function() {
+      var nextIndex = currentIndex + 1
+      initiateVideoChange(nextIndex < $$videos.length ? nextIndex : 0)
+    })
+  }
+
+  for (var i = 0; i < $$videoControls.length; i++) {
+    // remove 'playing' from controls which may be 
+    // there on page load because of turbolinks
+    $$videoControls[i].classList.remove('playing')
+
+    // listen for control clicks and initiate videos appropriately
+    $$videoControls[i].addEventListener('click', function() {
+      if (!this.classList.contains('playing')) {
+        initiateVideoChange(this.dataset.index)
+      }
+    })
+  }
+
+  // go to first video to start this thing
+  if ($$videos.length > 0) {
+    initiateVideoChange(0)
+  }
 }
 
-for (var i = 0; i < $$videoControls.length; i++) {
-  $$videoControls[i].addEventListener('click', function() {
-    if (!this.classList.contains('playing')) {
-      initialiateVideoChange(this.dataset.index)
-    }
-  })
-}
-
-// go to first video to start this thing
-if ($$videos.length > 0) {
-  initialiateVideoChange(0)
-}
+document.addEventListener('turbolinks:load', initiateVideos)
