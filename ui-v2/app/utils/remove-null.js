c01fb37c46c08411c500e237b4c3f59d3b448133
John Cowen <jcowen@hashicorp.com>
2018-07-06 13:09:23 +0100

    Don't clone prev, there's no need

--- ui-v2/app/utils/remove-null.js
+++ ui-v2/app/utils/remove-null.js
@@ -2,7 +2,7 @@ export default function(obj) {
   // non-recursive for the moment
   return Object.keys(obj).reduce(function(prev, item, i, arr) {
     if (obj[item] !== null) {
-      return { ...prev, ...{ [item]: obj[item] } };
+      prev[item] = obj[item];
     }
     return prev;
   }, {});
