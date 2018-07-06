6cecf2961d4b9dc5dbdc23e5f9d4e13750259ab9
MagnumOpus21 <sivaauturic@gmail.com>
2018-07-05 22:04:29 -0400

    Agent/Proxy : Properly passes env variables to child

--- agent/proxy/manager.go
+++ agent/proxy/manager.go
@@ -432,7 +432,7 @@ func (m *Manager) newProxy(mp *local.ManagedProxy) (Proxy, error) {
 		if err := m.configureLogDir(id, &cmd); err != nil {
 			return nil, fmt.Errorf("error configuring proxy logs: %s", err)
 		}
-
+		cmd.Env = os.Environ()
 		// Build the daemon structure
 		proxy.Command = &cmd
 		proxy.ProxyID = id
