41c28719077168c1307db37a0ce5a71cc6eb72bf
Miek Gieben <miek@miek.nl>
2018-07-07 08:30:57 +0100

    plugin/test: Fix documentation (#1948)
    Fix documentation and touch up plugin/forward/README.md
    
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- plugin/test/responsewriter.go
+++ plugin/test/responsewriter.go
@@ -10,10 +10,10 @@ import (
 // remote will always be 10.240.0.1 and port 40212. The local address is always 127.0.0.1 and
 // port 53.
 type ResponseWriter struct {
-	TCP bool
+	TCP bool // if TCP is true we return an TCP connection instead of an UDP one.
 }
 
-// LocalAddr returns the local address, always 127.0.0.1:53 (UDP).
+// LocalAddr returns the local address, 127.0.0.1:53 (UDP, TCP if t.TCP is true).
 func (t *ResponseWriter) LocalAddr() net.Addr {
 	ip := net.ParseIP("127.0.0.1")
 	port := 53
@@ -23,7 +23,7 @@ func (t *ResponseWriter) LocalAddr() net.Addr {
 	return &net.UDPAddr{IP: ip, Port: port, Zone: ""}
 }
 
-// RemoteAddr returns the remote address, always 10.240.0.1:40212 (UDP).
+// RemoteAddr returns the remote address, always 10.240.0.1:40212 (UDP, TCP is t.TCP is true).
 func (t *ResponseWriter) RemoteAddr() net.Addr {
 	ip := net.ParseIP("10.240.0.1")
 	port := 40212
@@ -52,13 +52,12 @@ func (t *ResponseWriter) TsigTimersOnly(bool) { return }
 func (t *ResponseWriter) Hijack() { return }
 
 // ResponseWriter6 returns fixed client and remote address in IPv6.  The remote
-// address is always fe80::42:ff:feca:4c65 and port 40212. The local address
-// is always ::1 and port 53.
+// address is always fe80::42:ff:feca:4c65 and port 40212. The local address is always ::1 and port 53.
 type ResponseWriter6 struct {
 	ResponseWriter
 }
 
-// LocalAddr returns the local address, always ::1, port 53 (UDP).
+// LocalAddr returns the local address, always ::1, port 53 (UDP, TCP is t.TCP is true).
 func (t *ResponseWriter6) LocalAddr() net.Addr {
 	if t.TCP {
 		return &net.TCPAddr{IP: net.ParseIP("::1"), Port: 53, Zone: ""}
@@ -66,7 +65,7 @@ func (t *ResponseWriter6) LocalAddr() net.Addr {
 	return &net.UDPAddr{IP: net.ParseIP("::1"), Port: 53, Zone: ""}
 }
 
-// RemoteAddr returns the remote address, always fe80::42:ff:feca:4c65 port 40212 (UDP).
+// RemoteAddr returns the remote address, always fe80::42:ff:feca:4c65 port 40212 (UDP, TCP is t.TCP is true).
 func (t *ResponseWriter6) RemoteAddr() net.Addr {
 	if t.TCP {
 		return &net.TCPAddr{IP: net.ParseIP("fe80::42:ff:feca:4c65"), Port: 40212, Zone: ""}
