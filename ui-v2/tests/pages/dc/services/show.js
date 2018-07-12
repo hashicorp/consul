40a270a4928079dc6548923bf14905b798c5ad52
John Cowen <jcowen@hashicorp.com>
2018-07-12 13:35:33 +0100

    Acceptance tests to search/look for service id's

--- ui-v2/tests/pages/dc/services/show.js
+++ ui-v2/tests/pages/dc/services/show.js
@@ -7,10 +7,12 @@ export default function(visitable, attribute, collection, text, filter) {
     healthy: collection('[data-test-healthy] [data-test-node]', {
       name: attribute('data-test-node'),
       address: text('header strong'),
+      id: text('header em'),
     }),
     unhealthy: collection('[data-test-unhealthy] [data-test-node]', {
       name: attribute('data-test-node'),
       address: text('header strong'),
+      id: text('header em'),
     }),
     filter: filter,
   };
