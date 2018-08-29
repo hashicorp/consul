1fd8d57fa3b2e0abb3956ffdf069b29efb2597f9
John Cowen <jcowen@hashicorp.com>
2018-08-29 16:08:58 +0100

    UI: Bugfix. Move to a different TextEncoder/Decoder
    1. The previously used TextEncoder/Decoder (used as a polyfill for
    browsers that don't have a native version) didn't expose an encoder via
    CommonJS. Use a different polyfill that exposes both a decoder and an
    encoder.
    2. The feature detection itself was flawed. This does a less error prone
    detection that ensures native encoding/decoding where available and polyfilled
    encoding/decoding where not available.

--- ui-v2/app/utils/atob.js
+++ ui-v2/app/utils/atob.js
@@ -1,8 +1,8 @@
-import TextEncoderLite from 'npm:text-encoder-lite';
+import TextEncoding from 'npm:text-encoding';
 import base64js from 'npm:base64-js';
 export default function(str, encoding = 'utf-8') {
   // str = String(str).trim();
   //decode
   const bytes = base64js.toByteArray(str);
-  return new (TextDecoder || TextEncoderLite)(encoding).decode(bytes);
+  return new ('TextDecoder' in window ? TextDecoder : TextEncoding.TextDecoder)(encoding).decode(bytes);
 }
