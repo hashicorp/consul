bfa5d0842a2ff2df3f34274665eaeff63524d29c
John Cowen <johncowen@users.noreply.github.com>
2018-08-29 19:11:19 +0100

    ui: Further simplify/refactor the actions/notification layer (#4573)
    1. Move the 'with-feedback' actions to a 'with-blocking-action' mixin
    which better describes what it does
    2. Additional set of unit tests almost over the entire layer to prove
    things work/add confidence for further changes
    
    The multiple 'with-action' mixins used for every 'index/edit' combo are
    now reduced down to only contain the functionality related to their
    specific routes, i.e. where to redirect.
    
    The actual functionality to block and carry out the action and then
    notify are 'almost' split out so that their respective classes/objects do
    one thing and one thing 'well'.
    
    Mixins are chosen for the moment as the decoration approach used by
    mixins feels better than multiple levels of inheritence, but I would
    like to take this fuether in the future to a 'compositional' based
    approach.
    
    There is still possible further work to be done here, but I'm a lot
    happier now this is reduced down into separate parts.

--- ui-v2/app/mixins/intention/with-actions.js
+++ ui-v2/app/mixins/intention/with-actions.js
@@ -1,62 +1,17 @@
 import Mixin from '@ember/object/mixin';
-import { get } from '@ember/object';
-import WithFeedback from 'consul-ui/mixins/with-feedback';
+import WithBlockingActions from 'consul-ui/mixins/with-blocking-actions';
 
 import { INTERNAL_SERVER_ERROR as HTTP_INTERNAL_SERVER_ERROR } from 'consul-ui/utils/http/status';
-export default Mixin.create(WithFeedback, {
-  actions: {
-    create: function(item) {
-      get(this, 'feedback').execute(
-        () => {
-          return get(this, 'repo')
-            .persist(item)
-            .then(item => {
-              return this.transitionTo('dc.intentions');
-            });
-        },
-        'create',
-        function(type, e) {
-          if (e.errors && e.errors[0]) {
-            const error = e.errors[0];
-            if (parseInt(error.status) === HTTP_INTERNAL_SERVER_ERROR) {
-              if (error.detail.indexOf('duplicate intention found:') === 0) {
-                return 'exists';
-              }
-            }
-          }
-          return type;
+export default Mixin.create(WithBlockingActions, {
+  errorCreate: function(type, e) {
+    if (e.errors && e.errors[0]) {
+      const error = e.errors[0];
+      if (parseInt(error.status) === HTTP_INTERNAL_SERVER_ERROR) {
+        if (error.detail.indexOf('duplicate intention found:') === 0) {
+          return 'exists';
         }
-      );
-    },
-    update: function(item) {
-      get(this, 'feedback').execute(() => {
-        return get(this, 'repo')
-          .persist(item)
-          .then(() => {
-            return this.transitionTo('dc.intentions');
-          });
-      }, 'update');
-    },
-    delete: function(item) {
-      get(this, 'feedback').execute(() => {
-        return (
-          get(this, 'repo')
-            // ember-changeset doesn't support `get`
-            // and `data` returns an object not a model
-            .remove(item)
-            .then(() => {
-              switch (this.routeName) {
-                case 'dc.intentions.index':
-                  return this.refresh();
-                default:
-                  return this.transitionTo('dc.intentions');
-              }
-            })
-        );
-      }, 'delete');
-    },
-    cancel: function(item) {
-      this.transitionTo('dc.intentions');
-    },
+      }
+    }
+    return type;
   },
 });
