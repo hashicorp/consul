89b709f05347d52ca88e60a8c99ae2abe4abac71
Pierre Souchay <p.souchay@criteo.com>
2018-09-09 18:12:08 +0200

    Fixed unstable unit test TestCatalog_ListServices_Stale

--- agent/consul/catalog_endpoint_test.go
+++ agent/consul/catalog_endpoint_test.go
@@ -1472,6 +1472,7 @@ func TestCatalog_ListServices_Stale(t *testing.T) {
 	defer os.RemoveAll(dir1)
 	defer s1.Shutdown()
 
+	testrpc.WaitForTestAgent(t, s1.RPC, "dc1")
 	dir2, s2 := testServerWithConfig(t, func(c *Config) {
 		c.ACLDatacenter = "dc1" // Enable ACLs!
 		c.Bootstrap = false     // Disable bootstrap
