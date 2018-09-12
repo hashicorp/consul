2b953441ab92b6bc448164d9a69cd27283292439
John Cowen <jcowen@hashicorp.com>
2018-09-12 18:18:07 +0100

    ui: Comment cleanup

--- ui-v2/app/utils/atob.js
+++ ui-v2/app/utils/atob.js
@@ -1,8 +1,9 @@
 import TextEncoding from 'npm:text-encoding';
 import base64js from 'npm:base64-js';
 export default function(str, encoding = 'utf-8') {
-  // str = String(str).trim();
-  //decode
+  // decode
   const bytes = base64js.toByteArray(str);
-  return new ('TextDecoder' in window ? TextDecoder : TextEncoding.TextDecoder)(encoding).decode(bytes);
+  return new ('TextDecoder' in window ? TextDecoder : TextEncoding.TextDecoder)(encoding).decode(
+    bytes
+  );
 }
