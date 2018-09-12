e5f300dd219f9a77cca909ac661824165a2a671d
John Cowen <johncowen@users.noreply.github.com>
2018-09-12 20:15:58 +0100

    UI: Bugfix. Move to a different TextEncoder/Decoder (#4613)
    1. The previously used TextEncoder/Decoder (used as a polyfill for
    browsers that don't have a native version) didn't expose an encoder via
    CommonJS. Use a different polyfill that exposes both a decoder and an
    encoder.
    2. The feature detection itself was flawed. This does a less error prone
    detection that ensures native encoding/decoding where available and polyfilled
    encoding/decoding where not available.

--- ui-v2/app/utils/btoa.js
+++ ui-v2/app/utils/btoa.js
@@ -1,7 +1,7 @@
-import TextEncoderLite from 'npm:text-encoder-lite';
+import TextEncoding from 'npm:text-encoding';
 import base64js from 'npm:base64-js';
 export default function(str, encoding = 'utf-8') {
   // encode
-  const bytes = new (TextEncoder || TextEncoderLite)(encoding).encode(str);
+  const bytes = new ('TextEncoder' in window ? TextEncoder : TextEncoding.TextEncoder)(encoding).encode(str);
   return base64js.fromByteArray(bytes);
 }
