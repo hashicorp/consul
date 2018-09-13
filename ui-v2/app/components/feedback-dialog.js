4611a152132306ec98878d5b30f911a8326ee6d2
John Cowen <jcowen@hashicorp.com>
2018-09-13 16:00:00 +0100

    ui: WIP tooltips and feedback-dialogs are the same thing - merge

--- ui-v2/app/components/feedback-dialog.js
+++ ui-v2/app/components/feedback-dialog.js
@@ -1,6 +1,8 @@
 import Component from '@ember/component';
 import { get, set } from '@ember/object';
 import { inject as service } from '@ember/service';
+import qsaFactory from 'consul-ui/utils/qsa-factory';
+const $$ = qsaFactory();
 
 import SlotsMixin from 'ember-block-slots';
 const STATE_READY = 'ready';
@@ -8,8 +10,9 @@ const STATE_SUCCESS = 'success';
 const STATE_ERROR = 'error';
 export default Component.extend(SlotsMixin, {
   wait: service('timeout'),
-  interval: null,
   classNames: ['with-feedback'],
+  transition: '',
+  transitionClassName: 'feedback-dialog-out',
   state: STATE_READY,
   permanent: true,
   init: function() {
@@ -17,26 +20,30 @@ export default Component.extend(SlotsMixin, {
     this.success = this._success.bind(this);
     this.error = this._error.bind(this);
   },
-  _success: function() {
-    set(this, 'state', STATE_SUCCESS);
-    get(this, 'wait')
-      .execute(3000, interval => {
-        clearInterval(get(this, 'interval'));
-        set(this, 'interval', interval);
+  applyTransition: function() {
+    const wait = get(this, 'wait').execute;
+    const className = get(this, 'transitionClassName');
+    wait(0)
+      .then(() => {
+        set(this, 'transition', className);
+        return wait(0);
       })
       .then(() => {
+        return new Promise(resolve => {
+          $$(`.${className}`, this.element)[0].addEventListener('transitionend', resolve);
+        });
+      })
+      .then(() => {
+        set(this, 'transition', '');
         set(this, 'state', STATE_READY);
       });
   },
+  _success: function() {
+    set(this, 'state', STATE_SUCCESS);
+    this.applyTransition();
+  },
   _error: function() {
     set(this, 'state', STATE_ERROR);
-    get(this, 'wait')
-      .execute(3000, interval => {
-        clearInterval(get(this, 'interval'));
-        set(this, 'interval', interval);
-      })
-      .then(() => {
-        set(this, 'state', STATE_READY);
-      });
+    this.applyTransition();
   },
 });
