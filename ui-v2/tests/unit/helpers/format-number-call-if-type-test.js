b688835f1026a1c916353cb9efacdbdc5115eaee
John Cowen <jcowen@hashicorp.com>
2018-09-20 13:32:07 +0100

    ui: Add some documentation for what the test names refer to.
    The callIfType 'helper util' is likely to be moved elsewhere,
    especially as the possibility of there being more 'helper utils' is
    likely.
    
    Once that's decided on, I'll likely expand on the documentation for
    'helper util'

--- ui-v2/tests/unit/helpers/format-number-call-if-type-test.js
+++ ui-v2/tests/unit/helpers/format-number-call-if-type-test.js
@@ -5,9 +5,17 @@ module('Unit | helper | format-number.callIfType');
 const formatNumber = callIfType('number')(function() {
   return true;
 });
-test('it returns the argument passed if not a number', function(assert) {
+/*
+ * This util is meant for usage with helpers only, therefore would usually be called with
+ * {{ format-number "1,000" }}
+ * The first argument in the helper here is eventually converted to the first element
+ * of an array by ember (the typical helper function signature)
+ * So below, test names refer to 'arguments' to mean 'helper arguments'
+ * i.e. {{helper-name "argument-one", "argument-two"}}
+*/
+test('it returns the helper argument passed if not a number', function(assert) {
   assert.equal(formatNumber(['1,000']), '1,000');
 });
-test("it returns the argument to locale string if it's a number", function(assert) {
+test("it returns the helper argument to locale string if it's a number", function(assert) {
   assert.ok(formatNumber([1000]));
 });
