b89b02632a10ddd145ba1f983d249ed7920aa60d
John Cowen <johncowen@users.noreply.github.com>
2018-09-21 10:04:45 +0100

    ui: format-number helper accepts a hash of toLocaleString options (#4703)

--- ui-v2/app/helpers/format-number.js
+++ ui-v2/app/helpers/format-number.js
@@ -2,8 +2,10 @@ import { helper } from '@ember/component/helper';
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toLocaleString
 // the actual helper
-const toLocaleString = function(num) {
-  return num.toLocaleString();
+const toLocaleString = function(num, options) {
+  // TODO: If I make locale configurable use an option
+  // not mutiple arguments
+  return num.toLocaleString(undefined, options);
 };
 // wrap this up to help with testing the unit
 export function callIfType(type) {
@@ -12,7 +14,7 @@ export function callIfType(type) {
       if (typeof params[0] !== type) {
         return params[0];
       }
-      return cb(params[0]);
+      return cb(params[0], hash);
     };
   };
 }
