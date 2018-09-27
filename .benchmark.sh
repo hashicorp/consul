1c95ede2acdf95babfa603818b8a20e3cebc12ad
Brad Beam <bradbeam@users.noreply.github.com>
2018-09-27 10:41:02 -0500

    Fixing benchmark code block (#2141)

--- .benchmark.sh
+++ .benchmark.sh
@@ -5,7 +5,7 @@ set -e +o pipefail
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     echo -e "NOTE: The CPU benchmarks are performed on Travis VMs and vary widly between runs," > .benchmark.body
     echo -e " you can't trust them. The memory benchmarks are OK\n\n" >> .benchmark.body
-    awk '/^benchmark old/ { printf "%s\n%s\n", "```", $0 };
+    awk '/^benchmark.*old/ { printf "%s\n%s\n", "```", $0 };
          /^$/ { print "```" };
          /^Bench/ { print $0 };
          END{ print "```" }' .benchmark.log >> .benchmark.body
