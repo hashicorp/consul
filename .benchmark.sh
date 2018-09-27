f9526742af0f052d54a0d274f3a9d7bcbbdc7a08
Brad Beam <bradbeam@users.noreply.github.com>
2018-09-27 10:05:05 -0500

    Add code block in benchcmp output (#2140)

--- .benchmark.sh
+++ .benchmark.sh
@@ -5,7 +5,10 @@ set -e +o pipefail
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     echo -e "NOTE: The CPU benchmarks are performed on Travis VMs and vary widly between runs," > .benchmark.body
     echo -e " you can't trust them. The memory benchmarks are OK\n\n" >> .benchmark.body
-    cat .benchmark.log >> .benchmark.body
+    awk '/^benchmark old/ { printf "%s\n%s\n", "```", $0 };
+         /^$/ { print "```" };
+         /^Bench/ { print $0 };
+         END{ print "```" }' .benchmark.log >> .benchmark.body
     jq -n --arg body "$(cat .benchmark.body)" '{body: $body}' > .benchmark.json
     curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
         --data-binary "@.benchmark.json" \
