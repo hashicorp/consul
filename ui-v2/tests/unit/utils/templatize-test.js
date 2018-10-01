42f06cba3115755437bb63169bf2672438a3d2c8
John Cowen <jcowen@hashicorp.com>
2018-10-01 14:43:39 +0100

    ui: ACLv2 add templatize tests

--- ui-v2/tests/unit/utils/templatize-test.js
+++ ui-v2/tests/unit/utils/templatize-test.js
@@ -3,8 +3,18 @@ import { module, test } from 'qunit';
 
 module('Unit | Utility | templatize');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = templatize();
-  assert.ok(result);
+test('it prefixes the word template to every string in the array', function(assert) {
+  const expected = ['template-one', 'template-two'];
+  const actual = templatize(['one', 'two']);
+  assert.deepEqual(actual, expected);
+});
+test('it returns an empty array when passed an empty array', function(assert) {
+  const expected = [];
+  const actual = templatize([]);
+  assert.deepEqual(actual, expected);
+});
+test('it returns an empty array when passed nothing', function(assert) {
+  const expected = [];
+  const actual = templatize();
+  assert.deepEqual(actual, expected);
 });
