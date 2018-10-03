a282e849e609c56d41a0899a8adf3ad8c91f382a
John Cowen <jcowen@hashicorp.com>
2018-10-03 12:15:29 +0100

    ui: Move the text encoding polyfill to a a proper detecting polyfill

--- ui-v2/app/utils/btoa.js
+++ ui-v2/app/utils/btoa.js
@@ -1,7 +1,6 @@
-import TextEncoding from 'npm:text-encoding';
 import base64js from 'npm:base64-js';
 export default function(str, encoding = 'utf-8') {
   // encode
-  const bytes = new ('TextEncoder' in window ? TextEncoder : TextEncoding.TextEncoder)(encoding).encode(str);
+  const bytes = new TextEncoder(encoding).encode(str);
   return base64js.fromByteArray(bytes);
 }
