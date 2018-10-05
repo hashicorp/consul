7b50589562d9c0d7813ae4c4fe762bc51f568809
John Cowen <jcowen@hashicorp.com>
2018-10-05 18:42:08 +0100

    ui: Improve layout of node cards on large and small screens
    1. The grid based unhealthy cards are now clamped to only four wide
    maximum. This means that on larger screen the cards are much wider
    meaning you can view more information. Grid gutters are also clamped at
    a certain ideal width screen, remaining responsive for anything below
    this.
    
    2. The healthy node columns are finally responsive following the same
    column rules as unhealthy nodes

--- ui-v2/app/components/list-collection.js
+++ ui-v2/app/components/list-collection.js
@@ -1,16 +1,28 @@
-import { computed, get } from '@ember/object';
+import { computed, get, set } from '@ember/object';
 import Component from 'ember-collection/components/ember-collection';
+import PercentageColumns from 'ember-collection/layouts/percentage-columns';
 import style from 'ember-computed-style';
 import WithResizing from 'consul-ui/mixins/with-resizing';
 import qsaFactory from 'consul-ui/utils/qsa-factory';
 const $$ = qsaFactory();
-
 export default Component.extend(WithResizing, {
   tagName: 'div',
   attributeBindings: ['style'],
   height: 500,
+  cellHeight: 113,
   style: style('getStyle'),
   classNames: ['list-collection'],
+  init: function() {
+    this._super(...arguments);
+    this.columns = [25, 25, 25, 25];
+  },
+  didReceiveAttrs: function() {
+    this._cellLayout = this['cell-layout'] = new PercentageColumns(
+      get(this, 'items.length'),
+      get(this, 'columns'),
+      get(this, 'cellHeight')
+    );
+  },
   getStyle: computed('height', function() {
     return {
       height: get(this, 'height'),
@@ -27,6 +39,36 @@ export default Component.extend(WithResizing, {
       this.set('height', Math.max(0, height));
       this.updateItems();
       this.updateScrollPosition();
+    }
+    const width = e.detail.width;
+    const len = get(this, 'columns.length');
+    switch (true) {
+      case width > 1013:
+        if (len != 4) {
+          set(this, 'columns', [25, 25, 25, 25]);
+        }
+        break;
+      case width > 744:
+        if (len != 3) {
+          set(this, 'columns', [33, 33, 34]);
+        }
+        break;
+      case width > 487:
+        if (len != 2) {
+          set(this, 'columns', [50, 50]);
+        }
+        break;
+      case width < 488:
+        if (len != 1) {
+          set(this, 'columns', [100]);
+        }
+    }
+    if (len !== get(this, 'columns.length')) {
+      this._cellLayout = this['cell-layout'] = new PercentageColumns(
+        get(this, 'items.length'),
+        get(this, 'columns'),
+        get(this, 'cellHeight')
+      );
     }
   },
 });
