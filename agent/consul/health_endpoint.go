bb2fc08c2ce4c93fade160a8cff3f7056a41d6d5
Rebecca Zanzig <rzazzl@hotmail.com>
2018-10-16 12:12:53 -0700

    Sort tags before using them in metrics
    This addresses the potential proliferation of metrics if a query of
    "?tag=foo&tag=bar" is treated differently than "?tag=bar&tag=foo".
    Now, tags are always sorted before being recorded, making these two
    emit the same metric.

--- agent/consul/health_endpoint.go
+++ agent/consul/health_endpoint.go
@@ -2,6 +2,7 @@ package consul
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/armon/go-metrics"
 	"github.com/hashicorp/consul/agent/consul/state"
@@ -171,6 +172,10 @@ func (h *Health) ServiceNodes(args *structs.ServiceSpecificRequest, reply *struc
 				[]metrics.Label{{Name: "service", Value: args.ServiceName}, {Name: "tag", Value: args.ServiceTag}})
 		}
 		if len(args.ServiceTags) > 0 {
+			// Sort tags so that the metric is the same even if the request
+			// tags are in a different order
+			sort.Strings(args.ServiceTags)
+
 			labels := []metrics.Label{{Name: "service", Value: args.ServiceName}}
 			for _, tag := range args.ServiceTags {
 				labels = append(labels, metrics.Label{Name: "tag", Value: tag})
