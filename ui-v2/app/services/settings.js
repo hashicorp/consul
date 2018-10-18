bb0c497e26e550c7085376b837c472410d1b49ba
John Cowen <jcowen@hashicorp.com>
2018-10-18 12:08:40 +0100

    UI: Move the storage prefixing into its own unit and test

--- ui-v2/app/services/settings.js
+++ ui-v2/app/services/settings.js
@@ -1,39 +1,16 @@
 import Service from '@ember/service';
 import { Promise } from 'rsvp';
 import { get } from '@ember/object';
-
+import getStorage from 'consul-ui/utils/storage/local-storage';
 const SCHEME = 'consul';
-const getValue = function(storage, path) {
-  let value = storage.getItem(`${SCHEME}:${path}`);
-  if (typeof value !== 'string') {
-    value = '""';
-  }
-  try {
-    value = JSON.parse(value);
-  } catch (e) {
-    value = '';
-  }
-  return value;
-};
-const setValue = function(storage, path, value) {
-  try {
-    value = JSON.stringify(value);
-  } catch (e) {
-    value = '""';
-  }
-  return storage.setItem(`${SCHEME}:${path}`, value);
-};
-const removeValue = function(storage, path) {
-  return storage.removeItem(`${SCHEME}:${path}`);
-};
+const storage = getStorage(SCHEME);
 export default Service.extend({
-  // TODO: change name
-  storage: window.localStorage,
+  storage: storage,
   findHeaders: function() {
     // TODO: if possible this should be a promise
     // TODO: Actually this has nothing to do with settings it should be in the adapter,
     // which probably can't work with a promise based interface :(
-    const token = getValue(get(this, 'storage'), 'token');
+    const token = get(this, 'storage').getValue('token');
     // TODO: The old UI always sent ?token=
     // replicate the old functionality here
     // but remove this to be cleaner if its not necessary
@@ -42,22 +19,15 @@ export default Service.extend({
     };
   },
   findAll: function(key) {
-    const storage = get(this, 'storage');
-    const item = Object.keys(storage).reduce((prev, item, i, arr) => {
-      if (item.indexOf(`${SCHEME}:`) === 0) {
-        prev[item] = getValue(get(this, 'storage'), item);
-      }
-      return prev;
-    }, {});
-    return Promise.resolve(item);
+    return Promise.resolve(get(this, 'storage').all());
   },
   findBySlug: function(slug) {
-    return Promise.resolve(getValue(get(this, 'storage'), slug));
+    return Promise.resolve(get(this, 'storage').getValue(slug));
   },
   persist: function(obj) {
     const storage = get(this, 'storage');
     Object.keys(obj).forEach((item, i) => {
-      setValue(storage, item, obj[item]);
+      storage.setValue(item, obj[item]);
     });
     return Promise.resolve(obj);
   },
@@ -68,7 +38,7 @@ export default Service.extend({
     }
     const storage = get(this, 'storage');
     const item = obj.reduce(function(prev, item, i, arr) {
-      removeValue(storage, item);
+      storage.removeValue(item);
       return prev;
     }, {});
     return Promise.resolve(item);
---
+++ ui-v2/tests/unit/utils/storage/local-storage-test.js
@@ -0,0 +1,68 @@
+import localStorage from 'consul-ui/utils/storage/local-storage';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | storage/local-storage');
+
+// Replace this with your real tests.
+const mockStorage = function(obj, encode = val => val, decode = val => val) {
+  return localStorage('test', obj, encode, decode);
+};
+test('getValue returns an empty string if the value is null', function(assert) {
+  const expected = '""';
+  const storage = mockStorage({
+    getItem: function(path) {
+      return null;
+    },
+  });
+  const actual = storage.getValue('test');
+  assert.equal(actual, expected);
+});
+test('getValue uses the scheme in the path', function(assert) {
+  assert.expect(1);
+  const expected = 'test:test';
+  const storage = mockStorage({
+    getItem: function(actual) {
+      assert.equal(actual, expected);
+      return '';
+    },
+  });
+  storage.getValue('test');
+});
+test('setValue uses the scheme in the path', function(assert) {
+  assert.expect(1);
+  const expected = 'test:test';
+  const storage = mockStorage({
+    setItem: function(actual, value) {
+      assert.equal(actual, expected);
+      return '';
+    },
+  });
+  storage.setValue('test');
+});
+test('setValue calls removeItem if the value is null', function(assert) {
+  assert.expect(1);
+  const expected = 'test:test';
+  const storage = mockStorage({
+    removeItem: function(actual) {
+      assert.equal(actual, expected);
+    },
+  });
+  storage.setValue('test', null);
+});
+test('all returns an object of kvs under the correct prefix/scheme', function(assert) {
+  const storage = mockStorage({
+    'tester:a': 'a',
+    b: 'b',
+    'test:a': 'a',
+    'test:b': 'b',
+    getItem: function(path) {
+      return this[path];
+    },
+  });
+  const expected = {
+    a: 'a',
+    b: 'b',
+  };
+  const actual = storage.all();
+  assert.deepEqual(actual, expected);
+});
