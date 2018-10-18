3ce2ab1c51031e26b4ea91a6ae8be9ff5dd445c6
Matt Keeler <mjkeeler7@gmail.com>
2018-10-18 14:37:16 -0400

    Update some more struct tests

--- agent/structs/acl_cache_test.go
+++ agent/structs/acl_cache_test.go
@@ -3,6 +3,7 @@ package structs
 import (
 	"testing"
 
+	"github.com/hashicorp/consul/acl"
 	"github.com/stretchr/testify/require"
 )
 
@@ -41,5 +42,64 @@ func TestStructs_ACLCaches(t *testing.T) {
 		})
 	})
 
-	// TODO (ACL-V2) test the other Get/Put/Remote/Purge functions
+	t.Run("Identities", func(t *testing.T) {
+		t.Parallel()
+		// 1 isn't valid due to a bug in golang-lru library
+		config := ACLCachesConfig{Identities: 4}
+
+		cache, err := NewACLCaches(&config)
+		require.NoError(t, err)
+		require.NotNil(t, cache)
+
+		cache.PutIdentity("foo", &ACLToken{})
+		entry := cache.GetIdentity("foo")
+		require.NotNil(t, entry)
+		require.NotNil(t, entry.Identity)
+	})
+
+	t.Run("Policies", func(t *testing.T) {
+		t.Parallel()
+		// 1 isn't valid due to a bug in golang-lru library
+		config := ACLCachesConfig{Policies: 4}
+
+		cache, err := NewACLCaches(&config)
+		require.NoError(t, err)
+		require.NotNil(t, cache)
+
+		cache.PutPolicy("foo", &ACLPolicy{})
+		entry := cache.GetPolicy("foo")
+		require.NotNil(t, entry)
+		require.NotNil(t, entry.Policy)
+	})
+
+	t.Run("ParsedPolicies", func(t *testing.T) {
+		t.Parallel()
+		// 1 isn't valid due to a bug in golang-lru library
+		config := ACLCachesConfig{ParsedPolicies: 4}
+
+		cache, err := NewACLCaches(&config)
+		require.NoError(t, err)
+		require.NotNil(t, cache)
+
+		cache.PutParsedPolicy("foo", &acl.Policy{})
+		entry := cache.GetParsedPolicy("foo")
+		require.NotNil(t, entry)
+		require.NotNil(t, entry.Policy)
+	})
+
+	t.Run("Authorizers", func(t *testing.T) {
+		t.Parallel()
+		// 1 isn't valid due to a bug in golang-lru library
+		config := ACLCachesConfig{Authorizers: 4}
+
+		cache, err := NewACLCaches(&config)
+		require.NoError(t, err)
+		require.NotNil(t, cache)
+
+		cache.PutAuthorizer("foo", acl.DenyAll())
+		entry := cache.GetAuthorizer("foo")
+		require.NotNil(t, entry)
+		require.NotNil(t, entry.Authorizer)
+		require.True(t, entry.Authorizer == acl.DenyAll())
+	})
 }
