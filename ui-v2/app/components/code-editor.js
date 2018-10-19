d98c1242dc46c15fd6f129333f38a7ad35985df7
John Cowen <johncowen@users.noreply.github.com>
2018-10-19 17:36:38 +0100

    ui: Adds multi syntax linting to the code editor (#4814)

--- ui-v2/app/components/code-editor.js
+++ ui-v2/app/components/code-editor.js
@@ -1,12 +1,57 @@
 import Component from '@ember/component';
-import qsaFactory from 'consul-ui/utils/dom/qsa-factory';
-const $$ = qsaFactory();
+import { get, set } from '@ember/object';
+import { inject as service } from '@ember/service';
+const DEFAULTS = {
+  tabSize: 2,
+  lineNumbers: true,
+  theme: 'hashi',
+  showCursorWhenSelecting: true,
+  gutters: ['CodeMirror-lint-markers'],
+  lint: true,
+};
 export default Component.extend({
-  mode: 'application/json',
+  settings: service('settings'),
+  helper: service('code-mirror'),
   classNames: ['code-editor'],
+  syntax: '',
+  onchange: function(value) {
+    get(this, 'settings').persist({
+      'code-editor': value,
+    });
+    this.setMode(value);
+  },
   onkeyup: function() {},
+  init: function() {
+    this._super(...arguments);
+    set(this, 'modes', get(this, 'helper').modes());
+  },
+  setMode: function(mode) {
+    set(this, 'options', {
+      ...DEFAULTS,
+      mode: mode.mime,
+    });
+    const editor = get(this, 'editor');
+    editor.setOption('mode', mode.mime);
+    get(this, 'helper').lint(editor, mode.mode);
+    set(this, 'mode', mode);
+  },
+  didInsertElement: function() {
+    set(this, 'editor', get(this, 'helper').getEditor(this.element));
+    get(this, 'settings')
+      .findBySlug('code-editor')
+      .then(mode => {
+        const modes = get(this, 'modes');
+        const syntax = get(this, 'syntax');
+        if (syntax) {
+          mode = modes.find(function(item) {
+            return item.name.toLowerCase() == syntax.toLowerCase();
+          });
+        }
+        mode = !mode ? modes[0] : mode;
+        this.setMode(mode);
+      });
+  },
   didAppear: function() {
-    const $editor = [...$$('textarea + div', this.element)][0];
-    $editor.CodeMirror.refresh();
+    get(this, 'editor').refresh();
   },
 });
--- ui-v2/ember-cli-build.js
+++ ui-v2/ember-cli-build.js
@@ -31,8 +31,14 @@ module.exports = function(defaults) {
       ]
     },
     'codemirror': {
-      modes: ['javascript','ruby'],
-      keyMaps: ['sublime']
+      keyMaps: ['sublime'],
+      addonFiles: [
+        'lint/lint.css',
+        'lint/lint.js',
+        'lint/json-lint.js',
+        'lint/yaml-lint.js',
+        'mode/loadmode.js'
+      ]
     },
     'ember-cli-uglify': {
       uglify: {
@@ -66,8 +72,21 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import('node_modules/text-encoding/lib/encoding-indexes.js', {outputFile: 'assets/encoding-indexes.js'})
-  app.import('node_modules/text-encoding/lib/encoding.js', {outputFile: 'assets/encoding.js'})
+
+  // TextEncoder/Decoder polyfill. See assets/index.html
+  app.import('node_modules/text-encoding/lib/encoding-indexes.js', {outputFile: 'assets/encoding-indexes.js'});
+  app.import('node_modules/text-encoding/lib/encoding.js', {outputFile: 'assets/encoding.js'});
+
+  // JSON linting support. Possibly dynamically loaded via CodeMirror linting. See components/code-editor.js
+  app.import('node_modules/jsonlint/lib/jsonlint.js', {outputFile: 'assets/codemirror/mode/javascript/javascript.js'});
+  app.import('node_modules/codemirror/mode/javascript/javascript.js', {outputFile: 'assets/codemirror/mode/javascript/javascript.js'});
+
+  // HCL/Ruby linting support. Possibly dynamically loaded via CodeMirror linting. See components/code-editor.js
+  app.import('node_modules/codemirror/mode/ruby/ruby.js', {outputFile: 'assets/codemirror/mode/ruby/ruby.js'});
+
+  // YAML linting support. Possibly dynamically loaded via CodeMirror linting. See components/code-editor.js
+  app.import('node_modules/js-yaml/dist/js-yaml.js', {outputFile: 'assets/codemirror/mode/yaml/yaml.js'});
+  app.import('node_modules/codemirror/mode/yaml/yaml.js', {outputFile: 'assets/codemirror/mode/yaml/yaml.js'});
   let tree = app.toTree();
   return tree;
 };
