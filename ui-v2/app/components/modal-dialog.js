bd17f5295a6eefe6ae83d21433f9e6f4dfb8ecaa
John Cowen <jcowen@hashicorp.com>
2018-10-19 13:41:18 +0100

    ui: Improve the modal slightly and add some comments for later

--- ui-v2/app/components/modal-dialog.js
+++ ui-v2/app/components/modal-dialog.js
@@ -28,6 +28,14 @@ export default Component.extend(SlotsMixin, WithResizing, {
     this.didAppear();
     this.onopen(e);
   },
+  didAppear: function() {
+    this._super(...arguments);
+    if (get(this, 'checked')) {
+      get(this, 'dom')
+        .root()
+        .classList.add(...templatize(['with-modal']));
+    }
+  },
   _close: function(e) {
     set(this, 'checked', false);
     const dialogPanel = get(this, 'dialog');
@@ -35,22 +43,40 @@ export default Component.extend(SlotsMixin, WithResizing, {
     if (dialogPanel.classList.contains(overflowing)) {
       dialogPanel.classList.remove(overflowing);
     }
+    // TODO: should we make a didDisappear?
+    get(this, 'dom')
+      .root()
+      .classList.remove(...templatize(['with-modal']));
     this.onclose(e);
   },
   didReceiveAttrs: function() {
     this._super(...arguments);
     // TODO: Why does setting name mean checked it false?
+    // It's because if it has a name then it is likely to be linked
+    // to HTML state rather than just being added via HTMLBars
+    // and therefore likely to be immediately on the page
+    // It's not our usecase just yet, but this should check the state
+    // of the thing its linked to, incase that has a `checked` of true
+    // right now we know ours is always false.
     if (get(this, 'name')) {
       set(this, 'checked', false);
     }
+    if (this.element) {
+      if (get(this, 'checked')) {
+        // TODO: probably need an event here
+        // possibly this.element for the target
+        // or find the input
+        this._open({ target: {} });
+      }
+    }
+  },
+  didInsertElement: function() {
+    this._super(...arguments);
     if (get(this, 'checked')) {
       // TODO: probably need an event here
       // possibly this.element for the target
       // or find the input
       this._open({ target: {} });
-      get(this, 'dom')
-        .root()
-        .classList.add(...templatize(['with-modal']));
     }
   },
   didDestroyElement: function() {
