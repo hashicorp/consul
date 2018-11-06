57437eb321d75e6336bfd77502e534023d768e2d
John Cowen <johncowen@users.noreply.github.com>
2018-11-06 09:13:23 +0000

    ui: Move ACL policy and token repos to use the RepositoryService (#4867)

--- ui-v2/app/services/repository/policy.js
+++ ui-v2/app/services/repository/policy.js
@@ -1,14 +1,15 @@
-import Service, { inject as service } from '@ember/service';
+import RepositoryService from 'consul-ui/services/repository';
 import { get } from '@ember/object';
-import { typeOf } from '@ember/utils';
-import { PRIMARY_KEY, SLUG_KEY } from 'consul-ui/models/policy';
 import { Promise } from 'rsvp';
 import statusFactory from 'consul-ui/utils/acls-status';
 import isValidServerErrorFactory from 'consul-ui/utils/http/acl/is-valid-server-error';
+import { PRIMARY_KEY, SLUG_KEY } from 'consul-ui/models/policy';
+
 const isValidServerError = isValidServerErrorFactory();
 const status = statusFactory(isValidServerError, Promise);
 const MODEL_NAME = 'policy';
-export default Service.extend({
+
+export default RepositoryService.extend({
   getModelName: function() {
     return MODEL_NAME;
   },
@@ -18,43 +19,10 @@ export default Service.extend({
   getSlugKey: function() {
     return SLUG_KEY;
   },
-  store: service('store'),
   status: function(obj) {
     return status(obj);
   },
   translate: function(item) {
     return get(this, 'store').translate('policy', get(item, 'Rules'));
-  },
-  findAllByDatacenter: function(dc) {
-    return get(this, 'store').query('policy', {
-      dc: dc,
-    });
-  },
-  findBySlug: function(slug, dc) {
-    return get(this, 'store').queryRecord('policy', {
-      id: slug,
-      dc: dc,
-    });
-  },
-  create: function(obj) {
-    return get(this, 'store').createRecord('policy', obj);
-  },
-  persist: function(item) {
-    return item.save();
-  },
-  remove: function(obj) {
-    let item = obj;
-    if (typeof obj.destroyRecord === 'undefined') {
-      item = obj.get('data');
-    }
-    if (typeOf(item) === 'object') {
-      item = get(this, 'store').peekRecord('policy', item[PRIMARY_KEY]);
-    }
-    return item.destroyRecord().then(item => {
-      return get(this, 'store').unloadRecord(item);
-    });
-  },
-  invalidate: function() {
-    get(this, 'store').unloadAll('policy');
   },
 });
