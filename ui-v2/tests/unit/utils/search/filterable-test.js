7eda9b01f1114f4a02302446a76a93defca3b595
John Cowen <johncowen@users.noreply.github.com>
2018-11-06 09:10:20 +0000

    UI: Async Search (#4859)
    This does several things to make improving the search experience easier
    moving forwards:
    
    1. Separate searching off from filtering. 'Searching' can be thought of
    as specifically 'text searching' whilst filtering is more of a
    boolean/flag search.
    2. Decouple the actual searching functionality to almost pure,
    isolated / unit testable units and unit test. (I still import embers get
    which, once I upgrade to 3.5, I shouldn't need)
    3. Searching rules are now configurable from the outside, i.e. not
    wrapped in Controllers or Components.
    4. General searching itself now can use an asynchronous approach based on
    events. This prepares for future possibilities of handing off the
    searching to a web worker or elsewhere, which should aid in large scale
    searching and prepares the way for other searching methods.
    5. Adds the possibility of have multiple searches in one
    template/route/page.
    
    Additionally, this adds a WithSearching mixin to aid linking the
    searching to ember in an ember-like way in a single place. Plus a
    WithListeners mixin to aid with cleaning up of event listeners on
    Controller/Component destruction.
    
    Post-initial work I slightly changed the API of create listeners:
    
    Returning the handler from a `remover` means you can re-add it again if you
    want to, this avoids having to save a reference to the handler elsewhere
    to do the same.
    
    The `remove` method itself now returns an array of handlers, again you
    might want to use these again or something, and its also more useful
    then just returning an empty array.
    
    The more I look at this the more I doubt that you'll ever use `remove`
    to remove individual handlers, you may aswell just use the `remover`
    returned from add. I've added some comments to reflect this, but they'll
    likely be removed once I'm absolutely sure of this.
    
    I also added some comments for WithSearching to explain possible further
    work re: moving `searchParams` so it can be `hung` off the
    controller object

---
+++ ui-v2/tests/unit/utils/search/filterable-test.js
@@ -0,0 +1,10 @@
+import searchFilterable from 'consul-ui/utils/search/filterable';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | search/filterable');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = searchFilterable();
+  assert.ok(result);
+});
