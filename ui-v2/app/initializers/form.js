4b43dfca4066d4799881028e40d52d30e00b18ca
John Cowen <jcowen@hashicorp.com>
2018-11-09 13:42:21 +0000

    ui: Move KV's to use a `form`

--- ui-v2/app/initializers/form.js
+++ ui-v2/app/initializers/form.js
@@ -1,3 +1,4 @@
+import kv from 'consul-ui/forms/kv';
 import acl from 'consul-ui/forms/acl';
 import token from 'consul-ui/forms/token';
 import policy from 'consul-ui/forms/policy';
@@ -5,6 +6,7 @@ export function initialize(application) {
   // Service-less injection using private properties at a per-project level
   const FormBuilder = application.resolveRegistration('service:form');
   const forms = {
+    kv: kv(),
     acl: acl(),
     token: token(),
     policy: policy(),
