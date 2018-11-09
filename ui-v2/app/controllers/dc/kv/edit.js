19917b476bd684b969b4b962eb95116684965599
John Cowen <jcowen@hashicorp.com>
2018-11-09 14:31:26 +0000

    ui: Standardize on variable order, form comes second

--- ui-v2/app/controllers/dc/kv/edit.js
+++ ui-v2/app/controllers/dc/kv/edit.js
@@ -12,11 +12,6 @@ export default Controller.extend({
     this.form = get(this, 'builder').form('kv');
   },
   setProperties: function(model) {
-    // TODO: Potentially save whether json has been clicked to the model,
-    // setting set(this, 'json', true) here will force the form to always default to code=on
-    // even if the user has selected code=off on another KV
-    // ideally we would save the value per KV, but I'd like to not do that on the model
-    // a set(this, 'json', valueFromSomeStorageJustForThisKV) would be added here
     // essentially this replaces the data with changesets
     this._super(
       Object.keys(model).reduce((prev, key, i) => {
@@ -47,6 +42,11 @@ export default Controller.extend({
             set(this.item, 'Key', `${parent !== '/' ? parent : ''}${target.value}`);
             break;
           case 'json':
+            // TODO: Potentially save whether json has been clicked to the model,
+            // setting set(this, 'json', true) here will force the form to always default to code=on
+            // even if the user has selected code=off on another KV
+            // ideally we would save the value per KV, but I'd like to not do that on the model
+            // a set(this, 'json', valueFromSomeStorageJustForThisKV) would be added here
             set(this, 'json', !get(this, 'json'));
             break;
           default:
