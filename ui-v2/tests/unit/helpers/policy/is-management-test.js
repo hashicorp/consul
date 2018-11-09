dec52a4f6256e2f2266212de8770149cc994d02d
John Cowen <johncowen@users.noreply.github.com>
2018-11-09 11:13:20 +0000

    ui: Prefer using `notOk(actual)` instead of `ok(!actual)` in tests (#4925)
    And make sure its consistent throughout the app

--- ui-v2/tests/unit/helpers/policy/is-management-test.js
+++ ui-v2/tests/unit/helpers/policy/is-management-test.js
@@ -9,5 +9,5 @@ test('it returns true if the policy is the management policy', function(assert) 
 });
 test("it returns false if the policy isn't the management policy", function(assert) {
   const actual = isManagement([{ ID: '00000000-0000-0000-0000-000000000000' }]);
-  assert.ok(!actual);
+  assert.notOk(actual);
 });
