4368040a4a8bc3c1b3d59bbd92112662227a7e36
John Cowen <johncowen@users.noreply.github.com>
2018-11-19 14:53:08 +0000

    UI: Move legacy ACLs, KVs and Intentions to use `form` functionality (#4936)
    Change legacy acls, kvs and intentions to use `form`s

--- ui-v2/tests/unit/controllers/dc/acls/edit-test.js
+++ ui-v2/tests/unit/controllers/dc/acls/edit-test.js
@@ -2,7 +2,7 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('controller:dc/acls/edit', 'Unit | Controller | dc/acls/edit', {
   // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  needs: ['service:dom', 'service:form'],
 });
 
 // Replace this with your real tests.
