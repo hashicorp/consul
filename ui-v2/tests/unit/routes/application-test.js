55ba2a22c6181e655c5c85ff57df85eb04586b5e
John Cowen <johncowen@users.noreply.github.com>
2018-11-19 14:47:20 +0000

    UI: dom usage refactoring (#4924)
    Move all the dom-things to use the dom service in tabular-collection, feedback-dialog, list-collection and node show. Move get-component-factory into utils/dom and use dom.root() in a few more places
    
    This includes an additional `dom.components` method which gives you a
    list of components matching the selector instead of just one.

--- ui-v2/tests/unit/routes/application-test.js
+++ ui-v2/tests/unit/routes/application-test.js
@@ -2,7 +2,7 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('route:application', 'Unit | Route | application', {
   // Specify the other units that are required for this test.
-  needs: ['service:repository/dc', 'service:settings'],
+  needs: ['service:repository/dc', 'service:settings', 'service:dom'],
 });
 
 test('it exists', function(assert) {
