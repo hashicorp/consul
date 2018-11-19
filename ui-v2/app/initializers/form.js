51baa121aaf95b30f1f7bc4b2c7b0819e4d17d3c
John Cowen <johncowen@users.noreply.github.com>
2018-11-19 14:53:08 +0000

    UI: Move legacy ACLs, KVs and Intentions to use `form` functionality (#4936)
    Change legacy acls, kvs and intentions to use `form`s

--- ui-v2/app/initializers/form.js
+++ ui-v2/app/initializers/form.js
@@ -1,11 +1,17 @@
+import kv from 'consul-ui/forms/kv';
+import acl from 'consul-ui/forms/acl';
 import token from 'consul-ui/forms/token';
 import policy from 'consul-ui/forms/policy';
+import intention from 'consul-ui/forms/intention';
 export function initialize(application) {
   // Service-less injection using private properties at a per-project level
   const FormBuilder = application.resolveRegistration('service:form');
   const forms = {
+    kv: kv(),
+    acl: acl(),
     token: token(),
     policy: policy(),
+    intention: intention(),
   };
   FormBuilder.reopen({
     form: function(name) {
