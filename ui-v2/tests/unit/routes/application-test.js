683278a2fc292e43e189e907140daaa7bc2f352d
John Cowen <jcowen@hashicorp.com>
2018-11-26 19:00:10 +0000

    ui: Resolve bad rebase and add in KV test for change of GET for KVs

--- ui-v2/tests/unit/routes/application-test.js
+++ ui-v2/tests/unit/routes/application-test.js
@@ -2,7 +2,14 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('route:application', 'Unit | Route | application', {
   // Specify the other units that are required for this test.
-  needs: ['service:repository/dc', 'service:settings', 'service:dom'],
+  needs: [
+    'service:repository/dc',
+    'service:settings',
+    'service:feedback',
+    'service:flashMessages',
+    'service:logger',
+    'service:dom',
+  ],
 });
 
 test('it exists', function(assert) {
