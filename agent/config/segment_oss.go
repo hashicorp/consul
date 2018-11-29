1eeb79114641f4074c0406d7ef718181117f106f
Daniel Kucera <github@danman.eu>
2018-11-29 11:24:51 +0100

    removed comments from consul validation

--- agent/config/segment_oss.go
+++ agent/config/segment_oss.go
@@ -4,14 +4,12 @@ package config
 
 import (
 	"fmt"
-//	"github.com/hashicorp/consul/agent/structs"
 )
 
 func (b *Builder) validateSegments(rt RuntimeConfig) error {
 	if len(rt.Segments) > rt.SegmentLimit {
 		return fmt.Errorf("Cannot exceed network segment limit of %d", rt.SegmentLimit)
 	}
-// 	takenPorts := make(map[int]string, len(rt.Segments))
 	for _, segment := range rt.Segments {
 		if segment.Name == "" {
 			return fmt.Errorf("Segment name cannot be blank")
@@ -19,13 +17,6 @@ func (b *Builder) validateSegments(rt RuntimeConfig) error {
 		if len(segment.Name) > rt.SegmentNameLimit {
 			return fmt.Errorf("Segment name %q exceeds maximum length of %d", segment.Name, rt.SegmentNameLimit)
 		}
-/* 		previous, ok := takenPorts[segment.Port]
-		if ok {
-			return fmt.Errorf("Segment %q port %d overlaps with segment %q", segment.Name, segment.Port, previous)
-		}
-		takenPorts[segment.Port] = segment.Name
-*/
 	}
-
 	return nil
 }
