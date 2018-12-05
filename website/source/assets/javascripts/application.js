59cd4f853a8675a3f43616bd0118009727857a31
RJ Spiker <spiker.rj@gmail.com>
2018-12-05 10:39:52 -0700

    website: update home hero to work with turbolinks

--- website/source/assets/javascripts/application.js
+++ website/source/assets/javascripts/application.js
@@ -4,6 +4,7 @@
 //= require hashicorp/mega-nav
 //= require hashicorp/sidebar
 //= require hashicorp/analytics
+//= require consul-connect/home-hero
 //= require analytics
 //= require gsap-custom
 //= require animations
--- website/source/assets/javascripts/consul-connect/home-hero.js
+++ website/source/assets/javascripts/consul-connect/home-hero.js
@@ -1,109 +1,103 @@
-var qs = document.querySelector.bind(document)
-var qsa = document.querySelectorAll.bind(document)
+document.addEventListener('turbolinks:load', function () {
+  var qsa = document.querySelectorAll.bind(document)
+  var $$wrappers = qsa('#home-hero .videos > div') // all the wrappers for the videos
+  var $$videos = qsa('#home-hero video') // all the videos
+  var $$videoControls = qsa('#home-hero .controls > div') // carousel controllers
+  var $$videoProgressBars =qsa('#home-hero .progress-bar span') // carousel controller progress bars
+  var currentIndex = $$videos.length - 1 // currently playing video, inits as last video
+  var playbackRate = 2 // video playback speed
 
-var $$wrappers = qsa('#home-hero .videos > div') // all the wrappers for the videos
-var $$videos = qsa('#home-hero video') // all the videos
-var $$videoControls = qsa('#home-hero .controls > div') // carousel controllers
-var currentIndex = 1 // currently playing video
-var playbackRate = 2 // video playback speed
-
-// initiate a video change
-function initiateVideoChange(index) {
-  var wrapper = $$wrappers[currentIndex]
-  var nextWrapper = $$wrappers[index]
-
-  // pause the current video
-  $$videos[currentIndex].pause()
-
-  // deactivate the current video
-  wrapper.classList.remove('active')
-  wrapper.classList.add('deactivate')
-
-  // after the current video animates out...
-  setTimeout(function() {
-    // remove transition effect so progress-bar doesn't slowly decline
-    var loadingBar = $$videoControls[currentIndex].querySelector(
-      '.progress-bar span'
-    )
-    loadingBar.style.transitionDuration = '0s'
-
-    // reset the current video
-    if (!isNaN($$videos[currentIndex].duration)) {
-      $$videos[currentIndex].currentTime = 0
-    }
-    $$videoControls[currentIndex].classList.remove('playing')
-
-    // stop deactivation
-    wrapper.classList.remove('deactivate')
-
-    // check if the video is loaded
-    // if not, listen for it to load
-    if ($$videos[index].classList.contains('loaded')) {
-      playVideo(index, nextWrapper)
-    } else {
-      $$videos[index].addEventListener(
-        'canplaythrough',
-        playVideo(index, nextWrapper)
-      )
-    }
-  }, 1000)
-}
-
-// activate and play a video
-function playVideo(index, wrapper) {
-  // toggle
-  $$videos[index].classList.add('loaded')
-
-  // activate the next video and start playing it
-  wrapper.classList.add('active')
-  $$videoControls[index].classList.add('playing')
-  $$videos[index].play()
-
-  $$videoControls[index].querySelector(
-    '.progress-bar span'
-  ).style.transitionDuration =
-    Math.ceil($$videos[index].duration / playbackRate).toString() + 's'
-
-  // set the currentIndex to be that of the current video's index
-  currentIndex = index
-}
-
-function initiateVideos() {
-  // remove 'active' from wrappers which may be 
-  // there on page load because of turbolinks
-  for (var i = 0; i < $$wrappers.length; i++) {
-    $$wrappers[i].classList.remove('active')
-  }
-
-  // loop through videos to set up options/listeners
-  for (var i = 0; i < $$videos.length; i++) {
-    // set video default speed
-    $$videos[i].playbackRate = playbackRate
-
-    // listen for video ending, then go to the next video
-    $$videos[i].addEventListener('ended', function() {
-      var nextIndex = currentIndex + 1
-      initiateVideoChange(nextIndex < $$videos.length ? nextIndex : 0)
-    })
-  }
-
-  for (var i = 0; i < $$videoControls.length; i++) {
-    // remove 'playing' from controls which may be 
-    // there on page load because of turbolinks
-    $$videoControls[i].classList.remove('playing')
-
-    // listen for control clicks and initiate videos appropriately
-    $$videoControls[i].addEventListener('click', function() {
-      if (!this.classList.contains('playing')) {
-        initiateVideoChange(this.dataset.index)
-      }
-    })
-  }
-
-  // go to first video to start this thing
+  // if there are hero videos on the page, intiate the video carousel
   if ($$videos.length > 0) {
-    initiateVideoChange(0)
+    setup()
   }
-}
 
-document.addEventListener('turbolinks:load', initiateVideos)
+  // initiate the video carousel
+  function setup() {
+    // loop through videos to set up options/listeners
+    for (var i = 0; i < $$wrappers.length; i++) {
+      // some reseting that may need to be done
+      // to work with turbolinks
+      $$wrappers[i].classList.remove('active')
+      $$videoProgressBars[i].style.width = 0
+      $$videos[i].addEventListener('loadeddata', function () { 
+        this.currentTime = 0 
+      })
+
+      // set video default speed
+      $$videos[i].playbackRate = playbackRate
+
+      // set up progress bar
+      setupProgressBar($$videoProgressBars[i], $$videos[i])
+
+      // listen for video ending, then go to the next video
+      $$videos[i].addEventListener('ended', function() {
+        var nextIndex = currentIndex + 1
+        initiateVideoChange(nextIndex < $$videos.length ? nextIndex : 0)
+      })
+
+      // listen for control clicks and initiate videos appropriately
+      $$videoControls[i].addEventListener('click', function() {
+        if (!this.classList.contains('playing')) {
+          initiateVideoChange(this.dataset.index)
+        }
+      })
+    }
+
+    // go to first video to start this thing
+    initiateVideoChange(0)    
+  }
+
+  // set up progress bar
+  function setupProgressBar(bar, video) {
+    setInterval(function () {
+      if (!isNaN(video.duration)) {
+        bar.style.width = `${(video.currentTime / video.duration) * 100}%`
+      }
+    }, 200)
+  }
+
+  // initiate a video change
+  function initiateVideoChange(nextIndex) {
+    var currentWrapper = $$wrappers[currentIndex]
+    var nextWrapper = $$wrappers[nextIndex]
+
+    // pause the current video
+    $$videos[currentIndex].pause()
+
+    // deactivate the current video
+    currentWrapper.classList.remove('active')
+    currentWrapper.classList.add('deactivate')
+    $$videoControls[currentIndex].classList.remove('playing')
+    $$videoControls[nextIndex].classList.add('playing')
+
+    // after the current video animates out...
+    setTimeout(function() {
+      // stop deactivation
+      currentWrapper.classList.remove('deactivate')
+      $$videos[currentIndex].currentTime = 0
+
+      // check if the next video is loaded
+      // if not, listen for it to load
+      if ($$videos[nextIndex].readyState === 4) {
+        $$videos[nextIndex].currentTime = 0
+        playVideo(nextIndex, nextWrapper)
+      } else {
+        $$videos[nextIndex].addEventListener(
+          'canplaythrough',
+          playVideo(nextIndex, nextWrapper)
+        )
+      }
+    }, 1000)
+  }
+
+  // activate and play a video
+  function playVideo(nextIndex, nextWrapper) {
+    // activate the next video and start playing it
+    nextWrapper.classList.add('active')
+    $$videos[nextIndex].play()
+
+    // set the currentIndex to be that of the current video's index
+    currentIndex = nextIndex
+  }
+})
