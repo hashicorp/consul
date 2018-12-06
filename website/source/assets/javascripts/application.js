6d21bb436e9a63b5dc9a33fae3ee059bd1c34cb7
RJ Spiker <spiker.rj@gmail.com>
2018-12-06 12:17:35 -0700

    website: fix carousel bugs

--- website/source/assets/javascripts/application.js
+++ website/source/assets/javascripts/application.js
@@ -4,6 +4,11 @@
 //= require hashicorp/mega-nav
 //= require hashicorp/sidebar
 //= require hashicorp/analytics
+
+//= require consul-connect/vendor/object-fit-images.min.js
+//= require consul-connect/vendor/siema.min.js
+//= require consul-connect/carousel
+
 //= require analytics
 //= require gsap-custom
 //= require animations
