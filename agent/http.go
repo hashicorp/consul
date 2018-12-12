f845381b51539aea92ba8d584c179644d1901675
Hans Hasselberg <me@hans.io>
2018-12-12 22:37:20 +0100

    server /robots.txt

--- agent/http.go
+++ agent/http.go
@@ -219,8 +219,7 @@ func (s *HTTPServer) handler(enableDebug bool) http.Handler {
 			uifs = &redirectFS{fs: uifs}
 		}
 
-		robotHandler := http.RedirectHandler("/ui/robots.txt", 307)
-		mux.Handle("/robots.txt", robotHandler)
+		mux.Handle("/robots.txt", http.FileServer(uifs))
 		mux.Handle("/ui/", http.StripPrefix("/ui/", http.FileServer(uifs)))
 	}
 
