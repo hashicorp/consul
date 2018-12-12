0e4435d08d94d3b42a25c8e57ac03a462ff8dada
Paul Banks <banks@banksco.de>
2018-12-12 13:51:18 +0000

    Add State storage and LastResult argument into Cache so that cache.Types can safely store additional data that is eventually expired.

--- agent/cache/entry.go
+++ agent/cache/entry.go
@@ -12,6 +12,13 @@ import (
 type cacheEntry struct {
 	// Fields pertaining to the actual value
 	Value interface{}
+	// State can be used to store info needed by the cache type but that should
+	// not be part of the result the client gets. For example the Connect Leaf
+	// type needs to store additional data about when it last attempted a renewal
+	// that is not part of the actual IssuedCert struct it returns. It's opaque to
+	// the Cache but allows types to store additional data that is coupled to the
+	// cache entry's lifetime and will be aged out by TTL etc.
+	State interface{}
 	Error error
 	Index uint64
 
