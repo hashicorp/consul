6b8c154441f4f4bcf5c8ee6cff888d28edac81fe
Yong Tang <yong.tang.github@outlook.com>
2018-12-16 01:04:41 -0800

    Update client-go to v10.0.0 (Kubernetes 1.13) (#2382)
    * Update client-go to v10.0.0 (Kubernetes 1.13)
    
    This fix updates client-go to v10.0.0 which matches
    Kubernetes 1.13 (released several days ago).
    
    Other changes in Gopkg.yaml:
    - Updated apimachinary, api, klog, yaml associated with k8s version
      go dep will not automatically match the version.
    - Added [prune] field (otherwise go dep will not prune automatically)
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Updated Gopkg.lock
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>
    
    * Updated vendor for client-go v10.0.0
    
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- vendor/github.com/DataDog/dd-trace-go/Rakefile
+++
@@ -1,4 +0,0 @@
-require_relative 'tasks/common'
-require_relative 'tasks/vendors'
-require_relative 'tasks/testing'
-require_relative 'tasks/benchmarks'
--- vendor/github.com/openzipkin/zipkin-go-opentracing/thrift/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-for f in *.thrift ; do
-	thrift -r --gen go:thrift_import=github.com/apache/thrift/lib/go/thrift,package_prefix=github.com/openzipkin/zipkin-go-opentracing/thrift/gen-go/ $f
-done
