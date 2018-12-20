a92fa407d544c9b8816c5128a22a567b8e5ce144
Alvin Huang <alvin@hashicorp.com>
2018-12-20 13:40:22 -0500

    output XML test results

--- ui-v2/testem.js
+++ ui-v2/testem.js
@@ -16,3 +16,21 @@ module.exports = {
     },
   },
 };
+
+// outputs XML reports for CI
+if (process.env.EMBER_TEST_REPORT) {
+  module.exports.report_file = process.env.EMBER_TEST_REPORT;
+  module.exports.xunit_intermediate_output = true;
+}
+
+/*
+ * ember-exam honors the `parallel` parameter in testem.js.
+ * By default this value is 1 which means it only uses one client.
+ * When this is set to -1 it uses the --split value of ember-exam.
+ *
+ * https://github.com/trentmwillis/ember-exam#split-test-parallelization
+ * https://github.com/trentmwillis/ember-exam/issues/108
+ */
+if (process.env.EMBER_EXAM_PARALLEL) {
+  module.exports.parallel = -1
+}
