417d3030bedcc1cce9575ef66bd3e9f9850f2dd2
John Cowen <johncowen@users.noreply.github.com>
2019-04-08 11:17:30 +0100

    ui: Adds uid to tabular-details for/id's used in toggling for uniqueness (#5584)

--- ui-v2/app/components/tabular-details.js
+++ ui-v2/app/components/tabular-details.js
@@ -1,11 +1,20 @@
 import Component from '@ember/component';
 import SlotsMixin from 'block-slots';
 import { inject as service } from '@ember/service';
-import { get } from '@ember/object';
+import { get, set } from '@ember/object';
+import { subscribe } from 'consul-ui/utils/computed/purify';
 
+let uid = 0;
 export default Component.extend(SlotsMixin, {
   dom: service('dom'),
   onchange: function() {},
+  init: function() {
+    this._super(...arguments);
+    set(this, 'uid', uid++);
+  },
+  inputId: subscribe('name', 'uid', function(name = 'name') {
+    return `tabular-details-${name}-toggle-${uid}_`;
+  }),
   actions: {
     click: function(e) {
       get(this, 'dom').clickFirstAnchor(e);
