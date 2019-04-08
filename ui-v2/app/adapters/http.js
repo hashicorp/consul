2b28c22d9c7f01171fa04d7f2969d9e346e82031
John Cowen <jcowen@hashicorp.com>
2019-04-08 10:34:24 +0000

    ui: Add some basic ember-data specfic error catching

--- ui-v2/app/adapters/http.js
+++ ui-v2/app/adapters/http.js
@@ -1,26 +1,85 @@
 import Adapter from 'ember-data/adapter';
+import {
+  AbortError,
+  TimeoutError,
+  ServerError,
+  UnauthorizedError,
+  ForbiddenError,
+  NotFoundError,
+  ConflictError,
+  InvalidError,
+  AdapterError,
+} from 'ember-data/adapters/errors';
 import { get } from '@ember/object';
 
 export default Adapter.extend({
   snapshotToJSON: function(snapshot, type, options) {
     return snapshot.attributes();
   },
+  error: function(err) {
+    const errors = [
+      {
+        status: `${err.statusCode}`,
+        title: 'The backend responded with an error',
+        detail: err.message,
+      },
+    ];
+    let error;
+    const detailedMessage = '';
+    try {
+      switch (err.statusCode) {
+        case 0:
+          error = new AbortError();
+          break;
+        case 401:
+          error = new UnauthorizedError(errors, detailedMessage);
+          break;
+        case 403:
+          error = new ForbiddenError(errors, detailedMessage);
+          break;
+        case 404:
+          error = new NotFoundError(errors, detailedMessage);
+          break;
+        case 408:
+          error = new TimeoutError();
+          break;
+        case 409:
+          error = new ConflictError(errors, detailedMessage);
+          break;
+        case 422:
+          error = new InvalidError(errors); //payload.errors
+          break;
+        default:
+          if (err.statusCode >= 500) {
+            error = new ServerError(errors, detailedMessage);
+          } else {
+            error = new AdapterError(errors, detailedMessage);
+          }
+      }
+    } catch (e) {
+      error = e;
+    }
+    throw error;
+  },
   query: function(store, type, query) {
     const serializer = store.serializerFor(type.modelName);
     return get(this, 'client')
       .request(request => this.requestForQuery(request, query))
+      .catch(e => this.error(e))
       .then(respond => serializer.respondForQuery(respond, query, type));
   },
   queryRecord: function(store, type, query) {
     const serializer = store.serializerFor(type.modelName);
     return get(this, 'client')
       .request(request => this.requestForQueryRecord(request, query))
+      .catch(e => this.error(e))
       .then(respond => serializer.respondForQueryRecord(respond, query));
   },
   findAll: function(store, type) {
     const serializer = store.serializerFor(type.modelName);
     return get(this, 'client')
       .request(request => this.requestForFindAll(request))
+      .catch(e => this.error(e))
       .then(respond => serializer.respondForFindAll(respond));
   },
   createRecord: function(store, type, snapshot) {
@@ -29,6 +88,7 @@ export default Adapter.extend({
     const serialized = serializer.serialize(snapshot, {});
     return get(this, 'client')
       .request(request => this.requestForCreateRecord(request, unserialized), serialized)
+      .catch(e => this.error(e))
       .then(respond => serializer.respondForCreateRecord(respond, unserialized, type));
   },
   updateRecord: function(store, type, snapshot) {
@@ -37,6 +97,7 @@ export default Adapter.extend({
     const serialized = serializer.serialize(snapshot, {});
     return get(this, 'client')
       .request(request => this.requestForUpdateRecord(request, unserialized), serialized)
+      .catch(e => this.error(e))
       .then(respond => serializer.respondForUpdateRecord(respond, unserialized, type));
   },
   deleteRecord: function(store, type, snapshot) {
@@ -45,6 +106,7 @@ export default Adapter.extend({
     const serialized = serializer.serialize(snapshot, {});
     return get(this, 'client')
       .request(request => this.requestForDeleteRecord(request, unserialized), serialized)
+      .catch(e => this.error(e))
       .then(respond => serializer.respondForDeleteRecord(respond, unserialized, type));
   },
 });
