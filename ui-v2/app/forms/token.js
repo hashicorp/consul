a8c7fe5b52c30f5df6b267b29b5a87f3cd2fee30
John Cowen <jcowen@hashicorp.com>
2019-04-10 11:41:01 +0000

    ui: Add roles to the token form

--- ui-v2/app/forms/token.js
+++ ui-v2/app/forms/token.js
@@ -1,9 +1,11 @@
 import validations from 'consul-ui/validations/token';
 import policy from 'consul-ui/forms/policy';
+import role from 'consul-ui/forms/role';
 import builderFactory from 'consul-ui/utils/form/builder';
 const builder = builderFactory();
 export default function(name = '', v = validations, form = builder) {
   return form(name, {})
     .setValidators(v)
-    .add(policy());
+    .add(policy())
+    .add(role());
 }
