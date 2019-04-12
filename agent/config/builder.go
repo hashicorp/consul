c1f4e3271387b4d18d610188880e8036b1952d26
Hans Hasselberg <me@hans.io>
2019-04-12 10:51:47 +0200

    enable connect on the server when using auto_encrypt

--- agent/config/builder.go
+++ agent/config/builder.go
@@ -557,9 +557,6 @@ func (b *Builder) Build() (rt RuntimeConfig, err error) {
 	consulRaftHeartbeatTimeout := b.durationVal("consul.raft.heartbeat_timeout", c.Consul.Raft.HeartbeatTimeout) * time.Duration(performanceRaftMultiplier)
 	consulRaftLeaderLeaseTimeout := b.durationVal("consul.raft.leader_lease_timeout", c.Consul.Raft.LeaderLeaseTimeout) * time.Duration(performanceRaftMultiplier)
 
-	autoEncryptTLS := b.boolVal(c.AutoEncrypt.TLS)
-	autoEncryptGossip := b.boolVal(c.AutoEncrypt.Gossip)
-
 	// Connect proxy defaults.
 	connectEnabled := b.boolVal(c.Connect.Enabled)
 	connectCAProvider := b.stringVal(c.Connect.CAProvider)
@@ -588,6 +585,13 @@ func (b *Builder) Build() (rt RuntimeConfig, err error) {
 			"csr_max_per_second": "CSRMaxPerSecond",
 			"csr_max_concurrent": "CSRMaxConcurrent",
 		})
+	}
+
+	autoEncryptTLS := b.boolVal(c.AutoEncrypt.TLS)
+	autoEncryptGossip := b.boolVal(c.AutoEncrypt.Gossip)
+
+	if autoEncryptTLS {
+		connectEnabled = true
 	}
 
 	datacenter := strings.ToLower(b.stringVal(c.Datacenter))
