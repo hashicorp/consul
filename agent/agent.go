fe109db778a1461747f6c60d5af2d3b761c636ac
Hans Hasselberg <me@hans.io>
2019-04-24 10:38:44 +0200

    stuff

--- agent/agent.go
+++ agent/agent.go
@@ -548,13 +548,13 @@ func (a *Agent) Start() error {
 
 func (a *Agent) setupClientAutoEncrypt() error {
 	client := a.delegate.(*consul.Client)
-	// we don't care about the error since we might have servers in StartJoin
 
 	addrs := a.config.StartJoinAddrsLAN
-	disco, err := discoverDiscover()
-	if err == nil {
-		addrs = append(addrs, retryJoinAddrs(disco, "LAN", a.config.RetryJoinLAN)...)
+	disco, err := newDiscover()
+	if err != nil && len(addrs) == 0 {
+		return err
 	}
+	addrs = append(addrs, retryJoinAddrs(disco, "LAN", a.config.RetryJoinLAN, a.logger)...)
 
 	reply, priv, err := client.AutoEncrypt(addrs, a.config.ServerPort, a.tokens.AgentToken())
 	if err != nil {
