6376d0718b973130c35efb2037eae55b01a2e71f
Hans Hasselberg <me@hans.io>
2019-05-07 10:28:09 +0200

    wip

--- agent/consul/auto_encrypt_endpoint_test.go
+++ agent/consul/auto_encrypt_endpoint_test.go
@@ -2,6 +2,7 @@ package consul
 
 import (
 	"crypto/x509"
+	"fmt"
 	"os"
 	"strings"
 	"testing"
@@ -18,60 +19,83 @@ import (
 func TestAutoEncryptSign(t *testing.T) {
 	t.Parallel()
 
+	goodRoot := "../../test/ca_path/cert1.crt"
+	badRoot := "../../test/ca/root.cer"
 	dir, s := testServerWithConfig(t, func(c *Config) {
 		c.AutoEncryptAllowTLS = true
 		c.Bootstrap = true
-		c.CAFile = "../../test/client_certs/rootca.crt"
-		c.CertFile = "../../test/client_certs/server.crt"
-		c.KeyFile = "../../test/client_certs/server.key"
+		c.CAFile = goodRoot
+		c.CertFile = "../../test/key/ourdomain.cer"
+		c.KeyFile = "../../test/key/ourdomain.key"
 	})
 	defer os.RemoveAll(dir)
 	defer s.Shutdown()
-	c := tlsutil.Config{AutoEncryptTLS: true}
-	codec := insecureRPCClient(t, s, c)
-	defer codec.Close()
 
-	testrpc.WaitForLeader(t, s.RPC, "dc1")
-
-	// Generate a CSR and request signing
-	id := &connect.SpiffeIDAgent{
-		Host:       strings.TrimSuffix("domain", "."),
-		Datacenter: "dc1",
-		Agent:      "uuid",
+	type variant struct {
+		Config   tlsutil.Config
+		RPCError bool
 	}
 
-	// Create a new private key
-	pk, _, err := connect.GeneratePrivateKey()
-	require.NoError(t, err)
-
-	// Create a CSR.
-	csr, err := connect.CreateCSR(id, pk)
-	require.NoError(t, err)
-	require.NotEmpty(t, csr)
-	args := &structs.CASignRequest{
-		Datacenter: "dc1",
-		CSR:        csr,
+	variants := []variant{
+		{Config: tlsutil.Config{AutoEncryptTLS: true}, RPCError: false},
+		{Config: tlsutil.Config{AutoEncryptTLS: true, VerifyServerHostname: false, CAFile: goodRoot}, RPCError: false},
+		{Config: tlsutil.Config{AutoEncryptTLS: true, VerifyServerHostname: true, CAFile: goodRoot}, RPCError: false},
+		{Config: tlsutil.Config{AutoEncryptTLS: true, VerifyServerHostname: false, CAFile: badRoot}, RPCError: false},
+		{Config: tlsutil.Config{AutoEncryptTLS: true, VerifyServerHostname: true, CAFile: badRoot}, RPCError: true},
 	}
-	var reply structs.SignResponse
-	require.NoError(t, msgpackrpc.CallWithCodec(codec, "AutoEncrypt.Sign", args, &reply))
 
-	// Get the current CA
-	state := s.fsm.State()
-	_, ca, err := state.CARootActive(nil)
-	require.NoError(t, err)
+	for i, v := range variants {
+		info := fmt.Sprintf("case %d", i)
+		codec := insecureRPCClient(t, s, v.Config)
+		defer codec.Close()
 
-	// Verify that the cert is signed by the CA
-	roots := x509.NewCertPool()
-	assert.True(t, roots.AppendCertsFromPEM([]byte(ca.RootCert)))
-	leaf, err := connect.ParseCert(reply.IssuedCert.CertPEM)
-	require.NoError(t, err)
-	_, err = leaf.Verify(x509.VerifyOptions{
-		Roots: roots,
-	})
-	require.NoError(t, err)
+		testrpc.WaitForLeader(t, s.RPC, "dc1")
 
-	// Verify other fields
-	require.Equal(t, "uuid", reply.IssuedCert.Agent)
-	require.Len(t, reply.ManualCARoots, 1)
-	require.Len(t, reply.ConnectCARoots.Roots, 1)
+		// Generate a CSR and request signing
+		id := &connect.SpiffeIDAgent{
+			Host:       strings.TrimSuffix("domain", "."),
+			Datacenter: "dc1",
+			Agent:      "uuid",
+		}
+
+		// Create a new private key
+		pk, _, err := connect.GeneratePrivateKey()
+		require.NoError(t, err, info)
+
+		// Create a CSR.
+		csr, err := connect.CreateCSR(id, pk)
+		require.NoError(t, err, info)
+		require.NotEmpty(t, csr, info)
+		args := &structs.CASignRequest{
+			Datacenter: "dc1",
+			CSR:        csr,
+		}
+		var reply structs.SignResponse
+		err = msgpackrpc.CallWithCodec(codec, "AutoEncrypt.Sign", args, &reply)
+		if v.RPCError {
+			require.Error(t, err, info)
+		} else {
+			require.NoError(t, err, info)
+		}
+
+		// Get the current CA
+		state := s.fsm.State()
+		_, ca, err := state.CARootActive(nil)
+		require.NoError(t, err, info)
+
+		// Verify that the cert is signed by the CA
+		roots := x509.NewCertPool()
+		assert.True(t, roots.AppendCertsFromPEM([]byte(ca.RootCert)))
+		leaf, err := connect.ParseCert(reply.IssuedCert.CertPEM)
+		require.NoError(t, err, info)
+		_, err = leaf.Verify(x509.VerifyOptions{
+			Roots: roots,
+		})
+		require.NoError(t, err, info)
+
+		// Verify other fields
+		require.Equal(t, "uuid", reply.IssuedCert.Agent, info)
+		require.Len(t, reply.ManualCARoots, 1, info)
+		require.Len(t, reply.ConnectCARoots.Roots, 1, info)
+	}
 }
