fc69b721c6b2ed6dd5587538d7b37f32b6e0bb67
John Cowen <johncowen@users.noreply.github.com>
2019-05-15 14:44:57 +0100

    ui: Reconcile ember-data store when records are deleted via blocking (#5745)
    * ui: Reconciliate ember-data store when records are deleted via blocking
    
    Currently we are barely using the ember-data store/cache, but it will
    still cache records in the store even though technically we aren't using
    it.
    
    This adds a SyncTime to every record that uses blocking queries so we
    can delete older records from the ember-data cache to prevent them
    building up
    
    * ui: Add basic timestamp method we can access from tests, fixup tests
    
    Adds a timestamp method that we can access from within tests so we can
    test that the SyncTime is being set.
    
    There is probably a better way to do this, but this is also probably the
    simplest approach - we are also likely to revisit this at a later date

--- ui-v2/app/serializers/application.js
+++ ui-v2/app/serializers/application.js
@@ -1,6 +1,6 @@
 import Serializer from 'ember-data/serializers/rest';
 
-import { get } from '@ember/object';
+import { set } from '@ember/object';
 import {
   HEADERS_SYMBOL as HTTP_HEADERS_SYMBOL,
   HEADERS_INDEX as HTTP_HEADERS_INDEX,
@@ -44,14 +44,17 @@ export default Serializer.extend({
       requestType
     );
   },
+  timestamp: function() {
+    return new Date().getTime();
+  },
   normalizeMeta: function(store, primaryModelClass, headers, payload, id, requestType) {
     const meta = {
       cursor: headers[HTTP_HEADERS_INDEX],
-      date: headers['date'],
     };
     if (requestType === 'query') {
-      meta.ids = payload.map(item => {
-        return get(item, this.primaryKey);
+      meta.date = this.timestamp();
+      payload.forEach(function(item) {
+        set(item, 'SyncTime', meta.date);
       });
     }
     return meta;
