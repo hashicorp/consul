8de55648301b5974e5f98a7bb9adb08dfeb53e3b
John Cowen <jcowen@hashicorp.com>
2019-05-16 17:16:12 +0000

    uiL Update page objects to be able to find new things in acc tests

--- ui-v2/tests/pages/dc/acls/roles/index.js
+++ ui-v2/tests/pages/dc/acls/roles/index.js
@@ -1,11 +1,22 @@
-export default function(visitable, deletable, creatable, clickable, attribute, collection, filter) {
+export default function(
+  visitable,
+  deletable,
+  creatable,
+  clickable,
+  attribute,
+  collection,
+  text,
+  filter
+) {
   return creatable({
     visit: visitable('/:dc/acls/roles'),
     roles: collection(
       '[data-test-tabular-row]',
       deletable({
         name: attribute('data-test-role', '[data-test-role]'),
-        policy: clickable('a'),
+        description: text('[data-test-description]'),
+        policy: text('[data-test-policy].policy', { multiple: true }),
+        serviceIdentity: text('[data-test-policy].policy-service-identity', { multiple: true }),
         actions: clickable('label'),
       })
     ),
