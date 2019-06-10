33bfddb905176a5d48aafc386978fabbb762bfa1
John Cowen <johncowen@users.noreply.github.com>
2019-06-10 23:55:30 +0100

    ui: For writing a ContentPath, we also need to cope with testing... (#5945)
    ...and potentially more environments
    
    Testing has more additional things in a separate index.html in `tests/`
    
    This make the entire thing a little saner and uses just javascriopt
    template literals instead of a pseudo handbrake synatx for our
    templating of these files.
    
    Intead of just templating the entire file this way, we still only
    template `{{content-for 'head'}}` and `{{content-for 'body'}}`
    in this way to ensure we support other plugins/addons

---
+++ ui-v2/lib/startup/templates/head.html.js
@@ -0,0 +1,37 @@
+module.exports = ({ appName, environment, rootURL, config }) => `
+  <!-- CONSUL_VERSION: ${config.CONSUL_VERSION} -->
+  <script>
+    var setConfig = function(appName, config) {
+      var $meta = document.querySelector('meta[name="' + appName + '/config/environment"]');
+      var defaultConfig = JSON.parse(decodeURIComponent($meta.getAttribute('content')));
+      (
+        function set(blob, config) {
+          Object.keys(config).forEach(
+            function(key) {
+              var value = config[key];
+              if(Object.prototype.toString.call(value) === '[object Object]') {
+                set(blob[key], config[key]);
+              } else {
+                blob[key] = config[key];
+              }
+            }
+          );
+        }
+      )(defaultConfig, config);
+      $meta.setAttribute('content', encodeURIComponent(JSON.stringify(defaultConfig)));
+    }
+    setConfig(
+      '${appName}',
+      {
+        rootURL: '${rootURL}'
+      }
+    );
+  </script>
+  <link rel="icon" type="image/png" href="${rootURL}assets/favicon-32x32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="${rootURL}assets/favicon-16x16.png" sizes="16x16">
+  <link integrity="" rel="stylesheet" href="${rootURL}assets/vendor.css">
+  <link integrity="" rel="stylesheet" href="${rootURL}assets/${appName}.css">
+  ${
+    environment === 'test' ? `<link rel="stylesheet" href="${rootURL}assets/test-support.css">` : ``
+  }
+`;
