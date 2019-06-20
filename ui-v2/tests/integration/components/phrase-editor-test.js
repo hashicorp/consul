e69942e9a409714c20015275d25536e465ff360e
John Cowen <jcowen@hashicorp.com>
2019-06-20 12:27:20 +0000

    ui: add some phrase-editor integration tests

--- ui-v2/tests/integration/components/phrase-editor-test.js
+++ ui-v2/tests/integration/components/phrase-editor-test.js
@@ -1,34 +1,50 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import { get } from '@ember/object';
 
 moduleForComponent('phrase-editor', 'Integration | Component | phrase editor', {
   integration: true,
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{phrase-editor}}`);
-
-  assert.equal(
+test('it renders a phrase', function(assert) {
+  this.set('value', ['phrase']);
+  this.render(hbs`{{phrase-editor value=value}}`);
+  assert.notEqual(
     this.$()
       .text()
-      .trim(),
-    'Search'
+      .trim()
+      .indexOf('phrase'),
+    -1
   );
-
-  // Template block usage:
-  this.render(hbs`
-    {{#phrase-editor}}
-      template block text
-    {{/phrase-editor}}
-  `);
-
-  assert.equal(
+});
+test('it calls onchange when a phrase is removed by clicking the phrase remove button and refocuses', function(assert) {
+  assert.expect(3);
+  this.set('value', ['phrase']);
+  this.on('change', function(e) {
+    assert.equal(e.target.value.length, 0);
+  });
+  this.render(hbs`{{phrase-editor value=value onchange=(action 'change')}}`);
+  assert.notEqual(
     this.$()
       .text()
-      .trim(),
-    'Search'
+      .trim()
+      .indexOf('phrase'),
+    -1
   );
+  const $input = this.$('input');
+  const $button = this.$('button');
+  $button.trigger('click');
+  assert.equal(document.activeElement, $input.get(0));
+});
+test('it calls onchange when a phrase is added', function(assert) {
+  assert.expect(1);
+  this.on('change', function(e) {
+    assert.equal(e.target.value.length, 2);
+  });
+  this.set('value', ['phrase']);
+  this.render(hbs`{{phrase-editor value=value onchange=(action 'change')}}`);
+  const $input = this.$('input');
+  $input.get(0).value = 'phrase 2';
+  $input.trigger('input');
+  $input.trigger('search');
 });
