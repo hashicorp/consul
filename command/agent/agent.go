53b25fbe383a42fa1cc80c9883bf0adaa9d79bb6
Hans Hasselberg <me@hans.io>
2019-06-28 00:57:29 +0200

    agent: check for gossip encrypt key (#6037)

--- command/agent/agent.go
+++ command/agent/agent.go
@@ -238,7 +238,7 @@ func (c *cmd) run(args []string) int {
 	c.UI.Info(fmt.Sprintf("  Cluster Addr: %v (LAN: %d, WAN: %d)", config.AdvertiseAddrLAN,
 		config.SerfPortLAN, config.SerfPortWAN))
 	c.UI.Info(fmt.Sprintf("       Encrypt: Gossip: %v, TLS-Outgoing: %v, TLS-Incoming: %v, Auto-Encrypt-TLS: %t",
-		config.EncryptKey, config.VerifyOutgoing, config.VerifyIncoming, config.AutoEncryptTLS || config.AutoEncryptAllowTLS))
+		config.EncryptKey != "", config.VerifyOutgoing, config.VerifyIncoming, config.AutoEncryptTLS || config.AutoEncryptAllowTLS))
 
 	// Enable log streaming
 	c.UI.Info("")
