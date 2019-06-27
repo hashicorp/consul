7f6532f7ba713e07438f075e70bc57c2f162c3fc
Hans Hasselberg <me@hans.io>
2019-06-27 21:04:59 +0200

    t.run

--- agent/retry_join_test.go
+++ agent/retry_join_test.go
@@ -21,42 +21,38 @@ func TestAgentRetryNewDiscover(t *testing.T) {
 func TestAgentRetryJoinAddrs(t *testing.T) {
 	d, err := newDiscover()
 	require.NoError(t, err)
-	type variant struct {
+
+	tests := []struct {
+		name     string
 		input    []string
 		expected []string
-	}
-
-	variants := []variant{
-		// handles nil
-		{nil, []string{}},
-
-		// handles empty input
-		{[]string{}, []string{}},
-
-		// handles one element
-		{[]string{"192.168.0.12"}, []string{"192.168.0.12"}},
-
-		// handles two elements
-		{
+	}{
+		{"handles nil", nil, []string{}},
+		{"handles empty input", []string{}, []string{}},
+		{"handles one element",
+			[]string{"192.168.0.12"},
+			[]string{"192.168.0.12"},
+		},
+		{"handles two elements",
 			[]string{"192.168.0.12", "192.168.0.13"},
 			[]string{"192.168.0.12", "192.168.0.13"},
 		},
-
-		// tries to resolve aws things, which fails but that is fine
-		{
+		{"tries to resolve aws things, which fails but that is fine",
 			[]string{"192.168.0.12", "provider=aws region=eu-west-1 tag_key=consul tag_value=tag access_key_id=a secret_access_key=a"},
 			[]string{"192.168.0.12"},
 		},
 	}
-	for i, v := range variants {
-		var buf bytes.Buffer
-		logger := log.New(&buf, "logger: ", log.Lshortfile)
-		require.Equal(t, v.expected, retryJoinAddrs(d, "LAN", v.input, logger), buf.String())
-		if i == 4 {
-			require.Contains(t, buf.String(), `Using provider "aws"`)
-		}
+	for i, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			var buf bytes.Buffer
+			logger := log.New(&buf, "logger: ", log.Lshortfile)
+			require.Equal(t, test.expected, retryJoinAddrs(d, "LAN", test.input, logger), buf.String())
+			if i == 4 {
+				require.Contains(t, buf.String(), `Using provider "aws"`)
+			}
+		})
 	}
-
-	// handles nil discover
-	require.Equal(t, []string{}, retryJoinAddrs(nil, "LAN", []string{"a"}, nil))
+	t.Run("handles nil discover", func(t *testing.T) {
+		require.Equal(t, []string{}, retryJoinAddrs(nil, "LAN", []string{"a"}, nil))
+	})
 }
