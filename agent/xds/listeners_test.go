0af97a4fe217bcccc1cfe9c8a8924758cc1fe5f8
Kyle Havlovitz <kylehav@gmail.com>
2019-07-03 17:23:50 -0700

    Fix listeners test cases for bind addr/port

--- agent/xds/listeners_test.go
+++ agent/xds/listeners_test.go
@@ -34,19 +34,22 @@ func TestListenersFromSnapshot(t *testing.T) {
 			setup:  nil, // Default snapshot
 		},
 		{
-			name: "listener-bind-address",
+			name:   "listener-bind-address",
+			create: proxycfg.TestConfigSnapshot,
 			setup: func(snap *proxycfg.ConfigSnapshot) {
 				snap.Proxy.Config["bind_address"] = "127.0.0.2"
 			},
 		},
 		{
-			name: "listener-bind-port",
+			name:   "listener-bind-port",
+			create: proxycfg.TestConfigSnapshot,
 			setup: func(snap *proxycfg.ConfigSnapshot) {
 				snap.Proxy.Config["bind_port"] = 8888
 			},
 		},
 		{
-			name: "listener-bind-address-port",
+			name:   "listener-bind-address-port",
+			create: proxycfg.TestConfigSnapshot,
 			setup: func(snap *proxycfg.ConfigSnapshot) {
 				snap.Proxy.Config["bind_address"] = "127.0.0.2"
 				snap.Proxy.Config["bind_port"] = 8888
