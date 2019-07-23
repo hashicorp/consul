2d267e77453d01b0eaac999d2e8827721f0a35b5
John Cowen <jcowen@hashicorp.com>
2019-07-23 12:40:25 +0000

    ui: Adds readonly meta data to the serviceInstance and node detail pages

--- ui-v2/tests/pages/dc/services/instance.js
+++ ui-v2/tests/pages/dc/services/instance.js
@@ -2,7 +2,14 @@ export default function(visitable, attribute, collection, text, radiogroup) {
   return {
     visit: visitable('/:dc/services/:service/:node/:id'),
     externalSource: attribute('data-test-external-source', 'h1 span'),
-    tabs: radiogroup('tab', ['service-checks', 'node-checks', 'addresses', 'upstreams', 'tags']),
+    tabs: radiogroup('tab', [
+      'service-checks',
+      'node-checks',
+      'addresses',
+      'upstreams',
+      'tags',
+      'meta-data',
+    ]),
     serviceChecks: collection('#service-checks [data-test-healthchecks] li', {}),
     nodeChecks: collection('#node-checks [data-test-healthchecks] li', {}),
     upstreams: collection('#upstreams [data-test-tabular-row]', {
@@ -14,6 +21,7 @@ export default function(visitable, attribute, collection, text, radiogroup) {
     addresses: collection('#addresses [data-test-tabular-row]', {
       address: text('[data-test-address]'),
     }),
+    metaData: collection('#meta-data [data-test-tabular-row]', {}),
     proxy: {
       type: attribute('data-test-proxy-type', '[data-test-proxy-type]'),
       destination: attribute('data-test-proxy-destination', '[data-test-proxy-destination]'),
