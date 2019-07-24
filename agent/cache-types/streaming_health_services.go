ee7b6d62936e1b3f2a5c3542b039b522f0394d21
Kyle Havlovitz <kylehav@gmail.com>
2019-07-24 03:19:13 -0700

    Pull out the streaming watch code to be more testable/reusable

--- agent/cache-types/streaming_health_services.go
+++ agent/cache-types/streaming_health_services.go
@@ -1,9 +1,7 @@
 package cachetype
 
 import (
-	"context"
 	"fmt"
-	"sync"
 	"time"
 
 	"github.com/hashicorp/consul/agent/consul/stream"
@@ -22,170 +20,65 @@ const (
 // StreamingHealthServices supports fetching discovering service instances via the
 // catalog using the streaming gRPC endpoint.
 type StreamingHealthServices struct {
-	Client stream.ConsulClient
+	Client StreamingClient
 }
 
 func (c *StreamingHealthServices) Fetch(opts cache.FetchOptions, req cache.Request) (cache.FetchResult, error) {
-	var result cache.FetchResult
-
 	// The request should be a ServiceSpecificRequest.
 	reqReal, ok := req.(*structs.ServiceSpecificRequest)
 	if !ok {
-		return result, fmt.Errorf(
+		return cache.FetchResult{}, fmt.Errorf(
 			"Internal cache failure: request wrong type: %T", req)
 	}
 
-	// Start a new subscription if one isn't already running.
-	var sub *subscriber
-	if opts.LastResult == nil || opts.LastResult.State == nil {
-		sub = &subscriber{
-			client:   c.Client,
-			service:  reqReal.ServiceName,
-			resultCh: make(chan interface{}, 0),
+	subscribeReq := stream.SubscribeRequest{
+		Topic: stream.Topic_ServiceHealth,
+		Key:   reqReal.ServiceName,
+	}
+	handler := healthServicesHandler{
+		state: make(map[string]structs.CheckServiceNode),
+	}
+	indexFunc := func(v interface{}) uint64 {
+		if v, ok := v.(*structs.IndexedCheckServiceNodes); ok {
+			return v.Index
 		}
-		go sub.run()
-	} else {
-		sub = opts.LastResult.State.(*subscriber)
+		return 0
 	}
 
-	result.State = sub
-
-	// If the requested index is lower than what we've already seen, return immediately.
-	sub.lock.RLock()
-	if sub.lastResult.Index > opts.MinIndex {
-		result.Value = &sub.lastResult
-		result.Index = sub.lastResult.Index
-		sub.lock.RUnlock()
-		return result, nil
-	}
-	sub.lock.RUnlock()
-
-	// Wait for an update based on the index we're blocking on.
-	timeout := time.After(StreamTimeout)
-WAIT:
-	select {
-	case r := <-sub.resultCh:
-		switch reply := r.(type) {
-		case *structs.IndexedCheckServiceNodes:
-			if reply.Index <= opts.MinIndex {
-				goto WAIT
-			}
-			result.Value = reply
-			result.Index = reply.Index
-		case error:
-			result.Value = reply
-		}
-	case <-timeout:
-		sub.lock.RLock()
-		result.Value = &sub.lastResult
-		result.Index = sub.lastResult.Index
-		sub.lock.RUnlock()
-	}
-
-	return result, nil
+	return watchSubscriber(c.Client, opts, subscribeReq, &handler, indexFunc)
 }
 
 func (c *StreamingHealthServices) SupportsBlocking() bool {
 	return true
 }
 
-// subscriber runs a subscription for a single topic/key combination.
-type subscriber struct {
-	client     stream.ConsulClient
-	service    string
-	lastResult structs.IndexedCheckServiceNodes
-	lastUpdate time.Time
-	resultCh   chan interface{}
-
-	cancelFunc func()
-
-	lock sync.RWMutex
+// healthServicesHandler maintains a view of the health of a service
+// based on incoming events from a Subscriber.
+type healthServicesHandler struct {
+	state map[string]structs.CheckServiceNode
 }
 
-// Close stops the subscriber by cancelling the context.
-func (s *subscriber) Close() error {
-	s.cancelFunc()
-	return nil
+// HandleEvent updates the handler's state based on register/deregister events.
+func (h *healthServicesHandler) HandleEvent(event *stream.Event) {
+	serviceHealth := event.GetServiceHealth()
+	node := serviceHealth.ServiceNode
+	id := fmt.Sprintf("%s/%s", node.Node.Node, node.Service.ID)
+
+	switch serviceHealth.Op {
+	case stream.CatalogOp_Register:
+		checkServiceNode := stream.FromCheckServiceNode(serviceHealth.ServiceNode)
+		h.state[id] = checkServiceNode
+	case stream.CatalogOp_Deregister:
+		delete(h.state, id)
+	}
 }
 
-// run starts a Subscribe call for the subscriber's key, receives the initial
-// snapshot of state and then sends the state to the watcher as soon as its
-// watched index requirement is met.
-func (s *subscriber) run() {
-	ctx, cancel := context.WithCancel(context.Background())
-	s.cancelFunc = cancel
-	defer s.cancelFunc()
-
-	// Start a new Subscribe call.
-	streamHandle, err := s.client.Subscribe(ctx, &stream.SubscribeRequest{
-		Topic: stream.Topic_ServiceHealth,
-		Key:   s.service,
-	})
-
-	// If something went wrong setting up the stream, return the error.
-	if err != nil {
-		// Do a blocking send since there should always be a Fetch goroutine watching
-		// on the startup of a subscriber.
-		s.resultCh <- err
-		return
+// State returns the current view of the state based on the events seen.
+func (h *healthServicesHandler) State(idx uint64) interface{} {
+	var result structs.IndexedCheckServiceNodes
+	for _, node := range h.state {
+		result.Nodes = append(result.Nodes, node)
 	}
-
-	// Run the main loop to receive events.
-	var snapshotDone bool
-	index := uint64(1)
-	state := make(map[string]structs.CheckServiceNode)
-	for {
-		event, err := streamHandle.Recv()
-		if err != nil {
-			// Do a non-blocking send of the error, in case there's no Fetch watching.
-			select {
-			case s.resultCh <- err:
-			default:
-			}
-			return
-		}
-
-		// Update our version of the state based on the event/op.
-		switch event.Topic {
-		case stream.Topic_ServiceHealth:
-			serviceHealth := event.GetServiceHealth()
-			node := serviceHealth.ServiceNode
-			id := fmt.Sprintf("%s/%s", node.Node.Node, node.Service.ID)
-			if event.Index > index {
-				index = event.Index
-			}
-
-			switch serviceHealth.Op {
-			case stream.CatalogOp_Register:
-				checkServiceNode := stream.FromCheckServiceNode(serviceHealth.ServiceNode)
-				state[id] = checkServiceNode
-			case stream.CatalogOp_Deregister:
-				delete(state, id)
-			}
-		case stream.Topic_EndOfSnapshot:
-			snapshotDone = true
-		}
-
-		// Don't go any further if the snapshot is still being sent.
-		if !snapshotDone {
-			continue
-		}
-
-		// Construct the most recent view of the state.
-		var result structs.IndexedCheckServiceNodes
-		for _, node := range state {
-			result.Nodes = append(result.Nodes, node)
-		}
-		result.Index = index
-		s.lock.Lock()
-		s.lastResult = result
-		s.lock.Unlock()
-
-		// Send the new view of the state to the watcher.
-		select {
-		case s.resultCh <- &result:
-			s.lastUpdate = time.Now()
-		default:
-		}
-	}
+	result.Index = idx
+	return &result
 }
