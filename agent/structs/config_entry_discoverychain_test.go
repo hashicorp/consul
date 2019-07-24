1dbd92e0918f3fe682c633ee9a8583087aa256a7
R.B. Boyer <rb@hashicorp.com>
2019-07-23 20:50:23 -0500

    connect: validate and test more of the L7 config entries (#6156)

--- agent/structs/config_entry_discoverychain_test.go
+++ agent/structs/config_entry_discoverychain_test.go
@@ -1,23 +1,265 @@
 package structs
 
 import (
+	"bytes"
+	"fmt"
+	"strings"
 	"testing"
 	"time"
 
+	"github.com/hashicorp/consul/acl"
 	"github.com/stretchr/testify/require"
 )
+
+func TestConfigEntries_ListRelatedServices_AndACLs(t *testing.T) {
+	// This test tests both of these because they are related functions.
+	t.Parallel()
+
+	newServiceACL := func(t *testing.T, canRead, canWrite []string) acl.Authorizer {
+		var buf bytes.Buffer
+		for _, s := range canRead {
+			buf.WriteString(fmt.Sprintf("service %q { policy = %q }\n", s, "read"))
+		}
+		for _, s := range canWrite {
+			buf.WriteString(fmt.Sprintf("service %q { policy = %q }\n", s, "write"))
+		}
+
+		policy, err := acl.NewPolicyFromSource("", 0, buf.String(), acl.SyntaxCurrent, nil)
+		require.NoError(t, err)
+
+		authorizer, err := acl.NewPolicyAuthorizer(acl.DenyAll(), []*acl.Policy{policy}, nil)
+		require.NoError(t, err)
+		return authorizer
+	}
+
+	type testACL struct {
+		name       string
+		authorizer acl.Authorizer
+		canRead    bool
+		canWrite   bool
+	}
+
+	defaultDenyCase := testACL{
+		name:       "deny",
+		authorizer: newServiceACL(t, nil, nil),
+		canRead:    false,
+		canWrite:   false,
+	}
+	readTestCase := testACL{
+		name:       "can read test",
+		authorizer: newServiceACL(t, []string{"test"}, nil),
+		canRead:    true,
+		canWrite:   false,
+	}
+	writeTestCase := testACL{
+		name:       "can write test",
+		authorizer: newServiceACL(t, nil, []string{"test"}),
+		canRead:    true,
+		canWrite:   true,
+	}
+	writeTestCaseDenied := testACL{
+		name:       "cannot write test",
+		authorizer: newServiceACL(t, nil, []string{"test"}),
+		canRead:    true,
+		canWrite:   false,
+	}
+
+	for _, tc := range []struct {
+		name           string
+		entry          discoveryChainConfigEntry
+		expectServices []string
+		expectACLs     []testACL
+	}{
+		{
+			name: "resolver: self",
+			entry: &ServiceResolverConfigEntry{
+				Kind: ServiceResolver,
+				Name: "test",
+			},
+			expectServices: nil,
+			expectACLs: []testACL{
+				defaultDenyCase,
+				readTestCase,
+				writeTestCase,
+			},
+		},
+		{
+			name: "resolver: redirect",
+			entry: &ServiceResolverConfigEntry{
+				Kind: ServiceResolver,
+				Name: "test",
+				Redirect: &ServiceResolverRedirect{
+					Service: "other",
+				},
+			},
+			expectServices: []string{"other"},
+			expectACLs: []testACL{
+				defaultDenyCase,
+				readTestCase,
+				writeTestCaseDenied,
+				{
+					name:       "can write test (with other:read)",
+					authorizer: newServiceACL(t, []string{"other"}, []string{"test"}),
+					canRead:    true,
+					canWrite:   true,
+				},
+			},
+		},
+		{
+			name: "resolver: failover",
+			entry: &ServiceResolverConfigEntry{
+				Kind: ServiceResolver,
+				Name: "test",
+				Subsets: map[string]ServiceResolverSubset{
+					"foo": {OnlyPassing: true},
+					"bar": {OnlyPassing: true},
+				},
+				Failover: map[string]ServiceResolverFailover{
+					"foo": ServiceResolverFailover{
+						Service: "other1",
+					},
+					"bar": ServiceResolverFailover{
+						Service: "other2",
+					},
+				},
+			},
+			expectServices: []string{"other1", "other2"},
+			expectACLs: []testACL{
+				defaultDenyCase,
+				readTestCase,
+				writeTestCaseDenied,
+				{
+					name:       "can write test (with other1:read and other2:read)",
+					authorizer: newServiceACL(t, []string{"other1", "other2"}, []string{"test"}),
+					canRead:    true,
+					canWrite:   true,
+				},
+			},
+		},
+		{
+			name: "splitter: self",
+			entry: &ServiceSplitterConfigEntry{
+				Kind: ServiceSplitter,
+				Name: "test",
+				Splits: []ServiceSplit{
+					{Weight: 100},
+				},
+			},
+			expectServices: nil,
+			expectACLs: []testACL{
+				defaultDenyCase,
+				readTestCase,
+				writeTestCase,
+			},
+		},
+		{
+			name: "splitter: some",
+			entry: &ServiceSplitterConfigEntry{
+				Kind: ServiceSplitter,
+				Name: "test",
+				Splits: []ServiceSplit{
+					{Weight: 25, Service: "b"},
+					{Weight: 25, Service: "a"},
+					{Weight: 50, Service: "c"},
+				},
+			},
+			expectServices: []string{"a", "b", "c"},
+			expectACLs: []testACL{
+				defaultDenyCase,
+				readTestCase,
+				writeTestCaseDenied,
+				{
+					name:       "can write test (with a:read, b:read, and c:read)",
+					authorizer: newServiceACL(t, []string{"a", "b", "c"}, []string{"test"}),
+					canRead:    true,
+					canWrite:   true,
+				},
+			},
+		},
+		{
+			name: "router: self",
+			entry: &ServiceRouterConfigEntry{
+				Kind: ServiceRouter,
+				Name: "test",
+			},
+			expectServices: []string{"test"},
+			expectACLs: []testACL{
+				defaultDenyCase,
+				readTestCase,
+				writeTestCase,
+			},
+		},
+		{
+			name: "router: some",
+			entry: &ServiceRouterConfigEntry{
+				Kind: ServiceRouter,
+				Name: "test",
+				Routes: []ServiceRoute{
+					{
+						Match: &ServiceRouteMatch{HTTP: &ServiceRouteHTTPMatch{
+							PathPrefix: "/foo",
+						}},
+						Destination: &ServiceRouteDestination{
+							Service: "foo",
+						},
+					},
+					{
+						Match: &ServiceRouteMatch{HTTP: &ServiceRouteHTTPMatch{
+							PathPrefix: "/bar",
+						}},
+						Destination: &ServiceRouteDestination{
+							Service: "bar",
+						},
+					},
+				},
+			},
+			expectServices: []string{"bar", "foo", "test"},
+			expectACLs: []testACL{
+				defaultDenyCase,
+				readTestCase,
+				writeTestCaseDenied,
+				{
+					name:       "can write test (with foo:read and bar:read)",
+					authorizer: newServiceACL(t, []string{"foo", "bar"}, []string{"test"}),
+					canRead:    true,
+					canWrite:   true,
+				},
+			},
+		},
+	} {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			// sanity check inputs
+			require.NoError(t, tc.entry.Normalize())
+			require.NoError(t, tc.entry.Validate())
+
+			got := tc.entry.ListRelatedServices()
+			require.Equal(t, tc.expectServices, got)
+
+			for _, a := range tc.expectACLs {
+				a := a
+				t.Run(a.name, func(t *testing.T) {
+					require.Equal(t, a.canRead, tc.entry.CanRead(a.authorizer))
+					require.Equal(t, a.canWrite, tc.entry.CanWrite(a.authorizer))
+				})
+			}
+		})
+	}
+}
 
 func TestServiceResolverConfigEntry(t *testing.T) {
 	t.Parallel()
 
-	for _, tc := range []struct {
+	type testcase struct {
 		name         string
 		entry        *ServiceResolverConfigEntry
 		normalizeErr string
 		validateErr  string
 		// check is called between normalize and validate
 		check func(t *testing.T, entry *ServiceResolverConfigEntry)
-	}{
+	}
+
+	cases := []testcase{
 		{
 			name:         "nil",
 			entry:        nil,
@@ -251,7 +493,39 @@ func TestServiceResolverConfigEntry(t *testing.T) {
 			},
 			validateErr: "Bad ConnectTimeout",
 		},
-	} {
+	}
+
+	// Bulk add a bunch of similar validation cases.
+	for _, invalidSubset := range invalidSubsetNames {
+		tc := testcase{
+			name: "invalid subset name: " + invalidSubset,
+			entry: &ServiceResolverConfigEntry{
+				Kind: ServiceResolver,
+				Name: "test",
+				Subsets: map[string]ServiceResolverSubset{
+					invalidSubset: {OnlyPassing: true},
+				},
+			},
+			validateErr: fmt.Sprintf("Subset %q is invalid", invalidSubset),
+		}
+		cases = append(cases, tc)
+	}
+
+	for _, goodSubset := range validSubsetNames {
+		tc := testcase{
+			name: "valid subset name: " + goodSubset,
+			entry: &ServiceResolverConfigEntry{
+				Kind: ServiceResolver,
+				Name: "test",
+				Subsets: map[string]ServiceResolverSubset{
+					goodSubset: {OnlyPassing: true},
+				},
+			},
+		}
+		cases = append(cases, tc)
+	}
+
+	for _, tc := range cases {
 		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
 			err := tc.entry.Normalize()
@@ -458,6 +732,372 @@ func TestServiceSplitterConfigEntry(t *testing.T) {
 				return
 			}
 			require.NoError(t, err)
+		})
+	}
+}
+
+func TestServiceRouterConfigEntry(t *testing.T) {
+	t.Parallel()
+
+	httpMatch := func(http *ServiceRouteHTTPMatch) *ServiceRouteMatch {
+		return &ServiceRouteMatch{HTTP: http}
+	}
+	httpMatchHeader := func(headers ...ServiceRouteHTTPMatchHeader) *ServiceRouteMatch {
+		return httpMatch(&ServiceRouteHTTPMatch{
+			Header: headers,
+		})
+	}
+	httpMatchParam := func(params ...ServiceRouteHTTPMatchQueryParam) *ServiceRouteMatch {
+		return httpMatch(&ServiceRouteHTTPMatch{
+			QueryParam: params,
+		})
+	}
+	toService := func(svc string) *ServiceRouteDestination {
+		return &ServiceRouteDestination{Service: svc}
+	}
+	routeMatch := func(match *ServiceRouteMatch) ServiceRoute {
+		return ServiceRoute{
+			Match:       match,
+			Destination: toService("other"),
+		}
+	}
+	makerouter := func(routes ...ServiceRoute) *ServiceRouterConfigEntry {
+		return &ServiceRouterConfigEntry{
+			Kind:   ServiceRouter,
+			Name:   "test",
+			Routes: routes,
+		}
+	}
+
+	type testcase struct {
+		name         string
+		entry        *ServiceRouterConfigEntry
+		normalizeErr string
+		validateErr  string
+		// check is called between normalize and validate
+		check func(t *testing.T, entry *ServiceRouterConfigEntry)
+	}
+
+	cases := []testcase{
+		{
+			name:         "nil",
+			entry:        nil,
+			normalizeErr: "config entry is nil",
+		},
+		{
+			name:        "no name",
+			entry:       &ServiceRouterConfigEntry{},
+			validateErr: "Name is required",
+		},
+		{
+			name:  "empty",
+			entry: makerouter(),
+		},
+		{
+			name: "1 empty route",
+			entry: makerouter(
+				ServiceRoute{},
+			),
+		},
+
+		{
+			name: "route with path exact",
+			entry: makerouter(routeMatch(httpMatch(&ServiceRouteHTTPMatch{
+				PathExact: "/exact",
+			}))),
+		},
+		{
+			name: "route with bad path exact",
+			entry: makerouter(routeMatch(httpMatch(&ServiceRouteHTTPMatch{
+				PathExact: "no-leading-slash",
+			}))),
+			validateErr: "PathExact doesn't start with '/'",
+		},
+		{
+			name: "route with path prefix",
+			entry: makerouter(routeMatch(httpMatch(&ServiceRouteHTTPMatch{
+				PathPrefix: "/prefix",
+			}))),
+		},
+		{
+			name: "route with bad path prefix",
+			entry: makerouter(routeMatch(httpMatch(&ServiceRouteHTTPMatch{
+				PathPrefix: "no-leading-slash",
+			}))),
+			validateErr: "PathPrefix doesn't start with '/'",
+		},
+		{
+			name: "route with path regex",
+			entry: makerouter(routeMatch(httpMatch(&ServiceRouteHTTPMatch{
+				PathRegex: "/regex",
+			}))),
+		},
+		{
+			name: "route with path exact and prefix",
+			entry: makerouter(routeMatch(httpMatch(&ServiceRouteHTTPMatch{
+				PathExact:  "/exact",
+				PathPrefix: "/prefix",
+			}))),
+			validateErr: "should only contain at most one of PathExact, PathPrefix, or PathRegex",
+		},
+		{
+			name: "route with path exact and regex",
+			entry: makerouter(routeMatch(httpMatch(&ServiceRouteHTTPMatch{
+				PathExact: "/exact",
+				PathRegex: "/regex",
+			}))),
+			validateErr: "should only contain at most one of PathExact, PathPrefix, or PathRegex",
+		},
+		{
+			name: "route with path prefix and regex",
+			entry: makerouter(routeMatch(httpMatch(&ServiceRouteHTTPMatch{
+				PathPrefix: "/prefix",
+				PathRegex:  "/regex",
+			}))),
+			validateErr: "should only contain at most one of PathExact, PathPrefix, or PathRegex",
+		},
+		{
+			name: "route with path exact, prefix, and regex",
+			entry: makerouter(routeMatch(httpMatch(&ServiceRouteHTTPMatch{
+				PathExact:  "/exact",
+				PathPrefix: "/prefix",
+				PathRegex:  "/regex",
+			}))),
+			validateErr: "should only contain at most one of PathExact, PathPrefix, or PathRegex",
+		},
+
+		{
+			name: "route with no name header",
+			entry: makerouter(routeMatch(httpMatchHeader(ServiceRouteHTTPMatchHeader{
+				Present: true,
+			}))),
+			validateErr: "missing required Name field",
+		},
+		{
+			name: "route with header present",
+			entry: makerouter(routeMatch(httpMatchHeader(ServiceRouteHTTPMatchHeader{
+				Name:    "foo",
+				Present: true,
+			}))),
+		},
+		{
+			name: "route with header not present",
+			entry: makerouter(routeMatch(httpMatchHeader(ServiceRouteHTTPMatchHeader{
+				Name:    "foo",
+				Present: true,
+				Invert:  true,
+			}))),
+		},
+		{
+			name: "route with header exact",
+			entry: makerouter(routeMatch(httpMatchHeader(ServiceRouteHTTPMatchHeader{
+				Name:  "foo",
+				Exact: "bar",
+			}))),
+		},
+		{
+			name: "route with header regex",
+			entry: makerouter(routeMatch(httpMatchHeader(ServiceRouteHTTPMatchHeader{
+				Name:  "foo",
+				Regex: "bar",
+			}))),
+		},
+		{
+			name: "route with header prefix",
+			entry: makerouter(routeMatch(httpMatchHeader(ServiceRouteHTTPMatchHeader{
+				Name:   "foo",
+				Prefix: "bar",
+			}))),
+		},
+		{
+			name: "route with header suffix",
+			entry: makerouter(routeMatch(httpMatchHeader(ServiceRouteHTTPMatchHeader{
+				Name:   "foo",
+				Suffix: "bar",
+			}))),
+		},
+		{
+			name: "route with header present and exact",
+			entry: makerouter(routeMatch(httpMatchHeader(ServiceRouteHTTPMatchHeader{
+				Name:    "foo",
+				Present: true,
+				Exact:   "bar",
+			}))),
+			validateErr: "should only contain one of Present, Exact, Prefix, Suffix, or Regex",
+		},
+		{
+			name: "route with header present and regex",
+			entry: makerouter(routeMatch(httpMatchHeader(ServiceRouteHTTPMatchHeader{
+				Name:    "foo",
+				Present: true,
+				Regex:   "bar",
+			}))),
+			validateErr: "should only contain one of Present, Exact, Prefix, Suffix, or Regex",
+		},
+		{
+			name: "route with header present and prefix",
+			entry: makerouter(routeMatch(httpMatchHeader(ServiceRouteHTTPMatchHeader{
+				Name:    "foo",
+				Present: true,
+				Prefix:  "bar",
+			}))),
+			validateErr: "should only contain one of Present, Exact, Prefix, Suffix, or Regex",
+		},
+		{
+			name: "route with header present and suffix",
+			entry: makerouter(routeMatch(httpMatchHeader(ServiceRouteHTTPMatchHeader{
+				Name:    "foo",
+				Present: true,
+				Suffix:  "bar",
+			}))),
+			validateErr: "should only contain one of Present, Exact, Prefix, Suffix, or Regex",
+		},
+		// NOTE: Some combinatoric cases for header operators (some 5 choose 2,
+		// all 5 choose 3, all 5 choose 4, all 5 choose 5) are omitted from
+		// testing.
+
+		////////////////
+		{
+			name: "route with no name query param",
+			entry: makerouter(routeMatch(httpMatchParam(ServiceRouteHTTPMatchQueryParam{
+				Value: "foo",
+			}))),
+			validateErr: "missing required Name field",
+		},
+
+		////////////////
+		{
+			name: "route with no match and prefix rewrite",
+			entry: makerouter(ServiceRoute{
+				Match: nil,
+				Destination: &ServiceRouteDestination{
+					Service:       "other",
+					PrefixRewrite: "/new",
+				},
+			}),
+			validateErr: "cannot make use of PrefixRewrite without configuring either PathExact or PathPrefix",
+		},
+		{
+			name: "route with path prefix match and prefix rewrite",
+			entry: makerouter(ServiceRoute{
+				Match: httpMatch(&ServiceRouteHTTPMatch{
+					PathPrefix: "/api",
+				}),
+				Destination: &ServiceRouteDestination{
+					Service:       "other",
+					PrefixRewrite: "/new",
+				},
+			}),
+		},
+		{
+			name: "route with path exact match and prefix rewrite",
+			entry: makerouter(ServiceRoute{
+				Match: httpMatch(&ServiceRouteHTTPMatch{
+					PathExact: "/api",
+				}),
+				Destination: &ServiceRouteDestination{
+					Service:       "other",
+					PrefixRewrite: "/new",
+				},
+			}),
+		},
+		{
+			name: "route with path regex match and prefix rewrite",
+			entry: makerouter(ServiceRoute{
+				Match: httpMatch(&ServiceRouteHTTPMatch{
+					PathRegex: "/api",
+				}),
+				Destination: &ServiceRouteDestination{
+					Service:       "other",
+					PrefixRewrite: "/new",
+				},
+			}),
+			validateErr: "cannot make use of PrefixRewrite without configuring either PathExact or PathPrefix",
+		},
+		{
+			name: "route with header match and prefix rewrite",
+			entry: makerouter(ServiceRoute{
+				Match: httpMatchHeader(ServiceRouteHTTPMatchHeader{
+					Name:  "foo",
+					Exact: "bar",
+				}),
+				Destination: &ServiceRouteDestination{
+					Service:       "other",
+					PrefixRewrite: "/new",
+				},
+			}),
+			validateErr: "cannot make use of PrefixRewrite without configuring either PathExact or PathPrefix",
+		},
+		{
+			name: "route with header match and prefix rewrite",
+			entry: makerouter(ServiceRoute{
+				Match: httpMatchParam(ServiceRouteHTTPMatchQueryParam{
+					Name:  "foo",
+					Value: "bar",
+				}),
+				Destination: &ServiceRouteDestination{
+					Service:       "other",
+					PrefixRewrite: "/new",
+				},
+			}),
+			validateErr: "cannot make use of PrefixRewrite without configuring either PathExact or PathPrefix",
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.entry.Normalize()
+			if tc.normalizeErr != "" {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), tc.normalizeErr)
+				return
+			}
+			require.NoError(t, err)
+
+			if tc.check != nil {
+				tc.check(t, tc.entry)
+			}
+
+			err = tc.entry.Validate()
+			if tc.validateErr != "" {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), tc.validateErr)
+				return
+			}
+			require.NoError(t, err)
+		})
+	}
+}
+
+var validSubsetNames = []string{
+	"a", "aa", "2a", "a2", "a2a", "a22a",
+	"1", "11", "10", "01",
+	"a-a", "a--a", "a--a--a",
+	"0-0", "0--0", "0--0--0",
+	strings.Repeat("a", 63),
+}
+
+var invalidSubsetNames = []string{
+	"A", "AA", "2A", "A2", "A2A", "A22A",
+	"A-A", "A--A", "A--A--A",
+	" ", " a", "a ", "a a",
+	"_", "_a", "a_", "a_a",
+	".", ".a", "a.", "a.a",
+	"-", "-a", "a-",
+	strings.Repeat("a", 64),
+}
+
+func TestValidateServiceSubset(t *testing.T) {
+	for _, name := range validSubsetNames {
+		t.Run(name, func(t *testing.T) {
+			require.NoError(t, validateServiceSubset(name))
+		})
+	}
+
+	for _, name := range invalidSubsetNames {
+		t.Run(name, func(t *testing.T) {
+			require.Error(t, validateServiceSubset(name))
 		})
 	}
 }
