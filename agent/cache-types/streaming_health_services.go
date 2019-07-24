84a417ae8bb96a00a6506732ca8f8bfc680b1737
Kyle Havlovitz <kylehav@gmail.com>
2019-07-23 18:21:33 -0700

    Simplify the streaming health cachetype

--- agent/cache-types/streaming_health_services.go
+++ agent/cache-types/streaming_health_services.go
@@ -2,7 +2,6 @@ package cachetype
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"sync"
 	"time"
@@ -20,28 +19,10 @@ const (
 	StreamTimeout = 10 * time.Minute
 )
 
-var ErrSnapshotTimeout = errors.New("timed out getting service health results")
-
 // StreamingHealthServices supports fetching discovering service instances via the
 // catalog using the streaming gRPC endpoint.
 type StreamingHealthServices struct {
-	client stream.ConsulClient
-
-	subscriptions map[string]*subscriber
-
-	lock sync.RWMutex
-}
-
-func NewStreamingHealthServices(client stream.ConsulClient) *StreamingHealthServices {
-	return &StreamingHealthServices{
-		client:        client,
-		subscriptions: make(map[string]*subscriber),
-	}
-}
-
-type streamingWatcher struct {
-	index    uint64
-	updateCh chan interface{}
+	Client stream.ConsulClient
 }
 
 func (c *StreamingHealthServices) Fetch(opts cache.FetchOptions, req cache.Request) (cache.FetchResult, error) {
@@ -54,72 +35,51 @@ func (c *StreamingHealthServices) Fetch(opts cache.FetchOptions, req cache.Reque
 			"Internal cache failure: request wrong type: %T", req)
 	}
 
-	service := reqReal.ServiceName
-	watcher := &streamingWatcher{
-		index:    opts.MinIndex,
-		updateCh: make(chan interface{}, 1),
-	}
-
-	// Start a subscriber for this key if one isn't already running.
-	c.lock.Lock()
-	key := req.CacheInfo().Key
-	subscription, ok := c.subscriptions[key]
-	if !ok {
-		subscription = &subscriber{
-			client:  c.client,
-			service: service,
-			key:     key,
-			watcher: watcher,
-			cleanupFunc: func() {
-				c.lock.Lock()
-				delete(c.subscriptions, key)
-				c.lock.Unlock()
-			},
+	// Start a new subscription if one isn't already running.
+	var sub *subscriber
+	if opts.LastResult == nil || opts.LastResult.State == nil {
+		sub = &subscriber{
+			client:   c.Client,
+			service:  reqReal.ServiceName,
+			resultCh: make(chan interface{}, 0),
 		}
-		c.subscriptions[key] = subscription
-		go subscription.run()
+		go sub.run()
 	} else {
-		// Update the watcher if there is an existing subscription.
-		subscription.watcher = watcher
+		sub = opts.LastResult.State.(*subscriber)
 	}
-	c.lock.Unlock()
 
-	// Set up a watcher if the requested index hasn't been reached yet,
-	// or if there hasn't been an initial state returned.
-	subscription.lock.Lock()
-	idx := subscription.lastResult.Index
-	blocking := opts.MinIndex >= idx || idx == 0
-	if blocking {
-		subscription.watcher = watcher
-	}
-	subscription.lock.Unlock()
+	result.State = sub
 
-	// If we don't need to block on a future index, and there's already a
-	// stored result, just return that.
-	if !blocking {
-		subscription.lock.RLock()
-		result.Value = &subscription.lastResult
-		result.Index = subscription.lastResult.Index
-		subscription.lock.RUnlock()
-
+	// If the requested index is lower than what we've already seen, return immediately.
+	sub.lock.RLock()
+	if sub.lastResult.Index > opts.MinIndex {
+		result.Value = &sub.lastResult
+		result.Index = sub.lastResult.Index
+		sub.lock.RUnlock()
 		return result, nil
 	}
+	sub.lock.RUnlock()
 
 	// Wait for an update based on the index we're blocking on.
+	timeout := time.After(StreamTimeout)
+WAIT:
 	select {
-	case r := <-watcher.updateCh:
+	case r := <-sub.resultCh:
 		switch reply := r.(type) {
 		case *structs.IndexedCheckServiceNodes:
+			if reply.Index <= opts.MinIndex {
+				goto WAIT
+			}
 			result.Value = reply
 			result.Index = reply.Index
 		case error:
 			result.Value = reply
 		}
-	case <-time.After(StreamTimeout):
-		c.lock.RLock()
-		result.Value = &subscription.lastResult
-		result.Index = subscription.lastResult.Index
-		c.lock.RUnlock()
+	case <-timeout:
+		sub.lock.RLock()
+		result.Value = &sub.lastResult
+		result.Index = sub.lastResult.Index
+		sub.lock.RUnlock()
 	}
 
 	return result, nil
@@ -131,15 +91,21 @@ func (c *StreamingHealthServices) SupportsBlocking() bool {
 
 // subscriber runs a subscription for a single topic/key combination.
 type subscriber struct {
-	client      stream.ConsulClient
-	service     string
-	key         string
-	watcher     *streamingWatcher
-	lastResult  structs.IndexedCheckServiceNodes
-	lastUpdate  time.Time
-	cleanupFunc func()
+	client     stream.ConsulClient
+	service    string
+	lastResult structs.IndexedCheckServiceNodes
+	lastUpdate time.Time
+	resultCh   chan interface{}
+
+	cancelFunc func()
 
 	lock sync.RWMutex
+}
+
+// Close stops the subscriber by cancelling the context.
+func (s *subscriber) Close() error {
+	s.cancelFunc()
+	return nil
 }
 
 // run starts a Subscribe call for the subscriber's key, receives the initial
@@ -147,8 +113,8 @@ type subscriber struct {
 // watched index requirement is met.
 func (s *subscriber) run() {
 	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	defer s.cleanupFunc()
+	s.cancelFunc = cancel
+	defer s.cancelFunc()
 
 	// Start a new Subscribe call.
 	streamHandle, err := s.client.Subscribe(ctx, &stream.SubscribeRequest{
@@ -157,44 +123,12 @@ func (s *subscriber) run() {
 	})
 
 	// If something went wrong setting up the stream, return the error.
-	sendErr := func(err error) {
-		s.lock.Lock()
-		if s.watcher != nil {
-			s.watcher.updateCh <- err
-		}
-		s.watcher = nil
-		s.lock.Unlock()
-	}
 	if err != nil {
-		sendErr(err)
+		// Do a blocking send since there should always be a Fetch goroutine watching
+		// on the startup of a subscriber.
+		s.resultCh <- err
 		return
 	}
-
-	// Set up a separate goroutine to shut down if the value has been
-	// un-fetched for long enough. Ideally there would be a way to extend
-	// the deadline of the context instead, but that isn't possible currently.
-	s.lastUpdate = time.Now()
-	go func() {
-		for {
-			s.lock.RLock()
-			deadline := s.lastUpdate.Add(StreamTimeout)
-			s.lock.RUnlock()
-			waitDuration := deadline.Sub(time.Now())
-			select {
-			case <-ctx.Done():
-				// Context finished already, nothing to do.
-				return
-			case <-time.After(waitDuration):
-				s.lock.RLock()
-				expired := time.Now().After(s.lastUpdate.Add(StreamTimeout))
-				s.lock.RUnlock()
-				if expired {
-					cancel()
-					return
-				}
-			}
-		}
-	}()
 
 	// Run the main loop to receive events.
 	var snapshotDone bool
@@ -203,13 +137,17 @@ func (s *subscriber) run() {
 	for {
 		event, err := streamHandle.Recv()
 		if err != nil {
-			sendErr(err)
+			// Do a non-blocking send of the error, in case there's no Fetch watching.
+			select {
+			case s.resultCh <- err:
+			default:
+			}
 			return
 		}
 
-		// If this isn't the special "end of snapshot" message, update our
-		// version of the state based on the event/op.
-		if !event.EndOfSnapshot {
+		// Update our version of the state based on the event/op.
+		switch event.Topic {
+		case stream.Topic_ServiceHealth:
 			serviceHealth := event.GetServiceHealth()
 			node := serviceHealth.ServiceNode
 			id := fmt.Sprintf("%s/%s", node.Node.Node, node.Service.ID)
@@ -224,7 +162,7 @@ func (s *subscriber) run() {
 			case stream.CatalogOp_Deregister:
 				delete(state, id)
 			}
-		} else {
+		case stream.Topic_EndOfSnapshot:
 			snapshotDone = true
 		}
 
@@ -239,14 +177,15 @@ func (s *subscriber) run() {
 			result.Nodes = append(result.Nodes, node)
 		}
 		result.Index = index
-
-		// Send the new view of the state to the watcher.
 		s.lock.Lock()
 		s.lastResult = result
-		if s.watcher != nil && index > s.watcher.index {
-			s.watcher.updateCh <- &result
-			s.lastUpdate = time.Now()
-		}
 		s.lock.Unlock()
+
+		// Send the new view of the state to the watcher.
+		select {
+		case s.resultCh <- &result:
+			s.lastUpdate = time.Now()
+		default:
+		}
 	}
 }
