266c288fc0ad42e0dff9e0307db9411e3fdcc9c7
John Cowen <johncowen@users.noreply.github.com>
2019-08-02 13:53:52 +0200

    ui: Adds readonly meta data to the serviceInstance and node detail pages (#6196)

--- ui-v2/app/helpers/object-entries.js
+++ ui-v2/app/helpers/object-entries.js
@@ -1,6 +1,9 @@
 import { helper } from '@ember/component/helper';
 
 export function objectEntries([obj = {}] /*, hash*/) {
+  if (obj == null) {
+    return [];
+  }
   return Object.entries(obj);
 }
 
