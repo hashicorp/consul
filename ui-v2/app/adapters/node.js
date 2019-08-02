ec751e0d67160904f6f967b3fe82120740948940
John Cowen <jcowen@hashicorp.com>
2019-08-02 11:12:42 +0000

    ui: add all the ember-data things required to call a new endpoint

--- ui-v2/app/adapters/node.js
+++ ui-v2/app/adapters/node.js
@@ -20,6 +20,38 @@ export default Adapter.extend({
     }
     return this.appendURL('internal/ui/node', [query.id], this.cleanQuery(query));
   },
+  urlForRequest: function({ type, snapshot, requestType }) {
+    switch (requestType) {
+      case 'queryLeader':
+        return this.urlForQueryLeader(snapshot, type.modelName);
+    }
+    return this._super(...arguments);
+  },
+  urlForQueryLeader: function(query, modelName) {
+    // https://www.consul.io/api/status.html#get-raft-leader
+    return this.appendURL('status/leader', [], this.cleanQuery(query));
+  },
+  isQueryLeader: function(url, method) {
+    return url.pathname === this.parseURL(this.urlForQueryLeader({})).pathname;
+  },
+  queryLeader: function(store, modelClass, id, snapshot) {
+    const params = {
+      store: store,
+      type: modelClass,
+      id: id,
+      snapshot: snapshot,
+      requestType: 'queryLeader',
+    };
+    // _requestFor is private... but these methods aren't, until they disappear..
+    const request = {
+      method: this.methodForRequest(params),
+      url: this.urlForRequest(params),
+      headers: this.headersForRequest(params),
+      data: this.dataForRequest(params),
+    };
+    // TODO: private..
+    return this._makeRequest(request);
+  },
   handleBatchResponse: function(url, response, primary, slug) {
     const dc = url.searchParams.get(API_DATACENTER_KEY) || '';
     return response.map((item, i, arr) => {
@@ -41,7 +73,21 @@ export default Adapter.extend({
     const method = requestData.method;
     if (status === HTTP_OK) {
       const url = this.parseURL(requestData.url);
+      let temp, port, address;
       switch (true) {
+        case this.isQueryLeader(url, method):
+          // This response is just an ip:port like `"10.0.0.1:8000"`
+          // split it and make it look like a `C`onsul.`R`esponse
+          // popping off the end for ports should cover us for IPv6 addresses
+          // as we should always get a `address:port` or `[a:dd:re:ss]:port` combo
+          temp = response.split(':');
+          port = temp.pop();
+          address = temp.join(':');
+          response = {
+            Address: address,
+            Port: port,
+          };
+          break;
         case this.isQueryRecord(url, method):
           response = this.handleSingleResponse(url, fillSlug(response), PRIMARY_KEY, SLUG_KEY);
           break;
