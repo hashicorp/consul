88b58e1a59c297060dd7068692fd3ecd59015db7
John Cowen <johncowen@users.noreply.github.com>
2019-08-05 19:10:00 +0200

    ui: add some additional env level URLs for easier linking (#6145)
    Right now we only use CONSUL_DOCS_URL, the others are for future usage

--- ui-v2/config/environment.js
+++ ui-v2/config/environment.js
@@ -64,7 +64,10 @@ module.exports = function(environment) {
       }
       return 'oss';
     })(),
-    CONSUL_DOCUMENTATION_URL: 'https://www.consul.io/docs',
+    CONSUL_HOME_URL: 'https://www.consul.io',
+    CONSUL_DOCS_URL: 'https://www.consul.io/docs',
+    CONSUL_DOCS_LEARN_URL: 'https://learn.hashicorp.com/consul',
+    CONSUL_DOCS_API_URL: 'https://www.consul.io/api',
     CONSUL_COPYRIGHT_URL: 'https://www.hashicorp.com',
     CONSUL_COPYRIGHT_YEAR: '2019',
   });
