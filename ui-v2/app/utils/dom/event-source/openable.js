1ea36f6da9d48aa1e6d0f7bef4db1dec0cfef9f6
John Cowen <jcowen@hashicorp.com>
2019-08-14 08:12:51 +0000

    ui: Revert classes to use ES5 style to workaround babel transp. probs
    Using a mixture of ES6 classes (and hence super) and arrow functions
    means that when babel transpiles the arrow functions down to ES5, a
    reference to this is moved before the call to super, hence causing a js
    error.
    
    Furthermore, we the testing environment no longer lets use use
    apply/call on the constructor.
    
    These errors only manifests during testing (only in the testing
    environment), the application itself runs fine with no problems without
    this change.
    
    Using ES5 style class definitions give us freedom to do all of the above
    without causing any errors, so we reverted these classes back to ES5
    class definitions

--- ui-v2/app/utils/dom/event-source/openable.js
+++ ui-v2/app/utils/dom/event-source/openable.js
@@ -4,21 +4,31 @@
  * @param {Class} eventSource - EventSource class to extend from
  */
 export default function(eventSource = EventSource) {
-  return class extends eventSource {
-    constructor(source, configuration) {
-      super(...arguments);
-      this.configuration = configuration;
-    }
-    open() {
-      switch (this.readyState) {
-        case 3: // CLOSING
-          this.readyState = 1;
-          break;
-        case 2: // CLOSED
-          eventSource.apply(this, [this.source, this.configuration]);
-          break;
-      }
-      return this;
-    }
+  const OpenableEventSource = function(source, configuration = {}) {
+    eventSource.apply(this, arguments);
+    this.configuration = configuration;
   };
+  OpenableEventSource.prototype = Object.assign(
+    Object.create(eventSource.prototype, {
+      constructor: {
+        value: OpenableEventSource,
+        configurable: true,
+        writable: true,
+      },
+    }),
+    {
+      open: function() {
+        switch (this.readyState) {
+          case 3: // CLOSING
+            this.readyState = 1;
+            break;
+          case 2: // CLOSED
+            eventSource.apply(this, [this.source, this.configuration]);
+            break;
+        }
+        return this;
+      },
+    }
+  );
+  return OpenableEventSource;
 }
