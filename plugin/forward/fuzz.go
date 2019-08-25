793bd324999ed301a7437931213dbbb2d766558f
Miek Gieben <miek@miek.nl>
2019-08-25 19:00:59 +0000

    plugin/forward: add fuzzing (#3188)
    * plugin/forward: add fuzzing
    
    Add fuzz.go for forward
    
    Signed-off-by: Miek Gieben <miek@miek.nl>
    
    * Make it compile
    
    Signed-off-by: Miek Gieben <miek@miek.nl>

---
+++ plugin/forward/fuzz.go
@@ -0,0 +1,34 @@
+// +build gofuzz
+
+package forward
+
+import (
+	"github.com/coredns/coredns/plugin/pkg/dnstest"
+	"github.com/coredns/coredns/plugin/pkg/fuzz"
+
+	"github.com/miekg/dns"
+)
+
+var f *Forward
+
+// abuse init to setup a environment to test against. This start another server to that will
+// reflect responses.
+func init() {
+	f = New()
+	s := dnstest.NewServer(r{}.reflectHandler)
+	f.proxies = append(f.proxies, NewProxy(s.Addr, "tcp"))
+	f.proxies = append(f.proxies, NewProxy(s.Addr, "udp"))
+}
+
+// Fuzz fuzzes forward.
+func Fuzz(data []byte) int {
+	return fuzz.Do(f, nil, data)
+}
+
+type r struct{}
+
+func (r r) reflectHandler(w dns.ResponseWriter, req *dns.Msg) {
+	m := new(dns.Msg)
+	m.SetReply(req)
+	w.WriteMsg(m)
+}
