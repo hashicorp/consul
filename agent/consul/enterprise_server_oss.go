4a20efda9bdceb1b3eb428d15ec743456d41dba8
Hans Hasselberg <me@hans.io>
2019-09-11 11:01:37 +0200

    agent: handleEnterpriseLeave (#6453)

--- agent/consul/enterprise_server_oss.go
+++ agent/consul/enterprise_server_oss.go
@@ -34,6 +34,10 @@ func (s *Server) handleEnterpriseRPCConn(rtype pool.RPCType, conn net.Conn, isTL
 	return false
 }
 
+func (s *Server) handleEnterpriseLeave() {
+	return
+}
+
 func (s *Server) enterpriseStats() map[string]map[string]string {
 	return nil
 }
