fa6718d02606852897c6c91cacf0f80c4d1a0243
Ingo Gottwald <in.gottwald@gmail.com>
2019-10-01 22:32:59 +0200

    Fix grpc test vet warning (#3341)
    This fixes the vet warning: the cancel function returned by
    context.WithTimeout should be called, not discarded, to avoid a context
    leak.
    
    Signed-off-by: Ingo Gottwald <in.gottwald@gmail.com>

--- test/grpc_test.go
+++ test/grpc_test.go
@@ -22,7 +22,8 @@ func TestGrpc(t *testing.T) {
 	}
 	defer g.Stop()
 
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 	conn, err := grpc.DialContext(ctx, tcp, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		t.Fatalf("Expected no error but got: %s", err)
