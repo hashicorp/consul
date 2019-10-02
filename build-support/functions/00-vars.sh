da6ea25435716d79d524acaea023547da4ee9168
Alvin Huang <17609145+alvin-huang@users.noreply.github.com>
2019-10-02 14:02:08 -0400

    check for go binary in ci builds

--- build-support/functions/00-vars.sh
+++ build-support/functions/00-vars.sh
@@ -9,13 +9,15 @@ GO_BUILD_CONTAINER_DEFAULT="consul-build-go"
 COLORIZE=${COLORIZE-1}
 
 # determine GOPATH and the first GOPATH to use for intalling binaries
-GOPATH=${GOPATH:-$(go env GOPATH)}
-case $(uname) in
-    CYGWIN*)
-        GOPATH="$(cygpath $GOPATH)"
-        ;;
-esac
-MAIN_GOPATH=$(cut -d: -f1 <<< "${GOPATH}")
+if command -v go >/dev/null; then
+   GOPATH=${GOPATH:-$(go env GOPATH)}
+   case $(uname) in
+         CYGWIN*)
+            GOPATH="$(cygpath $GOPATH)"
+            ;;
+   esac
+   MAIN_GOPATH=$(cut -d: -f1 <<< "${GOPATH}")
+fi
 
 # Build debugging output is off by default
 BUILD_DEBUG=${BUILD_DEBUG-0}
