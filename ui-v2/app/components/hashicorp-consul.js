2c9445ea0d20922b8439722ab3dbe8e8d244a69e
John Cowen <jcowen@hashicorp.com>
2019-10-17 12:37:20 +0000

    Add nspace menu into the main nav header, refactor the old dc menu also
    Both of these now work the same

--- ui-v2/app/components/hashicorp-consul.js
+++ ui-v2/app/components/hashicorp-consul.js
@@ -1,23 +1,15 @@
 import Component from '@ember/component';
-import { get, set } from '@ember/object';
 import { inject as service } from '@ember/service';
 export default Component.extend({
   dom: service('dom'),
-  isDropdownVisible: false,
   didInsertElement: function() {
     this.dom.root().classList.remove('template-with-vertical-menu');
   },
   actions: {
-    dropdown: function(e) {
-      if (get(this, 'dcs.length') > 0) {
-        set(this, 'isDropdownVisible', !this.isDropdownVisible);
-      }
-    },
     change: function(e) {
-      const dom = this.dom;
-      const win = dom.viewport();
-      const $root = dom.root();
-      const $body = dom.element('body');
+      const win = this.dom.viewport();
+      const $root = this.dom.root();
+      const $body = this.dom.element('body');
       if (e.target.checked) {
         $root.classList.add('template-with-vertical-menu');
         $body.style.height = $root.style.height = win.innerHeight + 'px';
