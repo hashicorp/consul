a15bda9c1d5684ea60f5d5efdca73f167adfd444
John Cowen <jcowen@hashicorp.com>
2019-10-17 12:45:00 +0000

    Fix an ember-data missing AdapterError by using the new org structure

--- ui-v2/app/adapters/http.js
+++ ui-v2/app/adapters/http.js
@@ -1,4 +1,5 @@
 import Adapter from 'ember-data/adapter';
+import AdapterError from '@ember-data/adapter/error';
 import {
   AbortError,
   TimeoutError,
@@ -8,9 +9,7 @@ import {
   NotFoundError,
   ConflictError,
   InvalidError,
-  AdapterError,
 } from 'ember-data/adapters/errors';
-
 // TODO: This is a little skeleton cb function
 // is to be replaced soon with something slightly more involved
 const responder = function(response) {
