acda9fe3872a75981bc56e93668fb6275a5d3912
John Cowen <jcowen@hashicorp.com>
2019-10-17 12:09:01 +0000

    Add Namespace to models and their serializers where required (inc uid)

--- ui-v2/app/utils/create-fingerprinter.js
+++ ui-v2/app/utils/create-fingerprinter.js
@@ -1,4 +1,4 @@
-export default function(foreignKey, hash = JSON.stringify) {
+export default function(foreignKey, nspaceKey, nspaceUndefinedName, hash = JSON.stringify) {
   return function(primaryKey, slugKey, foreignKeyValue) {
     if (foreignKeyValue == null || foreignKeyValue.length < 1) {
       throw new Error('Unable to create fingerprint, missing foreignKey value');
@@ -7,11 +7,13 @@ export default function(foreignKey, hash = JSON.stringify) {
       if (item[slugKey] == null || item[slugKey].length < 1) {
         throw new Error('Unable to create fingerprint, missing slug');
       }
+      const nspaceValue = item[nspaceKey] || nspaceUndefinedName;
       return {
         ...item,
         ...{
+          [nspaceKey]: nspaceValue,
           [foreignKey]: foreignKeyValue,
-          [primaryKey]: hash([foreignKeyValue, item[slugKey]]),
+          [primaryKey]: hash([nspaceValue, foreignKeyValue, item[slugKey]]),
         },
       };
     };
--- ui-v2/app/utils/http/consul.js
+++ ui-v2/app/utils/http/consul.js
@@ -1,3 +1,6 @@
-export const HEADERS_SYMBOL = '__consul_ui_http_headers__';
+// TODO: Need to make all these headers capital case
+export const HEADERS_NAMESPACE = 'X-Consul-Namespace';
 export const HEADERS_INDEX = 'x-consul-index';
 export const HEADERS_DIGEST = 'x-consul-contenthash';
+//
+export const HEADERS_SYMBOL = '__consul_ui_http_headers__';
