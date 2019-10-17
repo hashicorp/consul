0e531e0c5adb90233360530a910d6dea8741373e
John Cowen <jcowen@hashicorp.com>
2019-10-17 12:32:45 +0000

    Make intentions aware of namespacing

--- ui-v2/app/adapters/intention.js
+++ ui-v2/app/adapters/intention.js
@@ -1,6 +1,10 @@
 import Adapter, { DATACENTER_QUERY_PARAM as API_DATACENTER_KEY } from './application';
 import { FOREIGN_KEY as DATACENTER_KEY } from 'consul-ui/models/dc';
 import { SLUG_KEY } from 'consul-ui/models/intention';
+// Intentions use SourceNS and DestinationNS properties for namespacing
+// so we don't need to add the `?ns=` anywhere here
+
+// TODO: Update to use this.formatDatacenter()
 export default Adapter.extend({
   requestForQuery: function(request, { dc, index, id }) {
     return request`
@@ -24,14 +28,30 @@ export default Adapter.extend({
     return request`
       POST /v1/connect/intentions?${{ [API_DATACENTER_KEY]: data[DATACENTER_KEY] }}
 
-      ${serialized}
+      ${{
+        SourceNS: serialized.SourceNS,
+        DestinationNS: serialized.DestinationNS,
+        SourceName: serialized.SourceName,
+        DestinationName: serialized.DestinationName,
+        SourceType: serialized.SourceType,
+        Action: serialized.Action,
+        Description: serialized.Description,
+      }}
     `;
   },
   requestForUpdateRecord: function(request, serialized, data) {
     return request`
       PUT /v1/connect/intentions/${data[SLUG_KEY]}?${{ [API_DATACENTER_KEY]: data[DATACENTER_KEY] }}
 
-      ${serialized}
+      ${{
+        SourceNS: serialized.SourceNS,
+        DestinationNS: serialized.DestinationNS,
+        SourceName: serialized.SourceName,
+        DestinationName: serialized.DestinationName,
+        SourceType: serialized.SourceType,
+        Action: serialized.Action,
+        Description: serialized.Description,
+      }}
     `;
   },
   requestForDeleteRecord: function(request, serialized, data) {
--- ui-v2/app/serializers/intention.js
+++ ui-v2/app/serializers/intention.js
@@ -1,8 +1,7 @@
 import Serializer from './application';
-import { PRIMARY_KEY, SLUG_KEY, ATTRS } from 'consul-ui/models/intention';
+import { PRIMARY_KEY, SLUG_KEY } from 'consul-ui/models/intention';
 
 export default Serializer.extend({
   primaryKey: PRIMARY_KEY,
   slugKey: SLUG_KEY,
-  attrs: ATTRS,
 });
