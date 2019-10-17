fd5e5ee4a185d47f13c6de4267b6eeeb4d48caee
John Cowen <jcowen@hashicorp.com>
2019-10-17 12:05:46 +0000

    Add adapter tests for new `ns=` query parameter

--- ui-v2/tests/integration/adapters/session-test.js
+++ ui-v2/tests/integration/adapters/session-test.js
@@ -4,26 +4,57 @@ module('Integration | Adapter | session', function(hooks) {
   setupTest(hooks);
   const dc = 'dc-1';
   const id = 'session-id';
-  test('requestForQuery returns the correct url', function(assert) {
-    const adapter = this.owner.lookup('adapter:session');
-    const client = this.owner.lookup('service:client/http');
-    const node = 'node-id';
-    const expected = `GET /v1/session/node/${node}?dc=${dc}`;
-    const actual = adapter.requestForQuery(client.url, {
-      dc: dc,
-      id: node,
+  const undefinedNspace = 'default';
+  [undefinedNspace, 'team-1', undefined].forEach(nspace => {
+    test(`requestForQuery returns the correct url/method when nspace is ${nspace}`, function(assert) {
+      const adapter = this.owner.lookup('adapter:session');
+      const client = this.owner.lookup('service:client/http');
+      const node = 'node-id';
+      const expected = `GET /v1/session/node/${node}?dc=${dc}`;
+      let actual = adapter.requestForQuery(client.url, {
+        dc: dc,
+        id: node,
+        ns: nspace,
+      });
+      actual = actual.split('\n');
+      assert.equal(actual.shift().trim(), expected);
+      actual = actual.join('\n').trim();
+      assert.equal(actual, `${typeof nspace !== 'undefined' ? `ns=${nspace}` : ``}`);
     });
-    assert.equal(actual, expected);
-  });
-  test('requestForQueryRecord returns the correct url', function(assert) {
-    const adapter = this.owner.lookup('adapter:session');
-    const client = this.owner.lookup('service:client/http');
-    const expected = `GET /v1/session/info/${id}?dc=${dc}`;
-    const actual = adapter.requestForQueryRecord(client.url, {
-      dc: dc,
-      id: id,
+    test(`requestForQueryRecord returns the correct url/method when nspace is ${nspace}`, function(assert) {
+      const adapter = this.owner.lookup('adapter:session');
+      const client = this.owner.lookup('service:client/http');
+      const expected = `GET /v1/session/info/${id}?dc=${dc}`;
+      let actual = adapter.requestForQueryRecord(client.url, {
+        dc: dc,
+        id: id,
+        ns: nspace,
+      });
+      actual = actual.split('\n');
+      assert.equal(actual.shift().trim(), expected);
+      actual = actual.join('\n').trim();
+      assert.equal(actual, `${typeof nspace !== 'undefined' ? `ns=${nspace}` : ``}`);
     });
-    assert.equal(actual, expected);
+    test(`requestForDeleteRecord returns the correct url/method when the nspace is ${nspace}`, function(assert) {
+      const adapter = this.owner.lookup('adapter:session');
+      const client = this.owner.lookup('service:client/http');
+      const expected = `PUT /v1/session/destroy/${id}?dc=${dc}${
+        typeof nspace !== 'undefined' ? `&ns=${nspace}` : ``
+      }`;
+      const actual = adapter
+        .requestForDeleteRecord(
+          client.url,
+          {},
+          {
+            Datacenter: dc,
+            ID: id,
+            Namespace: nspace,
+          }
+        )
+        .split('\n')
+        .shift();
+      assert.equal(actual, expected);
+    });
   });
   test("requestForQuery throws if you don't specify an id", function(assert) {
     const adapter = this.owner.lookup('adapter:session');
@@ -42,21 +73,5 @@ module('Integration | Adapter | session', function(hooks) {
         dc: dc,
       });
     });
-  });
-  test('urlForDeleteRecord returns the correct url', function(assert) {
-    const adapter = this.owner.lookup('adapter:session');
-    const client = this.owner.lookup('service:client/http');
-    const expected = `PUT /v1/session/destroy/${id}?dc=${dc}`;
-    const actual = adapter
-      .requestForDeleteRecord(
-        client.url,
-        {},
-        {
-          Datacenter: dc,
-          ID: id,
-        }
-      )
-      .split('\n')[0];
-    assert.equal(actual, expected);
   });
 });
