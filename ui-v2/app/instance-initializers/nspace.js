4464b235d2596546f3720113ab41e6b1ced6812f
John Cowen <jcowen@hashicorp.com>
2019-11-14 14:01:22 +0000

    Ensure the current nspace is shown on the settings page
    The settings page is alittle strange as its the only URL that doesn't
    have dc and nspace informaiton in it. Therefore for DCs we save the last
    DC viewed in localStorage and read it from there is we can't figure it
    out from the URL.
    
    This takes the same approach for nspaces

--- ui-v2/app/instance-initializers/nspace.js
+++ ui-v2/app/instance-initializers/nspace.js
@@ -1,7 +1,7 @@
 import config from 'consul-ui/config/environment';
 export function initialize(container) {
   if (config.CONSUL_NSPACES_ENABLED) {
-    ['dc', 'dc.intentions.edit', 'dc.intentions.create'].forEach(function(item) {
+    ['dc', 'settings', 'dc.intentions.edit', 'dc.intentions.create'].forEach(function(item) {
       container.inject(`route:${item}`, 'nspaceRepo', 'service:repository/nspace/enabled');
       container.inject(`route:nspace.${item}`, 'nspaceRepo', 'service:repository/nspace/enabled');
     });
