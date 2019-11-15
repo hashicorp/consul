4831e7f9477dcdc7847b0a76fab09fcd17447bb2
Miek Gieben <miek@miek.nl>
2019-11-15 15:45:09 +0000

    bufsize: go gen and docs (#3449)
    Run 'go generate' and 'make -f Makefile.doc' to generate the chaos
    plugin author list and update the manual pages.
    
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- plugin/chaos/zowners.go
+++ plugin/chaos/zowners.go
@@ -1,4 +1,4 @@
 package chaos
 
 // Owners are all GitHub handlers of all maintainers.
-var Owners = []string{"bradbeam", "chrisohaver", "darshanime", "dilyevsky", "ekleiner", "fastest963", "greenpau", "grobie", "ihac", "inigohu", "isolus", "johnbelamaric", "miekg", "nchrisdk", "nitisht", "pmoroney", "rajansandeep", "rdrozhdzh", "rtreffer", "stp-ip", "superq", "varyoo", "yongtang"}
+var Owners = []string{"bradbeam", "chrisohaver", "darshanime", "dilyevsky", "ekleiner", "fastest963", "greenpau", "grobie", "ihac", "inigohu", "isolus", "johnbelamaric", "miekg", "nchrisdk", "nitisht", "pmoroney", "rajansandeep", "rdrozhdzh", "rtreffer", "stp-ip", "superq", "varyoo", "ykhr53", "yongtang"}
