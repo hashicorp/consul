b8a6dcb4de7e5950c1acaecba64e5053d08aadbb
Sarah Christoff <sarah.christoff13@gmail.com>
2019-11-20 14:07:51 -0600

    fix test i broke

--- agent/consul/issue_test.go
+++ agent/consul/issue_test.go
@@ -1,6 +1,7 @@
 package consul
 
 import (
+	"os"
 	"reflect"
 	"testing"
 
@@ -22,7 +23,7 @@ func makeLog(buf []byte) *raft.Log {
 // Testing for GH-300 and GH-279
 func TestHealthCheckRace(t *testing.T) {
 	t.Parallel()
-	fsm, err := consulfsm.New(nil, s.logger)
+	fsm, err := consulfsm.New(nil, os.Stderr)
 	if err != nil {
 		t.Fatalf("err: %v", err)
 	}
