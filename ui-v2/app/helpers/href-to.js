1a9a1fc4ff54a3b765576f940fd52ae7d0238673
John Cowen <johncowen@users.noreply.github.com>
2019-11-26 16:01:14 +0000

    ui: Adds `href-mut` helper for modifying current URLs (#6799)
    * Adds `href-mut` helper for modifying current URLs
    
    Sometimes you just need to link to your current route, but alter one of
    the parameters of the route. In our case the datacenter menu is a good
    example. When you change the datacenter from the node listing page we
    want the user to be taken to the node listing page on the newly selected
    datacenter, not a different route.
    
    This commit uses the router service to walk up the current route and
    replace parameters in the route that you specify as an argument to the
    helper. This then uses the newly mutated arguments and the existing
    routeName to pass through to the already existing hrefTo helper.

--- ui-v2/app/helpers/href-to.js
+++ ui-v2/app/helpers/href-to.js
@@ -3,37 +3,40 @@
 // (dynamic or wildcard) and encode or not depending on the type
 import { inject as service } from '@ember/service';
 import Helper from '@ember/component/helper';
-import { hrefTo } from 'ember-href-to/helpers/href-to';
+import { hrefTo as _hrefTo } from 'ember-href-to/helpers/href-to';
 
 import wildcard from 'consul-ui/utils/routing/wildcard';
 
 import { routes } from 'consul-ui/router';
 
 const isWildcard = wildcard(routes);
+export const hrefTo = function(owned, router, [targetRouteName, ...rest], namedArgs) {
+  if (isWildcard(targetRouteName)) {
+    rest = rest.map(function(item, i) {
+      return item
+        .split('/')
+        .map(encodeURIComponent)
+        .join('/');
+    });
+  }
+  if (namedArgs.params) {
+    return _hrefTo(owned, namedArgs.params);
+  } else {
+    // we don't check to see if nspaces are enabled here as routes
+    // with beginning with 'nspace' only exist if nspaces are enabled
+
+    // this globally converts non-nspaced href-to's to nspace aware
+    // href-to's only if you are within a namespace
+    if (router.currentRouteName.startsWith('nspace.') && targetRouteName.startsWith('dc.')) {
+      targetRouteName = `nspace.${targetRouteName}`;
+    }
+    return _hrefTo(owned, [targetRouteName, ...rest]);
+  }
+};
 
 export default Helper.extend({
   router: service('router'),
-  compute([targetRouteName, ...rest], namedArgs) {
-    if (isWildcard(targetRouteName)) {
-      rest = rest.map(function(item, i) {
-        return item
-          .split('/')
-          .map(encodeURIComponent)
-          .join('/');
-      });
-    }
-    if (namedArgs.params) {
-      return hrefTo(this, namedArgs.params);
-    } else {
-      // we don't check to see if nspaces are enabled here as routes
-      // with beginning with 'nspace' only exist if nspaces are enabled
-
-      // this globally converts non-nspaced href-to's to nspace aware
-      // href-to's only if you are within a namespace
-      if (this.router.currentRouteName.startsWith('nspace.') && targetRouteName.startsWith('dc.')) {
-        targetRouteName = `nspace.${targetRouteName}`;
-      }
-      return hrefTo(this, [targetRouteName, ...rest]);
-    }
+  compute(params, hash) {
+    return hrefTo(this, this.router, params, hash);
   },
 });
