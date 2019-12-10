4204aeaac0a6caac3601ffdd93a9418cd1648bea
John Cowen <jcowen@hashicorp.com>
2019-12-10 13:42:10 +0000

    Standardize on the 'repo is a plural' rule

--- ui-v2/app/instance-initializers/nspace.js
+++ ui-v2/app/instance-initializers/nspace.js
@@ -2,10 +2,10 @@ import config from 'consul-ui/config/environment';
 export function initialize(container) {
   if (config.CONSUL_NSPACES_ENABLED) {
     ['dc', 'settings', 'dc.intentions.edit', 'dc.intentions.create'].forEach(function(item) {
-      container.inject(`route:${item}`, 'nspaceRepo', 'service:repository/nspace/enabled');
-      container.inject(`route:nspace.${item}`, 'nspaceRepo', 'service:repository/nspace/enabled');
+      container.inject(`route:${item}`, 'nspacesRepo', 'service:repository/nspace/enabled');
+      container.inject(`route:nspace.${item}`, 'nspacesRepo', 'service:repository/nspace/enabled');
     });
-    container.inject('route:application', 'nspaceRepo', 'service:repository/nspace/enabled');
+    container.inject('route:application', 'nspacesRepo', 'service:repository/nspace/enabled');
     container
       .lookup('service:dom')
       .root()
