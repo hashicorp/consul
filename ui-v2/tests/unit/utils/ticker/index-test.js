0e878ef378106f1bed11e1d8ca412c223d51bbe8
John Cowen <jcowen@hashicorp.com>
2019-12-12 11:38:34 +0000

    ui: Add in the things we use for the animations

---
+++ ui-v2/tests/unit/utils/ticker/index-test.js
@@ -0,0 +1,10 @@
+import tickerIndex from 'consul-ui/utils/ticker/index';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | ticker/index', function() {
+  // Replace this with your real tests.
+  test('it works', function(assert) {
+    let result = tickerIndex();
+    assert.ok(result);
+  });
+});
