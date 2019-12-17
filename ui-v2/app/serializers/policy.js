a89a006dd5e6f34a0c4df92deed856ac1fda2886
John Cowen <johncowen@users.noreply.github.com>
2019-12-17 16:16:43 +0000

    ui: Remove `writable` usage from policy and use the request instead (#6934)

--- ui-v2/app/serializers/policy.js
+++ ui-v2/app/serializers/policy.js
@@ -1,8 +1,7 @@
 import Serializer from './application';
-import { PRIMARY_KEY, SLUG_KEY, ATTRS } from 'consul-ui/models/policy';
+import { PRIMARY_KEY, SLUG_KEY } from 'consul-ui/models/policy';
 
 export default Serializer.extend({
   primaryKey: PRIMARY_KEY,
   slugKey: SLUG_KEY,
-  attrs: ATTRS,
 });
