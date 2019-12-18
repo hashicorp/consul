34e6e23d43b55712486659ade4bab0f5fab05179
Matt Keeler <mkeeler@users.noreply.github.com>
2019-12-18 11:06:39 -0500

    Change how  namespaces are specified for the CLI (#6960)

--- command/flags/http.go
+++ command/flags/http.go
@@ -76,8 +76,7 @@ func (f *HTTPFlags) ServerFlags() *flag.FlagSet {
 
 func (f *HTTPFlags) NamespaceFlags() *flag.FlagSet {
 	fs := flag.NewFlagSet("", flag.ContinueOnError)
-	// TODO (namespaces) Do we want to allow setting via an env var? CONSUL_NAMESPACE
-	fs.Var(&f.namespace, "ns",
+	fs.Var(&f.namespace, "namespace",
 		"Specifies the namespace to query. If not provided, the namespace will"+
 			"default to the `default` namespace. Namespaces is a Consul Enterprise feature.")
 	return fs
