3faee222f248d0326f392d662817a973bcb25ea9
Matt Keeler <mkeeler@users.noreply.github.com>
2019-12-18 10:16:35 -0500

    OSS changes to allow for parsing the enterprise DNS config prop… (#6959)

--- agent/config/default_oss.go
+++ agent/config/default_oss.go
@@ -2,11 +2,22 @@
 
 package config
 
-// DefaultEnterpriseSource returns the consul agent configuration for the enterprise mode.
-// This should be merged in the tail after the DefaultConsulSource.
+// DefaultEnterpriseSource returns the consul agent configuration for enterprise mode.
+// These can be overridden by the user and therefore this source should be merged in the
+// head and processed before user configuration.
 func DefaultEnterpriseSource() Source {
 	return Source{
-		Name:   "enterprise",
+		Name:   "enterprise-defaults",
+		Format: "hcl",
+		Data:   ``,
+	}
+}
+
+// OverrideEnterpriseSource returns the consul agent configuration for the enterprise mode.
+// This should be merged in the tail after the DefaultConsulSource.
+func OverrideEnterpriseSource() Source {
+	return Source{
+		Name:   "enterprise-overrides",
 		Format: "hcl",
 		Data:   ``,
 	}
