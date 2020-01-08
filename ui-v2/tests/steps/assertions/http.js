d3eb17a39a099ef7d1a4872d0514032fe48771f8
John Cowen <jcowen@hashicorp.com>
2020-01-08 09:51:51 +0000

    Remove last few occurances of old style http assertions
    We had informally 'deprecated' our old style of http assertions that
    relied on the order of http calls (even though that order was not
    important for the assertion). Following on from our namespace work we
    removed the majority of the old occrances of these old style assertions.
    
    This commit removes the remaining few, and also then cleans up the
    assertions/http.js file to only include the ones we are using.
    
    This reduces our available step count further and prevents any confusion
    over the usage of the old types and the new types.

--- ui-v2/tests/steps/assertions/http.js
+++ ui-v2/tests/steps/assertions/http.js
@@ -1,7 +1,3 @@
-// TODO: This entire file/steps need refactoring out so that they don't depend on order
-// unless you specifically ask it to assert for order of requests
-// this should also let us simplify the entire API for these steps
-// an reword them to make more sense
 export default function(scenario, assert, lastNthRequest) {
   // lastNthRequest should return a
   // {
@@ -9,112 +5,7 @@ export default function(scenario, assert, lastNthRequest) {
   //   requestBody: '',
   //   requestHeaders: ''
   // }
-  const assertRequest = function(request, method, url) {
-    assert.equal(
-      request.method,
-      method,
-      `Expected the request method to be ${method}, was ${request.method}`
-    );
-    assert.equal(request.url, url, `Expected the request url to be ${url}, was ${request.url}`);
-  };
   scenario
-    .then('a $method request is made to "$endpoint" with the body from yaml\n$yaml', function(
-      method,
-      url,
-      data
-    ) {
-      const request = lastNthRequest(1);
-      assertRequest(request, method, url);
-      const body = JSON.parse(request.requestBody);
-      Object.keys(data).forEach(function(key, i, arr) {
-        assert.deepEqual(
-          body[key],
-          data[key],
-          `Expected the payload to contain ${key} equaling ${data[key]}, ${key} was ${body[key]}`
-        );
-      });
-    })
-    // TODO: This one can replace the above one, it covers more use cases
-    // also DRY it out a bit
-    .then('a $method request is made to "$endpoint" from yaml\n$yaml', function(method, url, yaml) {
-      const request = lastNthRequest(1);
-      assertRequest(request, method, url);
-      let data = yaml.body || {};
-      const body = JSON.parse(request.requestBody);
-      Object.keys(data).forEach(function(key, i, arr) {
-        assert.equal(
-          body[key],
-          data[key],
-          `Expected the payload to contain ${key} to equal ${body[key]}, ${key} was ${data[key]}`
-        );
-      });
-      data = yaml.headers || {};
-      const headers = request.requestHeaders;
-      Object.keys(data).forEach(function(key, i, arr) {
-        assert.equal(
-          headers[key],
-          data[key],
-          `Expected the payload to contain ${key} to equal ${headers[key]}, ${key} was ${data[key]}`
-        );
-      });
-    })
-    .then('a $method request is made to "$endpoint" with the body "$body"', function(
-      method,
-      url,
-      data
-    ) {
-      const request = lastNthRequest(1);
-      assertRequest(request, method, url);
-      assert.equal(
-        request.requestBody,
-        data,
-        `Expected the request body to be ${data}, was ${request.requestBody}`
-      );
-    })
-    .then('a $method request is made to "$endpoint" with no body', function(method, url) {
-      const request = lastNthRequest(1);
-      assertRequest(request, method, url);
-      assert.equal(
-        request.requestBody,
-        null,
-        `Expected the request body to be null, was ${request.requestBody}`
-      );
-    })
-
-    .then('a $method request is made to "$endpoint"', function(method, url) {
-      const request = lastNthRequest(1);
-      assertRequest(request, method, url);
-    })
-    .then('the last $method request was made to "$endpoint"', function(method, url) {
-      const request = lastNthRequest(0, method);
-      assertRequest(request, method, url);
-    })
-    .then(
-      'the last $method request was made to "$endpoint" with the body from yaml\n$yaml',
-      function(method, url, data) {
-        const request = lastNthRequest(0, method);
-        const body = JSON.parse(request.requestBody);
-        assert.ok(request, `Expected a ${method} request`);
-        assertRequest(request, method, url);
-        Object.keys(data).forEach(function(key, i, arr) {
-          assert.deepEqual(
-            body[key],
-            data[key],
-            `Expected the payload to contain ${key} equaling ${data[key]}, ${key} was ${body[key]}`
-          );
-        });
-      }
-    )
-    .then('the last $method requests were like yaml\n$yaml', function(method, data) {
-      const requests = lastNthRequest(null, method);
-      data.reverse().forEach(function(item, i, arr) {
-        assert.equal(
-          requests[i].url,
-          item,
-          `Expected the request url to be ${item}, was ${requests[i].url}`
-        );
-      });
-    })
     .then('the last $method requests included from yaml\n$yaml', function(method, data) {
       const requests = lastNthRequest(null, method);
       const a = new Set(data);
@@ -130,8 +21,6 @@ export default function(scenario, assert, lastNthRequest) {
       );
       assert.equal(diff.size, 0, `Expected requests "${[...diff].join(', ')}"`);
     })
-    // These 'was made to' methods should always be preferred as they don't rely
-    // on order of requests, then we should be able to deprecate most of the above
     .then('a $method request was made to "$endpoint"', function(method, url) {
       const requests = lastNthRequest(null, method);
       const request = requests.find(function(item) {
