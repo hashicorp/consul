ac613993bb0a7a6e96cf434dfde59f023afcbe8f
John Cowen <johncowen@users.noreply.github.com>
2020-01-20 14:15:15 +0000

    ui: Keyboard navigation improvements (#7090)
    * ui: Improve keyboard access for some custom components
    
    * ui: Add some accessibility related metadata

--- ui-v2/app/components/radio-group.js
+++ ui-v2/app/components/radio-group.js
@@ -1,5 +1,14 @@
 import Component from '@ember/component';
 
+const ENTER = 13;
 export default Component.extend({
-  tagName: 'fieldset',
+  tagName: '',
+  keyboardAccess: false,
+  actions: {
+    keydown: function(e) {
+      if (e.keyCode === ENTER) {
+        e.target.dispatchEvent(new MouseEvent('click'));
+      }
+    },
+  },
 });
