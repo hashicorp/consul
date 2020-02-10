d78e4f595176ea9f4a4855f78580d61f8d5cfec5
Alvin Huang <17609145+alvin-huang@users.noreply.github.com>
2020-02-10 08:41:02 -0500

    fix ember parallelization (#7221)

--- ui-v2/tests/test-helper.js
+++ ui-v2/tests/test-helper.js
@@ -1,11 +1,9 @@
 import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
-import { start } from 'ember-qunit';
 import './helpers/flash-message';
-import loadEmberExam from 'ember-exam/test-support/load';
+import start from 'ember-exam/test-support/start';
 
-loadEmberExam();
 const application = Application.create(config.APP);
 application.inject('component:copy-button', 'clipboard', 'service:clipboard/local-storage');
 setApplication(application);
