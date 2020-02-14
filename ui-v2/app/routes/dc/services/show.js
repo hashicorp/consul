65dd14daf304adf012cf1ba220ad0306ec952703
John Cowen <johncowen@users.noreply.github.com>
2020-02-14 14:46:17 +0000

    ui: Gracefully cope with 500 errors from the disco-chain API endpoint (#7291)
    When connect is disabled the discovery-chain endpoint returns a 500
    error status, which we previoulsy did not gracefully cope with.
    
    This commit gracefully copes with any errors from the disco-chain
    endpoint by supressing the error and hiding the Routing tab from the
    Service detail page.
    
    Acceptance test included

--- ui-v2/app/routes/dc/services/show.js
+++ ui-v2/app/routes/dc/services/show.js
@@ -24,7 +24,23 @@ export default Route.extend({
       return hash({
         chain: ['connect-proxy', 'mesh-gateway'].includes(get(model, 'item.Service.Kind'))
           ? null
-          : this.chainRepo.findBySlug(params.name, dc, nspace),
+          : this.chainRepo.findBySlug(params.name, dc, nspace).catch(function(e) {
+              const code = get(e, 'errors.firstObject.status');
+              // Currently we are specifically catching a 500, but we return null
+              // by default, so null for all errors.
+              // The extra code here is mainly for documentation purposes
+              // and for if we need to perform different actions based on the error code
+              // in the future
+              switch (code) {
+                case '500':
+                  // connect is likely to be disabled
+                  // we just return a null to hide the tab
+                  // `Connect must be enabled in order to use this endpoint`
+                  return null;
+                default:
+                  return null;
+              }
+            }),
         ...model,
       });
     });
