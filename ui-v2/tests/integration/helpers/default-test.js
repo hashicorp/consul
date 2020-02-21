408f1b1e1ece914d7a31b24d843242fbe0a4f073
John Cowen <johncowen@users.noreply.github.com>
2020-02-21 16:48:54 +0000

    ui: Upgrade to ember 3.16 Octane Edition (#7334)
    * v3.12.0...v3.16.0
    
    * Upgrades
    
    * Remove old wormhole fix
    
    * Fixup ember power select (camelcasing)
    
    * Fixup immedaitely closing dropdown
    
    When clicking on the syntax selector, it seemed like an extra click
    event was firing from the label, which then immediately closed the
    dropdown. By adding a for="" attribute this event isn't passed to the
    dropdown menu and therefore doesn't immediately close
    
    * Fix up integration tests with new style (plus standardize titles)
    
    * Temporarily disable some template linting rules
    
    * Add required methods (even though they aren't used anywhere)
    
    * Ensure event sources get closed on destruction

--- ui-v2/tests/integration/helpers/default-test.js
+++ ui-v2/tests/integration/helpers/default-test.js
@@ -3,7 +3,7 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('helper:default', function(hooks) {
+module('Integration | Helper | default', function(hooks) {
   setupRenderingTest(hooks);
 
   // Replace this with your real tests.
@@ -12,13 +12,13 @@ module('helper:default', function(hooks) {
 
     await render(hbs`{{default inputValue}}`);
 
-    assert.dom('*').hasText('1234');
+    assert.equal(this.element.textContent.trim(), '1234');
   });
   test('it renders the default value', async function(assert) {
     this.set('inputValue', '');
 
     await render(hbs`{{default inputValue '1234'}}`);
 
-    assert.dom('*').hasText('1234');
+    assert.equal(this.element.textContent.trim(), '1234');
   });
 });
