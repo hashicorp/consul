522dea91aab9f71e965a3a3ccab74246856d657d
John Cowen <jcowen@hashicorp.com>
2020-02-21 10:12:42 +0000

    v3.12.0...v3.16.0

--- ui-v2/testem.js
+++ ui-v2/testem.js
@@ -1,8 +1,13 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci: ['Chrome'],
-  launch_in_dev: ['Chrome'],
+  launch_in_ci: [
+    'Chrome'
+  ],
+  launch_in_dev: [
+    'Chrome'
+  ],
+  browser_start_timeout: 120,
   browser_args: {
     Chrome: {
       ci: [
