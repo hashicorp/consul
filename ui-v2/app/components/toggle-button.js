b2ad13ba1440492a49f1184746f78c6a032dab4b
John Cowen <jcowen@hashicorp.com>
2020-03-04 13:36:10 +0000

    Remove set

--- ui-v2/app/components/toggle-button.js
+++ ui-v2/app/components/toggle-button.js
@@ -1,6 +1,5 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
-import { set } from '@ember/object';
 
 export default Component.extend({
   dom: service('dom'),
