496214ad0559ffcb63f30534ad8692d50bd30467
John Cowen <johncowen@users.noreply.github.com>
2020-03-18 18:06:12 +0000

    ui: Adds blueprint for creating css-components (#7460)
    * ui: Adds blueprint for creating css-components
    
    CSS Components are CSS only components, which can be one of 2 types:
    
    1. A composable CSS component that is not commonly used on its own
    2. A CSS component that is commonly used along with a HTML/JS component,
    potentially made up of other composable CSS components.
    
    For type 1. you probably don't need the ./styles/component-name.scss
    file. But instead of complicating matters with options for the blueprint
    right now, we just rely on the user to delete the
    ./styles/component-name.scss file if they don't need it.
    
    We also don't automatically add this import to the
    ./styles/components/index.scss file for 2 reasons:
    
    1. We are potentially going to be moving the
    ./styles/components/index.scss file to ./styles/components.scss.
    2. If we aren't going to provide a CLI swicth to ask whether this is of
    component type 1. or component type 2. we don't want to automatically
    include things that the user might not need.
    
    Both of these 2 reasons are a little TBD and at some point in the future
    we'll probably iterate on this blueprint to make it even easier to make
    either type of CSS component.

---
+++ ui-v2/blueprints/css-component/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const path = require('path');
+module.exports = {
+  description: 'Generates a CSS component',
+
+  availableOptions: [],
+
+  root: __dirname,
+
+  fileMapTokens(options) {
+      return {
+        __path__() {
+          return path.join('styles', 'components');
+        }
+      };
+  },
+  locals(options) {
+    // Return custom template variables here.
+    return {
+    };
+  }
+
+  // afterInstall(options) {
+  //   // Perform extra work here.
+  // }
+};
