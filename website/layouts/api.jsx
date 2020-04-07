6dd5f8badbc66f7cfcf5b8509c9a0ffbbd1c6a86
Jeff Escalante <jescalan@users.noreply.github.com>
2020-04-07 19:56:08 -0400

    docs rendering

--- website/layouts/api.jsx
+++ website/layouts/api.jsx
@@ -1,30 +1,36 @@
 import DocsPage from '@hashicorp/react-docs-page'
 import order from '../data/api-navigation.js'
 import { frontMatter as data } from '../pages/api-docs/**/*.mdx'
+import { MDXProvider } from '@mdx-js/react'
 import Head from 'next/head'
 import Link from 'next/link'
+import EnterpriseAlert from '@hashicorp/react-enterprise-alert'
+
+const DEFAULT_COMPONENTS = { EnterpriseAlert }
 
 function ApiDocsLayoutWrapper(pageMeta) {
   function ApiDocsLayout(props) {
     return (
-      <DocsPage
-        {...props}
-        product="consul"
-        head={{
-          is: Head,
-          title: `${pageMeta.page_title} | Consul by HashiCorp`,
-          description: pageMeta.description,
-          siteName: 'Consul by HashiCorp',
-        }}
-        sidenav={{
-          Link,
-          category: 'api-docs',
-          currentPage: props.path,
-          data,
-          order,
-        }}
-        resourceURL={`https://github.com/hashicorp/consul/blob/master/website/pages/${pageMeta.__resourcePath}`}
-      />
+      <MDXProvider components={DEFAULT_COMPONENTS}>
+        <DocsPage
+          {...props}
+          product="consul"
+          head={{
+            is: Head,
+            title: `${pageMeta.page_title} | Consul by HashiCorp`,
+            description: pageMeta.description,
+            siteName: 'Consul by HashiCorp',
+          }}
+          sidenav={{
+            Link,
+            category: 'api-docs',
+            currentPage: props.path,
+            data,
+            order,
+          }}
+          resourceURL={`https://github.com/hashicorp/consul/blob/master/website/pages/${pageMeta.__resourcePath}`}
+        />
+      </MDXProvider>
     )
   }
 
--- website/layouts/intro.jsx
+++ website/layouts/intro.jsx
@@ -1,30 +1,36 @@
 import DocsPage from '@hashicorp/react-docs-page'
 import order from '../data/intro-navigation.js'
 import { frontMatter as data } from '../pages/intro/**/*.mdx'
+import { MDXProvider } from '@mdx-js/react'
 import Head from 'next/head'
 import Link from 'next/link'
+import EnterpriseAlert from '@hashicorp/react-enterprise-alert'
+
+const DEFAULT_COMPONENTS = { EnterpriseAlert }
 
 function IntroLayoutWrapper(pageMeta) {
   function IntroLayout(props) {
     return (
-      <DocsPage
-        {...props}
-        product="consul"
-        head={{
-          is: Head,
-          title: `${pageMeta.page_title} | Consul by HashiCorp`,
-          description: pageMeta.description,
-          siteName: 'Consul by HashiCorp',
-        }}
-        sidenav={{
-          Link,
-          category: 'intro',
-          currentPage: props.path,
-          data,
-          order,
-        }}
-        resourceURL={`https://github.com/hashicorp/consul/blob/master/website/pages/${pageMeta.__resourcePath}`}
-      />
+      <MDXProvider components={DEFAULT_COMPONENTS}>
+        <DocsPage
+          {...props}
+          product="consul"
+          head={{
+            is: Head,
+            title: `${pageMeta.page_title} | Consul by HashiCorp`,
+            description: pageMeta.description,
+            siteName: 'Consul by HashiCorp',
+          }}
+          sidenav={{
+            Link,
+            category: 'intro',
+            currentPage: props.path,
+            data,
+            order,
+          }}
+          resourceURL={`https://github.com/hashicorp/consul/blob/master/website/pages/${pageMeta.__resourcePath}`}
+        />
+      </MDXProvider>
     )
   }
 
