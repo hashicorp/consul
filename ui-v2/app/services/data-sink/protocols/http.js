ff34cf2ae8438c2df3c1dd63aded9dee1c415c27
John Cowen <johncowen@users.noreply.github.com>
2020-04-08 18:03:18 +0100

    ui: Logout button (#7604)
    * ui: Logout button
    
    This commit adds an easier way to logout of the UI using a logout button
    
    Notes:
    
    - Added a Logout button to the main navigation when you are logged in,
    meaning you have easy access to a way to log out of the UI.
    - Changed all wording to use 'Log in/out' vocabulary instad of 'stop
    using'.
    - The logout button opens a panel to show you your current ACL
    token and a logout button in order to logout.
    - When using legacy ACLs we don't show the current ACL token as legacy
    ACLs tokens only have secret values, whereas the new ACLs use a
    non-secret ID plus a secret ID (that we don't show).
    - We also added a new `<EmptyState />` component to use for all our
    empty states. We currently only use this for the ACLs disabled screen to
    provide more outgoing links to more readind material/documentation to
    help you to understand and enable ACLs.
    - The `<DataSink />` component is the sibling to our `<DataSource />`
    component and whilst is much simpler (as it doesn't require polling
    support), its tries to use the same code patterns for consistencies
    sake.
    - We had a fun problem with ember-data's `store.unloadAll` here, and in
    the end went with `store.init` to empty the ember-data store instead due
    to timing issues.
    - We've tried to use already existing patterns in the Consul UI here
    such as our preexisting `feedback` service, although these are likely to
    change in the future. The thinking here is to add this feature with as
    little change as possible.
    
    Overall this is a precursor to a much larger piece of work centered on
    auth in the UI. We figured this was a feature complete piece of work as
    it is and thought it was worthwhile to PR as a feature on its own, which
    also means the larger piece of work will be a smaller scoped PR also.

---
+++ ui-v2/app/services/data-sink/protocols/http.js
@@ -0,0 +1,33 @@
+import Service, { inject as service } from '@ember/service';
+import { setProperties } from '@ember/object';
+
+export default Service.extend({
+  settings: service('settings'),
+  intention: service('repository/intention'),
+  prepare: function(sink, data, instance) {
+    const [, dc, nspace, model, slug] = sink.split('/');
+    const repo = this[model];
+    if (slug === '') {
+      instance = repo.create({
+        Datacenter: dc,
+        Namespace: nspace,
+      });
+    } else {
+      if (typeof instance === 'undefined') {
+        instance = repo.peek(slug);
+      }
+    }
+    setProperties(instance, data);
+    return instance;
+  },
+  persist: function(sink, instance) {
+    const [, , , /*dc*/ /*nspace*/ model] = sink.split('/');
+    const repo = this[model];
+    return repo.persist(instance);
+  },
+  remove: function(sink, instance) {
+    const [, , , /*dc*/ /*nspace*/ model] = sink.split('/');
+    const repo = this[model];
+    return repo.remove(instance);
+  },
+});
