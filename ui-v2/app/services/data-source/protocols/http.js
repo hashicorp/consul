ccfad0d2ab914e542fc82f6afffa21a0bb23d996
John Cowen <johncowen@users.noreply.github.com>
2020-04-22 17:30:26 +0100

    ui: Use DataSources in ACLs area (#7681)
    * ui: Use Datasource for loading related data in ACLs area
    
    * ui: Use more manual cleanup for Controller event-sources
    
    * Update reconcile to use nspace and add SyncTime to role/policy
    
    * Use the correct value for nspace and dc (the one from the item itself)
    
    * Remove the // check, we no longer need it. Add some TODO

--- ui-v2/app/services/data-source/protocols/http.js
+++ ui-v2/app/services/data-source/protocols/http.js
@@ -5,9 +5,18 @@ export default Service.extend({
   datacenters: service('repository/dc'),
   namespaces: service('repository/nspace'),
   token: service('repository/token'),
+  policies: service('repository/policy'),
+  policy: service('repository/policy'),
+  roles: service('repository/role'),
   type: service('data-source/protocols/http/blocking'),
   source: function(src, configuration) {
-    const [, , /*nspace*/ dc, model, ...rest] = src.split('/');
+    // TODO: Consider adding/requiring nspace, dc, model, action, ...rest
+    const [, nspace, dc, model, ...rest] = src.split('/');
+    // TODO: Consider throwing if we have an empty nspace or dc
+    // we are going to use '*' for 'all' when we need that
+    // and an empty value is the same as 'default'
+    // reasoning for potentially doing it here is, uri's should
+    // always be complete, they should never have things like '///model'
     let find;
     const repo = this[model];
     if (typeof repo.reconcile === 'function') {
@@ -31,6 +40,13 @@ export default Service.extend({
         break;
       case 'token':
         find = configuration => repo.self(rest[1], dc);
+        break;
+      case 'roles':
+      case 'policies':
+        find = configuration => repo.findAllByDatacenter(dc, nspace, configuration);
+        break;
+      case 'policy':
+        find = configuration => repo.findBySlug(rest[0], dc, nspace, configuration);
         break;
     }
     return this.type.source(find, configuration);
