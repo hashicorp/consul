2a8e0eff1ee22460534a37f3926821ce266eed67
R.B. Boyer <rb@hashicorp.com>
2020-04-24 16:53:28 -0500

    update from pr

--- lib/testing_httpserver.go
+++ lib/testing_httpserver.go
@@ -11,6 +11,12 @@ import (
 // StartTestServer fires up a web server on a random unused port to serve the
 // given handler body. The address it is listening on is returned. When the
 // test case terminates the server will be stopped via cleanup functions.
+//
+// We can't directly use httptest.Server here because that only thinks a port
+// is free if it's not bound. Consul tests frequently reserve ports via
+// `sdk/freeport` so you can have one part of the test try to use a port and
+// _know_ nothing is listening. If you simply assumed unbound ports were free
+// you'd end up with test cross-talk and weirdness.
 func StartTestServer(t testing.T, handler http.Handler) string {
 	ports := freeport.MustTake(1)
 	t.Cleanup(func() {
