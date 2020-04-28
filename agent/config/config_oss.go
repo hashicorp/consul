82074b899353b62980942e373161d6e8b79c397d
Matt Keeler <mjkeeler7@gmail.com>
2020-04-28 09:45:33 -0400

    Update enterprise configurations to be in OSS
    This will emit warnings about the configs not doing anything but still allow them to be parsed.
    
    This also added the warnings for enterprise fields that we already had in OSS but didn’t change their enforcement behavior. For example, attempting to use a network segment will cause a hard error in OSS.
    
    # Conflicts:
    #	agent/config/config.go

--- agent/config/config_oss.go
+++ agent/config/config_oss.go
@@ -10,9 +10,3 @@ type EnterpriseMeta struct{}
 func (_ *EnterpriseMeta) ToStructs() structs.EnterpriseMeta {
 	return *structs.DefaultEnterpriseMeta()
 }
-
-// EnterpriseDNSConfig OSS stub
-type EnterpriseDNSConfig struct{}
-
-// EnterpriseACLConfig OSS stub
-type EnterpriseACLConfig struct{}
