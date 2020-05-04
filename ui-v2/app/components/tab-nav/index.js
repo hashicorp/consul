1fa012fbfcdd0048ccd17775e9e804442b171b7b
John Cowen <jcowen@hashicorp.com>
2020-05-04 14:27:36 +0000

    ui: Adds a tab selection animation to our app tabs
    1. Replace all mentions of `magenta` with a themeable CSS property.
    2. Add an easy way to inline style DOM nodes
    3. Use CSS properties to add tab animation

--- ui-v2/app/components/tab-nav/index.js
+++ ui-v2/app/components/tab-nav/index.js
@@ -1,9 +1,38 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+import { schedule } from '@ember/runloop';
 
 const ENTER = 13;
+const SELECTED = 'li.selected';
+const ANIMATABLE = 'animatable';
 export default Component.extend({
   name: 'tab',
   tagName: '',
+  dom: service('dom'),
+  init: function() {
+    this._super(...arguments);
+    this.guid = this.dom.guid(this);
+  },
+  didInsertElement: function() {
+    this._super(...arguments);
+    this.$nav = this.dom.element(`#${this.guid}`);
+    this.select(this.dom.element(SELECTED, this.$nav));
+    this.$nav.classList.add(ANIMATABLE);
+  },
+  didUpdateAttrs: function() {
+    schedule('afterRender', () => this.select(this.dom.element(SELECTED, this.$nav)));
+  },
+  select: function($el) {
+    this.dom.style(
+      {
+        '--selected-width': $el.offsetWidth,
+        '--selected-left': $el.offsetLeft,
+        '--selected-height': $el.offsetHeight,
+        '--selected-top': $el.offsetTop,
+      },
+      this.$nav
+    );
+  },
   actions: {
     keydown: function(e) {
       if (e.keyCode === ENTER) {
