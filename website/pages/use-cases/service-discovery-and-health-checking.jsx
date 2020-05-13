ecbedd5109bc061f726b29add81b9965fb5bd367
Brandon Romano <BrandonRRomano@gmail.com>
2020-05-13 01:44:54 -0700

    Update service discovery description

--- website/pages/use-cases/service-discovery-and-health-checking.jsx
+++ website/pages/use-cases/service-discovery-and-health-checking.jsx
@@ -5,7 +5,7 @@ export default function ServiceDiscoveryAndHealthCheckingPage() {
   return (
     <UseCaseLayout
       title="Service Discovery and Health Checking"
-      description="Service registry, integrated health checks, and DNS and HTTP interfaces enable any service to discover and be discovered by other services"
+      description="Discover, Register and Resolve services for application workloads across any cloud. Automatically add and remove services based on health checking."
     >
       <TempUseCaseChildren />
     </UseCaseLayout>
