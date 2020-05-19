7b6b6c9ba65cbedc50ce6da0e259a595352fc502
Jonathan Neal <jonathantneal@hotmail.com>
2020-05-19 16:58:09 -0400

    Lint CSS

--- website/.stylelintrc.js
+++ website/.stylelintrc.js
@@ -1,5 +1,7 @@
 module.exports = {
   ...require('@hashicorp/nextjs-scripts/.stylelintrc.js'),
+  /* Specify overrides here */
+  ignoreFiles: ['public/**/*.css'],
   rules: {
     'selector-pseudo-class-no-unknown': [
       true,
