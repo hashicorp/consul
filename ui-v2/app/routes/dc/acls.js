7e1e6e44c3221e3b5189164314b20bf52c00b42b
John Cowen <johncowen@users.noreply.github.com>
2020-05-21 18:28:51 +0100

    ui: remove some components/javascript we are no longer using (#7941)

--- ui-v2/app/routes/dc/acls.js
+++ ui-v2/app/routes/dc/acls.js
@@ -1,38 +1,3 @@
 import Route from '@ember/routing/route';
-import { get } from '@ember/object';
-import { inject as service } from '@ember/service';
 import WithBlockingActions from 'consul-ui/mixins/with-blocking-actions';
-export default Route.extend(WithBlockingActions, {
-  router: service('router'),
-  settings: service('settings'),
-  feedback: service('feedback'),
-  repo: service('repository/token'),
-  actions: {
-    authorize: function(secret, nspace) {
-      const dc = this.modelFor('dc').dc.Name;
-      return this.repo
-        .self(secret, dc)
-        .then(item => {
-          return this.settings.persist({
-            token: {
-              Namespace: get(item, 'Namespace'),
-              AccessorID: get(item, 'AccessorID'),
-              SecretID: secret,
-            },
-          });
-        })
-        .catch(e => {
-          this.feedback.execute(
-            () => {
-              return Promise.resolve();
-            },
-            'authorize',
-            function(type, e) {
-              return 'error';
-            },
-            {}
-          );
-        });
-    },
-  },
-});
+export default Route.extend(WithBlockingActions, {});
