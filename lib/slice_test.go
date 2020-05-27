ddd0a13e272050d4cbbfac8405266b42e11b87e2
R.B. Boyer <rb@hashicorp.com>
2020-05-27 11:31:10 -0500

    agent: handle re-bootstrapping in a secondary datacenter when WAN federation via mesh gateways is configured (#7931)
    The main fix here is to always union the `primary-gateways` list with
    the list of mesh gateways in the primary returned from the replicated
    federation states list. This will allow any replicated (incorrect) state
    to be supplemented with user-configured (correct) state in the config
    file. Eventually the game of random selection whack-a-mole will pick a
    winning entry and re-replicate the latest federation states from the
    primary. If the user-configured state is actually the incorrect one,
    then the same eventual correct selection process will work in that case,
    too.
    
    The secondary fix is actually to finish making wanfed-via-mgws actually
    work as originally designed. Once a secondary datacenter has replicated
    federation states for the primary AND managed to stand up its own local
    mesh gateways then all of the RPCs from a secondary to the primary
    SHOULD go through two sets of mesh gateways to arrive in the consul
    servers in the primary (one hop for the secondary datacenter's mesh
    gateway, and one hop through the primary datacenter's mesh gateway).
    This was neglected in the initial implementation. While everything
    works, ideally we should treat communications that go around the mesh
    gateways as just provided for bootstrapping purposes.
    
    Now we heuristically use the success/failure history of the federation
    state replicator goroutine loop to determine if our current mesh gateway
    route is working as intended. If it is, we try using the local gateways,
    and if those don't work we fall back on trying the primary via the union
    of the replicated state and the go-discover configuration flags.
    
    This can be improved slightly in the future by possibly initializing the
    gateway choice to local on startup if we already have replicated state.
    This PR does not address that improvement.
    
    Fixes #7339

--- lib/slice_test.go
+++ lib/slice_test.go
@@ -28,3 +28,25 @@ func TestStringSliceEqual(t *testing.T) {
 		})
 	}
 }
+
+func TestStringSliceMergeSorted(t *testing.T) {
+	for name, tc := range map[string]struct {
+		a, b   []string
+		expect []string
+	}{
+		"nil":              {nil, nil, nil},
+		"empty":            {[]string{}, []string{}, nil},
+		"one and none":     {[]string{"foo"}, []string{}, []string{"foo"}},
+		"one and one dupe": {[]string{"foo"}, []string{"foo"}, []string{"foo"}},
+		"one and one":      {[]string{"foo"}, []string{"bar"}, []string{"bar", "foo"}},
+		"two and one":      {[]string{"baz", "foo"}, []string{"bar"}, []string{"bar", "baz", "foo"}},
+		"two and two":      {[]string{"baz", "foo"}, []string{"bar", "egg"}, []string{"bar", "baz", "egg", "foo"}},
+		"two and two dupe": {[]string{"bar", "foo"}, []string{"bar", "egg"}, []string{"bar", "egg", "foo"}},
+	} {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			require.Equal(t, tc.expect, StringSliceMergeSorted(tc.a, tc.b))
+			require.Equal(t, tc.expect, StringSliceMergeSorted(tc.b, tc.a))
+		})
+	}
+}
