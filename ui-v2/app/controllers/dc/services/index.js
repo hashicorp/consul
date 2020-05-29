f40ebab95e8c8879b0335689e777c72eb252c3fd
John Cowen <jcowen@hashicorp.com>
2020-05-29 15:52:17 +0000

    ui: Add ability to sort service based on health

--- ui-v2/app/controllers/dc/services/index.js
+++ ui-v2/app/controllers/dc/services/index.js
@@ -31,4 +31,38 @@ export default Controller.extend(WithEventSource, {
 
     return proxies;
   }),
+  actions: {
+    healthStatusComparator: function(key, serviceA, serviceB) {
+      const [, dir] = key.split(':');
+      let a, b;
+      if (dir === 'asc') {
+        b = serviceA;
+        a = serviceB;
+      } else {
+        a = serviceA;
+        b = serviceB;
+      }
+      switch (true) {
+        case a.ChecksCritical > b.ChecksCritical:
+          return 1;
+        case a.ChecksCritical < b.ChecksCritical:
+          return -1;
+        default:
+          switch (true) {
+            case a.ChecksWarning > b.ChecksWarning:
+              return 1;
+            case a.ChecksWarning < b.ChecksWarning:
+              return -1;
+            default:
+              switch (true) {
+                case a.ChecksPassing < b.ChecksPassing:
+                  return 1;
+                case a.ChecksPassing > b.ChecksPassing:
+                  return -1;
+              }
+          }
+          return 0;
+      }
+    },
+  },
 });
