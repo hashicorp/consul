49f3af19d4b77ab341940b3bbd8d55ad3ff8ff65
Hans Hasselberg <me@hans.io>
2020-06-08 10:08:12 +0200

    agent: add option to disable agent cache for HTTP endpoints (#8023)
    This allows the operator to disable agent caching for the http endpoint.
    It is on by default for backwards compatibility and if disabled will
    ignore the url parameter `cached`.

--- agent/config/builder.go
+++ agent/config/builder.go
@@ -831,6 +831,7 @@ func (b *Builder) Build() (rt RuntimeConfig, err error) {
 		HTTPBlockEndpoints:  c.HTTPConfig.BlockEndpoints,
 		HTTPResponseHeaders: c.HTTPConfig.ResponseHeaders,
 		AllowWriteHTTPFrom:  b.cidrsVal("allow_write_http_from", c.HTTPConfig.AllowWriteHTTPFrom),
+		HTTPUseCache:        b.boolValWithDefault(c.HTTPConfig.UseCache, true),
 
 		// Telemetry
 		Telemetry: lib.TelemetryConfig{
