7983ab0fce30b42f2eb6667edc3f360baa3c60ac
Preetha <460133+preetapan@users.noreply.github.com>
2020-06-08 17:34:38 -0500

    Update banner.js to call out helm chart survey

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -2,7 +2,7 @@ export const ALERT_BANNER_ACTIVE = true
 
 // https://github.com/hashicorp/web-components/tree/master/packages/alert-banner
 export default {
-  tag: 'Announcing',
-  url: 'https://www.hashicorp.com/blog/announcing-hashicorp-consul-1-8',
-  text: 'HashiCorp Consul 1.8, now available in Beta',
+  tag: 'Survey',
+  url: 'bit.ly/2A5RZWz',
+  text: 'Do you use the Consul Helm chart? Fill out this 3-minute survey to help us improve it: https://bit.ly/2A5RZWz',
 }
