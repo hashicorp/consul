59cb39ac2134198d9f6470f5cce1c708db213ccf
Noel Quiles <3746694+EnMod@users.noreply.github.com>
2020-06-08 14:42:36 -0400

    Extract arrow icon to file

--- website/components/basic-hero/index.jsx
+++ website/components/basic-hero/index.jsx
@@ -1,5 +1,6 @@
 import Button from '@hashicorp/react-button'
 import InlineSvg from '@hashicorp/react-inline-svg'
+import RightArrowIcon from './img/right-arrow-icon.svg?include'
 
 export default function BasicHero({
   heading,
@@ -43,11 +44,7 @@ export default function BasicHero({
                     {links[2].text}
                   </span>
                   <span className="icon">
-                    <InlineSvg
-                      src={
-                        '<svg role="img" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20 12L14 18M4 12H20H4ZM20 12L14 6L20 12Z" stroke="#76767D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>'
-                      }
-                    />
+                    <InlineSvg src={RightArrowIcon} />
                   </span>
                 </a>
               </div>
