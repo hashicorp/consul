62b894a2d6f8b0a91a1945a88861d532e3ca316a
Preetha <460133+preetapan@users.noreply.github.com>
2020-06-08 19:47:33 -0500

    fix link

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -3,6 +3,6 @@ export const ALERT_BANNER_ACTIVE = true
 // https://github.com/hashicorp/web-components/tree/master/packages/alert-banner
 export default {
   tag: 'Survey',
-  url: 'bit.ly/2A5RZWz',
+  url: 'https://bit.ly/2A5RZWz',
   text: 'Do you use the Consul Helm chart? Fill out this 3-minute survey to help us improve it: https://bit.ly/2A5RZWz',
 }
