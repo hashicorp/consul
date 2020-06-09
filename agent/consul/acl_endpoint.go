0c5f6ea111930a69a08242e677a994e6d669a51c
Hans Hasselberg <me@hans.io>
2020-06-09 21:13:09 +0200

    acl: do not resolve local tokens from remote dcs (#8068)

--- agent/consul/acl_endpoint.go
+++ agent/consul/acl_endpoint.go
@@ -259,6 +259,7 @@ func (a *ACL) TokenRead(args *structs.ACLTokenGetRequest, reply *structs.ACLToke
 			}
 
 			reply.Index, reply.Token = index, token
+			reply.SourceDatacenter = args.Datacenter
 			return nil
 		})
 }
