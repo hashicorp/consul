90b7c737c34789bc7de6c8ad1bc99de19d1b5a6c
John Cowen <jcowen@hashicorp.com>
2020-06-12 12:03:53 +0000

    ui: Move healthcheck ordering to use new comparators

--- ui-v2/app/initializers/sort.js
+++ ui-v2/app/initializers/sort.js
@@ -1,10 +1,12 @@
 import service from 'consul-ui/sort/comparators/service';
+import check from 'consul-ui/sort/comparators/check';
 
 export function initialize(container) {
   // Service-less injection using private properties at a per-project level
   const Sort = container.resolveRegistration('service:sort');
   const comparators = {
     service: service(),
+    check: check(),
   };
   Sort.reopen({
     comparator: function(type) {
