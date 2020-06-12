38e05e828a3cedf4cc1e47b892701ad78561e6ed
Jeff Escalante <jescalan@users.noreply.github.com>
2020-06-11 20:58:05 -0400

    update dependencies, fapply upgrades, fix breaking changes

--- website/components/enterprise-alert/index.jsx
+++
@@ -1,5 +0,0 @@
-import EnterpriseAlert from '@hashicorp/react-enterprise-alert'
-
-export default function EnterpriseAlertConsul(props) {
-  return <EnterpriseAlert product="consul" {...props} />
-}
