1b9aaf551822d75abd4a781c884560c1298592ac
Preetha <460133+preetapan@users.noreply.github.com>
2020-06-15 17:27:16 -0500

    change survey->announcing

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -2,7 +2,7 @@ export const ALERT_BANNER_ACTIVE = true
 
 // https://github.com/hashicorp/web-components/tree/master/packages/alert-banner
 export default {
-  tag: 'Survey',
+  tag: 'Announcing',
   url: 'https://discuss.hashicorp.com/t/consul-1-8-0-rc1-released/9997',
   text: 'Release candidate for Consul 1.8.0 now available',
 }
