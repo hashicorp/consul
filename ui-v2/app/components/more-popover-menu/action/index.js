63c8c40c68fcb31397068882456cde885d6a9596
John Cowen <jcowen@hashicorp.com>
2020-06-19 11:49:47 +0000

    Autodetect height of submenus in popover-menus, remove reliance on names

--- ui-v2/app/components/more-popover-menu/action/index.js
+++ ui-v2/app/components/more-popover-menu/action/index.js
@@ -1,17 +1,23 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 import { set } from '@ember/object';
 
 import Slotted from 'block-slots';
 
 export default Component.extend(Slotted, {
   tagName: '',
+  dom: service('dom'),
+  init: function() {
+    this._super(...arguments);
+    this.guid = this.dom.guid(this);
+  },
   didInsertElement: function() {
     this._super(...arguments);
-    this.menu.addSubmenu(this.name);
+    this.menu.addSubmenu(this.guid);
   },
   didDestroyElement: function() {
     this._super(...arguments);
-    this.menu.removeSubmenu(this.name);
+    this.menu.removeSubmenu(this.guid);
   },
   willRender: function() {
     this._super(...arguments);
