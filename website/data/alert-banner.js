3b097febc7c90bcd1a3581cb0e688011f9764381
Brandon Romano <BrandonRRomano@gmail.com>
2020-06-22 04:15:18 -0700

    Merge pull request #8162 from hashicorp/nav-update
    Update Nav

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -3,6 +3,6 @@ export const ALERT_BANNER_ACTIVE = true
 // https://github.com/hashicorp/web-components/tree/master/packages/alert-banner
 export default {
   tag: 'Announcing',
-  url: 'https://www.hashicorp.com/blog/announcing-hashicorp-consul-1-8',
-  text: 'Consul 1.8 GA release available now',
+  url: 'https://www.hashicorp.com/blog/announcing-cloud-platform',
+  text: 'HashiCorp Cloud Platform Now in Private Beta with HCP Consul on AWS',
 }
