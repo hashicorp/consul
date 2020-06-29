f6e7ca7f78eb3057649e54818b9d2bbac33e23d5
R.B. Boyer <rb@hashicorp.com>
2020-06-29 16:08:22 -0500

    xds: version sniff envoy and switch regular expressions from 'regex' to 'safe_regex' on newer envoy versions
    - cut down on extra node metadata transmission
    - split the golden file generation to compare all envoy version

--- agent/xds/golden_test.go
+++ agent/xds/golden_test.go
@@ -2,24 +2,47 @@ package xds
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"path/filepath"
 	"testing"
 
 	envoy "github.com/envoyproxy/go-control-plane/envoy/api/v2"
 	"github.com/golang/protobuf/jsonpb"
+	"github.com/hashicorp/go-version"
 	"github.com/stretchr/testify/require"
 )
 
 // update allows golden files to be updated based on the current output.
 var update = flag.Bool("update", false, "update golden files")
 
+// goldenEnvoy is a special variant of golden() that silos each named test by
+// each supported envoy version
+func goldenEnvoy(t *testing.T, name, envoyVersion, got string) string {
+	require.NotEmpty(t, envoyVersion)
+
+	// We do version sniffing on the complete version, but only generate
+	// golden files ignoring the patch portion
+	version := version.Must(version.NewVersion(envoyVersion))
+	segments := version.Segments()
+	require.Len(t, segments, 3)
+
+	subname := fmt.Sprintf("envoy-%d-%d-x", segments[0], segments[1])
+
+	return golden(t, name, subname, got)
+}
+
 // golden reads and optionally writes the expected data to the golden file,
 // returning the contents as a string.
-func golden(t *testing.T, name, got string) string {
+func golden(t *testing.T, name, subname, got string) string {
 	t.Helper()
 
-	golden := filepath.Join("testdata", name+".golden")
+	suffix := ".golden"
+	if subname != "" {
+		suffix = fmt.Sprintf(".%s.golden", subname)
+	}
+
+	golden := filepath.Join("testdata", name+suffix)
 	if *update && got != "" {
 		err := ioutil.WriteFile(golden, []byte(got), 0644)
 		require.NoError(t, err)
