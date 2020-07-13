35fcf69914fe93f13b6a3c2673562a4734213546
John Cowen <johncowen@users.noreply.github.com>
2020-07-13 14:22:58 +0100

    ui: Move dc sorting to the view/template (#8297)
    * ui: Move dc sorting to the view/template
    
    * Cleanup unused coordinates service from Node repo
    
    * Fix up integration test to not expect a sorted result

--- ui-v2/app/services/repository/node.js
+++ ui-v2/app/services/repository/node.js
@@ -1,9 +1,7 @@
 import RepositoryService from 'consul-ui/services/repository';
-import { inject as service } from '@ember/service';
 
 const modelName = 'node';
 export default RepositoryService.extend({
-  coordinates: service('repository/coordinate'),
   getModelName: function() {
     return modelName;
   },
