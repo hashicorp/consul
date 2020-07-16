497ccc2d02d01333a20549cdf1396e8ecb3f3705
John Cowen <jcowen@hashicorp.com>
2020-07-16 18:34:52 +0000

    Disable all the old route based blocking query things

--- ui-v2/app/instance-initializers/event-source.js
+++ ui-v2/app/instance-initializers/event-source.js
@@ -4,20 +4,7 @@ export function initialize(container) {
   if (env('CONSUL_UI_DISABLE_REALTIME')) {
     return;
   }
-  ['node', 'coordinate', 'session', 'service', 'proxy', 'discovery-chain', 'intention']
-    .concat(env('CONSUL_NSPACES_ENABLED') ? ['nspace/enabled'] : [])
-    .map(function(item) {
-      // create repositories that return a promise resolving to an EventSource
-      return {
-        service: `repository/${item}/event-source`,
-        extend: 'repository/type/event-source',
-        // Inject our original respository that is used by this class
-        // within the callable of the EventSource
-        services: {
-          content: `repository/${item}`,
-        },
-      };
-    })
+  []
     .concat(
       ['policy', 'role'].map(function(item) {
         // create repositories that return a promise resolving to an EventSource
@@ -33,50 +20,6 @@ export function initialize(container) {
       })
     )
     .concat([
-      // These are the routes where we overwrite the 'default'
-      // repo service. Default repos are repos that return a promise resolving to
-      // an ember-data record or recordset
-      {
-        route: 'dc/nodes/index',
-        services: {
-          repo: 'repository/node/event-source',
-        },
-      },
-      {
-        route: 'dc/nodes/show',
-        services: {
-          repo: 'repository/node/event-source',
-          coordinateRepo: 'repository/coordinate/event-source',
-          sessionRepo: 'repository/session/event-source',
-        },
-      },
-      {
-        route: 'dc/services/index',
-        services: {
-          repo: 'repository/service/event-source',
-        },
-      },
-      {
-        route: 'dc/services/show',
-        services: {
-          repo: 'repository/service/event-source',
-          chainRepo: 'repository/discovery-chain/event-source',
-          intentionRepo: 'repository/intention/event-source',
-        },
-      },
-      {
-        route: 'dc/services/instance',
-        services: {
-          repo: 'repository/service/event-source',
-          proxyRepo: 'repository/proxy/event-source',
-        },
-      },
-      {
-        route: 'dc/intentions/index',
-        services: {
-          repo: 'repository/intention/event-source',
-        },
-      },
       {
         service: 'form',
         services: {
@@ -85,18 +28,6 @@ export function initialize(container) {
         },
       },
     ])
-    .concat(
-      env('CONSUL_NSPACES_ENABLED')
-        ? [
-            {
-              route: 'dc/nspaces/index',
-              services: {
-                repo: 'repository/nspace/enabled/event-source',
-              },
-            },
-          ]
-        : []
-    )
     .forEach(function(definition) {
       if (typeof definition.extend !== 'undefined') {
         // Create the class instances that we need
@@ -111,9 +42,6 @@ export function initialize(container) {
         // but hardcode this for the moment
         if (typeof definition.route !== 'undefined') {
           container.inject(`route:${definition.route}`, name, `service:${servicePath}`);
-          if (env('CONSUL_NSPACES_ENABLED') && definition.route.startsWith('dc/')) {
-            container.inject(`route:nspace/${definition.route}`, name, `service:${servicePath}`);
-          }
         } else {
           container.inject(`service:${definition.service}`, name, `service:${servicePath}`);
         }
