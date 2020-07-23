351d57dcd181946de47e324ff6368b3711630a33
kenia <keniavalladarez@gmail.com>
2020-07-23 15:47:11 -0400

    Add acls/roles sorting test

--- ui-v2/app/components/consul-policy-list/pageobject.js
+++ ui-v2/app/components/consul-policy-list/pageobject.js
@@ -1,5 +1,5 @@
 export default (collection, clickable, attribute, text, actions) => () => {
-  return collection('.consul-policy-list li:not(:first-child)', {
+  return collection('.consul-policy-list [data-test-list-row]', {
     name: attribute('data-test-policy', '[data-test-policy]'),
     description: text('[data-test-description]'),
     policy: clickable('a'),
