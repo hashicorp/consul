2179bb0decc107c0a80a2bf25e600bcae58b5c74
Kenia <19161242+kaxcode@users.noreply.github.com>
2020-07-29 09:41:40 -0400

    ui: Add sorting to ACLs tokens with tests (#8359)
    * Add sorting to ACLs tokens with tests
    
    * Create token comparator and implement in template
    
    * Upgrade @hashicorp/consul-api-double to 3.1.6
    
    * Add navigation test to acls tokens

--- ui-v2/app/initializers/sort.js
+++ ui-v2/app/initializers/sort.js
@@ -1,6 +1,7 @@
 import service from 'consul-ui/sort/comparators/service';
 import check from 'consul-ui/sort/comparators/check';
 import intention from 'consul-ui/sort/comparators/intention';
+import token from 'consul-ui/sort/comparators/token';
 
 export function initialize(container) {
   // Service-less injection using private properties at a per-project level
@@ -9,6 +10,7 @@ export function initialize(container) {
     service: service(),
     check: check(),
     intention: intention(),
+    token: token(),
   };
   Sort.reopen({
     comparator: function(type) {
--- ui-v2/tests/pages/dc/acls/tokens/index.js
+++ ui-v2/tests/pages/dc/acls/tokens/index.js
@@ -1,9 +1,9 @@
-export default function(visitable, creatable, text, tokens, filter) {
+export default function(visitable, creatable, text, tokens, popoverSelect) {
   return {
     visit: visitable('/:dc/acls/tokens'),
     update: text('[data-test-notification-update]'),
     tokens: tokens(),
-    filter: filter(),
+    sort: popoverSelect(),
     ...creatable(),
   };
 }
