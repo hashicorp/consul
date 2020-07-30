b5e858d3e1be1145689e772fc3403f0f07e355ee
freddygv <gh@freddygv.xyz>
2020-07-30 11:13:40 -0600

    Avoid panics during shutdown routine

--- agent/consul/server.go
+++ agent/consul/server.go
@@ -958,9 +958,13 @@ func (s *Server) Shutdown() error {
 	}
 
 	// Close the connection pool
-	s.connPool.Shutdown()
+	if s.connPool != nil {
+		s.connPool.Shutdown()
+	}
 
-	s.acls.Close()
+	if s.acls != nil {
+		s.acls.Close()
+	}
 
 	if s.config.NotifyShutdown != nil {
 		s.config.NotifyShutdown()
