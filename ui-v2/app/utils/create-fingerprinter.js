6dbdc8d69bb5c1c3300c60e9ea7280eab4fe2f69
Kenia <19161242+kaxcode@users.noreply.github.com>
2020-08-11 12:53:45 -0400

    ui: Add unique slug key id to proxy (#8479)

--- ui-v2/app/utils/create-fingerprinter.js
+++ ui-v2/app/utils/create-fingerprinter.js
@@ -4,16 +4,20 @@ export default function(foreignKey, nspaceKey, hash = JSON.stringify) {
       throw new Error('Unable to create fingerprint, missing foreignKey value');
     }
     return function(item) {
-      if (item[slugKey] == null || item[slugKey].length < 1) {
-        throw new Error('Unable to create fingerprint, missing slug');
-      }
+      const slugKeys = slugKey.split(',');
+      const slugValues = slugKeys.map(function(slugKey) {
+        if (item[slugKey] == null || item[slugKey].length < 1) {
+          throw new Error('Unable to create fingerprint, missing slug');
+        }
+        return item[slugKey];
+      });
       const nspaceValue = item[nspaceKey] || 'default';
       return {
         ...item,
         ...{
           [nspaceKey]: nspaceValue,
           [foreignKey]: foreignKeyValue,
-          [primaryKey]: hash([nspaceValue, foreignKeyValue, item[slugKey]]),
+          [primaryKey]: hash([nspaceValue, foreignKeyValue].concat(slugValues)),
         },
       };
     };
