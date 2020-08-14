a9e6fc1c5ce868fb8190f4ec061b6c04e2686fe7
kenia <keniavalladarez@gmail.com>
2020-08-14 13:10:08 -0400

    Create format-time helper

---
+++ ui-v2/app/helpers/format-time.js
@@ -0,0 +1,29 @@
+import { helper } from '@ember/component/helper';
+
+export default helper(function formatTime([params], hash) {
+  let day, hour, minute, seconds;
+  seconds = Math.floor(params / 1000);
+  minute = Math.floor(seconds / 60);
+  seconds = seconds % 60;
+  hour = Math.floor(minute / 60);
+  minute = minute % 60;
+  day = Math.floor(hour / 24);
+  hour = hour % 24;
+  const time = {
+    day: day,
+    hour: hour,
+    minute: minute,
+    seconds: seconds,
+  };
+
+  switch (true) {
+    case time.day !== 0:
+      return time.day + 'd';
+    case time.hour !== 0:
+      return time.hour + 'h';
+    case time.minute !== 0:
+      return time.minute + 'm';
+    default:
+      return time.seconds + 's';
+  }
+});
---
+++ ui-v2/tests/integration/helpers/format-time-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Helper | format-time', function(hooks) {
+  setupRenderingTest(hooks);
+
+  // Replace this with your real tests.
+  test('it renders', async function(assert) {
+    this.set('inputValue', '1234');
+
+    await render(hbs`{{format-time inputValue}}`);
+
+    assert.equal(this.element.textContent.trim(), '1234');
+  });
+});
