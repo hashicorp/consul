096850a5db40a142568d37ad59508d4b502f3f44
John Cowen <jcowen@hashicorp.com>
2020-08-20 16:19:08 +0000

    ui: Uses the new Mesh* properties throughout the app

--- ui-v2/app/controllers/dc/services/index.js
+++ ui-v2/app/controllers/dc/services/index.js
@@ -13,22 +13,4 @@ export default Controller.extend({
       return item.Kind !== 'connect-proxy';
     });
   }),
-  proxies: computed('items.[]', function() {
-    const proxies = {};
-    this.items
-      .filter(function(item) {
-        return item.Kind === 'connect-proxy';
-      })
-      .forEach(item => {
-        // Iterating to cover the usecase of a proxy being
-        // used by more than one service
-        if (item.ProxyFor) {
-          item.ProxyFor.forEach(service => {
-            proxies[service] = item;
-          });
-        }
-      });
-
-    return proxies;
-  }),
 });
