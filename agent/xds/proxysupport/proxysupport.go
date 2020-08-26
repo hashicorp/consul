0ceec77fcc81a9843f83ca21db2f0aeeecf99992
R.B. Boyer <rb@hashicorp.com>
2020-08-26 10:17:17 -0500

    xds: promote supported envoy versions field out of test code
    (backport of a portion of c599a2f5f411a5820b7f0b9b126e2d2270ba3a67 from #8424)

---
+++ agent/xds/proxysupport/proxysupport.go
@@ -0,0 +1,11 @@
+package proxysupport
+
+// EnvoyVersions lists the latest officially supported versions of envoy.
+//
+// see: https://www.consul.io/docs/connect/proxies/envoy#supported-versions
+var EnvoyVersions = []string{
+	"1.14.2",
+	"1.13.2",
+	"1.12.4",
+	"1.11.2",
+}
