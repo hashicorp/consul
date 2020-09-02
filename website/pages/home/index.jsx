c2d2f68c5a0761ac3b083f804c11093c73abce37
Jeff Escalante <jescalan@users.noreply.github.com>
2020-09-02 14:32:40 -0400

    again

--- website/pages/home/index.jsx
+++ website/pages/home/index.jsx
@@ -7,7 +7,7 @@ import LearnCallout from '../../components/learn-callout'
 import CaseStudyCarousel from '../../components/case-study-carousel'
 import ProductFeaturesList from '@hashicorp/react-product-features-list'
 
-console.log('test2')
+console.log('test3')
 
 export default function HomePage() {
   return (
