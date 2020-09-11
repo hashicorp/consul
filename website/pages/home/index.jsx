c8e14d63b30d48941912e7e251eea47fa167b783
Jeff Escalante <jescalan@users.noreply.github.com>
2020-09-11 16:27:06 -0400

    8

--- website/pages/home/index.jsx
+++ website/pages/home/index.jsx
@@ -7,7 +7,7 @@ import LearnCallout from '../../components/learn-callout'
 import CaseStudyCarousel from '../../components/case-study-carousel'
 import ProductFeaturesList from '@hashicorp/react-product-features-list'
 
-console.log('test7')
+console.log('test8')
 
 export default function HomePage() {
   return (
