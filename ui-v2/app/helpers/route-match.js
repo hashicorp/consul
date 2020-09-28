ffa1ac0ae4d0f9e94cbf20c1954050714ba9cc06
John Cowen <jcowen@hashicorp.com>
2020-09-28 17:05:50 +0000

    Make route match work with all types of ember objects

--- ui-v2/app/helpers/route-match.js
+++ ui-v2/app/helpers/route-match.js
@@ -1,17 +1,20 @@
 import { helper } from '@ember/component/helper';
 
 export default helper(function routeMatch([item], hash) {
-  const keys = Object.keys(item.data || item);
-  switch (true) {
-    case keys.includes('Present'):
+  const prop = ['Present', 'Exact', 'Prefix', 'Suffix', 'Regex'].find(
+    prop => typeof item[prop] !== 'undefined'
+  );
+
+  switch (prop) {
+    case 'Present':
       return `${item.Invert ? `NOT ` : ``}present`;
-    case keys.includes('Exact'):
+    case 'Exact':
       return `${item.Invert ? `NOT ` : ``}exactly matching "${item.Exact}"`;
-    case keys.includes('Prefix'):
+    case 'Prefix':
       return `${item.Invert ? `NOT ` : ``}prefixed by "${item.Prefix}"`;
-    case keys.includes('Suffix'):
+    case 'Suffix':
       return `${item.Invert ? `NOT ` : ``}suffixed by "${item.Suffix}"`;
-    case keys.includes('Regex'):
+    case 'Regex':
       return `${item.Invert ? `NOT ` : ``}matching the regex "${item.Regex}"`;
   }
   return '';
