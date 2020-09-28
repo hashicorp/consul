87b4b99b1ea1ecd597c605732d13b833ea9445b7
John Cowen <jcowen@hashicorp.com>
2020-09-28 17:05:16 +0000

    Change list-collection to support native scrolling instead of virtual

--- ui-v2/app/components/list-collection/index.js
+++ ui-v2/app/components/list-collection/index.js
@@ -9,20 +9,23 @@ const formatItemStyle = PercentageColumns.prototype.formatItemStyle;
 
 export default Component.extend(Slotted, {
   dom: service('dom'),
-  tagName: 'div',
-  attributeBindings: ['style'],
+  tagName: '',
   height: 500,
   cellHeight: 70,
   style: style('getStyle'),
-  classNames: ['list-collection'],
   checked: null,
+  scroll: 'virtual',
   init: function() {
     this._super(...arguments);
     this.columns = [100];
+    this.guid = this.dom.guid(this);
   },
   didInsertElement: function() {
     this._super(...arguments);
-    this.actions.resize.apply(this, [{ target: this.dom.viewport() }]);
+    this.$element = this.dom.element(`#${this.guid}`);
+    if (this.scroll === 'virtual') {
+      this.actions.resize.apply(this, [{ target: this.dom.viewport() }]);
+    }
   },
   didReceiveAttrs: function() {
     this._super(...arguments);
@@ -41,6 +44,9 @@ export default Component.extend(Slotted, {
     };
   },
   getStyle: computed('height', function() {
+    if (this.scroll !== 'virtual') {
+      return {};
+    }
     return {
       height: get(this, 'height'),
     };
@@ -49,12 +55,11 @@ export default Component.extend(Slotted, {
     resize: function(e) {
       // TODO: This top part is very similar to resize in tabular-collection
       // see if it make sense to DRY out
-      const dom = get(this, 'dom');
-      const $appContent = dom.element('main > div');
+      const $appContent = this.dom.element('main > div');
       if ($appContent) {
         const border = 1;
-        const rect = this.element.getBoundingClientRect();
-        const $footer = dom.element('footer[role="contentinfo"]');
+        const rect = this.$element.getBoundingClientRect();
+        const $footer = this.dom.element('footer[role="contentinfo"]');
         const space = rect.top + $footer.clientHeight + border;
         const height = e.target.innerHeight - space;
         this.set('height', Math.max(0, height));
