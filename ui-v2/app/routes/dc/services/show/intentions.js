02285651429fb1105264620cd9a1342ee3ef680f
John Cowen <jcowen@hashicorp.com>
2020-09-29 16:52:02 +0000

    Add intention outlet

--- ui-v2/app/routes/dc/services/show/intentions.js
+++ ui-v2/app/routes/dc/services/show/intentions.js
@@ -1,3 +1,12 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend();
+export default Route.extend({
+  model: function() {
+    return {
+      routeName: this.routeName,
+    };
+  },
+  setupController: function(controller, model) {
+    controller.setProperties(model);
+  },
+});
