ac177c69d170fab48efef0b6477038da85b798f1
Hans Hasselberg <me@hans.io>
2020-09-30 12:57:31 +0200

    fix potential panic

--- api/health_test.go
+++ api/health_test.go
@@ -234,6 +234,7 @@ func TestAPI_HealthChecks(t *testing.T) {
 		if meta.LastIndex == 0 {
 			r.Fatalf("bad: %v", meta)
 		}
+		require.NotEmpty(t, out)
 		checks[0].CreateIndex = out[0].CreateIndex
 		checks[0].ModifyIndex = out[0].ModifyIndex
 		require.Equal(r, checks, out)
