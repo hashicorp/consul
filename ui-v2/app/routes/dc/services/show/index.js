4d3031f13f14bf2d44db324152f9afdf39cf6df8
kenia <keniavalladarez@gmail.com>
2020-09-30 11:34:50 -0400

    Update to only show Topology tab when Connect is enabled

--- ui-v2/app/routes/dc/services/show/index.js
+++ ui-v2/app/routes/dc/services/show/index.js
@@ -1,6 +1,37 @@
-import Route from 'consul-ui/routing/route';
-import to from 'consul-ui/utils/routing/redirect-to';
+import Route from '@ember/routing/route';
+import { get } from '@ember/object';
 
 export default Route.extend({
-  redirect: to('instances'),
+  afterModel: function(model, transition) {
+    const parent = this.routeName
+      .split('.')
+      .slice(0, -1)
+      .join('.');
+    // the default selected tab depends on whether you have any healthchecks or not
+    // so check the length here.
+    let to = 'topology';
+    const parentModel = this.modelFor(parent);
+
+    const kind = get(parentModel, 'items.firstObject.Service.Kind');
+
+    switch (kind) {
+      case 'ingress-gateway':
+        if (!get(parentModel, 'topology.Exists')) {
+          to = 'upstreams';
+        }
+        break;
+      case 'terminating-gateway':
+        to = 'services';
+        break;
+      case 'mesh-gateway':
+        to = 'instances';
+        break;
+      default:
+        if (!get(parentModel, 'topology.Exists')) {
+          to = 'instances';
+        }
+    }
+
+    this.replaceWith(`${parent}.${to}`, parentModel);
+  },
 });
--- ui-v2/app/services/repository/topology.js
+++ ui-v2/app/services/repository/topology.js
@@ -1,19 +1,32 @@
 import RepositoryService from 'consul-ui/services/repository';
+import { inject as service } from '@ember/service';
+import { get, set } from '@ember/object';
+
 const modelName = 'topology';
+const ERROR_MESH_DISABLED = 'Connect must be enabled in order to use this endpoint';
+
 export default RepositoryService.extend({
+  dcs: service('repository/dc'),
   getModelName: function() {
     return modelName;
   },
-  findBySlug: function(id, dc, nspace, configuration = {}) {
-    const query = {
-      dc: dc,
-      ns: nspace,
-      id: id,
-    };
-    if (typeof configuration.cursor !== 'undefined') {
-      query.index = configuration.cursor;
-      query.uri = configuration.uri;
+  findBySlug: function(slug, dc, nspace, configuration = {}) {
+    const datacenter = this.dcs.peekOne(dc);
+    if (datacenter !== null && !get(datacenter, 'MeshEnabled')) {
+      return Promise.resolve();
     }
-    return this.store.queryRecord(this.getModelName(), query);
+    return this._super(...arguments).catch(e => {
+      const code = get(e, 'errors.firstObject.status');
+      const body = get(e, 'errors.firstObject.detail').trim();
+      switch (code) {
+        case '500':
+          if (datacenter !== null && body.endsWith(ERROR_MESH_DISABLED)) {
+            set(datacenter, 'MeshEnabled', false);
+          }
+          return;
+        default:
+          throw e;
+      }
+    });
   },
 });
