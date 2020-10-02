eb2a5372aaa3c31dcd2afcbb79b6bff4affe6fa8
John Cowen <jcowen@hashicorp.com>
2020-10-02 12:48:17 +0000

    ui: Defer requesting gateway related services until the tab is visible

--- ui-v2/app/routes/dc/services/show.js
+++ ui-v2/app/routes/dc/services/show.js
@@ -6,7 +6,7 @@ import { get } from '@ember/object';
 export default Route.extend({
   data: service('data-source/service'),
   settings: service('settings'),
-  model: function(params, transition = {}) {
+  model: function(params, transition) {
     const dc = this.modelFor('dc').dc.Name;
     const nspace = this.modelFor('nspace').nspace.substr(1);
     return hash({
@@ -19,32 +19,19 @@ export default Route.extend({
       urls: this.settings.findBySlug('urls'),
       chain: null,
       proxies: [],
-    })
-      .then(model => {
-        return ['connect-proxy', 'mesh-gateway', 'ingress-gateway', 'terminating-gateway'].includes(
-          get(model, 'items.firstObject.Service.Kind')
-        )
-          ? model
-          : hash({
-              ...model,
-              chain: this.data.source(uri => uri`/${nspace}/${dc}/discovery-chain/${params.name}`),
-              proxies: this.data.source(
-                uri => uri`/${nspace}/${dc}/proxies/for-service/${params.name}`
-              ),
-            });
-      })
-      .then(model => {
-        return ['ingress-gateway', 'terminating-gateway'].includes(
-          get(model, 'items.firstObject.Service.Kind')
-        )
-          ? hash({
-              ...model,
-              gatewayServices: this.data.source(
-                uri => uri`/${nspace}/${dc}/gateways/for-service/${params.name}`
-              ),
-            })
-          : model;
-      });
+    }).then(model => {
+      return ['connect-proxy', 'mesh-gateway', 'ingress-gateway', 'terminating-gateway'].includes(
+        get(model, 'items.firstObject.Service.Kind')
+      )
+        ? model
+        : hash({
+            ...model,
+            chain: this.data.source(uri => uri`/${nspace}/${dc}/discovery-chain/${params.name}`),
+            proxies: this.data.source(
+              uri => uri`/${nspace}/${dc}/proxies/for-service/${params.name}`
+            ),
+          });
+    });
   },
   setupController: function(controller, model) {
     this._super(...arguments);
