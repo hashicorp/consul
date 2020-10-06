c93e5d00900abdd7c96a74af82daaf3d59ecc3ef
Paul Banks <banks@banksco.de>
2020-10-06 19:56:55 +0100

    WIP sparkline component

--- ui-v2/lib/startup/templates/body.html.js
+++ ui-v2/lib/startup/templates/body.html.js
@@ -41,6 +41,7 @@ module.exports = ({ appName, environment, rootURL, config }) => `
       }
     };
   </script>
+  <script src="${rootURL}assets/metrics-providers/prometheus.js"></script>
   ${environment === 'production' ? `{{ range .ExtraScripts }} <script src="{{.}}"></script> {{ end }}` : ``}
   ${environment === 'test' ? `<script src="${rootURL}assets/tests.js"></script>` : ``}
 `;
