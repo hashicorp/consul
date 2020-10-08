ec084cf79be81c9e60a3f14db288e4133b7cd813
John Cowen <johncowen@users.noreply.github.com>
2020-10-08 16:00:52 +0100

    ui: Use a custom request for nspace deletion (#8878)
    * Turn repo.remove into a custom action to bypass ember-data deletion
    
    * Don't show actions on a deleting nspace

--- ui-v2/app/services/repository/nspace.js
+++ ui-v2/app/services/repository/nspace.js
@@ -12,6 +12,27 @@ export default RepositoryService.extend({
   getModelName: function() {
     return modelName;
   },
+  remove: function(item) {
+    // Namespace deletion is more of a soft delete.
+    // Therefore the namespace still exists once we've requested a delete/removal.
+    // This makes 'removing' more of a custom action rather than a standard
+    // ember-data delete.
+    // Here we use the same request for a delete but we bypass ember-data's
+    // destroyRecord/unloadRecord and serialization so we don't get
+    // ember data error messages when the UI tries to update a 'DeletedAt' property
+    // on an object that ember-data is trying to delete
+    const res = this.store.adapterFor('nspace').rpc(
+      (adapter, request, serialized, unserialized) => {
+        return adapter.requestForDeleteRecord(request, serialized, unserialized);
+      },
+      (serializer, respond, serialized, unserialized) => {
+        return item;
+      },
+      item,
+      'nspace'
+    );
+    return res;
+  },
   findAll: function(configuration = {}) {
     const query = {};
     if (typeof configuration.cursor !== 'undefined') {
