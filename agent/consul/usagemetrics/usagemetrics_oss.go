30540e406b0a7d4e1c853fc4220f2e62b84f4d1d
Chris Piraino <cpiraino@hashicorp.com>
2020-10-09 11:01:45 -0500

    Emit service usage metrics with correct labeling strategy (#8856)
    Previously, we would emit service usage metrics both with and without a
    namespace label attached. This is problematic in the case when you want
    to aggregate metrics together, i.e. "sum(consul.state.services)". This
    would cause services to be counted twice in that aggregate, once via the
    metric emitted with a namespace label, and once in the metric emited
    without any namespace label.

--- agent/consul/usagemetrics/usagemetrics_oss.go
+++ agent/consul/usagemetrics/usagemetrics_oss.go
@@ -2,6 +2,21 @@
 
 package usagemetrics
 
-import "github.com/hashicorp/consul/agent/consul/state"
+import (
+	"github.com/armon/go-metrics"
+	"github.com/hashicorp/consul/agent/consul/state"
+)
 
-func (u *UsageMetricsReporter) emitEnterpriseUsage(state.ServiceUsage) {}
+func (u *UsageMetricsReporter) emitServiceUsage(serviceUsage state.ServiceUsage) {
+	metrics.SetGaugeWithLabels(
+		[]string{"consul", "state", "services"},
+		float32(serviceUsage.Services),
+		u.metricLabels,
+	)
+
+	metrics.SetGaugeWithLabels(
+		[]string{"consul", "state", "service_instances"},
+		float32(serviceUsage.ServiceInstances),
+		u.metricLabels,
+	)
+}
