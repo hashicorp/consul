8b92440b0ae0bf22c82848be16db430cd6948d39
John Cowen <jcowen@hashicorp.com>
2020-10-21 18:22:37 +0000

    Add new pageobjects using the new noun.verb format

---
+++ ui/packages/consul-ui/app/components/power-select/pageobject.js
@@ -0,0 +1,18 @@
+import { clickable, isPresent } from 'ember-cli-page-object';
+import { options } from 'consul-ui/tests/lib/page-object';
+
+export default options => {
+  return {
+    present: isPresent('.ember-power-select-trigger'),
+    click: clickable('.ember-power-select-trigger'),
+    option: {
+      resetScope: true,
+      ...options.reduce((prev, item, i) => {
+        prev[item] = {
+          click: clickable(`[data-option-index='${i}']`),
+        };
+        return prev;
+      }, {}),
+    },
+  };
+};
