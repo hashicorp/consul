b3ec64c23fda6aa6f19b8a452f281315f7a56970
Kevin Pruett <kpruett@hashicorp.com>
2020-10-26 16:08:23 -0400

    Merge pull request #9021 from hashicorp/pruett.alertbanner-exp
    Expose `expirationDate` prop in <AlertBanner/>

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -6,4 +6,7 @@ export default {
   url: 'https://www.hashicorp.com/blog/consul-service-on-azure-production-tier',
   text: 'HashiCorp Consul Service on Azure Production SKU',
   linkText: 'Learn more',
+  // Set the `expirationDate prop with a datetime string (e.g. `2020-01-31T12:00:00-07:00`)
+  // if you'd like the component to stop showing at or after a certain date
+  expirationDate: null,
 }
