d5d4155e1a15780e5a60d875595ca5771c6509fc
John Cowen <johncowen@users.noreply.github.com>
2020-11-03 14:09:39 +0000

    ui: Storybook Install (#9049)
    * ui: Install storybook into the main project
    
    * Add a basic story for a notice
    
    * Remove empty dependencies

---
+++ ui/packages/consul-ui/.storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "stories": [
+    "../app/**/*.stories.mdx"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-docs",
+    "@storybook/addon-knobs",
+  ]
+}
