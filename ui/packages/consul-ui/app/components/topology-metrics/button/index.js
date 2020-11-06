2018097c7d83cfc9997c7c74ea15be1fe8b654ad
kenia <keniavalladarez@gmail.com>
2020-11-06 15:17:44 -0500

    Return a link to the create intention page

--- ui/packages/consul-ui/app/components/topology-metrics/button/index.js
+++ ui/packages/consul-ui/app/components/topology-metrics/button/index.js
@@ -1,12 +1,8 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
-import { inject as service } from '@ember/service';
 
 export default class TopoloyMetricsButton extends Component {
-  // =services
-  @service('repository/intention') repo;
-
   // =attributes
   @tracked showToggleablePopover = false;
 
@@ -14,13 +10,5 @@ export default class TopoloyMetricsButton extends Component {
   @action
   togglePopover() {
     this.showToggleablePopover = !this.showToggleablePopover;
-  }
-  @action
-  open() {
-    console.log('hello');
-  }
-  @action
-  createIntention(obj) {
-    this.repo.persist(obj);
   }
 }
