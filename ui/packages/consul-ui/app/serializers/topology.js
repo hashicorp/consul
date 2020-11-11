662787f093f6e5ad45b52a1d66bc69905776ccd5
John Cowen <jcowen@hashicorp.com>
2020-11-11 09:20:37 +0000

    Fixup serializer getter

--- ui/packages/consul-ui/app/serializers/topology.js
+++ ui/packages/consul-ui/app/serializers/topology.js
@@ -3,10 +3,13 @@ import { PRIMARY_KEY, SLUG_KEY } from 'consul-ui/models/topology';
 import { inject as service } from '@ember/service';
 
 export default class TopologySerializer extends Serializer {
+  @service('store') store;
+
   primaryKey = PRIMARY_KEY;
   slugKey = SLUG_KEY;
 
   respondForQueryRecord(respond, query) {
+    const intentionSerializer = this.store.serializerFor('intention');
     return super.respondForQueryRecord(function(cb) {
       return respond(function(headers, body) {
         body.Downstreams.forEach(item => {
