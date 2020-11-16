4d39305442e8a96ba81daab86d9d623800818a6b
Freddy <freddygv@users.noreply.github.com>
2020-11-16 16:37:19 -0700

    Add DC and NS support for Envoy metrics (#9207)
    This PR updates the tags that we generate for Envoy stats.
    
    Several of these come with breaking changes, since we can't keep two stats prefixes for a filter.

--- ui/packages/consul-ui/app/components/topology-metrics/stats/index.js
+++ ui/packages/consul-ui/app/components/topology-metrics/stats/index.js
@@ -14,7 +14,11 @@ export default class TopologyMetricsStats extends Component {
     } else {
       // For up/downstreams we need to pull out the stats for the service we
       // represent.
-      this.stats = event.data.stats[this.args.item];
+      if ((this.args.nspace || '').length === 0) {
+        this.args.nspace = 'default';
+      }
+      let entity = `${this.args.item}.${this.args.nspace}.${this.args.dc}`;
+      this.stats = event.data.stats[entity];
     }
     // Limit to 4 metrics for now.
     this.stats = (this.stats || []).slice(0, 4);
