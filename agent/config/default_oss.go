0a44906fe384069d31bb8ce9ed5bcdfe167ca852
Daniel Nephin <dnephin@hashicorp.com>
2020-11-20 18:14:17 -0500

    config: Use LiteralSource for some defaults
    Using the LiteralSource makes it much easier to find default values, because an IDE reports
    the location of a default. With an HCL string they are harder to discover.
    
    Also removes unnecessary mapstructure.Decodes of constant values.

--- agent/config/default_oss.go
+++ agent/config/default_oss.go
@@ -5,22 +5,12 @@ package config
 // DefaultEnterpriseSource returns the consul agent configuration for enterprise mode.
 // These can be overridden by the user and therefore this source should be merged in the
 // head and processed before user configuration.
-// TODO: return a LiteralSource (no decoding) instead of a FileSource
 func DefaultEnterpriseSource() Source {
-	return FileSource{
-		Name:   "enterprise-defaults",
-		Format: "hcl",
-		Data:   ``,
-	}
+	return LiteralSource{Name: "enterprise-defaults"}
 }
 
 // OverrideEnterpriseSource returns the consul agent configuration for the enterprise mode.
 // This should be merged in the tail after the DefaultConsulSource.
-// TODO: return a LiteralSource (no decoding) instead of a FileSource
 func OverrideEnterpriseSource() Source {
-	return FileSource{
-		Name:   "enterprise-overrides",
-		Format: "hcl",
-		Data:   ``,
-	}
+	return LiteralSource{Name: "enterprise-overrides"}
 }
