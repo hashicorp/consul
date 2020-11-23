2e94cc38d6ef128f36d234668572dfc5e4d4f67e
John Cowen <johncowen@users.noreply.github.com>
2020-11-23 12:01:45 +0000

    Use DataCollection component for service type things

--- ui/packages/consul-ui/app/components/consul/service/list/index.js
+++ ui/packages/consul-ui/app/components/consul/service/list/index.js
@@ -1,45 +1,7 @@
 import Component from '@glimmer/component';
-import { inject as service } from '@ember/service';
-import { sort } from '@ember/object/computed';
-import { action, defineProperty } from '@ember/object';
+import { action } from '@ember/object';
 
 export default class ConsulServiceList extends Component {
-  @service('filter') filter;
-  @service('sort') sort;
-  @service('search') search;
-
-  type = 'service';
-
-  get items() {
-    let comparator = 'comparator';
-    if (typeof this.comparator === 'function') {
-      comparator = this.comparator;
-    }
-    defineProperty(this, 'sorted', sort('searched', comparator));
-    return this.sorted;
-  }
-
-  get searched() {
-    if (typeof this.args.search === 'undefined') {
-      return this.filtered;
-    }
-    const predicate = this.search.predicate(this.type);
-    const options = {};
-    if (typeof this.args.filters.searchproperties !== 'undefined') {
-      options.properties = this.args.filters.searchproperties;
-    }
-    return this.filtered.filter(predicate(this.args.search, options));
-  }
-
-  get filtered() {
-    const predicate = this.filter.predicate(this.type);
-    return this.args.items.filter(predicate(this.args.filters));
-  }
-
-  get comparator() {
-    return this.sort.comparator('service')(this.args.sort);
-  }
-
   @action
   isLinkable(item) {
     return item.InstanceCount > 0;
--- ui/packages/consul-ui/app/components/consul/upstream/list/index.js
+++ ui/packages/consul-ui/app/components/consul/upstream/list/index.js
@@ -1,41 +1,7 @@
 import Component from '@glimmer/component';
-import { inject as service } from '@ember/service';
-import { sort } from '@ember/object/computed';
-import { action, defineProperty } from '@ember/object';
+import { action } from '@ember/object';
 
 export default class ConsulServiceList extends Component {
-  @service('filter') filter;
-  @service('sort') sort;
-  @service('search') search;
-
-  get items() {
-    let comparator = 'comparator';
-    if (typeof this.comparator === 'function') {
-      comparator = this.comparator;
-    }
-    defineProperty(this, 'sorted', sort('searched', comparator));
-    return this.sorted;
-  }
-
-  get searched() {
-    if (typeof this.args.search === 'undefined') {
-      return this.filtered;
-    }
-    const predicate = this.search.predicate('service');
-    return this.filtered.filter(
-      predicate(this.args.search, { properties: this.args.filters.searchproperties })
-    );
-  }
-
-  get filtered() {
-    const predicate = this.filter.predicate('service');
-    return this.args.items.filter(predicate(this.args.filters));
-  }
-
-  get comparator() {
-    return this.sort.comparator('service')(this.args.sort);
-  }
-
   @action
   isLinkable(item) {
     return item.InstanceCount > 0;
