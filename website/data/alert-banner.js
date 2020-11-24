6f24a4024e581472861b43895edc1fac1925f25f
David Yu <dyu@hashicorp.com>
2020-11-24 13:17:51 -0800

    Consul 1.9 GA Banner

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -3,8 +3,8 @@ export const ALERT_BANNER_ACTIVE = true
 // https://github.com/hashicorp/web-components/tree/master/packages/alert-banner
 export default {
   tag: 'Announcing',
-  url: 'https://www.hashicorp.com/blog/announcing-hashicorp-consul-1-9',
-  text: 'HashiCorp Consul 1.9 now available in beta.',
+  url: 'https://www.hashicorp.com/blog/announcing-general-availability-of-hashicorp-consul-1-9',
+  text: 'HashiCorp Consul 1.9 is now Generally Available (GA) .',
   linkText: 'Learn more',
   // Set the `expirationDate prop with a datetime string (e.g. `2020-01-31T12:00:00-07:00`)
   // if you'd like the component to stop showing at or after a certain date
