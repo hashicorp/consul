5f388d8bff6d093a20228520c6822c499467aade
John Cowen <johncowen@users.noreply.github.com>
2020-11-24 17:35:05 +0000

    Token search/sort/filter

--- ui/packages/consul-ui/app/services/search.js
+++ ui/packages/consul-ui/app/services/search.js
@@ -8,8 +8,8 @@ import acl from 'consul-ui/search/predicates/acl';
 import service from 'consul-ui/search/predicates/service';
 import node from 'consul-ui/search/predicates/node';
 import kv from 'consul-ui/search/predicates/kv';
+import token from 'consul-ui/search/predicates/token';
 
-import token from 'consul-ui/search/filters/token';
 import policy from 'consul-ui/search/filters/policy';
 import role from 'consul-ui/search/filters/role';
 // service instance
@@ -20,7 +20,6 @@ import nspace from 'consul-ui/search/filters/nspace';
 import filterableFactory from 'consul-ui/utils/search/filterable';
 const filterable = filterableFactory();
 const searchables = {
-  token: token(filterable),
   policy: policy(filterable),
   role: role(filterable),
   serviceInstance: serviceNode(filterable),
@@ -45,11 +44,12 @@ export const search = spec => {
 const predicates = {
   intention: search(intention),
   service: search(service),
+  ['service-instance']: search(serviceInstance),
+  ['upstream-instance']: upstreamInstance(),
   node: search(node),
   kv: search(kv),
-  ['service-instance']: search(serviceInstance),
   acl: search(acl),
-  ['upstream-instance']: upstreamInstance(),
+  token: search(token),
 };
 export default class SearchService extends Service {
   searchable(name) {
