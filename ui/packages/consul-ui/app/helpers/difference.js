72d9d0a5ecadd1470ac2034feab0dc84336a94d0
John Cowen <johncowen@users.noreply.github.com>
2020-11-30 17:19:44 +0000

    ui: Remove unused helpers or swap for helpers in ember-string-fns (#9294)
    * ui: Remove unused helpers or swap for helpers in ember-string-fns
    
    * starts-with > string-starts-with

--- ui/packages/consul-ui/app/helpers/difference.js
+++
@@ -1,9 +0,0 @@
-import { helper } from '@ember/component/helper';
-import { get } from '@ember/object';
-export function difference(params, hash) {
-  return params[0].filter(function(item) {
-    return !params[1].findBy('ID', get(item, 'ID'));
-  });
-}
-
-export default helper(difference);
