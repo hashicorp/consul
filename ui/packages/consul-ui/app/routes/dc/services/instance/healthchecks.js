1adb9fdb675e1292c866ebaf06a23f9beae8a978
John Cowen <johncowen@users.noreply.github.com>
2020-12-02 14:28:54 +0000

    Add Healthcheck search/sort/filtering

--- ui/packages/consul-ui/app/routes/dc/services/instance/healthchecks.js
+++ ui/packages/consul-ui/app/routes/dc/services/instance/healthchecks.js
@@ -1,6 +1,21 @@
 import Route from 'consul-ui/routing/route';
 
 export default class HealthchecksRoute extends Route {
+  queryParams = {
+    sortBy: 'sort',
+    status: 'status',
+    kind: 'kind',
+    check: 'check',
+    searchproperty: {
+      as: 'searchproperty',
+      empty: [['Name', 'CheckID', 'Notes', 'Output', 'ServiceTags']],
+    },
+    search: {
+      as: 'filter',
+      replace: true,
+    },
+  };
+
   model() {
     const parent = this.routeName
       .split('.')
