9d9e77f417f0a69fa817d11cf48b7093c5d5c00e
John Cowen <johncowen@users.noreply.github.com>
2020-12-02 15:42:18 +0000

    ui: Asyncify Service Model Hooks (#9312)
    * ui: Add Kind grouping computed properties
    
    * Use new computed properties and move model hooks to async methods

--- ui/packages/consul-ui/app/routes/dc/services/show.js
+++ ui/packages/consul-ui/app/routes/dc/services/show.js
@@ -1,6 +1,5 @@
 import { inject as service } from '@ember/service';
 import Route from 'consul-ui/routing/route';
-import { hash } from 'rsvp';
 import { get } from '@ember/object';
 import { action, setProperties } from '@ember/object';
 
@@ -24,54 +23,48 @@ export default class ShowRoute extends Route {
     this.refresh();
   }
 
-  model(params, transition) {
+  async model(params, transition) {
     const dc = this.modelFor('dc').dc.Name;
     const nspace = this.modelFor('nspace').nspace.substr(1);
-    return hash({
-      slug: params.name,
-      dc: dc,
-      nspace: nspace,
-      items: this.data.source(
-        uri => uri`/${nspace}/${dc}/service-instances/for-service/${params.name}`
-      ),
-      urls: this.config.get().dashboard_url_templates,
-      chain: null,
-      proxies: [],
-      topology: null,
-    })
-      .then(model => {
-        return ['connect-proxy', 'mesh-gateway', 'ingress-gateway', 'terminating-gateway'].includes(
-          get(model, 'items.firstObject.Service.Kind')
-        )
-          ? model
-          : hash({
-              ...model,
-              chain: this.data.source(uri => uri`/${nspace}/${dc}/discovery-chain/${params.name}`),
-              // Whilst `proxies` isn't used anywhere in the show templates
-              // it provides a relationship of ProxyInstance on the ServiceInstance
-              // which can respond at a completely different blocking rate to
-              // the ServiceInstance itself
-              proxies: this.data.source(
-                uri => uri`/${nspace}/${dc}/proxies/for-service/${params.name}`
-              ),
-            });
-      })
-      .then(model => {
-        let kind = get(model, 'items.firstObject.Service.Kind');
+    const slug = params.name;
+
+    let chain = null;
+    let topology = null;
+    let proxies = [];
+
+    const urls = this.config.get().dashboard_url_templates;
+    const items = await this.data.source(
+      uri => uri`/${nspace}/${dc}/service-instances/for-service/${params.name}`
+    );
+
+    const item = get(items, 'firstObject');
+    if (get(item, 'IsOrigin')) {
+      chain = await this.data.source(uri => uri`/${nspace}/${dc}/discovery-chain/${params.name}`);
+      proxies = await this.data.source(
+        uri => uri`/${nspace}/${dc}/proxies/for-service/${params.name}`
+      );
+
+      if (get(item, 'IsMeshOrigin')) {
+        let kind = get(item, 'Service.Kind');
         if (typeof kind === 'undefined') {
           kind = '';
         }
-        return ['mesh-gateway', 'terminating-gateway'].includes(
-          get(model, 'items.firstObject.Service.Kind')
-        )
-          ? model
-          : hash({
-              ...model,
-              topology: this.data.source(
-                uri => uri`/${nspace}/${dc}/topology/${params.name}/${kind}`
-              ),
-            });
-      });
+        topology = await this.data.source(
+          uri => uri`/${nspace}/${dc}/topology/${params.name}/${kind}`
+        );
+      }
+    }
+
+    return {
+      dc,
+      nspace,
+      slug,
+      items,
+      urls,
+      chain,
+      proxies,
+      topology,
+    };
   }
 
   setupController(controller, model) {
