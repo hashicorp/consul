ffef5d7946ad3926af69de288adb265b2a9b3bc0
John Cowen <johncowen@users.noreply.github.com>
2020-12-04 11:38:38 +0000

    Integrate ServiceInstance search bar for Node:ServiceInstances

--- ui/packages/consul-ui/app/routes/dc/nodes/show/services.js
+++ ui/packages/consul-ui/app/routes/dc/nodes/show/services.js
@@ -2,6 +2,13 @@ import Route from 'consul-ui/routing/route';
 
 export default class ServicesRoute extends Route {
   queryParams = {
+    sortBy: 'sort',
+    status: 'status',
+    source: 'source',
+    searchproperty: {
+      as: 'searchproperty',
+      empty: [['Name', 'Tags', 'ID', 'Address', 'Port', 'Service.Meta', 'Node.Meta']],
+    },
     search: {
       as: 'filter',
       replace: true,
