ee3a416a0ea07a7146d78e4cbc918ef7abfa997f
kenia <keniavalladarez@gmail.com>
2020-12-07 12:00:48 -0500

    Refactor metrics error message for ingress-gateway

--- ui/packages/consul-ui/app/routes/dc/services/show/topology.js
+++ ui/packages/consul-ui/app/routes/dc/services/show/topology.js
@@ -1,12 +1,21 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class TopologyRoute extends Route {
+  @service('ui-config') config;
+  @service('env') env;
+
   model() {
     const parent = this.routeName
       .split('.')
       .slice(0, -1)
       .join('.');
-    return this.modelFor(parent);
+
+    return {
+      ...this.modelFor(parent),
+      hasMetricsProvider: !!this.config.get().metrics_provider,
+      isRemoteDC: this.env.var('CONSUL_DATACENTER_LOCAL') !== this.modelFor('dc').dc.Name,
+    };
   }
 
   setupController(controller, model) {
