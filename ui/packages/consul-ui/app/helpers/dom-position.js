987c54941844ced230fdcd2bf66ca40024cb68bb
John Cowen <johncowen@users.noreply.github.com>
2020-12-09 17:48:08 +0000

    Rerrange things to use on-resize modifier for positioning

--- ui/packages/consul-ui/app/helpers/dom-position.js
+++ ui/packages/consul-ui/app/helpers/dom-position.js
@@ -1,16 +1,18 @@
 import Helper from '@ember/component/helper';
-import { inject as service } from '@ember/service';
 
 export default class DomPosition extends Helper {
-  @service('dom') dom;
-
-  compute([target, from], hash) {
-    const $target = this.dom.element(target);
-    const $from = this.dom.element(from);
-    const fromRect = $from.getBoundingClientRect();
-    const rect = $target.getBoundingClientRect();
-    rect.x = rect.x - fromRect.x;
-    rect.y = rect.y - fromRect.y;
-    return rect;
+  compute([target], { from }) {
+    if (typeof target === 'function') {
+      return entry => {
+        const $target = entry.target;
+        let rect = $target.getBoundingClientRect();
+        if (typeof from !== 'undefined') {
+          const fromRect = from.getBoundingClientRect();
+          rect.x = rect.x - fromRect.x;
+          rect.y = rect.y - fromRect.y;
+        }
+        return target(rect);
+      };
+    }
   }
 }
