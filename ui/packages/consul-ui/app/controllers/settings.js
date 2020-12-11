514270a413d70d3183fe7c570c39c87d5e8a6490
John Cowen <johncowen@users.noreply.github.com>
2020-12-11 11:43:13 +0000

    ui: Controller dead code removal (#9367)
    * ui: Controller dead code removal
    
    This commit removes a little code that became 'dead' as a result of
    previous PRs/commits
    
    * Remove a little more from the settings Controller
    
    * Remove CSS related to being able to set the dashboard_url in settings

--- ui/packages/consul-ui/app/controllers/settings.js
+++ ui/packages/consul-ui/app/controllers/settings.js
@@ -3,26 +3,8 @@ import { get, set } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default Controller.extend({
-  repo: service('settings'),
   dom: service('dom'),
-  timeout: service('timeout'),
-  confirming: false,
-  applyTransition: function() {
-    const tick = this.timeout.tick;
-    set(this, 'confirming', true);
-    tick().then(() => {
-      set(this, 'confirming', false);
-    });
-  },
   actions: {
-    key: function(e) {
-      this.applyTransition();
-      switch (true) {
-        case e.keyCode === 13:
-          // disable ENTER
-          e.preventDefault();
-      }
-    },
     change: function(e, value, item) {
       const event = this.dom.normalizeEvent(e, value);
       // TODO: Switch to using forms like the rest of the app
@@ -36,13 +18,6 @@ export default Controller.extend({
         case 'client[blocking]':
           set(this, 'item.client.blocking', !blocking);
           this.send('update', 'client', this.item.client);
-          break;
-        case 'urls[service]':
-          if (typeof get(this, 'item.urls') === 'undefined') {
-            set(this, 'item.urls', {});
-          }
-          set(this, 'item.urls.service', target.value);
-          this.send('update', 'urls', this.item.urls);
           break;
       }
     },
