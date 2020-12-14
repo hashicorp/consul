b5c13c1e366d729184394efe57e0019bfefc4a56
Jeff Escalante <jescalan@users.noreply.github.com>
2020-12-13 22:20:31 -0500

    add markdown page component

--- website/pages/security.jsx
+++ website/pages/security.jsx
@@ -1,10 +1,10 @@
-import MarkdownPage from 'components/_temporary-markdown-page'
-import generateStaticProps from 'components/_temporary-markdown-page/server'
+import MarkdownPage from '@hashicorp/react-markdown-page'
+import generateStaticProps from '@hashicorp/react-markdown-page/server'
 
-export default function CommunityToolsPage({ staticProps }) {
+export default function SecurityPage(staticProps) {
   return <MarkdownPage {...staticProps} />
 }
 
 export const getStaticProps = generateStaticProps({
-  pagePath: 'content/community-plugins.mdx',
+  pagePath: 'content/security.mdx',
 })
