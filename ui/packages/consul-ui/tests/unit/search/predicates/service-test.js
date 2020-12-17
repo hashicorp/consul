4835ea9b45de5a88ce87dec0023befba8afda807
John Cowen <johncowen@users.noreply.github.com>
2020-12-17 18:18:27 +0000

    Move tests around

--- ui/packages/consul-ui/tests/unit/search/predicates/service-test.js
+++ ui/packages/consul-ui/tests/unit/search/predicates/service-test.js
@@ -1,54 +1,63 @@
-import { search } from 'consul-ui/services/search';
-import spec from 'consul-ui/search/predicates/service';
 import { module, test } from 'qunit';
 
-module('Unit | Search | Filter | service', function() {
-  const predicate = search(spec);
+import ExactSearch from 'consul-ui/utils/search/exact';
+import predicates from 'consul-ui/search/predicates/service';
+
+module('Unit | Search | Predicate | service', function() {
   test('items are found by properties', function(assert) {
-    [
+    const actual = new ExactSearch(
+      [
+        {
+          Name: 'name-HIT',
+          Tags: [],
+        },
+        {
+          Name: 'name',
+          Tags: ['tag', 'tag-withHiT'],
+        },
+      ],
       {
-        Name: 'name-HIT',
-        Tags: [],
-      },
-      {
-        Name: 'name',
-        Tags: ['tag', 'tag-withHiT'],
-      },
-    ].forEach(function(item) {
-      const actual = predicate('hit')(item);
-      assert.ok(actual);
-    });
+        finders: predicates,
+      }
+    ).search('hit');
+    assert.equal(actual.length, 2);
   });
   test('items are not found', function(assert) {
-    [
+    const actual = new ExactSearch(
+      [
+        {
+          Name: 'name',
+        },
+        {
+          Name: 'name',
+          Tags: ['one', 'two'],
+        },
+      ],
       {
-        Name: 'name',
-      },
-      {
-        Name: 'name',
-        Tags: ['one', 'two'],
-      },
-    ].forEach(function(item) {
-      const actual = predicate('hit')(item);
-      assert.notOk(actual);
-    });
+        finders: predicates,
+      }
+    ).search('hit');
+    assert.equal(actual.length, 0);
   });
   test('tags can be empty', function(assert) {
-    [
+    const actual = new ExactSearch(
+      [
+        {
+          Name: 'name',
+        },
+        {
+          Name: 'name',
+          Tags: null,
+        },
+        {
+          Name: 'name',
+          Tags: [],
+        },
+      ],
       {
-        Name: 'name',
-      },
-      {
-        Name: 'name',
-        Tags: null,
-      },
-      {
-        Name: 'name',
-        Tags: [],
-      },
-    ].forEach(function(item) {
-      const actual = predicate('hit')(item);
-      assert.notOk(actual);
-    });
+        finders: predicates,
+      }
+    ).search('hit');
+    assert.equal(actual.length, 0);
   });
 });
