3e72b164b7356b29333442d88afff51229e711cb
John Cowen <jcowen@hashicorp.com>
2020-12-21 10:59:40 +0000

    Internationalize all of the service search-bar

--- ui/packages/consul-ui/app/utils/intl/missing-message.js
+++ ui/packages/consul-ui/app/utils/intl/missing-message.js
@@ -1,3 +1,9 @@
+// if we can't find the message, take the last part of the identifier and
+// ucfirst it so it looks human
 export default function missingMessage(key, locales) {
-  return;
+  const last = key
+    .split('.')
+    .pop()
+    .replaceAll('-', ' ');
+  return `${last.substr(0, 1).toUpperCase()}${last.substr(1)}`;
 }
