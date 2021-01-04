a95bd0b10fe8f1610ddf7259503309196e0fba7b
John Cowen <jcowen@hashicorp.com>
2021-01-04 17:06:44 +0000

    ui: Make sure we pass the nspace through to the API for nodes

--- ui-v2/app/adapters/node.js
+++ ui-v2/app/adapters/node.js
@@ -1,15 +1,26 @@
 import Adapter from './application';
 // TODO: Update to use this.formatDatacenter()
+
+// Node and Namespaces are a little strange in that Nodes don't belong in a
+// namespace whereas things that belong to a Node do (Health Checks and
+// Service Instances). So even though Nodes themselves don't require a
+// namespace filter, you sill needs to pass the namespace through to API
+// requests in order to get the correct information for the things that belong
+// to the node.
+
 export default Adapter.extend({
-  requestForQuery: function(request, { dc, index, id, uri }) {
+  requestForQuery: function(request, { dc, ns, index, id, uri }) {
     return request`
       GET /v1/internal/ui/nodes?${{ dc }}
       X-Request-ID: ${uri}
 
-      ${{ index }}
+      ${{
+        ...this.formatNspace(ns),
+        index,
+      }}
     `;
   },
-  requestForQueryRecord: function(request, { dc, index, id, uri }) {
+  requestForQueryRecord: function(request, { dc, ns, index, id, uri }) {
     if (typeof id === 'undefined') {
       throw new Error('You must specify an id');
     }
@@ -17,7 +28,10 @@ export default Adapter.extend({
       GET /v1/internal/ui/node/${id}?${{ dc }}
       X-Request-ID: ${uri}
 
-      ${{ index }}
+      ${{
+        ...this.formatNspace(ns),
+        index,
+      }}
     `;
   },
   requestForQueryLeader: function(request, { dc }) {
--- ui-v2/tests/integration/adapters/node-test.js
+++ ui-v2/tests/integration/adapters/node-test.js
@@ -1,28 +1,42 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import { env } from '../../../env';
+const shouldHaveNspace = function(nspace) {
+  return typeof nspace !== 'undefined' && env('CONSUL_NSPACES_ENABLED');
+};
 module('Integration | Adapter | node', function(hooks) {
   setupTest(hooks);
   const dc = 'dc-1';
   const id = 'node-name';
-  test('requestForQuery returns the correct url', function(assert) {
-    const adapter = this.owner.lookup('adapter:node');
-    const client = this.owner.lookup('service:client/http');
-    const expected = `GET /v1/internal/ui/nodes?dc=${dc}`;
-    const actual = adapter.requestForQuery(client.requestParams.bind(client), {
-      dc: dc,
+  const undefinedNspace = 'default';
+  [undefinedNspace, 'team-1', undefined].forEach(nspace => {
+    test(`requestForQuery returns the correct url when nspace is ${nspace}`, function(assert) {
+      const adapter = this.owner.lookup('adapter:node');
+      const client = this.owner.lookup('service:client/http');
+      const expected = `GET /v1/internal/ui/nodes?dc=${dc}${
+        shouldHaveNspace(nspace) ? `&ns=${nspace}` : ``
+      }`;
+      const actual = adapter.requestForQuery(client.requestParams.bind(client), {
+        dc: dc,
+        ns: nspace,
+      });
+      assert.equal(`${actual.method} ${actual.url}`, expected);
     });
-    assert.equal(`${actual.method} ${actual.url}`, expected);
-  });
-  test('requestForQueryRecord returns the correct url', function(assert) {
-    const adapter = this.owner.lookup('adapter:node');
-    const client = this.owner.lookup('service:client/http');
-    const expected = `GET /v1/internal/ui/node/${id}?dc=${dc}`;
-    const actual = adapter.requestForQueryRecord(client.requestParams.bind(client), {
-      dc: dc,
-      id: id,
+    test(`requestForQueryRecord returns the correct url when the nspace is ${nspace}`, function(assert) {
+      const adapter = this.owner.lookup('adapter:node');
+      const client = this.owner.lookup('service:client/http');
+      const expected = `GET /v1/internal/ui/node/${id}?dc=${dc}${
+        shouldHaveNspace(nspace) ? `&ns=${nspace}` : ``
+      }`;
+      const actual = adapter.requestForQueryRecord(client.requestParams.bind(client), {
+        dc: dc,
+        id: id,
+        ns: nspace,
+      });
+      assert.equal(`${actual.method} ${actual.url}`, expected);
     });
-    assert.equal(`${actual.method} ${actual.url}`, expected);
   });
+  // the following don't require nspacing
   test("requestForQueryRecord throws if you don't specify an id", function(assert) {
     const adapter = this.owner.lookup('adapter:node');
     const client = this.owner.lookup('service:client/http');
@@ -36,9 +50,9 @@ module('Integration | Adapter | node', function(hooks) {
     const adapter = this.owner.lookup('adapter:node');
     const client = this.owner.lookup('service:client/http');
     const expected = `GET /v1/status/leader?dc=${dc}`;
-    const actual = adapter.requestForQueryLeader(client.url, {
+    const actual = adapter.requestForQueryLeader(client.requestParams.bind(client), {
       dc: dc,
     });
-    assert.equal(actual, expected);
+    assert.equal(`${actual.method} ${actual.url}`, expected);
   });
 });
