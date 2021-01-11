39ab1cc60d2a522cca8fcb4d33c7ee662556cfd3
Mike Wickett <mwickett@hashicorp.com>
2021-01-11 11:17:59 -0500

    Merge pull request #9546 from hashicorp/mw.update-alert-banner-webinar
    website: update alert banner to promote webinar

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -4,10 +4,10 @@ export const ALERT_BANNER_ACTIVE = true
 export default {
   tag: 'Webinar',
   url:
-    'https://www.hashicorp.com/events/webinars/an-introduction-to-federation-on-hcs',
-  text: 'An Introduction to Federation on HCS',
+    'https://www.hashicorp.com/events/webinars/cloud-service-networking-secrets-management',
+  text: 'Getting Started with Service Networking & Secrets Management on Cloud',
   linkText: 'Register Now',
   // Set the `expirationDate prop with a datetime string (e.g. `2020-01-31T12:00:00-07:00`)
   // if you'd like the component to stop showing at or after a certain date
-  expirationDate: '2020-12-17T12:00:00-05:00',
+  expirationDate: '2021-02-09T11:59:00-05:00',
 }
