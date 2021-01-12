7bbe3ebef71af72942805135c4f9d2c91dbbd6df
John Cowen <jcowen@hashicorp.com>
2021-01-12 14:00:12 +0000

    Tweak the HTML for hashicorp-consul

--- ui/packages/consul-ui/app/components/hashicorp-consul/index.js
+++ ui/packages/consul-ui/app/components/hashicorp-consul/index.js
@@ -3,6 +3,7 @@ import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
 
 export default Component.extend({
+  tagName: '',
   dom: service('dom'),
 
   didInsertElement: function() {
