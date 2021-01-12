0712e03f337d56be60f71e7eadaf5c259170f28c
Chris Piraino <cpiraino@hashicorp.com>
2021-01-12 15:31:47 -0600

    Fix bug in usage metrics when multiple service instances are changed in a single transaction (#9440)
    * Fix bug in usage metrics that caused a negative count to occur
    
    There were a couple of instances were usage metrics would do the wrong
    thing and result in incorrect counts, causing the count to attempt to
    decrement below zero and return an error. The usage metrics did not
    account for various places where a single transaction could
    delete/update/add multiple service instances at once.
    
    We also remove the error when attempting to decrement below zero, and
    instead just make sure we do not accidentally underflow the unsigned
    integer. This is a more graceful failure than returning an error and not
    allowing a transaction to commit.
    
    * Add changelog

--- agent/consul/state/usage_oss.go
+++ agent/consul/state/usage_oss.go
@@ -3,12 +3,15 @@
 package state
 
 import (
+	"github.com/hashicorp/consul/agent/structs"
 	memdb "github.com/hashicorp/go-memdb"
 )
 
 type EnterpriseServiceUsage struct{}
 
-func addEnterpriseServiceUsage(map[string]int, memdb.Change, uniqueServiceState) {}
+func addEnterpriseServiceInstanceUsage(map[string]int, memdb.Change) {}
+
+func addEnterpriseServiceUsage(map[string]int, map[structs.ServiceName]uniqueServiceState) {}
 
 func compileEnterpriseUsage(tx ReadTxn, usage ServiceUsage) (ServiceUsage, error) {
 	return usage, nil
