db3400c22d445db6b5403d7d96ab6e41f72a35f9
Chris Piraino <cpiraino@hashicorp.com>
2021-01-12 15:31:47 -0600

    Fix bug in usage metrics when multiple service instances are changed in a single transaction (#9440)
    * Fix bug in usage metrics that caused a negative count to occur
    
    There were a couple of instances were usage metrics would do the wrong
    thing and result in incorrect counts, causing the count to attempt to
    decrement below zero and return an error. The usage metrics did not
    account for various places where a single transaction could
    delete/update/add multiple service instances at once.
    
    We also remove the error when attempting to decrement below zero, and
    instead just make sure we do not accidentally underflow the unsigned
    integer. This is a more graceful failure than returning an error and not
    allowing a transaction to commit.
    
    * Add changelog

--- agent/consul/state/usage_test.go
+++ agent/consul/state/usage_test.go
@@ -55,6 +55,43 @@ func TestStateStore_Usage_ServiceUsageEmpty(t *testing.T) {
 	require.Equal(t, usage.ServiceInstances, 0)
 }
 
+func TestStateStore_Usage_ServiceUsage_DeleteNode(t *testing.T) {
+	s := testStateStore(t)
+	testRegisterNode(t, s, 1, "node1")
+
+	svc1 := &structs.NodeService{
+		ID:      "service1",
+		Service: "test",
+		Address: "1.1.1.1",
+		Port:    1111,
+	}
+	svc2 := &structs.NodeService{
+		ID:      "service2",
+		Service: "test",
+		Address: "1.1.1.1",
+		Port:    1111,
+	}
+
+	// Register multiple instances on a single node to test that we do not
+	// double count deletions within the same transaction.
+	require.NoError(t, s.EnsureService(1, "node1", svc1))
+	require.NoError(t, s.EnsureService(2, "node1", svc2))
+
+	idx, usage, err := s.ServiceUsage()
+	require.NoError(t, err)
+	require.Equal(t, idx, uint64(2))
+	require.Equal(t, usage.Services, 1)
+	require.Equal(t, usage.ServiceInstances, 2)
+
+	require.NoError(t, s.DeleteNode(3, "node1"))
+
+	idx, usage, err = s.ServiceUsage()
+	require.NoError(t, err)
+	require.Equal(t, idx, uint64(3))
+	require.Equal(t, usage.Services, 0)
+	require.Equal(t, usage.ServiceInstances, 0)
+}
+
 func TestStateStore_Usage_Restore(t *testing.T) {
 	s := testStateStore(t)
 	restore := s.Restore()
@@ -67,12 +104,27 @@ func TestStateStore_Usage_Restore(t *testing.T) {
 			Address: "198.18.0.2",
 		},
 	})
+	restore.Registration(9, &structs.RegisterRequest{
+		Node: "test-node",
+		Service: &structs.NodeService{
+			ID:      "mysql1",
+			Service: "mysql",
+			Port:    8081,
+			Address: "198.18.0.2",
+		},
+	})
 	require.NoError(t, restore.Commit())
 
 	idx, count, err := s.NodeCount()
 	require.NoError(t, err)
 	require.Equal(t, idx, uint64(9))
 	require.Equal(t, count, 1)
+
+	idx, usage, err := s.ServiceUsage()
+	require.NoError(t, err)
+	require.Equal(t, idx, uint64(9))
+	require.Equal(t, usage.Services, 1)
+	require.Equal(t, usage.ServiceInstances, 2)
 }
 
 func TestStateStore_Usage_updateUsage_Underflow(t *testing.T) {
@@ -92,8 +144,12 @@ func TestStateStore_Usage_updateUsage_Underflow(t *testing.T) {
 	}
 
 	err := updateUsage(txn, changes)
-	require.Error(t, err)
-	require.Contains(t, err.Error(), "negative count")
+	require.NoError(t, err)
+
+	// Check that we do not underflow
+	u, err := txn.First("usage", "id", "nodes")
+	require.NoError(t, err)
+	require.Equal(t, 0, u.(*UsageEntry).Count)
 
 	// A insert a change to create a usage entry
 	changes = Changes{
@@ -128,8 +184,12 @@ func TestStateStore_Usage_updateUsage_Underflow(t *testing.T) {
 	}
 
 	err = updateUsage(txn, changes)
-	require.Error(t, err)
-	require.Contains(t, err.Error(), "negative count")
+	require.NoError(t, err)
+
+	// Check that we do not underflow
+	u, err = txn.First("usage", "id", "nodes")
+	require.NoError(t, err)
+	require.Equal(t, 0, u.(*UsageEntry).Count)
 }
 
 func TestStateStore_Usage_ServiceUsage_updatingServiceName(t *testing.T) {
