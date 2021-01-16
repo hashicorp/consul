b8ef48dfeb69b9517cf0106e76880c2dc76434ef
Ray Elder <ray@hashicorp.com>
2021-01-15 16:55:29 -0800

    Add close svg to Icon component

--- website/components/icon/index.jsx
+++ website/components/icon/index.jsx
@@ -1,12 +1,16 @@
 import svgDownload from './icons/download.svg?include'
 import svgVideo from './icons/video.svg?include'
 import svgPlay from './icons/play.svg?include'
+import svgClose from './icons/close.svg?include'
+import svgCloseRefined from './icons/close-refined.svg?include'
 import style from './icon.module.css'
 
 const iconDict = {
   download: svgDownload,
   video: svgVideo,
   play: svgPlay,
+  close: svgClose,
+  closeRefined: svgCloseRefined,
 }
 
 function Icon({ icon }) {
