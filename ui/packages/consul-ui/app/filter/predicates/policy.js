a8d9b9a8b916dff123cb924a3c42e6e321d4d2e3
John Cowen <johncowen@users.noreply.github.com>
2021-01-19 15:52:39 +0000

    ui: Add filtered by in Nspaces, Legacy ACLs, Tokens, Roles and Policies (#9531)

--- ui/packages/consul-ui/app/filter/predicates/policy.js
+++ ui/packages/consul-ui/app/filter/predicates/policy.js
@@ -1,11 +1,11 @@
 import setHelpers from 'mnemonist/set';
 
 export default {
-  kinds: {
+  kind: {
     'global-management': (item, value) => item.isGlobalManagement,
     standard: (item, value) => !item.isGlobalManagement,
   },
-  dcs: (item, values) => {
+  datacenter: (item, values) => {
     return (
       typeof item.Datacenters === 'undefined' ||
       setHelpers.intersectionSize(values, new Set(item.Datacenters)) > 0
