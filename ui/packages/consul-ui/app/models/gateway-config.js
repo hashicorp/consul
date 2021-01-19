d5afaab699c0548277656cc527872b9bb68df59e
John Cowen <jcowen@hashicorp.com>
2021-01-19 15:34:14 +0000

    Lint plus defaultValue = []

--- ui/packages/consul-ui/app/models/gateway-config.js
+++ ui/packages/consul-ui/app/models/gateway-config.js
@@ -1,5 +1,5 @@
 import Fragment from 'ember-data-model-fragments/fragment';
-import { fragmentArray, array } from 'ember-data-model-fragments/attributes';
+import { array } from 'ember-data-model-fragments/attributes';
 import { attr } from '@ember-data/model';
 
 export default class GatewayConfig extends Fragment {
@@ -8,5 +8,5 @@ export default class GatewayConfig extends Fragment {
   @attr('number', { defaultValue: () => 0 }) AssociatedServiceCount;
   // Addresses is only populated when asking for a list of services for a
   // specific gateway
-  @array('string') Addresses;
+  @array('string', { defaultValue: () => [] }) Addresses;
 }
