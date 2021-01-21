7d8ff3606c9e52cccb232231a830b8a7b68635d1
Zachary Shilton <4624598+zchsh@users.noreply.github.com>
2021-01-21 13:15:42 -0500

    Fix link color on home page (#9611)
    * Fix learn-callout theming
    
    * Fix link color in cloud-offerings-list

--- website/components/cloud-offerings-list/index.jsx
+++ website/components/cloud-offerings-list/index.jsx
@@ -3,21 +3,21 @@ import Button from '@hashicorp/react-button'
 export default function CloudOfferingsList({ offerings }) {
   return (
     <ul className="g-cloud-offerings-list">
-      {offerings.map(offering => (
+      {offerings.map((offering) => (
         <li key={offering.title}>
           <a
             href={offering.link.url}
             rel={offering.link.type === 'outbound' ? 'noopener' : undefined}
             target={offering.link.type === 'outbound' ? '_blank' : undefined}
           >
-            <img src={offering.image} alt={offering.title}/>
+            <img src={offering.image} alt={offering.title} />
             <span className="g-type-label-strong">{offering.eyebrow}</span>
             <h4>{offering.title}</h4>
             <p>{offering.description}</p>
             <Button
               title={offering.link.text}
               linkType={offering.link.type}
-              theme={{ variant: 'tertiary' }}
+              theme={{ variant: 'tertiary', brand: 'consul' }}
               url={offering.link.url}
             />
           </a>
