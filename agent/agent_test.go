f25a21960e87458952d81f3f5fedbdce1f15769a
R.B. Boyer <rb@hashicorp.com>
2021-01-25 17:30:38 -0600

    server: initialize mgw-wanfed to use local gateways more on startup (#9528)
    Fixes #9342

--- agent/agent_test.go
+++ agent/agent_test.go
@@ -4374,6 +4374,7 @@ func TestAgent_JoinWAN_viaMeshGateway(t *testing.T) {
 		}
 		# wanfed
 		primary_gateways = ["` + gwAddr + `"]
+		retry_interval_wan = "250ms"
 		connect {
 			enabled = true
 			enable_mesh_gateway_wan_federation = true
@@ -4399,6 +4400,7 @@ func TestAgent_JoinWAN_viaMeshGateway(t *testing.T) {
 		}
 		# wanfed
 		primary_gateways = ["` + gwAddr + `"]
+		retry_interval_wan = "250ms"
 		connect {
 			enabled = true
 			enable_mesh_gateway_wan_federation = true
