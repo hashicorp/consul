64a1d74c8b4a4c5f720931ffeb3afa72f13d73c2
John Cowen <johncowen@users.noreply.github.com>
2021-01-25 09:22:06 +0000

    ui: Fix filter by removal and provide more tests docs around it (#9608)
    Previously when removing a special 'searchproperty' filter, it would
    remove all the search property filters instead of only the one you had
    clicked.
    
    This fixes that up, adds tests and adds copious amounts of documentation
    around the filter data reshaping function for future self/somebody

---
+++ ui/packages/consul-ui/app/components/search-bar/utils.js
@@ -0,0 +1,38 @@
+export const diff = (a, b) => {
+  return a.filter(item => !b.includes(item));
+};
+/**
+ * filters accepts the args.filter @attribute which is shaped like
+ * {filterName: {default: ['Node', 'Address'], value: ['Address']}, ...}
+ * It will turn this into an array of 'filters' shaped like
+ * [{key: 'filterName', value: 'Address', selected: ["Node"]}]
+ * importantly 'selected' isn't what is currently 'selected' it is what selected
+ * will be once you remove this filter
+ * There is more explanation in the unit tests for this function so thats worthwhile
+ * checking if you are in amongst this
+ */
+export const filters = filters => {
+  return Object.entries(filters)
+    .filter(([key, value]) => {
+      if (key === 'searchproperty') {
+        return diff(value.default, value.value).length > 0;
+      }
+      return (value.value || []).length > 0;
+    })
+    .reduce((prev, [key, value]) => {
+      return prev.concat(
+        value.value.map(item => {
+          const obj = {
+            key: key,
+            value: item,
+          };
+          if (key !== 'searchproperty') {
+            obj.selected = diff(value.value, [item]);
+          } else {
+            obj.selected = value.value.length === 1 ? value.default : diff(value.value, [item]);
+          }
+          return obj;
+        })
+      );
+    }, []);
+};
