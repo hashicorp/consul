9718113f7dbb1fa7e26ac8ec43b5a427772fc4cf
John Cowen <jcowen@hashicorp.com>
2021-02-02 11:58:00 +0000

    Move <HashicorpConsul /> to use can helper

--- ui/packages/consul-ui/app/components/hashicorp-consul/index.js
+++ ui/packages/consul-ui/app/components/hashicorp-consul/index.js
@@ -2,16 +2,6 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
 export default class HashiCorpConsul extends Component {
-  // TODO: Right now this is the only place where we need permissions
-  // but we are likely to need it elsewhere, so probably need a nice helper
-  get canManageNspaces() {
-    return (
-      typeof (this.args.permissions || []).find(function(item) {
-        return item.Resource === 'operator' && item.Access === 'write' && item.Allow;
-      }) !== 'undefined'
-    );
-  }
-
   @action
   open() {
     this.authForm.focus();
