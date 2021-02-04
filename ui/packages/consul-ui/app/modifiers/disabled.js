f0cc8cb938ed0aa0d24750c06dd1446b6d7314fa
John Cowen <jcowen@hashicorp.com>
2021-02-04 17:07:06 +0000

    Adds a {{disabled}} modifier that works similar to fieldset
    In that you can set it to false on a parent node and all its sub nodes
    will be disabled. It has an added improvement that you can then separate
    allow children if you want to disable everything apart from one thing.
    
    One caveat here is that it relies on dom twiddling and is therefore
    disabling a parent is not currently very reactive and will need some
    MutationObserver work to make it fully reactive.

---
+++ ui/packages/consul-ui/app/modifiers/disabled.js
@@ -0,0 +1,17 @@
+import { modifier } from 'ember-modifier';
+
+export default modifier(function enabled($element, [bool], hash) {
+  if (['input', 'textarea', 'select', 'button'].includes($element.nodeName.toLowerCase())) {
+    if (bool) {
+      $element.disabled = bool;
+    } else {
+      $element.dataset.disabled = false;
+    }
+    return;
+  }
+  for (const $el of $element.querySelectorAll('input,textarea')) {
+    if ($el.dataset.disabled !== 'false') {
+      $el.disabled = bool;
+    }
+  }
+});
