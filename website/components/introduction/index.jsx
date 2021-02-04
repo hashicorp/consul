953af1a8721e65526b582716c16c0dfbcc250771
Ray Elder <ray@hashicorp.com>
2021-02-04 12:22:07 -0800

    Revert "Add speaker props to Introduction component"
    This reverts commit 0282747c3df8ddc73cd4d815f23bb473fab24b91.

--- website/components/introduction/index.jsx
+++ website/components/introduction/index.jsx
@@ -4,7 +4,7 @@ import Icon from '../icon'
 import style from './introduction.module.css'
 import Modal from '../modal'
 
-function Introduction({ brand, description, speakerName, speakerTitle }) {
+function Introduction({ brand, description }) {
   const [isModalShow, setIsModalShow] = useState(false)
 
   return (
@@ -25,10 +25,12 @@ function Introduction({ brand, description, speakerName, speakerTitle }) {
         </button>
         <div className={style.content}>
           <p className="g-type-display-5 mt-zero mb-zero">
-            Introduction to HashiCorp {brand}
+            Introduction to HashiCorp Consul
           </p>
-          <p className="mt-zero mb-zero">{speakerName}</p>
-          <p className="g-type-label mt-zero mb-zero">{speakerTitle}</p>
+          <p className="mt-zero mb-zero">Armon Dadgar</p>
+          <p className="g-type-label mt-zero mb-zero">
+            HashiCorp CTO and Co-founder
+          </p>
         </div>
       </div>
     </div>
