369637aff7ae296aaef766e4f3f9b328d9a2555f
John Cowen <johncowen@users.noreply.github.com>
2021-02-11 10:03:13 +0000

    ui: Update browser targets to ~2016 browsers (#9729)
    * ui: Reduce range of browsers in targets.js `'Chrome 55', 'Firefox 53', 'Safari 13', 'Edge 15'`
    
    * ui: Remove auto-prefixer

--- ui/packages/consul-ui/config/targets.js
+++ ui/packages/consul-ui/config/targets.js
@@ -1,14 +1,13 @@
 'use strict';
-
-const browsers = ['last 1 Chrome versions', 'last 1 Firefox versions', 'last 1 Safari versions'];
-
-const isCI = !!process.env.CI;
-const isProduction = process.env.EMBER_ENV === 'production';
-
-if (isCI || isProduction) {
-  browsers.push('ie 11');
-}
-
+// async/await support came with the below specified versions for Chrome,
+// Firefox and Edge. Async/await is is the newest ES6 feature we are not
+// transpiling. Safari's template literal support is a little problematic during
+// v12 in that it has a GC bug for tagged template literals. We don't currently
+// rely on this functionality so the bug wouldn't effect us, but in order to use
+// browser versions as a measure for ES6 features we need to specify Safari 13
+// for native, non-transpiled template literals. In reality template literals
+// came in Safari 9.1. Safari's async/await support came in Safari 10, so thats
+// the earliest Safari we cover in reality here.
 module.exports = {
-  browsers,
+  browsers: ['Chrome 55', 'Firefox 53', 'Safari 13', 'Edge 15'],
 };
