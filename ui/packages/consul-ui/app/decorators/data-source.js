3566e757a466b18f84b368dece9fd9cacd21e5bc
John Cowen <jcowen@hashicorp.com>
2021-02-15 17:04:49 +0000

    Use container to lookup instances w/class passed from decorator

--- ui/packages/consul-ui/app/decorators/data-source.js
+++ ui/packages/consul-ui/app/decorators/data-source.js
@@ -1,16 +1,15 @@
+import { runInDebug } from '@ember/debug';
 import wayfarer from 'wayfarer';
-import { singularize } from 'ember-inflector';
 
 const router = wayfarer();
+const routes = {};
 export default path => (target, propertyKey, desc) => {
+  runInDebug(() => {
+    routes[path] = { cls: target, method: propertyKey };
+  });
   router.on(path, function(params, owner) {
-    let name;
-    if (params.modelName) {
-      name = singularize(params.modelName);
-    } else {
-      name = target.getModelName.call();
-    }
-    const instance = owner.lookup(`service:repository/${name}`);
+    const container = owner.lookup('service:container');
+    const instance = container.get(target);
     return configuration => desc.value.apply(instance, [params, configuration]);
   });
   return desc;
@@ -18,3 +17,36 @@ export default path => (target, propertyKey, desc) => {
 export const match = path => {
   return router.match(path);
 };
+
+runInDebug(() => {
+  window.DataSourceRoutes = () => {
+    // debug time only way to access the application and be able to lookup
+    // services, don't use ConsulUi global elsewhere!
+    const container = window.ConsulUi.__container__.lookup('service:container');
+    const win = window.open('', '_blank');
+    win.document.write(`
+<body>
+  <pre>
+${Object.entries(routes)
+  .map(([key, value]) => {
+    let cls = container
+      .keyForClass(value.cls)
+      .split('/')
+      .pop();
+    cls = cls
+      .split('-')
+      .map(item => `${item[0].toUpperCase()}${item.substr(1)}`)
+      .join('');
+    return `${key}
+      ${cls}Repository.${value.method}(params)
+
+`;
+  })
+  .join('')}
+  </pre>
+</body>
+    `);
+    win.focus();
+    return;
+  };
+});
