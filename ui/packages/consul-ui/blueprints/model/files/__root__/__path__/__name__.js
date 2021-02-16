3ce386db00ba37e78e1d36f12517a9cb9c28c2df
John Cowen <jcowen@hashicorp.com>
2021-02-16 13:34:01 +0000

    ui: Update project blueprints for native classes

--- ui/packages/consul-ui/blueprints/model/files/__root__/__path__/__name__.js
+++ ui/packages/consul-ui/blueprints/model/files/__root__/__path__/__name__.js
@@ -1,10 +1,18 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
+//import { nullValue } from 'consul-ui/decorators/replace';
 
 export const PRIMARY_KEY = 'uid';
 export const SLUG_KEY = 'ID';
-export default Model.extend({
-  [PRIMARY_KEY]: attr('string'),
-  [SLUG_KEY]: attr('string'),
-  Datacenter: attr('string'),
-});
+export default class <%= classifiedModuleName %>Model extends Model {
+  @attr('string') uid;
+  @attr('string') ID;
+  @attr('string') Datacenter;
+
+  // @attr('string') Namespace; // Does this Model support namespaces?
+
+  // @nullValue([]) @attr({ defaultValue: () => [] }) MaybeNull; // Does a property sometimes return null?
+
+  // @attr('number') SyncTime; // Does this Model support blocking queries?
+  // @attr() meta; // {} // Does this Model support blocking queries?
+};
