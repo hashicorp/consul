139a8e61f0676a58875c8bd7a089c4273202f204
Lorna Song <lorna@hashicorp.com>
2021-02-23 18:31:13 -0500

    docs/nia: reorganize cli page into multiple pages
    Rename existing cli page as 'overview' and move it into a cli directory

--- website/data/docs-navigation.js
+++ website/data/docs-navigation.js
@@ -223,7 +223,11 @@ export default [
       },
       'architecture',
       'api',
-      'cli',
+      {
+        category: 'cli',
+        name: 'CLI',
+        content: ['cli-overview'],
+      },
       'configuration',
       'tasks',
       'network-drivers',
