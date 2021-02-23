39758178039c4fbb73e0fb4d1f9cc1e9d383448f
John Cowen <jcowen@hashicorp.com>
2021-02-23 12:52:00 +0000

    Readd setup controller

--- ui/packages/consul-ui/app/routes/dc/services/show/intentions.js
+++ ui/packages/consul-ui/app/routes/dc/services/show/intentions.js
@@ -1,4 +1,9 @@
 import Route from 'consul-ui/routing/route';
 import { inject as service } from '@ember/service';
 
-export default class IntentionsRoute extends Route {}
+export default class IntentionsRoute extends Route {
+  setupController(controller, model) {
+    super.setupController(...arguments);
+    controller.setProperties(model);
+  }
+}
