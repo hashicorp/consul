3e25bc9ff9910c21be5aa560dda35f1c0a80b72d
Lorna Song <lorna@hashicorp.com>
2021-02-24 11:58:03 -0500

    docs/nia: add enable/disable subcommand docs

--- website/data/docs-navigation.js
+++ website/data/docs-navigation.js
@@ -226,7 +226,7 @@ export default [
       {
         category: 'cli',
         name: 'CLI',
-        content: ['cli-overview'],
+        content: ['cli-overview', 'task'],
       },
       'configuration',
       'tasks',
