682f35718563806b4ea6611bd2bd42145c70d2f6
freddygv <gh@freddygv.xyz>
2021-03-11 16:26:55 -0700

    Fixup more tests

--- agent/agent_endpoint_test.go
+++ agent/agent_endpoint_test.go
@@ -397,7 +397,7 @@ func TestAgent_Service(t *testing.T) {
 		Service:     "web-sidecar-proxy",
 		Port:        8000,
 		Proxy:       expectProxy.ToAPI(),
-		ContentHash: "4c7d5f8d3748be6d",
+		ContentHash: "9d9b74b6df3fccd9",
 		Weights: api.AgentWeights{
 			Passing: 1,
 			Warning: 1,
@@ -411,7 +411,7 @@ func TestAgent_Service(t *testing.T) {
 	// Copy and modify
 	updatedResponse := *expectedResponse
 	updatedResponse.Port = 9999
-	updatedResponse.ContentHash = "713435ba1f5badcf"
+	updatedResponse.ContentHash = "a0d21e81f72cdf7b"
 
 	// Simple response for non-proxy service registered in TestAgent config
 	expectWebResponse := &api.AgentService{
