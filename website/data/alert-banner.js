db1293feeae1d53cf888a3d26fa6f2586f95d6d0
Mike Wickett <mike@wickett.ca>
2021-03-10 20:58:10 -0500

    Update alert banner

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -2,12 +2,12 @@ export const ALERT_BANNER_ACTIVE = true
 
 // https://github.com/hashicorp/web-components/tree/master/packages/alert-banner
 export default {
-  tag: 'Announcement',
+  tag: 'New',
   url:
     'https://cloud.hashicorp.com/?utm_source=consul_io&utm_content=alert_banner',
-  text: 'HashiCorp Consul is now generally available on HCP',
-  linkText: 'Learn More',
+  text: 'HCP Consul is now generally available on AWS',
+  linkText: 'Try today',
   // Set the `expirationDate prop with a datetime string (e.g. `2020-01-31T12:00:00-07:00`)
   // if you'd like the component to stop showing at or after a certain date
-  expirationDate: '2021-02-14T11:59:00-05:00',
+  expirationDate: '2021-04-14T11:59:00-05:00',
 }
