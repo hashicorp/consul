4a88ad24bfdeff7f8db417215f429979ac485b73
kenia <keniavalladarez@gmail.com>
2021-03-17 10:31:22 -0400

    Fixups

--- ui/packages/consul-ui/app/components/certificate/index.js
+++ ui/packages/consul-ui/app/components/certificate/index.js
@@ -2,9 +2,9 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
-export default class TopologyMetrics extends Component {
+export default class Certificate extends Component {
   // =attributes
-  @tracked show=false;
+  @tracked show = false;
 
   // =actions
   @action
