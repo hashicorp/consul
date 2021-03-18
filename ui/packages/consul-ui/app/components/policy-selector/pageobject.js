e1b8835578c9edd23139f821aa877d283dc34d75
John Cowen <jcowen@hashicorp.com>
2021-03-18 11:31:00 +0000

    Switch out old toggle names for ids

--- ui/packages/consul-ui/app/components/policy-selector/pageobject.js
+++ ui/packages/consul-ui/app/components/policy-selector/pageobject.js
@@ -5,7 +5,7 @@ export default (clickable, deletable, collection, alias, policyForm) => (
   return {
     scope: scope,
     create: clickable(createSelector),
-    form: policyForm('#new-policy-toggle + div'),
+    form: policyForm('#new-policy'),
     policies: alias('selectedOptions'),
     selectedOptions: collection(
       '[data-test-policies] [data-test-tabular-row]',
