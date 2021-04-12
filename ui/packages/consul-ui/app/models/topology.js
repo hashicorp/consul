f5849fd5002d0c6bbf72df58aef49abab4ae612d
kenia <keniavalladarez@gmail.com>
2021-04-12 09:19:49 -0400

    Create not explicitly defined intentions banner

--- ui/packages/consul-ui/app/models/topology.js
+++ ui/packages/consul-ui/app/models/topology.js
@@ -1,4 +1,5 @@
 import Model, { attr } from '@ember-data/model';
+import { computed } from '@ember/object';
 
 export const PRIMARY_KEY = 'uid';
 export const SLUG_KEY = 'ServiceName';
@@ -17,4 +18,24 @@ export default class Topology extends Model {
   @attr() Upstreams; // Service[]
   @attr() Downstreams; // Service[],
   @attr() meta; // {}
+
+  @computed('Upstreams', 'Downstreams')
+  get undefinedIntention() {
+    let undefinedUpstream = false;
+    let undefinedDownstream = false;
+
+    undefinedUpstream =
+      this.Upstreams.filter(
+        item =>
+          item.Source === 'specific-intention' && !item.TransparentProxy && item.Intention.Allowed
+      ).length !== 0;
+
+    undefinedDownstream =
+      this.Downstreams.filter(
+        item =>
+          item.Source === 'specific-intention' && !item.TransparentProxy && item.Intention.Allowed
+      ).length !== 0;
+
+    return undefinedUpstream || undefinedDownstream;
+  }
 }
