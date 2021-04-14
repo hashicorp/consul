451e68241f8fa5461765ed5430c5a8468caf0ad3
Jeff Escalante <jescalan@users.noreply.github.com>
2021-04-14 18:31:09 -0400

    update deps, add new downloads page

--- website/pages/downloads/index.jsx
+++ website/pages/downloads/index.jsx
@@ -1,64 +1,91 @@
-import VERSION from '../../data/version.js'
-import ProductDownloader from '@hashicorp/react-product-downloader'
-import Head from 'next/head'
-import HashiHead from '@hashicorp/react-head'
+import VERSION from 'data/version'
+import { productSlug } from 'data/metadata'
+import ProductDownloadsPage from '@hashicorp/react-product-downloads-page'
+import { generateStaticProps } from '@hashicorp/react-product-downloads-page/server'
+import s from './style.module.css'
 
-export default function DownloadsPage({ releaseData }) {
+export default function DownloadsPage(staticProps) {
   return (
-    <div id="p-downloads" className="g-container">
-      <HashiHead is={Head} title="Downloads | Consul by HashiCorp" />
-      <ProductDownloader
-        product="Consul"
-        version={VERSION}
-        releaseData={releaseData}
-        prerelease={{
-        type: 'beta', // the type of prerelease: beta, release candidate, etc.
-        name: 'v1.10.0', // the name displayed in text on the website
-        version: '1.10.0-beta1', // the actual version tag that was pushed to releases.hashicorp.com
+    <ProductDownloadsPage
+      getStartedDescription="Follow step-by-step tutorials on the essentials of Consul."
+      getStartedLinks={[
+        {
+          label: 'CLI Quickstart',
+          href:
+            'https://learn.hashicorp.com/collections/consul/getting-started',
+        },
+        {
+          label: 'HCP Consul',
+          href:
+            'https://learn.hashicorp.com/collections/consul/cloud-get-started',
+        },
+        {
+          label: 'HCS on Azure',
+          href: 'https://learn.hashicorp.com/collections/consul/hcs-azure',
+        },
+        {
+          label: 'Kubernetes Quickstart',
+          href:
+            'https: //learn.hashicorp.com/collections/consul/gs-consul-service-mesh',
+        },
+        {
+          label: 'View all Consul tutorials',
+          href: 'https://learn.hashicorp.com/consul',
+        },
+      ]}
+      logo={
+        <img
+          className={s.logo}
+          alt="Consul"
+          src={require('./img/consul-logo.svg')}
+        />
+      }
+      tutorialLink={{
+        href: 'https://learn.hashicorp.com/consul',
+        label: 'View Tutorials at HashiCorp Learn',
       }}
-      >
-        <p>
-          <a href="/docs/download-tools">&raquo; Download Consul Tools</a>
-        </p>
-        <div className="release-candidate">
-          <p>Note for ARM users:</p>
-
-          <ul>
-            <li>Use Armelv5 for all 32-bit armel systems</li>
-            <li>Use Armhfv6 for all armhf systems with v6+ architecture</li>
-            <li>Use Arm64 for all v8 64-bit architectures</li>
-          </ul>
-
+      merchandisingSlot={
+        <>
           <p>
-            The following commands can help determine the right version for your
-            system:
+            <a href="/docs/download-tools">&raquo; Download Consul Tools</a>
           </p>
+          <div className="release-candidate">
+            <p>Note for ARM users:</p>
 
-          <code>$ uname -m</code>
-          <br />
-          <code>
-            $ readelf -a /proc/self/exe | grep -q -c Tag_ABI_VFP_args && echo
-            &quot;armhf&quot; || echo &quot;armel&quot;
-          </code>
-        </div>
-      </ProductDownloader>
-    </div>
+            <ul>
+              <li>Use Armelv5 for all 32-bit armel systems</li>
+              <li>Use Armhfv6 for all armhf systems with v6+ architecture</li>
+              <li>Use Arm64 for all v8 64-bit architectures</li>
+            </ul>
+
+            <p>
+              The following commands can help determine the right version for
+              your system:
+            </p>
+
+            <code>$ uname -m</code>
+            <br />
+            <code>
+              $ readelf -a /proc/self/exe | grep -q -c Tag_ABI_VFP_args && echo
+              &quot;armhf&quot; || echo &quot;armel&quot;
+            </code>
+          </div>
+          <p>
+            A beta for consul v1.10.0 is available! The release can be
+            <a href="https://releases.hashicorp.com/consul/1.10.0-beta1/">
+              downloaded here
+            </a>
+          </p>
+        </>
+      }
+      {...staticProps}
+    />
   )
 }
 
 export async function getStaticProps() {
-  return fetch(`https://releases.hashicorp.com/consul/${VERSION}/index.json`)
-    .then((res) => res.json())
-    .then((releaseData) => ({ props: { releaseData } }))
-    .catch(() => {
-      throw new Error(
-        `--------------------------------------------------------
-        Unable to resolve version ${VERSION} on releases.hashicorp.com from link
-        <https://releases.hashicorp.com/consul/${VERSION}/index.json>. Usually this
-        means that the specified version has not yet been released. The downloads page
-        version can only be updated after the new version has been released, to ensure
-        that it works for all users.
-        ----------------------------------------------------------`
-      )
-    })
+  return generateStaticProps({
+    product: productSlug,
+    latestVersion: VERSION,
+  })
 }
