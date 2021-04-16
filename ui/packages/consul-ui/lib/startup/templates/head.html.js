d008dc3d859400bd0bd3d33cdc6acb1298adcf76
John Cowen <johncowen@users.noreply.github.com>
2021-04-16 12:13:06 +0100

    ui: Hoist DC menu into the top navigation bar (#10034)
    * Add story for %main-nav-vertical plus additions for hoisting menu items
    
    * Make sure we don't source app.css twice
    
    * Hoist the DC menu

--- ui/packages/consul-ui/lib/startup/templates/head.html.js
+++ ui/packages/consul-ui/lib/startup/templates/head.html.js
@@ -7,10 +7,9 @@ module.exports = ({ appName, environment, rootURL, config }) => `
   <link rel="icon" type="image/png" href="${rootURL}assets/favicon-32x32.png" sizes="32x32">
   <link rel="icon" type="image/png" href="${rootURL}assets/favicon-16x16.png" sizes="16x16">
   <link integrity="" rel="stylesheet" href="${rootURL}assets/vendor.css">
-  <link integrity="" rel="stylesheet" href="${rootURL}assets/${appName}.css">
-  ${
-    environment === 'development' ? `<link rel="stylesheet" href="${rootURL}assets/debug.css">` : ``
-  }
+  <link integrity="" rel="stylesheet" href="${rootURL}assets/${
+  environment === 'development' ? 'debug' : appName
+}.css">
   ${
     environment === 'test' ? `<link rel="stylesheet" href="${rootURL}assets/test-support.css">` : ``
   }
