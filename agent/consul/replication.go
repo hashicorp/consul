835d3d4258bf119f4c3b95ae0e3e9db49615ae4f
Matt Keeler <mkeeler@users.noreply.github.com>
2021-04-22 11:20:53 -0400

    Add replication metrics (#10073)
    # Conflicts:
    #	agent/consul/replication.go

--- agent/consul/replication.go
+++ agent/consul/replication.go
@@ -23,6 +23,7 @@ const (
 
 type ReplicatorDelegate interface {
 	Replicate(ctx context.Context, lastRemoteIndex uint64, logger hclog.Logger) (index uint64, exit bool, err error)
+	MetricName() string
 }
 
 type ReplicatorConfig struct {
@@ -104,6 +105,9 @@ func (r *Replicator) Run(ctx context.Context) error {
 		}
 
 		if err != nil {
+			metrics.SetGauge([]string{"leader", "replication", r.delegate.MetricName(), "status"},
+				0,
+			)
 			// reset the lastRemoteIndex when there is an RPC failure. This should cause a full sync to be done during
 			// the next round of replication
 			atomic.StoreUint64(&r.lastRemoteIndex, 0)
@@ -112,6 +116,12 @@ func (r *Replicator) Run(ctx context.Context) error {
 				r.logger.Warn("replication error (will retry if still leader)", "error", err)
 			}
 		} else {
+			metrics.SetGauge([]string{"leader", "replication", r.delegate.MetricName(), "status"},
+				1,
+			)
+			metrics.SetGauge([]string{"leader", "replication", r.delegate.MetricName(), "index"},
+				float32(index),
+			)
 			atomic.StoreUint64(&r.lastRemoteIndex, index)
 			r.logger.Debug("replication completed through remote index", "index", index)
 		}
@@ -134,6 +144,11 @@ type ReplicatorFunc func(ctx context.Context, lastRemoteIndex uint64, logger hcl
 
 type FunctionReplicator struct {
 	ReplicateFn ReplicatorFunc
+	Name        string
+}
+
+func (r *FunctionReplicator) MetricName() string {
+	return r.Name
 }
 
 func (r *FunctionReplicator) Replicate(ctx context.Context, lastRemoteIndex uint64, logger hclog.Logger) (uint64, bool, error) {
@@ -175,6 +190,10 @@ type IndexReplicatorDelegate interface {
 type IndexReplicator struct {
 	Delegate IndexReplicatorDelegate
 	Logger   hclog.Logger
+}
+
+func (r *IndexReplicator) MetricName() string {
+	return r.Delegate.MetricName()
 }
 
 func (r *IndexReplicator) Replicate(ctx context.Context, lastRemoteIndex uint64, _ hclog.Logger) (uint64, bool, error) {
