aa2099d12ad4678876a2cc91f2aa42785256d339
Matt Keeler <mkeeler@users.noreply.github.com>
2021-04-23 17:05:33 -0400

    Add prometheus guage definitions for replication metrics. (#10109)

--- agent/setup.go
+++ agent/setup.go
@@ -200,6 +200,7 @@ func getPrometheusDefs(cfg lib.TelemetryConfig) ([]prometheus.GaugeDefinition, [
 		consul.SessionGauges,
 		grpc.StatsGauges,
 		usagemetrics.Gauges,
+		consul.ReplicationGauges,
 		Gauges,
 	}
 	// Flatten definitions
