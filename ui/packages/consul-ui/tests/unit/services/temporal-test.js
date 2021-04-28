345765f7663c65afd610e7996a3d5cfa612eabb3
John Cowen <johncowen@users.noreply.github.com>
2021-04-28 12:12:56 +0100

    ui: Adds human formatting to nanosecond based session durations (#10062)
    * ui: Adds human formatting to nanosecond based session durations

---
+++ ui/packages/consul-ui/tests/unit/services/temporal-test.js
@@ -0,0 +1,12 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Service | temporal', function(hooks) {
+  setupTest(hooks);
+
+  // TODO: Replace this with your real tests.
+  test('it exists', function(assert) {
+    let service = this.owner.lookup('service:temporal');
+    assert.ok(service);
+  });
+});
