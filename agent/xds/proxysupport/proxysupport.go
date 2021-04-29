55600be4a9d8e92b7e0ee9bf2aad5d28cebc5f5e
R.B. Boyer <4903+rboyer@users.noreply.github.com>
2021-04-29 15:56:24 -0500

    [1.8.x] connect: update supported envoy versions to 1.14.7, 1.13.7, 1.12.7, 1.11.2 (#10106)

--- agent/xds/proxysupport/proxysupport.go
+++ agent/xds/proxysupport/proxysupport.go
@@ -7,7 +7,7 @@ package proxysupport
 //
 // see: https://www.consul.io/docs/connect/proxies/envoy#supported-versions
 var EnvoyVersions = []string{
-	"1.14.6",
+	"1.14.7",
 	"1.13.7",
 	"1.12.7",
 	"1.11.2",
