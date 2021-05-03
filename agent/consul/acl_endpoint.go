6c0c15154bacd2463437bcd6f007d87bf262f0de
Luke Kysow <1034429+lkysow@users.noreply.github.com>
2021-05-03 11:24:55 -0700

    Update error to be 403

--- agent/consul/acl_endpoint.go
+++ agent/consul/acl_endpoint.go
@@ -2380,7 +2380,7 @@ func (a *ACL) Login(args *structs.ACLLoginRequest, reply *structs.ACLToken) erro
 	if err != nil {
 		return err
 	} else if method == nil {
-		return fmt.Errorf("auth method %q not found", auth.AuthMethod)
+		return fmt.Errorf("%w: auth method %q not found", acl.ErrNotFound, auth.AuthMethod)
 	}
 
 	if err := a.enterpriseAuthMethodTypeValidation(method.Type); err != nil {
