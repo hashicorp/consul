c79bd8d7a4097887cac7e926fa9d0b1c37fd3aad
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2021-05-04 20:00:48 -0400

    website: replace hidden pages hot fix for /docs, /api-docs, and /commmands

--- website/pages/docs/[[...page]].jsx
+++ website/pages/docs/[[...page]].jsx
@@ -2,23 +2,15 @@ import { productName, productSlug } from 'data/metadata'
 import DocsPage from '@hashicorp/react-docs-page'
 import ConfigEntryReference from 'components/config-entry-reference'
 // Imports below are only used server-side
-/**
- * DEBT: short term patch for "hidden" docs-sidenav items.
- * See components/_temp-enable-hidden-pages for details.
- * Revert to importing from @hashicorp/react-docs-page/server
- * once https://app.asana.com/0/1100423001970639/1200197752405255/f
- * is complete.
- **/
 import {
   generateStaticPaths,
   generateStaticProps,
-} from 'components/_temp-enable-hidden-pages'
+} from '@hashicorp/react-docs-page/server'
 
 //  Configure the docs path
 const additionalComponents = { ConfigEntryReference }
 const baseRoute = 'docs'
 const navDataFile = `data/${baseRoute}-nav-data.json`
-const navDataFileHidden = `data/${baseRoute}-nav-data-hidden.json`
 const localContentDir = `content/${baseRoute}`
 const mainBranch = 'master'
 const product = { name: productName, slug: productSlug }
@@ -38,7 +30,6 @@ export async function getStaticPaths() {
   const paths = await generateStaticPaths({
     localContentDir,
     navDataFile,
-    navDataFileHidden,
   })
   return { paths, fallback: false }
 }
@@ -49,7 +40,6 @@ export async function getStaticProps({ params }) {
     localContentDir,
     mainBranch,
     navDataFile,
-    navDataFileHidden,
     params,
     product,
   })
