114fb73a29f1f62b7990e2e8ed2878c27e710618
Brandon Romano <brandon@hashicorp.com>
2021-05-17 15:23:10 -0700

    Merge pull request #10251 from hashicorp/br.hc-banner
    Updates AlertBanner for HashiConf EU

--- website/data/alert-banner.js
+++ website/data/alert-banner.js
@@ -2,12 +2,12 @@ export const ALERT_BANNER_ACTIVE = true
 
 // https://github.com/hashicorp/web-components/tree/master/packages/alert-banner
 export default {
-  tag: 'New',
-  url:
-    'https://cloud.hashicorp.com/?utm_source=consul_io&utm_content=alert_banner',
-  text: 'HCP Consul is now generally available on AWS',
-  linkText: 'Try today',
+  tag: 'June 8-11',
+  url: 'https://hashiconf.com/europe/?utm_source=DocsBanner',
+  text:
+    'The countdown to HashiConf Europe is on, and the full schedule is now live.',
+  linkText: 'View Schedule',
   // Set the `expirationDate prop with a datetime string (e.g. `2020-01-31T12:00:00-07:00`)
   // if you'd like the component to stop showing at or after a certain date
-  expirationDate: '2021-04-14T11:59:00-05:00',
+  expirationDate: null,
 }
