213524a6579bfe070015cc05970d82f335a94264
Rémi Lapeyre <remi.lapeyre@lenstra.fr>
2021-05-25 17:03:48 +0200

    Always set the Content-Type header when a body is present (#10204)
    * Always set the Content-Type header when a body is present
    
    Closes https://github.com/hashicorp/consul/issues/10011
    
    * Add Changelog entry
    
    * Add more Content-Type exceptions
    
    * Fix tests

--- api/api.go
+++ api/api.go
@@ -871,6 +871,12 @@ func (r *request) toHTTP() (*http.Request, error) {
 	req.Host = r.url.Host
 	req.Header = r.header
 
+	// Content-Type must always be set when a body is present
+	// See https://github.com/hashicorp/consul/issues/10011
+	if req.Body != nil && req.Header.Get("Content-Type") == "" {
+		req.Header.Set("Content-Type", "application/json")
+	}
+
 	// Setup auth
 	if r.config.HttpAuth != nil {
 		req.SetBasicAuth(r.config.HttpAuth.Username, r.config.HttpAuth.Password)
