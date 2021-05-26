0ddd0950a60e88a4b3715a3415bcf6c0fef68ec9
Dhia Ayachi <dhia@hashicorp.com>
2021-05-26 15:43:49 -0400

    fix IsErrEOF to return false when err is nil

--- lib/eof.go
+++ lib/eof.go
@@ -16,6 +16,9 @@ var yamuxSessionShutdown = yamux.ErrSessionShutdown.Error()
 // IsErrEOF returns true if we get an EOF error from the socket itself, or
 // an EOF equivalent error from yamux.
 func IsErrEOF(err error) bool {
+	if err == nil {
+		return false
+	}
 	if errors.Is(err, io.EOF) {
 		return true
 	}
