3a40bbf71bed8a548794a90f87bf14f3cc89d2f8
Jimmy Merritello <7191639+jmfury@users.noreply.github.com>
2021-05-28 09:58:15 -0500

    Iron out features section

--- website/pages/home/index.jsx
+++ website/pages/home/index.jsx
@@ -196,33 +196,38 @@ export default function HomePage() {
           }}
         />
       </div>
-      <SteppedFeatureList
-        features={[
-          {
-            title: 'Feature 1',
-            description:
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
-            learnMoreLink: '/',
-            content: (
-              <img
-                src="https://via.placeholder.com/600/000000/FFFFFF?text=Feature 1"
-                alt="Feature 1"
-              />
-            ),
-          },
-          {
-            title: 'Feature 2',
-            description:
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a vestibulum mi.',
-            content: (
-              <img
-                src="https://via.placeholder.com/600/000000/FFFFFF?text=Feature 2"
-                alt="Feature 2"
-              />
-            ),
-          },
-        ]}
-      />
+      <section className="features">
+        <div className="g-grid-container">
+          <h3 className="g-type-display-2">Features</h3>
+          <SteppedFeatureList
+            features={[
+              {
+                title: 'Feature 1',
+                description:
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+                learnMoreLink: '/',
+                content: (
+                  <img
+                    src="https://via.placeholder.com/600/000000/FFFFFF?text=Feature 1"
+                    alt="Feature 1"
+                  />
+                ),
+              },
+              {
+                title: 'Feature 2',
+                description:
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a vestibulum mi.',
+                content: (
+                  <img
+                    src="https://via.placeholder.com/600/000000/FFFFFF?text=Feature 2"
+                    alt="Feature 2"
+                  />
+                ),
+              },
+            ]}
+          />
+        </div>
+      </section>
       <CalloutBlade
         title="Consul with HashiCorp Stack"
         callouts={[
