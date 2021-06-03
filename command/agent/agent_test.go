a3a0bf61a239c23425cc8ddeb4b5c6b550412e19
Daniel Nephin <dnephin@hashicorp.com>
2021-06-03 12:16:45 -0400

    Merge pull request #10338 from hashicorp/dnephin/fix-logging-indent
    agent: remove leading whitespace from agent log lines

--- command/agent/agent_test.go
+++ command/agent/agent_test.go
@@ -2,18 +2,19 @@ package agent
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
 	"testing"
 
-	"github.com/hashicorp/consul/testrpc"
+	"github.com/mitchellh/cli"
 
 	"github.com/hashicorp/consul/agent"
 	"github.com/hashicorp/consul/sdk/testutil"
 	"github.com/hashicorp/consul/sdk/testutil/retry"
-	"github.com/mitchellh/cli"
+	"github.com/hashicorp/consul/testrpc"
 )
 
 // TestConfigFail should test command line flags that lead to an immediate error.
@@ -108,8 +109,8 @@ func TestRetryJoinFail(t *testing.T) {
 	t.Parallel()
 	tmpDir := testutil.TempDir(t, "consul")
 
-	ui := cli.NewMockUi()
-	cmd := New(ui, "", "", "", "", nil)
+	ui := newCaptureUI()
+	cmd := New(ui)
 
 	args := []string{
 		"-bind", "127.0.0.1",
@@ -128,8 +129,8 @@ func TestRetryJoinWanFail(t *testing.T) {
 	t.Parallel()
 	tmpDir := testutil.TempDir(t, "consul")
 
-	ui := cli.NewMockUi()
-	cmd := New(ui, "", "", "", "", nil)
+	ui := newCaptureUI()
+	cmd := New(ui)
 
 	args := []string{
 		"-server",
@@ -167,8 +168,8 @@ func TestProtectDataDir(t *testing.T) {
 		t.Fatalf("err: %v", err)
 	}
 
-	ui := cli.NewMockUi()
-	cmd := New(ui, "", "", "", "", nil)
+	ui := newCaptureUI()
+	cmd := New(ui)
 	args := []string{"-config-file=" + cfgFile.Name()}
 	if code := cmd.Run(args); code == 0 {
 		t.Fatalf("should fail")
@@ -186,8 +187,8 @@ func TestBadDataDirPermissions(t *testing.T) {
 		t.Fatalf("err: %v", err)
 	}
 
-	ui := cli.NewMockUi()
-	cmd := New(ui, "", "", "", "", nil)
+	ui := newCaptureUI()
+	cmd := New(ui)
 	args := []string{"-data-dir=" + dataDir, "-server=true", "-bind=10.0.0.1"}
 	if code := cmd.Run(args); code == 0 {
 		t.Fatalf("Should fail with bad data directory permissions")
@@ -195,4 +196,20 @@ func TestBadDataDirPermissions(t *testing.T) {
 	if out := ui.ErrorWriter.String(); !strings.Contains(out, "Permission denied") {
 		t.Fatalf("expected permission denied error, got: %s", out)
 	}
+}
+
+type captureUI struct {
+	*cli.MockUi
+}
+
+func (c *captureUI) Stdout() io.Writer {
+	return c.MockUi.OutputWriter
+}
+
+func (c *captureUI) Stderr() io.Writer {
+	return c.MockUi.ErrorWriter
+}
+
+func newCaptureUI() *captureUI {
+	return &captureUI{MockUi: cli.NewMockUi()}
 }
