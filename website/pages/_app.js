d23eae27f149cb996b13615ea1679341467432a6
Brandon Romano <brandon@hashicorp.com>
2021-06-10 14:54:48 -0700

    Revert changes to app.js file
    Not needed in the scope of this PR!

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -20,7 +20,7 @@ const { ConsentManager, openConsentManager } = createConsentManager({
   preset: 'oss',
 })
 
-function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   useAnchorLinkAnalytics()
   return (
     <ErrorBoundary FallbackComponent={Error}>
@@ -51,21 +51,3 @@ function App({ Component, pageProps }) {
     </ErrorBoundary>
   )
 }
-
-App.getInitialProps = async ({ Component, ctx }) => {
-  let pageProps = {}
-
-  if (Component.getInitialProps) {
-    pageProps = await Component.getInitialProps(ctx)
-  } else if (Component.isMDXComponent) {
-    // fix for https://github.com/mdx-js/mdx/issues/382
-    const mdxLayoutComponent = Component({}).props.originalType
-    if (mdxLayoutComponent.getInitialProps) {
-      pageProps = await mdxLayoutComponent.getInitialProps(ctx)
-    }
-  }
-
-  return { pageProps }
-}
-
-export default App
