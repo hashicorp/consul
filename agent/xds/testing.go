80c39f1083d68ab5f91c7fc78d66b0e21a09fb28
R.B. Boyer <4903+rboyer@users.noreply.github.com>
2021-06-15 15:21:07 -0500

    xds: adding more delta protocol tests (#10398)
    Fixes #10125

--- agent/xds/testing.go
+++ agent/xds/testing.go
@@ -30,6 +30,9 @@ type TestADSDeltaStream struct {
 	stubGrpcServerStream
 	sendCh chan *envoy_discovery_v3.DeltaDiscoveryResponse
 	recvCh chan *envoy_discovery_v3.DeltaDiscoveryRequest
+
+	mu      sync.Mutex
+	sendErr error
 }
 
 var _ ADSDeltaStream = (*TestADSDeltaStream)(nil)
@@ -45,8 +48,22 @@ func NewTestADSDeltaStream(t testing.T, ctx context.Context) *TestADSDeltaStream
 
 // Send implements ADSDeltaStream
 func (s *TestADSDeltaStream) Send(r *envoy_discovery_v3.DeltaDiscoveryResponse) error {
+	s.mu.Lock()
+	err := s.sendErr
+	s.mu.Unlock()
+
+	if err != nil {
+		return err
+	}
+
 	s.sendCh <- r
 	return nil
+}
+
+func (s *TestADSDeltaStream) SetSendErr(err error) {
+	s.mu.Lock()
+	s.sendErr = err
+	s.mu.Unlock()
 }
 
 // Recv implements ADSDeltaStream
@@ -65,6 +82,9 @@ type TestADSStream struct {
 	stubGrpcServerStream
 	sendCh chan *envoy_api_v2.DiscoveryResponse
 	recvCh chan *envoy_api_v2.DiscoveryRequest
+
+	mu      sync.Mutex
+	sendErr error
 }
 
 // NewTestADSStream makes a new TestADSStream
@@ -79,8 +99,22 @@ func NewTestADSStream(t testing.T, ctx context.Context) *TestADSStream {
 
 // Send implements ADSStream
 func (s *TestADSStream) Send(r *envoy_api_v2.DiscoveryResponse) error {
+	s.mu.Lock()
+	err := s.sendErr
+	s.mu.Unlock()
+
+	if err != nil {
+		return err
+	}
+
 	s.sendCh <- r
 	return nil
+}
+
+func (s *TestADSStream) SetSendErr(err error) {
+	s.mu.Lock()
+	s.sendErr = err
+	s.mu.Unlock()
 }
 
 // Recv implements ADSStream
@@ -197,6 +231,11 @@ func (e *TestEnvoy) SendReq(t testing.T, typeURL string, version, nonce uint64) 
 	}
 }
 
+func (e *TestEnvoy) SetSendErr(err error) {
+	e.stream.SetSendErr(err)
+	e.deltaStream.SetSendErr(err)
+}
+
 // SendDeltaReq sends a delta request from the test server.
 //
 // NOTE: the input request is mutated before sending by injecting the node.
@@ -207,19 +246,26 @@ func (e *TestEnvoy) SendDeltaReq(
 ) {
 	e.sendDeltaReq(t, typeURL, nil, req)
 }
+
 func (e *TestEnvoy) SendDeltaReqACK(
 	t testing.T,
 	typeURL string,
 	nonce uint64,
-	ack bool,
+) {
+	e.sendDeltaReq(t, typeURL, &nonce, nil)
+}
+
+func (e *TestEnvoy) SendDeltaReqNACK(
+	t testing.T,
+	typeURL string,
+	nonce uint64,
 	errorDetail *status.Status,
 ) {
-	req := &envoy_discovery_v3.DeltaDiscoveryRequest{}
-	if !ack {
-		req.ErrorDetail = errorDetail
-	}
-	e.sendDeltaReq(t, typeURL, &nonce, req)
+	e.sendDeltaReq(t, typeURL, &nonce, &envoy_discovery_v3.DeltaDiscoveryRequest{
+		ErrorDetail: errorDetail,
+	})
 }
+
 func (e *TestEnvoy) sendDeltaReq(
 	t testing.T,
 	typeURL string,
