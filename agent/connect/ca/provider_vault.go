02c49a13ac2879eb2c6cda7516e0acbd8985ede3
Dhia Ayachi <dhia@hashicorp.com>
2021-06-24 22:29:10 -0400

    remove extra newline in vault provider

--- agent/connect/ca/provider_vault.go
+++ agent/connect/ca/provider_vault.go
@@ -438,7 +438,7 @@ func (v *VaultProvider) Sign(csr *x509.CertificateRequest) (string, error) {
 		return "", fmt.Errorf("issuing_ca was not a string")
 	}
 
-	return fmt.Sprintf("%s\n%s\n", AddSingleNewline(cert), AddSingleNewline(ca)), nil
+	return fmt.Sprintf("%s%s", AddSingleNewline(cert), AddSingleNewline(ca)), nil
 }
 
 // SignIntermediate returns a signed CA certificate with a path length constraint
