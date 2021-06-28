6cdf1cc8395c1abc2ad89c5710eb5cebc596d09b
Dhia Ayachi <dhia@hashicorp.com>
2021-06-28 09:08:31 -0400

    remove new line from mock provider

--- agent/connect/ca/mock_Provider.go
+++ agent/connect/ca/mock_Provider.go
@@ -4,6 +4,7 @@ package ca
 
 import (
 	x509 "crypto/x509"
+
 	mock "github.com/stretchr/testify/mock"
 )
 
@@ -30,7 +31,7 @@ func (_m *MockProvider) ActiveIntermediate() (string, error) {
 		r1 = ret.Error(1)
 	}
 
-	return AddSingleNewline(r0), r1
+	return r0, r1
 }
 
 // ActiveRoot provides a mock function with given fields:
@@ -51,7 +52,7 @@ func (_m *MockProvider) ActiveRoot() (string, error) {
 		r1 = ret.Error(1)
 	}
 
-	return AddSingleNewline(r0), r1
+	return r0, r1
 }
 
 // Cleanup provides a mock function with given fields: providerTypeChange, config
@@ -100,7 +101,7 @@ func (_m *MockProvider) CrossSignCA(_a0 *x509.Certificate) (string, error) {
 		r1 = ret.Error(1)
 	}
 
-	return AddSingleNewline(r0), r1
+	return r0, r1
 }
 
 // GenerateIntermediate provides a mock function with given fields:
