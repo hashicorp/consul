b26a3ab3efb7f0fe6bcfcf8326a842e2af8bf13b
John Cowen <jcowen@hashicorp.com>
2021-06-28 12:19:40 +0100

    Use with-copyable modifier for our own CopyButton

--- ui/packages/consul-ui/app/components/copy-button/index.js
+++ ui/packages/consul-ui/app/components/copy-button/index.js
@@ -1,29 +1,9 @@
 import Component from '@glimmer/component';
-import { inject as service } from '@ember/service';
-import { action } from '@ember/object';
 import chart from './chart.xstate';
 
 export default class CopyButton extends Component {
-  @service('clipboard/os') clipboard;
-  @service('dom') dom;
-
   constructor() {
     super(...arguments);
     this.chart = chart;
-    this.guid = this.dom.guid(this);
-    this._listeners = this.dom.listeners();
-  }
-
-  @action
-  connect() {
-    this._listeners.add(this.clipboard.execute(`#${this.guid} button`), {
-      success: () => this.dispatch('SUCCESS'),
-      error: () => this.dispatch('ERROR'),
-    });
-  }
-
-  @action
-  disconnect() {
-    this._listeners.remove();
   }
 }
