b67b400a2067850afca1684e5b722ca45d168511
John Cowen <jcowen@hashicorp.com>
2021-07-21 12:45:06 +0100

    Rename clipboard test

--- ui/packages/consul-ui/tests/unit/services/clipboard/native-test.js
+++ ui/packages/consul-ui/tests/unit/services/clipboard/native-test.js
@@ -1,12 +1,12 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Service | clipboard/os', function(hooks) {
+module('Unit | Service | clipboard/native', function(hooks) {
   setupTest(hooks);
 
   // Replace this with your real tests.
   test('it exists', function(assert) {
-    let service = this.owner.lookup('service:clipboard/os');
+    let service = this.owner.lookup('service:clipboard/native');
     assert.ok(service);
   });
 });
