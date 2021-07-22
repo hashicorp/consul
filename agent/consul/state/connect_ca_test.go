3dde24d8c96347d87b39756144e8781351a26eb7
Dhia Ayachi <dhia@hashicorp.com>
2021-07-22 10:32:27 -0400

    config raft apply silent error (#10657)
    * return an error when the index is not valid
    
    * check response as bool when applying `CAOpSetConfig`
    
    * remove check for bool response
    
    * fix error message and add check to test
    
    * fix comment
    
    * add changelog

--- agent/consul/state/connect_ca_test.go
+++ agent/consul/state/connect_ca_test.go
@@ -4,6 +4,8 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/hashicorp/consul/sdk/testutil"
+
 	"github.com/hashicorp/go-memdb"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -59,9 +61,9 @@ func TestStore_CAConfigCAS(t *testing.T) {
 	ok, err := s.CACheckAndSetConfig(2, 0, &structs.CAConfiguration{
 		Provider: "static",
 	})
-	if ok || err != nil {
-		t.Fatalf("expected (false, nil), got: (%v, %#v)", ok, err)
-	}
+
+	require.False(t, ok)
+	testutil.RequireErrorContains(t, err, "ModifyIndex did not match existing")
 
 	// Check that the index is untouched and the entry
 	// has not been updated.
