41ed50b0fbbf5d87ad216159cae653b37b33a539
Jared Kirschner <jkirschner@hashicorp.com>
2021-08-19 13:31:24 -0400

    Allow log filename to be templated with timestamp
    Valid template variables include:
    - timestamp-human: human-readable timestamp in format
                       YEAR-MONTH-DATE_HOUR-MINUTE-SECOND-NANOSECOND
    - timestamp-unix: Unix timestamp integer
    
    If one of the template variables above exists in the configured log filename,
    the Unix timestamp will not be automatically injected into the filename
    immediately preceding the file extension.
    
    Example usage: configured filename input => output log filename
    	"consul" => "consul-1629423611350800686.log"
    	"${timestamp-unix}-consul" => "1629423611350800686-consul.log"
    	"log-${timestamp-human}.txt" => "log-2021-08-20_01-40-11-350800686.txt"

--- logging/logfile_test.go
+++ logging/logfile_test.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"regexp"
 	"sort"
 	"testing"
 	"time"
@@ -115,6 +116,110 @@ func TestLogFile_FileRotation_Disabled(t *testing.T) {
 	logFile.Write([]byte("[INFO] Second File"))
 	logFile.Write([]byte("[INFO] Third File"))
 	require.Len(t, listDir(t, tempDir), 1)
+}
+
+func TestLogFile_FileName(t *testing.T) {
+	cases := []struct {
+		desc                   string
+		inputFilename          string
+		outputFilenamePattern  string             
+		expectError            string
+	}{
+		{
+			desc: "append extension if not given",
+			// Test whether a Unix timestamp and .log extension is appended if the
+			// input filename has no extension.
+			// For example:
+			// "consul" => "consul-1629417801659820807.log"
+			inputFilename: "consul",
+			outputFilenamePattern: `consul-\d+.log`,
+		},
+		{
+			desc: "leave extension if given",
+			// Test whether a Unix timestamp is injected before the extension if the
+			// input filename has a non-.log extension.
+			// For example:
+			// "consul.txt" => "consul-1629417801659820807.txt"
+			inputFilename: "consul.txt",
+			outputFilenamePattern: `consul-\d+.txt`,
+		},
+		{
+			desc: "template with human readable timestamp",
+			// Test whether a human-readable timestamp is injected at the location
+			// specified in the template.
+			// For example:
+			// "con-${timestamp-human}-sul" => "con-2021-08-19_20-13-08-322108785-sul.log"
+			inputFilename: "con-${timestamp-human}-sul",
+			outputFilenamePattern: `con-\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}-\d{9}-sul.log`,
+		},
+		{
+			desc: "template with unix timestamp",
+			// Test whether a human-readable timestamp is injected at the location
+			// specified in the template.
+			// For example:
+			// "${timestamp-unix}-consul" => "1629418506846614551-consul.log"
+			inputFilename: "${timestamp-unix}-consul",
+			outputFilenamePattern: `\d+-consul.log`,
+		},
+		{
+			desc: "template with multiple fields",
+			// Test whether multiple fields can be injected. This isn't expected to be
+			// a real use case, but we want to test this extreme case.
+			// For example:
+			// "${timestamp-unix}-con${timestamp-unix}sul-${timestamp-human}.txt" =>
+			// "1629418506846614551-con1629418506846614551sul-2021-08-19_20-13-08-322108785.txt"
+			inputFilename: "${timestamp-unix}-con${timestamp-unix}sul-${timestamp-human}.txt",
+			outputFilenamePattern: `\d+-con\d+sul-\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}-\d{9}.txt`,
+		},
+		{
+			desc: "template valid but no known variables used",
+			// Test whether a Unix timestamp is injected if a filename template is used,
+			// but no known timestamp variables (timestamp-unix, timestamp-human) are
+			// referenced.
+			// For example:
+			// "{1+3}" => "4-1629417801659820807.log"
+			inputFilename: "${1+3}",
+			outputFilenamePattern: `4-\d+.log`,
+		},
+		{
+			desc: "template invalid unknown variable",
+			// Test whether an error occurs when an unknown variable is referenced
+			// in a template filename.
+			// For example:
+			// "${abc}-consul" => error
+			inputFilename: "${abc}-consul",
+			expectError: "failed to evaluate log filename template",
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.desc, func(t *testing.T) {
+			require := require.New(t)
+
+			tempDir := testutil.TempDir(t, t.Name())
+			logFile := LogFile{
+				fileName: c.inputFilename,
+				logPath:  tempDir,
+				MaxBytes: 10,
+				MaxFiles: -1,
+			}
+
+			n, err := logFile.Write([]byte("[INFO] Hello World"))
+			if c.expectError != "" {
+				require.Equal(n, 0)
+				require.Contains(err.Error(), c.expectError)
+			} else {
+				// Should be one file in the temporary directory
+				logFiles := listDir(t, tempDir)
+				require.Len(listDir(t, tempDir), 1)
+
+				// The filename should match the specified pattern
+				matched, err := regexp.Match(c.outputFilenamePattern, []byte(logFiles[0]))
+				require.True(matched)
+				require.Nil(err)	
+			}
+		})
+	}
 }
 
 func listDir(t *testing.T, name string) []string {
