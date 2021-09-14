e6f198c2df0f589bb9ca6bafaa65de9d0f414893
John Cowen <jcowen@hashicorp.com>
2021-09-14 15:54:10 +0100

    Tokens still needs a repo service for CRUD

--- ui/packages/consul-ui/app/routes/dc/acls/tokens/index.js
+++ ui/packages/consul-ui/app/routes/dc/acls/tokens/index.js
@@ -1,7 +1,10 @@
 import Route from 'consul-ui/routing/route';
+import { inject as service } from '@ember/service';
+
 import WithBlockingActions from 'consul-ui/mixins/with-blocking-actions';
 
 export default class IndexRoute extends Route.extend(WithBlockingActions) {
+  @service('repository/token') repo;
   queryParams = {
     sortBy: 'sort',
     kind: 'kind',
