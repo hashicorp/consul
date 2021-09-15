4bdd2c1e8e0ab1198fede24d54033dfdabac70cc
Pamela Bortnick <pbortnick@gmail.com>
2021-09-15 14:56:37 -0400

    Start new page

---
+++ website/pages/consul-on-kubernetes/index.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+// import s from './style.module.css'
+
+export default function CommunityPage() {
+  return (
+    <div>
+      <Head>
+        <title key="title">Consul on Kubernetes</title>
+      </Head>
+      {/* header */}
+      {/* side by side section */}
+      {/* features list section */}
+      {/* get started section */}
+    </div>
+  )
+}
