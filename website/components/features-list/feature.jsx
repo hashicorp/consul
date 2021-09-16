fef9cf5ae6688ffb7320f476c67246219c2f58ee
Pamela Bortnick <pbortnick@gmail.com>
2021-09-16 14:33:00 -0400

    Add a lot of styling and background image

--- website/components/features-list/feature.jsx
+++ website/components/features-list/feature.jsx
@@ -11,26 +11,26 @@ export default function Feature({
 }) {
   return (
     <div className={s.featureContainer}>
-      <div className={s.featureText}>
-        <div className={s.listNumber}>
-          <span className="g-type-display-5">{number}</span>
+      <div className={s.featureTextContainer}>
+        <span className={s.listNumber}>{number}</span>
+        <div className={s.featureText}>
+          <h3 className={s.featureTitle}>{title}</h3>
+          <p className={s.featureSubtitle}>{subtitle}</p>
+          <div className={s.infoSection}>
+            {infoSections.map(({ heading, content }) => (
+              <div className={s.infoSectionItem} key={`${title}-${heading}`}>
+                <h4 className={s.infoTitle}>{heading}</h4>
+                {content}
+              </div>
+            ))}
+          </div>
+          <Link href={cta.url}>
+            <a className={s.cta}>
+              <span>{cta.text}</span>
+              <img src="/img/icons/arrow.svg" alt="cta-arrow" />
+            </a>
+          </Link>
         </div>
-        <div>
-          <h3 className="g-type-display-2">{title}</h3>
-          <p className="g-type-body-large ">{subtitle}</p>
-          {infoSections.map(({ heading, content }) => (
-            <div className={s.infoSection} key={`${title}-${heading}`}>
-              <h4 className="g-type-display-5">{title}</h4>
-              {content}
-            </div>
-          ))}
-        </div>
-        <Link href={cta.url}>
-          <a className={s.cta}>
-            <span>{cta.text}</span>
-            <img src="/img/icons/arrow.svg" alt="cta-arrow" />
-          </a>
-        </Link>
       </div>
       <div className={s.featureImage}>
         <img src={image} alt={title} />
--- website/components/features-list/index.jsx
+++ website/components/features-list/index.jsx
@@ -3,12 +3,19 @@ import s from './style.module.css'
 
 export default function FeaturesList({ title, features }) {
   return (
-    <div className={s.root}>
-      <h2 className="g-type-display-1">{title}</h2>
-      <div className={s.featuresContainer}>
-        {features.map((feature, i) => (
-          <Feature {...feature} number={i + 1} key={feature.title} />
-        ))}
+    <div
+      className={s.featureListContainer}
+      style={{
+        backgroundImage: `url(${require('./images/top-right-design.svg')})`,
+      }}
+    >
+      <div className="g-grid-container">
+        <h2 className={s.title}>{title}</h2>
+        <div className={s.featuresContainer}>
+          {features.map((feature, i) => (
+            <Feature {...feature} number={i + 1} key={feature.title} />
+          ))}
+        </div>
       </div>
     </div>
   )
