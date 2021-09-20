372e77d93d8e4cee762ce20f82e962851a064d48
Pamela Bortnick <pbortnick@gmail.com>
2021-09-20 12:30:56 -0400

    Add content and more styles

--- website/components/side-by-side/index.jsx
+++ website/components/side-by-side/index.jsx
@@ -2,7 +2,7 @@ import s from './style.module.css'
 
 export default function SideBySide({ left, right }) {
   return (
-    <div>
+    <div className={s.sideBySide}>
       <div className={s.leftSide}>{left}</div>
       <div className={s.rightSide}>{right}</div>
     </div>
