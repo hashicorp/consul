2d23f92b35981cb0ed9d2508e5f3f94ac1e28bcd
Evan Culver <eculver@hashicorp.com>
2021-09-22 09:30:45 -0700

    add 1.19.x versions to test config

--- agent/xds/envoy_versioning_test.go
+++ agent/xds/envoy_versioning_test.go
@@ -121,6 +121,7 @@ func TestDetermineSupportedProxyFeaturesFromString(t *testing.T) {
 		"1.16.0", "1.16.1", "1.16.2", "1.16.3", "1.16.4", "1.16.5",
 		"1.17.0", "1.17.1", "1.17.2", "1.17.3", "1.17.4",
 		"1.18.0", "1.18.1", "1.18.2", "1.18.3", "1.18.4",
+		"1.19.0", "1.19.1",
 	} {
 		cases[v] = testcase{expect: supportedProxyFeatures{}}
 	}
