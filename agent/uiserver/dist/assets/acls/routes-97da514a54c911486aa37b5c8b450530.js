178cb3fa9b20edb02f1a942e6ab4c980341f43ce
kisunji <ckim@hashicorp.com>
2021-09-23 16:17:30 -0400

    Remove artifacts

--- agent/uiserver/dist/assets/acls/routes-97da514a54c911486aa37b5c8b450530.js
+++
@@ -1,2 +0,0 @@
-(function(t="consulUi",e=document){const s=e.getElementsByTagName("script")
-s[s.length-1].dataset[t+"Routes"]=JSON.stringify({dc:{acls:{tokens:{_options:{abilities:["read tokens"]}}}}})})()
