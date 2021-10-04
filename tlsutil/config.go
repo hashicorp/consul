59fa1a4b05df1a072fdaf0d515b06d7b9cf601c4
Daniel Nephin <dnephin@hashicorp.com>
2021-10-04 16:18:57 -0400

    Merge pull request #11210 from hashicorp/dnephin/fix-raft-authz-error
    rpc: include error for AuthorizeServerConn failures

--- tlsutil/config.go
+++ tlsutil/config.go
@@ -930,6 +930,10 @@ func (c *Configurator) wrapALPNTLSClient(dc, nodeName, alpnProto string, conn ne
 	return tlsConn, nil
 }
 
+type TLSConn interface {
+	ConnectionState() tls.ConnectionState
+}
+
 // AuthorizeServerConn is used to validate that the connection is being established
 // by a Consul server in the same datacenter.
 //
@@ -939,7 +943,7 @@ func (c *Configurator) wrapALPNTLSClient(dc, nodeName, alpnProto string, conn ne
 //
 // Note this check is only performed if VerifyServerHostname is enabled, otherwise
 // it does no authorization.
-func (c *Configurator) AuthorizeServerConn(dc string, conn *tls.Conn) error {
+func (c *Configurator) AuthorizeServerConn(dc string, conn TLSConn) error {
 	if !c.VerifyServerHostname() {
 		return nil
 	}
@@ -968,7 +972,7 @@ func (c *Configurator) AuthorizeServerConn(dc string, conn *tls.Conn) error {
 		if err == nil {
 			return nil
 		}
-		multierror.Append(errs, err)
+		errs = multierror.Append(errs, err)
 	}
 	return fmt.Errorf("AuthorizeServerConn failed certificate validation for certificate with a SAN.DNSName of %v: %w", expected, errs)
 
