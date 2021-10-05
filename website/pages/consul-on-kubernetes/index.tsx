cf4c5f7ed52a8c8b6b707ad80d40e659010f7d4f
Pamela Bortnick <pbortnick@gmail.com>
2021-10-05 12:50:51 -0400

    Add meta title

--- website/pages/consul-on-kubernetes/index.tsx
+++ website/pages/consul-on-kubernetes/index.tsx
@@ -22,6 +22,7 @@ export default function ConsulOnKubernetesPage() {
         image="https://www.consul.io/img/og-image.png"
         twitterCard="summary_large_image"
       >
+        <meta name="og:title" property="og:title" content={pageTitle} />
         <meta name="twitter:title" content={pageTitle} />
         <meta name="twitter:description" content={pageDescription} />
         <meta name="author" content="@HashiCorp" />
