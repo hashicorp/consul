b80f622404284c882af2ed8841b407cee1bff14c
Pamela Bortnick <pbortnick@gmail.com>
2021-10-05 12:45:43 -0400

    Test 2

--- website/pages/consul-on-kubernetes/index.tsx
+++ website/pages/consul-on-kubernetes/index.tsx
@@ -22,7 +22,6 @@ export default function ConsulOnKubernetesPage() {
         image="https://www.consul.io/img/og-image.png"
         twitterCard="summary_large_image"
       >
-        <meta name="og:title" property="og:title" content={pageTitle} />
         <meta name="twitter:title" content={pageTitle} />
         <meta name="twitter:description" content={pageDescription} />
         <meta name="author" content="@HashiCorp" />
