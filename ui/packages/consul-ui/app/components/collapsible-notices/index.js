731f3f64e5447304277a85f8c2e98c7a4484ffe0
kenia <keniavalladarez@gmail.com>
2021-10-05 12:04:13 -0400

    Refactor conditions for collapsible banners

--- ui/packages/consul-ui/app/components/collapsible-notices/index.js
+++ ui/packages/consul-ui/app/components/collapsible-notices/index.js
@@ -1,3 +1,16 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
-export default class CollapsibleNotices extends Component {}
+export default class CollapsibleNotices extends Component {
+  // =attributes
+  @tracked collapsible = false;
+
+  @action
+  countNotices() {
+    const allNotices = [
+      ...document.querySelectorAll('div.collapsible-notices > div.notices > .notice'),
+    ];
+    this.collapsible = allNotices.length > 2;
+  }
+}
