3b1c88385b88b647f844cf5ff655423450f41fca
kenia <keniavalladarez@gmail.com>
2021-10-06 10:12:15 -0400

    Refactor collapsible notices with a helper

--- ui/packages/consul-ui/app/components/collapsible-notices/index.js
+++ ui/packages/consul-ui/app/components/collapsible-notices/index.js
@@ -1,16 +1,3 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
 
-export default class CollapsibleNotices extends Component {
-  // =attributes
-  @tracked collapsible = false;
-
-  @action
-  countNotices() {
-    const allNotices = [
-      ...document.querySelectorAll('div.collapsible-notices > div.notices > .notice'),
-    ];
-    this.collapsible = allNotices.length > 2;
-  }
-}
+export default class CollapsibleNotices extends Component {}
