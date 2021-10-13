7d8b91359a0e20c0025b7402547f78e71b22333d
FFMMM <FFMMM@users.noreply.github.com>
2021-10-12 17:21:05 -0700

    test client does not emit autopilot metric
    Signed-off-by: FFMMM <FFMMM@users.noreply.github.com>

--- agent/metrics_test.go
+++ agent/metrics_test.go
@@ -1,11 +1,54 @@
 package agent
 
 import (
+	"github.com/stretchr/testify/require"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 )
+
+func checkForShortTesting(t *testing.T) {
+	if testing.Short() {
+		t.Skip("too slow for testing.Short")
+	}
+}
+
+func recordPromMetrics(t *testing.T, a *TestAgent, respRec *httptest.ResponseRecorder) {
+	req, err := http.NewRequest("GET", "/v1/agent/metrics?format=prometheus", nil)
+	if err != nil {
+		require.NoError(t, err, "Failed to generate new http request.")
+	}
+
+	_, err = a.srv.AgentMetrics(respRec, req)
+	if err != nil {
+		require.NoError(t, err, "Failed to serve agent metrics")
+	}
+}
+
+func assertMetricExistsWithValue(t *testing.T,respRec *httptest.ResponseRecorder, metric string, value string) {
+	if respRec.Body.String() == "" {
+		t.Fatalf("Response body is empty.")
+	}
+
+	// eg "consul_autopilot_healthy NaN"
+	target := metric + " " + value
+
+	if !strings.Contains(respRec.Body.String(), target) {
+		t.Fatalf("Could not find the metric \"%s\" with value \"%s\" in the /v1/agent/metrics response", metric, value)
+	}
+}
+
+func assertMetricNotExists(t *testing.T,respRec *httptest.ResponseRecorder, metric string) {
+	if respRec.Body.String() == "" {
+		t.Fatalf("Response body is empty.")
+	}
+
+	if strings.Contains(respRec.Body.String(), metric) {
+		t.Fatalf("Didn't expect to find the metric \"%s\" in the /v1/agent/metrics response", metric)
+	}
+}
+
 
 // TestHTTPHandlers_AgentMetrics_ConsulAutopilotHealthy_Prometheus adds testing around
 // the published autopilot metrics on https://www.consul.io/docs/agent/telemetry#autopilot
@@ -13,41 +56,44 @@ func TestHTTPHandlers_AgentMetrics_ConsulAutopilotHealthy_Prometheus(t *testing.
 	checkForShortTesting(t)
 	// This test cannot use t.Parallel() since we modify global state, ie the global metrics instance
 
-	// don't bootstrap agent so as not to
-	// become a leader
-	hcl := `
-	telemetry = {
-		prometheus_retention_time = "5s",
-		disable_hostname = true
-	}
-	bootstrap = false
+	t.Run("Check consul_autopilot_healthy metric value on startup", func(t *testing.T) {
+		// don't bootstrap agent so as not to
+		// become a leader
+		hcl := `
+		telemetry = {
+			prometheus_retention_time = "5s",
+			disable_hostname = true
+		}
+		bootstrap = false
+		`
+
+		a := StartTestAgent(t, TestAgent{HCL: hcl})
+		defer a.Shutdown()
+
+		respRec := httptest.NewRecorder()
+		recordPromMetrics(t, a, respRec)
+
+		assertMetricExistsWithValue(t, respRec, "consul_autopilot_healthy", "NaN")
+	})
+
+	t.Run("Check autopilot metrics on clients", func(t *testing.T) {
+		hcl := `
+		telemetry = {
+			prometheus_retention_time = "5s"
+			disable_hostname = true
+			metrics_prefix = "agent_2"
+		}
+		bootstrap = false
+		server = false
 	`
 
-	a := StartTestAgent(t, TestAgent{HCL: hcl})
-	defer a.Shutdown()
+		a := StartTestAgent(t, TestAgent{HCL: hcl})
+		defer a.Shutdown()
 
-	req, err := http.NewRequest("GET", "/v1/agent/metrics?format=prometheus", nil)
-	if err != nil {
-		t.Fatalf("Failed to generate new http request. err: %v", err)
-	}
+		respRec := httptest.NewRecorder()
+		recordPromMetrics(t, a, respRec)
 
-	respRec := httptest.NewRecorder()
-	_, err = a.srv.AgentMetrics(respRec, req)
-	if err != nil {
-		t.Fatalf("Failed to serve agent metrics. err: %v", err)
-	}
-
-	t.Run("Check consul_autopilot_healthy metric value on startup", func(t *testing.T) {
-		target := "consul_autopilot_healthy NaN"
-		keyValue := strings.Split(target, " ")
-		if !strings.Contains(respRec.Body.String(), target) {
-			t.Fatalf("Could not find the metric \"%s\" with value \"%s\" in the /v1/agent/metrics response", keyValue[0], keyValue[1])
-		}
+		// todo -- uncomment test once client metric is emitted anymore for clients
+		//assertMetricNotExists(t, respRec, "consul_autopilot_healthy")
 	})
-}
-
-func checkForShortTesting(t *testing.T) {
-	if testing.Short() {
-		t.Skip("too slow for testing.Short")
-	}
 }
