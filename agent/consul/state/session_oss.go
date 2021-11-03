5cf047e05b03176a12515244e365ba7cdc346292
Dhia Ayachi <dhia@hashicorp.com>
2021-11-03 09:19:54 -0400

    clean up unused functions

--- agent/consul/state/session_oss.go
+++ agent/consul/state/session_oss.go
@@ -171,7 +171,7 @@ func (s *Store) SessionList(ws memdb.WatchSet, entMeta *structs.EnterpriseMeta) 
 	var result structs.Sessions
 
 	// Query all of the active sessions.
-	sessions, err := getWithTxn(tx, tableSessions, indexID+"_prefix", "", nil)
+	sessions, err := tx.Get(tableSessions, indexID+"_prefix", Query{Value: "", EnterpriseMeta: m})
 	if err != nil {
 		return 0, nil, fmt.Errorf("failed session lookup: %s", err)
 	}
