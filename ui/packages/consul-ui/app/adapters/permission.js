bb50e843514ef6b08d9a5348f1aa9c85442534d0
John Cowen <johncowen@users.noreply.github.com>
2021-11-04 12:15:56 +0000

    ui: Ensure the tokens default nspace is passed thru to the auth endpoint (#11472)
    Most HTTP API calls will use the default namespace of the calling token to additionally filter/select the data used for the response if one is not specified by the frontend.
    
    The internal permissions/authorize endpoint does not do this (you can ask for permissions from different namespaces in on request).
    
    Therefore this PR adds the tokens default namespace in the frontend only to our calls to the authorize endpoint. I tried to do it in a place that made it feel like it's getting added in the backend, i.e. in a place which was least likely to ever require changing or thinking about.
    
    Note:  We are probably going to change this internal endpoint to also inspect the tokens default namespace on the backend. At which point we can revert this commit/PR.

--- ui/packages/consul-ui/app/adapters/permission.js
+++ ui/packages/consul-ui/app/adapters/permission.js
@@ -3,6 +3,7 @@ import { inject as service } from '@ember/service';
 
 export default class PermissionAdapter extends Adapter {
   @service('env') env;
+  @service('settings') settings;
 
   requestForAuthorize(request, { dc, ns, partition, resources = [], index }) {
     // the authorize endpoint is slightly different to all others in that it
@@ -29,8 +30,21 @@ export default class PermissionAdapter extends Adapter {
 
   authorize(store, type, id, snapshot) {
     return this.rpc(
-      function(adapter, request, serialized, unserialized) {
-        return adapter.requestForAuthorize(request, serialized, unserialized);
+      async (adapter, request, serialized, unserialized) => {
+        // the authorize endpoint does not automatically take into account the
+        // default namespace of the token on the backend. This means that we
+        // need to add the default namespace of the token on the frontend
+        // instead. Decided this is the best place for it as its almost hidden
+        // from the rest of the app so from an app eng point of view it almost
+        // feels like it does happen on the backend.
+        const nspacesEnabled = this.env.var('CONSUL_NSPACES_ENABLED');
+        if(nspacesEnabled) {
+          const token = await this.settings.findBySlug('token');
+          if(typeof serialized.ns === 'undefined' || serialized.ns.length === 0) {
+            serialized.ns = token.Namespace;
+          }
+        }
+        return adapter.requestForAuthorize(request, serialized);
       },
       function(serializer, respond, serialized, unserialized) {
         // Completely skip the serializer here
