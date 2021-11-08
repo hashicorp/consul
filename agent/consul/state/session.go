e2ccee563dd1693f0ad5b676724be47758752a96
Dhia Ayachi <dhia@hashicorp.com>
2021-11-08 10:03:18 -0500

    partition `indexID` and `indexSession` of `tableSessionChecks`

--- agent/consul/state/session.go
+++ agent/consul/state/session.go
@@ -65,10 +65,7 @@ func sessionChecksTableSchema() *memdb.TableSchema {
 				Name:         indexID,
 				AllowMissing: false,
 				Unique:       true,
-				Indexer: indexerSingle{
-					readIndex:  indexFromNodeCheckIDSession,
-					writeIndex: indexFromNodeCheckIDSession,
-				},
+				Indexer:      idCheckIndexer(),
 			},
 			indexNodeCheck: {
 				Name:         indexNodeCheck,
@@ -80,10 +77,7 @@ func sessionChecksTableSchema() *memdb.TableSchema {
 				Name:         indexSession,
 				AllowMissing: false,
 				Unique:       false,
-				Indexer: indexerSingle{
-					readIndex:  indexFromString,
-					writeIndex: indexSessionCheckFromSession,
-				},
+				Indexer:      sessionCheckIndexer(),
 			},
 		},
 	}
@@ -429,8 +423,11 @@ func (s *Store) deleteSessionTxn(tx WriteTxn, idx uint64, sessionID string, entM
 		return fmt.Errorf("unknown session behavior %#v", session.Behavior)
 	}
 
+	if entMeta == nil {
+		entMeta = structs.DefaultEnterpriseMetaInDefaultPartition()
+	}
 	// Delete any check mappings.
-	mappings, err := tx.Get(tableSessionChecks, indexSession, sessionID)
+	mappings, err := tx.Get(tableSessionChecks, indexSession, Query{Value: sessionID, EnterpriseMeta: *entMeta})
 	if err != nil {
 		return fmt.Errorf("failed session checks lookup: %s", err)
 	}
