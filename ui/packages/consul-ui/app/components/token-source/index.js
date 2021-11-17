5215fe1a1c802195d2a82c6a9225cd3fd2b07e36
John Cowen <jcowen@hashicorp.com>
2021-11-17 12:32:34 +0000

    ui: Upgrades token sourcing related components to Glimmer+docs

--- ui/packages/consul-ui/app/components/token-source/index.js
+++ ui/packages/consul-ui/app/components/token-source/index.js
@@ -1,37 +1,47 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 import chart from './chart.xstate';
-export default Component.extend({
-  onchange: function() {},
-  init: function() {
-    this._super(...arguments);
+
+export default class TokenSource extends Component {
+  @tracked provider;
+  @tracked jwt;
+
+  constructor() {
+    super(...arguments);
     this.chart = chart;
-  },
-  actions: {
-    isSecret: function() {
-      return this.type === 'secret';
-    },
-    change: function(e) {
-      e.data.toJSON = function() {
-        return {
-          AccessorID: this.AccessorID,
-          // TODO: In the past we've always ignored the SecretID returned
-          // from the server and used what the user typed in instead, now
-          // as we don't know the SecretID when we use SSO we use the SecretID
-          // in the response
-          SecretID: this.SecretID,
-          Namespace: this.Namespace,
-          Partition: this.Partition,
-          ...{
-            AuthMethod: typeof this.AuthMethod !== 'undefined' ? this.AuthMethod : undefined,
-            // TODO: We should be able to only set namespaces if they are enabled
-            // but we might be testing for nspaces everywhere
-            // Namespace: typeof this.Namespace !== 'undefined' ? this.Namespace : undefined
-          },
-        };
+  }
+
+  @action
+  isSecret() {
+    return this.args.type === 'secret';
+  }
+
+  @action
+  change(e) {
+    e.data.toJSON = function() {
+      return {
+        AccessorID: this.AccessorID,
+        // TODO: In the past we've always ignored the SecretID returned
+        // from the server and used what the user typed in instead, now
+        // as we don't know the SecretID when we use SSO we use the SecretID
+        // in the response
+        SecretID: this.SecretID,
+        Namespace: this.Namespace,
+        Partition: this.Partition,
+        ...{
+          AuthMethod: typeof this.AuthMethod !== 'undefined' ? this.AuthMethod : undefined,
+          // TODO: We should be able to only set namespaces if they are enabled
+          // but we might be testing for nspaces everywhere
+          // Namespace: typeof this.Namespace !== 'undefined' ? this.Namespace : undefined
+        },
       };
-      // TODO: We should probably put the component into idle state
-      this.onchange(e);
-    },
-  },
-});
+    };
+    // TODO: We should probably put the component into idle state
+    if(typeof this.args.onchange === 'function') {
+      this.args.onchange(e);
+    }
+  }
+
+}
