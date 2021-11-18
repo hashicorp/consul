ef1276711890e42034d64a6dbff661bba10edd8e
John Cowen <jcowen@hashicorp.com>
2021-11-18 15:23:52 +0000

    Bare minimum admin partitioned SSO

--- ui/packages/consul-ui/app/components/oidc-select/index.js
+++ ui/packages/consul-ui/app/components/oidc-select/index.js
@@ -1,20 +1,9 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import chart from './chart.xstate';
 
-export default Component.extend({
-  tagName: '',
-  onchange: function() {},
-  onerror: function() {},
-  init: function() {
-    this._super(...arguments);
+export default class OidcSelect extends Component {
+  constructor() {
+    super(...arguments);
     this.chart = chart;
-  },
-  didReceiveAttrs: function() {
-    // This gets called no matter which attr has changed
-    // such as disabled, thing is once we are in loaded state
-    // it doesn't do anything anymore
-    if (typeof this.items !== 'undefined') {
-      this.dispatch('SUCCESS');
-    }
-  },
-});
+  }
+}
