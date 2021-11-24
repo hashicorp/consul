3f131dcf34500381dba6abaf62e9c9226639ca54
John Cowen <johncowen@users.noreply.github.com>
2021-11-24 18:14:07 +0000

    ui: Notifications re-organization/re-style (#11577)
    - Moves where they appear up to the <App /> component.
    - Instead of a <Notification /> wrapping component to move whatever you use for a notification up to where they need to appear (via ember-cli-flash), we now use a {{notification}} modifier now we have modifiers.
    - Global notifications/flashes are no longer special styles of their own. You just use the {{notification}} modifier to hoist whatever component/element you want up to the top of the page. This means we can re-use our existing <Notice /> component for all our global UI notifications (this is the user visible change here)

--- ui/packages/consul-ui/app/components/hashicorp-consul/index.js
+++ ui/packages/consul-ui/app/components/hashicorp-consul/index.js
@@ -1,7 +1,10 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class HashiCorpConsul extends Component {
+  @service('flashMessages') flashMessages;
+
   @action
   open() {
     this.authForm.focus();
