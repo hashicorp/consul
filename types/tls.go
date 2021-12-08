e4ad6856235f10ca1d598eeb03b6b93396b66dd9
Mike Morris <mikemorris@users.noreply.github.com>
2021-12-08 17:22:00 -0500

    types: update TLSCipherSuite to string type, remove Human* functions

--- types/tls.go
+++ types/tls.go
@@ -1,5 +1,9 @@
 package types
 
+import (
+	"fmt"
+)
+
 // TLSVersion is a strongly-typed string for TLS versions
 type TLSVersion string
 
@@ -40,13 +44,6 @@ var (
 		"tls12": TLSv1_2,
 		"tls13": TLSv1_3,
 	}
-	HumanTLSVersionStrings = map[TLSVersion]string{
-		TLSVersionAuto: "Allow implementation to select TLS version",
-		TLSv1_0:        "TLS 1.0",
-		TLSv1_1:        "TLS 1.1",
-		TLSv1_2:        "TLS 1.2",
-		TLSv1_3:        "TLS 1.3",
-	}
 	// NOTE: these currently map to the deprecated config strings to support the
 	// deployment pattern of upgrading servers first. This map should eventually
 	// be removed and any lookups updated to instead use the TLSVersion string
@@ -69,66 +66,77 @@ var (
 // and as supported cipher suites in the Go runtime change.
 //
 // The naming convention for cipher suites changed in TLS 1.3
-// but constant values should still be globally unqiue
-// Handling validation on a subset of TLSCipherSuite constants
-// would be a future exercise if cipher suites for TLS 1.3 ever
-// become configurable in BoringSSL, Envoy, or other implementation
-type TLSCipherSuite uint16
+// but constant values should still be globally unqiue.
+//
+// Handling validation on distinct sets of TLS 1.3 and TLS 1.2 TLSCipherSuite
+// constants would be a future exercise if cipher suites for TLS 1.3 ever
+// become configurable in BoringSSL, Envoy, or other implementation.
+type TLSCipherSuite string
 
 const (
-	// Envoy cipher suites also used by Consul agent
-	TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256       TLSCipherSuite = 0xc02b
-	TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256                = 0xcca9 // Not used by Consul agent yet
-	TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256                        = 0xc02f
-	TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256                  = 0xcca8 // Not used by Consul agent yet
-	TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA                         = 0xc009
-	TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA                           = 0xc013
-	TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384                      = 0xc02c
-	TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384                        = 0xc030
-	TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA                         = 0xc00a
-	TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA                           = 0xc014
+	// Cipher suites used by Envoy but not used by Consul agent yet
+	TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256 = "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256"
+	TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256   = "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256"
 
-	// Older cipher suites not supported for Consul agent TLS, will eventually be removed from Envoy defaults
-	TLS_RSA_WITH_AES_128_GCM_SHA256 = 0x009c
-	TLS_RSA_WITH_AES_128_CBC_SHA    = 0x002f
-	TLS_RSA_WITH_AES_256_GCM_SHA384 = 0x009d
-	TLS_RSA_WITH_AES_256_CBC_SHA    = 0x0035
+	// Cipher suites used by both Envoy and Consul agent
+	TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256 = "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256"
+	TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256   = "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"
+	TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA    = "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA"
+	TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA      = "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA"
+	TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384 = "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384"
+	TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384   = "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
+	TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA    = "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA"
+	TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA      = "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA"
+
+	// Older cipher suites not supported for Consul agent TLS,
+	// will eventually be removed from Envoy defaults
+	TLS_RSA_WITH_AES_128_GCM_SHA256 = "TLS_RSA_WITH_AES_128_GCM_SHA256"
+	TLS_RSA_WITH_AES_128_CBC_SHA    = "TLS_RSA_WITH_AES_128_CBC_SHA"
+	TLS_RSA_WITH_AES_256_GCM_SHA384 = "TLS_RSA_WITH_AES_256_GCM_SHA384"
+	TLS_RSA_WITH_AES_256_CBC_SHA    = "TLS_RSA_WITH_AES_256_CBC_SHA"
 
 	// Additional cipher suites used by Consul agent but not Envoy
 	// TODO: these are both explicitly listed as insecure and disabled in the Go source, should they be removed?
 	// https://cs.opensource.google/go/go/+/refs/tags/go1.17.3:src/crypto/tls/cipher_suites.go;l=329-330
-	TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256 = 0x0023
-	TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256   = 0xc027
+	TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256 = "TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256"
+	TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256   = "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256"
 )
 
 var (
-	TLSCipherSuites = map[string]TLSCipherSuite{
+	ConsulAgentTLSCipherSuites = map[string]TLSCipherSuite{
+		// "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256": TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+		"TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA":    TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+		"TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256": TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+		"TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA":    TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+		"TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384": TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+
+		// "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256": TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+		"TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA":    TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+		"TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256": TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		"TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA":    TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+		"TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384": TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+
+		"TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256": TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
+		"TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256":   TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
+	}
+	EnvoyTLSCipherSuites = map[string]TLSCipherSuite{
 		"TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256": TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
 		"TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA":          TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
 		"TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256":       TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 		"TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA":          TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
 		"TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384":       TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-		"TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256":   TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
-		"TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA":            TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-		"TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256":         TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-		"TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA":            TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-		"TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384":         TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+
+		"TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256": TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+		"TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA":          TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+		"TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256":       TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		"TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA":          TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+		"TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384":       TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 
 		"TLS_RSA_WITH_AES_128_GCM_SHA256": TLS_RSA_WITH_AES_128_GCM_SHA256,
 		"TLS_RSA_WITH_AES_128_CBC_SHA":    TLS_RSA_WITH_AES_128_CBC_SHA,
 		"TLS_RSA_WITH_AES_256_GCM_SHA384": TLS_RSA_WITH_AES_256_GCM_SHA384,
 		"TLS_RSA_WITH_AES_256_CBC_SHA":    TLS_RSA_WITH_AES_256_CBC_SHA,
-
-		"TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256": TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
-		"TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256":   TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
 	}
-	HumanTLSCipherSuiteStrings = func() map[TLSCipherSuite]string {
-		inverted := make(map[TLSCipherSuite]string, len(TLSCipherSuites))
-		for k, v := range TLSCipherSuites {
-			inverted[v] = k
-		}
-		return inverted
-	}()
 	EnvoyTLSCipherSuiteStrings = map[TLSCipherSuite]string{
 		TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256:       "ECDHE-ECDSA-AES128-GCM-SHA256",
 		TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256: "ECDHE-ECDSA-CHACHA20-POLY1305",
