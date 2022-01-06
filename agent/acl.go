c3a827035b5d8c2be1332f578c5a4fb50984fbb8
Blake Covarrubias <blake@covarrubi.as>
2022-01-06 12:38:37 -0800

    api: Return 404 when deregistering a non-existent check (#11950)
    Update the `/agent/check/deregister/` API endpoint to return a 404
    HTTP response code when an attempt is made to de-register a check ID
    that does not exist on the agent.
    
    This brings the behavior of /agent/check/deregister/ in line with the
    behavior of /agent/service/deregister/ which was changed in #10632 to
    similarly return a 404 when de-registering non-existent services.
    
    Fixes #5821

--- agent/acl.go
+++ agent/acl.go
@@ -143,7 +143,9 @@ func (a *Agent) vetCheckUpdateWithAuthorizer(authz acl.Authorizer, checkID struc
 			}
 		}
 	} else {
-		return fmt.Errorf("Unknown check %q", checkID.String())
+		return NotFoundError{Reason: fmt.Sprintf(
+			"Unknown check ID %q. Ensure that the check ID is passed, not the check name.",
+			checkID.String())}
 	}
 
 	return nil
