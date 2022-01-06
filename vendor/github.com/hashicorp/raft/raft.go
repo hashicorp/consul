472f4ff6c8c606ba4f32edc7492df088fd8eeedd
Dhia Ayachi <dhia@hashicorp.com>
2022-01-06 14:48:52 -0500

    fix vendors after upgrading raft

--- vendor/github.com/hashicorp/raft/raft.go
+++ vendor/github.com/hashicorp/raft/raft.go
@@ -214,15 +214,13 @@ func (r *Raft) runFollower() {
 				}
 			} else {
 				metrics.IncrCounter([]string{"raft", "transition", "heartbeat_timeout"}, 1)
-				if inConfig(r.configurations.latest, r.localID) {
+				if hasVote(r.configurations.latest, r.localID) {
 					r.logger.Warn("heartbeat timeout reached, starting election", "last-leader", lastLeader)
 					r.setState(Candidate)
 					return
-				} else {
-					if !didWarn {
-						r.logger.Warn("heartbeat timeout reached, not part of stable configuration, not triggering a leader election")
-						didWarn = true
-					}
+				} else if !didWarn {
+					r.logger.Warn("heartbeat timeout reached, not part of a stable configuration or a non-voter, not triggering a leader election")
+					didWarn = true
 				}
 			}
 
