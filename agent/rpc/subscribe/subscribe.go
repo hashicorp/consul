f29769eb77473e056475e83878da52ddd2c2c0dc
Daniel Upton <daniel@floppy.co>
2022-01-11 16:37:54 +0000

    streaming: remove support for empty `Key`
    This is a pre-requisite for splitting the internal topic buffer by key.
    
    While we technically could support subscribing to all of a topic's
    events by passing an empty key, it would require publishing events on
    multiple buffers (a key-specific and a global buffer) and as there isn't
    a compelling real world use-case for this, it doesn't seem worth the
    extra complexity.

--- agent/rpc/subscribe/subscribe.go
+++ agent/rpc/subscribe/subscribe.go
@@ -57,6 +57,10 @@ func (h *Server) Subscribe(req *pbsubscribe.SubscribeRequest, serverStream pbsub
 		return err
 	}
 
+	if req.Key == "" {
+		return status.Error(codes.InvalidArgument, "Key is required")
+	}
+
 	sub, err := h.Backend.Subscribe(toStreamSubscribeRequest(req, entMeta))
 	if err != nil {
 		return err
--- agent/rpc/subscribe/subscribe_test.go
+++ agent/rpc/subscribe/subscribe_test.go
@@ -30,6 +30,35 @@ import (
 	"github.com/hashicorp/consul/types"
 )
 
+func TestServer_Subscribe_KeyIsRequired(t *testing.T) {
+	require := require.New(t)
+
+	backend, err := newTestBackend()
+	require.NoError(err)
+
+	addr := runTestServer(t, NewServer(backend, hclog.New(nil)))
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	t.Cleanup(cancel)
+
+	conn, err := gogrpc.DialContext(ctx, addr.String(), gogrpc.WithInsecure())
+	require.NoError(err)
+	t.Cleanup(logError(t, conn.Close))
+
+	client := pbsubscribe.NewStateChangeSubscriptionClient(conn)
+
+	stream, err := client.Subscribe(ctx, &pbsubscribe.SubscribeRequest{
+		Topic: pbsubscribe.Topic_ServiceHealth,
+		Key:   "",
+	})
+	require.NoError(err)
+
+	_, err = stream.Recv()
+	require.Error(err)
+	require.Equal(codes.InvalidArgument.String(), status.Code(err).String())
+	require.Contains(err.Error(), "Key is required")
+}
+
 func TestServer_Subscribe_IntegrationWithBackend(t *testing.T) {
 	backend, err := newTestBackend()
 	require.NoError(t, err)
@@ -878,6 +907,8 @@ func assertNoEvents(t *testing.T, chEvents chan eventOrError) {
 
 func logError(t *testing.T, f func() error) func() {
 	return func() {
+		t.Helper()
+
 		if err := f(); err != nil {
 			t.Logf(err.Error())
 		}
