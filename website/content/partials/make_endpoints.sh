41529cdfab7eea5a1d3418658e12db0d7afcb2e3
Mark Anderson <manderson@hashicorp.com>
2022-01-26 20:30:00 -0800

    Updates for review feedback
    Signed-off-by: Mark Anderson <manderson@hashicorp.com>

--- website/content/partials/make_endpoints.sh
+++ website/content/partials/make_endpoints.sh
@@ -1,18 +1,25 @@
 TSV_IN="API_ENDPOINTS.tsv"
 HEADER="endpoint_header.mdx"
-MASTER="endpoints_master.mdx"
+ALL="endpoints_all.mdx"
 
 generate_file() {
-    (cat $HEADER; grep $1:$2 $MASTER) > endpoints_$1_$2.mdx
+  case ""$2"" in
+  "read")
+    (cat $HEADER; pcregrep $1:$2 $ALL) > endpoints_$1_$2.mdx
+    ;;
+  "write")
+    (cat $HEADER; pcregrep $1':(read|list|write)' $ALL)  > endpoints_$1_$2.mdx
+    ;;
+  esac
 }
 
-awk -F"\t" 'BEGIN{OFS="|"}{print $1, "[\["$3"\] " $2"]("$4")", $7, $8 ;}' > $MASTER < "$TSV_IN"
+awk -F"\t" 'BEGIN{OFS="|"}{print $1, "[\["$3"\] " $2"]("$4")", $7, $8 ;}' > $ALL < "$TSV_IN"
 
 for RESOURCE in acl agent event intentions key keyring node operator query service session
 do
     # todo add list
-    for ACCESS in read write 
+    for ACCESS in read write
     do
-	generate_file $RESOURCE $ACCESS 
+	generate_file $RESOURCE $ACCESS
     done
 done
