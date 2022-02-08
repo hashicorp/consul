8029cfd74ac0d0fb503e3e7a3a6d3e6406f15b06
Dhia Ayachi <dhia@hashicorp.com>
2022-02-08 14:00:24 -0500

    fix linux vs windows syscall

---
+++ agent/config/watcher_unix.go
@@ -0,0 +1,29 @@
+//go:build !windows
+// +build !windows
+
+package config
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+)
+
+func (w Watcher) getInode(filename string) (uint64, error) {
+	realFilename := filename
+	if linkedFile, err := os.Readlink(filename); err == nil {
+		realFilename = linkedFile
+	}
+	fileInfo, err := os.Stat(realFilename)
+	if err != nil {
+		return 0, err
+	}
+
+	stat, ok := fileInfo.Sys().(*syscall.Stat_t)
+	if !ok {
+		return 0, fmt.Errorf("not a syscall.Stat_t %v", fileInfo.Sys())
+	}
+
+	w.logger.Info("read inode ", "inode", stat.Ino)
+	return stat.Ino, nil
+}
