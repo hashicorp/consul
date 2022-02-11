c1b4e33deedbf8bf4cba0952da25e9dda7ad8895
John Cowen <johncowen@users.noreply.github.com>
2022-02-11 09:52:27 +0000

    ui: Exclude Service Health from Node listing page (#12248)
    This commit excludes the health of any service instances from the Node Listing page. This means that if you are viewing the Node listing page you will only see failing nodes if there are any Node Checks failing, Service Instance Health checks are no longer taken into account.
    
    Co-authored-by: Jamie White <jamie@jgwhite.co.uk>

--- ui/packages/consul-ui/app/models/node.js
+++ ui/packages/consul-ui/app/models/node.js
@@ -31,7 +31,9 @@ export default class Node extends Model {
   // ProxyServiceInstances are all instances that are connect-proxies
   @filter('Services', item => item.Service.Kind === 'connect-proxy') ProxyServiceInstances;
 
-  @computed('Checks.[]', 'ChecksCritical', 'ChecksPassing', 'ChecksWarning')
+  @filter('Checks', item => item.ServiceID === '') NodeChecks;
+
+  @computed('ChecksCritical', 'ChecksPassing', 'ChecksWarning')
   get Status() {
     switch (true) {
       case this.ChecksCritical !== 0:
@@ -45,18 +47,18 @@ export default class Node extends Model {
     }
   }
 
-  @computed('Checks.[]')
+  @computed('NodeChecks.[]')
   get ChecksCritical() {
-    return this.Checks.filter(item => item.Status === 'critical').length;
+    return this.NodeChecks.filter(item => item.Status === 'critical').length;
   }
 
-  @computed('Checks.[]')
+  @computed('NodeChecks.[]')
   get ChecksPassing() {
-    return this.Checks.filter(item => item.Status === 'passing').length;
+    return this.NodeChecks.filter(item => item.Status === 'passing').length;
   }
 
-  @computed('Checks.[]')
+  @computed('NodeChecks.[]')
   get ChecksWarning() {
-    return this.Checks.filter(item => item.Status === 'warning').length;
+    return this.NodeChecks.filter(item => item.Status === 'warning').length;
   }
 }
