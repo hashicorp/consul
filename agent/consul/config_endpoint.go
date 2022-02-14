814b93af562f02d20ff9787fbc17eb0c6102710f
R.B. Boyer <4903+rboyer@users.noreply.github.com>
2022-02-14 13:46:03 -0600

    Update agent/consul/config_endpoint.go
    Co-authored-by: Chris S. Kim <ckim@hashicorp.com>

--- agent/consul/config_endpoint.go
+++ agent/consul/config_endpoint.go
@@ -113,9 +113,11 @@ func (c *ConfigEntry) Apply(args *structs.ConfigEntryRequest, reply *bool) error
 	return nil
 }
 
-// shouldSkipOperation detects if the result of the operation has already
-// happened and skips it. It is ok if this incorrectly detects something as
-// changed when it in fact has not, the important thing is that it doesn't do
+// shouldSkipOperation returns true if the result of the operation has 
+// already happened and is safe to skip. 
+// 
+// It is ok if this incorrectly detects something as changed when it 
+// in fact has not, the important thing is that it doesn't do
 // the reverse and incorrectly detect a change as a no-op.
 func (c *ConfigEntry) shouldSkipOperation(args *structs.ConfigEntryRequest) (bool, error) {
 	state := c.srv.fsm.State()
