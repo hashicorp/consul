5f43bbfa994a0962d757b2c58e8e5df6b59ccad6
Dhia Ayachi <dhia@hashicorp.com>
2022-02-18 15:50:48 -0500

    back date file instead of sleeping

--- agent/config/file_watcher_test.go
+++ agent/config/file_watcher_test.go
@@ -154,9 +154,9 @@ func TestEventWatcherMove(t *testing.T) {
 
 func TestEventReconcileMove(t *testing.T) {
 	filepath := createTempConfigFile(t, "temp_config1")
-	//make sure mod time is different between the 2 files.
-	time.Sleep(10 * time.Millisecond)
 	filepath2 := createTempConfigFile(t, "temp_config2")
+	err := os.Chtimes(filepath, time.Now(), time.Now().Add(-1*time.Second))
+	require.NoError(t, err)
 	w, err := NewFileWatcher([]string{filepath}, hclog.New(&hclog.LoggerOptions{}))
 	require.NoError(t, err)
 	w.Start(context.Background())
