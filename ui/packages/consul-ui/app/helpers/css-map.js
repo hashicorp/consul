12da9957dc9ceaf6e22b83f626314cc66052e69f
John Cowen <jcowen@hashicorp.com>
2022-02-25 13:01:23 +0000

    Add css-map helper

---
+++ ui/packages/consul-ui/app/helpers/css-map.js
@@ -0,0 +1,16 @@
+import { helper } from '@ember/component/helper';
+import { CSSResult } from '@lit/reactive-element';
+
+/**
+ * Conditionally maps cssInfos to an array ready for ShadowDom::styles
+ * usage.
+ *
+ * @typedef {([CSSResult, boolean] | [CSSResult])} cssInfo
+ * @param {(cssInfo | string)[]} entries - An array of 'entry-like' arrays of `cssInfo`s to map
+ */
+const cssMap = entries => {
+  return entries
+    .filter(entry => (entry instanceof CSSResult ? true : entry[entry.length - 1]))
+    .map(entry => (entry instanceof CSSResult ? entry : entry[0]))
+};
+export default helper(cssMap);
