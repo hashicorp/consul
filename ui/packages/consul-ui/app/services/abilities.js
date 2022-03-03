62c95888c5df1a3aef26ba6e413efff16b70482c
John Cowen <jcowen@hashicorp.com>
2022-03-03 11:05:09 +0000

    can service > abilities service, backwards incompat change of classname

--- ui/packages/consul-ui/app/services/abilities.js
+++ ui/packages/consul-ui/app/services/abilities.js
@@ -1,6 +1,6 @@
 import Service from 'ember-can/services/can';
 
-export default class CanService extends Service {
+export default class AbilitiesService extends Service {
   parse(str) {
     // It's nicer to talk about SSO but technically its part of the authMethod
     // ability, we probably only need 'use SSO' but if we need more, reassess
