0e122479fab92e359e0e2976708f970c95dd90aa
John Cowen <johncowen@users.noreply.github.com>
2022-03-09 09:03:15 +0000

    ui: Add license endpoint/datasource (#12506)
    * ui: Add auto-pilot/state endpoint usage (merged into DC models) (#12514)
    
    * ui: Catalog Health Overview DataSource (#12520)

---
+++ ui/packages/consul-ui/app/models/license.js
@@ -0,0 +1,18 @@
+import Model, { attr } from '@ember-data/model';
+
+export const PRIMARY_KEY = 'uri';
+
+export default class License extends Model {
+  @attr('string') uri;
+  @attr('boolean') Valid;
+
+  @attr('number') SyncTime;
+  @attr() meta; // {}
+
+  @attr('string') Datacenter;
+  @attr('string') Namespace;
+  @attr('string') Partition;
+
+  @attr() License; // {}
+  // @attr() Warnings; // []
+}
