a09d6f1a40e430619ba2478133f7997b73a51e30
Mark Anderson <manderson@hashicorp.com>
2022-03-11 19:51:24 -0800

    add new entmeta stuff.
    Signed-off-by: Mark Anderson <manderson@hashicorp.com>

--- agent/structs/structs_oss.go
+++ agent/structs/structs_oss.go
@@ -4,150 +4,62 @@
 package structs
 
 import (
-	"hash"
-
 	"github.com/hashicorp/consul/acl"
 	"github.com/hashicorp/consul/types"
 )
 
-var emptyEnterpriseMeta = EnterpriseMeta{}
-
-// EnterpriseMeta stub
-type EnterpriseMeta struct{}
-
-func (m *EnterpriseMeta) ToEnterprisePolicyMeta() *acl.EnterprisePolicyMeta {
-	return nil
-}
-
-func (m *EnterpriseMeta) estimateSize() int {
-	return 0
-}
-
-func (m *EnterpriseMeta) addToHash(_ hash.Hash, _ bool) {
-	// do nothing
-}
-
-func (m *EnterpriseMeta) Merge(_ *EnterpriseMeta) {
-	// do nothing
-}
-
-func (m *EnterpriseMeta) MergeNoWildcard(_ *EnterpriseMeta) {
-	// do nothing
-}
-
-func (m *EnterpriseMeta) Matches(_ *EnterpriseMeta) bool {
-	return true
-}
-
-func (m *EnterpriseMeta) IsSame(_ *EnterpriseMeta) bool {
-	return true
-}
-
-func (m *EnterpriseMeta) LessThan(_ *EnterpriseMeta) bool {
-	return false
-}
-
-func (m *EnterpriseMeta) WithWildcardNamespace() *EnterpriseMeta {
-	return &emptyEnterpriseMeta
-}
-
-func (m *EnterpriseMeta) UnsetPartition() {
-	// do nothing
-}
+// TODO(acl-move-enterprise-meta) sync this with enterprise
+var emptyEnterpriseMeta = acl.EnterpriseMeta{}
 
 // TODO(partition): stop using this
-func NewEnterpriseMetaInDefaultPartition(_ string) EnterpriseMeta {
+func NewEnterpriseMetaInDefaultPartition(_ string) acl.EnterpriseMeta {
 	return emptyEnterpriseMeta
-}
-
-func NewEnterpriseMetaWithPartition(_, _ string) EnterpriseMeta {
-	return emptyEnterpriseMeta
-}
-
-func (m *EnterpriseMeta) NamespaceOrDefault() string {
-	return IntentionDefaultNamespace
-}
-
-func NamespaceOrDefault(_ string) string {
-	return IntentionDefaultNamespace
-}
-
-func (m *EnterpriseMeta) NamespaceOrEmpty() string {
-	return ""
-}
-
-func (m *EnterpriseMeta) InDefaultNamespace() bool {
-	return true
-}
-
-func (m *EnterpriseMeta) PartitionOrDefault() string {
-	return "default"
-}
-
-func EqualPartitions(_, _ string) bool {
-	return true
-}
-
-func IsDefaultPartition(partition string) bool {
-	return true
-}
-
-func PartitionOrDefault(_ string) string {
-	return "default"
-}
-
-func (m *EnterpriseMeta) PartitionOrEmpty() string {
-	return ""
-}
-
-func (m *EnterpriseMeta) InDefaultPartition() bool {
-	return true
 }
 
 // ReplicationEnterpriseMeta stub
-func ReplicationEnterpriseMeta() *EnterpriseMeta {
+func ReplicationEnterpriseMeta() *acl.EnterpriseMeta {
 	return &emptyEnterpriseMeta
 }
 
 // TODO(partition): stop using this
-func DefaultEnterpriseMetaInDefaultPartition() *EnterpriseMeta {
+func WildcardEnterpriseMetaInDefaultPartition() *acl.EnterpriseMeta {
+	return &emptyEnterpriseMeta
+}
+
+// TODO(partition): stop using this
+func DefaultEnterpriseMetaInDefaultPartition() *acl.EnterpriseMeta {
 	return &emptyEnterpriseMeta
 }
 
 // DefaultEnterpriseMetaInPartition stub
-func DefaultEnterpriseMetaInPartition(_ string) *EnterpriseMeta {
-	return &emptyEnterpriseMeta
-}
-
-func NodeEnterpriseMetaInPartition(_ string) *EnterpriseMeta {
-	return &emptyEnterpriseMeta
-}
-
-// TODO(partition): stop using this
-func NodeEnterpriseMetaInDefaultPartition() *EnterpriseMeta {
-	return &emptyEnterpriseMeta
-}
-
-// TODO(partition): stop using this
-func WildcardEnterpriseMetaInDefaultPartition() *EnterpriseMeta {
+func DefaultEnterpriseMetaInPartition(_ string) *acl.EnterpriseMeta {
 	return &emptyEnterpriseMeta
 }
 
 // WildcardEnterpriseMetaInPartition stub
-func WildcardEnterpriseMetaInPartition(_ string) *EnterpriseMeta {
+func WildcardEnterpriseMetaInPartition(_ string) *acl.EnterpriseMeta {
+	return &emptyEnterpriseMeta
+}
+
+func NewEnterpriseMetaWithPartition(_, _ string) acl.EnterpriseMeta {
+	return emptyEnterpriseMeta
+}
+
+func NodeEnterpriseMetaInPartition(_ string) *acl.EnterpriseMeta {
+	return &emptyEnterpriseMeta
+}
+
+// TODO(partition): stop using this
+func NodeEnterpriseMetaInDefaultPartition() *acl.EnterpriseMeta {
 	return &emptyEnterpriseMeta
 }
 
 // FillAuthzContext stub
-func (_ *EnterpriseMeta) FillAuthzContext(_ *acl.AuthorizerContext) {}
-
 func (_ *Node) FillAuthzContext(_ *acl.AuthorizerContext) {}
 
 func (_ *Coordinate) FillAuthzContext(_ *acl.AuthorizerContext) {}
 
 func (_ *NodeInfo) FillAuthzContext(_ *acl.AuthorizerContext) {}
-
-func (_ *EnterpriseMeta) Normalize() {}
 
 // FillAuthzContext stub
 func (_ *DirEntry) FillAuthzContext(_ *acl.AuthorizerContext) {}
@@ -155,7 +67,7 @@ func (_ *DirEntry) FillAuthzContext(_ *acl.AuthorizerContext) {}
 // FillAuthzContext stub
 func (_ *RegisterRequest) FillAuthzContext(_ *acl.AuthorizerContext) {}
 
-func (_ *RegisterRequest) GetEnterpriseMeta() *EnterpriseMeta {
+func (_ *RegisterRequest) GetEnterpriseMeta() *acl.EnterpriseMeta {
 	return nil
 }
 
@@ -168,15 +80,15 @@ func (_ *TxnServiceOp) FillAuthzContext(_ *acl.AuthorizerContext) {}
 // OSS Stub
 func (_ *TxnCheckOp) FillAuthzContext(_ *acl.AuthorizerContext) {}
 
-func NodeNameString(node string, _ *EnterpriseMeta) string {
+func NodeNameString(node string, _ *acl.EnterpriseMeta) string {
 	return node
 }
 
-func ServiceIDString(id string, _ *EnterpriseMeta) string {
+func ServiceIDString(id string, _ *acl.EnterpriseMeta) string {
 	return id
 }
 
-func ParseServiceIDString(input string) (string, *EnterpriseMeta) {
+func ParseServiceIDString(input string) (string, *acl.EnterpriseMeta) {
 	return input, DefaultEnterpriseMetaInDefaultPartition()
 }
 
@@ -189,7 +101,7 @@ func ServiceIDFromString(input string) ServiceID {
 	return ServiceID{ID: id}
 }
 
-func ParseServiceNameString(input string) (string, *EnterpriseMeta) {
+func ParseServiceNameString(input string) (string, *acl.EnterpriseMeta) {
 	return input, DefaultEnterpriseMetaInDefaultPartition()
 }
 
--- agent/structs/structs_oss_test.go
+++ agent/structs/structs_oss_test.go
@@ -8,39 +8,41 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+
+	"github.com/hashicorp/consul/acl"
 )
 
 var enterpriseMetaField = "EnterpriseMeta"
 
 func TestServiceID_String(t *testing.T) {
 	t.Run("value", func(t *testing.T) {
-		sid := NewServiceID("the-id", &EnterpriseMeta{})
+		sid := NewServiceID("the-id", &acl.EnterpriseMeta{})
 		require.Equal(t, "the-id", fmt.Sprintf("%v", sid))
 	})
 	t.Run("pointer", func(t *testing.T) {
-		sid := NewServiceID("the-id", &EnterpriseMeta{})
+		sid := NewServiceID("the-id", &acl.EnterpriseMeta{})
 		require.Equal(t, "the-id", fmt.Sprintf("%v", &sid))
 	})
 }
 
 func TestCheckID_String(t *testing.T) {
 	t.Run("value", func(t *testing.T) {
-		cid := NewCheckID("the-id", &EnterpriseMeta{})
+		cid := NewCheckID("the-id", &acl.EnterpriseMeta{})
 		require.Equal(t, "the-id", fmt.Sprintf("%v", cid))
 	})
 	t.Run("pointer", func(t *testing.T) {
-		cid := NewCheckID("the-id", &EnterpriseMeta{})
+		cid := NewCheckID("the-id", &acl.EnterpriseMeta{})
 		require.Equal(t, "the-id", fmt.Sprintf("%v", &cid))
 	})
 }
 
 func TestServiceName_String(t *testing.T) {
 	t.Run("value", func(t *testing.T) {
-		sn := NewServiceName("the-id", &EnterpriseMeta{})
+		sn := NewServiceName("the-id", &acl.EnterpriseMeta{})
 		require.Equal(t, "the-id", fmt.Sprintf("%v", sn))
 	})
 	t.Run("pointer", func(t *testing.T) {
-		sn := NewServiceName("the-id", &EnterpriseMeta{})
+		sn := NewServiceName("the-id", &acl.EnterpriseMeta{})
 		require.Equal(t, "the-id", fmt.Sprintf("%v", &sn))
 	})
 }
