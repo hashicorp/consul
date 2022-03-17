de5d781d5571b5b7bdbfb452a5e6a29af4b0e091
Dhia Ayachi <dhia@hashicorp.com>
2022-03-17 15:48:09 -0400

    remove redundant oss func declaration

--- proto/pbcommon/common_oss.go
+++ proto/pbcommon/common_oss.go
@@ -9,10 +9,6 @@ import (
 
 var DefaultEnterpriseMeta = EnterpriseMeta{}
 
-func EnterpriseMetaToStructs(_ *EnterpriseMeta) structs.EnterpriseMeta {
-	return *structs.DefaultEnterpriseMetaInDefaultPartition()
-}
-
 func NewEnterpriseMetaFromStructs(_ structs.EnterpriseMeta) *EnterpriseMeta {
 	return &EnterpriseMeta{}
 }
