5c425455c00f047a275aab1f67d0dce7e98b250b
R.B. Boyer <rb@hashicorp.com>
2022-03-29 13:07:51 -0500

    add missing line from prototest.AssertDeepEqual

--- proto/prototest/testing.go
+++ proto/prototest/testing.go
@@ -3,11 +3,15 @@ package prototest
 import (
 	"testing"
 
+	"github.com/golang/protobuf/proto"
 	"github.com/google/go-cmp/cmp"
 )
 
 func AssertDeepEqual(t *testing.T, x, y interface{}, opts ...cmp.Option) {
 	t.Helper()
+
+	opts = append(opts, cmp.Comparer(proto.Equal))
+
 	if diff := cmp.Diff(x, y, opts...); diff != "" {
 		t.Fatalf("assertion failed: values are not equal\n--- expected\n+++ actual\n%v", diff)
 	}
