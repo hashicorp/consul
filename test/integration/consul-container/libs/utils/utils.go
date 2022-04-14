d80998270fb31186a70f2edfd0e2dae752eabfc3
Dhia Ayachi <dhia@hashicorp.com>
2022-04-14 09:45:41 -0400

    use uuid instead of random string

--- test/integration/consul-container/libs/utils/utils.go
+++ test/integration/consul-container/libs/utils/utils.go
@@ -1,21 +1,13 @@
 package utils
 
 import (
-	"math/rand"
-	"time"
+	"github.com/hashicorp/go-uuid"
 )
 
 func RandName(name string) string {
-	return name + randomStr(20)
-}
-func randomStr(length int) string {
-	const charset = "abcdefghijklmnopqrstuvwxyz" +
-		"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	var seededRand = rand.New(
-		rand.NewSource(time.Now().UnixNano()))
-	b := make([]byte, length)
-	for i := range b {
-		b[i] = charset[seededRand.Intn(len(charset))]
+	generateUUID, err := uuid.GenerateUUID()
+	if err != nil {
+		return ""
 	}
-	return string(b)
+	return name + generateUUID
 }
