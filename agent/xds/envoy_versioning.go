94c3bd581ddc02e6dd6678b99a663ac9fbb4a03d
Evan Culver <eculver@hashicorp.com>
2022-04-15 13:00:25 -0700

    Remove unnecessary version constraint

--- agent/xds/envoy_versioning.go
+++ agent/xds/envoy_versioning.go
@@ -13,8 +13,6 @@ var (
 	// the zero'th point release of the last element of proxysupport.EnvoyVersions.
 	minSupportedVersion = version.Must(version.NewVersion("1.19.0"))
 
-	minVersionToForceLDSandCDSToAlwaysUseWildcardsOnReconnect = version.Must(version.NewVersion("1.19.0"))
-
 	specificUnsupportedVersions = []unsupportedVersion{}
 )
 
