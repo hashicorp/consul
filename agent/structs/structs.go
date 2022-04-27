e31e1a2f2b0311f453ae75dacca903e9a5c976c5
R.B. Boyer <4903+rboyer@users.noreply.github.com>
2022-04-27 10:39:45 -0500

    [1.11.x] health: ensure /v1/health/service/:service endpoint returns the most recent results when a filter is used with streaming
    Backport of #12640 to 1.11.x

--- agent/structs/structs.go
+++ agent/structs/structs.go
@@ -355,6 +355,8 @@ const (
 	QueryBackendStreaming
 )
 
+func (q QueryBackend) GoString() string { return q.String() }
+
 func (q QueryBackend) String() string {
 	switch q {
 	case QueryBackendBlocking:
