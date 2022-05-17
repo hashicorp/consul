5f8fb0eccb16d30c9ff4b57670984362ba6982ce
Hui Kang <hui.kang@hashicorp.com>
2022-05-17 18:18:39 +0000

    backport of commit 93cf872574f18e65b185477331750c510dc8ad00

--- lib/telemetry_test.go
+++ lib/telemetry_test.go
@@ -1,9 +1,40 @@
 package lib
 
 import (
+	"os"
 	"testing"
+
+	"github.com/hashicorp/consul/logging"
+	"github.com/stretchr/testify/require"
 )
 
-func TestInitTelemetry(t *testing.T) {
-	// TODO: add test cases for init telemetry sink
+func newCfg() TelemetryConfig {
+	cfg := TelemetryConfig{
+		StatsdAddr:    "statsd.host:1234",
+		StatsiteAddr:  "statsite.host:1234",
+		DogstatsdAddr: "mydog.host:8125",
+	}
+	return cfg
+}
+
+func TestConfigureSinks(t *testing.T) {
+	cfg := newCfg()
+	sinks, err := configureSinks(cfg, "hostname", nil)
+	require.Error(t, err)
+	// 3 sinks: statsd, statsite, inmem
+	require.Equal(t, 3, len(sinks))
+}
+
+func TestInitTelemetryRetrySuccess(t *testing.T) {
+	logger, err := logging.Setup(logging.Config{
+		LogLevel: "INFO",
+	}, os.Stdout)
+	require.NoError(t, err)
+	cfg := newCfg()
+	_, err = InitTelemetry(cfg, logger)
+	require.Error(t, err)
+
+	// TODO: we couldn't extract the metrics sinks from the
+	// global metrics due to it's limitation
+	// fanoutSink := metrics.Default()}
 }
