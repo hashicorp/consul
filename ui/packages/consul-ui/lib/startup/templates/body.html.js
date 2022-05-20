f46140b93f9298aefe9688e18abb4e66ff6ae2d1
John Cowen <jcowen@hashicorp.com>
2022-05-20 11:02:05 +0100

    Alter the frontend varname case to match the backend

--- ui/packages/consul-ui/lib/startup/templates/body.html.js
+++ ui/packages/consul-ui/lib/startup/templates/body.html.js
@@ -66,7 +66,7 @@ ${
 {{if .NamespacesEnabled}}
   <script src="${rootURL}assets/consul-nspaces/routes.js"></script>
 {{end}}
-{{if .HcpEnabled}}
+{{if .HCPEnabled}}
   <script src="${rootURL}assets/consul-hcp/routes.js"></script>
 {{end}}
 `
