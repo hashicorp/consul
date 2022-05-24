87b69872f8754a040c0676869eb541bf2f56cb78
cskh <hui.kang@hashicorp.com>
2022-05-24 15:28:35 -0400

    Revert "* fix: non-leader agents return 404 on Get Intention exact api"
    This reverts commit 3b1f9277ab5eff37afe5a30fe6aef5dfac3a391b.

--- agent/intentions_endpoint_test.go
+++ agent/intentions_endpoint_test.go
@@ -1,7 +1,6 @@
 package agent
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -312,17 +311,6 @@ func TestIntentionCheck(t *testing.T) {
 		value := obj.(*structs.IntentionQueryCheckResponse)
 		require.True(t, value.Allowed)
 	})
-}
-
-type testSrv struct {
-	delegate
-}
-
-func (s *testSrv) RPC(method string, args interface{}, reply interface{}) error {
-	return fmt.Errorf("rpc error making call: %w", errors.New("Intention not found"))
-}
-func (s *testSrv) Shutdown() error {
-	return nil
 }
 
 func TestIntentionPutExact(t *testing.T) {
