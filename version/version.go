1867b4af2898e000638b0370ec0b48626f03fdde
Evan Culver <eculver@hashicorp.com>
2022-05-25 10:19:34 -0700

    Reset dev version suffix

--- version/version.go
+++ version/version.go
@@ -22,7 +22,7 @@ var (
 	// A pre-release marker for the version. If this is "" (empty string)
 	// then it means that it is a final release. Otherwise, this is a pre-release
 	// such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = ""
+	VersionPrerelease = "dev"
 )
 
 // GetHumanVersion composes the parts of the version in a way that's suitable
