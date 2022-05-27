428e32706e3135eebfa49ee7a418efa5c2156741
Mathew Estafanous <56979977+Mathew-Estafanous@users.noreply.github.com>
2022-05-27 17:33:27 +0200

    Replace CLI command registry with a new pattern. (#12729)

---
+++ command/registry_oss.go
@@ -0,0 +1,8 @@
+package command
+
+import (
+	"github.com/hashicorp/consul/command/cli"
+	mcli "github.com/mitchellh/cli"
+)
+
+func registerEnterpriseCommands(_ cli.Ui, _ map[string]mcli.CommandFactory) {}
