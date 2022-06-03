019aeaa57de97100d57f8ab9c771365b1cfcf7b8
R.B. Boyer <4903+rboyer@users.noreply.github.com>
2022-06-03 16:42:50 -0500

    peering: update how cross-peer upstreams and represented in proxycfg and rendered in xds (#13362)
    This removes unnecessary, vestigal remnants of discovery chains.

--- agent/rpc/peering/subscription_manager.go
+++ agent/rpc/peering/subscription_manager.go
@@ -208,8 +208,17 @@ func (m *subscriptionManager) handleEvent(ctx context.Context, state *subscripti
 			Datacenter: m.config.Datacenter,
 			Service:    sn.Name,
 		}
+		sni := connect.PeeredServiceSNI(
+			sn.Name,
+			sn.NamespaceOrDefault(),
+			sn.PartitionOrDefault(),
+			state.peerName,
+			m.trustDomain,
+		)
 		peerMeta := &pbservice.PeeringServiceMeta{
+			SNI:      []string{sni},
 			SpiffeID: []string{spiffeID.URI().String()},
+			Protocol: "tcp",
 		}
 
 		// skip checks since we just generated one from scratch
