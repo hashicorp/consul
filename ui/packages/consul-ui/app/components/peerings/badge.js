522bf6030ddd3f90bcf4bd4ad29084a296f077b4
Michael Klein <michael@firstiwaslike.com>
2022-06-05 11:46:53 +0200

    Add tooltip Peerings:Badge

--- ui/packages/consul-ui/app/components/peerings/badge.js
+++ ui/packages/consul-ui/app/components/peerings/badge.js
@@ -8,14 +8,20 @@ const BADGE_LOOKUP = {
   INITIAL: {
     bg: 'bg-[#F9F2FF]',
     color: 'text-[#911CED]',
+    tooltip:
+      'A token has been generated for this peer, but has not yet been initiated in the remote peer.',
   },
   FAILING: {
     bg: 'bg-[#FFF5F5]',
     color: 'text-[#C00005]',
+    tooltip:
+      'This peering connection has some intermittent errors (usually network related). It will continue to retry. ',
   },
   TERMINATED: {
     bg: 'bg-[#F1F2F3]',
     color: 'text-[#3B3D45]',
+    tooltip:
+      'Someone in the other peer may have deleted this peering connection.',
   },
 };
 export default class PeeingsBadge extends Component {
@@ -33,5 +39,9 @@ export default class PeeingsBadge extends Component {
 
   get color() {
     return this.styles.color;
+  }
+
+  get tooltip() {
+    return this.styles.tooltip;
   }
 }
