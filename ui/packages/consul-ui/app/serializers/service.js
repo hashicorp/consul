5ae70bb1e1e52d85eeed8293d1174a94db931a44
Michael Klein <michael@firstiwaslike.com>
2022-06-16 10:55:34 +0200

    Use replace decorator instead of serializer for empty peerName

--- ui/packages/consul-ui/app/serializers/service.js
+++ ui/packages/consul-ui/app/serializers/service.js
@@ -36,12 +36,6 @@ export default class ServiceSerializer extends Serializer {
                 });
               }
             });
-          body.forEach(item => {
-            // the API sends an empty string which means there is no peering
-            if (item.PeerName === '') {
-              item.PeerName = undefined;
-            }
-          });
 
           return cb(headers, body);
         }),
