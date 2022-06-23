8bdf179c28600997aa41b3fc7b07165e89f14367
Michael Klein <michael@firstiwaslike.com>
2022-06-23 12:18:28 +0200

    Remove unused service import peers.index

--- ui/packages/consul-ui/app/routes/dc/peers/index.js
+++ ui/packages/consul-ui/app/routes/dc/peers/index.js
@@ -1,5 +1,4 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 
 export default class PeersRoute extends Route {
