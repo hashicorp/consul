c9fa1b579b4e6002cb320571f20e5b0f40086951
John Cowen <jcowen@hashicorp.com>
2022-06-29 15:02:40 +0100

    ui: Add peer searching and sorting
    Initial name search and sort only, more to come here

--- ui/packages/consul-ui/app/routes/dc/peers/index.js
+++ ui/packages/consul-ui/app/routes/dc/peers/index.js
@@ -1,10 +1,24 @@
-import Route from '@ember/routing/route';
+import Route from 'consul-ui/routing/route';
 import { action } from '@ember/object';
 
 export default class PeersRoute extends Route {
-  model() {
+  queryParams = {
+    sortBy: 'sort',
+    searchproperty: {
+      as: 'searchproperty',
+      empty: [['Name']],
+    },
+    search: {
+      as: 'filter',
+      replace: true,
+    },
+  };
+
+  async model() {
+    const parent = await super.model();
     return this.store.findAll('peer').then(peers => {
       return {
+        ...parent,
         peers,
         loadPeers: this.loadPeers,
       };
