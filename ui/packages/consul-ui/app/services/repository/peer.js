cbfbc9ad851a831f1b65f665f78a89683d9e084e
John Cowen <jcowen@hashicorp.com>
2022-06-30 13:23:37 +0100

    ui: Add peer repo with listing datasource

---
+++ ui/packages/consul-ui/app/services/repository/peer.js
@@ -0,0 +1,35 @@
+import RepositoryService from 'consul-ui/services/repository';
+import dataSource from 'consul-ui/decorators/data-source';
+
+export default class PeerService extends RepositoryService {
+
+  getModelName() {
+    return 'peer';
+  }
+
+  @dataSource('/:partition/:ns/:dc/peers')
+  async fetchAll({dc, ns, partition}, configuration, request) {
+    return (await request`
+      GET /v1/peerings?${{partition}}
+    `)(
+      (headers, body, cache) => {
+        return {
+          meta: {
+            version: 2,
+            interval: 1000,
+            // uri: uri,
+          },
+          body: body.map(item => {
+            return cache(
+              {
+                ...item,
+                Datacenter: '',
+                Partition: '',
+              },
+              uri => `peer:///${partition}/${ns}/${dc}/peer/${item.Name}`
+            );
+          })
+        };
+      });
+  }
+}
