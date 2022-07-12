0a3b755d6dbb9ea5473246feb433d86d875c6f15
John Cowen <jcowen@hashicorp.com>
2022-07-12 12:13:29 +0100

    ui: Add a modal.opened property for inspecting whether the modal is open

--- ui/packages/consul-ui/app/components/modal-dialog/index.js
+++ ui/packages/consul-ui/app/components/modal-dialog/index.js
@@ -1,4 +1,5 @@
 import Component from '@ember/component';
+import { set } from '@ember/object';
 import Slotted from 'block-slots';
 import A11yDialog from 'a11y-dialog';
 
@@ -6,13 +7,16 @@ export default Component.extend(Slotted, {
   tagName: '',
   onclose: function() {},
   onopen: function() {},
+  isOpen: false,
   actions: {
     connect: function($el) {
       this.dialog = new A11yDialog($el);
+      this.dialog.on('show', () => set(this, 'isOpen', true));
+      this.dialog.on('hide', () => set(this, 'isOpen', false));
       this.dialog.on('hide', () => this.onclose({ target: $el }));
       this.dialog.on('show', () => this.onopen({ target: $el }));
       if (this.open) {
-        this.dialog.show();
+        this.actions.open.apply(this, []);
       }
     },
     disconnect: function($el) {
