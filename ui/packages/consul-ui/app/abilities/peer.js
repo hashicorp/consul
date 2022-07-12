808c7e8651927b813a8e3c4aca3dc9074cb71065
Michael Klein <michael@firstiwaslike.com>
2022-07-12 15:30:46 +0200

    use super call in abilities.peer#canDelete

--- ui/packages/consul-ui/app/abilities/peer.js
+++ ui/packages/consul-ui/app/abilities/peer.js
@@ -12,7 +12,7 @@ export default class PeerAbility extends BaseAbility {
   }
   get canDelete() {
     // TODO: Need to confirm these states
-    return !['DELETING', 'TERMINATED', 'UNDEFINED'].includes(this.item.State) && this.canWrite;
+    return !['DELETING', 'TERMINATED', 'UNDEFINED'].includes(this.item.State) && super.canDelete;
   }
 
   get canUse() {
