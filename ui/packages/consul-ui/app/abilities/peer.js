75768a203906bfbf5cf8b5d14930f798d44e188b
Michael Klein <Michael@firstiwaslike.com>
2022-07-12 17:16:47 +0200

    ui: peer permission handling (#13724)
    * Request peering permissions when peerings is active
    
    * Update peering ability to use peering resource
    
    * fix canDelete peer permission to check write permission
    
    * use super call in abilities.peer#canDelete

--- ui/packages/consul-ui/app/abilities/peer.js
+++ ui/packages/consul-ui/app/abilities/peer.js
@@ -4,7 +4,7 @@ import { inject as service } from '@ember/service';
 export default class PeerAbility extends BaseAbility {
   @service('env') env;
 
-  resource = 'operator';
+  resource = 'peering';
   segmented = false;
 
   get isLinkable() {
@@ -12,7 +12,7 @@ export default class PeerAbility extends BaseAbility {
   }
   get canDelete() {
     // TODO: Need to confirm these states
-    return !['DELETING', 'TERMINATED', 'UNDEFINED'].includes(this.item.State);
+    return !['DELETING', 'TERMINATED', 'UNDEFINED'].includes(this.item.State) && super.canDelete;
   }
 
   get canUse() {
