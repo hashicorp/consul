a5dd10d183b5c27fea07f145c42c9b488b2087af
Daniel Upton <daniel@floppy.co>
2022-07-12 11:43:42 +0100

    proxycfg-glue: server-local implementation of `FederationStateListMeshGateways`
    This is the OSS portion of enterprise PR 2265.
    
    This PR provides a server-local implementation of the
    proxycfg.FederationStateListMeshGateways interface based on blocking queries.

--- agent/agent.go
+++ agent/agent.go
@@ -4247,6 +4247,7 @@ func (a *Agent) proxyDataSources() proxycfg.DataSources {
 		sources.ConfigEntry = proxycfgglue.ServerConfigEntry(deps)
 		sources.ConfigEntryList = proxycfgglue.ServerConfigEntryList(deps)
 		sources.CompiledDiscoveryChain = proxycfgglue.ServerCompiledDiscoveryChain(deps, proxycfgglue.CacheCompiledDiscoveryChain(a.cache))
+		sources.FederationStateListMeshGateways = proxycfgglue.ServerFederationStateListMeshGateways(deps)
 		sources.GatewayServices = proxycfgglue.ServerGatewayServices(deps)
 		sources.Health = proxycfgglue.ServerHealth(deps, proxycfgglue.ClientHealth(a.rpcClientHealth))
 		sources.Intentions = proxycfgglue.ServerIntentions(deps)
