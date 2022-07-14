3c0f20f238c7f96aa676b655f87f5c9d662dc0c7
Ezequiel Fernández Ponce <20102608+ezfepo@users.noreply.github.com>
2022-07-14 13:56:23 -0300

    [CONSUL-190] Support consul windows compilation in Docker (#14)

--- build-support-windows/build-images.sh
+++ build-support-windows/build-images.sh
@@ -7,23 +7,29 @@ export ENVOY_VERSION
 
 echo "Building Images"
 
+# Build Windows Consul Image
+docker build -t "windows/consul" -f ../Dockerfile-windows ../
+
+# Build Windows Consul-Dev Image
+./Dockerfile-consul-dev-windows.sh
+
 # Pull Windows Nanoserver image
-docker.exe pull mcr.microsoft.com/windows/nanoserver:1809
-# Re tag Pulled image
-docker.exe tag mcr.microsoft.com/windows/nanoserver:1809 "${HASHICORP_DOCKER_PROXY}/windows/nanoserver"
+docker pull mcr.microsoft.com/windows/nanoserver:1809
+# Tag Windows Nanoserver image
+docker tag mcr.microsoft.com/windows/nanoserver:1809 "${HASHICORP_DOCKER_PROXY}/windows/nanoserver"
 
-# Pull Envoy-Windows Image
-docker.exe pull "envoyproxy/envoy-windows:v${ENVOY_VERSION}"
-# Re tag Pulled image
-docker.exe tag "envoyproxy/envoy-windows:v${ENVOY_VERSION}" "${HASHICORP_DOCKER_PROXY}/windows/envoy-windows:v${ENVOY_VERSION}"
-
-# Build Fortio Windows Image
-docker.exe build -t "${HASHICORP_DOCKER_PROXY}/windows/fortio" -f Dockerfile-fortio-windows .
-
-# Build Socat-Windows Image
-docker.exe build -t "${HASHICORP_DOCKER_PROXY}/windows/socat" -f Dockerfile-socat-windows .
+# Pull Windows Envoy Image
+docker pull "envoyproxy/envoy-windows:v${ENVOY_VERSION}"
+# Tag Windows Envoy image
+docker tag "envoyproxy/envoy-windows:v${ENVOY_VERSION}" "${HASHICORP_DOCKER_PROXY}/windows/envoy-windows:v${ENVOY_VERSION}"
 
 # Build Bats-Core-Windows Image
-docker build -t "${HASHICORP_DOCKER_PROXY}/windows/bats:1.7.0" . -f Dockerfile-bats-core-windows
+docker build -t "${HASHICORP_DOCKER_PROXY}/windows/bats:1.7.0" -f Dockerfile-bats-core-windows .
+
+# Build Windows Fortio Image
+docker build -t "${HASHICORP_DOCKER_PROXY}/windows/fortio" -f Dockerfile-fortio-windows .
+
+# Build Windows Socat Image
+docker build -t "${HASHICORP_DOCKER_PROXY}/windows/socat" -f Dockerfile-socat-windows .
 
 echo "Building Complete!"
