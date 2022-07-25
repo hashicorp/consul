aeee2d1d1656dd7d1a442432f0a77ee4becb1641
Chris S. Kim <ckim@hashicorp.com>
2022-07-25 12:33:21 -0400

    Add error if PeeringState is not specified during write if there is an existing peering

--- agent/consul/state/peering.go
+++ agent/consul/state/peering.go
@@ -214,7 +214,7 @@ func (s *Store) PeeringWrite(idx uint64, p *pbpeering.Peering) error {
 		}
 
 		if p.State == 0 {
-			p.State = pbpeering.PeeringState_PENDING
+			return fmt.Errorf("missing peering state when updating existing peering")
 		}
 		// TODO(peering): Confirm behavior when /peering/token is called more than once.
 		// We may need to avoid clobbering existing values.
