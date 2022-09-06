c84527132d065ccb0b34d7bd9045adb82ba0c002
Michael Klein <michael@firstiwaslike.com>
2022-09-06 10:39:05 +0200

    Fix - fn needs to be passed function tab-nav
    We create functions in the component file instead
    so that fn-helper stops complaining about the
    need to pass a function.

--- ui/packages/consul-ui/app/components/tab-nav/index.js
+++ ui/packages/consul-ui/app/components/tab-nav/index.js
@@ -1,3 +1,12 @@
 import Component from '@glimmer/component';
 
-export default class TabNav extends Component {}
+function noop() {}
+export default class TabNav extends Component {
+  get onClick() {
+    return this.args.onclick || noop;
+  }
+
+  get onTabClicked() {
+    return this.args.onTabClicked || noop;
+  }
+}
