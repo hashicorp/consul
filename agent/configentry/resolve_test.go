e7c190bf6b0fbfad9026787df845eb72b3bee623
cskh <hui.kang@hashicorp.com>
2022-09-11 21:50:09 -0400

    resolve conflict

--- agent/configentry/resolve_test.go
+++ agent/configentry/resolve_test.go
@@ -44,6 +44,30 @@ func Test_ComputeResolvedServiceConfig(t *testing.T) {
 				},
 			},
 		},
+		{
+			name: "proxy with local_connect_timeout_ms and local_request_timeout_ms",
+			args: args{
+				scReq: &structs.ServiceConfigRequest{
+					Name: "sid",
+				},
+				entries: &ResolvedServiceConfigSet{
+					ServiceDefaults: map[structs.ServiceID]*structs.ServiceConfigEntry{
+						sid: {
+							MaxInboundConnections: 20,
+							LocalConnectTimeoutMs: 20000,
+							LocalRequestTimeoutMs: 30000,
+						},
+					},
+				},
+			},
+			want: &structs.ServiceConfigResponse{
+				ProxyConfig: map[string]interface{}{
+					"max_inbound_connections":  20,
+					"local_connect_timeout_ms": 20000,
+					"local_request_timeout_ms": 30000,
+				},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
