b598e8d6147a0ab542f5d71425ec3c639d852642
John Cowen <johncowen@users.noreply.github.com>
2022-09-12 10:59:26 +0100

    ui: Add initial peering listing acceptance tests (#14522)

---
+++ ui/packages/consul-ui/tests/pages/dc/peers/index.js
@@ -0,0 +1,7 @@
+export default function(visitable, creatable, items, popoverSelect) {
+  return creatable({
+    visit: visitable('/:dc/peers'),
+    peers: items(),
+    sort: popoverSelect('[data-test-sort-control]'),
+  });
+}
