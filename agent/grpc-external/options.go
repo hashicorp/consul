9467975ac8699f7b2bf7def25fda508970f8f2d8
Maliz <maliheh.monshizadeh@hashicorp.com>
2022-09-22 23:01:27 -0700

    add test for query options utility functions

--- agent/grpc-external/options.go
+++ agent/grpc-external/options.go
@@ -2,6 +2,7 @@ package external
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/hashicorp/consul/agent/structs"
 	"github.com/mitchellh/mapstructure"
@@ -14,15 +15,22 @@ func QueryOptionsFromContext(ctx context.Context) (structs.QueryOptions, error) 
 	options := structs.QueryOptions{}
 	md, ok := metadata.FromIncomingContext(ctx)
 	if !ok {
-		return options, nil
+		return options, fmt.Errorf("could not get metadata from context")
 	}
-	err := mapstructure.Decode(md, options)
+
+	m := map[string]string{}
+	for k, v := range md {
+		m[k] = v[0]
+	}
+
+	err := mapstructure.Decode(m, &options)
 	if err != nil {
 		return structs.QueryOptions{}, err
 	}
-	// toks, ok := md[metadataKeyToken]
-	// if ok && len(toks) > 0 {
-	// 	options.Token = toks[0]
+
+	// // toks, ok := md[metadataKeyToken]
+	// // if ok && len(toks) > 0 {
+	// // 	options.Token = toks[0]
 	// }
 	return options, nil
 }
@@ -30,9 +38,13 @@ func QueryOptionsFromContext(ctx context.Context) (structs.QueryOptions, error) 
 // ContextWithQueryOptions returns a context with the given query options attached.
 func ContextWithQueryOptions(ctx context.Context, options structs.QueryOptions) (context.Context, error) {
 	md := metadata.MD{}
-	err := mapstructure.Decode(options, md)
+	m := map[string]interface{}{}
+	err := mapstructure.Decode(options, &m)
 	if err != nil {
 		return nil, err
+	}
+	for k, v := range m {
+		md.Set(k, fmt.Sprintf("%v", v))
 	}
 	return metadata.NewOutgoingContext(ctx, md), nil
 }
