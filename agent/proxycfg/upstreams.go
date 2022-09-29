4ba260958c04323c133d86773e301b2ea6809b93
Ashwin Venkatesh <ashwin@hashicorp.com>
2022-09-29 13:19:04 -0400

    bug: watch local mesh gateways in non-default partitions with agentless (#14799)

--- agent/proxycfg/upstreams.go
+++ agent/proxycfg/upstreams.go
@@ -319,7 +319,7 @@ func (s *handlerUpstreams) resetWatchesFromChain(
 			}
 		case structs.MeshGatewayModeLocal:
 			gk = GatewayKey{
-				Partition:  s.source.NodePartitionOrDefault(),
+				Partition:  s.proxyID.PartitionOrDefault(),
 				Datacenter: s.source.Datacenter,
 			}
 		}
