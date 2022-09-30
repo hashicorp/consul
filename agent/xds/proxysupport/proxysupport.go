843fc233ce66d3f549ac08625555f864d58a2f26
Evan Culver <eculver@hashicorp.com>
2022-09-30 15:52:12 -0700

    connect: Bump Envoy 1.20 to 1.20.7, 1.21 to 1.21.5 and 1.22 to 1.22.5

--- agent/xds/proxysupport/proxysupport.go
+++ agent/xds/proxysupport/proxysupport.go
@@ -7,8 +7,8 @@ package proxysupport
 //
 // see: https://www.consul.io/docs/connect/proxies/envoy#supported-versions
 var EnvoyVersions = []string{
-	"1.22.2",
-	"1.21.4",
-	"1.20.6",
+	"1.22.5",
+	"1.21.5",
+	"1.20.7",
 	"1.19.5",
 }
