e2ca73ecbe3ab02ab730884f45aee8edd70d5703
Michael Klein <michael@firstiwaslike.com>
2022-10-05 12:06:55 +0200

    Bring back edit-route for peerings

--- ui/packages/consul-peerings/vendor/consul-peerings/routes.js
+++ ui/packages/consul-peerings/vendor/consul-peerings/routes.js
@@ -1,30 +1,46 @@
-(routes => routes({
-  dc: {
-    peers: {
-      _options: { 
-        path: '/peers'
-      },
-      index: {
+((routes) =>
+  routes({
+    dc: {
+      peers: {
         _options: {
-          path: '/',
-          queryParams: {
-            sortBy: 'sort',
-            state: 'state',
-            searchproperty: {
-              as: 'searchproperty',
-              empty: [['Name', 'ID']],
+          path: "/peers",
+        },
+        index: {
+          _options: {
+            path: "/",
+            queryParams: {
+              sortBy: "sort",
+              state: "state",
+              searchproperty: {
+                as: "searchproperty",
+                empty: [["Name", "ID"]],
+              },
+              search: {
+                as: "filter",
+                replace: true,
+              },
             },
-            search: {
-              as: 'filter',
-              replace: true,
+          },
+        },
+        edit: {
+          _options: {
+            path: "/:name",
+          },
+          addresses: {
+            _options: {
+              path: "/addresses",
             },
           },
         },
       },
     },
-  },
-}))(
-  (json, data = (typeof document !== 'undefined' ? document.currentScript.dataset : module.exports)) => {
+  }))(
+  (
+    json,
+    data = typeof document !== "undefined"
+      ? document.currentScript.dataset
+      : module.exports
+  ) => {
     data[`routes`] = JSON.stringify(json);
   }
 );
