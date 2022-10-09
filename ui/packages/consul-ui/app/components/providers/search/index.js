79565760191ccdddf24e156ddf75d9e44ffd6dee
Michael Klein <michael@firstiwaslike.com>
2022-10-09 13:00:22 +0200

    Return all items without search term exported

--- ui/packages/consul-ui/app/components/providers/search/index.js
+++ ui/packages/consul-ui/app/components/providers/search/index.js
@@ -4,19 +4,23 @@ export default class SearchProvider extends Component {
   get items() {
     const { items, search, searchProperties } = this.args;
 
-    const regex = new RegExp(`${search}`, 'ig');
+    if (search.length > 0) {
+      const regex = new RegExp(`${search}`, 'ig');
 
-    return items.filter((item) => {
-      const matchesInSearchProperties = searchProperties.reduce((acc, searchProperty) => {
-        const match = item[searchProperty].match(regex);
-        if (match) {
-          return [...acc, match];
-        } else {
-          return acc;
-        }
-      }, []);
-      return matchesInSearchProperties.length > 0;
-    });
+      return items.filter((item) => {
+        const matchesInSearchProperties = searchProperties.reduce((acc, searchProperty) => {
+          const match = item[searchProperty].match(regex);
+          if (match) {
+            return [...acc, match];
+          } else {
+            return acc;
+          }
+        }, []);
+        return matchesInSearchProperties.length > 0;
+      });
+    } else {
+      return items;
+    }
   }
 
   get data() {
