f530b5d172f0742f5e89d4a95a2620d321adfde2
Daniel Upton <daniel@floppy.co>
2022-10-11 18:06:01 +0100

    Fix flaky test

--- agent/proxycfg/deep-copy.sh
+++ agent/proxycfg/deep-copy.sh
@@ -5,9 +5,10 @@ deep-copy -pointer-receiver \
   -o ./proxycfg.deepcopy.go \
   -type ConfigSnapshot \
   -type ConfigSnapshotUpstreams \
+  -type PeerServersValue \
+  -type PeeringServiceValue \
   -type configSnapshotConnectProxy \
   -type configSnapshotIngressGateway \
   -type configSnapshotMeshGateway \
   -type configSnapshotTerminatingGateway \
-  -type PeerServersValue \
   ./
