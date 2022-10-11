06b67a992838027e0cd7936145b0286c1a5d823e
Tyler Wendlandt <wenincode@gmail.com>
2022-10-11 09:59:53 -0600

    ui: hide node name and meta on agentless instances (#14921)
    * [NET-950] hide node name and meta on agentless instances
    
    * Fix linting issues and set synthetic-node value
    
    * ui: changelog entry

---
+++ ui/packages/consul-ui/app/components/consul/service-instance/list/index.js
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+
+export default class ServiceInstanceList extends Component {
+  get areAllExternalSourcesMatching() {
+    const firstSource = this.args.items[0]?.Service?.Meta?.['external-source'];
+
+    const matching = this.args.items.every(
+      (instance) => instance.Service?.Meta?.['external-source'] === firstSource
+    );
+    return matching;
+  }
+}
