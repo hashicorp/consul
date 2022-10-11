40e18c0e456b3c86c0c69e67d774263d175d1aaa
Michael Klein <michael@firstiwaslike.com>
2022-10-11 11:17:19 +0200

    Add peer as a belongs-to to service
    Working with a peer model as a relationship is much
    easier than to workaround a non-relationship in
    imported services. This is currently only relevant
    for imported-services where we know the peer
    in advance.

--- ui/packages/consul-ui/app/services/repository/service.js
+++ ui/packages/consul-ui/app/services/repository/service.js
@@ -1,8 +1,11 @@
 import RepositoryService from 'consul-ui/services/repository';
 import dataSource from 'consul-ui/decorators/data-source';
+import { inject as service } from '@ember/service';
 
 const modelName = 'service';
 export default class ServiceService extends RepositoryService {
+  @service store;
+
   getModelName() {
     return modelName;
   }
@@ -12,9 +15,21 @@ export default class ServiceService extends RepositoryService {
     return super.findAll(...arguments);
   }
 
-  @dataSource('/:partition/:ns/:dc/services/:peer')
-  async findAllImportedServices() {
-    return super.findAll(...arguments);
+  @dataSource('/:partition/:ns/:dc/services/:peer/:peerId')
+  async findAllImportedServices(params, configuration) {
+    // remember peer.id so that we can add it to to the service later on to setup relationship
+    const { peerId } = params;
+
+    // don't send peerId with query
+    delete params.peerId;
+
+    // assign the peer as a belongs-to. we don't have access to any information
+    // we could use to do this in the serializer so we need to do it manually here
+    return super.findAll(params, configuration).then((services) => {
+      const peer = this.store.peekRecord('peer', peerId);
+      services.forEach((service) => (service.peer = peer));
+      return services;
+    });
   }
 
   @dataSource('/:partition/:ns/:dc/gateways/for-service/:gateway')
