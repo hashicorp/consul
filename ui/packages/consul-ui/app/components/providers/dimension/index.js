19fbdb7a0b9bd1208fe5a460a458407f628bdbfd
Michael Klein <michael@firstiwaslike.com>
2022-10-12 19:19:23 +0200

    Fix linting error dimension service

--- ui/packages/consul-ui/app/components/providers/dimension/index.js
+++ ui/packages/consul-ui/app/components/providers/dimension/index.js
@@ -1,7 +1,6 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
 import { ref } from 'ember-ref-bucket';
 import { htmlSafe } from '@ember/template';
 
