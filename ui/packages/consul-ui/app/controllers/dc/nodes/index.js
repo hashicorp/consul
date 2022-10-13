4845b90c8ee69544dd4681e572d2ce2b7389131e
wenincode <tyler.wendlandt@hashicorp.com>
2022-10-12 18:16:39 -0600

    Add banner for agentless node notice

--- ui/packages/consul-ui/app/controllers/dc/nodes/index.js
+++ ui/packages/consul-ui/app/controllers/dc/nodes/index.js
@@ -1,3 +1,9 @@
 import PeeredResourceController from 'consul-ui/controllers/_peered-resource';
+import { action } from '@ember/object';
 
-export default class DcNodesController extends PeeredResourceController {}
+export default class DcNodesController extends PeeredResourceController {
+  @action
+  dismissAgentlessNotice() {
+    console.log('dismiss this here')
+  }
+}
