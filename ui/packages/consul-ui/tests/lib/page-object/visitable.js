f0cdbb00b3cf9f4d07ea6418b9a1577df223dcc3
wenincode <tyler.wendlandt@hashicorp.com>
2022-10-19 17:26:25 -0600

    Pre-populate partition on sso login

--- ui/packages/consul-ui/tests/lib/page-object/visitable.js
+++ ui/packages/consul-ui/tests/lib/page-object/visitable.js
@@ -66,6 +66,8 @@ export function visitable(path, encoder = encodeURIComponent) {
           let path = paths.shift();
           if (typeof dynamicSegmentsAndQueryParams.nspace !== 'undefined') {
             path = `/:nspace${path}`;
+          } else if (typeof dynamicSegmentsAndQueryParams.partition !== 'undefined') {
+            path = `/:partition${path}`;
           }
           params = assign({}, dynamicSegmentsAndQueryParams);
           let fullPath;
