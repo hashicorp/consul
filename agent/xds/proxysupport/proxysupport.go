d4e876c3781b2623a4085c883c35b2fa1978b614
Evan Culver <eculver@hashicorp.com>
2022-10-19 18:35:48 -0500

    connect: Add Envoy 1.24 to integration tests, remove Envoy 1.20

--- agent/xds/proxysupport/proxysupport.go
+++ agent/xds/proxysupport/proxysupport.go
@@ -7,8 +7,8 @@ package proxysupport
 //
 // see: https://www.consul.io/docs/connect/proxies/envoy#supported-versions
 var EnvoyVersions = []string{
-	"1.23.1",
+	"1.24.0",
+	"1.23.2",
 	"1.22.5",
 	"1.21.5",
-	"1.20.7",
 }
