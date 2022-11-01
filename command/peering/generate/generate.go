01bfab3f08336fa8aa8e6174e4fcb9c7a988733c
Nitya Dhanushkodi <nitya@hashicorp.com>
2022-11-01 06:05:35 +0000

    backport of commit 4199a7c30ab874b272e5188df9c61e7077f16a2a

--- command/peering/generate/generate.go
+++ command/peering/generate/generate.go
@@ -41,6 +41,12 @@ func (c *cmd) init() {
 		"Metadata to associate with the peering, formatted as key=value. This flag "+
 			"may be specified multiple times to set multiple metadata fields.")
 
+	c.flags.Var((*flags.AppendSliceValue)(&c.externalAddresses), "server-external-addresses",
+		"A list of addresses to put into the generated token, formatted as a comma-separate list. "+
+			"Addresses are the form of <host or IP>:port. "+
+			"This could be used to specify load balancer(s) or external IPs to reach the servers from "+
+			"the dialing side, and will override any server addresses obtained from the \"consul\" service.")
+
 	c.flags.StringVar(
 		&c.format,
 		"format",
@@ -78,9 +84,10 @@ func (c *cmd) Run(args []string) int {
 	peerings := client.Peerings()
 
 	req := api.PeeringGenerateTokenRequest{
-		PeerName:  c.name,
-		Partition: c.http.Partition(),
-		Meta:      c.meta,
+		PeerName:                c.name,
+		Partition:               c.http.Partition(),
+		Meta:                    c.meta,
+		ServerExternalAddresses: c.externalAddresses,
 	}
 
 	res, _, err := peerings.GenerateToken(context.Background(), req, &api.WriteOptions{})
