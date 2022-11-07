da9fa68aa5a492c00f6768b6462b12597b8702ca
Evan Culver <eculver@hashicorp.com>
2022-11-07 11:30:42 -0800

    Remove bad field merge

--- agent/proxycfg/snapshot.go
+++ agent/proxycfg/snapshot.go
@@ -643,12 +643,6 @@ type configSnapshotIngressGateway struct {
 	TracingStrategy   string
 	TracingPercentage float64
 
-	// WatchedDiscoveryChains is a map of upstream.Identifier() -> CancelFunc's
-	// in order to cancel any watches when the ingress gateway configuration is
-	// changed. Ingress gateways need this because discovery chain watches are
-	// added and removed through the lifecycle of single proxycfg.state instance.
-	WatchedDiscoveryChains map[UpstreamID]context.CancelFunc
-
 	// Upstreams is a list of upstreams this ingress gateway should serve traffic
 	// to. This is constructed from the ingress-gateway config entry, and uses
 	// the GatewayServices RPC to retrieve them.
