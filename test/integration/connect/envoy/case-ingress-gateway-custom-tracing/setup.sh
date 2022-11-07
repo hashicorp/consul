c734f06f50815499d0492141a63b846b06fde475
Evan Culver <eculver@hashicorp.com>
2022-11-07 15:32:21 -0800

    Remove dead code

--- test/integration/connect/envoy/case-ingress-gateway-custom-tracing/setup.sh
+++ test/integration/connect/envoy/case-ingress-gateway-custom-tracing/setup.sh
@@ -13,6 +13,3 @@ gen_envoy_bootstrap ingress-gateway-random-sampling-0 20000 primary true
 gen_envoy_bootstrap ingress-gateway-random-sampling-100 20001 primary true
 gen_envoy_bootstrap ingress-gateway-client-sampling-0 20002 primary true
 gen_envoy_bootstrap ingress-gateway-client-sampling-100 20003 primary true
-
-# gen_envoy_bootstrap s1 19000
-# gen_envoy_bootstrap s2 19001
