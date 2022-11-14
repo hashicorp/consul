2dfe2f23e0f64c2ad773c442bee2abcd5706ce19
freddygv <freddy@hashicorp.com>
2022-11-14 12:03:23 -0700

    Ensure naming collisions do not grant import read

--- agent/structs/structs_oss.go
+++ agent/structs/structs_oss.go
@@ -67,7 +67,9 @@ func (n *Node) OverridePartition(_ string) {
 
 func (_ *Coordinate) FillAuthzContext(_ *acl.AuthorizerContext) {}
 
-func (_ *NodeInfo) FillAuthzContext(_ *acl.AuthorizerContext) {}
+func (n *NodeInfo) FillAuthzContext(ctx *acl.AuthorizerContext) {
+	ctx.Peer = n.PeerName
+}
 
 // FillAuthzContext stub
 func (_ *DirEntry) FillAuthzContext(_ *acl.AuthorizerContext) {}
