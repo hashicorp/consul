b7bd0aaf9b3a6a52fe03f231a3fdb2e9670edecc
Dhia Ayachi <dhia@hashicorp.com>
2022-11-24 12:48:01 -0500

    refactor to apply configs for limiters under a prefix

--- agent/consul/multilimiter/multilimiter_test.go
+++ agent/consul/multilimiter/multilimiter_test.go
@@ -25,7 +25,7 @@ func TestNewMultiLimiter(t *testing.T) {
 }
 
 func TestRateLimiterUpdate(t *testing.T) {
-	c := Config{LimiterConfig: LimiterConfig{Rate: 0.1}, CleanupCheckLimit: 1 * time.Millisecond, CleanupCheckInterval: 10 * time.Millisecond}
+	c := Config{LimiterConfig: LimiterConfig{Rate: 0.1}, ReconcileCheckLimit: 1 * time.Millisecond, ReconcileCheckInterval: 10 * time.Millisecond}
 	m := NewMultiLimiter(c)
 	key := makeKey("test")
 	m.Allow(Limited{key: key})
@@ -48,7 +48,7 @@ func TestRateLimiterUpdate(t *testing.T) {
 func TestRateLimiterCleanup(t *testing.T) {
 
 	// Create a limiter and Allow a key, check the key exists
-	c := Config{LimiterConfig: LimiterConfig{Rate: 0.1}, CleanupCheckLimit: 1 * time.Millisecond, CleanupCheckInterval: 10 * time.Millisecond}
+	c := Config{LimiterConfig: LimiterConfig{Rate: 0.1}, ReconcileCheckLimit: 1 * time.Millisecond, ReconcileCheckInterval: 10 * time.Millisecond}
 	m := NewMultiLimiter(c)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -60,14 +60,14 @@ func TestRateLimiterCleanup(t *testing.T) {
 	require.True(t, ok)
 	require.NotNil(t, l)
 
-	// Wait > CleanupCheckInterval and check that the key was cleaned up
+	// Wait > ReconcileCheckInterval and check that the key was cleaned up
 	time.Sleep(20 * time.Millisecond)
 	limiters = m.limiters.Load()
 	l, ok = limiters.Get(key)
 	require.False(t, ok)
 	require.Nil(t, l)
 
-	// Stop the cleanup routine, check that a key is not cleaned up after > CleanupCheckInterval
+	// Stop the cleanup routine, check that a key is not cleaned up after > ReconcileCheckInterval
 	cancel()
 	m.Allow(Limited{key: key})
 	time.Sleep(20 * time.Millisecond)
@@ -80,58 +80,78 @@ func TestRateLimiterCleanup(t *testing.T) {
 func TestRateLimiterUpdateConfig(t *testing.T) {
 
 	// Create a MultiLimiter m with a config c and check the config is applied
-	c := Config{LimiterConfig: LimiterConfig{Rate: 0.1}, CleanupCheckLimit: 1 * time.Millisecond, CleanupCheckInterval: 10 * time.Millisecond}
+	c := Config{LimiterConfig: LimiterConfig{Rate: 0.1}, ReconcileCheckLimit: 100 * time.Millisecond, ReconcileCheckInterval: 10 * time.Millisecond}
 	m := NewMultiLimiter(c)
 	require.Equal(t, *m.config.Load(), c)
 
-	// Allow an IP and check defaultConfig is applied to that IP
-	ip := makeKey("127.0.0.1")
-	m.Allow(ipLimited{key: ip})
-	l, ok := m.limiters.Load().Get(ip)
-	require.True(t, ok)
-	require.NotNil(t, l)
-	limiter := l.(*Limiter)
-	require.True(t, c.Equal(limiter.config.Load()))
+	t.Run("Allow a key and check defaultConfig is applied to that key", func(t *testing.T) {
+		ipNoPrefix := Key("", "127.0.0.1")
+		m.Allow(ipLimited{key: ipNoPrefix})
+		l, ok := m.limiters.Load().Get(ipNoPrefix)
+		require.True(t, ok)
+		require.NotNil(t, l)
+		limiter := l.(*Limiter)
+		require.True(t, c.isApplied(limiter.limiter))
+	})
 
-	// Allow an IP with prefix and check defaultConfig is applied to that IP
-	prefix := "namespace.write"
-	ip = makeKey(prefix, "127.0.0.1")
-	m.Allow(ipLimited{key: ip})
-	l, ok = m.limiters.Load().Get(ip)
-	require.True(t, ok)
-	require.NotNil(t, l)
-	limiter = l.(*Limiter)
-	require.True(t, c.Equal(limiter.config.Load()))
+	t.Run("Allow 2 keys with prefix and check defaultConfig is applied to those keys", func(t *testing.T) {
+		prefix := "namespace.write"
+		ip := Key(prefix, "127.0.0.1")
+		m.Allow(ipLimited{key: ip})
+		ip2 := Key(prefix, "127.0.0.2")
+		m.Allow(ipLimited{key: ip2})
+		l, ok := m.limiters.Load().Get(ip)
+		require.True(t, ok)
+		require.NotNil(t, l)
+		limiter := l.(*Limiter)
+		require.True(t, c.LimiterConfig.isApplied(limiter.limiter))
+	})
+	t.Run("Apply a config to 'namespace.write' check the config is applied to existing keys under that prefix", func(t *testing.T) {
+		prefix := "namespace.write"
+		ip := Key(prefix, "127.0.0.1")
+		c3 := LimiterConfig{Rate: 2}
+		m.UpdateConfig(c3, []byte(prefix))
+		// call reconcileLimitedOnce to make sure the update is applied
+		m.reconcileLimitedOnce(context.Background())
+		m.Allow(ipLimited{key: ip})
+		l3, ok3 := m.limiters.Load().Get(ip)
+		require.True(t, ok3)
+		require.NotNil(t, l3)
+		limiter3 := l3.(*Limiter)
+		require.True(t, c3.isApplied(limiter3.limiter))
+	})
+	t.Run("Allow an IP with prefix and check prefix config is applied to new keys under that prefix", func(t *testing.T) {
+		c := LimiterConfig{Rate: 3}
+		prefix := "namespace.read"
+		m.UpdateConfig(c, []byte(prefix))
+		ip := Key(prefix, "127.0.0.1")
+		m.Allow(ipLimited{key: ip})
+		l, ok := m.limiters.Load().Get(ip)
+		require.True(t, ok)
+		require.NotNil(t, l)
+		limiter := l.(*Limiter)
+		require.True(t, c.isApplied(limiter.limiter))
+	})
 
-	// Update m config to c2 and check that c2 applied to m
-	c2 := LimiterConfig{Rate: 1}
-	m.UpdateConfig(c2, []byte(prefix))
-	// call cleanupLimitedOnce to make sure the update is applied
-	m.cleanupLimitedOnce(context.Background())
-	m.Allow(ipLimited{key: ip})
-	l, ok = m.limiters.Load().Get(ip)
-	require.True(t, ok)
-	require.NotNil(t, l)
-	limiter = l.(*Limiter)
-	require.Equal(t, c2, *limiter.config.Load())
-	time.Sleep(20 * time.Millisecond)
-	m.cleanupLimitedOnce(context.Background())
-	limiters := m.limiters.Load()
-	l, ok = limiters.Get(ip)
-	require.False(t, ok)
-	require.Nil(t, l)
-
-	c3 := LimiterConfig{Rate: 2}
-	m.UpdateConfig(c3, []byte(prefix))
-	// call cleanupLimitedOnce to make sure the update is applied
-	m.cleanupLimitedOnce(context.Background())
-	m.Allow(ipLimited{key: ip})
-	l, ok = m.limiters.Load().Get(ip)
-	require.True(t, ok)
-	require.NotNil(t, l)
-	limiter = l.(*Limiter)
-	require.Equal(t, c3, *limiter.config.Load())
-
+	t.Run("Allow an IP with prefix and check after it's cleaned new Allow would give it the right config", func(t *testing.T) {
+		prefix := "namespace.read"
+		ip := Key(prefix, "127.0.0.1")
+		c := LimiterConfig{Rate: 1}
+		m.UpdateConfig(c, []byte(prefix))
+		// call reconcileLimitedOnce to make sure the update is applied
+		m.reconcileLimitedOnce(context.Background())
+		m.Allow(ipLimited{key: ip})
+		l, ok := m.limiters.Load().Get(ip)
+		require.True(t, ok)
+		require.NotNil(t, l)
+		limiter := l.(*Limiter)
+		require.True(t, c.isApplied(limiter.limiter))
+		time.Sleep(200 * time.Millisecond)
+		m.reconcileLimitedOnce(context.Background())
+		l, ok = m.limiters.Load().Get(ip)
+		require.False(t, ok)
+		require.Nil(t, l)
+	})
 }
 
 type ipLimited struct {
