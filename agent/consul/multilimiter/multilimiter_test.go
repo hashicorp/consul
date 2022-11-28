e06c540d5d2005061138045984ffb44d0e5cdd1f
Dhia Ayachi <dhia@hashicorp.com>
2022-11-28 11:53:34 -0500

    add fuzz tests and fix bugs found. Refactor reconcile loop to have a simpler logic

--- agent/consul/multilimiter/multilimiter_test.go
+++ agent/consul/multilimiter/multilimiter_test.go
@@ -1,9 +1,13 @@
 package multilimiter
 
 import (
+	"bytes"
 	"context"
 	"encoding/binary"
+	"fmt"
+	radix "github.com/hashicorp/go-immutable-radix"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/time/rate"
 	"math/rand"
 	"testing"
 	"time"
@@ -27,7 +31,7 @@ func TestNewMultiLimiter(t *testing.T) {
 func TestRateLimiterUpdate(t *testing.T) {
 	c := Config{LimiterConfig: LimiterConfig{Rate: 0.1}, ReconcileCheckLimit: 1 * time.Millisecond, ReconcileCheckInterval: 10 * time.Millisecond}
 	m := NewMultiLimiter(c)
-	key := makeKey("test")
+	key := makeKey([]byte("test"))
 	m.Allow(Limited{key: key})
 	limiters := m.limiters.Load()
 	l1, ok1 := limiters.Get(key)
@@ -53,7 +57,7 @@ func TestRateLimiterCleanup(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	m.Run(ctx)
-	key := makeKey("test")
+	key := makeKey([]byte("test"))
 	m.Allow(Limited{key: key})
 	limiters := m.limiters.Load()
 	l, ok := limiters.Get(key)
@@ -85,7 +89,7 @@ func TestRateLimiterUpdateConfig(t *testing.T) {
 	require.Equal(t, *m.config.Load(), c)
 
 	t.Run("Allow a key and check defaultConfig is applied to that key", func(t *testing.T) {
-		ipNoPrefix := Key("", "127.0.0.1")
+		ipNoPrefix := Key([]byte(""), []byte("127.0.0.1"))
 		m.Allow(ipLimited{key: ipNoPrefix})
 		l, ok := m.limiters.Load().Get(ipNoPrefix)
 		require.True(t, ok)
@@ -95,10 +99,10 @@ func TestRateLimiterUpdateConfig(t *testing.T) {
 	})
 
 	t.Run("Allow 2 keys with prefix and check defaultConfig is applied to those keys", func(t *testing.T) {
-		prefix := "namespace.write"
-		ip := Key(prefix, "127.0.0.1")
+		prefix := []byte("namespace.write")
+		ip := Key(prefix, []byte("127.0.0.1"))
 		m.Allow(ipLimited{key: ip})
-		ip2 := Key(prefix, "127.0.0.2")
+		ip2 := Key(prefix, []byte("127.0.0.2"))
 		m.Allow(ipLimited{key: ip2})
 		l, ok := m.limiters.Load().Get(ip)
 		require.True(t, ok)
@@ -107,10 +111,10 @@ func TestRateLimiterUpdateConfig(t *testing.T) {
 		require.True(t, c.LimiterConfig.isApplied(limiter.limiter))
 	})
 	t.Run("Apply a config to 'namespace.write' check the config is applied to existing keys under that prefix", func(t *testing.T) {
-		prefix := "namespace.write"
-		ip := Key(prefix, "127.0.0.1")
+		prefix := []byte("namespace.write")
+		ip := Key(prefix, []byte("127.0.0.1"))
 		c3 := LimiterConfig{Rate: 2}
-		m.UpdateConfig(c3, []byte(prefix))
+		m.UpdateConfig(c3, prefix)
 		// call reconcileLimitedOnce to make sure the update is applied
 		m.reconcileLimitedOnce(context.Background())
 		m.Allow(ipLimited{key: ip})
@@ -122,9 +126,9 @@ func TestRateLimiterUpdateConfig(t *testing.T) {
 	})
 	t.Run("Allow an IP with prefix and check prefix config is applied to new keys under that prefix", func(t *testing.T) {
 		c := LimiterConfig{Rate: 3}
-		prefix := "namespace.read"
-		m.UpdateConfig(c, []byte(prefix))
-		ip := Key(prefix, "127.0.0.1")
+		prefix := []byte("namespace.read")
+		m.UpdateConfig(c, prefix)
+		ip := Key(prefix, []byte("127.0.0.1"))
 		m.Allow(ipLimited{key: ip})
 		l, ok := m.limiters.Load().Get(ip)
 		require.True(t, ok)
@@ -134,10 +138,10 @@ func TestRateLimiterUpdateConfig(t *testing.T) {
 	})
 
 	t.Run("Allow an IP with prefix and check after it's cleaned new Allow would give it the right config", func(t *testing.T) {
-		prefix := "namespace.read"
-		ip := Key(prefix, "127.0.0.1")
+		prefix := []byte("namespace.read")
+		ip := Key(prefix, []byte("127.0.0.1"))
 		c := LimiterConfig{Rate: 1}
-		m.UpdateConfig(c, []byte(prefix))
+		m.UpdateConfig(c, prefix)
 		// call reconcileLimitedOnce to make sure the update is applied
 		m.reconcileLimitedOnce(context.Background())
 		m.Allow(ipLimited{key: ip})
@@ -152,6 +156,90 @@ func TestRateLimiterUpdateConfig(t *testing.T) {
 		require.False(t, ok)
 		require.Nil(t, l)
 	})
+}
+
+func FuzzSingleConfig(f *testing.F) {
+	c := Config{LimiterConfig: LimiterConfig{Rate: 0.1}, ReconcileCheckLimit: 100 * time.Millisecond, ReconcileCheckInterval: 10 * time.Millisecond}
+	m := NewMultiLimiter(c)
+	require.Equal(f, *m.config.Load(), c)
+	f.Add(makeKey(randIP()))
+	f.Add(makeKey(randIP(), randIP()))
+	f.Add(makeKey(randIP(), randIP(), randIP()))
+	f.Add(makeKey(randIP(), randIP(), randIP(), randIP()))
+	f.Fuzz(func(t *testing.T, ff []byte) {
+		m.Allow(Limited{key: ff})
+		checkLimiter(t, ff, m.limiters.Load())
+		checkTree(t, m.limiters.Load())
+	})
+}
+
+func FuzzSplitKey(f *testing.F) {
+	f.Add(makeKey(randIP(), randIP()))
+	f.Add(makeKey(randIP(), randIP(), randIP()))
+	f.Add(makeKey(randIP(), randIP(), randIP(), randIP()))
+	f.Add([]byte(""))
+	f.Fuzz(func(t *testing.T, ff []byte) {
+		prefix, suffix := splitKey(ff)
+		require.NotNil(t, prefix)
+		require.NotNil(t, suffix)
+		if len(prefix) == 0 && len(suffix) == 0 {
+			return
+		}
+		joined := bytes.Join([][]byte{prefix, suffix}, []byte(separator))
+		require.Equal(t, ff, joined)
+		require.False(t, bytes.Contains(prefix, []byte(separator)))
+	})
+}
+
+func checkLimiter(t require.TestingT, ff []byte, Tree *radix.Tree) {
+	v, ok := Tree.Get(ff)
+	require.True(t, ok)
+	require.NotNil(t, v)
+}
+
+func FuzzUpdateConfig(f *testing.F) {
+
+	f.Add(bytes.Join([][]byte{[]byte(""), makeKey(randIP()), makeKey(randIP(), randIP()), makeKey(randIP(), randIP(), randIP()), makeKey(randIP(), randIP(), randIP(), randIP())}, []byte(",")))
+	f.Fuzz(func(t *testing.T, ff []byte) {
+		cm := Config{LimiterConfig: LimiterConfig{Rate: 0.1}, ReconcileCheckLimit: 100000 * time.Hour, ReconcileCheckInterval: 10 * time.Millisecond}
+		m := NewMultiLimiter(cm)
+		keys := bytes.Split(ff, []byte(","))
+		for _, f := range keys {
+			prefix, _ := splitKey(f)
+			c := LimiterConfig{Rate: rate.Limit(rand.Float64()), Burst: rand.Int()}
+			m.UpdateConfig(c, prefix)
+			m.Allow(Limited{key: f})
+		}
+		m.reconcileLimitedOnce(context.Background())
+		checkTree(t, m.limiters.Load())
+	})
+
+}
+
+func checkTree(t require.TestingT, tree *radix.Tree) {
+	iterator := tree.Root().Iterator()
+	kp, v, ok := iterator.Next()
+	for ok {
+		switch c := v.(type) {
+		case *Limiter:
+			if c.limiter != nil {
+				prefix, _ := splitKey(kp)
+				v, _ := tree.Get(prefix)
+				switch c2 := v.(type) {
+				case *Limiter:
+					if c2.config != nil {
+						config := c2.config.Load()
+						applied := config.isApplied(c.limiter)
+						require.True(t, applied, fmt.Sprintf("%v,%v", kp, prefix))
+					}
+
+				}
+			}
+		default:
+			require.Nil(t, v)
+		}
+		kp, v, ok = iterator.Next()
+	}
 }
 
 type ipLimited struct {
