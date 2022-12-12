f87110aa4ab7f259e1402efa694e43b38af3991c
Nathan Coleman <nathan.coleman@hashicorp.com>
2022-12-12 16:51:21 -0500

    Stub unit tests for peering export command

---
+++ command/peering/export/export_test.go
@@ -0,0 +1,25 @@
+package export
+
+import "testing"
+
+func TestExportCommand(t *testing.T) {
+	t.Run("service is required", func(t *testing.T) {
+
+	})
+
+	t.Run("peer is required", func(t *testing.T) {
+
+	})
+
+	t.Run("valid peer name is required", func(t *testing.T) {
+
+	})
+
+	t.Run("initial config entry should be created", func(t *testing.T) {
+
+	})
+
+	t.Run("existing config entry should be updated", func(t *testing.T) {
+		
+	})
+}
