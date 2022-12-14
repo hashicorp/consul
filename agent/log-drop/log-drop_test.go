e5758a9d336973eac0ce7c641379794f8d55467b
Dhia Ayachi <dhia@hashicorp.com>
2022-12-14 12:10:20 -0500

    fix tests to remove sleep

--- agent/log-drop/log-drop_test.go
+++ agent/log-drop/log-drop_test.go
@@ -2,11 +2,11 @@ package logdrop
 
 import (
 	"context"
+	"github.com/hashicorp/consul/sdk/testutil/retry"
 	"github.com/hashicorp/go-hclog"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"testing"
-	"time"
 )
 
 func TestNewLogDrop(t *testing.T) {
@@ -15,19 +15,20 @@ func TestNewLogDrop(t *testing.T) {
 	ld := NewLogDropSink(context.Background(), "test", 10, mockLogger, func(_ Log) {})
 	require.NotNil(t, ld)
 	ld.Accept("test Log", hclog.Info, "hello", "test", 0)
-	time.Sleep(10 * time.Millisecond)
-	mockLogger.AssertNumberOfCalls(t, "Log", 1)
+	retry.Run(t, func(r *retry.R) {
+		mockLogger.AssertNumberOfCalls(r, "Log", 1)
+	})
+
 }
 
 func TestLogDroppedWhenChannelFilled(t *testing.T) {
 	mockLogger := NewMockLogger(t)
 	ctx, cancelFunc := context.WithCancel(context.Background())
 	called := false
+	cancelFunc()
 	ld := NewLogDropSink(ctx, "test", 1, mockLogger, func(l Log) {
 		called = true
 	})
-	cancelFunc()
-	time.Sleep(1 * time.Second)
 	for i := 0; i < 2; i++ {
 		ld.Accept("test", hclog.Debug, "hello")
 	}
