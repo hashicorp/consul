468ab58109be8eef70cba74b40bd8e7a6d29268d
Dhia Ayachi <dhia@hashicorp.com>
2022-12-15 09:41:44 -0500

    rename and add go docs

--- agent/log-drop/log-drop.go
+++ agent/log-drop/log-drop.go
@@ -5,26 +5,31 @@ import (
 	"github.com/hashicorp/go-hclog"
 )
 
-//go:generate mockery --name Logger --inpackage
-type Logger interface {
-	Log(level hclog.Level, msg string, args ...interface{})
+// SinkAdapter mimic the interface from hclog.SinkAdapter
+//
+//go:generate mockery --name SinkAdapter --inpackage
+type SinkAdapter interface {
+	Accept(name string, level hclog.Level, msg string, args ...interface{})
 }
 
 type Log struct {
+	n string
 	s string
 	i []interface{}
 	l hclog.Level
 }
 
 type logDropSink struct {
-	logger Logger
+	sink   SinkAdapter
 	logCh  chan Log
 	name   string
 	dropFn func(l Log)
 }
 
+// Accept consume a log and push it into a channel,
+// if the channel is filled it will call dropFn
 func (r *logDropSink) Accept(name string, level hclog.Level, msg string, args ...interface{}) {
-	r.pushLog(Log{l: level, s: msg, i: args})
+	r.pushLog(Log{n: name, l: level, s: msg, i: args})
 }
 
 func (r *logDropSink) pushLog(l Log) {
@@ -39,16 +44,19 @@ func (r *logDropSink) logConsumer(ctx context.Context) {
 	for {
 		select {
 		case l := <-r.logCh:
-			r.logger.Log(l.l, l.s, l.i)
+			r.sink.Accept(l.n, l.l, l.s, l.i)
 		case <-ctx.Done():
 			return
 		}
 	}
 }
 
-func NewLogDropSink(ctx context.Context, name string, depth int, logger Logger, dropFn func(l Log)) hclog.SinkAdapter {
+// NewLogDropSink create a log SinkAdapter that wrap another SinkAdapter
+// It also create a go routine for consuming logs, the given context need to be canceled
+// to properly deallocate the SinkAdapter.
+func NewLogDropSink(ctx context.Context, name string, depth int, sink SinkAdapter, dropFn func(l Log)) hclog.SinkAdapter {
 	r := &logDropSink{
-		logger: logger,
+		sink:   sink,
 		logCh:  make(chan Log, depth),
 		name:   name,
 		dropFn: dropFn,
--- agent/log-drop/log-drop_test.go
+++ agent/log-drop/log-drop_test.go
@@ -7,31 +7,45 @@ import (
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"testing"
+	"time"
 )
 
 func TestNewLogDrop(t *testing.T) {
-	mockLogger := NewMockLogger(t)
-	mockLogger.On("Log", hclog.Info, "hello", []interface{}{"test", 0}).Return()
+	mockLogger := NewMockSinkAdapter(t)
+	mockLogger.On("Accept", "test Accept", hclog.Info, "hello", []interface{}{"test", 0}).Return()
 	ld := NewLogDropSink(context.Background(), "test", 10, mockLogger, func(_ Log) {})
 	require.NotNil(t, ld)
-	ld.Accept("test Log", hclog.Info, "hello", "test", 0)
+	ld.Accept("test Accept", hclog.Info, "hello", "test", 0)
 	retry.Run(t, func(r *retry.R) {
-		mockLogger.AssertNumberOfCalls(r, "Log", 1)
+		mockLogger.AssertNumberOfCalls(r, "Accept", 1)
 	})
 
 }
 
 func TestLogDroppedWhenChannelFilled(t *testing.T) {
-	mockLogger := NewMockLogger(t)
+	mockLogger := NewMockSinkAdapter(t)
 	ctx, cancelFunc := context.WithCancel(context.Background())
-	called := false
-	cancelFunc()
+	defer cancelFunc()
+	block := make(chan interface{})
+	mockLogger.On("Accept", "test", hclog.Debug, "hello", []interface{}(nil)).Run(func(args mock.Arguments) {
+		<-block
+	})
+
+	var called = make(chan interface{})
 	ld := NewLogDropSink(ctx, "test", 1, mockLogger, func(l Log) {
-		called = true
+		close(called)
 	})
 	for i := 0; i < 2; i++ {
 		ld.Accept("test", hclog.Debug, "hello")
 	}
-	mockLogger.AssertNotCalled(t, "Log", mock.Anything)
-	require.True(t, called)
+
+	select {
+	case <-called:
+		close(block)
+	case <-time.After(100 * time.Millisecond):
+		t.Fatal("timeout waiting for drop func to be called")
+	}
+	retry.Run(t, func(r *retry.R) {
+		mockLogger.AssertNumberOfCalls(r, "Accept", 1)
+	})
 }
