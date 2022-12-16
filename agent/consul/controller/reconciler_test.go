ab199a11b0d37f3f943936c8f26eaa43918e0b6f
Andrew Stucki <andrew.stucki@hashicorp.com>
2022-12-16 16:49:26 -0500

    Add async reconciliation controller subpackage (#15534)
    * Add async reconciliation controller subpackage
    
    * Address initial feedback
    
    * Add tests for panic assertions
    
    * Fix comment

---
+++ agent/consul/controller/reconciler_test.go
@@ -0,0 +1,61 @@
+package controller
+
+import (
+	"context"
+	"sync"
+)
+
+type testReconciler struct {
+	received chan Request
+	response error
+	mutex    sync.Mutex
+	stepChan chan struct{}
+	stopChan chan struct{}
+	ctx      context.Context
+}
+
+func (r *testReconciler) Reconcile(ctx context.Context, req Request) error {
+	if r.stepChan != nil {
+		select {
+		case <-r.stopChan:
+			return nil
+		case <-r.stepChan:
+		}
+	}
+
+	select {
+	case <-r.stopChan:
+		return nil
+	case r.received <- req:
+	}
+
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+	return r.response
+}
+
+func (r *testReconciler) setResponse(err error) {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+	r.response = err
+}
+
+func (r *testReconciler) step() {
+	r.stepChan <- struct{}{}
+}
+
+func (r *testReconciler) stop() {
+	close(r.stopChan)
+}
+
+func newTestReconciler(stepping bool) *testReconciler {
+	r := &testReconciler{
+		received: make(chan Request, 1000),
+		stopChan: make(chan struct{}),
+	}
+	if stepping {
+		r.stepChan = make(chan struct{})
+	}
+
+	return r
+}
