674005f97c6e2cb04bd2085b1b5ff9d422a5c7cc
Andrew Stucki <andrew.stucki@hashicorp.com>
2022-12-21 11:20:48 -0500

    APIGateway TCPRoute validation (#15855)
    * Add APIGateway validation
    
    * Fix comment
    
    * Add additional validations
    
    * Add cert ref validation
    
    * Add protobuf definitions
    
    * Tabs to spaces
    
    * Fix up field types
    
    * Add API structs
    
    * Move struct fields around a bit
    
    * Add TCPRoute normalization and validation
    
    * Address PR feedback
    
    * Add forgotten Status
    
    * Add some more field docs in api package
    
    * Fix test

--- api/config_entry_routes.go
+++ api/config_entry_routes.go
@@ -9,7 +9,16 @@ type TCPRouteConfigEntry struct {
 	// service. This should match the name provided in the service definition.
 	Name string
 
+	// Parents is a list of gateways that this route should be bound to.
+	Parents []ResourceReference
+	// Services is a list of TCP-based services that this should route to.
+	// Currently, this must specify at maximum one service.
+	Services []TCPService
+
 	Meta map[string]string `json:",omitempty"`
+
+	// Status is the asynchronous status which a TCPRoute propagates to the user.
+	Status ConfigEntryStatus
 
 	// CreateIndex is the Raft index this entry was created at. This is a
 	// read-only field.
@@ -67,6 +76,22 @@ func (a *TCPRouteConfigEntry) GetCreateIndex() uint64 {
 
 func (a *TCPRouteConfigEntry) GetModifyIndex() uint64 {
 	return a.ModifyIndex
+}
+
+// TCPService is a service reference for a TCPRoute
+type TCPService struct {
+	Name string
+	// Weight specifies the proportion of requests forwarded to the referenced service.
+	// This is computed as weight/(sum of all weights in the list of services).
+	Weight int
+
+	// Partition is the partition the config entry is associated with.
+	// Partitioning is a Consul Enterprise feature.
+	Partition string `json:",omitempty"`
+
+	// Namespace is the namespace the config entry is associated with.
+	// Namespacing is a Consul Enterprise feature.
+	Namespace string `json:",omitempty"`
 }
 
 // HTTPRouteConfigEntry manages the configuration for a HTTP route
