d00e244549d9dc2bfafb4319d95b19237ed0ad7c
Semir Patel <semir.patel@hashicorp.com>
2023-01-05 11:34:06 -0600

    Dedupe testing instance of inmem metrics sink into agent/metrics package

--- agent/consul/rate/handler_test.go
+++ agent/consul/rate/handler_test.go
@@ -3,20 +3,17 @@ package rate
 import (
 	"bytes"
 	"context"
-	"fmt"
 	"net"
 	"net/netip"
 	"testing"
-	"time"
 
-	"github.com/armon/go-metrics"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 
 	"github.com/hashicorp/go-hclog"
 
 	"github.com/hashicorp/consul/agent/consul/multilimiter"
+	"github.com/hashicorp/consul/agent/metrics"
 )
 
 //
@@ -223,9 +220,7 @@ func TestHandler(t *testing.T) {
 	}
 	for desc, tc := range testCases {
 		t.Run(desc, func(t *testing.T) {
-			// Setup metrics to test they are recorded
-			sink := testSetupMetrics(t)
-
+			sink := metrics.TestSetupMetrics(t, "")
 			limiter := newMockLimiter(t)
 			limiter.On("UpdateConfig", mock.Anything, mock.Anything).Return()
 			for _, c := range tc.checks {
@@ -259,63 +254,10 @@ func TestHandler(t *testing.T) {
 			}
 
 			if tc.expectMetric {
-				assertCounter(t, sink, tc.expectMetricName, tc.expectMetricCount)
+				metrics.AssertCounter(t, sink, tc.expectMetricName, tc.expectMetricCount)
 			}
 		})
 	}
-}
-
-func assertCounter(t *testing.T, sink *metrics.InmemSink, name string, value float64) {
-	t.Helper()
-
-	data := sink.Data()
-
-	var got float64
-	for _, intv := range data {
-		intv.RLock()
-		// Note that InMemSink uses SampledValue and treats the _Sum_ not the Count
-		// as the entire value.
-		if sample, ok := intv.Counters[name]; ok {
-			got += sample.Sum
-		}
-		intv.RUnlock()
-	}
-
-	if !assert.Equal(t, value, got) {
-		// no nice way to dump this - this is copied from private method in
-		// InMemSink used for dumping to stdout on SIGUSR1.
-		buf := bytes.NewBuffer(nil)
-		for _, intv := range data {
-			intv.RLock()
-			for name, val := range intv.Gauges {
-				fmt.Fprintf(buf, "[%v][G] '%s': %0.3f\n", intv.Interval, name, val.Value)
-			}
-			for name, vals := range intv.Points {
-				for _, val := range vals {
-					fmt.Fprintf(buf, "[%v][P] '%s': %0.3f\n", intv.Interval, name, val)
-				}
-			}
-			for name, agg := range intv.Counters {
-				fmt.Fprintf(buf, "[%v][C] '%s': %s\n", intv.Interval, name, agg.AggregateSample)
-			}
-			for name, agg := range intv.Samples {
-				fmt.Fprintf(buf, "[%v][S] '%s': %s\n", intv.Interval, name, agg.AggregateSample)
-			}
-			intv.RUnlock()
-		}
-		t.Log(buf.String())
-	}
-}
-
-func testSetupMetrics(t *testing.T) *metrics.InmemSink {
-	// Record for ages (5 mins) so we can be confident that our assertions won't
-	// fail on silly long test runs due to dropped data.
-	s := metrics.NewInmemSink(10*time.Second, 300*time.Second)
-	cfg := metrics.DefaultConfig("")
-	cfg.EnableHostname = false
-	cfg.EnableRuntimeMetrics = false
-	metrics.NewGlobal(cfg, s)
-	return s
 }
 
 func TestNewHandlerWithLimiter_CallsUpdateConfig(t *testing.T) {
