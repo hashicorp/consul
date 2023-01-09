0d630ccff88c3ee207aa1def6d32dd97b9809a63
cskh <hui.kang@hashicorp.com>
2023-01-09 11:00:13 -0500

    Test matrix

--- test/integration/consul-container/test/upgrade/badauthz_test.go
+++ test/integration/consul-container/test/upgrade/badauthz_test.go
@@ -2,7 +2,9 @@ package upgrade
 
 import (
 	"context"
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 
@@ -20,35 +22,52 @@ import (
 // 3. Upgrade the cluster
 // 4. Write the service-intention to allow the connection
 func TestBadauthz_UpgradeToTarget_fromLatest(t *testing.T) {
-	cluster := topology.BasicSingleClusterTopology(t, &libcluster.Options{
-		Datacenter: "dc1",
-		NumServer:  1,
-		NumClient:  1,
-		Version:    *libutils.LatestVersion,
-	})
-	defer func() {
-		err := cluster.Terminate()
-		require.NoErrorf(t, err, "termining cluster")
-	}()
+	type testcase struct {
+		oldversion    string
+		targetVersion string
+	}
+	tcs := []testcase{
+		//{
+		//  TODO: Recreat config during upgrade due to the breaking change to ports.grpc_tls
+		// 	oldversion:    "1.13",
+		// 	targetVersion: *libutils.TargetVersion,
+		// },
+		{
+			oldversion:    "1.14",
+			targetVersion: *libutils.TargetVersion,
+		},
+	}
 
-	// Register an static-server service
-	clientNodes, err := cluster.Clients()
-	require.NoError(t, err)
-	require.True(t, len(clientNodes) > 0)
-	_, _, err = libservice.CreateAndRegisterStaticServerAndSidecar(clientNodes[0])
-	require.NoError(t, err)
-	client := clientNodes[0].GetClient()
-	libassert.CatalogServiceExists(t, client, "static-server")
-	libassert.CatalogServiceExists(t, client, "static-server-sidecar-proxy")
+	run := func(t *testing.T, tc testcase) {
+		cluster := topology.BasicSingleClusterTopology(t, &libcluster.Options{
+			Datacenter: "dc1",
+			NumServer:  1,
+			NumClient:  1,
+			Version:    tc.oldversion,
+		})
+		defer func() {
+			err := cluster.Terminate()
+			require.NoErrorf(t, err, "termining cluster")
+		}()
 
-	// Register an static-client service
-	serverNodes, err := cluster.Servers()
-	require.NoError(t, err)
-	require.True(t, len(serverNodes) > 0)
-	staticClientSvcSidecar, err := libservice.CreateAndRegisterStaticClientSidecar(serverNodes[0], "", true)
-	require.NoError(t, err)
+		// Register an static-server service
+		clientNodes, err := cluster.Clients()
+		require.NoError(t, err)
+		require.True(t, len(clientNodes) > 0)
+		_, _, err = libservice.CreateAndRegisterStaticServerAndSidecar(clientNodes[0])
+		require.NoError(t, err)
+		client := clientNodes[0].GetClient()
+		libassert.CatalogServiceExists(t, client, "static-server")
+		libassert.CatalogServiceExists(t, client, "static-server-sidecar-proxy")
 
-	cluster.ConfigEntryWrite(`
+		// Register an static-client service
+		serverNodes, err := cluster.Servers()
+		require.NoError(t, err)
+		require.True(t, len(serverNodes) > 0)
+		staticClientSvcSidecar, err := libservice.CreateAndRegisterStaticClientSidecar(serverNodes[0], "", true)
+		require.NoError(t, err)
+
+		cluster.ConfigEntryWrite(`
 	Kind = "service-intentions"
 	Name = "static-server"
 	Sources = [
@@ -58,16 +77,16 @@ func TestBadauthz_UpgradeToTarget_fromLatest(t *testing.T) {
   		}
 	]
 	`)
-	_, port := staticClientSvcSidecar.GetAddr()
-	libassert.HTTPServiceFailTcpConnection(t, "localhost", port)
+		_, port := staticClientSvcSidecar.GetAddr()
+		libassert.HTTPServiceFailTcpConnection(t, "localhost", port)
 
-	// Upgrade the cluster to targetVersion
-	t.Logf("Upgrade to version %s", *libutils.TargetVersion)
-	err = cluster.StandardUpgrade(t, context.Background(), *libutils.TargetVersion)
-	require.NoError(t, err)
+		// Upgrade the cluster to targetVersion
+		t.Logf("Upgrade to version %s", *libutils.TargetVersion)
+		err = cluster.StandardUpgrade(t, context.Background(), tc.targetVersion)
+		require.NoError(t, err)
 
-	// Verify intentions work after upgrade
-	err = cluster.ConfigEntryWrite(`
+		// Verify intentions work after upgrade
+		err = cluster.ConfigEntryWrite(`
 	Kind = "service-intentions"
 	Name = "static-server"
 	Sources = [
@@ -77,8 +96,17 @@ func TestBadauthz_UpgradeToTarget_fromLatest(t *testing.T) {
 			}
 	]
 	`)
-	require.NoError(t, err)
+		require.NoError(t, err)
 
-	_, port = staticClientSvcSidecar.GetAddr()
-	libassert.HTTPServiceEchoes(t, "localhost", port)
+		_, port = staticClientSvcSidecar.GetAddr()
+		libassert.HTTPServiceEchoes(t, "localhost", port)
+	}
+
+	for _, tc := range tcs {
+		t.Run(fmt.Sprintf("upgrade from %s to %s", tc.oldversion, tc.targetVersion),
+			func(t *testing.T) {
+				run(t, tc)
+			})
+		time.Sleep(3 * time.Second)
+	}
 }
