4d66e4b22531e4b8c93de88a00b22a7a345be8f7
Connor Kelly <connor.kelly@hashicorp.com>
2023-01-10 15:54:59 -0600

    CC-778 use 0s if we cannot marshal to JSON

--- agent/hcp/duration.go
+++ agent/hcp/duration.go
@@ -1,20 +1,18 @@
 package hcp
 
 import (
-	"fmt"
 	"strings"
 	"time"
 
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/types/known/durationpb"
 )
 
 func formatDuration(d time.Duration) string {
 	protoDuration := durationpb.New(d)
-	dur := fmt.Sprintf("%d.%.9d", protoDuration.Seconds, protoDuration.Nanos)
-	// remove trailing 0's
-	dur = strings.TrimSuffix(dur, "000")
-	dur = strings.TrimSuffix(dur, "000")
-	dur = strings.TrimSuffix(dur, ".000")
-	// add trailing s
-	return dur + "s"
+	durByte, err := protojson.Marshal(protoDuration)
+	if err != nil {
+		return "0s"
+	}
+	return strings.Trim(string(durByte), `"`)
 }
