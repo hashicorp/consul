33156c80aeaa09366ac93a5ef1502480658b2232
Sarah Alsmiller <sarah.alsmiller@hashicorp.com>
2023-01-12 15:05:07 -0600

    checkpoint

--- agent/consul/gateways/controller_gateways.go
+++ agent/consul/gateways/controller_gateways.go
@@ -2,17 +2,15 @@ package gateways
 
 import (
 	"context"
-
-	"github.com/hashicorp/go-hclog"
-
+	"fmt"
 	"github.com/hashicorp/consul/agent/consul/controller"
 	"github.com/hashicorp/consul/agent/consul/fsm"
 	"github.com/hashicorp/consul/agent/consul/gateways/updater"
 	"github.com/hashicorp/consul/agent/consul/state"
 	"github.com/hashicorp/consul/agent/consul/stream"
-
 	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/consul/agent/structs"
+	"errors"
 )
 
 type apiGatewayReconciler struct {
@@ -31,63 +29,64 @@ func (r apiGatewayReconciler) Reconcile(ctx context.Context, req controller.Requ
 
 	if entry == nil {
 		r.logger.Error("cleaning up deleted gateway object", "request", req)
-		if err := r.updater.Delete(&structs.BoundGatewayConfigEntry{
-			Kind:           structs.BoundGateway,
+		if err := r.updater.Delete(&structs.BoundAPIGatewayConfigEntry{
+			Kind:           structs.BoundAPIGateway,
 			Name:           req.Name,
-			EnterpriseMeta: req.Meta,
+			EnterpriseMeta: *req.Meta,
 		}); err != nil {
 			return err
 		}
 		return nil
 	}
 
-	gateway := entry.(*structs.GatewayConfigEntry)
+	gateway := entry.(*structs.BoundAPIGatewayConfigEntry)
 	// TODO: do initial distributed validation here, if we're invalid, then set the status
 
-	var state *structs.BoundGatewayConfigEntry
-	_, boundEntry, err := store.ConfigEntry(nil, structs.BoundGateway, req.Name, &req.Meta)
+	var state *structs.BoundAPIGatewayConfigEntry
+	_, boundEntry, err := store.ConfigEntry(nil, structs.BoundAPIGateway, req.Name, req.Meta)
 	if err != nil {
 		return err
 	}
 	if boundEntry == nil {
-		state = &structs.BoundGatewayConfigEntry{
-			Kind:           structs.BoundGateway,
+		state = &structs.BoundAPIGatewayConfigEntry{
+			Kind:           structs.BoundAPIGateway,
 			Name:           gateway.Name,
 			EnterpriseMeta: gateway.EnterpriseMeta,
 		}
 	} else {
-		state = boundEntry.(*structs.BoundGatewayConfigEntry)
+		state = boundEntry.(*structs.BoundAPIGatewayConfigEntry)
 	}
 
 	r.logger.Debug("started reconciling gateway")
-	routes, err := BindTCPRoutes(store, state)
-	if err != nil {
-		return err
-	}
+	for _, listener := range state.Listeners {
+		for _, route := range listener.Routes {
+			fmt.Println(route)
+			//routes that didn't have a gateway that exists
+			_, routeEntry, err := store.ConfigEntry(nil, route.Kind, route.Name, &route.EnterpriseMeta)
+			if err != nil {
+				return err
+			}
 
-	// now update the gateway state
-	r.logger.Debug("persisting gateway state", "state", state)
-	if err := r.updater.Update(state); err != nil {
-		r.logger.Error("error persisting state", "error", err)
-		return err
-	}
-
-	// then update the gateway status
-	r.logger.Debug("persisting gateway status", "gateway", gateway)
-	if err := r.updater.UpdateStatus(gateway); err != nil {
-		return err
-	}
-
-	// and update the route statuses
-	for _, route := range routes {
-		r.logger.Debug("persisting route status", "route", route)
-		if err := r.updater.UpdateStatus(route); err != nil {
-			return err
+			var routeBoundRouter structs.BoundRouter
+			switch routeEntry.GetKind() {
+			case structs.TCPRoute:
+				fmt.Println("tcp")
+				routeBoundRouter = (routeEntry).(*structs.TCPRouteConfigEntry)
+			case structs.HTTPRoute:
+				fmt.Println("not implemented")
+			 default:
+				return errors.New("route type doesn't exist")
+			}
+			gateways, routesWithNoGateways, err := BindRouteToGateways(store, routeBoundRouter)
+			if err != nil {
+				return err
+			}
 		}
 	}
 
-	r.logger.Debug("finished reconciling gateway")
-	return nil
+
+
+
 }
 }
 
