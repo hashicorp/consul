a0d6a6aa0c769cb0e8fe105f2ac9fa7e1a58fd66
Derek Menteer <derek.menteer@hashicorp.com>
2023-01-19 11:43:59 -0600

    Decrease polling frequency on peer proxy config data.
    This is a temporary fix to reduce the performance cost of fetching
    peering data in proxycfg. A better long-term fix should be added
    that uses streaming or blocking-queries to ensure updates are
    triggered only when changes are made, rather than polling.

--- agent/cache-types/trust_bundles.go
+++ agent/cache-types/trust_bundles.go
@@ -35,7 +35,7 @@ func (r *TrustBundleListRequest) CacheInfo() cache.RequestInfo {
 
 		// OPTIMIZE(peering): Cache.notifyPollingQuery polls at this interval. We need to revisit how that polling works.
 		//        	          Using an exponential backoff when the result hasn't changed may be preferable.
-		MaxAge: 1 * time.Second,
+		MaxAge: 10 * time.Second,
 	}
 
 	v, err := hashstructure.Hash([]interface{}{
