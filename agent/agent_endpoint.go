5c06ccc69582e42284e5a4c70d0d2b3fb62e2a36
cskh <hui.kang@hashicorp.com>
2023-01-20 16:59:51 +0000

    backport of commit a42e86ffd8d23a4883d22bbbcd9f6c4176ad88b0

--- agent/agent_endpoint.go
+++ agent/agent_endpoint.go
@@ -320,6 +320,7 @@ func (s *HTTPHandlers) AgentServices(resp http.ResponseWriter, req *http.Request
 	var filterExpression string
 	s.parseFilter(req, &filterExpression)
 
+	s.defaultMetaPartitionToAgent(&entMeta)
 	authz, err := s.agent.delegate.ResolveTokenAndDefaultMeta(token, &entMeta, nil)
 	if err != nil {
 		return nil, err
@@ -404,6 +405,7 @@ func (s *HTTPHandlers) AgentService(resp http.ResponseWriter, req *http.Request)
 	}
 
 	// need to resolve to default the meta
+	s.defaultMetaPartitionToAgent(&entMeta)
 	_, err := s.agent.delegate.ResolveTokenAndDefaultMeta(token, &entMeta, nil)
 	if err != nil {
 		return nil, err
@@ -477,6 +479,7 @@ func (s *HTTPHandlers) AgentChecks(resp http.ResponseWriter, req *http.Request) 
 		return nil, err
 	}
 
+	s.defaultMetaPartitionToAgent(&entMeta)
 	authz, err := s.agent.delegate.ResolveTokenAndDefaultMeta(token, &entMeta, nil)
 	if err != nil {
 		return nil, err
@@ -739,6 +742,7 @@ func (s *HTTPHandlers) AgentRegisterCheck(resp http.ResponseWriter, req *http.Re
 		return nil, HTTPError{StatusCode: http.StatusBadRequest, Reason: "Bad check status"}
 	}
 
+	s.defaultMetaPartitionToAgent(&args.EnterpriseMeta)
 	authz, err := s.agent.delegate.ResolveTokenAndDefaultMeta(token, &args.EnterpriseMeta, nil)
 	if err != nil {
 		return nil, err
@@ -787,7 +791,9 @@ func (s *HTTPHandlers) AgentRegisterCheck(resp http.ResponseWriter, req *http.Re
 
 func (s *HTTPHandlers) AgentDeregisterCheck(resp http.ResponseWriter, req *http.Request) (interface{}, error) {
 	id := strings.TrimPrefix(req.URL.Path, "/v1/agent/check/deregister/")
-	checkID := structs.NewCheckID(types.CheckID(id), nil)
+
+	entMeta := acl.NewEnterpriseMetaWithPartition(s.agent.config.PartitionOrDefault(), "")
+	checkID := structs.NewCheckID(types.CheckID(id), &entMeta)
 
 	// Get the provided token, if any, and vet against any ACL policies.
 	var token string
@@ -879,7 +885,8 @@ func (s *HTTPHandlers) AgentCheckUpdate(resp http.ResponseWriter, req *http.Requ
 }
 
 func (s *HTTPHandlers) agentCheckUpdate(resp http.ResponseWriter, req *http.Request, checkID types.CheckID, status string, output string) (interface{}, error) {
-	cid := structs.NewCheckID(checkID, nil)
+	entMeta := acl.NewEnterpriseMetaWithPartition(s.agent.config.PartitionOrDefault(), "")
+	cid := structs.NewCheckID(checkID, &entMeta)
 
 	// Get the provided token, if any, and vet against any ACL policies.
 	var token string
@@ -969,6 +976,7 @@ func (s *HTTPHandlers) AgentHealthServiceByID(resp http.ResponseWriter, req *htt
 	s.parseToken(req, &token)
 
 	// need to resolve to default the meta
+	s.defaultMetaPartitionToAgent(&entMeta)
 	var authzContext acl.AuthorizerContext
 	authz, err := s.agent.delegate.ResolveTokenAndDefaultMeta(token, &entMeta, &authzContext)
 	if err != nil {
@@ -1026,6 +1034,7 @@ func (s *HTTPHandlers) AgentHealthServiceByName(resp http.ResponseWriter, req *h
 	var token string
 	s.parseToken(req, &token)
 
+	s.defaultMetaPartitionToAgent(&entMeta)
 	// need to resolve to default the meta
 	var authzContext acl.AuthorizerContext
 	authz, err := s.agent.delegate.ResolveTokenAndDefaultMeta(token, &entMeta, &authzContext)
@@ -1103,6 +1112,7 @@ func (s *HTTPHandlers) AgentRegisterService(resp http.ResponseWriter, req *http.
 	var token string
 	s.parseToken(req, &token)
 
+	s.defaultMetaPartitionToAgent(&args.EnterpriseMeta)
 	authz, err := s.agent.delegate.ResolveTokenAndDefaultMeta(token, &args.EnterpriseMeta, nil)
 	if err != nil {
 		return nil, err
@@ -1219,7 +1229,8 @@ func (s *HTTPHandlers) AgentRegisterService(resp http.ResponseWriter, req *http.
 
 func (s *HTTPHandlers) AgentDeregisterService(resp http.ResponseWriter, req *http.Request) (interface{}, error) {
 	serviceID := strings.TrimPrefix(req.URL.Path, "/v1/agent/service/deregister/")
-	sid := structs.NewServiceID(serviceID, nil)
+	entMeta := acl.NewEnterpriseMetaWithPartition(s.agent.config.PartitionOrDefault(), "")
+	sid := structs.NewServiceID(serviceID, &entMeta)
 
 	// Get the provided token, if any, and vet against any ACL policies.
 	var token string
@@ -1255,7 +1266,8 @@ func (s *HTTPHandlers) AgentDeregisterService(resp http.ResponseWriter, req *htt
 func (s *HTTPHandlers) AgentServiceMaintenance(resp http.ResponseWriter, req *http.Request) (interface{}, error) {
 	// Ensure we have a service ID
 	serviceID := strings.TrimPrefix(req.URL.Path, "/v1/agent/service/maintenance/")
-	sid := structs.NewServiceID(serviceID, nil)
+	entMeta := acl.NewEnterpriseMetaWithPartition(s.agent.config.PartitionOrDefault(), "")
+	sid := structs.NewServiceID(serviceID, &entMeta)
 
 	if sid.ID == "" {
 		return nil, HTTPError{StatusCode: http.StatusBadRequest, Reason: "Missing service ID"}
