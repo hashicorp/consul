179f5769a1baee04937c7c8a4ec87b583f00e568
Sarah Alsmiller <sarah.alsmiller@hashicorp.com>
2023-01-19 18:54:37 -0600

    checkpoint, pull in thomas's code

--- agent/consul/gateways/controller_gateways.go
+++ agent/consul/gateways/controller_gateways.go
@@ -38,7 +38,7 @@ func (r apiGatewayReconciler) Reconcile(ctx context.Context, req controller.Requ
 	}
 
 	gateway := entry.(*structs.BoundAPIGatewayConfigEntry)
-	// TODO: do initial distributed validation here, if we're invalid, then set the status
+	// TODO: validation?
 
 	var state *structs.BoundAPIGatewayConfigEntry
 	boundEntry, err := r.store.GetConfigEntry(structs.BoundAPIGateway, req.Name, req.Meta)
@@ -56,51 +56,51 @@ func (r apiGatewayReconciler) Reconcile(ctx context.Context, req controller.Requ
 	}
 
 	r.logger.Debug("started reconciling gateway")
+	routes := []structs.BoundRoute{}
 	for _, listener := range state.Listeners {
-		//TODO swap over to Thomases method
 		for _, route := range listener.Routes {
-			fmt.Println(route)
-			//routes that didn't have a gateway that exists
 			routeEntry, err := r.store.GetConfigEntry(route.Kind, route.Name, &route.EnterpriseMeta)
 			if err != nil {
 				return err
 			}
 
-			var routeBoundRouter structs.BoundRouter
+			var routeBoundRouter structs.BoundRoute
 			switch routeEntry.GetKind() {
 			case structs.TCPRoute:
 				fmt.Println("tcp")
 				routeBoundRouter = (routeEntry).(*structs.TCPRouteConfigEntry)
+				routes = append(routes, routeBoundRouter)
 			case structs.HTTPRoute:
 				fmt.Println("not implemented")
 			default:
 				return errors.New("route type doesn't exist")
 			}
-			_, _, err = BindRouteToGateways(store, routeBoundRouter)
-			if err != nil {
-				return err
-			}
-
-			// now update the gateway state
-			r.logger.Debug("persisting gateway state", "state", state)
-			if err := r.store.Update(state); err != nil {
-				r.logger.Error("error persisting state", "error", err)
-				return err
-			}
-
-			// then update the gateway status
-			r.logger.Debug("persisting gateway status", "gateway", gateway)
-			if err := r.store.UpdateStatus(gateway); err != nil {
-				return err
-			}
-
-			//// and update the route statuses ?
-			//r.logger.Debug("persisting route status", "route", route)
-			//if err := r.store.UpdateStatus(route); err != nil {
-			//	return err
-			//}
 
 		}
+
+		boundGateway, routeErrors, err := BindRoutesToGateways(structs.gateway, routes)
+
+		// now update the gateway state
+		r.logger.Debug("persisting gateway state", "state", state)
+		if err := r.store.Update(&boundGateway); err != nil {
+			r.logger.Error("error persisting state", "error", err)
+			return err
+		}
+
+		// then update the gateway status
+		r.logger.Debug("persisting gateway status", "gateway", gateway)
+		if err := r.store.UpdateStatus(gateway); err != nil {
+			return err
+		}
+
+		//// and update the route statuses
+		for route, err := range routeErrors {
+			r.logger.Debug("persisting route status", "route", route)
+			if err := r.store.UpdateStatus(route); err != nil {
+				return err
+			}
+		}
+
 	}
 
 	return nil
