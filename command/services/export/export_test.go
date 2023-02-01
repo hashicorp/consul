baa79104121db5b19cf4e71aae11e05d24d988b6
20sr20 <sujata@hashicorp.com>
2023-02-01 12:33:55 -0800

    cleaned up partition stuff

--- command/services/export/export_test.go
+++ command/services/export/export_test.go
@@ -43,7 +43,7 @@ func TestExportCommand(t *testing.T) {
 		require.Contains(t, ui.ErrorWriter.String(), "Missing the required -name flag")
 	})
 
-	t.Run("peer or partition is required", func(t *testing.T) {
+	t.Run("peer is required", func(t *testing.T) {
 
 		ui := cli.NewMockUi()
 		cmd := New(ui)
@@ -54,7 +54,7 @@ func TestExportCommand(t *testing.T) {
 
 		code := cmd.Run(args)
 		require.Equal(t, 1, code, "err: %s", ui.ErrorWriter.String())
-		require.Contains(t, ui.ErrorWriter.String(), "Must provide -consumer-peers or -consumer-partitions flag")
+		require.Contains(t, ui.ErrorWriter.String(), "Must provide -consumer-peers flag")
 	})
 
 	t.Run("valid peer name is required", func(t *testing.T) {
@@ -71,22 +71,7 @@ func TestExportCommand(t *testing.T) {
 		require.Equal(t, 1, code, "err: %s", ui.ErrorWriter.String())
 		require.Contains(t, ui.ErrorWriter.String(), "Invalid peer")
 	})
-
-	t.Run("valid partition name is required", func(t *testing.T) {
-
-		ui := cli.NewMockUi()
-		cmd := New(ui)
-
-		args := []string{
-			"-name=testservice",
-			"-consumer-partitions=par1,",
-		}
-
-		code := cmd.Run(args)
-		require.Equal(t, 1, code, "err: %s", ui.ErrorWriter.String())
-		require.Contains(t, ui.ErrorWriter.String(), "Invalid partition")
-	})
-
+	
 	t.Run("initial config entry should be created", func(t *testing.T) {
 
 		ui := cli.NewMockUi()
@@ -96,7 +81,6 @@ func TestExportCommand(t *testing.T) {
 			"-http-addr=" + a.HTTPAddr(),
 			"-name=testservice",
 			"-consumer-peers=a,b",
-			"-consumer-partitions=par1,par2",
 		}
 
 		code := cmd.Run(args)
@@ -113,22 +97,20 @@ func TestExportCommand(t *testing.T) {
 			"-http-addr=" + a.HTTPAddr(),
 			"-name=testservice",
 			"-consumer-peers=a,b",
-			"-consumer-partitions=par1,par2",
 		}
 
 		code := cmd.Run(args)
 		require.Equal(t, 0, code)
-		require.Contains(t, ui.OutputWriter.String(), `Successfully exported service testservice to peers "a,b" and to partitions "par1,par2"`)
+		require.Contains(t, ui.OutputWriter.String(), `Successfully exported service testservice to peers "a,b"`)
 
 		args = []string{
 			"-http-addr=" + a.HTTPAddr(),
 			"-name=testservice",
 			"-consumer-peers=c",
-			"-consumer-partitions=par3",
 		}
 
 		code = cmd.Run(args)
 		require.Equal(t, 0, code)
-		require.Contains(t, ui.OutputWriter.String(), `Successfully exported service testservice to peers "c" and to partitions "par3"`)
+		require.Contains(t, ui.OutputWriter.String(), `Successfully exported service testservice to peers "c"`)
 	})
 }
