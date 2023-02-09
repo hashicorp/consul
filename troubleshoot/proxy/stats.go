7543a35bd909d8a29c88bdbe115e20c45fb63c22
Maliz <maliheh.monshizadeh@hashicorp.com>
2023-02-08 21:39:48 -0800

    troubleshoot basic envoy stats for an upstream

--- troubleshoot/proxy/stats.go
+++ troubleshoot/proxy/stats.go
@@ -3,8 +3,9 @@ package troubleshoot
 import (
 	"encoding/json"
 	"fmt"
-
 	envoy_admin_v3 "github.com/envoyproxy/go-control-plane/envoy/admin/v3"
+	"github.com/hashicorp/consul/troubleshoot/validate"
+	"strings"
 )
 
 type statsJson struct {
@@ -14,6 +15,42 @@ type statsJson struct {
 type simpleMetric struct {
 	Value int64  `json:"value,omitempty"`
 	Name  string `json:"name,omitempty"`
+}
+
+func (t *Troubleshoot) troubleshootStats(upstreamEnvoyID string) (validate.Messages, error) {
+
+	statMessages := validate.Messages{}
+	envoyId := strings.Split(upstreamEnvoyID, "?")
+
+	rejectionStats, err := t.getEnvoyStats(envoyId[0] + ".*update_rejected")
+	if err != nil {
+		return nil, fmt.Errorf("could not get config rejection stats from envoy admin API: %w", err)
+	}
+
+	for _, rs := range rejectionStats {
+		if rs.Value != 0 {
+			msg := validate.Message{
+				Success: true,
+				Message: fmt.Sprintf("Upstream has %v rejected configurations", rs.Value),
+			}
+			statMessages = append(statMessages, msg)
+		}
+	}
+
+	connectionFailureStats, err := t.getEnvoyStats(envoyId[0] + ".*upstream_cx_connect_fail")
+	if err != nil {
+		return nil, fmt.Errorf("could not get connection failure stats from envoy admin API: %w", err)
+	}
+	for _, cfs := range connectionFailureStats {
+		if cfs.Value != 0 {
+			msg := validate.Message{
+				Success: true,
+				Message: fmt.Sprintf("upstream has %v connection failure(s).", cfs.Value),
+			}
+			statMessages = append(statMessages, msg)
+		}
+	}
+	return statMessages, nil
 }
 
 func (t *Troubleshoot) getEnvoyStats(filter string) ([]*envoy_admin_v3.SimpleMetric, error) {
