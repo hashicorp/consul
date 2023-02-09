2703522f8d6713c6c418417f6648cf9fb0fd80c4
Andrew Stucki <andrew.stucki@hashicorp.com>
2023-02-09 11:32:10 -0500

    Add basic smoke test to make sure an APIGateway runs (#16217)

---
+++ test/integration/connect/envoy/case-api-gateway-smoke/setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -euo pipefail
+
+upsert_config_entry primary '
+kind = "api-gateway"
+name = "api-gateway"
+listeners = [
+  {
+    port     = 9999
+    protocol = "tcp"
+  }
+]
+'
+
+upsert_config_entry primary '
+kind = "tcp-route"
+name = "api-gateway-route"
+services = [
+  {
+    name               = "s1"
+  }
+]
+parents = [
+  {
+    name = "api-gateway"
+  }
+]
+'
+
+register_services primary
+
+gen_envoy_bootstrap api-gateway 20000 primary true
+gen_envoy_bootstrap s1 19000
+gen_envoy_bootstrap s2 19001
