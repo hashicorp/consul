e8a628eaa7b3e38080835e8b8d9b9672d2207898
Maliz <maliheh.monshizadeh@hashicorp.com>
2023-02-09 14:25:44 -0800

    move comment

--- test/integration/consul-container/libs/service/service.go
+++ test/integration/consul-container/libs/service/service.go
@@ -9,8 +9,8 @@ import (
 // Service represents a process that will be registered with the
 // Consul catalog, including Consul components such as sidecars and gateways
 type Service interface {
-	// Export a service to the peering cluster
 	Exec(ctx context.Context, cmd []string) (int, io.Reader, error)
+	// Export a service to the peering cluster
 	Export(partition, peer string, client *api.Client) error
 	GetAddr() (string, int)
 	GetLogs() (string, error)
