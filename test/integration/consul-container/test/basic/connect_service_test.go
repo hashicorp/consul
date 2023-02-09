d72ad5fb950b7cb43cd8bf4ef08ec49ee417e90f
Anita Akaeze <anita.akaeze@hashicorp.com>
2023-02-09 09:45:31 -0500

    Merge pull request #4216 from hashicorp/NET-2252-add-assert-fortioname (#16212)
    NET-2252: integration tests: add assert.FortioName

--- test/integration/consul-container/test/basic/connect_service_test.go
+++ test/integration/consul-container/test/basic/connect_service_test.go
@@ -1,6 +1,7 @@
 package basic
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -33,6 +34,7 @@ func TestBasicConnectService(t *testing.T) {
 
 	libassert.AssertContainerState(t, clientService, "running")
 	libassert.HTTPServiceEchoes(t, "localhost", port, "")
+	libassert.AssertFortioName(t, fmt.Sprintf("http://localhost:%d", port), "static-server")
 }
 
 func createCluster(t *testing.T) *libcluster.Cluster {
@@ -72,8 +74,10 @@ func createServices(t *testing.T, cluster *libcluster.Cluster) libservice.Servic
 	client := node.GetClient()
 	// Create a service and proxy instance
 	serviceOpts := &libservice.ServiceOpts{
-		Name: libservice.StaticServerServiceName,
-		ID:   "static-server",
+		Name:     libservice.StaticServerServiceName,
+		ID:       "static-server",
+		HTTPPort: 8080,
+		GRPCPort: 8079,
 	}
 
 	// Create a service and proxy instance
