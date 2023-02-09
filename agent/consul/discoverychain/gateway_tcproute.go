4758020099faa749aa5f63dfaeb252038476b3e0
Thomas Eckert <teckert@hashicorp.com>
2023-02-09 09:51:34 -0500

    Update agent/consul/discoverychain/gateway_tcproute.go
    Co-authored-by: Nathan Coleman <nathan.coleman@hashicorp.com>

--- agent/consul/discoverychain/gateway_tcproute.go
+++ agent/consul/discoverychain/gateway_tcproute.go
@@ -3,7 +3,7 @@ package discoverychain
 import "github.com/hashicorp/consul/agent/structs"
 
 func synthesizeTCPRouteDiscoveryChain(route structs.TCPRouteConfigEntry) []structs.IngressService {
-	services := []structs.IngressService{}
+	services := make([]structs.IngressService, 0, len(route.Services))
 	for _, service := range route.Services {
 		ingress := structs.IngressService{
 			Name:           service.Name,
