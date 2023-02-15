d2f454ce560e6a5a0d719c7d1ec7a006712218a0
valeriia-ruban <valeriia.ruban@hashicorp.com>
2023-02-15 14:04:20 -0800

    fixed tests

--- ui/packages/consul-ui/tests/lib/page-object/createDeletable.js
+++ ui/packages/consul-ui/tests/lib/page-object/createDeletable.js
@@ -7,7 +7,8 @@ export default function (clickable) {
       ...obj,
       ...{
         delete: clickable(scope + '[data-test-delete]'),
-        confirmDelete: clickable(scope + 'button.type-delete'),
+        confirmDelete: clickable(scope + '[data-test-delete]'),
+        confirmInlineDelete: clickable(scope + 'button.type-delete'),
       },
     };
   };
