cddf86f337500da2c0db3a1b6c266b653ed0f63d
Jeff Boruszak <104028618+boruszak@users.noreply.github.com>
2023-02-23 11:58:39 -0600

    Docs/cluster peering 1.15 updates (#16291)
    * initial commit
    
    * initial commit
    
    * Overview updates
    
    * Overview page improvements
    
    * More Overview improvements
    
    * improvements
    
    * Small fixes/updates
    
    * Updates
    
    * Overview updates
    
    * Nav data
    
    * More nav updates
    
    * Fix
    
    * updates
    
    * Updates + tip test
    
    * Directory test
    
    * refining
    
    * Create restructure w/ k8s
    
    * Single usage page
    
    * Technical Specification
    
    * k8s pages
    
    * typo
    
    * L7 traffic management
    
    * Manage connections
    
    * k8s page fix
    
    * Create page tab corrections
    
    * link to k8s
    
    * intentions
    
    * corrections
    
    * Add-on intention descriptions
    
    * adjustments
    
    * Missing </CodeTabs>
    
    * Diagram improvements
    
    * Final diagram update
    
    * Apply suggestions from code review
    
    Co-authored-by: trujillo-adam <47586768+trujillo-adam@users.noreply.github.com>
    Co-authored-by: Tu Nguyen <im2nguyen@users.noreply.github.com>
    Co-authored-by: David Yu <dyu@hashicorp.com>
    
    * diagram name fix
    
    * Fixes
    
    * Updates to index.mdx
    
    * Tech specs page corrections
    
    * Tech specs page rename
    
    * update link to tech specs
    
    * K8s - new pages + tech specs
    
    * k8s - manage peering connections
    
    * k8s L7 traffic management
    
    * Separated establish connection pages
    
    * Directory fixes
    
    * Usage clean up
    
    * k8s docs edits
    
    * Updated nav data
    
    * CodeBlock Component fix
    
    * filename
    
    * CodeBlockConfig removal
    
    * Redirects
    
    * Update k8s filenames
    
    * Reshuffle k8s tech specs for clarity, fmt yaml files
    
    * Update general cluster peering docs, reorder CLI > API > UI, cross link to kubernetes
    
    * Fix config rendering in k8s usage docs, cross link to general usage from k8s docs
    
    * fix legacy link
    
    * update k8s docs
    
    * fix nested list rendering
    
    * redirect fix
    
    * page error
    
    ---------
    
    Co-authored-by: trujillo-adam <47586768+trujillo-adam@users.noreply.github.com>
    Co-authored-by: Tu Nguyen <im2nguyen@users.noreply.github.com>
    Co-authored-by: David Yu <dyu@hashicorp.com>
    Co-authored-by: Tu Nguyen <im2nguyen@gmail.com>

--- website/redirects.js
+++ website/redirects.js
@@ -4,4 +4,16 @@
 // modify or delete existing redirects without first verifying internally.
 // Next.js redirect documentation: https://nextjs.org/docs/api-reference/next.config.js/redirects
 
-module.exports = []
+module.exports = [
+  {
+    source: '/docs/connect/cluster-peering/create-manage-peering',
+    destination:
+      '/docs/connect/cluster-peering/usage/establish-cluster-peering',
+    permanent: true,
+  },
+  {
+    source: '/docs/connect/cluster-peering/k8s',
+    destination: '/docs/k8s/connect/cluster-peering/k8s-tech-specs',
+    permanent: true,
+  },
+]
