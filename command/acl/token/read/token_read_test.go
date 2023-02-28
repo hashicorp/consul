hashicorp/consul

commit 9199e99e21126e06ec2c0ea43cd47663edd4da90

Update token language to distinguish Accessor and Secret ID usage (#16044)
* remove legacy tokens

* remove lingering legacy token references from docs

* update language and naming for token secrets and accessor IDs

* updates all tokenID references to clarify accessorID

* remove token type references and lookup tokens by accessorID index

* remove unnecessary constants

* replace additional tokenID param names

* Add warning info for deprecated -id parameter

Co-authored-by: Paul Glass <pglass@hashicorp.com>

* Update field comment

Co-authored-by: Paul Glass <pglass@hashicorp.com>

---------

Co-authored-by: Paul Glass <pglass@hashicorp.com>

command/acl/token/read/token_read.go
@@ -25,11 +25,13 @@ type cmd struct {
 	http  *flags.HTTPFlags
 	help  string
 
-	tokenID  string
-	self     bool
-	showMeta bool
-	format   string
-	expanded bool
+	tokenAccessorID string
+	self            bool
+	showMeta        bool
+	format          string
+	expanded        bool
+
+	tokenID string // DEPRECATED
 }
 
 func (c *cmd) init() {
@@ -37,10 +39,10 @@ func (c *cmd) init() {
 	c.flags.BoolVar(&c.showMeta, "meta", false, "Indicates that token metadata such "+
 		"as the content hash and Raft indices should be shown for each entry")
 	c.flags.BoolVar(&c.self, "self", false, "Indicates that the current HTTP token "+
-		"should be read by secret ID instead of expecting a -id option")
+		"should be read by secret ID instead of expecting a -accessor-id option")
 	c.flags.BoolVar(&c.expanded, "expanded", false, "Indicates that the contents of the "+
 		" policies and roles affecting the token should also be shown.")
-	c.flags.StringVar(&c.tokenID, "id", "", "The Accessor ID of the token to read. "+
+	c.flags.StringVar(&c.tokenAccessorID, "accessor-id", "", "The Accessor ID of the token to read. "+
 		"It may be specified as a unique ID prefix but will error if the prefix "+
 		"matches multiple token Accessor IDs")
 	c.flags.StringVar(
@@ -54,6 +56,10 @@ func (c *cmd) init() {
 	flags.Merge(c.flags, c.http.ServerFlags())
 	flags.Merge(c.flags, c.http.MultiTenancyFlags())
 	c.help = flags.Usage(help, c.flags)
+
+	// Deprecations
+	c.flags.StringVar(&c.tokenID, "id", "",
+		"DEPRECATED. Use -accessor-id instead.")
 }
 
 func (c *cmd) Run(args []string) int {
@@ -61,9 +67,15 @@ func (c *cmd) Run(args []string) int {
 		return 1
 	}
 
-	if c.tokenID == "" && !c.self {
-		c.UI.Error(fmt.Sprintf("Must specify the -id parameter"))
-		return 1
+	tokenAccessor := c.tokenAccessorID
+	if tokenAccessor == "" {
+		if c.tokenID == "" {
+			c.UI.Error("Must specify the -accessor-id parameter")
+			return 1
+		} else {
+			tokenAccessor = c.tokenID
+			c.UI.Warn("Use the -accessor-id parameter to specify token by Accessor ID")
+		}
 	}
 
 	client, err := c.http.APIClient()
@@ -75,27 +87,27 @@ func (c *cmd) Run(args []string) int {
 	var t *api.ACLToken
 	var expanded *api.ACLTokenExpanded
 	if !c.self {
-		tokenID, err := acl.GetTokenIDFromPartial(client, c.tokenID)
+		tok, err := acl.GetTokenAccessorIDFromPartial(client, tokenAccessor)
 		if err != nil {
 			c.UI.Error(fmt.Sprintf("Error determining token ID: %v", err))
 			return 1
 		}
 
 		if !c.expanded {
-			t, _, err = client.ACL().TokenRead(tokenID, nil)
+			t, _, err = client.ACL().TokenRead(tok, nil)
 		} else {
-			expanded, _, err = client.ACL().TokenReadExpanded(tokenID, nil)
+			expanded, _, err = client.ACL().TokenReadExpanded(tok, nil)
 		}
 
 		if err != nil {
-			c.UI.Error(fmt.Sprintf("Error reading token %q: %v", tokenID, err))
+			c.UI.Error(fmt.Sprintf("Error reading token %q: %v", tok, err))
 			return 1
 		}
 	} else {
 		// TODO: consider updating this CLI command and underlying HTTP API endpoint
 		// to support expanded read of a "self" token, which is a much better user workflow.
 		if c.expanded {
-			c.UI.Error("Cannot use both -expanded and -self. Instead, use -expanded and -id=<accessor id>.")
+			c.UI.Error("Cannot use both -expanded and -self. Instead, use -expanded and -accessor-id=<accessor id>.")
 			return 1
 		}
 
@@ -139,17 +151,17 @@ func (c *cmd) Help() string {
 const (
 	synopsis = "Read an ACL token"
 	help     = `
-Usage: consul acl token read [options] -id TOKENID
+Usage: consul acl token read [options] -accessor-id TOKENID
 
   This command will retrieve and print out the details of
   a single token.
 
   Using a partial ID:
 
-          $ consul acl token read -id 4be56c77-82
+          $ consul acl token read -accessor-id 4be56c77-82
 
   Using the full ID:
 
-          $ consul acl token read -id 4be56c77-8244-4c7d-b08c-667b8c71baed
+          $ consul acl token read -accessor-id 4be56c77-8244-4c7d-b08c-667b8c71baed
 `
 )

command/acl/token/read/token_read_test.go
@@ -6,12 +6,13 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/hashicorp/consul/agent"
-	"github.com/hashicorp/consul/api"
-	"github.com/hashicorp/consul/testrpc"
 	"github.com/mitchellh/cli"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/hashicorp/consul/agent"
+	"github.com/hashicorp/consul/api"
+	"github.com/hashicorp/consul/testrpc"
 )
 
 func TestTokenReadCommand_noTabs(t *testing.T) {
@@ -56,7 +57,7 @@ func TestTokenReadCommand_Pretty(t *testing.T) {
 	args := []string{
 		"-http-addr=" + a.HTTPAddr(),
 		"-token=root",
-		"-id=" + token.AccessorID,
+		"-accessor-id=" + token.AccessorID,
 	}
 
 	code := cmd.Run(args)
@@ -103,7 +104,7 @@ func TestTokenReadCommand_JSON(t *testing.T) {
 	args := []string{
 		"-http-addr=" + a.HTTPAddr(),
 		"-token=root",
-		"-id=" + token.AccessorID,
+		"-accessor-id=" + token.AccessorID,
 		"-format=json",
 	}
 

acl/policy.go
@@ -5,13 +5,6 @@ import (
 	"strings"
 )
 
-type SyntaxVersion int
-
-const (
-	SyntaxCurrent SyntaxVersion = iota
-	SyntaxLegacy
-)
-
 const (
 	PolicyDeny  = "deny"
 	PolicyRead  = "read"

agent/acl_endpoint.go
@@ -295,7 +295,7 @@ func (s *HTTPHandlers) ACLTokenCRUD(resp http.ResponseWriter, req *http.Request)
 		return nil, aclDisabled
 	}
 
-	var fn func(resp http.ResponseWriter, req *http.Request, tokenID string) (interface{}, error)
+	var fn func(resp http.ResponseWriter, req *http.Request, tokenAccessorID string) (interface{}, error)
 
 	switch req.Method {
 	case "GET":
@@ -311,16 +311,16 @@ func (s *HTTPHandlers) ACLTokenCRUD(resp http.ResponseWriter, req *http.Request)
 		return nil, MethodNotAllowedError{req.Method, []string{"GET", "PUT", "DELETE"}}
 	}
 
-	tokenID := strings.TrimPrefix(req.URL.Path, "/v1/acl/token/")
-	if strings.HasSuffix(tokenID, "/clone") && req.Method == "PUT" {
-		tokenID = tokenID[:len(tokenID)-6]
+	tokenAccessorID := strings.TrimPrefix(req.URL.Path, "/v1/acl/token/")
+	if strings.HasSuffix(tokenAccessorID, "/clone") && req.Method == "PUT" {
+		tokenAccessorID = tokenAccessorID[:len(tokenAccessorID)-6]
 		fn = s.ACLTokenClone
 	}
-	if tokenID == "" && req.Method != "PUT" {
-		return nil, HTTPError{StatusCode: http.StatusBadRequest, Reason: "Missing token ID"}
+	if tokenAccessorID == "" && req.Method != "PUT" {
+		return nil, HTTPError{StatusCode: http.StatusBadRequest, Reason: "Missing token AccessorID"}
 	}
 
-	return fn(resp, req, tokenID)
+	return fn(resp, req, tokenAccessorID)
 }
 
 func (s *HTTPHandlers) ACLTokenSelf(resp http.ResponseWriter, req *http.Request) (interface{}, error) {
@@ -336,7 +336,7 @@ func (s *HTTPHandlers) ACLTokenSelf(resp http.ResponseWriter, req *http.Request)
 		return nil, nil
 	}
 
-	// copy the token parameter to the ID
+	// copy the token secret parameter to the ID
 	args.TokenID = args.Token
 
 	if args.Datacenter == "" {
@@ -364,10 +364,10 @@ func (s *HTTPHandlers) ACLTokenCreate(resp http.ResponseWriter, req *http.Reques
 	return s.aclTokenSetInternal(req, "", true)
 }
 
-func (s *HTTPHandlers) ACLTokenGet(resp http.ResponseWriter, req *http.Request, tokenID string) (interface{}, error) {
+func (s *HTTPHandlers) ACLTokenGet(resp http.ResponseWriter, req *http.Request, tokenAccessorID string) (interface{}, error) {
 	args := structs.ACLTokenGetRequest{
 		Datacenter:  s.agent.config.Datacenter,
-		TokenID:     tokenID,
+		TokenID:     tokenAccessorID,
 		TokenIDType: structs.ACLTokenAccessor,
 	}
 
@@ -407,11 +407,11 @@ func (s *HTTPHandlers) ACLTokenGet(resp http.ResponseWriter, req *http.Request, 
 	return out.Token, nil
 }
 
-func (s *HTTPHandlers) ACLTokenSet(_ http.ResponseWriter, req *http.Request, tokenID string) (interface{}, error) {
-	return s.aclTokenSetInternal(req, tokenID, false)
+func (s *HTTPHandlers) ACLTokenSet(_ http.ResponseWriter, req *http.Request, tokenAccessorID string) (interface{}, error) {
+	return s.aclTokenSetInternal(req, tokenAccessorID, false)
 }
 
-func (s *HTTPHandlers) aclTokenSetInternal(req *http.Request, tokenID string, create bool) (interface{}, error) {
+func (s *HTTPHandlers) aclTokenSetInternal(req *http.Request, tokenAccessorID string, create bool) (interface{}, error) {
 	args := structs.ACLTokenSetRequest{
 		Datacenter: s.agent.config.Datacenter,
 		Create:     create,
@@ -425,10 +425,8 @@ func (s *HTTPHandlers) aclTokenSetInternal(req *http.Request, tokenID string, cr
 		return nil, HTTPError{StatusCode: http.StatusBadRequest, Reason: fmt.Sprintf("Token decoding failed: %v", err)}
 	}
 
-	if !create {
-		if args.ACLToken.AccessorID != tokenID {
-			return nil, HTTPError{StatusCode: http.StatusBadRequest, Reason: "Token Accessor ID in URL and payload do not match"}
-		}
+	if !create && args.ACLToken.AccessorID != tokenAccessorID {
+		return nil, HTTPError{StatusCode: http.StatusBadRequest, Reason: "Token Accessor ID in URL and payload do not match"}
 	}
 
 	var out structs.ACLToken
@@ -439,10 +437,10 @@ func (s *HTTPHandlers) aclTokenSetInternal(req *http.Request, tokenID string, cr
 	return &out, nil
 }
 
-func (s *HTTPHandlers) ACLTokenDelete(resp http.ResponseWriter, req *http.Request, tokenID string) (interface{}, error) {
+func (s *HTTPHandlers) ACLTokenDelete(resp http.ResponseWriter, req *http.Request, tokenAccessorID string) (interface{}, error) {
 	args := structs.ACLTokenDeleteRequest{
 		Datacenter: s.agent.config.Datacenter,
-		TokenID:    tokenID,
+		TokenID:    tokenAccessorID,
 	}
 	s.parseToken(req, &args.Token)
 	if err := s.parseEntMeta(req, &args.EnterpriseMeta); err != nil {
@@ -456,7 +454,7 @@ func (s *HTTPHandlers) ACLTokenDelete(resp http.ResponseWriter, req *http.Reques
 	return true, nil
 }
 
-func (s *HTTPHandlers) ACLTokenClone(resp http.ResponseWriter, req *http.Request, tokenID string) (interface{}, error) {
+func (s *HTTPHandlers) ACLTokenClone(resp http.ResponseWriter, req *http.Request, tokenAccessorID string) (interface{}, error) {
 	if s.checkACLDisabled() {
 		return nil, aclDisabled
 	}
@@ -475,7 +473,7 @@ func (s *HTTPHandlers) ACLTokenClone(resp http.ResponseWriter, req *http.Request
 	s.parseToken(req, &args.Token)
 
 	// Set this for the ID to clone
-	args.ACLToken.AccessorID = tokenID
+	args.ACLToken.AccessorID = tokenAccessorID
 
 	var out structs.ACLToken
 	if err := s.agent.RPC(req.Context(), "ACL.TokenClone", args, &out); err != nil {

agent/acl_test.go
@@ -37,7 +37,7 @@ type TestACLAgent struct {
 
 // NewTestACLAgent does just enough so that all the code within agent/acl.go can work
 // Basically it needs a local state for some of the vet* functions, a logger and a delegate.
-// The key is that we are the delegate so we can control the ResolveToken responses
+// The key is that we are the delegate so we can control the ResolveTokenSecret responses
 func NewTestACLAgent(t *testing.T, name string, hcl string, resolveAuthz authzResolver, resolveIdent identResolver) *TestACLAgent {
 	t.Helper()
 
@@ -89,9 +89,9 @@ func NewTestACLAgent(t *testing.T, name string, hcl string, resolveAuthz authzRe
 	return a
 }
 
-func (a *TestACLAgent) ResolveToken(secretID string) (acl.Authorizer, error) {
+func (a *TestACLAgent) ResolveTokenSecret(secretID string) (acl.Authorizer, error) {
 	if a.resolveAuthzFn == nil {
-		return nil, fmt.Errorf("ResolveToken call is unexpected - no authz resolver callback set")
+		return nil, fmt.Errorf("ResolveTokenSecret call is unexpected - no authz resolver callback set")
 	}
 
 	_, authz, err := a.resolveAuthzFn(secretID)
@@ -99,7 +99,7 @@ func (a *TestACLAgent) ResolveToken(secretID string) (acl.Authorizer, error) {
 }
 
 func (a *TestACLAgent) ResolveTokenAndDefaultMeta(secretID string, entMeta *acl.EnterpriseMeta, authzContext *acl.AuthorizerContext) (resolver.Result, error) {
-	authz, err := a.ResolveToken(secretID)
+	authz, err := a.ResolveTokenSecret(secretID)
 	if err != nil {
 		return resolver.Result{}, err
 	}

agent/agent_endpoint_test.go
@@ -4418,7 +4418,7 @@ func testAgent_RegisterServiceDeregisterService_Sidecar(t *testing.T, extraHCL s
 			}
 			`,
 			enableACL: true,
-			policies:  ``, // No token rules means no valid token
+			policies:  ``, // No policy means no valid token
 			wantNS:    nil,
 			wantErr:   "Permission denied",
 		},

agent/consul/acl.go
@@ -41,7 +41,7 @@ var ACLSummaries = []prometheus.SummaryDefinition{
 
 // These must be kept in sync with the constants in command/agent/acl.go.
 const (
-	// anonymousToken is the token ID we re-write to if there is no token ID
+	// anonymousToken is the token SecretID we re-write to if there is no token ID
 	// provided.
 	anonymousToken = "anonymous"
 
@@ -993,35 +993,35 @@ func (r *ACLResolver) resolveLocallyManagedToken(token string) (structs.ACLIdent
 	return r.resolveLocallyManagedEnterpriseToken(token)
 }
 
-// ResolveToken to an acl.Authorizer and structs.ACLIdentity. The acl.Authorizer
-// can be used to check permissions granted to the token, and the ACLIdentity
-// describes the token and any defaults applied to it.
-func (r *ACLResolver) ResolveToken(token string) (resolver.Result, error) {
+// ResolveTokenSecret to an acl.Authorizer and structs.ACLIdentity. The acl.Authorizer
+// can be used to check permissions granted to the token using its secret, and the
+// ACLIdentity describes the token and any defaults applied to it.
+func (r *ACLResolver) ResolveTokenSecret(tokenSecretID string) (resolver.Result, error) {
 	if !r.ACLsEnabled() {
 		return resolver.Result{Authorizer: acl.ManageAll()}, nil
 	}
 
-	if acl.RootAuthorizer(token) != nil {
+	if acl.RootAuthorizer(tokenSecretID) != nil {
 		return resolver.Result{}, acl.ErrRootDenied
 	}
 
 	// handle the anonymous token
-	if token == "" {
-		token = anonymousToken
+	if tokenSecretID == "" {
+		tokenSecretID = anonymousToken
 	}
 
-	if ident, authz, ok := r.resolveLocallyManagedToken(token); ok {
+	if ident, authz, ok := r.resolveLocallyManagedToken(tokenSecretID); ok {
 		return resolver.Result{Authorizer: authz, ACLIdentity: ident}, nil
 	}
 
 	defer metrics.MeasureSince([]string{"acl", "ResolveToken"}, time.Now())
 
-	identity, policies, err := r.resolveTokenToIdentityAndPolicies(token)
+	identity, policies, err := r.resolveTokenToIdentityAndPolicies(tokenSecretID)
 	if err != nil {
 		r.handleACLDisabledError(err)
 		if IsACLRemoteError(err) {
 			r.logger.Error("Error resolving token", "error", err)
-			ident := &missingIdentity{reason: "primary-dc-down", token: token}
+			ident := &missingIdentity{reason: "primary-dc-down", token: tokenSecretID}
 			return resolver.Result{Authorizer: r.down, ACLIdentity: ident}, nil
 		}
 
@@ -1074,11 +1074,11 @@ func (r *ACLResolver) ACLsEnabled() bool {
 }
 
 func (r *ACLResolver) ResolveTokenAndDefaultMeta(
-	token string,
+	tokenSecretID string,
 	entMeta *acl.EnterpriseMeta,
 	authzContext *acl.AuthorizerContext,
 ) (resolver.Result, error) {
-	result, err := r.ResolveToken(token)
+	result, err := r.ResolveTokenSecret(tokenSecretID)
 	if err != nil {
 		return resolver.Result{}, err
 	}
@@ -1119,9 +1119,9 @@ func filterACLWithAuthorizer(logger hclog.Logger, authorizer acl.Authorizer, sub
 // filterACL uses the ACLResolver to resolve the token in an acl.Authorizer,
 // then uses the acl.Authorizer to filter subj. Any entities in subj that are
 // not authorized for read access will be removed from subj.
-func filterACL(r *ACLResolver, token string, subj interface{}) error {
+func filterACL(r *ACLResolver, tokenSecretID string, subj interface{}) error {
 	// Get the ACL from the token
-	authorizer, err := r.ResolveToken(token)
+	authorizer, err := r.ResolveTokenSecret(tokenSecretID)
 	if err != nil {
 		return err
 	}

agent/consul/acl_endpoint.go
@@ -703,7 +703,7 @@ func (a *ACL) TokenBatchRead(args *structs.ACLTokenBatchGetRequest, reply *struc
 		return err
 	}
 
-	authz, err := a.srv.ResolveToken(args.Token)
+	authz, err := a.srv.ResolveTokenSecret(args.Token)
 	if err != nil {
 		return err
 	}
@@ -796,7 +796,7 @@ func (a *ACL) PolicyBatchRead(args *structs.ACLPolicyBatchGetRequest, reply *str
 		return err
 	}
 
-	authz, err := a.srv.ResolveToken(args.Token)
+	authz, err := a.srv.ResolveTokenSecret(args.Token)
 	if err != nil {
 		return err
 	}
@@ -1182,7 +1182,7 @@ func (a *ACL) RoleBatchRead(args *structs.ACLRoleBatchGetRequest, reply *structs
 		return err
 	}
 
-	authz, err := a.srv.ResolveToken(args.Token)
+	authz, err := a.srv.ResolveTokenSecret(args.Token)
 	if err != nil {
 		return err
 	}
@@ -2115,7 +2115,7 @@ func (a *ACL) Authorize(args *structs.RemoteACLAuthorizationRequest, reply *[]st
 		return err
 	}
 
-	authz, err := a.srv.ResolveToken(args.Token)
+	authz, err := a.srv.ResolveTokenSecret(args.Token)
 	if err != nil {
 		return err
 	}

commit 26820219cd7b448b10dc5d7f567b1a8857102f60

cli: ensure acl token read -self works (#16445)
Fixes a regression in #16044

The consul acl token read -self cli command should not require an -accessor-id because typically the persona invoking this would not already know the accessor id of their own token.

command/acl/token/read/token_read.go
@@ -67,17 +67,6 @@ func (c *cmd) Run(args []string) int {
 		return 1
 	}
 
-	tokenAccessor := c.tokenAccessorID
-	if tokenAccessor == "" {
-		if c.tokenID == "" {
-			c.UI.Error("Must specify the -accessor-id parameter")
-			return 1
-		} else {
-			tokenAccessor = c.tokenID
-			c.UI.Warn("Use the -accessor-id parameter to specify token by Accessor ID")
-		}
-	}
-
 	client, err := c.http.APIClient()
 	if err != nil {
 		c.UI.Error(fmt.Sprintf("Error connecting to Consul agent: %s", err))
@@ -87,6 +76,17 @@ func (c *cmd) Run(args []string) int {
 	var t *api.ACLToken
 	var expanded *api.ACLTokenExpanded
 	if !c.self {
+		tokenAccessor := c.tokenAccessorID
+		if tokenAccessor == "" {
+			if c.tokenID == "" {
+				c.UI.Error("Must specify the -accessor-id parameter")
+				return 1
+			} else {
+				tokenAccessor = c.tokenID
+				c.UI.Warn("Use the -accessor-id parameter to specify token by Accessor ID")
+			}
+		}
+
 		tok, err := acl.GetTokenAccessorIDFromPartial(client, tokenAccessor)
 		if err != nil {
 			c.UI.Error(fmt.Sprintf("Error determining token ID: %v", err))

command/acl/token/read/token_read_test.go
@@ -116,3 +116,50 @@ func TestTokenReadCommand_JSON(t *testing.T) {
 	err = json.Unmarshal([]byte(ui.OutputWriter.String()), &jsonOutput)
 	require.NoError(t, err, "token unmarshalling error")
 }
+
+func TestTokenReadCommand_Self(t *testing.T) {
+	if testing.Short() {
+		t.Skip("too slow for testing.Short")
+	}
+
+	t.Parallel()
+
+	a := agent.NewTestAgent(t, `
+	primary_datacenter = "dc1"
+	acl {
+		enabled = true
+		tokens {
+			initial_management = "root"
+		}
+	}`)
+
+	defer a.Shutdown()
+	testrpc.WaitForLeader(t, a.RPC, "dc1")
+
+	ui := cli.NewMockUi()
+	cmd := New(ui)
+
+	// Create a token
+	client := a.Client()
+
+	token, _, err := client.ACL().TokenCreate(
+		&api.ACLToken{Description: "test"},
+		&api.WriteOptions{Token: "root"},
+	)
+	assert.NoError(t, err)
+
+	args := []string{
+		"-http-addr=" + a.HTTPAddr(),
+		"-token=" + token.SecretID,
+		"-self",
+	}
+
+	code := cmd.Run(args)
+	assert.Equal(t, code, 0)
+	assert.Empty(t, ui.ErrorWriter.String())
+
+	output := ui.OutputWriter.String()
+	assert.Contains(t, output, fmt.Sprintf("test"))
+	assert.Contains(t, output, token.AccessorID)
+	assert.Contains(t, output, token.SecretID)
+}