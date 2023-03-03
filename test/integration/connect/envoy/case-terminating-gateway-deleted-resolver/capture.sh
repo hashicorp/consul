156af61283b1a83fef887daf6f6a8e0a1aabe7ab
Andrew Stucki <andrew.stucki@hashicorp.com>
2023-03-03 09:34:47 -0500

    Use state test and drop integration test

--- test/integration/connect/envoy/case-terminating-gateway-deleted-resolver/capture.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-snapshot_envoy_admin localhost:20000 terminating-gateway primary || true
-snapshot_envoy_admin localhost:19000 s1 primary || true
--- test/integration/connect/envoy/case-terminating-gateway-deleted-resolver/service_s2-v1.hcl
+++
@@ -1,9 +0,0 @@
-services {
-  id   = "s2-v1"
-  name = "s2"
-  port = 8182
-
-  meta {
-    version = "v1"
-  }
-}
