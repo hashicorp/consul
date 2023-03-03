411bdf26b85e0ff11413280ffe190b2b07e46ba4
Ashvitha Sridharan <ashvitha.sridharan@hashicorp.com>
2023-03-03 15:54:16 -0500

    Update the xDS generation to handle HCP metrics

--- api/connect.go
+++ api/connect.go
@@ -1,5 +1,8 @@
 package api
 
+// HCPMetricsCollectorName is the service name for the HCP Metrics Collector
+const HCPMetricsCollectorName string = "hcp-metrics-collector"
+
 // Connect can be used to work with endpoints related to Connect, the
 // feature for securely connecting services within Consul.
 type Connect struct {
