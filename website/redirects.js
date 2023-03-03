14364abd1dfbcd8f7cd56029d6378850094e61f2
trujillo-adam <ajosetru@gmail.com>
2023-03-03 10:53:44 -0800

    additional links to fix

--- website/redirects.js
+++ website/redirects.js
@@ -13,7 +13,7 @@ module.exports = [
   },
   {
     source: '/consul/docs/connect/cluster-peering/k8s',
-    destination: '/consul/docs/k8s/connect/cluster-peering/k8s-tech-specs',
+    destination: '/consul/docs/k8s/connect/cluster-peering/tech-specs',
     permanent: true,
   },
 ]
