89e5da08b55c42e2d9005e9dbb1a553eabccc822
Semir Patel <semir.patel@hashicorp.com>
2023-03-08 13:29:08 -0600

    Bump version for next release

--- version/version.go
+++ version/version.go
@@ -14,7 +14,7 @@ var (
 	//
 	// Version must conform to the format expected by github.com/hashicorp/go-version
 	// for tests to work.
-	Version = "1.14.5"
+	Version = "1.14.6"
 
 	// https://semver.org/#spec-item-10
 	VersionMetadata = ""
