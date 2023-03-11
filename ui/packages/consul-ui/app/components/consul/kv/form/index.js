ea84ea70fb09fc220e04e876fd64ee96a8e8cf6b
valeriia-ruban <valeriia.ruban@hashicorp.com>
2023-03-10 21:55:47 -0800

    upd

--- ui/packages/consul-ui/app/components/consul/kv/form/index.js
+++ ui/packages/consul-ui/app/components/consul/kv/form/index.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
-import { get, set } from '@ember/object';
+import { set } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default Component.extend({
