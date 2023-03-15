1f7023267abd4ce17249477860367f298720ef00
Dhia Ayachi <dhia@hashicorp.com>
2023-03-15 20:18:44 +0000

    backport of commit a2d560517ca76e0848ce5cd16c73d54c0896cec1

--- test/integration/consul-container/test/snapshot/snapshot_restore_test.go
+++ test/integration/consul-container/test/snapshot/snapshot_restore_test.go
@@ -3,7 +3,6 @@ package snapshot
 import (
 	"fmt"
 	"github.com/hashicorp/consul/api"
-	"github.com/hashicorp/consul/sdk/testutil"
 	libcluster "github.com/hashicorp/consul/test/integration/consul-container/libs/cluster"
 	libtopology "github.com/hashicorp/consul/test/integration/consul-container/libs/topology"
 	"github.com/hashicorp/consul/test/integration/consul-container/libs/utils"
@@ -13,6 +12,18 @@ import (
 )
 
 func TestSnapshotRestore(t *testing.T) {
+
+	cases := []libcluster.LogStore{libcluster.LogStore_WAL, libcluster.LogStore_BoltDB}
+
+	for _, c := range cases {
+		t.Run(fmt.Sprintf("test log store: %s", c), func(t *testing.T) {
+			testSnapShotRestoreForLogStore(t, c)
+		})
+	}
+}
+
+func testSnapShotRestoreForLogStore(t *testing.T, logStore libcluster.LogStore) {
+
 	const (
 		numServers = 3
 	)
@@ -25,6 +36,7 @@ func TestSnapshotRestore(t *testing.T) {
 			Datacenter:      "dc1",
 			ConsulImageName: utils.LatestImageName,
 			ConsulVersion:   utils.LatestVersion,
+			LogStore:        logStore,
 		},
 		ApplyDefaultProxySettings: true,
 	})
@@ -33,21 +45,17 @@ func TestSnapshotRestore(t *testing.T) {
 	libcluster.WaitForLeader(t, cluster, client)
 	libcluster.WaitForMembers(t, client, 3)
 
-	testutil.RunStep(t, "Create Data", func(t *testing.T) {
-		for i := 0; i < 100; i++ {
-			_, err := client.KV().Put(&api.KVPair{Key: fmt.Sprintf("key-%d", i), Value: []byte(fmt.Sprintf("value-%d", i))}, nil)
-			require.NoError(t, err)
-		}
-	})
+	for i := 0; i < 100; i++ {
+		_, err := client.KV().Put(&api.KVPair{Key: fmt.Sprintf("key-%d", i), Value: []byte(fmt.Sprintf("value-%d", i))}, nil)
+		require.NoError(t, err)
+	}
 
 	var snapshot io.ReadCloser
-	testutil.RunStep(t, "Save snapshot", func(t *testing.T) {
-		var err error
-		snapshot, _, err = client.Snapshot().Save(nil)
-		require.NoError(t, err)
-	})
+	var err error
+	snapshot, _, err = client.Snapshot().Save(nil)
+	require.NoError(t, err)
 
-	err := cluster.Terminate()
+	err = cluster.Terminate()
 	require.NoError(t, err)
 	// Create a fresh cluster from scratch
 	cluster2, _, _ := libtopology.NewCluster(t, &libtopology.ClusterConfig{
@@ -57,26 +65,25 @@ func TestSnapshotRestore(t *testing.T) {
 			Datacenter:      "dc1",
 			ConsulImageName: utils.LatestImageName,
 			ConsulVersion:   utils.LatestVersion,
+			LogStore:        logStore,
 		},
 		ApplyDefaultProxySettings: true,
 	})
 	client2 := cluster2.APIClient(0)
 
-	testutil.RunStep(t, "Restore saved snapshot", func(t *testing.T) {
-		libcluster.WaitForLeader(t, cluster2, client2)
-		libcluster.WaitForMembers(t, client2, 3)
+	libcluster.WaitForLeader(t, cluster2, client2)
+	libcluster.WaitForMembers(t, client2, 3)
 
-		// Restore the saved snapshot
-		require.NoError(t, client2.Snapshot().Restore(nil, snapshot))
+	// Restore the saved snapshot
+	require.NoError(t, client2.Snapshot().Restore(nil, snapshot))
 
-		libcluster.WaitForLeader(t, cluster2, client2)
-
-	})
+	libcluster.WaitForLeader(t, cluster2, client2)
 
 	for i := 0; i < 100; i++ {
-		kv, _, err := client2.KV().Get(fmt.Sprintf("key-%d", i), nil)
+		kv, _, err := client2.KV().Get(fmt.Sprintf("key-%d", i), &api.QueryOptions{AllowStale: true})
 		require.NoError(t, err)
 		require.Equal(t, kv.Key, fmt.Sprintf("key-%d", i))
 		require.Equal(t, kv.Value, []byte(fmt.Sprintf("value-%d", i)))
 	}
+
 }
