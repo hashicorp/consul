63783744dc3676579d4c335689272ec28ee1fb91
Ronald Ekambi <ronekambi@gmail.com>
2023-03-21 11:06:59 -0400

    copyright headers for agent folder

--- agent/uiserver/dist/assets/consul-lock-sessions/routes-7718d309039e9f8b3b185656b6dd7f05.js
+++ agent/uiserver/dist/assets/consul-lock-sessions/routes-7718d309039e9f8b3b185656b6dd7f05.js
@@ -1,1 +1,6 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 ((s,e=("undefined"!=typeof document?document.currentScript.dataset:module.exports))=>{e.routes=JSON.stringify(s)})({dc:{nodes:{show:{sessions:{_options:{path:"/lock-sessions"}}}}}})
--- agent/uiserver/testdata/foo.js
+++ agent/uiserver/testdata/foo.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 var Foo = {};
 
 Foo.hello = function(){
