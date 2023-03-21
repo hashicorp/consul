e8b821388128c276355eebfe474bc44d53943322
Ronald Ekambi <ronekambi@gmail.com>
2023-03-21 11:06:59 -0400

    copyright headers for agent folder

--- agent/uiserver/dist/assets/consul-nspaces/routes-71c32de6a0307211d1299dac7688bfbf.js
+++ agent/uiserver/dist/assets/consul-nspaces/routes-71c32de6a0307211d1299dac7688bfbf.js
@@ -1,1 +1,6 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 ((e,t=("undefined"!=typeof document?document.currentScript.dataset:module.exports))=>{t.routes=JSON.stringify(e)})({dc:{nspaces:{_options:{path:"/namespaces",abilities:["read nspaces"]},index:{_options:{path:"/",queryParams:{sortBy:"sort",searchproperty:{as:"searchproperty",empty:[["Name","Description","Role","Policy"]]},search:{as:"filter",replace:!0}}}},edit:{_options:{path:"/:name"}},create:{_options:{template:"../edit",path:"/create",abilities:["create nspaces"]}}}}})
--- agent/uiserver/dist/assets/consul-ui/routes-debug-41d0902009004c6875ddb9882b4ee3f6.js
+++ agent/uiserver/dist/assets/consul-ui/routes-debug-41d0902009004c6875ddb9882b4ee3f6.js
@@ -1,1 +1,6 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 ((e,r=("undefined"!=typeof document?document.currentScript.dataset:module.exports))=>{r.routes=JSON.stringify(e)})({"oauth-provider-debug":{_options:{path:"/oauth-provider-debug",queryParams:{redirect_uri:"redirect_uri",response_type:"response_type",scope:"scope"}}}})
