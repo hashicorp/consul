bdd944bd574437d51671fe8d81c5c340647832a2
Ronald Ekambi <ronekambi@gmail.com>
2023-03-21 11:06:59 -0400

    copyright headers for agent folder

--- agent/uiserver/dist/assets/consul-partitions/routes-1bdd3b7ae99c7d7ce0425b2412f10d5e.js
+++ agent/uiserver/dist/assets/consul-partitions/routes-1bdd3b7ae99c7d7ce0425b2412f10d5e.js
@@ -1,1 +1,6 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 ((t,e=("undefined"!=typeof document?document.currentScript.dataset:module.exports))=>{e.routes=JSON.stringify(t)})({dc:{partitions:{_options:{path:"/partitions",abilities:["read partitions"]},index:{_options:{path:"/",queryParams:{sortBy:"sort",searchproperty:{as:"searchproperty",empty:[["Name","Description"]]},search:{as:"filter",replace:!0}}}},edit:{_options:{path:"/:name"}},create:{_options:{template:"../edit",path:"/create",abilities:["create partitions"]}}}}})
