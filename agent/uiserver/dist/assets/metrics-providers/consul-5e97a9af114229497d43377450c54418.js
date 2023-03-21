63d1d0c9f02dcc7d7635300bb6e902aeb9be57ce
Ronald Ekambi <ronekambi@gmail.com>
2023-03-21 11:06:59 -0400

    copyright headers for agent folder

--- agent/uiserver/dist/assets/metrics-providers/consul-5e97a9af114229497d43377450c54418.js
+++ agent/uiserver/dist/assets/metrics-providers/consul-5e97a9af114229497d43377450c54418.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 (function(r){const e=["init","serviceRecentSummarySeries","serviceRecentSummaryStats","upstreamRecentSummaryStats","downstreamRecentSummaryStats"]
 r.consul=new class{constructor(){this.registry={},this.providers={}}registerMetricsProvider(r,t){for(var i of e)if("function"!=typeof t[i])throw new Error(`Can't register metrics provider '${r}': missing ${i} method.`)
 this.registry[r]=t}getMetricsProvider(r,e){if(!(r in this.registry))throw new Error(`Metrics Provider '${r}' is not registered.`)
