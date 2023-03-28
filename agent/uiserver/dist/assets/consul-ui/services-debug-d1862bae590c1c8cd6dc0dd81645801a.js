9846c659dc509c417dd6f1f5618ac906705d47b8
Ronald Ekambi <ronekambi@gmail.com>
2023-03-28 07:33:13 -0400

    fix proto files and remove headers in agent/uiserver folder

--- agent/uiserver/dist/assets/consul-ui/services-debug-d1862bae590c1c8cd6dc0dd81645801a.js
+++ agent/uiserver/dist/assets/consul-ui/services-debug-d1862bae590c1c8cd6dc0dd81645801a.js
@@ -1,6 +1,1 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 ((e,i=("undefined"!=typeof document?document.currentScript.dataset:module.exports))=>{i.services=JSON.stringify(e)})({"route:application":{class:"consul-ui/routing/application-debug"},"service:intl":{class:"consul-ui/services/i18n-debug"}})
