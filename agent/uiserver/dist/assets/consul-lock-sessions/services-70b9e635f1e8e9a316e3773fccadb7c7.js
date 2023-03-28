8c76d168fd4ddb789c66b450b67aabb6bb19a9e1
Ronald Ekambi <ronekambi@gmail.com>
2023-03-28 14:53:10 -0400

    fix merge conflicts

--- agent/uiserver/dist/assets/consul-lock-sessions/services-70b9e635f1e8e9a316e3773fccadb7c7.js
+++ agent/uiserver/dist/assets/consul-lock-sessions/services-70b9e635f1e8e9a316e3773fccadb7c7.js
@@ -1,6 +1,1 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 ((e,t=("undefined"!=typeof document?document.currentScript.dataset:module.exports))=>{t.services=JSON.stringify(e)})({})
--- agent/uiserver/dist/assets/consul-partitions/services-1a3b6937a8bc5f6e68df884b1650eaf0.js
+++ agent/uiserver/dist/assets/consul-partitions/services-1a3b6937a8bc5f6e68df884b1650eaf0.js
@@ -1,6 +1,1 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 ((e,t=("undefined"!=typeof document?document.currentScript.dataset:module.exports))=>{t.services=JSON.stringify(e)})({"component:consul/partition/selector":{class:"consul-ui/components/consul/partition/selector"}})
