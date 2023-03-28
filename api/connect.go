4b2137c2faa7375ebf09b56ac6f32ffbbf964576
Ronald <roncodingenthusiast@users.noreply.github.com>
2023-03-28 21:12:41 +0100

    Add copyright headers for acl, api and bench folders (#16706)
    * copyright headers for agent folder
    
    * Ignore test data files
    
    * fix proto files and remove headers in agent/uiserver folder
    
    * ignore deep-copy files
    
    * copyright headers for agent folder
    
    * fix merge conflicts
    
    * copyright headers for agent folder
    
    * Ignore test data files
    
    * fix proto files
    
    * ignore agent/uiserver folder for now
    
    * copyright headers for agent folder
    
    * Add copyright headers for acl, api and bench folders

--- api/connect.go
+++ api/connect.go
@@ -1,3 +1,6 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 package api
 
 // HCPMetricsCollectorName is the service name for the HCP Metrics Collector
--- api/operator_keyring.go
+++ api/operator_keyring.go
@@ -1,3 +1,6 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 package api
 
 // keyringRequest is used for performing Keyring operations
