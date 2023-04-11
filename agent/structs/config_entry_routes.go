722a8e4b6ebd130cce47d0c4e2f401801af8523b
hc-github-team-consul-core <github-team-consul-core@hashicorp.com>
2023-04-11 12:28:25 -0400

    Backport of APIGW: Routes with duplicate parents should be invalid into release/1.15.x (#16962)
    * backport of commit 7a3d2d8669a4f424d46f12c8b2abd0712a0a5950
    
    * backport of commit ec3bcfe2bf7f9e241322979f45bad65310c55dbe
    
    * backport of commit ec34e3f5e82b5338a00a7b3b85c47778e179cb1b
    
    ---------
    
    Co-authored-by: jm96441n <john.maguire@hashicorp.com>

--- agent/structs/config_entry_routes.go
+++ agent/structs/config_entry_routes.go
@@ -1,6 +1,7 @@
 package structs
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 
@@ -140,10 +141,17 @@ func (e *HTTPRouteConfigEntry) Validate() error {
 		APIGateway: true,
 	}
 
+	uniques := make(map[ResourceReference]struct{}, len(e.Parents))
+
 	for _, parent := range e.Parents {
 		if !validParentKinds[parent.Kind] {
 			return fmt.Errorf("unsupported parent kind: %q, must be 'api-gateway'", parent.Kind)
 		}
+
+		if _, ok := uniques[parent]; ok {
+			return errors.New("route parents must be unique")
+		}
+		uniques[parent] = struct{}{}
 	}
 
 	if err := validateConfigEntryMeta(e.Meta); err != nil {
@@ -531,10 +539,19 @@ func (e *TCPRouteConfigEntry) Validate() error {
 	if len(e.Services) > 1 {
 		return fmt.Errorf("tcp-route currently only supports one service")
 	}
+
+	uniques := make(map[ResourceReference]struct{}, len(e.Parents))
+
 	for _, parent := range e.Parents {
 		if !validParentKinds[parent.Kind] {
 			return fmt.Errorf("unsupported parent kind: %q, must be 'api-gateway'", parent.Kind)
 		}
+
+		if _, ok := uniques[parent]; ok {
+			return errors.New("route parents must be unique")
+		}
+		uniques[parent] = struct{}{}
+
 	}
 
 	if err := validateConfigEntryMeta(e.Meta); err != nil {
