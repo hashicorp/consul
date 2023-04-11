f8d53be8b4b42523eea8ff578725a137a249cebe
20sr20 <sujata@hashicorp.com>
2023-04-11 10:08:33 -0700

    Consolidated export_oss_test.go and export_test.go

--- command/services/export/export_test.go
+++ command/services/export/export_test.go
@@ -30,7 +30,19 @@ func TestExportCommand(t *testing.T) {
 	a := agent.NewTestAgent(t, ``)
 	t.Cleanup(func() { _ = a.Shutdown() })
 	testrpc.WaitForTestAgent(t, a.RPC, "dc1")
+	t.Run("peer is required", func(t *testing.T) {
 
+		ui := cli.NewMockUi()
+		cmd := New(ui)
+
+		args := []string{
+			"-name=testservice",
+		}
+
+		code := cmd.Run(args)
+		require.Equal(t, 1, code, "err: %s", ui.ErrorWriter.String())
+		require.Contains(t, ui.ErrorWriter.String(), "Missing the required -consumer-peers flag")
+	})
 	t.Run("service name is required", func(t *testing.T) {
 
 		ui := cli.NewMockUi()
