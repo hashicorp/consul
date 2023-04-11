966d062383fbcd1d33d9827c0656cac4fabbd123
Derek Menteer <derek.menteer@hashicorp.com>
2023-04-11 08:49:52 -0500

    Remove deprecated service-defaults upstream behavior.
    Prior to this change, peer services would be targeted by service-default
    overrides as long as the new `peer` field was not found in the config entry.
    This commit removes that deprecated backwards-compatibility behavior. Now
    it is necessary to specify the `peer` field in order for upstream overrides
    to apply to a peer upstream.

--- agent/service_manager_test.go
+++ agent/service_manager_test.go
@@ -829,9 +829,6 @@ func fixPersistedServiceConfigForTest(content []byte) ([]byte, error) {
 		return nil, err
 	}
 	// Sort the output, since it's randomized and causes flaky tests otherwise.
-	sort.Slice(parsed.Defaults.UpstreamIDConfigs, func(i, j int) bool {
-		return parsed.Defaults.UpstreamIDConfigs[i].Upstream.ID < parsed.Defaults.UpstreamIDConfigs[j].Upstream.ID
-	})
 	sort.Slice(parsed.Defaults.UpstreamConfigs, func(i, j int) bool {
 		return parsed.Defaults.UpstreamConfigs[i].Upstream.String() < parsed.Defaults.UpstreamConfigs[j].Upstream.String()
 	})
