6cb1579313608a34e5994973de361a959dca8972
Nick Irvine <115657443+nfi-hashicorp@users.noreply.github.com>
2023-04-12 16:04:12 -0700

    test many clusters

---
+++ test/integration/consul-container/test_many_clusters/many_clusters_test.go
@@ -0,0 +1,28 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/consul/test/integration/consul-container/libs/cluster"
+	"github.com/hashicorp/consul/test/integration/consul-container/libs/topology"
+	"github.com/hashicorp/consul/test/integration/consul-container/libs/utils"
+)
+
+func TestManyClusters(t *testing.T) {
+	const n = 100
+	for i := 0; i < n; i++ {
+		t.Run(fmt.Sprintf("cluster %d", i), func(t *testing.T) {
+			topology.NewCluster(t, &topology.ClusterConfig{
+				NumServers: 1,
+				NumClients: 1,
+				BuildOpts: &cluster.BuildOptions{
+					Datacenter:    fmt.Sprintf("dc%d", i),
+					ConsulVersion: utils.TargetVersion,
+				},
+				ApplyDefaultProxySettings: true,
+			})
+		})
+
+	}
+}
