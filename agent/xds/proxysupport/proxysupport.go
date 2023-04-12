9ce63a0df3af6f929630c161fb9f4a24f1a6e3a1
Nathan Coleman <nathan.coleman@hashicorp.com>
2023-04-12 17:42:54 -0400

    Update list of Envoy versions (#16891)
    * Update list of Envoy versions
    
    * Add changelog entry
    
    * Add newly-released Envoy 1.23.8
    
    * Add newly-released Envoy version 1.22.11

--- agent/xds/proxysupport/proxysupport.go
+++ agent/xds/proxysupport/proxysupport.go
@@ -7,8 +7,8 @@ package proxysupport
 //
 // see: https://www.consul.io/docs/connect/proxies/envoy#supported-versions
 var EnvoyVersions = []string{
-	"1.23.1",
-	"1.22.5",
-	"1.21.5",
+	"1.23.8",
+	"1.22.11",
+	"1.21.6",
 	"1.20.7",
 }
