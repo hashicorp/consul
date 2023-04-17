e1600245bb98bf64b2e284430098456b8705ccd0
John Eikenberry <jae@zhar.net>
2023-04-17 16:15:45 -0700

    add mocking tests

--- test/integration/matrix/download.go
+++ test/integration/matrix/download.go
@@ -8,21 +8,20 @@ import (
 	"strings"
 )
 
+// isolate data fetching functions to fake for testing
+var aboutRelease = func(product, version string) []byte {
+	url := "https://api.releases.hashicorp.com/v1/releases/%s/%s"
+	return getBody(fmt.Sprintf(url, product, version))
+}
+var lastTwenty = func(product, _ string) []byte {
+	url := "https://api.releases.hashicorp.com/v1/releases/%s?license_class=oss&limit=20"
+	return getBody(fmt.Sprintf(url, product))
+}
+
 // downloadURL returns the URL (string) to the (zipped) binary for AMD64
 // for a given product and version (eg. vault and 1.13.2).
 func downloadURL(product, version string) string {
-	releaseURL := "https://api.releases.hashicorp.com/v1/releases/%s/%s"
-	resp, err := http.Get(fmt.Sprintf(releaseURL, product, version))
-	if err != nil {
-		panic(err)
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	resp.Body.Close()
-
+	jsonBody := aboutRelease(product, version)
 	type build struct {
 		Arch, OS, URL string
 	}
@@ -30,7 +29,7 @@ func downloadURL(product, version string) string {
 		Builds []build
 	}
 	var rel releasesJSON
-	if err := json.Unmarshal(body, &rel); err != nil {
+	if err := json.Unmarshal(jsonBody, &rel); err != nil {
 		panic(err)
 	}
 
@@ -46,29 +45,20 @@ func downloadURL(product, version string) string {
 // latestReleases for 'product' it checks throught the last 20 releases for the
 // 3 most recent minor versions (semantic versioning, major.MINOR.micro).
 func latestReleases(product string) []string {
-	lastTwenty := "https://api.releases.hashicorp.com/v1/releases/%s?license_class=oss&limit=20"
-	resp, err := http.Get(fmt.Sprintf(lastTwenty, product))
-	if err != nil {
-		panic(err)
-	}
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	resp.Body.Close()
+	jsonBody := lastTwenty(product, "")
 
 	type jsonVersion struct {
 		Version string
 	}
 	var versions []jsonVersion
-	if err := json.Unmarshal(body, &versions); err != nil {
+	if err := json.Unmarshal(jsonBody, &versions); err != nil {
 		panic(err)
 	}
 
 	unique := make(map[string]struct{}, 3)
 	result := []string{}
 	for _, v := range versions {
-		if len(unique) > 3 {
+		if len(unique) >= 3 {
 			break
 		}
 		vl := strings.Split(v.Version, ".")
@@ -80,4 +70,18 @@ func latestReleases(product string) []string {
 	}
 
 	return result
+}
+
+func getBody(url string) []byte {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	resp.Body.Close()
+	return body
 }
--- test/integration/matrix/download_test.go
+++ test/integration/matrix/download_test.go
@@ -3,30 +3,65 @@ package test
 import (
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
+
+const testVersions = `[{"version":"1.1.0"},{"version":"1.2.0"},{"version":"1.3.0"},{"version":"1.4.0"},{"version":"1.5.0"}]`
+const testDownload = `{ "builds": [{"arch": "amd64", "os": "linux", "url": "goodurl" }, {"arch": "amd64", "os": "freebsd", "url": "goodurl" }, {"arch": "risc-v", "os": "linux", "url": "badurl"}, {"arch": "arm64", "os": "openbsd", "url": "badurl" }]}`
 
 func TestDownload(t *testing.T) {
 
+	// calls API
 	t.Run("latestReleases", func(t *testing.T) {
-		// should return ~3 entries
 		consuls := latestReleases("consul")
-		if len(consuls) < 2 || len(consuls) > 4 {
+		if len(consuls) != 3 {
 			t.Errorf("weird number of versions?? `%v`", consuls)
 		}
-		// should return ~3 entries
 		vaults := latestReleases("vault")
-		if len(vaults) < 2 || len(vaults) > 4 {
+		if len(vaults) != 3 {
 			t.Errorf("weird number of versions?? `%v`", vaults)
 		}
 	})
 
+	// calls API
 	t.Run("downloadURL", func(t *testing.T) {
 		for _, v := range latestReleases("vault") {
+			v := v
 			url := downloadURL("vault", v)
-			if !strings.HasPrefix(url, "http") || !strings.HasSuffix(url, "zip") {
+			if !(strings.HasPrefix(url, "http") && strings.HasSuffix(url, "zip")) && url != "goodurl" {
 				t.Errorf("had url: %s", url)
 			}
 		}
 	})
 
+	// Table drivin mocked coverage to verify behavior
+	//
+	defer func(lt, ar func(string, string) []byte) {
+		lastTwenty = lt
+		aboutRelease = ar
+	}(lastTwenty, aboutRelease)
+	// comment these 2 out to run against external (actual) API
+	lastTwenty = func(string, string) []byte { return []byte(testVersions) }
+	//aboutRelease = func(string, string) []byte { return []byte(testDownload) }
+
+	t.Run("latestReleases-table-test", func(t *testing.T) {
+		tests := []struct {
+			name, json string
+			expected   []string
+		}{
+			{name: "base", json: testVersions, expected: []string{"1.1.0", "1.2.0", "1.3.0"}},
+			{name: "multiple-1.1s", json: `[{"version":"1.1.0"},{"version":"1.1.1"},{"version":"1.2.0"},{"version":"1.2.1"},{"version":"1.3.0"}]`, expected: []string{"1.1.0", "1.2.0", "1.3.0"}},
+			{name: "duplicates", json: `[{"version":"1.1.0"},{"version":"1.1.0"},{"version":"1.2.0"},{"version":"1.2.0"},{"version":"1.3.0"}]`, expected: []string{"1.1.0", "1.2.0", "1.3.0"}},
+			{name: "no-third", json: `[{"version":"1.1.0"},{"version":"1.1.1"},{"version":"1.2.0"}]`, expected: []string{"1.1.0", "1.2.0"}},
+		}
+		for _, tc := range tests {
+			tc := tc
+			t.Run(tc.name, func(t *testing.T) {
+				lastTwenty = func(string, string) []byte { return []byte(tc.json) }
+				result := latestReleases("dummy")
+				require.EqualValues(t, tc.expected, result)
+			})
+		}
+	})
 }
