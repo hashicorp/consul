34da823416f0ad6a355b8e7952caf8fd3bea0db0
John Eikenberry <jae@zhar.net>
2023-04-19 14:27:26 -0700

    added cleanup code and refactored a bit

--- test/integration/matrix/main_test.go
+++ test/integration/matrix/main_test.go
@@ -9,72 +9,112 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-const (
-	rootPath   = "root"
-	intrPath   = "intermediate"
-	leafPath   = "leaf"
-	policyName = "ca"
-)
-
 func TestMatrix(t *testing.T) {
-	// for version-matrix
-	// run consul version
-	// run vault version
-	// test..
-	//     start vault/consul
+	matrix := NewMatrix()
+	for i := 0; i < 10; i++ {
+		fmt.Printf("===============================================>%v\n\n\n", i)
+		consul, vault, more := matrix.NextPair(t)
+		if !more {
+			return
+		}
+		t.Run("demo", func(t *testing.T) {
+			demo(t, consul, vault)
+		})
+		consul.Stop()
+		vault.Stop()
+	}
+}
+
+func TestDemo(t *testing.T) {
 	vault := NewTestVaultServer(t, "vault")
 	defer vault.Stop()
 	consul := NewTestConsulServer(t, "consul")
 	defer consul.Stop()
-	//     config vault/consul
-	setup := func(t *testing.T, v TestVaultServer, c TestConsulServer) {
-		// vault
-		err := v.Client().Sys().Mount(rootPath+"/", &vapi.MountInput{Type: "pki"})
-		require.NoError(t, err)
-		err = v.Client().Sys().Mount(intrPath+"/", &vapi.MountInput{Type: "pki"})
-		require.NoError(t, err)
-		err = v.Client().Sys().PutPolicy(policyName, policyRules)
-		require.NoError(t, err)
-		secret, err := v.Client().Auth().Token().Create(
-			&vapi.TokenCreateRequest{Policies: []string{"ca"}})
-		token := secret.Auth.ClientToken
-		_ = token
-		require.NoError(t, err)
-		// consul
-		// XXX why can't I use `token` here???
-		_, err = c.Client().Connect().CASetConfig(
-			caConf(v.Addr, v.RootToken), nil)
-		require.NoError(t, err)
-		caconf, _, err := c.Client().Connect().CAGetConfig(nil)
-		require.NoError(t, err)
-		fmt.Printf("%#v\n\n", caconf)
-		roots, _, err := c.Client().Agent().ConnectCARoots(nil)
-		require.NoError(t, err)
-		fmt.Printf("%#v\n\n", roots.Roots[0])
-		leaf, md, err := c.Client().Agent().ConnectCALeaf(leafPath, nil)
-		require.NoError(t, err)
-		fmt.Printf("%#v\n\n", leaf)
-		fmt.Printf("%#v\n\n", md)
-		leaf, md, err = c.Client().Agent().ConnectCALeaf(leafPath, nil)
-		require.NoError(t, err)
-		fmt.Printf("%#v\n\n", md)
-		fmt.Printf("%#v\n\n", leaf)
-		out, err := exec.Command("curl", "-s", "-verbose", "--header",
-			"X-Consul-Token: "+v.RootToken,
-			c.HTTPAddr+"/v1/agent/connect/ca/leaf/leaf").CombinedOutput()
-		require.NoError(t, err)
-		fmt.Printf("%s\n", out)
-		out, err = exec.Command("curl", "-s", "-verbose", "--header",
-			"X-Consul-Token: "+v.RootToken,
-			c.HTTPAddr+"/v1/agent/connect/ca/leaf/leaf").CombinedOutput()
-		require.NoError(t, err)
-		fmt.Printf("%s\n", out)
-	}
-	setup(t, vault, consul)
-	//     run tests
-	//     cleanup vault
-	//     cleanup consul
+
+	t.Run("demo", func(t *testing.T) {
+		demo(t, consul, vault)
+	})
 }
+
+// Vault as a Consul Service Mesh Certificate Authority demo in code
+func demo(t *testing.T, c TestConsulServer, v TestVaultServer) {
+	// vault setup
+	err := v.Client().Sys().Mount(rootPath+"/", &vapi.MountInput{Type: "pki"})
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		err := v.Client().Sys().Unmount(rootPath + "/")
+		require.NoError(t, err)
+	})
+	err = v.Client().Sys().Mount(intrPath+"/", &vapi.MountInput{Type: "pki"})
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		err := v.Client().Sys().Unmount(intrPath + "/")
+		require.NoError(t, err)
+	})
+	err = v.Client().Sys().PutPolicy(policyName, policyRules)
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		err := v.Client().Sys().DeletePolicy(policyName)
+		require.NoError(t, err)
+	})
+	secret, err := v.Client().Auth().Token().Create(
+		&vapi.TokenCreateRequest{Policies: []string{policyName}})
+	token := secret.Auth.ClientToken
+	t.Cleanup(func() {
+		err := v.Client().Auth().Token().RevokeTree(token)
+		require.NoError(t, err)
+	})
+	require.NoError(t, err)
+	// consul setup
+	// XXX shouldn't the above created token work here?
+	_, err = c.Client().Connect().CASetConfig(
+		caConf(v.Addr, v.RootToken, rootPath, intrPath), nil)
+	require.NoError(t, err)
+	// can't undo this... maybe add note that new tests that touch this
+	// will need to overwrite other setups
+
+	// tests
+	caconf, _, err := c.Client().Connect().CAGetConfig(nil)
+	require.NoError(t, err)
+	require.Equal(t, caconf.Provider, "vault")
+	roots, _, err := c.Client().Agent().ConnectCARoots(nil)
+	require.NoError(t, err)
+	require.Len(t, roots.Roots, 2)
+	leaf, _, err := c.Client().Agent().ConnectCALeaf(leafPath, nil)
+	require.NoError(t, err)
+	certpem1 := leaf.CertPEM
+	require.Contains(t, certpem1, "CERTIFICATE")
+	require.Contains(t, leaf.PrivateKeyPEM, "PRIVATE")
+	leaf, _, err = c.Client().Agent().ConnectCALeaf(leafPath, nil)
+	require.NoError(t, err)
+	certpem2 := leaf.CertPEM
+	require.Contains(t, certpem2, "CERTIFICATE")
+	require.Contains(t, leaf.PrivateKeyPEM, "PRIVATE")
+	require.Equal(t, certpem1, certpem2)
+
+	// curlTests(t, c, v)
+}
+
+// tutorial's curl commands
+func curlTests(t *testing.T, c TestConsulServer, v TestVaultServer) {
+	out, err := exec.Command("curl", "-s", "-verbose", "--header",
+		"X-Consul-Token: "+v.RootToken,
+		c.HTTPAddr+"/v1/agent/connect/ca/leaf/leaf").CombinedOutput()
+	require.NoError(t, err)
+	fmt.Printf("%s\n", out)
+	out, err = exec.Command("curl", "-s", "-verbose", "--header",
+		"X-Consul-Token: "+v.RootToken,
+		c.HTTPAddr+"/v1/agent/connect/ca/leaf/leaf").CombinedOutput()
+	require.NoError(t, err)
+	fmt.Printf("%s\n", out)
+}
+
+const (
+	rootPath   = "connect_root"
+	intrPath   = "connect_dc1_inter"
+	leafPath   = "leaf"
+	policyName = "ca"
+)
 
 const policyRules = `
 path "/sys/mounts/connect_root" {
