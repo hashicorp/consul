8324ccf447246c8d1995ce6a7787f91ada86fa26
R.B. Boyer <rb@hashicorp.com>
2023-04-24 16:20:53 -0500

    retry certain errors

--- sprawl/configentries.go
+++ sprawl/configentries.go
@@ -2,6 +2,10 @@ package sprawl
 
 import (
 	"fmt"
+	"strings"
+	"time"
+
+	"github.com/hashicorp/consul/api"
 
 	"github.com/hashicorp/consul-topology/topology"
 )
@@ -19,6 +23,18 @@ func (s *Sprawl) populateInitialConfigEntries(cluster *topology.Cluster) error {
 	for _, ce := range cluster.InitialConfigEntries {
 		_, _, err := client.ConfigEntries().Set(ce, nil)
 		if err != nil {
+			if ce.GetKind() == api.ServiceIntentions && strings.Contains(err.Error(), intentionsMigrationError) {
+				logger.Warn("known error writing initial config entry; trying again",
+					"kind", ce.GetKind(),
+					"name", ce.GetName(),
+					"namespace", ce.GetNamespace(),
+					"partition", ce.GetPartition(),
+					"error", err,
+				)
+
+				time.Sleep(500 * time.Millisecond)
+				continue
+			}
 			return fmt.Errorf(
 				"error persisting config entry kind=%q name=%q namespace=%q partition=%q: %w",
 				ce.GetKind(),
@@ -38,3 +54,5 @@ func (s *Sprawl) populateInitialConfigEntries(cluster *topology.Cluster) error {
 
 	return nil
 }
+
+const intentionsMigrationError = `Intentions are read only while being upgraded to config entries`
