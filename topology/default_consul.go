674ae98d09504c48117bccd224c67df6b1e92ecb
R.B. Boyer <rb@hashicorp.com>
2023-04-26 14:17:35 -0500

    bump versions

--- topology/default_consul.go
+++ topology/default_consul.go
@@ -1,4 +1,4 @@
 package topology
 
-const DefaultConsulImage = "hashicorp/consul:1.15.1"
-const DefaultConsulEnterpriseImage = "hashicorp/consul-enterprise:1.15.1-ent"
+const DefaultConsulImage = "hashicorp/consul:1.15.2"
+const DefaultConsulEnterpriseImage = "hashicorp/consul-enterprise:1.15.2-ent"
