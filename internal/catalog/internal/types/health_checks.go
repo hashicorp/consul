32cde9380454a381d500fc62fc327370aa49dbe0
Matt Keeler <mjkeeler7@gmail.com>
2023-05-02 17:09:16 -0400

    Add type validations for the catalog resources
    Also adding some common resource validation error types to the internal/resource package.

--- internal/catalog/internal/types/health_checks.go
+++ internal/catalog/internal/types/health_checks.go
@@ -7,6 +7,7 @@ import (
 	"github.com/hashicorp/consul/internal/resource"
 	pbcatalog "github.com/hashicorp/consul/proto-public/pbcatalog/v1alpha1"
 	"github.com/hashicorp/consul/proto-public/pbresource"
+	"github.com/hashicorp/go-multierror"
 )
 
 const (
@@ -27,6 +28,71 @@ func RegisterHealthChecks(r resource.Registry) {
 	r.Register(resource.Registration{
 		Type:     HealthChecksV1Alpha1Type,
 		Proto:    &pbcatalog.HealthChecks{},
-		Validate: nil,
+		Validate: ValidateHealthChecks,
 	})
+}
+
+func ValidateHealthChecks(res *pbresource.Resource) error {
+	var checks pbcatalog.HealthChecks
+
+	if err := res.Data.UnmarshalTo(&checks); err != nil {
+		return resource.NewErrDataParse(&checks, err)
+	}
+
+	var err error
+
+	// Validate the workload selector
+	if selErr := validateSelector(checks.Workloads, false); selErr != nil {
+		err = multierror.Append(err, resource.ErrInvalidField{
+			Name:    "workloads",
+			Wrapped: selErr,
+		})
+	}
+
+	// Validate each check
+	for idx, check := range checks.HealthChecks {
+		if checkErr := validateCheck(check); checkErr != nil {
+			err = multierror.Append(err, resource.ErrInvalidListElement{
+				Name:    "checks",
+				Index:   idx,
+				Wrapped: checkErr,
+			})
+		}
+	}
+
+	return err
+}
+
+func validateCheck(check *pbcatalog.HealthCheck) error {
+	var err error
+	// Validate the check name
+	if check.Name == "" {
+		err = multierror.Append(err, resource.ErrInvalidField{
+			Name:    "name",
+			Wrapped: resource.ErrMissing,
+		})
+	} else if !isValidDNSLabel(check.Name) {
+		err = multierror.Append(err, resource.ErrInvalidField{
+			Name:    "name",
+			Wrapped: errNotDNSLabel,
+		})
+	}
+
+	// Validate the definition
+	if check.Definition == nil {
+		err = multierror.Append(err, resource.ErrInvalidField{
+			Name:    "definition",
+			Wrapped: resource.ErrMissing,
+		})
+	}
+
+	// In theory it would be nice to validate the individual check definition.
+	// However whether a check is valid will be up for interpretation by
+	// the check executor. The executor may default some addressing,
+	// allow for templating etc. Therefore we cannot really know at admission
+	// time whether the check will be executable. Therefore it is expected
+	// that check executors will update the status of the resource to note
+	// whether it was valid for that executor.
+
+	return err
 }
--- internal/catalog/internal/types/service_endpoints.go
+++ internal/catalog/internal/types/service_endpoints.go
@@ -4,9 +4,12 @@
 package types
 
 import (
+	"math"
+
 	"github.com/hashicorp/consul/internal/resource"
 	pbcatalog "github.com/hashicorp/consul/proto-public/pbcatalog/v1alpha1"
 	"github.com/hashicorp/consul/proto-public/pbresource"
+	"github.com/hashicorp/go-multierror"
 )
 
 const (
@@ -29,4 +32,88 @@ func RegisterServiceEndpoints(r resource.Registry) {
 		Proto:    &pbcatalog.ServiceEndpoints{},
 		Validate: nil,
 	})
+}
+
+func ValidateServiceEndpoints(res *pbresource.Resource) error {
+	var svcEndpoints pbcatalog.ServiceEndpoints
+
+	if err := res.Data.UnmarshalTo(&svcEndpoints); err != nil {
+		return resource.NewErrDataParse(&svcEndpoints, err)
+	}
+
+	var err error
+	for idx, endpoint := range svcEndpoints.Endpoints {
+		if endpointErr := validateEndpoint(endpoint, res); endpointErr != nil {
+			err = multierror.Append(err, resource.ErrInvalidListElement{
+				Name:    "endpoints",
+				Index:   idx,
+				Wrapped: endpointErr,
+			})
+		}
+	}
+
+	return err
+}
+
+func validateEndpoint(endpoint *pbcatalog.Endpoint, res *pbresource.Resource) error {
+	var err error
+
+	// Validate the target ref if not nil. When it is nil we are assuming that
+	// the endpoints are being managed for an external service that has no
+	// corresponding workloads that Consul has knowledge of.
+	if endpoint.TargetRef != nil {
+		// Validate the target reference
+		if refErr := validateReference(WorkloadType, res.Id.GetTenancy(), endpoint.TargetRef); refErr != nil {
+			err = multierror.Append(err, resource.ErrInvalidField{
+				Name:    "target_ref",
+				Wrapped: refErr,
+			})
+		}
+	}
+
+	// Validate the endpoint Addresses
+	for addrIdx, addr := range endpoint.Addresses {
+		if addrErr := validateWorkloadAddress(addr, endpoint.Ports); addrErr != nil {
+			err = multierror.Append(err, resource.ErrInvalidListElement{
+				Name:    "addresses",
+				Index:   addrIdx,
+				Wrapped: addrErr,
+			})
+		}
+	}
+
+	// Ensure that the endpoint has at least 1 port.
+	if len(endpoint.Ports) < 1 {
+		err = multierror.Append(err, resource.ErrInvalidField{
+			Name:    "ports",
+			Wrapped: resource.ErrEmpty,
+		})
+	}
+
+	// Validate the endpoints ports
+	for portName, port := range endpoint.Ports {
+		// Port names must be DNS labels
+		if portNameErr := validatePortName(portName); portNameErr != nil {
+			err = multierror.Append(err, resource.ErrInvalidMapKey{
+				Map:     "ports",
+				Key:     portName,
+				Wrapped: portNameErr,
+			})
+		}
+
+		// As the physical port is the real port the endpoint will be bound to
+		// it must be in the standard 1-65535 range.
+		if port.Port < 1 || port.Port > math.MaxUint16 {
+			err = multierror.Append(err, resource.ErrInvalidMapValue{
+				Map: "ports",
+				Key: portName,
+				Wrapped: resource.ErrInvalidField{
+					Name:    "phsical_port",
+					Wrapped: errInvalidPhysicalPort,
+				},
+			})
+		}
+	}
+
+	return err
 }
