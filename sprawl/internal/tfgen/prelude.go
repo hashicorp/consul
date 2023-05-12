f30c0df4577af662d6c62dce17908386b055cedf
Nick Irvine <115657443+nfi-hashicorp@users.noreply.github.com>
2023-05-12 06:47:47 -0700

    use any 2.x version of kreuzwerker/docker (#2)

--- sprawl/internal/tfgen/prelude.go
+++ sprawl/internal/tfgen/prelude.go
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "2.11.0"
+      version = "~> 2.0"
     }
   }
   required_version = ">= 0.13"
