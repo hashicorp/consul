5ad357051bc860f6fd4d21a3514851239e429604
Hans Hasselberg <hans@hashicorp.com>
2023-05-12 16:36:43 +0200

    fmt

--- agent/hcp/client.go
+++ agent/hcp/client.go
@@ -80,7 +80,7 @@ func httpClient(c config.CloudConfig) (*httptransport.Runtime, error) {
 }
 
 func (c *hcpClient) FetchBootstrap(ctx context.Context) (*BootstrapConfig, error) {
-  version := version.GetHumanVersion()
+	version := version.GetHumanVersion()
 	params := hcpgnm.NewAgentBootstrapConfigParamsWithContext(ctx).
 		WithID(c.resource.ID).
 		WithLocationOrganizationID(c.resource.Organization).
