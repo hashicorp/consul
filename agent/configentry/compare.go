3aab58ab41857559f48e3a268192f3dedd5dc5df
R.B. Boyer <rb@hashicorp.com>
2023-05-23 11:37:03 -0500

    extract some config entry helpers into package

---
+++ agent/configentry/compare.go
@@ -0,0 +1,37 @@
+package configentry
+
+import (
+	"sort"
+
+	"github.com/hashicorp/consul/agent/structs"
+)
+
+func SortSlice(configs []structs.ConfigEntry) {
+	sort.SliceStable(configs, func(i, j int) bool {
+		return Less(configs[i], configs[j])
+	})
+}
+
+func Less(first structs.ConfigEntry, second structs.ConfigEntry) bool {
+	if first.GetKind() < second.GetKind() {
+		return true
+	}
+	if first.GetKind() > second.GetKind() {
+		return false
+	}
+
+	if first.GetEnterpriseMeta().LessThan(second.GetEnterpriseMeta()) {
+		return true
+	}
+	if second.GetEnterpriseMeta().LessThan(first.GetEnterpriseMeta()) {
+		return false
+	}
+
+	return first.GetName() < second.GetName()
+}
+
+func EqualID(e1, e2 structs.ConfigEntry) bool {
+	return e1.GetKind() == e2.GetKind() &&
+		e1.GetEnterpriseMeta().IsSame(e2.GetEnterpriseMeta()) &&
+		e1.GetName() == e2.GetName()
+}
