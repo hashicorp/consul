ddae32e75209994edaf0c2dc4ba5e8b5aec9618e
skpratt <sarah.pratt@hashicorp.com>
2023-05-29 11:14:49 -0500

    split out ent test

--- command/version/formatter_test.go
+++ command/version/formatter_test.go
@@ -36,32 +36,18 @@ func golden(t *testing.T, name, got string) string {
 
 func TestFormat(t *testing.T) {
 	buildDate, _ := time.Parse(time.RFC3339, "2022-06-01T13:18:45Z")
-	info := map[string]VersionInfo{
-		"normal": {
-			HumanVersion: "1.99.3-beta1",
-			Version:      "1.99.3",
-			Prerelease:   "beta1",
-			Revision:     "5e5dbedd47a5f875b60e241c5555a9caab595246",
-			BuildDate:    buildDate,
-			FIPS:         "",
-			RPC: RPCVersionInfo{
-				Default: 2,
-				Min:     1,
-				Max:     3,
-			}},
-		"FIPS": {
-			HumanVersion: "1.98.0",
-			Version:      "1.98.0",
-			Prerelease:   "",
-			FIPS:         "FIPS 140-2 Enabled, crypto module somemodule",
-			Revision:     "a8dne83d47a5f875b60e241c5555a9dneiifn95u",
-			BuildDate:    buildDate,
-			RPC: RPCVersionInfo{
-				Default: 2,
-				Min:     1,
-				Max:     3,
-			},
-		}}
+	info := VersionInfo{
+		HumanVersion: "1.99.3-beta1",
+		Version:      "1.99.3",
+		Prerelease:   "beta1",
+		Revision:     "5e5dbedd47a5f875b60e241c5555a9caab595246",
+		BuildDate:    buildDate,
+		RPC: RPCVersionInfo{
+			Default: 2,
+			Min:     1,
+			Max:     3,
+		},
+	}
 
 	formatters := map[string]Formatter{
 		"pretty": newPrettyFormatter(),
@@ -69,20 +55,15 @@ func TestFormat(t *testing.T) {
 		"json": newJSONFormatter(),
 	}
 
-	for k, v := range info {
-		for fmtName, formatter := range formatters {
-			t.Run(fmtName, func(t *testing.T) {
-				actual, err := formatter.Format(&v)
-				require.NoError(t, err)
+	for fmtName, formatter := range formatters {
+		t.Run(fmtName, func(t *testing.T) {
+			actual, err := formatter.Format(&info)
+			require.NoError(t, err)
 
-				gName := fmt.Sprintf("%s", fmtName)
-				if k == "FIPS" {
-					gName = fmt.Sprintf("%s_%s", fmtName, k)
-				}
+			gName := fmt.Sprintf("%s", fmtName)
 
-				expected := golden(t, gName, actual)
-				require.Equal(t, expected, actual)
-			})
-		}
+			expected := golden(t, gName, actual)
+			require.Equal(t, expected, actual)
+		})
 	}
 }
