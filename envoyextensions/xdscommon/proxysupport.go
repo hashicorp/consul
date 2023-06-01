f6fa926638e1e567ae21bd8c6303095ee94623da
Michael Zalimeni <michael.zalimeni@hashicorp.com>
2023-06-01 16:27:24 -0400

    Update list of Envoy versions

--- envoyextensions/xdscommon/proxysupport.go
+++ envoyextensions/xdscommon/proxysupport.go
@@ -9,9 +9,9 @@ import "strings"
 //
 // see: https://www.consul.io/docs/connect/proxies/envoy#supported-versions
 var EnvoyVersions = []string{
-	"1.25.4",
-	"1.24.6",
-	"1.23.8",
+	"1.25.6",
+	"1.24.7",
+	"1.23.9",
 	"1.22.11",
 }
 
