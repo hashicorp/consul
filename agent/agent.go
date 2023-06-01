hashicorp/consul

commit f34703fc63c00987ed3a7659a06a672e4c5bce6c

agent: move checkStateSnapshot
Move the field into the struct for addServiceLocked. Also don't require
setting a default value, so that the callers can leave it as nil if they
don't already have a snapshot.

agent/agent.go
@@ -515,7 +515,8 @@ func (a *Agent) Start(ctx context.Context) error {
 	a.serviceManager.Start()
 
 	// Load checks/services/metadata.
-	if err := a.loadServices(c, nil); err != nil {
+	emptyCheckSnapshot := map[structs.CheckID]*structs.HealthCheck{}
+	if err := a.loadServices(c, emptyCheckSnapshot); err != nil {
 		return err
 	}
 	if err := a.loadChecks(c, nil); err != nil {
@@ -1894,15 +1895,14 @@ func (a *Agent) readPersistedServiceConfigs() (map[structs.ServiceID]*structs.Se
 // This entry is persistent and the agent will make a best effort to
 // ensure it is registered
 func (a *Agent) AddService(req AddServiceRequest) error {
+	a.stateLock.Lock()
+	defer a.stateLock.Unlock()
+
 	rl := addServiceLockedRequest{
 		AddServiceRequest:    req,
 		serviceDefaults:      serviceDefaultsFromCache(a.baseDeps, req),
 		persistServiceConfig: true,
 	}
-	a.stateLock.Lock()
-	defer a.stateLock.Unlock()
-
-	rl.snap = a.State.Checks(structs.WildcardEnterpriseMeta())
 	return a.addServiceLocked(rl)
 }
 
@@ -1936,6 +1936,11 @@ type addServiceLockedRequest struct {
 	// serviceDefaults is called when the Agent.stateLock is held, so it must
 	// never attempt to acquire that lock.
 	serviceDefaults func(context.Context) (*structs.ServiceConfigResponse, error)
+
+	// checkStateSnapshot may optionally be set to a snapshot of the checks in
+	// the local.State. If checkStateSnapshot is nil, addServiceInternal will
+	// callState.Checks to get the snapshot.
+	checkStateSnapshot map[structs.CheckID]*structs.HealthCheck
 }
 
 // AddServiceRequest is the union of arguments for calling both
@@ -1955,7 +1960,6 @@ type AddServiceRequest struct {
 	token                 string
 	replaceExistingChecks bool
 	Source                configSource
-	snap                  map[structs.CheckID]*structs.HealthCheck
 }
 
 type addServiceInternalRequest struct {
@@ -2001,6 +2005,13 @@ func (a *Agent) addServiceInternal(req addServiceInternalRequest) error {
 		existingChecks[check.CompoundCheckID()] = false
 	}
 
+	// Note, this is explicitly a nil check instead of len() == 0 because
+	// Agent.Start does not have a snapshot, and we don't want to query
+	// State.Checks each time.
+	if req.checkStateSnapshot == nil {
+		req.checkStateSnapshot = a.State.Checks(structs.WildcardEnterpriseMeta())
+	}
+
 	// Create an associated health check
 	for i, chkType := range req.chkTypes {
 		checkID := string(chkType.CheckID)
@@ -2035,7 +2046,7 @@ func (a *Agent) addServiceInternal(req addServiceInternalRequest) error {
 		}
 
 		// Restore the fields from the snapshot.
-		prev, ok := req.snap[cid]
+		prev, ok := req.checkStateSnapshot[cid]
 		if ok {
 			check.Output = prev.Output
 			check.Status = prev.Status
@@ -3100,10 +3111,10 @@ func (a *Agent) loadServices(conf *config.RuntimeConfig, snap map[structs.CheckI
 				token:                 service.Token,
 				replaceExistingChecks: false, // do default behavior
 				Source:                ConfigSourceLocal,
-				snap:                  snap,
 			},
 			serviceDefaults:      serviceDefaultsFromStruct(persistedServiceConfigs[sid]),
 			persistServiceConfig: false, // don't rewrite the file with the same data we just read
+			checkStateSnapshot:   snap,
 		})
 		if err != nil {
 			return fmt.Errorf("Failed to register service %q: %v", service.Name, err)
@@ -3120,10 +3131,10 @@ func (a *Agent) loadServices(conf *config.RuntimeConfig, snap map[structs.CheckI
 					token:                 sidecarToken,
 					replaceExistingChecks: false, // do default behavior
 					Source:                ConfigSourceLocal,
-					snap:                  snap,
 				},
 				serviceDefaults:      serviceDefaultsFromStruct(persistedServiceConfigs[sidecarServiceID]),
 				persistServiceConfig: false, // don't rewrite the file with the same data we just read
+				checkStateSnapshot:   snap,
 			})
 			if err != nil {
 				return fmt.Errorf("Failed to register sidecar for service %q: %v", service.Name, err)
@@ -3218,10 +3229,10 @@ func (a *Agent) loadServices(conf *config.RuntimeConfig, snap map[structs.CheckI
 					token:                 p.Token,
 					replaceExistingChecks: false, // do default behavior
 					Source:                source,
-					snap:                  snap,
 				},
 				serviceDefaults:      serviceDefaultsFromStruct(persistedServiceConfigs[serviceID]),
 				persistServiceConfig: false, // don't rewrite the file with the same data we just read
+				checkStateSnapshot:   snap,
 			})
 			if err != nil {
 				return fmt.Errorf("failed adding service %q: %s", serviceID, err)

agent/service_manager.go
@@ -86,12 +86,7 @@ func (s *ServiceManager) registerOnce(args addServiceInternalRequest) error {
 	s.agent.stateLock.Lock()
 	defer s.agent.stateLock.Unlock()
 
-	if args.snap == nil {
-		args.snap = s.agent.snapshotCheckState()
-	}
-
-	err := s.agent.addServiceInternal(args)
-	if err != nil {
+	if err := s.agent.addServiceInternal(args); err != nil {
 		return fmt.Errorf("error updating service registration: %v", err)
 	}
 	return nil
@@ -200,10 +195,6 @@ func (w *serviceConfigWatch) RegisterAndStart(ctx context.Context, wg *sync.Wait
 	// make a copy of the AddServiceRequest
 	req := w.registration
 	req.Service = merged
-
-	// TODO: move this line? it seems out of place. Maybe this default should
-	// be set in addServiceInternal
-	req.snap = w.agent.snapshotCheckState() // requires Agent.stateLock
 
 	err = w.agent.addServiceInternal(addServiceInternalRequest{
 		addServiceLockedRequest: req,

commit e40464a87a5bab887289455f4b84788bca1f16f3

Avoid unnecessary health check flapping for sidecar services
Commit f34703fc63c introduced a regression, such that it passes an
empty map as the check snapshot on startup:

    	emptyCheckSnapshot := map[structs.CheckID]*structs.HealthCheck{}
    	if err := a.loadServices(c, emptyCheckSnapshot); err != nil {

If there is no `consul reload`, this empty map will be used for
comparison indefinitely, such that the following if-block will never be
executed:

       		// Restore the fields from the snapshot.
       		prev, ok := req.checkStateSnapshot[cid]
       		if ok {
       			check.Output = prev.Output
       			check.Status = prev.Status
       		}

Subsequently, every time `handleUpdate` in agent/service_manager.go is
triggered, the health checks for sidecar services will flap to critical
for several seconds.

If there has been a `consul reload`, and it happened while the health
checks were still critical (e.g. shortly after agent startup), then the
flapping will also happen, as the check snapshot with critical status
will be used for comparison indefinitely.

This commit fixes the issue by always passing nil as the check snapshot.

Note that `handleUpdate` can happen once every 10 minutes even though
there is no config change for a specific service, as long as there is
any config entries modification within the 10 minutes. As such, on
systems with Consul agents >= 1.10, after applying this fix, there
should be a significant decrease in `Synced check` log lines,
`Catalog.Register` RPC calls, and also disk writes on Consul servers.

Fixes #16663

agent/agent.go
@@ -716,8 +716,7 @@ func (a *Agent) Start(ctx context.Context) error {
 	}
 
 	// Load checks/services/metadata.
-	emptyCheckSnapshot := map[structs.CheckID]*structs.HealthCheck{}
-	if err := a.loadServices(c, emptyCheckSnapshot); err != nil {
+	if err := a.loadServices(c, nil); err != nil {
 		return err
 	}
 	if err := a.loadChecks(c, nil); err != nil {
@@ -4189,7 +4188,7 @@ func (a *Agent) reloadConfigInternal(newCfg *config.RuntimeConfig) error {
 	}
 
 	// Reload service/check definitions and metadata.
-	if err := a.loadServices(newCfg, snap); err != nil {
+	if err := a.loadServices(newCfg, nil); err != nil {
 		return fmt.Errorf("Failed reloading services: %s", err)
 	}
 	if err := a.loadChecks(newCfg, snap); err != nil {