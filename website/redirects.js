9b35e53111e51f9e094f8d0665d040e55c6a6b69
trujillo-adam <ajosetru@gmail.com>
2023-06-13 02:38:27 +0000

    backport of commit e7c164fe905309892f42d8df5e195b0904f935a6

--- website/redirects.js
+++ website/redirects.js
@@ -37,4 +37,20 @@ module.exports = [
     destination: `/consul/docs/connect/intentions`,
     permanent: true,
   },
+  {
+    source: '/consul/docs/v1.16.x/connect/transparent-proxy',
+    destination: '/consul/docs/v1.16.x/k8s/connect/transparent-proxy',
+    permanent: true,
+  },
+  {
+    source: '/consul/docs/agent/limits/init-rate-limits',
+    destination: '/consul/docs/agent/limits/usage/init-rate-limits',
+    permanent: true,
+  },
+  {
+    source: '/consul/docs/agent/limits/set-global-traffic-rate-limits',
+    destination:
+      '/consul/docs/agent/limits/usage/set-global-traffic-rate-limits',
+    permanent: true,
+  },
 ]
