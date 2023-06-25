f9c1b2ac730801e050af048fbc764abbf77df498
absolutelightning <ashesh.vidyut@hashicorp.com>
2023-06-25 08:32:11 +0530

    version bump to 14

--- build-support-windows/build-consul-dev-image.sh
+++ build-support-windows/build-consul-dev-image.sh
@@ -4,7 +4,7 @@ cd ../
 rm -rf dist
 
 export GOOS=windows GOARCH=amd64
-VERSION=1.13.3
+VERSION=1.14.3
 CONSUL_BUILDDATE=$(date +"%Y-%m-%dT%H:%M:%SZ")
 GIT_IMPORT=github.com/hashicorp/consul/version
 GOLDFLAGS=" -X $GIT_IMPORT.Version=$VERSION -X $GIT_IMPORT.VersionPrerelease=dev -X $GIT_IMPORT.BuildDate=$CONSUL_BUILDDATE "
