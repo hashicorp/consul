2e8ed697b2ffc0f543aec35646fa6ae16974af8f
wenincode <tyler.wendlandt@hashicorp.com>
2023-07-06 13:37:52 -0600

    Update link passed to cloud

--- ui/packages/consul-ui/app/helpers/hcp-link-url.js
+++ ui/packages/consul-ui/app/helpers/hcp-link-url.js
@@ -4,7 +4,7 @@ function substring([dc]) {
   const url = new URL('localhost:4200/services/consul/clusters/self-managed/link-existing');
 
   url.searchParams.append('dc', dc);
-  url.searchParams.append('ui', `${window.location.host}/ui`);
+  url.searchParams.append('ui', `${window.location.host}/ui/${dc}/services/link`);
 
   return url;
 }
