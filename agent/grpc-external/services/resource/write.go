29226cf8e38fb9f339cf41d1c911ea3ef2150b92
Semir Patel <semir.patel@hashicorp.com>
2023-07-18 17:18:54 -0500

    Mutate before validate

--- agent/grpc-external/services/resource/write.go
+++ agent/grpc-external/services/resource/write.go
@@ -72,12 +72,12 @@ func (s *Server) Write(ctx context.Context, req *pbresource.WriteRequest) (*pbre
 		)
 	}
 
-	if err = reg.Validate(req.Resource); err != nil {
-		return nil, status.Error(codes.InvalidArgument, err.Error())
-	}
-
 	if err = reg.Mutate(req.Resource); err != nil {
 		return nil, status.Errorf(codes.Internal, "failed mutate hook: %v", err.Error())
+	}
+
+	if err = reg.Validate(req.Resource); err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 
 	// At the storage backend layer, all writes are CAS operations.
