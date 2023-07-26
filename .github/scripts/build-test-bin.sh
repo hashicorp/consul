f2e95aaa50690aab42f431d5dbd92df43e0c179b
Nick Irvine <115657443+nfi-hashicorp@users.noreply.github.com>
2023-07-26 14:44:12 -0700

    save hashlog as artifact

---
+++ .github/scripts/build-test-bin.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -euo pipefail
+
+package_dir=$1
+
+go test -c -tags="$GOTAGS" -o "$package_dir/test.bin" "$package_dir" 2>&1 | tee "$package_dir/test.bin.buildlog"
+grep '^HASH ' "$package_dir/test.bin.buildlog" > "$package_dir/test.bin.hashlog"
