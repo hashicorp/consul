0a032a0e5c57241c384b0930acc3151cc33473f4
Nick Irvine <115657443+nfi-hashicorp@users.noreply.github.com>
2023-07-26 16:30:07 -0700

    better hashlog filter

--- .github/scripts/build-test-bin.sh
+++ .github/scripts/build-test-bin.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-set -euo pipefail
+set -xeuo pipefail
 
 package_dir=$1
 
-GODEBUG=gocachehash=1 go test -c -tags="$GOTAGS" -o "$package_dir/test.bin" "$package_dir" 2>&1 | tee "$package_dir/test.bin.buildlog"
-grep '^HASH ' "$package_dir/test.bin.buildlog" > "$package_dir/test.bin.hashlog"
+export GODEBUG=gocachehash=1 
+go test -c -tags="${GOTAGS:-}" -o "$package_dir/test.bin" "$package_dir" 2>&1 | tee "$package_dir/test.bin.buildlog" | grep -qv '^HASH'
+grep -q '^HASH' "$package_dir/test.bin.buildlog" > "$package_dir/test.bin.hashlog"
