4c382265d4df48e42f9bc7171561cf1527faa02a
Nick Irvine <115657443+nfi-hashicorp@users.noreply.github.com>
2023-07-26 15:15:59 -0700

    reset mtime?

--- .github/scripts/build-test-bin.sh
+++ .github/scripts/build-test-bin.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 package_dir=$1
 
-go test -c -tags="$GOTAGS" -o "$package_dir/test.bin" "$package_dir" 2>&1 | tee "$package_dir/test.bin.buildlog"
+GODEBUG=gocachehash=1 go test -c -tags="$GOTAGS" -o "$package_dir/test.bin" "$package_dir" 2>&1 | tee "$package_dir/test.bin.buildlog"
 grep '^HASH ' "$package_dir/test.bin.buildlog" > "$package_dir/test.bin.hashlog"
