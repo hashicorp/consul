b867e3011d8210449647f955f7a2a8493c34cd75
Ashvitha Sridharan <ashvitha.sridharan@hashicorp.com>
2023-07-27 16:19:20 -0400

    Improve telemetry_config_provider.go tests

--- agent/hcp/telemetry_provider_test.go
+++ agent/hcp/telemetry_provider_test.go
@@ -18,12 +18,181 @@ import (
 )
 
 const defaultTestRefreshInterval = 100 * time.Millisecond
+const sinkServiceName = "test.telemetry_config_provider"
 
 type testConfig struct {
 	filters         string
 	endpoint        string
 	labels          map[string]string
 	refreshInterval time.Duration
+}
+
+func TestNewTelemetryConfigProvider(t *testing.T) {
+	t.Parallel()
+	for name, tc := range map[string]struct {
+		opts    *providerParams
+		wantErr string
+	}{
+		"success": {
+			opts: &providerParams{
+				hcpClient:       client.NewMockClient(t),
+				metricsConfig:   &client.MetricsConfig{},
+				refreshInterval: 1 * time.Second,
+			},
+		},
+		"failsWithMissingHCPClient": {
+			opts: &providerParams{
+				metricsConfig: &client.MetricsConfig{},
+			},
+			wantErr: "missing HCP client",
+		},
+		"failsWithMissingMetricsConfig": {
+			opts: &providerParams{
+				hcpClient: client.NewMockClient(t),
+			},
+			wantErr: "missing metrics config",
+		},
+		"failsWithInvalidRefreshInterval": {
+			opts: &providerParams{
+				hcpClient:       client.NewMockClient(t),
+				metricsConfig:   &client.MetricsConfig{},
+				refreshInterval: 0 * time.Second,
+			},
+			wantErr: "invalid refresh interval",
+		},
+	} {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+			cfgProvider, err := NewHCPProvider(ctx, tc.opts)
+			if tc.wantErr != "" {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), tc.wantErr)
+				require.Nil(t, cfgProvider)
+				return
+			}
+			require.NotNil(t, cfgProvider)
+		})
+	}
+}
+
+func TestTelemetryConfigProvider(t *testing.T) {
+	for name, tc := range map[string]struct {
+		mockExpect func(*client.MockClient)
+		metricKey  []string
+		optsInputs *testConfig
+		expected   *testConfig
+	}{
+		"noChanges": {
+			optsInputs: &testConfig{
+				endpoint: "http://test.com/v1/metrics",
+				filters:  "test",
+				labels: map[string]string{
+					"test_label": "123",
+				},
+				refreshInterval: defaultTestRefreshInterval,
+			},
+			expected: &testConfig{
+				endpoint: "http://test.com/v1/metrics",
+				labels: map[string]string{
+					"test_label": "123",
+				},
+				filters: "test",
+			},
+			metricKey: internalMetricRefreshSuccess,
+		},
+		"newConfig": {
+			optsInputs: &testConfig{
+				endpoint: "http://test.com/v1/metrics",
+				filters:  "test",
+				labels: map[string]string{
+					"test_label": "123",
+				},
+				refreshInterval: 2 * time.Second,
+			},
+			expected: &testConfig{
+				endpoint: "http://newendpoint/v1/metrics",
+				filters:  "consul",
+				labels: map[string]string{
+					"new_label": "1234",
+				},
+				refreshInterval: 2 * time.Second,
+			},
+			metricKey: internalMetricRefreshSuccess,
+		},
+		"sameConfigHCPClientFailure": {
+			optsInputs: &testConfig{
+				endpoint: "http://test.com/v1/metrics",
+				filters:  "test",
+				labels: map[string]string{
+					"test_label": "123",
+				},
+				refreshInterval: defaultTestRefreshInterval,
+			},
+			mockExpect: func(m *client.MockClient) {
+				m.EXPECT().FetchTelemetryConfig(mock.Anything).Return(nil, fmt.Errorf("failure"))
+			},
+			expected: &testConfig{
+				endpoint: "http://test.com/v1/metrics",
+				filters:  "test",
+				labels: map[string]string{
+					"test_label": "123",
+				},
+				refreshInterval: defaultTestRefreshInterval,
+			},
+			metricKey: internalMetricRefreshFailure,
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			sink := initGlobalSink()
+			mockClient := client.NewMockClient(t)
+			if tc.mockExpect != nil {
+				tc.mockExpect(mockClient)
+			} else {
+				mockCfg, err := testTelemetryCfg(tc.expected)
+				require.NoError(t, err)
+				mockClient.EXPECT().FetchTelemetryConfig(mock.Anything).Return(mockCfg, nil)
+			}
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			dynamicCfg, err := testDynamicCfg(tc.optsInputs)
+			require.NoError(t, err)
+
+			provider := &hcpProviderImpl{
+				hcpClient: mockClient,
+				cfg:       dynamicCfg,
+				ticker:    time.NewTicker(defaultTestRefreshInterval),
+			}
+
+			// Use a time chan to trigger updates manually.
+			timeChan := make(chan time.Time, 1)
+
+			go provider.run(ctx, timeChan)
+
+			// Send to the channel twice to ensure we hit the case statement
+			// and once again to ensure the getUpdate() function executed.
+			timeChan <- time.Now()
+			timeChan <- time.Now()
+
+			require.EventuallyWithTf(t, func(collect *assert.CollectT) {
+				// Verify endpoint provider returns correct config values.
+				assert.Equal(collect, tc.expected.endpoint, provider.GetEndpoint().String())
+				assert.Equal(collect, tc.expected.filters, provider.GetFilters().String())
+				assert.Equal(collect, tc.expected.labels, provider.GetLabels())
+
+				// Verify count for transform success metric.
+				sv := collectSinkMetric(sink, tc.metricKey)
+				assert.NotNil(t, sv.AggregateSample)
+				if sv.AggregateSample != nil {
+					require.GreaterOrEqual(t, sv.AggregateSample.Count, 1)
+				}
+			}, 5*time.Second, 100*time.Millisecond, "failed to get update in time")
+		})
+	}
 }
 
 func TestDynamicConfigEquals(t *testing.T) {
@@ -80,216 +249,9 @@ func TestDynamicConfigEquals(t *testing.T) {
 	}
 }
 
-func TestNewTelemetryConfigProvider(t *testing.T) {
-	t.Parallel()
-	for name, tc := range map[string]struct {
-		opts    *providerParams
-		wantErr string
-	}{
-		"success": {
-			opts: &providerParams{
-				hcpClient:       client.NewMockClient(t),
-				metricsConfig:   &client.MetricsConfig{},
-				refreshInterval: 1 * time.Second,
-			},
-		},
-		"failsWithMissingHCPClient": {
-			opts: &providerParams{
-				metricsConfig: &client.MetricsConfig{},
-			},
-			wantErr: "missing HCP client",
-		},
-		"failsWithMissingMetricsConfig": {
-			opts: &providerParams{
-				hcpClient: client.NewMockClient(t),
-			},
-			wantErr: "missing metrics config",
-		},
-		"failsWithInvalidRefreshInterval": {
-			opts: &providerParams{
-				hcpClient:       client.NewMockClient(t),
-				metricsConfig:   &client.MetricsConfig{},
-				refreshInterval: 0 * time.Second,
-			},
-			wantErr: "invalid refresh interval",
-		},
-	} {
-		tc := tc
-		t.Run(name, func(t *testing.T) {
-			t.Parallel()
-			ctx, cancel := context.WithCancel(context.Background())
-			defer cancel()
-			cfgProvider, err := NewHCPProvider(ctx, tc.opts)
-			if tc.wantErr != "" {
-				require.Error(t, err)
-				require.Contains(t, err.Error(), tc.wantErr)
-				require.Nil(t, cfgProvider)
-				return
-			}
-			require.NotNil(t, cfgProvider)
-		})
-	}
-}
-
-func TestTelemetryConfigProvider_Success(t *testing.T) {
-	for name, tc := range map[string]struct {
-		optsInputs *testConfig
-		expected   *testConfig
-	}{
-		"noChanges": {
-			optsInputs: &testConfig{
-				endpoint: "http://test.com/v1/metrics",
-				filters:  "test",
-				labels: map[string]string{
-					"test_label": "123",
-				},
-			},
-			expected: &testConfig{
-				endpoint: "http://test.com/v1/metrics",
-				labels: map[string]string{
-					"test_label": "123",
-				},
-				filters: "test",
-			},
-		},
-		"newConfig": {
-			optsInputs: &testConfig{
-				endpoint: "http://test.com/v1/metrics",
-				filters:  "test",
-				labels: map[string]string{
-					"test_label": "123",
-				},
-			},
-			expected: &testConfig{
-				endpoint: "http://newendpoint/v1/metrics",
-				filters:  "consul",
-				labels: map[string]string{
-					"new_label": "1234",
-				},
-			},
-		},
-	} {
-		t.Run(name, func(t *testing.T) {
-			// Init global metrics sink.
-			serviceName := "test.telemetry_config_provider"
-			sink := initGlobalSink(serviceName)
-
-			// Setup client mock to return the expected config.
-			mockClient := client.NewMockClient(t)
-
-			mockCfg, err := testTelemetryCfg(tc.expected)
-			require.NoError(t, err)
-
-			mockClient.EXPECT().FetchTelemetryConfig(mock.Anything).Return(mockCfg, nil)
-
-			// Setup TelemetryConfigProvider with opts inputs.
-			optsCfg, err := testTelemetryCfg(tc.optsInputs)
-			require.NoError(t, err)
-
-			ctx, cancel := context.WithCancel(context.Background())
-			defer cancel()
-
-			opts := &providerParams{
-				metricsConfig:   optsCfg.MetricsConfig,
-				hcpClient:       mockClient,
-				refreshInterval: defaultTestRefreshInterval,
-			}
-
-			configProvider, err := NewHCPProvider(ctx, opts)
-			require.NoError(t, err)
-
-			// TODO: Test this by having access to the ticker directly.
-			require.EventuallyWithTf(t, func(c *assert.CollectT) {
-				// Collect sink metrics.
-				key := serviceName + "." + strings.Join(internalMetricRefreshSuccess, ".")
-				intervals := sink.Data()
-				sv := intervals[0].Counters[key]
-
-				// Verify count for transform failure metric.
-				assert.NotNil(c, sv.AggregateSample)
-				// Check for nil, as in some eventually ticks, the AggregateSample isn't populated yet.
-				if sv.AggregateSample != nil {
-					assert.GreaterOrEqual(c, sv.AggregateSample.Count, 1)
-				}
-
-				assert.Equal(c, tc.expected.endpoint, configProvider.GetEndpoint().String())
-				assert.Equal(c, tc.expected.filters, configProvider.GetFilters().String())
-				assert.Equal(c, tc.expected.labels, configProvider.GetLabels())
-			}, 2*time.Second, defaultTestRefreshInterval, "failed to update telemetry config expected")
-		})
-	}
-}
-
-func TestTelemetryConfigProvider_UpdateFailuresWithMetrics(t *testing.T) {
-	for name, tc := range map[string]struct {
-		expected *testConfig
-		expect   func(*client.MockClient)
-	}{
-		"failsWithHCPClientFailure": {
-			expected: &testConfig{
-				filters: "test",
-				labels: map[string]string{
-					"test_label": "123",
-				},
-				endpoint: "http://test.com/v1/metrics",
-			},
-			expect: func(m *client.MockClient) {
-				m.EXPECT().FetchTelemetryConfig(mock.Anything).Return(nil, fmt.Errorf("failure"))
-			},
-		},
-	} {
-		t.Run(name, func(t *testing.T) {
-			// Init global metrics sink.
-			serviceName := "test.telemetry_config_provider"
-			sink := initGlobalSink(serviceName)
-
-			testTelemetryCfg, err := testTelemetryCfg(tc.expected)
-			require.NoError(t, err)
-
-			ctx, cancel := context.WithCancel(context.Background())
-			defer cancel()
-
-			mockClient := client.NewMockClient(t)
-			tc.expect(mockClient)
-
-			opts := &providerParams{
-				metricsConfig:   testTelemetryCfg.MetricsConfig,
-				hcpClient:       mockClient,
-				refreshInterval: defaultTestRefreshInterval,
-			}
-
-			configProvider, err := NewHCPProvider(ctx, opts)
-			require.NoError(t, err)
-
-			// Eventually tries to run assertions every 100 ms to verify
-			// if failure metrics and the dynamic config have been updated as expected.
-			// TODO: Use ticker directly to test this.
-			require.EventuallyWithTf(t, func(c *assert.CollectT) {
-				// Collect sink metrics.
-				key := serviceName + "." + strings.Join(internalMetricRefreshFailure, ".")
-				intervals := sink.Data()
-				sv := intervals[0].Counters[key]
-
-				// Verify count for transform failure metric.
-				assert.NotNil(c, sv.AggregateSample)
-				// Check for nil, as in some eventually ticks, the AggregateSample isn't populated yet.
-				if sv.AggregateSample != nil {
-					assert.GreaterOrEqual(c, sv.AggregateSample.Count, 1)
-				}
-
-				// Upon failures, config should not have changed.
-				assert.Equal(c, tc.expected.endpoint, configProvider.GetEndpoint().String())
-				assert.Equal(c, tc.expected.filters, configProvider.GetFilters().String())
-				assert.Equal(c, tc.expected.labels, configProvider.GetLabels())
-			}, 3*time.Second, defaultTestRefreshInterval, "failed to get expected failure metrics")
-		})
-	}
-}
-
-// TODO: Add race test.
-
-func initGlobalSink(serviceName string) *metrics.InmemSink {
-	cfg := metrics.DefaultConfig(serviceName)
+// initGlobalSink is a helper function to initialize a Go metrics inmemsink.
+func initGlobalSink() *metrics.InmemSink {
+	cfg := metrics.DefaultConfig(sinkServiceName)
 	cfg.EnableHostname = false
 
 	sink := metrics.NewInmemSink(10*time.Second, 10*time.Second)
@@ -298,6 +260,17 @@ func initGlobalSink(serviceName string) *metrics.InmemSink {
 	return sink
 }
 
+// collectSinkMetric is a helper function to obtain a measurement from the Go metrics inmemsink.
+func collectSinkMetric(sink *metrics.InmemSink, metricKey []string) metrics.SampledValue {
+	// Collect sink metrics.
+	key := sinkServiceName + "." + strings.Join(metricKey, ".")
+	intervals := sink.Data()
+	sv := intervals[0].Counters[key]
+
+	return sv
+}
+
+// testDynamicCfg converts testConfig inputs to a dynamicConfig to be used in tests.
 func testDynamicCfg(testCfg *testConfig) (*dynamicConfig, error) {
 	filters, err := regexp.Compile(testCfg.filters)
 	if err != nil {
@@ -316,6 +289,7 @@ func testDynamicCfg(testCfg *testConfig) (*dynamicConfig, error) {
 	}, nil
 }
 
+// testTelemetryCfg converts testConfig inputs to a TelemetryConfig to be used in tests.
 func testTelemetryCfg(testCfg *testConfig) (*client.TelemetryConfig, error) {
 	filters, err := regexp.Compile(testCfg.filters)
 	if err != nil {
