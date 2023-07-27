66b742ea978db065d0222c271cb568d52b46f657
Nick Irvine <115657443+nfi-hashicorp@users.noreply.github.com>
2023-07-26 17:36:35 -0700

    disable hashlog

--- .github/scripts/build-test-bin.sh
+++ .github/scripts/build-test-bin.sh
@@ -4,6 +4,6 @@ set -xeuo pipefail
 
 package_dir=$1
 
-export GODEBUG=gocachehash=1 
-go test -c -tags="${GOTAGS:-}" -o "$package_dir/test.bin" "$package_dir" 2>&1 | tee "$package_dir/test.bin.buildlog" | grep -qv '^HASH'
-grep -q '^HASH' "$package_dir/test.bin.buildlog" > "$package_dir/test.bin.hashlog"
+#export GODEBUG=gocachehash=1 
+go test -tags="${GOTAGS:-}" -c -o "$package_dir/test.bin" "$package_dir" 2>&1 # | tee "$package_dir/test.bin.buildlog" | grep -qv '^HASH'
+# grep -q '^HASH' "$package_dir/test.bin.buildlog" > "$package_dir/test.bin.hashlog"
