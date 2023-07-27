a2b983c2d2310d9e5e18ce6e8e9c8da2918d2763
Ashvitha Sridharan <ashvitha.sridharan@hashicorp.com>
2023-07-27 17:33:35 +0000

    backport of commit e3839b31abba0b455a61dc90baf2455d3c705707

--- agent/hcp/telemetry_provider.go
+++ agent/hcp/telemetry_provider.go
@@ -33,8 +33,6 @@ var _ telemetry.EndpointProvider = &hcpProviderImpl{}
 type hcpProviderImpl struct {
 	// cfg holds configuration that can be dynamically updated.
 	cfg *dynamicConfig
-	// cfgHash is used to check if two dynamicConfig objects are equal.
-	cfgHash uint64
 
 	// A reader-writer mutex is used as the provider is read heavy.
 	// OTEL components access telemetryConfig during metrics collection and export (read).
@@ -45,12 +43,27 @@ type hcpProviderImpl struct {
 }
 
 // dynamicConfig is a set of configurable settings for metrics collection, processing and export.
+// fields MUST be exported to compute hash for equals method.
 type dynamicConfig struct {
-	endpoint *url.URL
-	labels   map[string]string
-	filters  *regexp.Regexp
+	Endpoint *url.URL
+	Labels   map[string]string
+	Filters  *regexp.Regexp
 	// refreshInterval controls the interval at which configuration is fetched from HCP to refresh config.
-	refreshInterval time.Duration
+	RefreshInterval time.Duration
+}
+
+func (d *dynamicConfig) equals(newCfg *dynamicConfig) (bool, error) {
+	currHash, err := hashstructure.Hash(*d, hashstructure.FormatV2, nil)
+	if err != nil {
+		return false, err
+	}
+
+	newHash, err := hashstructure.Hash(*newCfg, hashstructure.FormatV2, nil)
+	if err != nil {
+		return false, err
+	}
+
+	return currHash == newHash, err
 }
 
 // providerParams is used to initialize a hcpProviderImpl.
@@ -60,8 +73,8 @@ type providerParams struct {
 	hcpClient       client.Client
 }
 
-// NewHCPProviderImpl initializes and starts a HCP Telemetry provider with provided params.
-func NewHCPProviderImpl(ctx context.Context, params *providerParams) (*hcpProviderImpl, error) {
+// NewHCPProvider initializes and starts a HCP Telemetry provider with provided params.
+func NewHCPProvider(ctx context.Context, params *providerParams) (*hcpProviderImpl, error) {
 	if params.hcpClient == nil {
 		return nil, errors.New("missing HCP client")
 	}
@@ -75,20 +88,14 @@ func NewHCPProviderImpl(ctx context.Context, params *providerParams) (*hcpProvid
 	}
 
 	cfg := &dynamicConfig{
-		endpoint:        params.metricsConfig.Endpoint,
-		labels:          params.metricsConfig.Labels,
-		filters:         params.metricsConfig.Filters,
-		refreshInterval: params.refreshInterval,
-	}
-
-	hash, err := calculateHash(cfg)
-	if err != nil {
-		return nil, fmt.Errorf("failed to calculate hash: %w", err)
+		Endpoint:        params.metricsConfig.Endpoint,
+		Labels:          params.metricsConfig.Labels,
+		Filters:         params.metricsConfig.Filters,
+		RefreshInterval: params.refreshInterval,
 	}
 
 	t := &hcpProviderImpl{
 		cfg:       cfg,
-		cfgHash:   hash,
 		hcpClient: params.hcpClient,
 	}
 
@@ -100,14 +107,14 @@ func NewHCPProviderImpl(ctx context.Context, params *providerParams) (*hcpProvid
 // run continously checks for updates to the telemetry configuration by making a request to HCP.
 // Modification of config only occurs if changes are detected to decrease write locks that block read locks.
 func (t *hcpProviderImpl) run(ctx context.Context) {
-	ticker := time.NewTicker(t.cfg.refreshInterval)
+	ticker := time.NewTicker(t.cfg.RefreshInterval)
 	defer ticker.Stop()
 	for {
 		select {
 		case <-ticker.C:
-			if newCfg, newHash, hasChanged := t.checkUpdate(ctx); hasChanged {
-				t.modifyTelemetryConfig(newCfg, newHash)
-				ticker.Reset(newCfg.refreshInterval)
+			if newCfg, sameCfg := t.checkUpdate(ctx); !sameCfg {
+				t.modifyTelemetryConfig(newCfg)
+				ticker.Reset(newCfg.RefreshInterval)
 			}
 		case <-ctx.Done():
 			return
@@ -117,7 +124,7 @@ func (t *hcpProviderImpl) run(ctx context.Context) {
 
 // checkUpdate makes a HTTP request to HCP to return a new metrics configuration and true, if config changed.
 // checkUpdate does not update the metricsConfig field to prevent acquiring the write lock unnecessarily.
-func (t *hcpProviderImpl) checkUpdate(ctx context.Context) (*dynamicConfig, uint64, bool) {
+func (t *hcpProviderImpl) checkUpdate(ctx context.Context) (*dynamicConfig, bool) {
 	logger := hclog.FromContext(ctx).Named("telemetry_config_provider")
 
 	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
@@ -127,38 +134,37 @@ func (t *hcpProviderImpl) checkUpdate(ctx context.Context) (*dynamicConfig, uint
 	if err != nil {
 		logger.Error("failed to fetch telemetry config from HCP", "error", err)
 		metrics.IncrCounter(internalMetricRefreshFailure, 1)
-		return nil, 0, false
+		return nil, false
 	}
 
 	newDynamicConfig := &dynamicConfig{
-		filters:         telemetryCfg.MetricsConfig.Filters,
-		endpoint:        telemetryCfg.MetricsConfig.Endpoint,
-		labels:          telemetryCfg.MetricsConfig.Labels,
-		refreshInterval: telemetryCfg.RefreshConfig.RefreshInterval,
+		Filters:         telemetryCfg.MetricsConfig.Filters,
+		Endpoint:        telemetryCfg.MetricsConfig.Endpoint,
+		Labels:          telemetryCfg.MetricsConfig.Labels,
+		RefreshInterval: telemetryCfg.RefreshConfig.RefreshInterval,
 	}
-
-	newHash, err := calculateHash(newDynamicConfig)
-	if err != nil {
-		logger.Error("failed to calculate hash for new config", "error", err)
-		metrics.IncrCounter(internalMetricRefreshFailure, 1)
-		return nil, 0, false
-	}
-
-	metrics.IncrCounter(internalMetricRefreshSuccess, 1)
 
 	t.rw.RLock()
 	defer t.rw.RUnlock()
 
-	return newDynamicConfig, newHash, newHash == t.cfgHash
+	equal, err := t.cfg.equals(newDynamicConfig)
+	if err != nil {
+		logger.Error("failed to calculate hash for new config", "error", err)
+		metrics.IncrCounter(internalMetricRefreshFailure, 1)
+		return nil, false
+	}
+
+	metrics.IncrCounter(internalMetricRefreshSuccess, 1)
+
+	return newDynamicConfig, equal
 }
 
 // modifynewTelemetryConfig acquires a write lock to modify it with a given newTelemetryConfig object.
-func (t *hcpProviderImpl) modifyTelemetryConfig(newCfg *dynamicConfig, newHash uint64) {
+func (t *hcpProviderImpl) modifyTelemetryConfig(newCfg *dynamicConfig) {
 	t.rw.Lock()
 	defer t.rw.Unlock()
 
 	t.cfg = newCfg
-	t.cfgHash = newHash
 }
 
 // GetEndpoint acquires a read lock to return endpoint configuration for consumers.
@@ -166,7 +172,7 @@ func (t *hcpProviderImpl) GetEndpoint() *url.URL {
 	t.rw.RLock()
 	defer t.rw.RUnlock()
 
-	return t.cfg.endpoint
+	return t.cfg.Endpoint
 }
 
 // GetFilters acquires a read lock to return filters configuration for consumers.
@@ -174,7 +180,7 @@ func (t *hcpProviderImpl) GetFilters() *regexp.Regexp {
 	t.rw.RLock()
 	defer t.rw.RUnlock()
 
-	return t.cfg.filters
+	return t.cfg.Filters
 }
 
 // GetLabels acquires a read lock to return labels configuration for consumers.
@@ -182,10 +188,5 @@ func (t *hcpProviderImpl) GetLabels() map[string]string {
 	t.rw.RLock()
 	defer t.rw.RUnlock()
 
-	return t.cfg.labels
-}
-
-// calculateHash returns a uint64 hash for data that can be used for comparisons.
-func calculateHash(cfg *dynamicConfig) (uint64, error) {
-	return hashstructure.Hash(*cfg, hashstructure.FormatV2, nil)
+	return t.cfg.Labels
 }
