3ac8f9681f1f85f1a588eaee982b0bd6c39fe533
Nick Irvine <115657443+nfi-hashicorp@users.noreply.github.com>
2023-07-31 15:19:13 -0700

    test caching in go-tests.yml; no split

--- .github/scripts/set_test_package_matrix.sh
+++ .github/scripts/set_test_package_matrix.sh
@@ -3,9 +3,14 @@
 # SPDX-License-Identifier: MPL-2.0
 
 set -euo pipefail
-export RUNNER_COUNT=$1
 
 # set matrix var to list of unique packages containing tests
-matrix="$(go list -json="ImportPath,TestGoFiles" ./... | jq --compact-output '. | select(.TestGoFiles != null) | .ImportPath' | jq --slurp --compact-output '.' | jq --argjson runnercount $RUNNER_COUNT  -cM '[_nwise(length / $runnercount | floor)]'))"
+export RUNNER_COUNT=$1
+matrix="$( \
+  go list -json="ImportPath,TestGoFiles" ./... | \
+  jq --compact-output '. | select(.TestGoFiles != null) | .ImportPath' | \
+  jq --slurp --compact-output '.' | \
+  jq --argjson runnercount $RUNNER_COUNT  -cM '[_nwise(length / $runnercount | floor)]' \
+)"
 
 echo "matrix=${matrix}" >> "${GITHUB_OUTPUT}"
