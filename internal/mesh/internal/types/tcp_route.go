610826337a80548abd4c3f5da7d890c0a3a0402e
R.B. Boyer <rb@hashicorp.com>
2023-08-01 16:10:51 -0500

    mesh: adding the protobuf types and resources backing mesh config v2

---
+++ internal/mesh/internal/types/tcp_route.go
@@ -0,0 +1,32 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package types
+
+import (
+	"github.com/hashicorp/consul/internal/resource"
+	pbmesh "github.com/hashicorp/consul/proto-public/pbmesh/v1alpha1"
+	"github.com/hashicorp/consul/proto-public/pbresource"
+)
+
+const (
+	TCPRouteKind = "TCPRoute"
+)
+
+var (
+	TCPRouteV1Alpha1Type = &pbresource.Type{
+		Group:        GroupName,
+		GroupVersion: VersionV1Alpha1,
+		Kind:         TCPRouteKind,
+	}
+
+	TCPRouteType = TCPRouteV1Alpha1Type
+)
+
+func RegisterTCPRoute(r resource.Registry) {
+	r.Register(resource.Registration{
+		Type:     TCPRouteV1Alpha1Type,
+		Proto:    &pbmesh.TCPRoute{},
+		Validate: nil,
+	})
+}
