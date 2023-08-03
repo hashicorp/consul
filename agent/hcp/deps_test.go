e67ba410f8a4d8d6be394b3cf5e50402f46fc563
Ashvitha Sridharan <ashvitha.sridharan@hashicorp.com>
2023-08-03 10:49:55 -0400

    Fix deps test which requires a maybe

--- agent/hcp/deps_test.go
+++ agent/hcp/deps_test.go
@@ -2,10 +2,8 @@ package hcp
 
 import (
 	"context"
-	"net/url"
-	"regexp"
+	"errors"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
@@ -21,29 +19,15 @@ type mockMetricsClient struct {
 func TestSink(t *testing.T) {
 	t.Parallel()
 
-	u, _ := url.Parse("https://test.com/v1/metrics")
-	filters, _ := regexp.Compile("test")
-
-	c := client.NewMockClient(t)
-	mt := mockTelemetryConfig(1*time.Second, u, filters)
-	c.EXPECT().FetchTelemetryConfig(mock.Anything).Return(mt, nil)
+	// FetchTelemetryConfig is stubbed for TelemetryConfigProvider,which periodically fetches metrics configuration from HCP.
+	c := &client.MockClient{}
+	c.EXPECT().FetchTelemetryConfig(mock.Anything).Return(nil, errors.New("fetch error: using default provider settings"))
 
 	mc := mockMetricsClient{}
-	ctx := context.Background()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 	s, err := sink(ctx, c, mc)
 
 	require.NotNil(t, s)
 	require.NoError(t, err)
-}
-
-func mockTelemetryConfig(refreshInterval time.Duration, metricsEndpoint *url.URL, filters *regexp.Regexp) *client.TelemetryConfig {
-	return &client.TelemetryConfig{
-		MetricsConfig: &client.MetricsConfig{
-			Endpoint: metricsEndpoint,
-			Filters:  filters,
-		},
-		RefreshConfig: &client.RefreshConfig{
-			RefreshInterval: refreshInterval,
-		},
-	}
 }
