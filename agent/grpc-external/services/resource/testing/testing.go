c043f459abb47f89d7938f7242262cda34a41951
Xinyi Wang <xinyi.wang@hashicorp.com>
2023-08-03 16:40:47 -0700

    code refactor

--- agent/grpc-external/services/resource/testing/testing.go
+++ agent/grpc-external/services/resource/testing/testing.go
@@ -8,6 +8,8 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 
+	"github.com/hashicorp/go-uuid"
+
 	"github.com/hashicorp/consul/acl"
 	"github.com/hashicorp/consul/acl/resolver"
 	svc "github.com/hashicorp/consul/agent/grpc-external/services/resource"
@@ -17,7 +19,6 @@ import (
 	"github.com/hashicorp/consul/internal/storage/inmem"
 	"github.com/hashicorp/consul/proto-public/pbresource"
 	"github.com/hashicorp/consul/sdk/testutil"
-	"github.com/hashicorp/go-uuid"
 )
 
 func randomACLIdentity(t *testing.T) structs.ACLIdentity {
@@ -47,42 +48,7 @@ func AuthorizerFrom(t *testing.T, policyStrs ...string) resolver.Result {
 // RunResourceService runs a Resource Service for the duration of the test and
 // returns a client to interact with it. ACLs will be disabled.
 func RunResourceService(t *testing.T, registerFns ...func(resource.Registry)) pbresource.ResourceServiceClient {
-	t.Helper()
-
-	backend, err := inmem.NewBackend()
-	require.NoError(t, err)
-
-	ctx, cancel := context.WithCancel(context.Background())
-	t.Cleanup(cancel)
-	go backend.Run(ctx)
-
-	registry := resource.NewRegistry()
-	for _, fn := range registerFns {
-		fn(registry)
-	}
-
-	server := grpc.NewServer()
-
-	svc.NewServer(svc.Config{
-		Backend:     backend,
-		Registry:    registry,
-		Logger:      testutil.Logger(t),
-		ACLResolver: resolver.DANGER_NO_AUTH{},
-	}).Register(server)
-
-	pipe := internal.NewPipeListener()
-	go server.Serve(pipe)
-	t.Cleanup(server.Stop)
-
-	conn, err := grpc.Dial("",
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithContextDialer(pipe.DialContext),
-		grpc.WithBlock(),
-	)
-	require.NoError(t, err)
-	t.Cleanup(func() { _ = conn.Close() })
-
-	return pbresource.NewResourceServiceClient(conn)
+	return RunResourceServiceWithACL(t, resolver.DANGER_NO_AUTH{}, registerFns...)
 }
 
 func RunResourceServiceWithACL(t *testing.T, aclResolver svc.ACLResolver, registerFns ...func(resource.Registry)) pbresource.ResourceServiceClient {
