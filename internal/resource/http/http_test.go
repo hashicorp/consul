c3775a765d5a190109dd0958a7db7a55c5f0966f
Xinyi Wang <xinyi.wang@hashicorp.com>
2023-08-03 12:05:47 -0700

    add owner test

--- internal/resource/http/http_test.go
+++ internal/resource/http/http_test.go
@@ -11,9 +11,9 @@ import (
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 
-	"github.com/hashicorp/consul/acl"
 	resourceSvc "github.com/hashicorp/consul/agent/grpc-external/services/resource"
 	svctest "github.com/hashicorp/consul/agent/grpc-external/services/resource/testing"
+	pbdemov1 "github.com/hashicorp/consul/proto/private/pbdemo/v1"
 
 	"github.com/hashicorp/consul/internal/resource"
 	"github.com/hashicorp/consul/internal/resource/demo"
@@ -22,8 +22,8 @@ import (
 	"github.com/hashicorp/consul/sdk/testutil"
 )
 
-const testACLTokenArtistV2WritePolicy = acl.AnonymousTokenID
-const testACLTokenArtistV2ReadPolicy = "00000000-0000-0000-0000-000000000001"
+const testACLTokenArtistReadPolicy = "00000000-0000-0000-0000-000000000001"
+const testACLTokenArtistWritePolicy = "00000000-0000-0000-0000-000000000002"
 
 func parseToken(req *http.Request, token *string) {
 	*token = req.Header.Get("x-Consul-Token")
@@ -110,14 +110,24 @@ func TestResourceHandler_InputValidation(t *testing.T) {
 
 func TestResourceWriteHandler(t *testing.T) {
 	aclResolver := &resourceSvc.MockACLResolver{}
-	aclResolver.On("ResolveTokenAndDefaultMeta", testACLTokenArtistV2WritePolicy, mock.Anything, mock.Anything).
-		Return(svctest.AuthorizerFrom(t, demo.ArtistV2WritePolicy), nil)
-	aclResolver.On("ResolveTokenAndDefaultMeta", testACLTokenArtistV2ReadPolicy, mock.Anything, mock.Anything).
-		Return(svctest.AuthorizerFrom(t, demo.ArtistV2ReadPolicy), nil)
+	aclResolver.On("ResolveTokenAndDefaultMeta", testACLTokenArtistReadPolicy, mock.Anything, mock.Anything).
+		Return(svctest.AuthorizerFrom(t, demo.ArtistV1ReadPolicy, demo.ArtistV2ReadPolicy), nil)
+	aclResolver.On("ResolveTokenAndDefaultMeta", testACLTokenArtistWritePolicy, mock.Anything, mock.Anything).
+		Return(svctest.AuthorizerFrom(t, demo.ArtistV1WritePolicy, demo.ArtistV2WritePolicy), nil)
 
 	client := svctest.RunResourceServiceWithACL(t, aclResolver, demo.RegisterTypes)
 
-	resourceHandler := resourceHandler{
+	v1ArtistHandler := resourceHandler{
+		resource.Registration{
+			Type:  demo.TypeV1Artist,
+			Proto: &pbdemov1.Artist{},
+		},
+		client,
+		parseToken,
+		hclog.NewNullLogger(),
+	}
+
+	v2ArtistHandler := resourceHandler{
 		resource.Registration{
 			Type:  demo.TypeV2Artist,
 			Proto: &pbdemov2.Artist{},
@@ -129,7 +139,7 @@ func TestResourceWriteHandler(t *testing.T) {
 
 	t.Run("should be blocked if the token is not authorized", func(t *testing.T) {
 		rsp := httptest.NewRecorder()
-		req := httptest.NewRequest("PUT", "/keith-urban?partition=default&peer_name=local&namespace=default", strings.NewReader(`
+		req := httptest.NewRequest("PUT", "/demo/v2/artist/keith-urban?partition=default&peer_name=local&namespace=default", strings.NewReader(`
 			{
 				"metadata": {
 					"foo": "bar"
@@ -141,16 +151,16 @@ func TestResourceWriteHandler(t *testing.T) {
 			}
 		`))
 
-		req.Header.Add("x-consul-token", testACLTokenArtistV2ReadPolicy)
+		req.Header.Add("x-consul-token", testACLTokenArtistReadPolicy)
 
-		resourceHandler.ServeHTTP(rsp, req)
+		v2ArtistHandler.ServeHTTP(rsp, req)
 
 		require.Equal(t, http.StatusForbidden, rsp.Result().StatusCode)
 	})
 
 	t.Run("should write to the resource backend", func(t *testing.T) {
 		rsp := httptest.NewRecorder()
-		req := httptest.NewRequest("PUT", "/keith-urban?partition=default&peer_name=local&namespace=default", strings.NewReader(`
+		req := httptest.NewRequest("PUT", "/demo/v2/artist/keith-urban?partition=default&peer_name=local&namespace=default", strings.NewReader(`
 			{
 				"metadata": {
 					"foo": "bar"
@@ -162,9 +172,9 @@ func TestResourceWriteHandler(t *testing.T) {
 			}
 		`))
 
-		req.Header.Add("x-consul-token", testACLTokenArtistV2WritePolicy)
+		req.Header.Add("x-consul-token", testACLTokenArtistWritePolicy)
 
-		resourceHandler.ServeHTTP(rsp, req)
+		v2ArtistHandler.ServeHTTP(rsp, req)
 
 		require.Equal(t, http.StatusOK, rsp.Result().StatusCode)
 
@@ -186,5 +196,59 @@ func TestResourceWriteHandler(t *testing.T) {
 		var artist pbdemov2.Artist
 		require.NoError(t, readRsp.Resource.Data.UnmarshalTo(&artist))
 		require.Equal(t, "Keith Urban", artist.Name)
+	})
+
+	t.Run("should write to the resource backend with owner", func(t *testing.T) {
+		rsp := httptest.NewRecorder()
+		req := httptest.NewRequest("PUT", "/demo/v1/artist/keith-urban-v1?partition=default&peer_name=local&namespace=default", strings.NewReader(`
+			{
+				"metadata": {
+					"foo": "bar"
+				},
+				"data": {
+					"name": "Keith Urban V1",
+					"genre": "GENRE_COUNTRY"
+				},
+				"owner": {
+					"name": "keith-urban",
+					"type": {
+						"group": "demo",
+						"group_version": "v2",
+						"kind": "Artist"
+					},
+					"tenancy": {
+						"partition": "default",
+						"peer_name": "local",
+						"namespace": "default"
+					}
+				}
+			}
+		`))
+
+		req.Header.Add("x-consul-token", testACLTokenArtistWritePolicy)
+
+		v1ArtistHandler.ServeHTTP(rsp, req)
+
+		require.Equal(t, http.StatusOK, rsp.Result().StatusCode)
+
+		var result map[string]any
+		require.NoError(t, json.NewDecoder(rsp.Body).Decode(&result))
+		require.Equal(t, "Keith Urban V1", result["data"].(map[string]any)["name"])
+		require.Equal(t, "keith-urban-v1", result["id"].(map[string]any)["name"])
+
+		readRsp, err := client.Read(testutil.TestContext(t), &pbresource.ReadRequest{
+			Id: &pbresource.ID{
+				Type:    demo.TypeV1Artist,
+				Tenancy: demo.TenancyDefault,
+				Name:    "keith-urban-v1",
+			},
+		})
+		require.NoError(t, err)
+		require.NotNil(t, readRsp.Resource)
+		require.Equal(t, "keith-urban", readRsp.Resource.Owner.Name)
+
+		var artist pbdemov1.Artist
+		require.NoError(t, readRsp.Resource.Data.UnmarshalTo(&artist))
+		require.Equal(t, "Keith Urban V1", artist.Name)
 	})
 }
