b005940cb3b5ead295384a8f7f8b87f96443a91a
Michael Zalimeni <michael.zalimeni@hashicorp.com>
2023-08-10 12:05:21 -0400

    Add logging to locality policy application
    In OSS, this is currently a no-op.

--- agent/xds/locality_policy.go
+++ agent/xds/locality_policy.go
@@ -5,16 +5,18 @@ package xds
 
 import (
 	"fmt"
+	"github.com/hashicorp/go-hclog"
 
 	"github.com/hashicorp/consul/agent/structs"
 )
 
-func groupedEndpoints(locality *structs.Locality, policy *structs.DiscoveryPrioritizeByLocality, csns structs.CheckServiceNodes) ([]structs.CheckServiceNodes, error) {
+func groupedEndpoints(logger hclog.Logger, locality *structs.Locality, policy *structs.DiscoveryPrioritizeByLocality, csns structs.CheckServiceNodes) ([]structs.CheckServiceNodes, error) {
 	switch {
 	case policy == nil || policy.Mode == "" || policy.Mode == "none":
 		return []structs.CheckServiceNodes{csns}, nil
 	case policy.Mode == "failover":
-		return prioritizeByLocalityFailover(locality, csns), nil
+		log := logger.Named("locality")
+		return prioritizeByLocalityFailover(log, locality, csns), nil
 	default:
 		return nil, fmt.Errorf("unexpected priortize-by-locality mode %q", policy.Mode)
 	}
