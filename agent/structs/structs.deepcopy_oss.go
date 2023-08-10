6ef8be21e646b4ac026b3d07332fcfda7aa7db73
jm96441n <john.maguire@hashicorp.com>
2023-08-10 17:00:12 -0400

    Add build tags for deepcopy oss file

--- agent/structs/structs.deepcopy_oss.go
+++ agent/structs/structs.deepcopy_oss.go
@@ -1,3 +1,9 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+//go:build !consulent
+// +build !consulent
+
 package structs
 
 // DeepCopy generates a deep copy of *APIGatewayJWTRequirement
