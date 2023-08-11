559c61e6b6f93270bb50944e9fb063c94bdf92e8
Poonam Jadhav <poonam.jadhav@hashicorp.com>
2023-08-11 15:52:51 -0400

    Net-2712/resource hcl parsing (#18250)
    * Initial protohcl implementation
    
    Co-authored-by: Matt Keeler <mkeeler@users.noreply.github.com>
    Co-authored-by: Daniel Upton <daniel@floppy.co>
    
    * resourcehcl: implement resource decoding on top of protohcl
    
    Co-authored-by: Daniel Upton <daniel@floppy.co>
    
    * fix: resolve ci failures
    
    * test: add additional unmarshalling tests
    
    * refactor: update function test to clean protohcl package imports
    
    ---------
    
    Co-authored-by: Matt Keeler <mkeeler@users.noreply.github.com>
    Co-authored-by: Daniel Upton <daniel@floppy.co>

--- internal/resource/registry.go
+++ internal/resource/registry.go
@@ -6,6 +6,7 @@ package resource
 import (
 	"fmt"
 	"regexp"
+	"strings"
 	"sync"
 
 	"google.golang.org/protobuf/proto"
@@ -170,4 +171,16 @@ func (r *TypeRegistry) Types() []Registration {
 
 func ToGVK(resourceType *pbresource.Type) string {
 	return fmt.Sprintf("%s.%s.%s", resourceType.Group, resourceType.GroupVersion, resourceType.Kind)
+}
+
+func ParseGVK(gvk string) (*pbresource.Type, error) {
+	parts := strings.Split(gvk, ".")
+	if len(parts) != 3 {
+		return nil, fmt.Errorf("GVK string must be in the form <Group>.<GroupVersion>.<Kind>, got: %s", gvk)
+	}
+	return &pbresource.Type{
+		Group:        parts[0],
+		GroupVersion: parts[1],
+		Kind:         parts[2],
+	}, nil
 }
