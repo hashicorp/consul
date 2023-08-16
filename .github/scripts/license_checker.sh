29a0562b405958a23137414cb776ceb60ab0afbb
Nathan Coleman <nathan.coleman@hashicorp.com>
2023-08-16 12:58:07 -0400

    Add license-checker action that fails when any backported file contains BUSL header

---
+++ .github/scripts/license_checker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: BUSL-1.1
+
+
+busl_files=$(grep -r 'SPDX-License-Identifier: BUSL' --exclude=./.github/scripts/license_checker.sh .)
+
+# If we do not find a file in .changelog/, we fail the check
+if [ -n "$busl_files" ]; then
+    echo "Found BUSL occurrences in the PR branch!"
+    echo -e $busl_files
+    exit 1
+else
+    echo "Did not find any occurrences of BUSL in the PR branch"
+    exit 0
+fi
