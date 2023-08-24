ddb8c22ee1f4488c02355674a4e795a0532aa348
John Landa <john.landa@hashicorp.com>
2023-08-24 11:43:35 -0600

    traffic permissions mapper framing, traffic permissions controller updates.

--- internal/auth/internal/types/computed_traffic_permissions.go
+++ internal/auth/internal/types/computed_traffic_permissions.go
@@ -28,8 +28,16 @@ func RegisterComputedTrafficPermission(r resource.Registry) {
 	})
 }
 
-func ValidateComputedTrafficPermission(_ *pbresource.Resource) error {
-	// TODO Marshal and validate
+func ValidateComputedTrafficPermission(res *pbresource.Resource) error {
+	var ctp pbauth.ComputedTrafficPermission
+
+	if err := res.Data.UnmarshalTo(&ctp); err != nil {
+		return resource.NewErrDataParse(&ctp, err)
+	}
+
+	// There isn't really anything else to validate other than that it is unmarshallable -- which it should always be.
+	// It is valid for the ComputedTrafficPermissions to be totally empty as this would mean that the default behavior
+	// applies for all sources.
 
 	return nil
 }
