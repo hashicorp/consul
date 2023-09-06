61bd7a6c9d30d3d385d4d5b05d1ea93e3720a8f0
Nathan Coleman <nathan.coleman@hashicorp.com>
2023-09-06 10:07:50 -0400

    Stub APIGateway mesh type

---
+++ internal/mesh/internal/types/api_gateway.go
@@ -0,0 +1,34 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package types
+
+import (
+	"github.com/hashicorp/consul/internal/resource"
+	pbmesh "github.com/hashicorp/consul/proto-public/pbmesh/v1alpha1"
+	"github.com/hashicorp/consul/proto-public/pbresource"
+)
+
+const (
+	APIGatewayKind = "APIGateway"
+)
+
+var (
+	APIGatewayV1Alpha1Type = &pbresource.Type{
+		Group:        GroupName,
+		GroupVersion: VersionV1Alpha1,
+		Kind:         APIGatewayKind,
+	}
+
+	APIGatewayType = APIGatewayV1Alpha1Type
+)
+
+func RegisterAPIGateway(r resource.Registry) {
+	r.Register(resource.Registration{
+		Type:     APIGatewayV1Alpha1Type,
+		Proto:    &pbmesh.APIGateway{},
+		Scope:    resource.ScopeNamespace,
+		Mutate:   nil,
+		Validate: nil,
+	})
+}
