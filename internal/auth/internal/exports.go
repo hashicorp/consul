0cb02c19e0c44e041bb2662f00bab7b68b535515
skpratt <sarah.pratt@hashicorp.com>
2023-09-07 11:59:31 -0500

    register new types

---
+++ internal/auth/internal/exports.go
@@ -0,0 +1,35 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package auth
+
+import (
+	"github.com/hashicorp/consul/internal/auth/internal/types"
+	"github.com/hashicorp/consul/internal/resource"
+)
+
+var (
+	// API Group Information
+
+	APIGroup        = types.GroupName
+	VersionV1Alpha1 = types.VersionV1Alpha1
+	CurrentVersion  = types.CurrentVersion
+
+	// Resource Kind Names.
+
+	WorkloadIdentity = types.WorkloadIdentityKind
+
+	// Resource Types for the v1alpha1 version.
+
+	WorkloadIdentityV1Alpha1Type = types.WorkloadIdentityV1Alpha1Type
+
+	// Resource Types for the latest version.
+
+	WorkloadIdentityType = types.WorkloadIdentityType
+)
+
+// RegisterTypes adds all resource types within the "auth" API group
+// to the given type registry
+func RegisterTypes(r resource.Registry) {
+	types.Register(r)
+}
