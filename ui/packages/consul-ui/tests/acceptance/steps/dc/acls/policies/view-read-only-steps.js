dfc3e2e790714f43857f5863ba67981a134cee09
hc-github-team-consul-core <github-team-consul-core@hashicorp.com>
2023-09-11 16:13:19 -0500

    Backport of feat: Change global-read-only policy to non editable into release/1.16.x (#18749)
    * backport of commit 9c44587f1aad116d852bc41f51ec52724bbee4fc
    
    ---------
    
    Co-authored-by: valeriia-ruban <valeriia.ruban@hashicorp.com>

---
+++ ui/packages/consul-ui/tests/acceptance/steps/dc/acls/policies/view-read-only-steps.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+import steps from '../../../steps';
+
+// step definitions that are shared between features should be moved to the
+// tests/acceptance/steps/steps.js file
+
+export default function (assert) {
+  return steps(assert).then('I should find a file', function () {
+    assert.ok(true, this.step);
+  });
+}
