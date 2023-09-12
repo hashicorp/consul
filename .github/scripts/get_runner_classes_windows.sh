63a0ed2a9aee99a8774dd883538cd3f8aea20484
absolutelightning <ashesh.vidyut@hashicorp.com>
2023-09-12 17:23:05 +0530

    revert machines

--- .github/scripts/get_runner_classes_windows.sh
+++ .github/scripts/get_runner_classes_windows.sh
@@ -18,10 +18,9 @@ case "$GITHUB_REPOSITORY" in
 	;;
 *)
 	# shellcheck disable=SC2129
-	echo "compute-small=['self-hosted', 'ondemand', 'os=windows-2019', 'type=m6a.4xlarge']" >>"$GITHUB_OUTPUT"
-	echo "compute-medium=['self-hosted', 'ondemand', 'os=windows-2019', 'type=m6a.8xlarge']" >>"$GITHUB_OUTPUT"
-	echo "compute-large=['self-hosted', 'ondemand', 'os=windows-2019', 'type=m6a.12xlarge']" >>"$GITHUB_OUTPUT"
-	# m5d.8xlarge is equivalent to our xl custom runner in CE
-	echo "compute-xl=['self-hosted', 'ondemand', 'os=windows-2019', 'type=m6a.16xlarge']" >>"$GITHUB_OUTPUT"
+	echo "compute-small=['windows-2019']" >>"$GITHUB_OUTPUT"
+	echo "compute-medium=['windows-2019']" >>"$GITHUB_OUTPUT"
+	echo "compute-large=['windows-2019']" >>"$GITHUB_OUTPUT"
+	echo "compute-xl=['windows-2019']" >>"$GITHUB_OUTPUT"
 	;;
 esac
