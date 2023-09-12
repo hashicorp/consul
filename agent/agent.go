0a2344a944db27cf0f5566a55291c4aeec464c8b
Gerard Nguyen <gerard@hashicorp.com>
2023-09-12 23:37:56 +1000

    Show latest config in /v1/agent/self (#18714)
    * Show latest config in /v1/agent/self
    
    * remove license string in backport

--- agent/agent.go
+++ agent/agent.go
@@ -4195,6 +4195,9 @@ func (a *Agent) reloadConfigInternal(newCfg *config.RuntimeConfig) error {
 	a.enableDebug.Store(newCfg.EnableDebug)
 	a.config.EnableDebug = newCfg.EnableDebug
 
+	// update Agent config with new config
+	a.config = newCfg.DeepCopy()
+
 	return nil
 }
 
