1763589f555660c351a4e886b6009cc68e5cecd0
absolutelightning <ashesh.vidyut@hashicorp.com>
2023-09-18 19:57:23 +0530

    address nits

--- api/catalog_test.go
+++ api/catalog_test.go
@@ -220,16 +220,16 @@ func TestAPI_CatalogServices_NodeMetaFilter(t *testing.T) {
 	})
 }
 
-func TestAPI_CatalogServices_NodeMetaViaFilter(t *testing.T) {
+func TestAPI_CatalogServices_FilterExpr_NodeMeta(t *testing.T) {
 	t.Parallel()
 	meta := map[string]string{"somekey": "somevalue", "synthetic": "true"}
 	c, s := makeClientWithConfig(t, nil, func(conf *testutil.TestServerConfig) {
 		conf.NodeMeta = meta
-		conf.NodeName = "foobar"
 	})
 	defer s.Stop()
 
 	catalog := c.Catalog()
+	// Make sure we get the service back when filtering by filter expression
 	retry.Run(t, func(r *retry.R) {
 		services, meta, err := catalog.Services(&QueryOptions{Filter: "NodeMeta[\"synthetic\"] == true and NodeMeta[\"somekey\"] == somevalue"})
 		if err != nil {
