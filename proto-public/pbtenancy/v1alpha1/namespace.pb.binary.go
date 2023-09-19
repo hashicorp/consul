5e580681e009f8eb2152cb3fdd34781132d09dff
Dhia Ayachi <dhia@hashicorp.com>
2023-09-19 14:12:50 -0400

    remove ACL for now

--- proto-public/pbtenancy/v1alpha1/namespace.pb.binary.go
+++ proto-public/pbtenancy/v1alpha1/namespace.pb.binary.go
@@ -16,23 +16,3 @@ func (msg *Namespace) MarshalBinary() ([]byte, error) {
 func (msg *Namespace) UnmarshalBinary(b []byte) error {
 	return proto.Unmarshal(b, msg)
 }
-
-// MarshalBinary implements encoding.BinaryMarshaler
-func (msg *ACLConfig) MarshalBinary() ([]byte, error) {
-	return proto.Marshal(msg)
-}
-
-// UnmarshalBinary implements encoding.BinaryUnmarshaler
-func (msg *ACLConfig) UnmarshalBinary(b []byte) error {
-	return proto.Unmarshal(b, msg)
-}
-
-// MarshalBinary implements encoding.BinaryMarshaler
-func (msg *ACLLink) MarshalBinary() ([]byte, error) {
-	return proto.Marshal(msg)
-}
-
-// UnmarshalBinary implements encoding.BinaryUnmarshaler
-func (msg *ACLLink) UnmarshalBinary(b []byte) error {
-	return proto.Unmarshal(b, msg)
-}
