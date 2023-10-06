68dc2ece50e214cc68e3c9acdac9b16d943fe34d
Dhia Ayachi <dhia@hashicorp.com>
2023-10-06 11:00:16 -0400

    only change verifier parameters if it's disabled (default value)

--- agent/consul/server.go
+++ agent/consul/server.go
@@ -1115,8 +1115,10 @@ func (s *Server) setupRaft(isCatalogResourceExperiment bool) error {
 		// Only use WAL if there is no existing raft.db, even if it's enabled.
 		if s.config.LogStoreConfig.Backend == LogStoreBackendDefault && !boltFileExists && isCatalogResourceExperiment {
 			s.config.LogStoreConfig.Backend = LogStoreBackendWAL
-			s.config.LogStoreConfig.Verification.Enabled = true
-			s.config.LogStoreConfig.Verification.Interval = 1 * time.Minute
+			if !s.config.LogStoreConfig.Verification.Enabled {
+				s.config.LogStoreConfig.Verification.Enabled = true
+				s.config.LogStoreConfig.Verification.Interval = 1 * time.Minute
+			}
 			if err = initWAL(); err != nil {
 				return err
 			}
