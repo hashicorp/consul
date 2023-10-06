0e81f06e5ea9cd2555d535422801a281f1c010d3
Nitya Dhanushkodi <nitya@hashicorp.com>
2023-10-05 17:52:17 -0700

    xdsv2: support l7 by adding xfcc policy/headers, tweaking routes, and make a bunch of listeners l7 tests pass

--- agent/xds/listeners_test.go
+++ agent/xds/listeners_test.go
@@ -70,6 +70,7 @@ func makeListenerDiscoChainTests(enterprise bool) []listenerTestCase {
 			create: func(t testinf.T) *proxycfg.ConfigSnapshot {
 				return proxycfg.TestConfigSnapshotDiscoveryChain(t, "splitter-with-resolver-redirect-multidc", enterprise, nil, nil)
 			},
+			alsoRunTestForV2: true,
 		},
 		{
 			name: "connect-proxy-with-tcp-chain",
@@ -91,6 +92,7 @@ func makeListenerDiscoChainTests(enterprise bool) []listenerTestCase {
 					},
 				)
 			},
+			alsoRunTestForV2: true,
 		},
 		{
 			name: "connect-proxy-with-http2-chain",
@@ -105,6 +107,7 @@ func makeListenerDiscoChainTests(enterprise bool) []listenerTestCase {
 					},
 				)
 			},
+			alsoRunTestForV2: true,
 		},
 		{
 			name: "connect-proxy-with-grpc-chain",
@@ -119,6 +122,7 @@ func makeListenerDiscoChainTests(enterprise bool) []listenerTestCase {
 					},
 				)
 			},
+			alsoRunTestForV2: true,
 		},
 		{
 			name: "connect-proxy-with-chain-external-sni",
@@ -132,6 +136,7 @@ func makeListenerDiscoChainTests(enterprise bool) []listenerTestCase {
 			create: func(t testinf.T) *proxycfg.ConfigSnapshot {
 				return proxycfg.TestConfigSnapshotDiscoveryChain(t, "simple-with-overrides", enterprise, nil, nil)
 			},
+			alsoRunTestForV2: true,
 		},
 		{
 			name: "connect-proxy-with-tcp-chain-failover-through-remote-gateway",
@@ -308,6 +313,7 @@ func TestListenersFromSnapshot(t *testing.T) {
 					ns.Proxy.Config["protocol"] = "grpc"
 				}, nil)
 			},
+			alsoRunTestForV2: true,
 		},
 		{
 			name: "listener-bind-address",
@@ -365,6 +371,7 @@ func TestListenersFromSnapshot(t *testing.T) {
 					ns.Proxy.Config["protocol"] = "http2"
 				}, nil)
 			},
+			alsoRunTestForV2: true,
 		},
 		{
 			name: "listener-balance-inbound-connections",
@@ -391,6 +398,7 @@ func TestListenersFromSnapshot(t *testing.T) {
 					ns.Proxy.Config["protocol"] = "http"
 				}, nil)
 			},
+			alsoRunTestForV2: true,
 		},
 		{
 			name: "http-public-listener-no-xfcc",
@@ -412,6 +420,7 @@ func TestListenersFromSnapshot(t *testing.T) {
 						},
 					})
 			},
+			alsoRunTestForV2: true,
 		},
 		{
 			name: "http-listener-with-timeouts",
@@ -423,6 +432,7 @@ func TestListenersFromSnapshot(t *testing.T) {
 					ns.Proxy.Config["local_idle_timeout_ms"] = 3456
 				}, nil)
 			},
+			alsoRunTestForV2: true,
 		},
 		{
 			name: "http-upstream",
@@ -431,6 +441,7 @@ func TestListenersFromSnapshot(t *testing.T) {
 					ns.Proxy.Upstreams[0].Config["protocol"] = "http"
 				}, nil)
 			},
+			alsoRunTestForV2: true,
 		},
 		{
 			name: "custom-public-listener",
@@ -558,6 +569,7 @@ func TestListenersFromSnapshot(t *testing.T) {
 					}
 				}, nil)
 			},
+			alsoRunTestForV2: true,
 		},
 		{
 			name: "expose-paths-local-app-paths",
@@ -1220,10 +1232,12 @@ func TestListenersFromSnapshot(t *testing.T) {
 			create: func(t testinf.T) *proxycfg.ConfigSnapshot {
 				return proxycfg.TestConfigSnapshotTransparentProxyHTTPUpstream(t)
 			},
+			alsoRunTestForV2: true,
 		},
 		{
-			name:   "transparent-proxy-with-resolver-redirect-upstream",
-			create: proxycfg.TestConfigSnapshotTransparentProxyResolverRedirectUpstream,
+			name:             "transparent-proxy-with-resolver-redirect-upstream",
+			create:           proxycfg.TestConfigSnapshotTransparentProxyResolverRedirectUpstream,
+			alsoRunTestForV2: true,
 		},
 		{
 			name:             "transparent-proxy-catalog-destinations-only",
