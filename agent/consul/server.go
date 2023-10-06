7e827dfa06b477d010fc36845e7ec95fcf0ee8d3
Dhia Ayachi <dhia@hashicorp.com>
2023-10-06 10:57:17 -0400

    activate verifier when running WAL with experimental features

--- agent/consul/server.go
+++ agent/consul/server.go
@@ -1115,6 +1115,8 @@ func (s *Server) setupRaft(isCatalogResourceExperiment bool) error {
 		// Only use WAL if there is no existing raft.db, even if it's enabled.
 		if s.config.LogStoreConfig.Backend == LogStoreBackendDefault && !boltFileExists && isCatalogResourceExperiment {
 			s.config.LogStoreConfig.Backend = LogStoreBackendWAL
+			s.config.LogStoreConfig.Verification.Enabled = true
+			s.config.LogStoreConfig.Verification.Interval = 1 * time.Minute
 			if err = initWAL(); err != nil {
 				return err
 			}
