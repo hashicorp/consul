ad2f474f5378b81e920821a24be50d1f5bfc6344
Ashesh Vidyut <134911583+absolutelightning@users.noreply.github.com>
2023-10-08 18:18:31 +0530

    NET-4135 - Fix NodeMeta filtering Catalog List Services API (#18322)
    * logs for debugging
    
    * Init
    
    * white spaces fix
    
    * added change log
    
    * Fix tests
    
    * fix typo
    
    * using queryoptionfilter to populate args.filter
    
    * tests
    
    * fix test
    
    * fix tests
    
    * fix tests
    
    * fix tests
    
    * fix tests
    
    * fix variable name
    
    * fix tests
    
    * fix tests
    
    * fix tests
    
    * Update .changelog/18322.txt
    
    Co-authored-by: Ganesh S <ganesh.seetharaman@hashicorp.com>
    
    * fix change log
    
    * address nits
    
    * removed unused line
    
    * doing join only when filter has nodemeta
    
    * fix tests
    
    * fix tests
    
    * Update agent/consul/catalog_endpoint.go
    
    Co-authored-by: R.B. Boyer <4903+rboyer@users.noreply.github.com>
    
    * fix tests
    
    * removed unwanted code
    
    ---------
    
    Co-authored-by: Ganesh S <ganesh.seetharaman@hashicorp.com>
    Co-authored-by: R.B. Boyer <4903+rboyer@users.noreply.github.com>

--- agent/consul/state/catalog.go
+++ agent/consul/state/catalog.go
@@ -1138,7 +1138,7 @@ func terminatingGatewayVirtualIPsSupported(tx ReadTxn, ws memdb.WatchSet) (bool,
 }
 
 // Services returns all services along with a list of associated tags.
-func (s *Store) Services(ws memdb.WatchSet, entMeta *acl.EnterpriseMeta, peerName string) (uint64, structs.ServiceNodes, error) {
+func (s *Store) Services(ws memdb.WatchSet, entMeta *acl.EnterpriseMeta, peerName string, joinServiceNodes bool) (uint64, structs.ServiceNodes, error) {
 	tx := s.db.Txn(false)
 	defer tx.Abort()
 
@@ -1156,11 +1156,14 @@ func (s *Store) Services(ws memdb.WatchSet, entMeta *acl.EnterpriseMeta, peerNam
 	for service := services.Next(); service != nil; service = services.Next() {
 		result = append(result, service.(*structs.ServiceNode))
 	}
-	parsedResult, err := parseServiceNodes(tx, ws, result, entMeta, peerName)
-	if err != nil {
-		return 0, nil, fmt.Errorf("failed querying and parsing services :%s", err)
+	if joinServiceNodes {
+		parsedResult, err := parseServiceNodes(tx, ws, result, entMeta, peerName)
+		if err != nil {
+			return 0, nil, fmt.Errorf("failed querying and parsing services :%s", err)
+		}
+		return idx, parsedResult, nil
 	}
-	return idx, parsedResult, nil
+	return idx, result, nil
 }
 
 func (s *Store) ServiceList(ws memdb.WatchSet, entMeta *acl.EnterpriseMeta, peerName string) (uint64, structs.ServiceList, error) {
