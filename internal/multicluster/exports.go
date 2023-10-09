422156211552209e15c732d61f834fe056375620
absolutelightning <ashesh.vidyut@hashicorp.com>
2023-10-09 16:22:31 +0530

    register types

--- internal/multicluster/exports.go
+++ internal/multicluster/exports.go
@@ -1,1 +1,22 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
 package multicluster
+
+import (
+	"github.com/hashicorp/consul/internal/multicluster/internal/types"
+	"github.com/hashicorp/consul/internal/resource"
+)
+
+var (
+	// API Group Information
+	APIGroup        = types.GroupName
+	VersionV1Alpha1 = types.VersionV1Alpha1
+	CurrentVersion  = types.CurrentVersion
+)
+
+// RegisterTypes adds all resource types within the "tenancy" API group
+// to the given type registry
+func RegisterTypes(r resource.Registry) {
+	types.Register(r)
+}
