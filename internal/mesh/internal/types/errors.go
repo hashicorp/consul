e94d6ceca6ba825101e6591b37410579c68ab2cb
Iryna Shustava <ishustava@users.noreply.github.com>
2023-10-13 15:21:39 -0600

    mesh: add validation hook to proxy configuration (#19186)

---
+++ internal/mesh/internal/types/errors.go
@@ -0,0 +1,14 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+package types
+
+import (
+	"errors"
+)
+
+var (
+	errInvalidPort               = errors.New("port number is outside the range 1 to 65535")
+	errInvalidExposePathProtocol = errors.New("invalid protocol: only HTTP and HTTP2 protocols are allowed")
+	errMissingProxyConfigData    = errors.New("at least one of \"bootstrap_config\" or \"dynamic_config\" fields must be set")
+)
