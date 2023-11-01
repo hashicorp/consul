0212042a3c1b094df5f99ed93dbf5313b2d9fda5
trujillo-adam <47586768+trujillo-adam@users.noreply.github.com>
2023-11-01 10:40:51 -0700

    added redirect for conf entries 1.8.x (#19460)
    fixed merge conflict

--- website/redirects.js
+++ website/redirects.js
@@ -19,8 +19,76 @@ module.exports = [
   {
     source:
       '/consul/docs/connect/gateways/mesh-gateway/service-to-service-traffic-datacenters',
-    destination:
-      '/consul/docs/k8s/deployment-configurations/multi-cluster',
+    destination: '/consul/docs/k8s/deployment-configurations/multi-cluster',
     permanent: true,
-  }
+  },
+  {
+    source: '/consul/docs/agi-gateway',
+    destination: '/consul/docs/connect/gateways/agi-gateway',
+    permanent: true,
+  },
+  {
+    source: '/consul/docs/api-gateway/install',
+    destination: '/consul/docs/connect/gateways/api-gateway/deploy/install-k8s',
+    permanent: true,
+  },
+  {
+    source: '/consul/docs/api-gateway/usage/reroute-http-requests',
+    destination:
+      '/consul/docs/connect/gateways/api-gateway/define-routes/reroute-http-requests',
+    permanent: true,
+  },
+  {
+    source: '/consul/docs/api-gateway/usage/route-to-peered-services',
+    destination:
+      '/consul/docs/connect/gateways/api-gateway/route-to-peered-services',
+    permanent: true,
+  },
+  {
+    source: '/consul/docs/api-gateway/usage/errors',
+    destination: '/consul/docs/connect/gateways/api-gateway/errors',
+    permanent: true,
+  },
+  {
+    source: '/consul/docs/api-gateway/usage/usage',
+    destination:
+      '/consul/docs/connect/gateways/api-gateway/deploy/listeners-k8s',
+    permanent: true,
+  },
+  {
+    source: '/consul/docs/api-gateway/upgrades',
+    destination: '/consul/docs/connect/gateways/api-gateway/upgrades-k8s',
+    permanent: true,
+  },
+  {
+    source: '/consul/docs/api-gateway/configuration/',
+    destination: '/consul/docs/connect/gateways/api-gateway/configuration/',
+    permanent: true,
+  },
+  {
+    source: '/consul/docs/api-gateway/configuration/:slug',
+    destination:
+      '/consul/docs/connect/gateways/api-gateway/configuration/:slug',
+    permanent: true,
+  },
+  {
+    source: '/consul/docs/connect/failover',
+    destination: '/consul/docs/connect/manage-traffic/failover',
+    permanent: true,
+  },
+  {
+    source: '/consul/docs/connect/l7',
+    destination: '/consul/docs/connect/manage-traffic',
+    permanent: true,
+  },
+  {
+    source: '/consul/docs/connect/l7/:slug',
+    destination: '/consul/docs/connect/manage-traffic/:slug',
+    permanent: true,
+  },
+  {
+    source: '/consul/docs/v1.8.x/connect/config-entries/:slug',
+    destination: '/consul/docs/v1.8.x/agent/config-entries/:slug',
+    permanent: true,
+  },
 ]
