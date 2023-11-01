3a0a7eae92a0dee47bda65606ecbc303faab884e
John Murret <john.murret@hashicorp.com>
2023-11-01 10:25:46 -0600

    process envoy resource by walking the map.  use a map rather than array for envoy resource to prevent duplication.

--- agent/xdsv2/cluster_resources.go
+++ agent/xdsv2/cluster_resources.go
@@ -6,7 +6,6 @@ package xdsv2
 import (
 	"errors"
 	"fmt"
-
 	envoy_cluster_v3 "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
 	envoy_core_v3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	envoy_aggregate_cluster_v3 "github.com/envoyproxy/go-control-plane/envoy/extensions/clusters/aggregate/v3"
@@ -19,41 +18,11 @@ import (
 	"github.com/hashicorp/consul/proto-public/pbmesh/v2beta1/pbproxystate"
 )
 
-func (pr *ProxyResources) doesEnvoyClusterAlreadyExist(name string) bool {
-	// TODO(proxystate): consider using a map instead of [] for this kind of lookup
-	for _, envoyCluster := range pr.envoyResources[xdscommon.ClusterType] {
-		if envoyCluster.(*envoy_cluster_v3.Cluster).Name == name {
-			return true
-		}
-	}
-	return false
-}
-
-func (pr *ProxyResources) makeXDSClusters() ([]proto.Message, error) {
-	clusters := make([]proto.Message, 0)
-
-	for clusterName := range pr.proxyState.Clusters {
-		protoCluster, err := pr.makeClusters(clusterName)
-		// TODO: aggregate errors for clusters and still return any properly formed clusters.
-		if err != nil {
-			return nil, err
-		}
-		clusters = append(clusters, protoCluster...)
-	}
-
-	return clusters, nil
-}
-
-func (pr *ProxyResources) makeClusters(name string) ([]proto.Message, error) {
-	clusters := make([]proto.Message, 0)
+func (pr *ProxyResources) makeClusters(name string) (map[string]proto.Message, error) {
+	clusters := make(map[string]proto.Message)
 	proxyStateCluster, ok := pr.proxyState.Clusters[name]
 	if !ok {
 		return nil, fmt.Errorf("cluster %q not found", name)
-	}
-
-	if pr.doesEnvoyClusterAlreadyExist(name) {
-		// don't error
-		return []proto.Message{}, nil
 	}
 
 	switch proxyStateCluster.Group.(type) {
@@ -64,7 +33,7 @@ func (pr *ProxyResources) makeClusters(name string) ([]proto.Message, error) {
 			return nil, err
 		}
 		for _, c := range clusters {
-			clusters = append(clusters, c)
+			clusters[c.Name] = c
 		}
 
 	case *pbproxystate.Cluster_EndpointGroup:
@@ -73,7 +42,7 @@ func (pr *ProxyResources) makeClusters(name string) ([]proto.Message, error) {
 		if err != nil {
 			return nil, err
 		}
-		clusters = append(clusters, cluster)
+		clusters[cluster.Name] = cluster
 
 	default:
 		return nil, errors.New("cluster group type should be Endpoint Group or Failover Group")
@@ -207,8 +176,8 @@ func (pr *ProxyResources) makeEnvoyPassthroughCluster(name string, protocol pbpr
 	return cluster, nil
 }
 
-func (pr *ProxyResources) makeEnvoyAggregateCluster(name string, protocol pbproxystate.Protocol, fg *pbproxystate.FailoverGroup) ([]*envoy_cluster_v3.Cluster, error) {
-	var clusters []*envoy_cluster_v3.Cluster
+func (pr *ProxyResources) makeEnvoyAggregateCluster(name string, protocol pbproxystate.Protocol, fg *pbproxystate.FailoverGroup) (map[string]*envoy_cluster_v3.Cluster, error) {
+	clusters := make(map[string]*envoy_cluster_v3.Cluster)
 	if fg != nil {
 		var egNames []string
 		for _, eg := range fg.EndpointGroups {
@@ -217,7 +186,7 @@ func (pr *ProxyResources) makeEnvoyAggregateCluster(name string, protocol pbprox
 				return nil, err
 			}
 			egNames = append(egNames, cluster.Name)
-			clusters = append(clusters, cluster)
+			clusters[cluster.Name] = cluster
 		}
 		aggregateClusterConfig, err := anypb.New(&envoy_aggregate_cluster_v3.ClusterConfig{
 			Clusters: egNames,
@@ -245,7 +214,7 @@ func (pr *ProxyResources) makeEnvoyAggregateCluster(name string, protocol pbprox
 		if err != nil {
 			return nil, err
 		}
-		clusters = append(clusters, c)
+		clusters[c.Name] = c
 	}
 	return clusters, nil
 }
@@ -376,9 +345,38 @@ func addEnvoyLBToCluster(dynamicConfig *pbproxystate.DynamicEndpointGroupConfig,
 	return nil
 }
 
-// TODO(proxystate): In a future PR this will create clusters and add it to ProxyResources.proxyState
-// Currently, we do not traverse the listener -> endpoint paths and instead just generate each resource by iterating
-// through its top level map. In the future we want to traverse these paths to ensure each listener has a cluster, etc.
 func (pr *ProxyResources) makeEnvoyClusterFromL4Destination(l4 *pbproxystate.L4Destination) error {
+	switch l4.Destination.(type) {
+	case *pbproxystate.L4Destination_Cluster:
+		clusterName := l4.GetCluster().Name
+		clusters, _ := pr.makeClusters(clusterName)
+		for name, cluster := range clusters {
+			pr.envoyResources[xdscommon.ClusterType][name] = cluster
+		}
+
+		eps := pr.proxyState.GetEndpoints()[clusterName]
+		if eps != nil {
+			protoEndpoint := makeEnvoyClusterLoadAssignment(clusterName, eps.Endpoints)
+			pr.envoyResources[xdscommon.EndpointType][protoEndpoint.ClusterName] = protoEndpoint
+		}
+
+	case *pbproxystate.L4Destination_WeightedClusters:
+		psWeightedClusters := l4.GetWeightedClusters()
+		for _, psCluster := range psWeightedClusters.GetClusters() {
+			clusters, _ := pr.makeClusters(psCluster.Name)
+			for name, cluster := range clusters {
+				pr.envoyResources[xdscommon.ClusterType][name] = cluster
+			}
+
+			eps := pr.proxyState.GetEndpoints()[psCluster.Name]
+			if eps != nil {
+				protoEndpoint := makeEnvoyClusterLoadAssignment(psCluster.Name, eps.Endpoints)
+				pr.envoyResources[xdscommon.EndpointType][psCluster.Name] = protoEndpoint
+			}
+		}
+	default:
+		return errors.New("cluster group type should be Endpoint Group or Failover Group")
+	}
+
 	return nil
 }
--- agent/xdsv2/resources.go
+++ agent/xdsv2/resources.go
@@ -30,46 +30,41 @@ func NewResourceGenerator(
 
 type ProxyResources struct {
 	proxyState     *proxytracker.ProxyState
-	envoyResources map[string][]proto.Message
+	envoyResources map[string]map[string]proto.Message
 }
 
 func (g *ResourceGenerator) AllResourcesFromIR(proxyState *proxytracker.ProxyState) (map[string][]proto.Message, error) {
 	pr := &ProxyResources{
 		proxyState:     proxyState,
-		envoyResources: make(map[string][]proto.Message),
+		envoyResources: make(map[string]map[string]proto.Message),
 	}
+	pr.envoyResources[xdscommon.ListenerType] = make(map[string]proto.Message)
+	pr.envoyResources[xdscommon.RouteType] = make(map[string]proto.Message)
+	pr.envoyResources[xdscommon.ClusterType] = make(map[string]proto.Message)
+	pr.envoyResources[xdscommon.EndpointType] = make(map[string]proto.Message)
+
 	err := pr.generateXDSResources()
 	if err != nil {
 		return nil, fmt.Errorf("failed to generate xDS resources for ProxyState: %v", err)
 	}
-	return pr.envoyResources, nil
+	envoyResources := make(map[string][]proto.Message)
+	envoyResources[xdscommon.ListenerType] = make([]proto.Message, 0)
+	envoyResources[xdscommon.RouteType] = make([]proto.Message, 0)
+	envoyResources[xdscommon.ClusterType] = make([]proto.Message, 0)
+	envoyResources[xdscommon.EndpointType] = make([]proto.Message, 0)
+	for resourceTypeName, resourceMap := range pr.envoyResources {
+		for _, resource := range resourceMap {
+			envoyResources[resourceTypeName] = append(envoyResources[resourceTypeName], resource)
+		}
+	}
+	return envoyResources, nil
 }
 
 func (pr *ProxyResources) generateXDSResources() error {
-	listeners, err := pr.makeXDSListeners()
+	err := pr.makeXDSListeners()
 	if err != nil {
 		return err
 	}
-
-	pr.envoyResources[xdscommon.ListenerType] = listeners
-
-	clusters, err := pr.makeXDSClusters()
-	if err != nil {
-		return err
-	}
-	pr.envoyResources[xdscommon.ClusterType] = clusters
-
-	endpoints, err := pr.makeXDSEndpoints()
-	if err != nil {
-		return err
-	}
-	pr.envoyResources[xdscommon.EndpointType] = endpoints
-
-	routes, err := pr.makeXDSRoutes()
-	if err != nil {
-		return err
-	}
-	pr.envoyResources[xdscommon.RouteType] = routes
 
 	return nil
 }
