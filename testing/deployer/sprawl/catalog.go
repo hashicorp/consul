70bcd082dc524aa6a4c31c686a42c48c33d90703
R.B. Boyer <rb@hashicorp.com>
2023-11-03 20:37:38 +0000

    backport of commit ebc913ffbd196841242f45d27a9c6c3ad1957e94

--- testing/deployer/sprawl/catalog.go
+++ testing/deployer/sprawl/catalog.go
@@ -297,7 +297,16 @@ func (s *Sprawl) registerCatalogNode(
 	node *topology.Node,
 ) error {
 	if node.IsV2() {
-		return s.registerCatalogNodeV2(cluster, node)
+
+		// TODO(rb): nodes are optional in v2 and won't be used in k8s by
+		// default. There are some scoping issues with the Node Type in 1.17 so
+		// disable it for now.
+		//
+		// To re-enable you also need to link it to the Workload by setting the
+		// NodeName field.
+		//
+		// return s.registerCatalogNodeV2(cluster, node)
+		return nil
 	}
 	return s.registerCatalogNodeV1(cluster, node)
 }
@@ -538,7 +547,8 @@ func serviceInstanceToResources(
 				Metadata: svc.Meta,
 			},
 			Data: &pbcatalog.Workload{
-				NodeName: node.PodName(),
+				// TODO(rb): disabling this until node scoping makes sense again
+				// NodeName: node.PodName(),
 				Identity: svc.ID.Name,
 				Ports:    wlPorts,
 				Addresses: []*pbcatalog.WorkloadAddress{
