f8a40d81a1c9d89b9166339bf3e8c281de288f19
R.B. Boyer <rb@hashicorp.com>
2023-11-08 16:08:50 -0600

    random: only conditionally build the tproxy-enhanced dataplane image

--- testing/deployer/sprawl/internal/build/docker.go
+++ testing/deployer/sprawl/internal/build/docker.go
@@ -102,6 +102,14 @@ func DockerImages(
 	built := make(map[string]struct{})
 	for _, c := range t.Clusters {
 		for _, n := range c.Nodes {
+			needsTproxy := false
+			for _, svc := range n.Services {
+				if svc.EnableTransparentProxy {
+					needsTproxy = true
+					break
+				}
+			}
+
 			joint := n.Images.EnvoyConsulImage()
 			if _, ok := built[joint]; joint != "" && !ok {
 				logger.Info("building envoy+consul image", "image", joint)
@@ -145,7 +153,7 @@ func DockerImages(
 			}
 
 			cdpTproxy := n.Images.LocalDataplaneTProxyImage()
-			if _, ok := built[cdpTproxy]; cdpTproxy != "" && !ok {
+			if _, ok := built[cdpTproxy]; cdpTproxy != "" && !ok && needsTproxy {
 				logger.Info("building image", "image", cdpTproxy)
 				logw := logger.Named("docker_dataplane_tproxy").StandardWriter(&hclog.StandardLoggerOptions{ForceLevel: hclog.Debug})
 				err := run.DockerExecWithStderr(context.TODO(), []string{
