a42ce0c1fdb255552fa0369754b759cddb6a3a56
John Murret <john.murret@hashicorp.com>
2023-11-13 17:38:31 -0700

    fix runner count logic in set_test_package_matrix.sh from adding an additional runner

--- .github/scripts/set_test_package_matrix.sh
+++ .github/scripts/set_test_package_matrix.sh
@@ -5,7 +5,17 @@
 set -euo pipefail
 export RUNNER_COUNT=$1
 
+if ((RUNNER_COUNT == 1 ))
+then
+  EFFECTIVE_RUNNER_COUNT=$RUNNER_COUNT
+elif ((RUNNER_COUNT > 1 ))
+then
+  EFFECTIVE_RUNNER_COUNT=$((RUNNER_COUNT-1))
+else
+  echo ERROR: RUNNER_COUNT must be greater than zero.
+  exit 1 # terminate and indicate error
+fi
 # set matrix var to list of unique packages containing tests
-matrix="$(go list -json="ImportPath,TestGoFiles" ./... | jq --compact-output '. | select(.TestGoFiles != null) | .ImportPath' | shuf | jq --slurp --compact-output '.' | jq --argjson runnercount $RUNNER_COUNT  -cM '[_nwise(length / $runnercount | floor)]'))"
+matrix="$(go list -json="ImportPath,TestGoFiles" ./... | jq --compact-output '. | select(.TestGoFiles != null) | .ImportPath' | shuf | jq --slurp --compact-output '.' | jq --argjson runnercount $EFFECTIVE_RUNNER_COUNT  -cM '[_nwise(length / $runnercount | floor)]'))"
 
 echo "matrix=${matrix}" >> "${GITHUB_OUTPUT}"
