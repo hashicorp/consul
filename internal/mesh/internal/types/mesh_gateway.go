7e126c639c64075d7a3a5438e00c3821740d7643
Nathan Coleman <nathan.coleman@hashicorp.com>
2023-11-14 16:22:45 -0500

    Implement ACLs hook for reading + writing MeshGateway type

--- internal/mesh/internal/types/mesh_gateway.go
+++ internal/mesh/internal/types/mesh_gateway.go
@@ -4,16 +4,26 @@
 package types
 
 import (
+	"github.com/hashicorp/consul/acl"
 	"github.com/hashicorp/consul/internal/resource"
 	pbmesh "github.com/hashicorp/consul/proto-public/pbmesh/v2beta1"
+	"github.com/hashicorp/consul/proto-public/pbresource"
 )
 
 func RegisterMeshGateway(r resource.Registry) {
 	r.Register(resource.Registration{
-		Type:     pbmesh.MeshGatewayType,
-		Proto:    &pbmesh.MeshGateway{},
-		Scope:    resource.ScopePartition,
-		ACLs:     nil, // TODO NET-6423
+		Type:  pbmesh.MeshGatewayType,
+		Proto: &pbmesh.MeshGateway{},
+		Scope: resource.ScopePartition,
+		ACLs: &resource.ACLHooks{
+			Read: func(authorizer acl.Authorizer, context *acl.AuthorizerContext, _ *pbresource.ID, _ *pbresource.Resource) error {
+				return authorizer.ToAllowAuthorizer().MeshReadAllowed(context)
+			},
+			Write: func(authorizer acl.Authorizer, context *acl.AuthorizerContext, _ *pbresource.Resource) error {
+				return authorizer.ToAllowAuthorizer().MeshWriteAllowed(context)
+			},
+			List: resource.NoOpACLListHook,
+		},
 		Mutate:   nil, // TODO NET-6425
 		Validate: nil, // TODO NET-6424
 	})
