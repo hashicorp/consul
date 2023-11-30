22a6d5d62bd60064246df7573859352d83b509e5
Nick Irvine <115657443+nfi-hashicorp@users.noreply.github.com>
2023-11-30 10:24:44 -0800

    create gotestdoc command

---
+++ internal/tools/gotestdoc/main.go
@@ -0,0 +1,204 @@
+// gotestdoc parses well-formed comments in Go tests to extract BDD-like
+// test specs, without the burden of some goofy DSL.
+
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/doc/comment"
+	"go/parser"
+	"go/token"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/hashicorp/go-multierror"
+)
+
+type TestDoc struct {
+	Givens      []comment.Block
+	GivenPerms  []Perm
+	Expects     []Expect
+	ExpectPerms []Perm
+}
+
+type Expect struct {
+	When comment.Block
+	Then comment.Block
+}
+
+type Perm comment.Block
+
+const (
+	kwGiven  = "Given"
+	kwExpect = "Expect"
+	kwPerms  = "With permutations:"
+)
+
+const (
+	fsmStart       = "start"
+	fsmGivenBody   = "givenBody"
+	fsmGivenPerms  = "givenPerms"
+	fsmExpectBody  = "expectBody"
+	fsmExpectPerms = "expectPerms"
+)
+
+func parseExpectItem(li *comment.ListItem) (Expect, error) {
+	// TODO
+	return Expect{}, nil
+}
+
+func parseFuncComment(s string) (*TestDoc, error) {
+	docParser := comment.Parser{
+		// TODO: links and symbols
+	}
+	doc := docParser.Parse(s)
+	ret := TestDoc{}
+	var errs error
+	// TODO; probably a better way to do FSM, but meh
+	fsm := fsmStart
+	for _, b := range doc.Content {
+		switch v := b.(type) {
+		case *comment.Heading:
+			// TODO: not sure when len > 1?
+			if len(v.Text) != 1 {
+				errs = multierror.Append(errs, fmt.Errorf("len of heading text != 1: %#v", v))
+				continue
+			}
+			text := v.Text[0]
+			switch fsm {
+			case fsmStart:
+				vt, ok := text.(comment.Plain)
+				if !ok {
+					errs = multierror.Append(errs, fmt.Errorf("given heading should be plain; is: %#v", v))
+					continue
+				}
+				if vt != kwGiven {
+					errs = multierror.Append(errs, fmt.Errorf("given heading should be 'Given'; is: %#v", vt))
+					continue
+				}
+				fsm = fsmGivenBody
+				continue
+			case fsmGivenBody, fsmGivenPerms:
+				vt, ok := text.(comment.Plain)
+				if !ok {
+					errs = multierror.Append(errs, fmt.Errorf("Expect heading should be plain; is: %#v", v))
+					continue
+				}
+				if vt != kwExpect {
+					errs = multierror.Append(errs, fmt.Errorf("Expect heading should be 'Expect'; is: %#v", vt))
+					continue
+				}
+				fsm = fsmExpectBody
+				continue
+			default:
+				log.Printf("unhandled Heading state: %s", fsm)
+				continue
+			}
+		case *comment.Paragraph:
+			switch fsm {
+			case fsmGivenBody:
+				if p, ok := v.Text[0].(comment.Plain); ok && p == kwPerms {
+					fsm = fsmGivenPerms
+					continue
+				}
+				ret.Givens = append(ret.Givens, v)
+			case fsmGivenPerms:
+				// TODO: append? treat as single?
+				log.Printf("unhandled Paragraph in given perms")
+			case fsmExpectBody:
+				if p, ok := v.Text[0].(comment.Plain); ok && p == kwPerms {
+					fsm = fsmExpectPerms
+					continue
+				}
+				errs = multierror.Append(errs, fmt.Errorf("no paragraphs in expect body"))
+				continue
+			case fsmExpectPerms:
+				// TODO: append
+				log.Printf("unhandled Paragraph in expect perms")
+			default:
+				log.Printf("unhandled Paragraph state: %s", fsm)
+				continue
+			}
+		case *comment.List:
+			switch fsm {
+			case fsmExpectBody:
+				for i, li := range v.Items {
+					ex, err := parseExpectItem(li)
+					if err != nil {
+						errs = multierror.Append(errs, fmt.Errorf("parsing expect item %d (%q): %q", i, li.Number, err))
+					}
+					ret.Expects = append(ret.Expects, ex)
+				}
+			case fsmGivenBody:
+				ret.Givens = append(ret.Givens, v)
+			case fsmGivenPerms:
+				ret.GivenPerms = append(ret.GivenPerms, v)
+			case fsmExpectPerms:
+				ret.ExpectPerms = append(ret.ExpectPerms, v)
+			default:
+				log.Printf("unhandled List state: %s", fsm)
+				continue
+			}
+
+		default:
+			switch fsm {
+			case fsmGivenBody:
+				ret.Givens = append(ret.Givens, v)
+			case fsmGivenPerms:
+				ret.GivenPerms = append(ret.GivenPerms, v)
+			case fsmExpectPerms:
+				ret.ExpectPerms = append(ret.ExpectPerms, v)
+			default:
+				log.Printf("unhandled state for misc block type: %s", fsm)
+				continue
+			}
+			log.Printf("unhandled block type: %T", b)
+			continue
+		}
+	}
+	return &ret, errs
+}
+
+func main() {
+	// TODO: real arg parsing
+	filename := os.Args[1]
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, filename, nil, parser.ParseComments)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	testDocs := map[string]*TestDoc{}
+	errs := map[string]error{}
+
+	for _, d := range file.Decls {
+		switch v := d.(type) {
+		case *ast.FuncDecl:
+			funcname := v.Name.String()
+			// TODO: also check that its signature is (t *testing.T)
+			// TODO: handle duplicates?
+			if strings.HasPrefix(funcname, "Test") {
+				td, err := parseFuncComment(v.Doc.Text())
+				if err != nil {
+					errs[funcname] = err
+					continue
+				}
+				testDocs[funcname] = td
+			}
+		}
+	}
+
+	if len(errs) > 0 {
+		spew.Printf("ERRORS: %#v", errs)
+	}
+	if len(testDocs) > 0 {
+		spew.Dump(testDocs)
+	}
+	if len(errs) > 0 {
+		os.Exit(1)
+	}
+
+}
---
+++ internal/tools/gotestdoc/main_test.go
@@ -0,0 +1,3 @@
+// In gotestdoc's tests, we write in gotestdoc style for dogfooding purposes.
+// Normally gotestdoc would be overkill for unit tests.
+package main
