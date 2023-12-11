0d7cade9d3c7865cf576bdcfcedf192caa2bcfa3
Derek Menteer <105233703+hashi-derek@users.noreply.github.com>
2023-12-11 12:05:07 -0600

    Backport of: Fix ClusterLoadAssignment timeouts dropping endpoints. into 1.15 (#19886)
    Fix ClusterLoadAssignment timeouts dropping endpoints.
    
    When a large number of upstreams are configured on a single envoy
    proxy, there was a chance that it would timeout when waiting for
    ClusterLoadAssignments. While this doesn't always immediately cause
    issues, consul-dataplane instances appear to consistently drop
    endpoints from their configurations after an xDS connection is
    re-established (the server dies, random disconnect, etc).
    
    This commit adds an `xds_fetch_timeout_ms` config to service registrations
    so that users can set the value higher for large instances that have
    many upstreams. The timeout can be disabled by setting a value of `0`.
    
    This configuration was introduced to reduce the risk of causing a
    breaking change for users if there is ever a scenario where endpoints
    would never be received. Rather than just always blocking indefinitely
    or for a significantly longer period of time, this config will affect
    only the service instance associated with it.

--- agent/xds/listeners_test.go
+++ agent/xds/listeners_test.go
@@ -1069,7 +1069,7 @@ func TestListenersFromSnapshot(t *testing.T) {
 		{
 			name: "transparent-proxy-http-upstream",
 			create: func(t testinf.T) *proxycfg.ConfigSnapshot {
-				return proxycfg.TestConfigSnapshotTransparentProxyHTTPUpstream(t)
+				return proxycfg.TestConfigSnapshotTransparentProxyHTTPUpstream(t, nil)
 			},
 		},
 		{
