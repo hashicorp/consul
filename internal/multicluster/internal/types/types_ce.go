7ebdc3e72be66e477e9a6df9154605d935c269e3
Tauhid <tauhidanjum@gmail.com>
2023-12-11 11:42:52 +0530

    Adding sameness group protobuff in consul CE

---
+++ internal/multicluster/internal/types/types_ce.go
@@ -0,0 +1,11 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: BUSL-1.1
+
+//go:build !consulent
+
+package types
+
+import "github.com/hashicorp/consul/internal/resource"
+
+func RegisterEnterprise(r resource.Registry) {
+}
