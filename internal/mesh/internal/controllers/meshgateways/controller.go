2a7593b15217fee61ad3b98af2cb2ef8ecf66cec
Nathan Coleman <nathan.coleman@hashicorp.com>
2023-12-11 18:07:55 -0500

    NET-6899 Create name-aligned Service when reconciling MeshGateway resource
    The Service has an owner reference added to it indicating that it belongs to a MeshGateway

--- internal/mesh/internal/controllers/meshgateways/controller.go
+++ internal/mesh/internal/controllers/meshgateways/controller.go
@@ -5,10 +5,14 @@ package meshgateways
 
 import (
 	"context"
-	"errors"
+
+	"google.golang.org/protobuf/types/known/anypb"
 
 	"github.com/hashicorp/consul/internal/controller"
+	"github.com/hashicorp/consul/internal/resource"
+	pbcatalog "github.com/hashicorp/consul/proto-public/pbcatalog/v2beta1"
 	pbmesh "github.com/hashicorp/consul/proto-public/pbmesh/v2beta1"
+	"github.com/hashicorp/consul/proto-public/pbresource"
 )
 
 const (
@@ -24,7 +28,36 @@ func Controller() *controller.Controller {
 
 type reconciler struct{}
 
+// Reconcile is responsible for creating a Service w/ a MeshGateway owner,
+// in addition to other things discussed in the RFC.
 func (r *reconciler) Reconcile(ctx context.Context, rt controller.Runtime, req controller.Request) error {
+	rt.Logger = rt.Logger.With("resource-id", req.ID)
+	rt.Logger.Trace("reconciling mesh gateway")
+
+	service := &pbcatalog.Service{
+		Workloads: &pbcatalog.WorkloadSelector{
+			// Assumes that our MeshGateway Deployment in K8s is named the same as the MeshGateway resource in Consul
+			// TODO(nathancoleman) Fetch the MeshGateway and use WorkloadSelector from there
+			Prefixes: []string{req.ID.Name},
+		},
+	}
+
+	serviceData, err := anypb.New(service)
+	if err != nil {
+		return err
+	}
+
+	_, err = rt.Client.Write(ctx, &pbresource.WriteRequest{
+		Resource: &pbresource.Resource{
+			Id:    resource.ReplaceType(pbcatalog.ServiceType, req.ID),
+			Owner: req.ID,
+			Data:  serviceData,
+		},
+	})
+	if err != nil {
+		return err
+	}
+
 	// TODO NET-6426, NET-6427, NET-6428, NET-6429, NET-6430, NET-6431, NET-6432
-	return errors.New("not implemented")
+	return nil
 }
