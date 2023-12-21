555c04532aca42d129090927473d5b251a8dd2e0
Nick Ethier <nethier@hashicorp.com>
2023-12-21 14:20:31 -0500

    pbhcp: add TelemetryState resource

---
+++ internal/hcp/internal/types/telemetry_state.go
@@ -0,0 +1,61 @@
+package types
+
+import (
+	"errors"
+
+	"github.com/hashicorp/go-multierror"
+
+	"github.com/hashicorp/consul/internal/resource"
+	pbhcp "github.com/hashicorp/consul/proto-public/pbhcp/v1"
+)
+
+type DecodedTelemetryState = resource.DecodedResource[*pbhcp.TelemetryState]
+
+var (
+	telemetryStateConfigurationNameError = errors.New("only a single Telemetry resource is allowed and it must be named global")
+)
+
+func RegisterTelemetryState(r resource.Registry) {
+	r.Register(resource.Registration{
+		Type:     pbhcp.TelemetryStateType,
+		Proto:    &pbhcp.TelemetryState{},
+		Scope:    resource.ScopeCluster,
+		Validate: ValidateTelemetryState,
+	})
+}
+
+var ValidateTelemetryState = resource.DecodeAndValidate(validateTelemetryState)
+
+func validateTelemetryState(res *DecodedTelemetryState) error {
+	var err error
+
+	if res.Id.Name != "global" {
+		err = multierror.Append(err, resource.ErrInvalidField{
+			Name:    "name",
+			Wrapped: telemetryStateConfigurationNameError,
+		})
+	}
+
+	if res.Data.ClientId == "" {
+		err = multierror.Append(err, resource.ErrInvalidField{
+			Name:    "client_id",
+			Wrapped: resource.ErrMissing,
+		})
+	}
+
+	if res.Data.ClientSecret == "" {
+		err = multierror.Append(err, resource.ErrInvalidField{
+			Name:    "client_secret",
+			Wrapped: resource.ErrMissing,
+		})
+	}
+
+	if res.Data.ResourceId == "" {
+		err = multierror.Append(err, resource.ErrInvalidField{
+			Name:    "resource_id",
+			Wrapped: resource.ErrMissing,
+		})
+	}
+
+	return err
+}
