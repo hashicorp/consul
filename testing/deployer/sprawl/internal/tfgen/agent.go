8e2d4e3aaf4e3cdd8ac1584eeafae53dbd813019
cskh <hui.kang@hashicorp.com>
2024-01-03 12:52:49 -0500

    upgrade test(LTS): import remaining agent config from usage profile (#19999)
    * upgrade test(LTS): import remaining agent config from usage profile

--- testing/deployer/sprawl/internal/tfgen/agent.go
+++ testing/deployer/sprawl/internal/tfgen/agent.go
@@ -25,6 +25,12 @@ func (g *Generator) generateAgentHCL(node *topology.Node, enableV2, enableV2Tena
 
 	var b HCLBuilder
 
+	// We first write ExtraConfig since it could be overwritten by specific
+	// configurations below
+	if node.ExtraConfig != "" {
+		b.format(node.ExtraConfig)
+	}
+
 	b.add("server", node.IsServer())
 	b.add("bind_addr", "0.0.0.0")
 	b.add("client_addr", "0.0.0.0")
@@ -49,6 +55,7 @@ func (g *Generator) generateAgentHCL(node *topology.Node, enableV2, enableV2Tena
 	// speed up leaves
 	b.addBlock("performance", func() {
 		b.add("leave_drain_time", "50ms")
+		b.add("raft_multiplier", 1)
 	})
 
 	b.add("primary_datacenter", node.Datacenter)
@@ -61,6 +68,9 @@ func (g *Generator) generateAgentHCL(node *topology.Node, enableV2, enableV2Tena
 	// }
 
 	if node.Segment != nil {
+		if node.Kind != topology.NodeKindClient {
+			panic("segment only applies to client agent")
+		}
 		b.add("segment", node.Segment.Name)
 		b.addSlice("retry_join", []string{
 			fmt.Sprintf("server.%s-consulcluster.lan:%d", node.Cluster, node.Segment.Port),
