565d8b094f172a664aedf99de5a289203a64b6bf
Derek Menteer <derek.menteer@hashicorp.com>
2024-01-09 19:09:22 +0000

    backport of commit 7a91738824bcca579b96c76bfb397793d816797a

--- agent/consul/leader_test.go
+++ agent/consul/leader_test.go
@@ -2087,7 +2087,7 @@ func TestDatacenterSupportsIntentionsAsConfigEntries(t *testing.T) {
 			RaftIndex: got.RaftIndex,
 			Hash:      got.GetHash(),
 		}
-
+		got.Hash = 0
 		require.Equal(t, expect, got)
 	})
 
