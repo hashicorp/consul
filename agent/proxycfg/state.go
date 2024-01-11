75139aee0874e2f264d81e8f90fb27e2e7cd06ce
Dhia Ayachi <dhia@hashicorp.com>
2024-01-11 20:48:49 +0000

    backport of commit 4ac54f10bc0cca4ae7304906498bdfebcd0f98b9

--- agent/proxycfg/state.go
+++ agent/proxycfg/state.go
@@ -541,7 +541,7 @@ func watchMeshGateway(ctx context.Context, opts gatewayWatchOpts) error {
 		QueryOptions:   structs.QueryOptions{Token: opts.token},
 		ServiceKind:    structs.ServiceKindMeshGateway,
 		UseServiceKind: true,
-		NodesOnly:      false,
+		NodesOnly:      true,
 		Source:         opts.source,
 		EnterpriseMeta: *structs.DefaultEnterpriseMetaInPartition(opts.key.Partition),
 	}, correlationId, opts.notifyCh)
