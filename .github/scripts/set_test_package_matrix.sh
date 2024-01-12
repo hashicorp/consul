52ab488c6af97f0e300f00509cbba79ccbc34ebd
John Murret <john.murret@hashicorp.com>
2024-01-12 13:04:25 -0700

    always run agent tests alone on their own runner

--- .github/scripts/set_test_package_matrix.sh
+++ .github/scripts/set_test_package_matrix.sh
@@ -9,9 +9,11 @@ if ((RUNNER_COUNT < 1 ))
 then
   echo ERROR: RUNNER_COUNT must be greater than zero.
   exit 1 # terminate and indicate error
+else
+  EFFECTIVE_RUNNER_COUNT=$((RUNNER_COUNT-1))
 fi
 
 # set matrix var to list of unique packages containing tests
-matrix="$(go list -json="ImportPath,TestGoFiles" ./... | jq --compact-output '. | select(.TestGoFiles != null) | .ImportPath' | shuf | jq --slurp --compact-output '.' | jq --argjson runnercount $RUNNER_COUNT  -cM '[_nwise(length / $runnercount | ceil)]'))"
+matrix="$(go list -json="ImportPath,TestGoFiles" ./... | jq --compact-output '. | select(.TestGoFiles != null) | select(.ImportPath != "github.com/hashicorp/consul/agent") | .ImportPath' | shuf | jq --slurp --compact-output '.' | jq --argjson runnercount $EFFECTIVE_RUNNER_COUNT  -cM '[_nwise(length / $runnercount | ceil)]' | jq --compact-output  '. += [["github.com/hashicorp/consul/agent"]]'))"
 
 echo "matrix=${matrix}" >> "${GITHUB_OUTPUT}"
