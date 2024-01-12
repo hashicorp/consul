c14bf5a1aa5fe4d0fddc332b327fb634a9afd181
John Murret <john.murret@hashicorp.com>
2024-01-12 13:18:17 -0700

    prevent runners less than 2

--- .github/scripts/set_test_package_matrix.sh
+++ .github/scripts/set_test_package_matrix.sh
@@ -5,13 +5,10 @@
 set -euo pipefail
 export RUNNER_COUNT=$1
 
-if ((RUNNER_COUNT < 1 ))
+if ((RUNNER_COUNT < 2 ))
 then
-  echo ERROR: RUNNER_COUNT must be greater than zero.
+  echo ERROR: RUNNER_COUNT must be greater than 1. Otherwise use the non-split unit test cod in .github/workflows/reusable-unit.yml.
   exit 1 # terminate and indicate error
-elif ((RUNNER_COUNT == 1 ))
-then
-  EFFECTIVE_RUNNER_COUNT=1
 else
   EFFECTIVE_RUNNER_COUNT=$((RUNNER_COUNT-1))
 fi
@@ -19,5 +16,4 @@ fi
 # set matrix var to list of unique packages containing tests
 matrix="$(go list -json="ImportPath,TestGoFiles" ./... | jq --compact-output '. | select(.TestGoFiles != null) | select(.ImportPath != "github.com/hashicorp/consul/agent") | .ImportPath' | shuf | jq --slurp --compact-output '.' | jq --argjson runnercount $EFFECTIVE_RUNNER_COUNT  -cM '[_nwise(length / $runnercount | ceil)]' | jq --compact-output  '. += [["github.com/hashicorp/consul/agent"]]'))"
 
-echo "matrix=${matrix}"
-#>> "${GITHUB_OUTPUT}"
+echo "matrix=${matrix}" >> "${GITHUB_OUTPUT}"
