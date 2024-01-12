78590c162b3542d20a628f605bde01385286581e
John Murret <john.murret@hashicorp.com>
2024-01-12 13:15:14 -0700

    deal with runner count = 1

--- .github/scripts/set_test_package_matrix.sh
+++ .github/scripts/set_test_package_matrix.sh
@@ -9,6 +9,9 @@ if ((RUNNER_COUNT < 1 ))
 then
   echo ERROR: RUNNER_COUNT must be greater than zero.
   exit 1 # terminate and indicate error
+elif ((RUNNER_COUNT == 1 ))
+then
+  EFFECTIVE_RUNNER_COUNT=1
 else
   EFFECTIVE_RUNNER_COUNT=$((RUNNER_COUNT-1))
 fi
@@ -16,4 +19,5 @@ fi
 # set matrix var to list of unique packages containing tests
 matrix="$(go list -json="ImportPath,TestGoFiles" ./... | jq --compact-output '. | select(.TestGoFiles != null) | select(.ImportPath != "github.com/hashicorp/consul/agent") | .ImportPath' | shuf | jq --slurp --compact-output '.' | jq --argjson runnercount $EFFECTIVE_RUNNER_COUNT  -cM '[_nwise(length / $runnercount | ceil)]' | jq --compact-output  '. += [["github.com/hashicorp/consul/agent"]]'))"
 
-echo "matrix=${matrix}" >> "${GITHUB_OUTPUT}"
+echo "matrix=${matrix}"
+#>> "${GITHUB_OUTPUT}"
