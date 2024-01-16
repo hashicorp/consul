56b8de4da9b654a51f344a2482c7a9a7a399ca26
Derek Menteer <derek.menteer@hashicorp.com>
2024-01-16 14:16:49 +0000

    backport of commit fead5971308afb9d035ae2f031d5fbd0d9e2b3b3

--- agent/grpc-external/services/peerstream/stream_test.go
+++ agent/grpc-external/services/peerstream/stream_test.go
@@ -1175,7 +1175,7 @@ func TestStreamResources_Server_CARootUpdates(t *testing.T) {
 
 func TestStreamResources_Server_AckNackNonce(t *testing.T) {
 	srv, store := newTestServer(t, func(c *Config) {
-		c.incomingHeartbeatTimeout = 10 * time.Millisecond
+		c.incomingHeartbeatTimeout = 5 * time.Second
 	})
 
 	p := writePeeringToBeDialed(t, store, 1, "my-peer")
@@ -1222,7 +1222,7 @@ func TestStreamResources_Server_AckNackNonce(t *testing.T) {
 	})
 	// Add in a sleep to prevent the test from flaking.
 	// The mock client expects certain calls to be made.
-	time.Sleep(50 * time.Millisecond)
+	time.Sleep(500 * time.Millisecond)
 }
 
 // Test that when the client doesn't send a heartbeat in time, the stream is disconnected.
