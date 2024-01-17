259ffd850530da4940163a478e9e13a8c3d56d60
Matt Keeler <mjkeeler7@gmail.com>
2024-01-17 14:37:56 -0500

    Migrate the Failover controller to use the controller cache
    Note that other controllers still rely on the failover mapper and so that bit of code cannot be deleted yet.

--- internal/catalog/catalogtest/run_test.go
+++ internal/catalog/catalogtest/run_test.go
@@ -7,8 +7,6 @@ import (
 	"testing"
 
 	"github.com/hashicorp/consul/internal/catalog"
-	"github.com/hashicorp/consul/internal/catalog/internal/controllers"
-	"github.com/hashicorp/consul/internal/controller"
 	"github.com/hashicorp/consul/internal/controller/controllertest"
 	"github.com/hashicorp/consul/internal/resource/reaper"
 	rtest "github.com/hashicorp/consul/internal/resource/resourcetest"
@@ -19,25 +17,23 @@ var (
 	clientOpts = rtest.ConfigureTestCLIFlags()
 )
 
-func runInMemResourceServiceAndControllers(t *testing.T, deps controllers.Dependencies) pbresource.ResourceServiceClient {
+func runInMemResourceServiceAndControllers(t *testing.T) pbresource.ResourceServiceClient {
 	t.Helper()
 
 	return controllertest.NewControllerTestBuilder().
 		WithResourceRegisterFns(catalog.RegisterTypes).
 		WithControllerRegisterFns(
 			reaper.RegisterControllers,
-			func(mgr *controller.Manager) {
-				catalog.RegisterControllers(mgr, deps)
-			},
+			catalog.RegisterControllers,
 		).Run(t)
 }
 
 func TestControllers_Integration(t *testing.T) {
-	client := runInMemResourceServiceAndControllers(t, catalog.DefaultControllerDependencies())
+	client := runInMemResourceServiceAndControllers(t)
 	RunCatalogV2Beta1IntegrationTest(t, client, clientOpts.ClientOptions(t)...)
 }
 
 func TestControllers_Lifecycle(t *testing.T) {
-	client := runInMemResourceServiceAndControllers(t, catalog.DefaultControllerDependencies())
+	client := runInMemResourceServiceAndControllers(t)
 	RunCatalogV2Beta1LifecycleIntegrationTest(t, client, clientOpts.ClientOptions(t)...)
 }
