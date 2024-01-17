784e6e2735015648b255b3999ccfb25a92f51f09
trujillo-adam <ajosetru@gmail.com>
2024-01-17 11:06:55 -0800

    fix redirect link errors

--- website/redirects.js
+++ website/redirects.js
@@ -211,5 +211,238 @@ module.exports = [
     source: '/consul/docs/v1.8.x/connect/config-entries/:slug',
     destination: '/consul/docs/v1.8.x/agent/config-entries/:slug',
     permanent: true,
+  }, // START redirects for API gateway and ingress gateway topics - Consul IA refresh 2024
+  {
+    source: '/consul/docs/connect/gateways',
+    destination: '/consul/docs/access',
+    permanent: true,
   },
+  {
+    source:
+      '/consul/docs/:version(v1.(?:8|9|10|11|12|13|14|15|16|17).x)/access/:slug*',
+    destination: '/consul/docs/:version/connect/gateways/:slug',
+    permanent: true,
+  },
+  {
+    source: '/consul/docs/connect/gateways/api-gateway',
+    destination: '/consul/docs/access/api-gateway',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/:version(v1.(?:8|9|10|11|12|13|14|15|16|17).x)/access/api-gateway//:slug*',
+    destination: '/consul/docs/:version/connect/gateways/api-gateway/:slug',
+    permanent: true,
+  },
+  {
+    source: '/consul/docs/connect/gateways/api-gateway/tech-specs',
+    destination: '/consul/docs/access/api-gateway/tech-specs',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/:version(v1.(?:8|9|10|11|12|13|14|15|16|17).x)/access/api-gateway/tech-specs/:slug*',
+    destination:
+      '/consul/docs/:version/connect/gateways/api-gateway/tech-specs/:slug',
+    permanent: true,
+  },
+  {
+    source: '/consul/docs/connect/gateways/api-gateway/install-k8s',
+    destination: '/consul/docs/access/api-gateway/install',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/:version(v1.(?:8|9|10|11|12|13|14|15|16|17).x)/access/api-gateway/install/:slug*',
+    destination:
+      '/consul/docs/:version/connect/gateways/api-gateway/install-k8s/:slug',
+    permanent: true,
+  },
+  {
+    source: '/consul/docs/connect/gateways/api-gateway/upgrades-k8s',
+    destination: '/consul/docs/upgrade/api-gateway',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/:version(v1.(?:8|9|10|11|12|13|14|15|16|17).x)/upgrade/api-gateway/:slug*',
+    destination:
+      '/consul/docs/:version/connect/gateways/api-gateway/upgrades-k8s/:slug',
+    permanent: true,
+  },
+  {
+    source: '/consul/docs/connect/gateways/api-gateway/deploy/listeners-vms',
+    destination: '/consul/docs/access/api-gateway/deploy-listener/vm',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/:version(v1.(?:8|9|10|11|12|13|14|15|16|17).x)/access/api-gateway/deploy-listener/vm/:slug*',
+    destination:
+      '/consul/docs/:version/connect/gateways/api-gateway/deploy/listeners-vms/:slug',
+    permanent: true,
+  },
+  {
+    source: '/consul/docs/connect/gateways/api-gateway/deploy/listeners-k8s',
+    destination: '/consul/docs/access/api-gateway/deploy-listener/k8s',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/:version(v1.(?:8|9|10|11|12|13|14|15|16|17).x)/access/api-gateway/deploy-listener/k8s/:slug*',
+    destination:
+      '/consul/docs/:version/connect/gateways/api-gateway/deploy/listeners-k8s/:slug',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/connect/gateways/api-gateway/define-routes/routes-vms',
+    destination: '/consul/docs/access/api-gateway/define-route/vm',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/:version(v1.(?:8|9|10|11|12|13|14|15|16|17).x)/access/api-gateway/define-route/vm/:slug*',
+    destination:
+      '/consul/docs/:version/connect/gateways/api-gateway/define-routes/routes-vms/:slug',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/connect/gateways/api-gateway/define-routes/routes-k8s',
+    destination: '/consul/docs/access/api-gateway/define-route/k8s',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/:version(v1.(?:8|9|10|11|12|13|14|15|16|17).x)/access/api-gateway/define-route/k8s/:slug*',
+    destination:
+      '/consul/docs/:version/connect/gateways/api-gateway/define-routes/routes-k8s/:slug',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/connect/gateways/api-gateway/define-routes/reroute-http-requests',
+    destination: '/consul/docs/access/api-gateway/define-route/reroute',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/:version(v1.(?:8|9|10|11|12|13|14|15|16|17).x)/access/api-gateway/define-route/reroute/:slug*',
+    destination:
+      '/consul/docs/:version/connect/gateways/api-gateway/define-routes/reroute-http-requests/:slug',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/connect/gateways/api-gateway/define-routes/route-to-peered-services',
+    destination: '/consul/docs/access/api-gateway/route/peer',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/:version(v1.(?:8|9|10|11|12|13|14|15|16|17).x)/access/api-gateway/route/peer/:slug*',
+    destination:
+      '/consul/docs/:version/connect/gateways/api-gateway/define-routes/route-to-peered-services/:slug',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/connect/gateways/api-gateway/secure-traffic/encrypt-vms',
+    destination: '/consul/docs/access/api-gateway/secure/encrypt',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/:version(v1.(?:8|9|10|11|12|13|14|15|16|17).x)/access/api-gateway/secure/encrypt/:slug*',
+    destination:
+      '/consul/docs/:version/connect/gateways/api-gateway/secure-traffic/encrypt-vms/:slug',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/connect/gateways/api-gateway/secure-traffic/verify-jwts-vms',
+    destination: '/consul/docs/access/api-gateway/secure/jwt-vm',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/:version(v1.(?:8|9|10|11|12|13|14|15|16|17).x)/access/api-gateway/secure/jwt-vm/:slug*',
+    destination:
+      '/consul/docs/:version/connect/gateways/api-gateway/secure-traffic/verify-jwts-vms/:slug',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/connect/gateways/api-gateway/secure-traffic/verify-jwts-k8s',
+    destination: '/consul/docs/access/api-gateway/secure/jwt-k8s',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/:version(v1.(?:8|9|10|11|12|13|14|15|16|17).x)/access/api-gateway/secure/jwt-k8s/:slug*',
+    destination:
+      '/consul/docs/:version/connect/gateways/api-gateway/secure-traffic/verify-jwts-k8s/:slug',
+    permanent: true,
+  },
+  {
+    source: '/consul/docs/connect/gateways/api-gateway/errors',
+    destination: '/consul/docs/common-error-messages/api-gateway',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/:version(v1.(?:8|9|10|11|12|13|14|15|16|17).x)/common-error-messages/api-gateway/:slug*',
+    destination:
+      '/consul/docs/:version/connect/gateways/api-gateway/errors/:slug',
+    permanent: true,
+  },
+  {
+    source: '/consul/docs/connect/gateways/ingress-gateway',
+    destination: '/consul/docs/access/ingress-gateway',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/:version(v1.(?:8|9|10|11|12|13|14|15|16|17).x)/access/ingress-gateway/:slug*',
+    destination: '/consul/docs/:version/connect/gateways/ingress-gateway/:slug',
+    permanent: true,
+  },
+  {
+    source: '/consul/docs/connect/gateways/ingress-gateway/usage',
+    destination: '/consul/docs/access/ingress-gateway/create/vms',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/:version(v1.(?:8|9|10|11|12|13|14|15|16|17).x)/access/ingress-gateway/create/vms/:slug*',
+    destination:
+      '/consul/docs/:version/connect/gateways/ingress-gateway/usage/:slug',
+    permanent: true,
+  },
+  {
+    source: '/consul/docs/connect/gateways/ingress-gateway/usage',
+    destination: '/consul/docs/access/ingress-gateway/create/vms',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/:version(v1.(?:8|9|10|11|12|13|14|15|16|17).x)/access/ingress-gateway/create/vms/:slug*',
+    destination:
+      '/consul/docs/:version/connect/gateways/ingress-gateway/usage/:slug',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/connect/gateways/ingress-gateway/tls-external-service',
+    destination: '/consul/docs/access/ingress-gateway/tls-external-service',
+    permanent: true,
+  },
+  {
+    source:
+      '/consul/docs/:version(v1.(?:8|9|10|11|12|13|14|15|16|17).x)/access/ingress-gateway/tls-external-service/:slug*',
+    destination:
+      '/consul/docs/:version/connect/gateways/ingress-gateway/tls-external-service/:slug',
+    permanent: true,
+  }, // END redirects for API gateway and ingress gateway topics - Consul IA refresh 2024
 ]
