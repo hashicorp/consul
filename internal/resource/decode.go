bd5abdb70e2c4007747134bc6a9d82932187d4ef
Matt Keeler <mjkeeler7@gmail.com>
2024-01-17 14:37:55 -0500

    Some some more decoding helpers

--- internal/resource/decode.go
+++ internal/resource/decode.go
@@ -58,6 +58,22 @@ func Decode[T proto.Message](res *pbresource.Resource) (*DecodedResource[T], err
 	}, nil
 }
 
+// DecodeList will generically decode the provided resource list into a list of 2-field
+// structures that holds onto the original Resource and the decoded contents.
+//
+// Returns an ErrDataParse on unmarshalling errors.
+func DecodeList[T proto.Message](resources []*pbresource.Resource) ([]*DecodedResource[T], error) {
+	var decoded []*DecodedResource[T]
+	for _, res := range resources {
+		d, err := Decode[T](res)
+		if err != nil {
+			return nil, err
+		}
+		decoded = append(decoded, d)
+	}
+	return decoded, nil
+}
+
 // GetDecodedResource will generically read the requested resource using the
 // client and either return nil on a NotFound or decode the response value.
 func GetDecodedResource[T proto.Message](ctx context.Context, client pbresource.ResourceServiceClient, id *pbresource.ID) (*DecodedResource[T], error) {
