ae71483a57f8562f59b43243aeb367c7ce639535
Xinyi Wang <xinyi.wang@hashicorp.com>
2024-01-19 16:59:26 -0800

    update the flag explanation

--- command/resource/client/grpc-resource-flags.go
+++ command/resource/client/grpc-resource-flags.go
@@ -15,7 +15,8 @@ type ResourceFlags struct {
 func (f *ResourceFlags) ResourceFlags() *flag.FlagSet {
 	fs := flag.NewFlagSet("", flag.ContinueOnError)
 	fs.Var(&f.namespace, "namespace",
-		"Specifies the namespace to query. It will default to the `default` namespace.`")
+		"Specifies the namespace to query. If not provided, the namespace will be inferred "+
+			"from the request's ACL token, or will default to the `default` namespace.")
 	fs.Var(&f.partition, "partition",
 		"Specifies the admin partition to query. If not provided, the admin partition will be inferred "+
 			"from the request's ACL token, or will default to the `default` admin partition. "+
