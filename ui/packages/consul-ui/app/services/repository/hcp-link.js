a0de3beabf7f5a9d910434ef4f6a0d448edf47ee
valeriia-ruban <valeriia.ruban@hashicorp.com>
2024-01-22 11:26:04 -0800

    feat: add api call to hcp/link endpoint

---
+++ ui/packages/consul-ui/app/services/repository/hcp-link.js
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+import RepositoryService from 'consul-ui/services/repository';
+import dataSource from 'consul-ui/decorators/data-source';
+
+const modelName = 'hcp-link';
+export default class HcpLinkService extends RepositoryService {
+  getModelName() {
+    return modelName;
+  }
+
+  @dataSource('/:partition/:ns/:dc/hcp-links')
+  async findAll() {
+    // TODO: check which method exactly is called here
+    return super.findAll(...arguments);
+  }
+}
