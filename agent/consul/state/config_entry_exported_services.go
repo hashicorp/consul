a7362db204462b783d34c91d1d97423e3b33779c
Tauhid Anjum <tauhidanjum@gmail.com>
2024-01-23 14:43:52 +0530

    Backport of NET-5824 Exported services api (#20015) (#20315)
    NET-5824 Exported services api (#20015)
    
    * Exported services api implemented
    
    * Tests added, refactored code
    
    * Adding server tests
    
    * changelog added
    
    * Proto gen added
    
    * Adding codegen changes
    
    * changing url, response object
    
    * Fixing lint error by having namespace and partition directly
    
    * Tests changes
    
    * refactoring tests
    
    * Simplified uniqueness logic for exported services, sorted the response in order of service name
    
    * Fix lint errors, refactored code

--- agent/consul/state/config_entry_exported_services.go
+++ agent/consul/state/config_entry_exported_services.go
@@ -5,10 +5,13 @@ package state
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/hashicorp/consul/acl"
 	"github.com/hashicorp/consul/agent/configentry"
 	"github.com/hashicorp/consul/agent/structs"
+	"github.com/hashicorp/consul/lib"
+	"github.com/hashicorp/consul/proto/private/pbconfigentry"
 	"github.com/hashicorp/go-memdb"
 )
 
@@ -63,4 +66,109 @@ func getExportedServicesConfigEntryTxn(
 		return 0, nil, fmt.Errorf("invalid service config type %T", entry)
 	}
 	return idx, export, nil
+}
+
+// ResolvedExportedServices returns the list of exported services along with consumers.
+// Sameness Groups and wild card entries are resolved.
+func (s *Store) ResolvedExportedServices(ws memdb.WatchSet, entMeta *acl.EnterpriseMeta) (uint64, []*pbconfigentry.ResolvedExportedService, error) {
+	tx := s.db.ReadTxn()
+	defer tx.Abort()
+
+	return resolvedExportedServicesTxn(tx, ws, entMeta)
+}
+
+func resolvedExportedServicesTxn(tx ReadTxn, ws memdb.WatchSet, entMeta *acl.EnterpriseMeta) (uint64, []*pbconfigentry.ResolvedExportedService, error) {
+	var resp []*pbconfigentry.ResolvedExportedService
+
+	// getSimplifiedExportedServices resolves the sameness group information to partitions and peers.
+	maxIdx, exports, err := getSimplifiedExportedServices(tx, ws, nil, *entMeta)
+	if err != nil {
+		return 0, nil, err
+	}
+	if exports == nil {
+		return maxIdx, nil, nil
+	}
+
+	var exportedServices []structs.ExportedService
+
+	for _, svc := range exports.Services {
+		// Prevent exporting the "consul" service.
+		if svc.Name == structs.ConsulServiceName {
+			continue
+		}
+
+		// If this isn't a wildcard, we can simply add it to the list of exportedServices and move to the next entry.
+		if svc.Name != structs.WildcardSpecifier {
+			exportedServices = append(exportedServices, svc)
+			continue
+		}
+
+		svcEntMeta := acl.NewEnterpriseMetaWithPartition(entMeta.PartitionOrDefault(), svc.Namespace)
+
+		// If all services in the namespace are exported by the wildcard, query those service names.
+		idx, typicalServices, err := serviceNamesOfKindTxn(tx, ws, structs.ServiceKindTypical, svcEntMeta)
+		if err != nil {
+			return 0, nil, fmt.Errorf("failed to get typical service names: %w", err)
+		}
+
+		maxIdx = lib.MaxUint64(maxIdx, idx)
+
+		for _, sn := range typicalServices {
+			// Prevent exporting the "consul" service.
+			if sn.Service.Name != structs.ConsulServiceName {
+				exportedServices = append(exportedServices, structs.ExportedService{
+					Name:      sn.Service.Name,
+					Namespace: sn.Service.NamespaceOrDefault(),
+					Consumers: svc.Consumers,
+				})
+			}
+		}
+	}
+
+	uniqueExportedServices := getUniqueExportedServices(exportedServices, entMeta)
+	resp = prepareExportedServicesResponse(uniqueExportedServices, entMeta)
+
+	return maxIdx, resp, nil
+}
+
+// getUniqueExportedServices removes duplicate services and consumers. Services are also sorted in ascending order
+func getUniqueExportedServices(exportedServices []structs.ExportedService, entMeta *acl.EnterpriseMeta) []structs.ExportedService {
+	// Services -> ServiceConsumers
+	var exportedServicesMapper = make(map[structs.ServiceName]map[structs.ServiceConsumer]struct{})
+	for _, svc := range exportedServices {
+		svcEntMeta := acl.NewEnterpriseMetaWithPartition(entMeta.PartitionOrDefault(), svc.Namespace)
+		svcName := structs.NewServiceName(svc.Name, &svcEntMeta)
+
+		for _, c := range svc.Consumers {
+			cons, ok := exportedServicesMapper[svcName]
+			if !ok {
+				cons = make(map[structs.ServiceConsumer]struct{})
+				exportedServicesMapper[svcName] = cons
+			}
+			cons[c] = struct{}{}
+		}
+	}
+
+	uniqueExportedServices := make([]structs.ExportedService, 0, len(exportedServicesMapper))
+
+	for svc, cons := range exportedServicesMapper {
+		consumers := make([]structs.ServiceConsumer, 0, len(cons))
+		for con := range cons {
+			consumers = append(consumers, con)
+		}
+
+		uniqueExportedServices = append(uniqueExportedServices, structs.ExportedService{
+			Name:      svc.Name,
+			Namespace: svc.NamespaceOrDefault(),
+			Consumers: consumers,
+		})
+
+	}
+
+	sort.Slice(uniqueExportedServices, func(i, j int) bool {
+		return (uniqueExportedServices[i].Name < uniqueExportedServices[j].Name) ||
+			(uniqueExportedServices[i].Name == uniqueExportedServices[j].Name && uniqueExportedServices[i].Namespace < uniqueExportedServices[j].Namespace)
+	})
+
+	return uniqueExportedServices
 }
