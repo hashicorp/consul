7042752cb39b6a3eec819f2c26d911768e2592e0
valeriia-ruban <valeriia.ruban@hashicorp.com>
2024-01-24 00:38:49 -0800

    update approach to get the linking status

--- ui/packages/consul-ui/app/services/repository/hcp-link.js
+++ ui/packages/consul-ui/app/services/repository/hcp-link.js
@@ -6,15 +6,37 @@
 import RepositoryService from 'consul-ui/services/repository';
 import dataSource from 'consul-ui/decorators/data-source';
 
-const modelName = 'hcp-link';
 export default class HcpLinkService extends RepositoryService {
-  getModelName() {
-    return modelName;
-  }
-
   @dataSource('/:partition/:ns/:dc/hcp-link')
-  async findAll() {
-    // TODO: check which method exactly is called here
-    return super.findAll(...arguments);
+  async fetch({ partition, ns, dc }, { uri }, request) {
+    let result;
+    try {
+      result = (
+        await request`
+      GET /api/hcp/v2/link/global
+    `
+      )((headers, body) => {
+        return {
+          meta: {
+            version: 2,
+            uri: uri,
+          },
+          body: {
+            isLinked: (body.status['consul.io/hcp/link']['conditions'] || []).some(
+              (condition) => condition.type === 'linked' && condition.state === 'STATE_TRUE'
+            ),
+          },
+          headers,
+        };
+      });
+    } catch (e) {
+      // set linked to false if the global link is not found
+      if (e.statusCode === 404) {
+        result = Promise.resolve({ isLinked: false });
+      } else {
+        result = Promise.resolve(null);
+      }
+    }
+    return result;
   }
 }
