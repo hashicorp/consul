c4a15ff818b6e2b2a8bcfd5f98739350245d851f
valeriia-ruban <valeriia.ruban@hashicorp.com>
2024-01-25 12:59:23 -0800

    update purple banner links

--- ui/packages/consul-ui/app/services/hcp-link-status.js
+++ ui/packages/consul-ui/app/services/hcp-link-status.js
@@ -5,22 +5,31 @@
 
 import Service from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 
 const LOCAL_STORAGE_KEY = 'consul:hideHcpLinkBanner';
 
 export default class HcpLinkStatus extends Service {
+  @service('repository/hcpLink') hcpLinkRepo;
   @tracked
   alreadyLinked = false;
   @tracked
   userDismissedBanner = false;
+  @tracked
+  isAlreadyFetchedLinkingStatus = false;
 
   get shouldDisplayBanner() {
-    return !this.alreadyLinked && !this.userDismissedBanner;
+    return this.isAlreadyFetchedLinkingStatus && !this.alreadyLinked && !this.userDismissedBanner;
   }
 
   constructor() {
     super(...arguments);
     this.userDismissedBanner = !!localStorage.getItem(LOCAL_STORAGE_KEY);
+    this.hcpLinkRepo.fetch().then((hcpLinkedData) => {
+      debugger;
+      this.isAlreadyFetchedLinkingStatus = true;
+      this.alreadyLinked = hcpLinkedData.data?.isLinked;
+    });
   }
 
   userHasLinked() {
