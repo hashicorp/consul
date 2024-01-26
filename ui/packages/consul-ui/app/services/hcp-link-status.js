590371525c6baf5843ed5de737d5e389efee59fd
valeriia-ruban <valeriia.ruban@hashicorp.com>
2024-01-25 21:17:04 -0800

    fixed lint issue

--- ui/packages/consul-ui/app/services/hcp-link-status.js
+++ ui/packages/consul-ui/app/services/hcp-link-status.js
@@ -5,7 +5,6 @@
 
 import Service from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-import { inject as service } from '@ember/service';
 
 const LOCAL_STORAGE_KEY = 'consul:hideHcpLinkBanner';
 
