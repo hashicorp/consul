e8479b8d2517909386afed3451783b1de0f9a92b
hc-github-team-consul-core <github-team-consul-core@hashicorp.com>
2024-01-26 14:57:16 -0600

    Backport of Fix CICD docker image rate limits. into release/1.15.x (#20363)
    Fix CICD docker image rate limits.
    
    The docker image used in CICD was referencing `registry.k8s.io/pause:3.3`,
    which appears to no longer function correctly. This commit swaps over to a
    Hashicorp mirrored image that shouldn't have rate limits or disappearing
    images.
    
    Co-authored-by: Derek Menteer <derek.menteer@hashicorp.com>

--- test/integration/consul-container/libs/cluster/container.go
+++ test/integration/consul-container/libs/cluster/container.go
@@ -525,7 +525,7 @@ func startContainer(ctx context.Context, req testcontainers.ContainerRequest) (t
 	})
 }
 
-const pauseImage = "registry.k8s.io/pause:3.3"
+const pauseImage = "hashiderek/pause"
 
 type containerOpts struct {
 	configFile        string
