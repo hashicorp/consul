d0243b618d4139162f308a0abe6a50cb44a73831
Eric Haberkorn <erichaberkorn@gmail.com>
2024-02-01 12:08:26 -0500

    Change the multicluster group to v2 (#20430)

--- internal/mesh/internal/controllers/gatewayproxy/fetcher/data_fetcher_test.go
+++ internal/mesh/internal/controllers/gatewayproxy/fetcher/data_fetcher_test.go
@@ -21,7 +21,7 @@ import (
 	pbauth "github.com/hashicorp/consul/proto-public/pbauth/v2beta1"
 	pbcatalog "github.com/hashicorp/consul/proto-public/pbcatalog/v2beta1"
 	pbmesh "github.com/hashicorp/consul/proto-public/pbmesh/v2beta1"
-	pbmulticluster "github.com/hashicorp/consul/proto-public/pbmulticluster/v2beta1"
+	pbmulticluster "github.com/hashicorp/consul/proto-public/pbmulticluster/v2"
 	"github.com/hashicorp/consul/proto-public/pbresource"
 	"github.com/hashicorp/consul/sdk/testutil"
 )
--- testing/deployer/util/v2.go
+++ testing/deployer/util/v2.go
@@ -6,7 +6,7 @@ package util
 import (
 	"fmt"
 
-	pbmulticluster "github.com/hashicorp/consul/proto-public/pbmulticluster/v2beta1"
+	pbmulticluster "github.com/hashicorp/consul/proto-public/pbmulticluster/v2"
 	"github.com/hashicorp/consul/proto-public/pbresource"
 )
 
