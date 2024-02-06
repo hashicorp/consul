8282444c4e08694784441d1c69bf5e0a7e3b8ea8
Nick Cellino <nick.cellino@hashicorp.com>
2024-02-06 02:29:50 +0000

    backport of commit 1d91dffbd128e3d1208875f163eac01b751652e6

--- agent/hcp/link_monitor_test.go
+++ agent/hcp/link_monitor_test.go
@@ -20,12 +20,104 @@ import (
 	"github.com/hashicorp/consul/agent/hcp/bootstrap/constants"
 	hcpclient "github.com/hashicorp/consul/agent/hcp/client"
 	"github.com/hashicorp/consul/agent/hcp/config"
+	hcpctl "github.com/hashicorp/consul/internal/hcp"
 	pbhcp "github.com/hashicorp/consul/proto-public/pbhcp/v2"
 	"github.com/hashicorp/consul/proto-public/pbresource"
 	"github.com/hashicorp/consul/sdk/testutil"
 )
 
 func TestMonitorHCPLink_Ok(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	t.Cleanup(cancel)
+
+	linkWatchCh := make(chan *pbresource.WatchEvent)
+	mgr := NewMockManager(t)
+
+	mockHCPClient := hcpclient.NewMockClient(t)
+	mockHcpClientFn := func(_ config.CloudConfig) (hcpclient.Client, error) {
+		return mockHCPClient, nil
+	}
+
+	loadMgmtTokenFn := func(ctx context.Context, logger hclog.Logger, hcpClient hcpclient.Client, dataDir string) (string, error) {
+		return "test-mgmt-token", nil
+	}
+
+	dataDir := testutil.TempDir(t, "test-link-controller")
+	os.Mkdir(filepath.Join(dataDir, constants.SubDir), os.ModeDir)
+	existingCfg := config.CloudConfig{
+		AuthURL: "test.com",
+	}
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		MonitorHCPLink(
+			ctx, hclog.New(&hclog.LoggerOptions{Output: io.Discard}), mgr, linkWatchCh, mockHcpClientFn,
+			loadMgmtTokenFn, existingCfg, dataDir,
+		)
+	}()
+
+	// Set up a link
+	link := pbhcp.Link{
+		ResourceId:   "abc",
+		ClientId:     "def",
+		ClientSecret: "ghi",
+		AccessLevel:  pbhcp.AccessLevel_ACCESS_LEVEL_GLOBAL_READ_WRITE,
+	}
+	expectedCfg := config.CloudConfig{
+		ResourceID:      link.ResourceId,
+		ClientID:        link.ClientId,
+		ClientSecret:    link.ClientSecret,
+		AuthURL:         "test.com",
+		ManagementToken: "test-mgmt-token",
+	}
+	linkResource, err := anypb.New(&link)
+	require.NoError(t, err)
+
+	// Create link, expect HCP manager to be updated and started
+	mgr.EXPECT().Start(mock.Anything).Return(nil).Once()
+	mgr.EXPECT().UpdateConfig(mockHCPClient, expectedCfg)
+	linkWatchCh <- &pbresource.WatchEvent{
+		Event: &pbresource.WatchEvent_Upsert_{
+			Upsert: &pbresource.WatchEvent_Upsert{
+				Resource: &pbresource.Resource{
+					Id: &pbresource.ID{
+						Name: "global",
+						Type: pbhcp.LinkType,
+					},
+					Status: map[string]*pbresource.Status{
+						hcpctl.StatusKey: {
+							Conditions: []*pbresource.Condition{hcpctl.ConditionValidatedSuccess},
+						},
+					},
+					Data: linkResource,
+				},
+			},
+		},
+	}
+
+	// Delete link, expect HCP manager to be stopped
+	mgr.EXPECT().Stop().Return(nil).Once()
+	linkWatchCh <- &pbresource.WatchEvent{
+		Event: &pbresource.WatchEvent_Delete_{
+			Delete: &pbresource.WatchEvent_Delete{},
+		},
+	}
+
+	// Wait for MonitorHCPLink to return before assertions run
+	close(linkWatchCh)
+	wg.Wait()
+
+	// Ensure hcp-config directory is removed
+	file := filepath.Join(dataDir, constants.SubDir)
+	if _, err := os.Stat(file); err == nil || !os.IsNotExist(err) {
+		require.Fail(t, "should have removed hcp-config directory")
+	}
+}
+
+func TestMonitorHCPLink_ValidationError(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	t.Cleanup(cancel)
 
@@ -62,23 +154,10 @@ func TestMonitorHCPLink_Ok(t *testing.T) {
 		ClientSecret: "ghi",
 		AccessLevel:  pbhcp.AccessLevel_ACCESS_LEVEL_GLOBAL_READ_WRITE,
 	}
-	existingManagerCfg := config.CloudConfig{
-		AuthURL: "test.com",
-	}
-	expectedCfg := config.CloudConfig{
-		ResourceID:      link.ResourceId,
-		ClientID:        link.ClientId,
-		ClientSecret:    link.ClientSecret,
-		AuthURL:         "test.com",
-		ManagementToken: "test-mgmt-token",
-	}
 	linkResource, err := anypb.New(&link)
 	require.NoError(t, err)
 
 	// Create link, expect HCP manager to be updated and started
-	mgr.EXPECT().Start(mock.Anything).Return(nil).Once()
-	mgr.EXPECT().GetCloudConfig().Return(existingManagerCfg)
-	mgr.EXPECT().UpdateConfig(mockHCPClient, expectedCfg)
 	linkWatchCh <- &pbresource.WatchEvent{
 		Event: &pbresource.WatchEvent_Upsert_{
 			Upsert: &pbresource.WatchEvent_Upsert{
@@ -86,6 +165,11 @@ func TestMonitorHCPLink_Ok(t *testing.T) {
 					Id: &pbresource.ID{
 						Name: "global",
 						Type: pbhcp.LinkType,
+					},
+					Status: map[string]*pbresource.Status{
+						hcpctl.StatusKey: {
+							Conditions: []*pbresource.Condition{hcpctl.ConditionValidatedFailed},
+						},
 					},
 					Data: linkResource,
 				},
@@ -127,6 +211,9 @@ func TestMonitorHCPLink_Ok_ReadOnly(t *testing.T) {
 	loadMgmtTokenFn := func(ctx context.Context, logger hclog.Logger, hcpClient hcpclient.Client, dataDir string) (string, error) {
 		return "test-mgmt-token", nil
 	}
+	existingManagerCfg := config.CloudConfig{
+		AuthURL: "test.com",
+	}
 
 	var wg sync.WaitGroup
 
@@ -135,7 +222,7 @@ func TestMonitorHCPLink_Ok_ReadOnly(t *testing.T) {
 		defer wg.Done()
 		MonitorHCPLink(
 			ctx, hclog.New(&hclog.LoggerOptions{Output: io.Discard}), mgr, linkWatchCh, mockHcpClientFn,
-			loadMgmtTokenFn, config.CloudConfig{}, "",
+			loadMgmtTokenFn, existingManagerCfg, "",
 		)
 	}()
 
@@ -146,9 +233,6 @@ func TestMonitorHCPLink_Ok_ReadOnly(t *testing.T) {
 		ClientId:     "def",
 		ClientSecret: "ghi",
 		AccessLevel:  pbhcp.AccessLevel_ACCESS_LEVEL_GLOBAL_READ_ONLY,
-	}
-	existingManagerCfg := config.CloudConfig{
-		AuthURL: "test.com",
 	}
 	expectedCfg := config.CloudConfig{
 		ResourceID:      link.ResourceId,
@@ -162,7 +246,6 @@ func TestMonitorHCPLink_Ok_ReadOnly(t *testing.T) {
 
 	// Create link, expect HCP manager to be updated and started
 	mgr.EXPECT().Start(mock.Anything).Return(nil).Once()
-	mgr.EXPECT().GetCloudConfig().Return(existingManagerCfg)
 	mgr.EXPECT().UpdateConfig(mockHCPClient, expectedCfg)
 	linkWatchCh <- &pbresource.WatchEvent{
 		Event: &pbresource.WatchEvent_Upsert_{
@@ -171,6 +254,11 @@ func TestMonitorHCPLink_Ok_ReadOnly(t *testing.T) {
 					Id: &pbresource.ID{
 						Name: "global",
 						Type: pbhcp.LinkType,
+					},
+					Status: map[string]*pbresource.Status{
+						hcpctl.StatusKey: {
+							Conditions: []*pbresource.Condition{hcpctl.ConditionValidatedSuccess},
+						},
 					},
 					Data: linkResource,
 				},
