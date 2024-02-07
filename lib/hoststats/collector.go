c067761bc03c3a6a7f161f528eed414cb37f0e6b
Melissa Kam <mkam@hashicorp.com>
2024-02-07 21:45:52 +0000

    backport of commit cdfb10b454067f6fe6d7f09f465554d5ac066576

--- lib/hoststats/collector.go
+++ lib/hoststats/collector.go
@@ -87,7 +87,7 @@ func (c *Collector) collect() {
 	// Determine up-time
 	uptime, err := host.Uptime()
 	if err != nil {
-		c.logger.Error("failed to collect uptime stats", "error", err)
+		c.logger.Debug("failed to collect uptime stats", "error", err)
 		uptime = 0
 	}
 	hs.Uptime = uptime
@@ -95,7 +95,7 @@ func (c *Collector) collect() {
 	// Collect memory stats
 	mstats, err := c.collectMemoryStats()
 	if err != nil {
-		c.logger.Error("failed to collect memory stats", "error", err)
+		c.logger.Debug("failed to collect memory stats", "error", err)
 		mstats = &MemoryStats{}
 	}
 	hs.Memory = mstats
@@ -103,7 +103,7 @@ func (c *Collector) collect() {
 	// Collect cpu stats
 	cpus, err := c.collectCPUStats()
 	if err != nil {
-		c.logger.Error("failed to collect cpu stats", "error", err)
+		c.logger.Debug("failed to collect cpu stats", "error", err)
 		cpus = []*CPUStats{}
 	}
 	hs.CPU = cpus
@@ -112,7 +112,7 @@ func (c *Collector) collect() {
 	if c.dataDir != "" {
 		diskStats, err := c.collectDiskStats(c.dataDir)
 		if err != nil {
-			c.logger.Error("failed to collect dataDir disk stats", "error", err)
+			c.logger.Debug("failed to collect dataDir disk stats", "error", err)
 		}
 		hs.DataDirStats = diskStats
 	}
