50679d48fde8d6affc838f86003b07f06b24c536
Michael Zalimeni <michael.zalimeni@hashicorp.com>
2024-02-12 17:09:32 -0500

    [NET-7948] Bump Envoy version on 1.15.x (LTS) to address multiple CVEs (#20590)
    security: Bump Envoy versions to address CVEs

--- envoyextensions/xdscommon/proxysupport.go
+++ envoyextensions/xdscommon/proxysupport.go
@@ -12,9 +12,9 @@ import "strings"
 //
 // see: https://www.consul.io/docs/connect/proxies/envoy#supported-versions
 var EnvoyVersions = []string{
-	"1.28.0",
-	"1.27.2",
-	"1.26.6",
+	"1.28.1",
+	"1.27.3",
+	"1.26.7",
 	"1.25.11",
 	"1.24.12",
 	"1.23.12",
