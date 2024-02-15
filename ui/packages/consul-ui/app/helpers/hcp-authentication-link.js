6bbcc993a42f94a2184b3b3a427a650e4cc1e981
valeriia-ruban <valeriia.ruban@hashicorp.com>
2024-02-14 20:45:51 -0800

    fixed tests

--- ui/packages/consul-ui/app/helpers/hcp-authentication-link.js
+++ ui/packages/consul-ui/app/helpers/hcp-authentication-link.js
@@ -7,10 +7,6 @@ import Helper from '@ember/component/helper';
 import { inject as service } from '@ember/service';
 
 /**
- * A resourceId Looks like:
- * organization/b4432207-bb9c-438e-a160-b98923efa979/project/4b09958c-fa91-43ab-8029-eb28d8cee9d4/hashicorp.consul.global-network-manager.cluster/test-from-api
- * organization/${organizationId}/project/${projectId}/hashicorp.consul.global-network-manager.cluster/${clusterName}
- *
  * A HCP URL looks like:
  * https://portal.cloud.hashicorp.com/services/consul/clusters/self-managed/link-existing?cluster_name=test-from-api&cluster_version=1.18.0&cluster_access_mode=CONSUL_ACCESS_LEVEL_GLOBAL_READ_WRITE&redirect_url=localhost:8500/services
  */
