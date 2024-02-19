65dc6f261ff0ace70bfda4052e630de517cc5fab
Ashesh Vidyut <ashesh.vidyut@hashicorp.com>
2024-02-19 14:45:49 +0530

    root not pointer

--- internal/radix/adaptive/tree.go
+++ internal/radix/adaptive/tree.go
@@ -131,8 +131,8 @@ func (t *Txn[T]) Delete(key []byte) T {
 	return oldVal
 }
 
-func (t *Txn[T]) Root() *Node[T] {
-	return t.root
+func (t *Txn[T]) Root() Node[T] {
+	return *t.root
 }
 
 func (t *Txn[T]) Commit() *RadixTree[T] {
