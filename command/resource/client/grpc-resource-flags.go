d05df36d777b754a20951977212c579cb2733d87
Xinyi Wang <xinyi.wang@hashicorp.com>
2024-02-23 08:36:04 -0800

    Revert "clean up http client (#20342)"
    This reverts commit 2b89025eabd6240b0cd4f3e6f0f327531567e339.

--- command/resource/client/grpc-resource-flags.go
+++ command/resource/client/grpc-resource-flags.go
@@ -8,6 +8,7 @@ import "flag"
 type ResourceFlags struct {
 	partition TValue[string]
 	namespace TValue[string]
+	peername  TValue[string]
 	stale     TValue[bool]
 }
 
@@ -20,6 +21,7 @@ func (f *ResourceFlags) ResourceFlags() *flag.FlagSet {
 	fs.Var(&f.namespace, "namespace",
 		"Specifies the namespace to query. If not provided, the namespace will be inferred "+
 			"from the request's ACL token, or will default to the `default` namespace.")
+	fs.Var(&f.peername, "peer", "Specifies the name of peer to query. By default, it is `local`.")
 	fs.Var(&f.stale, "stale",
 		"Permit any Consul server (non-leader) to respond to this request. This "+
 			"allows for lower latency and higher throughput, but can result in "+
@@ -34,6 +36,10 @@ func (f *ResourceFlags) Partition() string {
 
 func (f *ResourceFlags) Namespace() string {
 	return f.namespace.String()
+}
+
+func (f *ResourceFlags) Peername() string {
+	return f.peername.String()
 }
 
 func (f *ResourceFlags) Stale() bool {
