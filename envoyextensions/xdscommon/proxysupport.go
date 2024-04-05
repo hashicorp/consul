14187a5a5ed631c130bc094708705c7ece67fa29
dduzgun-security <deniz.duzgun@hashicorp.com>
2024-04-05 11:41:15 -0400

    Backport security fixes

--- envoyextensions/xdscommon/proxysupport.go
+++ envoyextensions/xdscommon/proxysupport.go
@@ -12,8 +12,8 @@ import "strings"
 //
 // see: https://www.consul.io/docs/connect/proxies/envoy#supported-versions
 var EnvoyVersions = []string{
-	"1.27.3",
-	"1.26.7",
+	"1.27.4",
+	"1.26.8",
 	"1.25.11",
 	"1.24.12",
 }
