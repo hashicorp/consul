19b8f9196f8694697ac1fd76b1c17c215cb7eb68
socialsister <seekseat@qq.com>
2024-04-26 14:29:55 +0800

    chore: fix some comment

--- agent/agent_endpoint_test.go
+++ agent/agent_endpoint_test.go
@@ -8214,7 +8214,7 @@ func TestAgent_Version(t *testing.T) {
 	assert.NotNil(t, obj.HumanVersion)
 }
 
-// Thie tests that a proxy with an ExposeConfig is returned as expected.
+// This tests that a proxy with an ExposeConfig is returned as expected.
 func TestAgent_Services_ExposeConfig(t *testing.T) {
 	if testing.Short() {
 		t.Skip("too slow for testing.Short")
