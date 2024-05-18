5db0a383bb961a9edf7331889ec512e9c225a5fb
Vijay Srinivas <vijayraghav22@gmail.com>
2024-05-18 22:11:07 +0530

    update for https path expose

--- agent/structs/connect_proxy_config.go
+++ agent/structs/connect_proxy_config.go
@@ -775,6 +775,15 @@ type ExposePath struct {
 
 	// ParsedFromCheck is set if this path was parsed from a registered check
 	ParsedFromCheck bool `json:",omitempty" alias:"parsed_from_check"`
+
+	// CAFile is the path to the CA file for the exposed external HTTPS path
+	CAFile string `json:",omitempty" alias:"cafile"`
+
+	// CertFile is the path to the Certificate file for the exposed external HTTPS path
+	CertFile string `json:",omitempty" alias:"certfile"`
+
+	// KeyFile is the path to the Private key file for the exposed external HTTPS path
+	KeyFile string `json:",omitempty" alias:"keyfile"`
 }
 
 func (t *ExposePath) UnmarshalJSON(data []byte) (err error) {
