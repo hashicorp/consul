6ac6d1b6d4d872d8abb7977c88e9a0e7ad6e07ac
Ashesh Vidyut <ashesh.vidyut@hashicorp.com>
2024-05-23 15:45:20 +0530

    fix imports

--- agent/configentry/merge_service_config.go
+++ agent/configentry/merge_service_config.go
@@ -6,8 +6,8 @@ package configentry
 import (
 	"fmt"
 
-	"github.com/hashicorp/go-hclog"
 	"github.com/absolutelightning/go-memdb"
+	"github.com/hashicorp/go-hclog"
 	"github.com/imdario/mergo"
 	"github.com/mitchellh/copystructure"
 
