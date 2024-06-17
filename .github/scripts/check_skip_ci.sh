c56e2161ee5625e8caf2760f0896503893277995
Michael Zalimeni <michael.zalimeni@hashicorp.com>
2024-06-17 15:59:28 +0000

    backport of commit a9ce440dfa01ec2b0e990f54bfce54d7d1deb372

--- .github/scripts/check_skip_ci.sh
+++ .github/scripts/check_skip_ci.sh
@@ -5,7 +5,7 @@
 set -euo pipefail
 
 # Get the list of changed files
-# Using `git merge-base` ensures that we're always comparing against the correct branch point. 
+# Using `git merge-base` ensures that we're always comparing against the correct branch point.
 #For example, given the commits:
 #
 # A---B---C---D---W---X---Y---Z # origin/main
@@ -16,27 +16,34 @@ set -euo pipefail
 files_to_check=$(git diff --name-only "$(git merge-base origin/$SKIP_CHECK_BRANCH HEAD~)"...HEAD)
 
 # Define the directories to check
-skipped_directories=("docs/" "ui/" "website/" "grafana/")
+skipped_directories=("docs/" "ui/" "website/" "grafana/" ".changelog/")
 
 # Loop through the changed files and find directories/files outside the skipped ones
-for file_to_check in "${files_to_check[@]}"; do
+files_to_check_array=($files_to_check)
+for file_to_check in "${files_to_check_array[@]}"; do
 	file_is_skipped=false
+	echo "checking file: $file_to_check"
+
+	# Allow changes to:
+	# - This script
+	# - Files in the skipped directories
+	# - Markdown files
 	for dir in "${skipped_directories[@]}"; do
-		if [[ "$file_to_check" == "$dir"* ]] || [[ "$file_to_check" == *.md && "$dir" == *"/" ]]; then
+		if [[ "$file_to_check" == */check_skip_ci.sh ]] ||
+		   [[ "$file_to_check" == "$dir"* ]] ||
+		   [[ "$file_to_check" == *.md ]]; then
 			file_is_skipped=true
 			break
 		fi
 	done
+
 	if [ "$file_is_skipped" != "true" ]; then
-		echo -e $file_to_check
-        SKIP_CI=false
-		echo "Changes detected in non-documentation files - skip-ci: $SKIP_CI"
-        echo "skip-ci=$SKIP_CI" >> "$GITHUB_OUTPUT"
+		echo -e "non-skippable file changed: $file_to_check"
+		echo "Changes detected in non-documentation files - will not skip tests and build"
+        echo "skip-ci=false" >> "$GITHUB_OUTPUT"
 		exit 0 ## if file is outside of the skipped_directory exit script
 	fi
 done
 
-echo -e "$files_to_check"
-SKIP_CI=true
-echo "Changes detected in only documentation files - skip-ci: $SKIP_CI"
-echo "skip-ci=$SKIP_CI" >> "$GITHUB_OUTPUT"
+echo "Changes detected in only documentation files - skipping tests and build"
+echo "skip-ci=true" >> "$GITHUB_OUTPUT"
