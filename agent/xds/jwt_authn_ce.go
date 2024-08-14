1fa428552b30f7da868315d5b1540af49acadb9d
John Maguire <john.maguire@hashicorp.com>
2024-08-14 15:41:02 -0400

    [NET-10719] Fix cluster generation for jwt clusters for external jwt providers (#21604)
    * Fix cluster generation for jwt clusters for external jwt providers
    
    * add changelog

--- agent/xds/jwt_authn_ce.go
+++ agent/xds/jwt_authn_ce.go
@@ -8,8 +8,11 @@ package xds
 import (
 	envoy_http_jwt_authn_v3 "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/http/jwt_authn/v3"
 	envoy_http_v3 "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
+	"google.golang.org/protobuf/proto"
 
+	"github.com/hashicorp/consul/agent/proxycfg"
 	"github.com/hashicorp/consul/agent/structs"
+	"github.com/hashicorp/go-hclog"
 )
 
 type GatewayAuthFilterBuilder struct {
@@ -21,4 +24,8 @@ type GatewayAuthFilterBuilder struct {
 
 func (g *GatewayAuthFilterBuilder) makeGatewayAuthFilters() ([]*envoy_http_v3.HttpFilter, error) {
 	return nil, nil
+}
+
+func makeAPIGatewayJWKClusters(_ hclog.Logger, _ *proxycfg.ConfigSnapshot) []proto.Message {
+	return nil
 }
