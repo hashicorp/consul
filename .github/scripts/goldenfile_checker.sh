61deb4a20e6069171cdd6e8e4103484c3b09b585
jm96441n <john.maguire@hashicorp.com>
2024-08-16 18:14:57 +0000

    backport of commit 31539a28be069f646a982642ca9a87c63115443a

--- .github/scripts/goldenfile_checker.sh
+++ .github/scripts/goldenfile_checker.sh
@@ -7,10 +7,10 @@ set -euo pipefail
 # check if there is a diff in the xds testdata directory after running `make envoy-regen`
 make envoy-regen
 
-echo "$GITHUB_BASE_REF"
-echo $(git merge-base HEAD "origin/$GITHUB_BASE_REF")
-changed_xds_files=$(git --no-pager diff --name-only HEAD "$(git merge-base HEAD "origin/$GITHUB_BASE_REF")" | egrep "agent/xds/testdata/.*")
-
+echo "origin/$GIT_BRANCH_REF"
+echo "getting changed files"
+changed_xds_files=$(git --no-pager diff --name-only HEAD "$(git merge-base HEAD "origin/$GITHUB_BRANCH_REF")" | egrep "agent/xds/testdata/.*" || true)
+echo "got changed files"
 # If we do not find a file in .changelog/, we fail the check
 if [ -z "$changed_xds_files" ]; then
   # pass status check if no changes were found for xds files
