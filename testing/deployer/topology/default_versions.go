dfc15ec33eefa2b921e0249f0b1d301f13aece52
Sarah Alsmiller <sarah.alsmiller@hashicorp.com>
2024-09-13 10:52:45 -0500

    hard update all 1.3 dataplane to 1.6

--- testing/deployer/topology/default_versions.go
+++ testing/deployer/topology/default_versions.go
@@ -5,9 +5,10 @@
 
 package topology
 
+//TODO @sarah.alsmiller figure out if we can delete this
 const (
-	DefaultConsulCEImage         = "hashicorp/consul:1.17.0"
-	DefaultConsulEnterpriseImage = "hashicorp/consul-enterprise:1.17.0-ent"
-	DefaultEnvoyImage            = "envoyproxy/envoy:v1.27.2"
-	DefaultDataplaneImage        = "hashicorp/consul-dataplane:1.3.0"
+	DefaultConsulCEImage         = "hashicorp/consul:1.19.0"
+	DefaultConsulEnterpriseImage = "hashicorp/consul-enterprise:1.19.0-ent"
+	DefaultEnvoyImage            = "envoyproxy/envoy:v1.28.5"
+	DefaultDataplaneImage        = "hashicorp/consul-dataplane:1.5.0"
 )
