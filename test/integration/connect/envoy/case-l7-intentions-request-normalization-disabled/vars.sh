54bc8bb313f2e024965ee75af0237e6754ae2079
Michael Zalimeni <michael.zalimeni@hashicorp.com>
2024-10-10 16:01:02 -0400

    test: add request normalization integration bats tests
    Add both "positive" and "negative" test suites, showing normalization in
    action as well as expected results when it is not enabled, for the same
    set of test cases.
    
    Also add some alternative service container test helpers for verifying
    raw HTTP request paths, which is difficult to do with Fortio.

---
+++ test/integration/connect/envoy/case-l7-intentions-request-normalization-disabled/vars.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: BUSL-1.1
+
+# Allow for non-normalized path testing by using alternative container.
+export SERVICE_CONTAINER="echo"
