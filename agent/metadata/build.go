fcf694d32e63e2478842f70e84fceec671220e84
R.B. Boyer <rb@hashicorp.com>
2025-01-31 16:12:30 -0600

    metadata: memoize the parsed build versions

--- agent/metadata/build.go
+++ agent/metadata/build.go
@@ -4,12 +4,51 @@
 package metadata
 
 import (
+	"sync"
+
 	"github.com/hashicorp/go-version"
 	"github.com/hashicorp/serf/serf"
 )
 
+type versionTuple struct {
+	Value *version.Version
+	Err   error
+}
+
+var versionCache sync.Map // string->versionTuple
+
 // Build extracts the Consul version info for a member.
 func Build(m *serf.Member) (*version.Version, error) {
-	str := versionFormat.FindString(m.Tags["build"])
+	build := m.Tags["build"]
+
+	ok, v, err := getMemoizedBuildVersion(build)
+	if ok {
+		return v, err
+	}
+
+	v, err = parseBuildAsVersion(build)
+
+	versionCache.Store(build, versionTuple{Value: v, Err: err})
+
+	return v, err
+}
+
+func getMemoizedBuildVersion(build string) (bool, *version.Version, error) {
+	rawTuple, ok := versionCache.Load(build)
+	if !ok {
+		return false, nil, nil
+	}
+	tuple, ok := rawTuple.(versionTuple)
+	if !ok {
+		return false, nil, nil
+	}
+	if tuple.Err != nil {
+		return true, nil, tuple.Err
+	}
+	return true, tuple.Value, nil
+}
+
+func parseBuildAsVersion(build string) (*version.Version, error) {
+	str := versionFormat.FindString(build)
 	return version.NewVersion(str)
 }
