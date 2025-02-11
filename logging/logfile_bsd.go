5484f3815d018a77d3e36ed19fd6971c7c4e03c5
Dhia Ayachi <dhia.ayachi@gmail.com>
2025-02-11 09:49:15 -0500

    fix logging time for bsd

--- logging/logfile_bsd.go
+++ logging/logfile_bsd.go
@@ -13,6 +13,6 @@ import (
 
 func (l *LogFile) createTime(stat os.FileInfo) time.Time {
 	stat_t := stat.Sys().(*syscall.Stat_t)
-	createTime := stat_t.Ctim
+	createTime := stat_t.Ctimespec
 	return time.Unix(int64(createTime.Sec), int64(createTime.Nsec)) //nolint:unconvert
 }
