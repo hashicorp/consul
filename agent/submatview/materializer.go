61e9c3f6388dda4e442acab32c7b1cd35cbd3321
Mukul Anand <anandmukul93@gmail.com>
2025-02-12 18:31:14 +0530

    fixing error logging with key, value, updating changelog

--- agent/submatview/materializer.go
+++ agent/submatview/materializer.go
@@ -206,9 +206,9 @@ func (m *materializer) handleError(req *pbsubscribe.SubscribeRequest, err error)
 
 	switch {
 	case errors.Is(err, stream.ErrACLChanged):
-		logger.Info("subscribe call failed due to ACL change", err)
+		logger.Info("subscribe call failed due to ACL change", "error", err)
 	default:
-		logger.Error("subscribe call failed", err)
+		logger.Error("subscribe call failed", "error", err)
 	}
 }
 
