d60860a9b19807a9c9579f4fef455ca129d58671
hc-github-team-consul-core <github-team-consul-core@hashicorp.com>
2025-05-12 22:58:29 +0530

    Backport of feature: add LUA script Envoy extension support for API gateway into release/1.20.x (#22333)
    backport of commit c130367cb08bcb36283682fb54057892ce8b2b22
    
    Co-authored-by: Vikramarjuna <vkrmrjun@gmail.com>

--- agent/envoyextensions/builtin/lua/lua_test.go
+++ agent/envoyextensions/builtin/lua/lua_test.go
@@ -6,7 +6,13 @@ package lua
 import (
 	"testing"
 
+	envoy_core_v3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	envoy_listener_v3 "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
+	envoy_lua_v3 "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/http/lua/v3"
+	envoy_http_v3 "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/anypb"
 
 	"github.com/hashicorp/consul/api"
 	"github.com/hashicorp/consul/envoyextensions/extensioncommon"
@@ -17,17 +23,15 @@ func TestConstructor(t *testing.T) {
 		m := map[string]interface{}{
 			"ProxyType": "connect-proxy",
 			"Listener":  "inbound",
-			"Script":    "lua-script",
+			"Script":    "function envoy_on_request(request_handle) request_handle:headers():add('test', 'test') end",
 		}
-
 		for k, v := range overrides {
 			m[k] = v
 		}
-
 		return m
 	}
 
-	cases := map[string]struct {
+	tests := map[string]struct {
 		extensionName string
 		arguments     map[string]interface{}
 		expected      lua
@@ -59,7 +63,16 @@ func TestConstructor(t *testing.T) {
 			expected: lua{
 				ProxyType: "connect-proxy",
 				Listener:  "inbound",
-				Script:    "lua-script",
+				Script:    "function envoy_on_request(request_handle) request_handle:headers():add('test', 'test') end",
+			},
+			ok: true,
+		},
+		"api gateway proxy type": {
+			arguments: makeArguments(map[string]interface{}{"ProxyType": "api-gateway"}),
+			expected: lua{
+				ProxyType: "api-gateway",
+				Listener:  "inbound",
+				Script:    "function envoy_on_request(request_handle) request_handle:headers():add('test', 'test') end",
 			},
 			ok: true,
 		},
@@ -68,23 +81,21 @@ func TestConstructor(t *testing.T) {
 			expected: lua{
 				ProxyType: "connect-proxy",
 				Listener:  "inbound",
-				Script:    "lua-script",
+				Script:    "function envoy_on_request(request_handle) request_handle:headers():add('test', 'test') end",
 			},
 			ok: true,
 		},
 	}
 
-	for n, tc := range cases {
-		t.Run(n, func(t *testing.T) {
-
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
 			extensionName := api.BuiltinLuaExtension
 			if tc.extensionName != "" {
 				extensionName = tc.extensionName
 			}
 
-			svc := api.CompoundServiceName{Name: "svc"}
 			ext := extensioncommon.RuntimeConfig{
-				ServiceName: svc,
+				ServiceName: api.CompoundServiceName{Name: "svc"},
 				EnvoyExtension: api.EnvoyExtension{
 					Name:      extensionName,
 					Arguments: tc.arguments,
@@ -101,4 +112,162 @@ func TestConstructor(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestLuaExtension_PatchFilter(t *testing.T) {
+	makeFilter := func(filters []*envoy_http_v3.HttpFilter) *envoy_listener_v3.Filter {
+		hcm := &envoy_http_v3.HttpConnectionManager{
+			HttpFilters: filters,
+		}
+		any, err := anypb.New(hcm)
+		require.NoError(t, err)
+		return &envoy_listener_v3.Filter{
+			Name: "envoy.filters.network.http_connection_manager",
+			ConfigType: &envoy_listener_v3.Filter_TypedConfig{
+				TypedConfig: any,
+			},
+		}
+	}
+
+	makeLuaFilter := func(script string) *envoy_http_v3.HttpFilter {
+		luaConfig := &envoy_lua_v3.Lua{
+			DefaultSourceCode: &envoy_core_v3.DataSource{
+				Specifier: &envoy_core_v3.DataSource_InlineString{
+					InlineString: script,
+				},
+			},
+		}
+		return &envoy_http_v3.HttpFilter{
+			Name: "envoy.filters.http.lua",
+			ConfigType: &envoy_http_v3.HttpFilter_TypedConfig{
+				TypedConfig: mustMarshalAny(luaConfig),
+			},
+		}
+	}
+
+	tests := map[string]struct {
+		extension      *lua
+		filter         *envoy_listener_v3.Filter
+		isInbound      bool
+		expectedFilter *envoy_listener_v3.Filter
+		expectPatched  bool
+		expectError    string
+	}{
+		"non-http filter is ignored": {
+			extension: &lua{
+				ProxyType: "connect-proxy",
+				Listener:  "inbound",
+				Script:    "function envoy_on_request(request_handle) end",
+			},
+			filter: &envoy_listener_v3.Filter{
+				Name: "envoy.filters.network.tcp_proxy",
+			},
+			expectedFilter: &envoy_listener_v3.Filter{
+				Name: "envoy.filters.network.tcp_proxy",
+			},
+			expectPatched: false,
+		},
+		"listener direction mismatch": {
+			extension: &lua{
+				ProxyType: "connect-proxy",
+				Listener:  "inbound",
+				Script:    "function envoy_on_request(request_handle) end",
+			},
+			filter: makeFilter([]*envoy_http_v3.HttpFilter{
+				{Name: "envoy.filters.http.router"},
+			}),
+			isInbound:      false,
+			expectedFilter: makeFilter([]*envoy_http_v3.HttpFilter{{Name: "envoy.filters.http.router"}}),
+			expectPatched:  false,
+		},
+		"successful patch with router filter": {
+			extension: &lua{
+				ProxyType: "connect-proxy",
+				Listener:  "inbound",
+				Script:    "function envoy_on_request(request_handle) end",
+			},
+			filter: makeFilter([]*envoy_http_v3.HttpFilter{
+				{Name: "envoy.filters.http.router"},
+			}),
+			isInbound: true,
+			expectedFilter: makeFilter([]*envoy_http_v3.HttpFilter{
+				makeLuaFilter("function envoy_on_request(request_handle) end"),
+				{Name: "envoy.filters.http.router"},
+			}),
+			expectPatched: true,
+		},
+		"successful patch with multiple filters": {
+			extension: &lua{
+				ProxyType: "connect-proxy",
+				Listener:  "inbound",
+				Script:    "function envoy_on_request(request_handle) end",
+			},
+			filter: makeFilter([]*envoy_http_v3.HttpFilter{
+				{Name: "envoy.filters.http.other1"},
+				{Name: "envoy.filters.http.router"},
+				{Name: "envoy.filters.http.other2"},
+			}),
+			isInbound: true,
+			expectedFilter: makeFilter([]*envoy_http_v3.HttpFilter{
+				{Name: "envoy.filters.http.other1"},
+				makeLuaFilter("function envoy_on_request(request_handle) end"),
+				{Name: "envoy.filters.http.router"},
+				{Name: "envoy.filters.http.other2"},
+			}),
+			expectPatched: true,
+		},
+		"invalid filter config": {
+			extension: &lua{
+				ProxyType: "connect-proxy",
+				Listener:  "inbound",
+				Script:    "function envoy_on_request(request_handle) end",
+			},
+			filter: &envoy_listener_v3.Filter{
+				Name: "envoy.filters.network.http_connection_manager",
+				ConfigType: &envoy_listener_v3.Filter_TypedConfig{
+					TypedConfig: &anypb.Any{},
+				},
+			},
+			isInbound:      true,
+			expectedFilter: nil,
+			expectPatched:  false,
+			expectError:    "error unmarshalling filter",
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			direction := extensioncommon.TrafficDirectionOutbound
+			if tc.isInbound {
+				direction = extensioncommon.TrafficDirectionInbound
+			}
+
+			payload := extensioncommon.FilterPayload{
+				Message:          tc.filter,
+				TrafficDirection: direction,
+			}
+
+			filter, patched, err := tc.extension.PatchFilter(payload)
+
+			if tc.expectError != "" {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), tc.expectError)
+				return
+			}
+
+			require.NoError(t, err)
+			require.Equal(t, tc.expectPatched, patched)
+			if tc.expectedFilter != nil {
+				require.Equal(t, tc.expectedFilter, filter)
+			}
+		})
+	}
+}
+
+func mustMarshalAny(m proto.Message) *anypb.Any {
+	a, err := anypb.New(m)
+	if err != nil {
+		panic(err)
+	}
+	return a
 }
