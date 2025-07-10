63d4cfe43c934cfb479c23812bd1ec00e3df0dcc
suresh-hashicorp <suresh.kumardunga@hashicorp.com>
2025-07-10 10:47:12 +0530

    [UI] format ipv6 address to collapsed form (#22468)
    * format ipv6 address to collapsed form
    
    * add changelog
    
    * fix format-ipaddr helper tests

---
+++ ui/packages/consul-ui/app/helpers/format-ipaddr.js
@@ -0,0 +1,7 @@
+import { helper } from '@ember/component/helper';
+import { processIpAddress } from 'consul-ui/utils/process-ip-address';
+
+export default helper(function formatIpaddr([ipaddress]) {
+  const value = processIpAddress(ipaddress);
+  return value ? value : '';
+});
---
+++ ui/packages/consul-ui/tests/integration/helpers/format-ipaddr-test.js
@@ -0,0 +1,35 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Helper | format-ipaddr', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders the given value', async function (assert) {
+    this.set('inputValue', '192.168.1.1');
+
+    await render(hbs`<div>{{format-ipaddr this.inputValue}}</div>`);
+
+    assert.dom(this.element).hasText('192.168.1.1');
+
+    // await render(hbs`<div>{{format-ipaddr '2001::85a3::8a2e:370:7334'}}</div>`);
+    // assert.dom(this.element).doesNotHaveText('2001::85a3::8a2e:370:7334');
+  });
+
+  test('it should return an empty string for invalid IP addresses', async function (assert) {
+    this.set('inputValue', '2001::85a3::8a2e:370:7334');
+
+    await render(hbs`<div>{{format-ipaddr this.inputValue}}</div>`);
+
+    assert.dom(this.element).hasText('');
+  });
+
+  test('it should return a collapsed IPv6 address', async function (assert) {
+    this.set('inputValue', '2001:0db8:0000:0000:0000:ff00:0042:8329');
+
+    await render(hbs`<div>{{format-ipaddr this.inputValue}}</div>`);
+
+    assert.dom(this.element).hasText('[2001:db8::ff00:42:8329]');
+  });
+});
