554205869aa7b4365089d6832efd5146e1210a5c
hc-github-team-consul-core <github-team-consul-core@hashicorp.com>
2025-07-14 09:49:51 +0530

    Backport of Updating dependency for security issues into release/1.21.x (#22488)
    * backport of commit ab44fabf5999d136e532c60e541536fb88dd1171
    
    * Updating codegen changes
    
    * Updating ubi9-minimal to 9.6
    
    ---------
    
    Co-authored-by: P Ajay Rao <pajay.rao@hashicorp.com>

--- ui/packages/consul-ui/tests/unit/utils/process-ip-address-test.js
+++ ui/packages/consul-ui/tests/unit/utils/process-ip-address-test.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
 import { processIpAddress } from 'consul-ui/utils/process-ip-address';
 import { module, test } from 'qunit';
 
