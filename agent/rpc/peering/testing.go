3cd71e75c415c96162eea6be1338ebdc4fa9ae9c
anilvpatel <anilkumarvinodbhai.patel@hashicorp.com>
2025-08-19 11:51:23 +0530

    fix: used constants for recurring strings

--- agent/rpc/peering/testing.go
+++ agent/rpc/peering/testing.go
@@ -37,6 +37,8 @@ not valid
 var validAddress = "1.2.3.4:80"
 var validHostnameAddress = "foo.bar.baz:80"
 var validIPv6Address = "[2001:db8::1]:80"
+var invalidIPv6Address = "[2001:db8::1:80]"
+var ipv6AddressWithoutPort = "2001:db8::1"
 
 var validServerName = "server.consul"
 
