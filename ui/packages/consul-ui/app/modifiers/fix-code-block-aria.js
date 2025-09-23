c40090784bfa9fbeb073366c21927f2661946b42
hc-github-team-consul-core <github-team-consul-core@hashicorp.com>
2025-09-23 14:55:33 +0530

    Backport of UI accessibility bugs into release/1.21.x (#22814)
    backport of commit eadffc49814278f7c89af4d9e96e6f786d531dd2
    
    Co-authored-by: suresh-hashicorp <suresh.kumardunga@hashicorp.com>

---
+++ ui/packages/consul-ui/app/modifiers/fix-code-block-aria.js
@@ -0,0 +1,13 @@
+import { modifier } from 'ember-modifier';
+
+export default modifier(function fixCodeBlockAria(element) {
+  function fixAria() {
+    // Fix HDS CodeBlock ARIA issue - add role to pre elements with aria-labelledby
+    element.querySelectorAll('pre[aria-labelledby]:not([role])').forEach((pre) => {
+      pre.setAttribute('role', 'region');
+    });
+  }
+
+  setTimeout(fixAria, 100);
+  new MutationObserver(fixAria).observe(element, { childList: true, subtree: true });
+});
