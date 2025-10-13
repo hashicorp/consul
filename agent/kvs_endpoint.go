870f4980fd4a2bade621b04e31f45f0b0621bc33
Manisha Kumari <manisha.kumari@hashicorp.com>
2025-10-14 00:36:35 +0530

    changed byteReader to maxByteReader
    Signed-off-by: Manisha Kumari <manisha.kumari@hashicorp.com>

--- agent/kvs_endpoint.go
+++ agent/kvs_endpoint.go
@@ -244,7 +244,7 @@ func (s *HTTPHandlers) KVSPut(resp http.ResponseWriter, req *http.Request, args 
 	case req.ContentLength <= 0 && req.Body != nil:
 		// LimitReader to limit copy of large requests with no Content-Length
 		//+1 ensures we can detect if the body is too large
-		limitedReader := io.LimitReader(req.Body, maxSize+1)
+		limitedReader := http.MaxBytesReader(nil, req.Body, maxSize+1)
 		buf = new(bytes.Buffer)
 		copiedBytes, err := io.Copy(buf, limitedReader)
 		if err != nil {
