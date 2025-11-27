8e1e08d002fc15aadb29734a94a164f70db5d196
Suresh Dunga <suresh.kumardunga@hashicorp.com>
2025-11-27 11:20:47 +0530

    refactor(ui): modernize menu-panel component to Glimmer
    - Convert from classic Ember component to Glimmer component
    - Replace Component.extend() with ES6 class syntax
    - Use @tracked decorator for isConfirmation property
    - Use @action decorator instead of actions hash
    - Replace next() with schedule('afterRender') for clarity
    - Replace set() with direct property assignment
    - Remove Slotted mixin (not needed in Glimmer)
    - Update template to use {{on-insert this.connect}}
    - Maintain backward compatibility with no-op change function

--- ui/packages/consul-ui/app/components/menu-panel/index.js
+++ ui/packages/consul-ui/app/components/menu-panel/index.js
@@ -3,34 +3,28 @@
  * SPDX-License-Identifier: BUSL-1.1
  */
 
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import { next } from '@ember/runloop';
-import { set } from '@ember/object';
+import { schedule } from '@ember/runloop';
 
-import Slotted from 'block-slots';
+export default class MenuPanelComponent extends Component {
+  @service dom;
 
-export default Component.extend(Slotted, {
-  tagName: '',
-  dom: service('dom'),
-  isConfirmation: false,
-  actions: {
-    connect: function ($el) {
-      next(() => {
-        if (!this.isDestroyed) {
-          // if theres only a single choice in the menu and it doesn't have an
-          // immediate button/link/label to click then it will be a
-          // confirmation/informed action
-          const isConfirmationMenu = this.dom.element(
-            'li:only-child > [role="menu"]:first-child',
-            $el
-          );
-          set(this, 'isConfirmation', typeof isConfirmationMenu !== 'undefined');
-        }
-      });
-    },
-    change: function (e) {
-      // not being used
-    },
-  },
-});
+  @tracked isConfirmation = false;
+
+  @action
+  connect(element) {
+    schedule('afterRender', () => {
+      // if theres only a single choice in the menu and it doesn't have an
+      // immediate button/link/label to click then it will be a
+      // confirmation/informed action
+      const isConfirmationMenu = this.dom.element(
+        'li:only-child > [role="menu"]:first-child',
+        element
+      );
+      this.isConfirmation = typeof isConfirmationMenu !== 'undefined';
+    });
+  }
+}
