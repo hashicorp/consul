d9fa40ef5cdabe2ac28910214ccf2ec604c17c1c
Rishabh Gupta <rishabh.gupta@hashicorp.com>
2025-12-17 11:26:46 +0530

    package upgrades

--- ui/packages/consul-ui/config/deprecation-workflow.js
+++ ui/packages/consul-ui/config/deprecation-workflow.js
@@ -7,18 +7,21 @@
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
-    { handler: 'log', matchId: 'ember-cli-page-object.string-properties-on-definition' },
-    { handler: 'log', matchId: 'ember-sinon-qunit.test' },
-    { handler: 'log', matchId: 'ember-qunit.deprecate-legacy-apis' },
-    { handler: 'log', matchId: 'ember-can.can-service' },
-    { handler: 'log', matchId: 'ember-data:model.toJSON' },
-    { handler: 'log', matchId: 'ember-cli-page-object.is-property' },
-    { handler: 'log', matchId: 'ember-views.partial' },
-    { handler: 'log', matchId: 'ember-component.send-action' },
-    { handler: 'log', matchId: 'ember-cli-page-object.multiple' },
-    { handler: 'log', matchId: 'computed-property.override' },
-    { handler: 'log', matchId: 'autotracking.mutation-after-consumption' },
-    { handler: 'log', matchId: 'ember-data:legacy-test-helper-support' },
-    { handler: 'log', matchId: 'ember-data:Model.data' },
+    { handler: 'throw', matchId: 'ember-views.partial' }, // CRITICAL
+    { handler: 'throw', matchId: 'ember-component.send-action' }, // CRITICAL
+    { handler: 'throw', matchId: 'computed-property.override' }, // CRITICAL
+    { handler: 'throw', matchId: 'ember-cli-page-object.string-properties-on-definition' },
+    { handler: 'throw', matchId: 'ember-sinon-qunit.test' },
+    { handler: 'throw', matchId: 'ember-qunit.deprecate-legacy-apis' },
+    { handler: 'throw', matchId: 'ember-can.can-service' },
+    { handler: 'throw', matchId: 'ember-data:model.toJSON' },
+    { handler: 'throw', matchId: 'ember-cli-page-object.is-property' },
+    { handler: 'throw', matchId: 'ember-views.partial' },
+    { handler: 'throw', matchId: 'ember-component.send-action' },
+    { handler: 'throw', matchId: 'ember-cli-page-object.multiple' },
+    { handler: 'throw', matchId: 'computed-property.override' },
+    { handler: 'throw', matchId: 'autotracking.mutation-after-consumption' },
+    { handler: 'throw', matchId: 'ember-data:legacy-test-helper-support' },
+    { handler: 'throw', matchId: 'ember-data:Model.data' },
   ],
 };
