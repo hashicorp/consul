c606ef1acad3ca1ff947076b66a4efefca93388c
hc-github-team-consul-core <github-team-consul-core@hashicorp.com>
2026-01-02 18:18:01 +0530

    Backport of remove: hcp related code in agent for HCP consul EoL into release/1.22.x (#23111)
    backport of commit ff4d947eb0009e69d3e92e9dab5be96fc8a82b04
    
    Co-authored-by: Sreeram Narayanan <sreeram.narayanan@hashicorp.com>

--- agent/hcp/bootstrap/constants/constants.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: BUSL-1.1
-
-// Package constants declares some constants for use in the HCP bootstrapping
-// process. It is in its own package with no other dependencies in order
-// to avoid a dependency cycle.
-package constants
-
-const SubDir = "hcp-config"
