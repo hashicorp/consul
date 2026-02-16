905ddf38b0aae1f227827911d2278f6c9017e2fa
Rishabh Gupta <rishabh.gupta@hashicorp.com>
2026-02-16 16:41:03 +0530

    Rishabh gupta/UI/merge package addons into main app (#23215)
    * move from a monorepo structure to single consul-ui package
    
    * adds changelog and code cleanup of remnant package files
    
    * linting fixes
    
    * reduce verbnose deprecation logs
    
    * fixes for ent tests

---
+++ ui/packages/consul-ui/app/components/consul/partition/list/test-support.js
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
+
+export const selectors = () => ({
+  ['.consul-partition-list']: {
+    row: {
+      $: '[data-test-list-row]',
+      partition: 'a',
+      name: '[data-test-partition]',
+      description: '[data-test-description]',
+    },
+  },
+});
+export const pageObject = (collection, clickable, attribute, text, actions) => () => {
+  return collection('.consul-partition-list [data-test-list-row]', {
+    partition: clickable('a'),
+    name: attribute('data-test-partition', '[data-test-partition]'),
+    description: text('[data-test-description]'),
+    ...actions(['edit', 'delete']),
+  });
+};
