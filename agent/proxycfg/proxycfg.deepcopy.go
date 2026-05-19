944bb2068f5b170751e4d24b8a4c325627986696
Sanika Chavan <sanika.vikaschavan@hashicorp.com>
2026-05-19 16:32:28 +0530

    add gateway failures

--- agent/proxycfg/proxycfg.deepcopy.go
+++ agent/proxycfg/proxycfg.deepcopy.go
@@ -4,11 +4,12 @@ package proxycfg
 
 import (
 	"context"
+	"time"
+
 	"github.com/hashicorp/consul/agent/structs"
 	"github.com/hashicorp/consul/agent/xds/config"
 	"github.com/hashicorp/consul/proto/private/pbpeering"
 	"github.com/hashicorp/consul/types"
-	"time"
 )
 
 // DeepCopy generates a deep copy of *ConfigSnapshot
@@ -594,6 +595,18 @@ func (o *configSnapshotIngressGateway) DeepCopy() *configSnapshotIngressGateway 
 		if o.Defaults.PassiveHealthCheck.EnforcingConsecutive5xx != nil {
 			cp.Defaults.PassiveHealthCheck.EnforcingConsecutive5xx = new(uint32)
 			*cp.Defaults.PassiveHealthCheck.EnforcingConsecutive5xx = *o.Defaults.PassiveHealthCheck.EnforcingConsecutive5xx
+		}
+		if o.Defaults.PassiveHealthCheck.EnforcingConsecutiveGatewayFailure != nil {
+			cp.Defaults.PassiveHealthCheck.EnforcingConsecutiveGatewayFailure = new(uint32)
+			*cp.Defaults.PassiveHealthCheck.EnforcingConsecutiveGatewayFailure = *o.Defaults.PassiveHealthCheck.EnforcingConsecutiveGatewayFailure
+		}
+		if o.Defaults.PassiveHealthCheck.Consecutive5xx != nil {
+			cp.Defaults.PassiveHealthCheck.Consecutive5xx = new(uint32)
+			*cp.Defaults.PassiveHealthCheck.Consecutive5xx = *o.Defaults.PassiveHealthCheck.Consecutive5xx
+		}
+		if o.Defaults.PassiveHealthCheck.ConsecutiveGatewayFailure != nil {
+			cp.Defaults.PassiveHealthCheck.ConsecutiveGatewayFailure = new(uint32)
+			*cp.Defaults.PassiveHealthCheck.ConsecutiveGatewayFailure = *o.Defaults.PassiveHealthCheck.ConsecutiveGatewayFailure
 		}
 		if o.Defaults.PassiveHealthCheck.MaxEjectionPercent != nil {
 			cp.Defaults.PassiveHealthCheck.MaxEjectionPercent = new(uint32)
