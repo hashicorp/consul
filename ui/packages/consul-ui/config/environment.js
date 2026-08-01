834b112b0ef38265992bcd66543b6cde15101a08
Rishabh Gupta <rishabh.gupta@hashicorp.com>
2026-08-02 01:44:13 +0530

    environment values reverted

--- ui/packages/consul-ui/config/environment.js
+++ ui/packages/consul-ui/config/environment.js
@@ -87,10 +87,10 @@ module.exports = function (environment, $ = process.env) {
     // The following variables are runtime variables that are overwritten when
     // the go binary serves the index.html page
     operatorConfig: {
-      ACLsEnabled: true,
+      ACLsEnabled: false,
       NamespacesEnabled: false,
       SSOEnabled: false,
-      PeeringEnabled: true,
+      PeeringEnabled: false,
       PartitionsEnabled: false,
       HCPEnabled: false,
       LocalDatacenter: env('CONSUL_DATACENTER_LOCAL', 'dc1'),
