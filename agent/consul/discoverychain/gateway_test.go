hashicorp/consul

commit 4ae45d4b5af216d62c776dbf66527cca36faa320

(apigw) resolve service subsets for routes during apigw discoverychain synth (#23294)

agent/consul/discoverychain/gateway_httproute.go
@@ -5,9 +5,12 @@ package discoverychain
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/hashicorp/consul/agent/structs"
 )
+
+const maxComposedRoutes = 256
 
 // compareHTTPRules implements the non-hostname order of precedence for routes specified by the K8s Gateway API spec.
 // https://gateway-api.sigs.k8s.io/v1alpha2/references/spec/#gateway.networking.k8s.io/v1alpha2.HTTPRouteRule
@@ -42,12 +45,12 @@ func httpServiceDefault(entry structs.ConfigEntry, meta map[string]string) *stru
 	}
 }
 
-func synthesizeHTTPRouteDiscoveryChain(route structs.HTTPRouteConfigEntry) (structs.IngressService, *structs.ServiceRouterConfigEntry, []*structs.ServiceSplitterConfigEntry, []*structs.ServiceConfigEntry) {
+func synthesizeHTTPRouteDiscoveryChain(route structs.HTTPRouteConfigEntry, serviceRouters map[structs.ServiceName][]*structs.ServiceRoute) (structs.IngressService, *structs.ServiceRouterConfigEntry, []*structs.ServiceSplitterConfigEntry, []*structs.ServiceConfigEntry) {
 	meta := route.GetMeta()
 	splitters := []*structs.ServiceSplitterConfigEntry{}
 	defaults := []*structs.ServiceConfigEntry{}
 
-	router, splits, upstreamDefaults := httpRouteToDiscoveryChain(route)
+	router, splits, upstreamDefaults := httpRouteToDiscoveryChain(route, serviceRouters)
 	serviceDefault := httpServiceDefault(router, meta)
 	defaults = append(defaults, serviceDefault)
 	for _, split := range splits {
@@ -68,7 +71,7 @@ func synthesizeHTTPRouteDiscoveryChain(route structs.HTTPRouteConfigEntry) (stru
 	return ingress, router, splitters, defaults
 }
 
-func httpRouteToDiscoveryChain(route structs.HTTPRouteConfigEntry) (*structs.ServiceRouterConfigEntry, []*structs.ServiceSplitterConfigEntry, []*structs.ServiceConfigEntry) {
+func httpRouteToDiscoveryChain(route structs.HTTPRouteConfigEntry, serviceRouters map[structs.ServiceName][]*structs.ServiceRoute) (*structs.ServiceRouterConfigEntry, []*structs.ServiceSplitterConfigEntry, []*structs.ServiceConfigEntry) {
 	router := &structs.ServiceRouterConfigEntry{
 		Kind:           structs.ServiceRouter,
 		Name:           route.GetName(),
@@ -123,6 +126,55 @@ func httpRouteToDiscoveryChain(route structs.HTTPRouteConfigEntry) (*structs.Ser
 				Protocol:       "http",
 				EnterpriseMeta: service.EnterpriseMeta,
 			})
+
+			applyHTTPRouteFilters(&destination, rule)
+
+			httpMatches := rule.Matches
+			if len(httpMatches) == 0 {
+				httpMatches = []structs.HTTPMatch{{
+					Path: structs.HTTPPathMatch{
+						Match: structs.HTTPPathMatchPrefix,
+						Value: "/",
+					},
+				}}
+			}
+
+			serviceRouterRoutes := lookupServiceRouterRules(serviceRouters, service)
+			if shouldComposeServiceRouter(httpMatches, serviceRouterRoutes) {
+				for _, match := range httpMatches {
+					httpMatch := &structs.ServiceRouteMatch{HTTP: httpRouteMatchToServiceRouteHTTPMatch(match)}
+					composed := false
+
+					for _, svcRoute := range serviceRouterRoutes {
+						mergedMatch, ok := mergeServiceRouteMatch(httpMatch, svcRoute.Match)
+						if !ok {
+							continue
+						}
+						mergedDest := mergeServiceRouteDestination(&destination, svcRoute.Destination)
+						router.Routes = append(router.Routes, structs.ServiceRoute{
+							Match:       mergedMatch,
+							Destination: mergedDest,
+						})
+						composed = true
+					}
+
+					if !composed {
+						router.Routes = append(router.Routes, structs.ServiceRoute{
+							Match:       httpMatch,
+							Destination: &destination,
+						})
+					}
+				}
+			} else {
+				for _, match := range httpMatches {
+					router.Routes = append(router.Routes, structs.ServiceRoute{
+						Match:       &structs.ServiceRouteMatch{HTTP: httpRouteMatchToServiceRouteHTTPMatch(match)},
+						Destination: &destination,
+					})
+				}
+			}
+
+			continue
 		} else {
 			// create a virtual service to split
 			destination.Service = fmt.Sprintf("%s-%d", route.GetName(), idx)
@@ -176,24 +228,7 @@ func httpRouteToDiscoveryChain(route structs.HTTPRouteConfigEntry) (*structs.Ser
 			}
 		}
 
-		if rule.Filters.RetryFilter != nil {
-
-			destination.NumRetries = rule.Filters.RetryFilter.NumRetries
-			destination.RetryOnConnectFailure = rule.Filters.RetryFilter.RetryOnConnectFailure
-
-			if len(rule.Filters.RetryFilter.RetryOn) > 0 {
-				destination.RetryOn = rule.Filters.RetryFilter.RetryOn
-			}
-
-			if len(rule.Filters.RetryFilter.RetryOnStatusCodes) > 0 {
-				destination.RetryOnStatusCodes = rule.Filters.RetryFilter.RetryOnStatusCodes
-			}
-		}
-
-		if rule.Filters.TimeoutFilter != nil {
-			destination.IdleTimeout = rule.Filters.TimeoutFilter.IdleTimeout
-			destination.RequestTimeout = rule.Filters.TimeoutFilter.RequestTimeout
-		}
+		applyHTTPRouteFilters(&destination, rule)
 
 		// for each match rule a ServiceRoute is created for the service-router
 		// if there are no rules a single route with the destination is set
@@ -211,6 +246,350 @@ func httpRouteToDiscoveryChain(route structs.HTTPRouteConfigEntry) (*structs.Ser
 	}
 
 	return router, splitters, defaults
+}
+
+func applyHTTPRouteFilters(destination *structs.ServiceRouteDestination, rule structs.HTTPRouteRule) {
+	if rule.Filters.RetryFilter != nil {
+		destination.NumRetries = rule.Filters.RetryFilter.NumRetries
+		destination.RetryOnConnectFailure = rule.Filters.RetryFilter.RetryOnConnectFailure
+
+		if len(rule.Filters.RetryFilter.RetryOn) > 0 {
+			destination.RetryOn = rule.Filters.RetryFilter.RetryOn
+		}
+
+		if len(rule.Filters.RetryFilter.RetryOnStatusCodes) > 0 {
+			destination.RetryOnStatusCodes = rule.Filters.RetryFilter.RetryOnStatusCodes
+		}
+	}
+
+	if rule.Filters.TimeoutFilter != nil {
+		destination.IdleTimeout = rule.Filters.TimeoutFilter.IdleTimeout
+		destination.RequestTimeout = rule.Filters.TimeoutFilter.RequestTimeout
+	}
+}
+
+func lookupServiceRouterRules(serviceRouters map[structs.ServiceName][]*structs.ServiceRoute, service structs.HTTPService) []*structs.ServiceRoute {
+	if len(serviceRouters) == 0 {
+		return nil
+	}
+	return serviceRouters[service.ServiceName()]
+}
+
+func shouldComposeServiceRouter(httpMatches []structs.HTTPMatch, serviceRoutes []*structs.ServiceRoute) bool {
+	if len(serviceRoutes) == 0 {
+		return false
+	}
+	if len(httpMatches) == 0 {
+		return true
+	}
+	if len(serviceRoutes) > maxComposedRoutes/len(httpMatches) {
+		return false
+	}
+	return true
+}
+
+func mergeServiceRouteMatch(httpMatch, svcMatch *structs.ServiceRouteMatch) (*structs.ServiceRouteMatch, bool) {
+	if httpMatch == nil || httpMatch.IsEmpty() {
+		return cloneServiceRouteMatch(svcMatch), true
+	}
+	if svcMatch == nil || svcMatch.IsEmpty() {
+		return cloneServiceRouteMatch(httpMatch), true
+	}
+
+	mergedHTTP, ok := mergeServiceRouteHTTPMatch(httpMatch.HTTP, svcMatch.HTTP)
+	if !ok {
+		return nil, false
+	}
+	return &structs.ServiceRouteMatch{HTTP: mergedHTTP}, true
+}
+
+func mergeServiceRouteHTTPMatch(a, b *structs.ServiceRouteHTTPMatch) (*structs.ServiceRouteHTTPMatch, bool) {
+	if a == nil || a.IsEmpty() {
+		return cloneServiceRouteHTTPMatch(b), true
+	}
+	if b == nil || b.IsEmpty() {
+		return cloneServiceRouteHTTPMatch(a), true
+	}
+
+	merged := cloneServiceRouteHTTPMatch(a)
+	if merged == nil {
+		merged = &structs.ServiceRouteHTTPMatch{}
+	}
+
+	path, ok := mergePathMatch(a, b)
+	if !ok {
+		return nil, false
+	}
+	merged.PathExact = path.pathExact
+	merged.PathPrefix = path.pathPrefix
+	merged.PathRegex = path.pathRegex
+	merged.CaseInsensitive = a.CaseInsensitive && b.CaseInsensitive
+
+	merged.Header = append(append([]structs.ServiceRouteHTTPMatchHeader{}, a.Header...), b.Header...)
+	merged.QueryParam = append(append([]structs.ServiceRouteHTTPMatchQueryParam{}, a.QueryParam...), b.QueryParam...)
+
+	merged.Methods = mergeHTTPMethods(a.Methods, b.Methods)
+	if len(merged.Methods) == 0 && len(a.Methods) > 0 && len(b.Methods) > 0 {
+		return nil, false
+	}
+
+	return merged, true
+}
+
+type mergedPath struct {
+	pathExact  string
+	pathPrefix string
+	pathRegex  string
+}
+
+func mergePathMatch(a, b *structs.ServiceRouteHTTPMatch) (mergedPath, bool) {
+	aPath, aOK := extractPathMatch(a)
+	bPath, bOK := extractPathMatch(b)
+
+	if !aOK && !bOK {
+		return mergedPath{}, true
+	}
+	if !aOK {
+		return mergedPath{pathExact: b.PathExact, pathPrefix: b.PathPrefix, pathRegex: b.PathRegex}, true
+	}
+	if !bOK {
+		return mergedPath{pathExact: a.PathExact, pathPrefix: a.PathPrefix, pathRegex: a.PathRegex}, true
+	}
+
+	switch aPath.kind {
+	case "exact":
+		switch bPath.kind {
+		case "exact":
+			if aPath.value != bPath.value {
+				return mergedPath{}, false
+			}
+			return mergedPath{pathExact: aPath.value}, true
+		case "prefix":
+			if strings.HasPrefix(aPath.value, bPath.value) {
+				return mergedPath{pathExact: aPath.value}, true
+			}
+			return mergedPath{}, false
+		case "regex":
+			if aPath.value == bPath.value {
+				return mergedPath{pathExact: aPath.value}, true
+			}
+			return mergedPath{}, false
+		}
+	case "prefix":
+		switch bPath.kind {
+		case "exact":
+			if strings.HasPrefix(bPath.value, aPath.value) {
+				return mergedPath{pathExact: bPath.value}, true
+			}
+			return mergedPath{}, false
+		case "prefix":
+			if strings.HasPrefix(aPath.value, bPath.value) {
+				return mergedPath{pathPrefix: aPath.value}, true
+			}
+			if strings.HasPrefix(bPath.value, aPath.value) {
+				return mergedPath{pathPrefix: bPath.value}, true
+			}
+			return mergedPath{}, false
+		case "regex":
+			if aPath.value == bPath.value {
+				return mergedPath{pathPrefix: aPath.value}, true
+			}
+			return mergedPath{}, false
+		}
+	case "regex":
+		switch bPath.kind {
+		case "regex":
+			if aPath.value != bPath.value {
+				return mergedPath{}, false
+			}
+			return mergedPath{pathRegex: aPath.value}, true
+		case "exact":
+			if aPath.value == bPath.value {
+				return mergedPath{pathExact: bPath.value}, true
+			}
+			return mergedPath{}, false
+		case "prefix":
+			if aPath.value == bPath.value {
+				return mergedPath{pathPrefix: bPath.value}, true
+			}
+			return mergedPath{}, false
+		}
+	}
+
+	return mergedPath{}, false
+}
+
+type pathMatch struct {
+	kind  string
+	value string
+}
+
+func extractPathMatch(m *structs.ServiceRouteHTTPMatch) (pathMatch, bool) {
+	if m == nil {
+		return pathMatch{}, false
+	}
+	switch {
+	case m.PathExact != "":
+		return pathMatch{kind: "exact", value: m.PathExact}, true
+	case m.PathPrefix != "":
+		return pathMatch{kind: "prefix", value: m.PathPrefix}, true
+	case m.PathRegex != "":
+		return pathMatch{kind: "regex", value: m.PathRegex}, true
+	default:
+		return pathMatch{}, false
+	}
+}
+
+func mergeHTTPMethods(a, b []string) []string {
+	if len(a) == 0 {
+		return append([]string(nil), b...)
+	}
+	if len(b) == 0 {
+		return append([]string(nil), a...)
+	}
+	set := make(map[string]struct{}, len(a))
+	for _, method := range a {
+		set[method] = struct{}{}
+	}
+	var out []string
+	for _, method := range b {
+		if _, ok := set[method]; ok {
+			out = append(out, method)
+		}
+	}
+	return out
+}
+
+func mergeServiceRouteDestination(httpDest, svcDest *structs.ServiceRouteDestination) *structs.ServiceRouteDestination {
+	if svcDest == nil {
+		return cloneServiceRouteDestination(httpDest)
+	}
+	merged := cloneServiceRouteDestination(svcDest)
+	if httpDest == nil {
+		return merged
+	}
+
+	if merged.Service == "" {
+		merged.Service = httpDest.Service
+	}
+	if merged.ServiceSubset == "" {
+		merged.ServiceSubset = httpDest.ServiceSubset
+	}
+	if merged.Namespace == "" {
+		merged.Namespace = httpDest.Namespace
+	}
+	if merged.Partition == "" {
+		merged.Partition = httpDest.Partition
+	}
+
+	if httpDest.PrefixRewrite != "" {
+		merged.PrefixRewrite = httpDest.PrefixRewrite
+	}
+	if httpDest.RequestTimeout != 0 {
+		merged.RequestTimeout = httpDest.RequestTimeout
+	}
+	if httpDest.IdleTimeout != 0 {
+		merged.IdleTimeout = httpDest.IdleTimeout
+	}
+	if httpDest.NumRetries != 0 {
+		merged.NumRetries = httpDest.NumRetries
+	}
+	if httpDest.RetryOnConnectFailure {
+		merged.RetryOnConnectFailure = true
+	}
+	if len(httpDest.RetryOn) > 0 {
+		merged.RetryOn = httpDest.RetryOn
+	}
+	if len(httpDest.RetryOnStatusCodes) > 0 {
+		merged.RetryOnStatusCodes = httpDest.RetryOnStatusCodes
+	}
+
+	merged.RequestHeaders = mergeHeaderModifiers(merged.RequestHeaders, httpDest.RequestHeaders)
+	merged.ResponseHeaders = mergeHeaderModifiers(merged.ResponseHeaders, httpDest.ResponseHeaders)
+
+	return merged
+}
+
+func mergeHeaderModifiers(base, overlay *structs.HTTPHeaderModifiers) *structs.HTTPHeaderModifiers {
+	if base == nil && overlay == nil {
+		return nil
+	}
+	if base == nil {
+		return cloneHeaderModifiers(overlay)
+	}
+	if overlay == nil {
+		return cloneHeaderModifiers(base)
+	}
+
+	merged := &structs.HTTPHeaderModifiers{
+		Add:    make(map[string]string),
+		Set:    make(map[string]string),
+		Remove: []string{},
+	}
+
+	for k, v := range base.Add {
+		merged.Add[k] = v
+	}
+	for k, v := range base.Set {
+		merged.Set[k] = v
+	}
+	merged.Remove = append(merged.Remove, base.Remove...)
+
+	for k, v := range overlay.Add {
+		merged.Add[k] = v
+	}
+	for k, v := range overlay.Set {
+		merged.Set[k] = v
+	}
+	merged.Remove = append(merged.Remove, overlay.Remove...)
+
+	return merged
+}
+
+func cloneServiceRouteMatch(in *structs.ServiceRouteMatch) *structs.ServiceRouteMatch {
+	if in == nil {
+		return nil
+	}
+	return &structs.ServiceRouteMatch{HTTP: cloneServiceRouteHTTPMatch(in.HTTP)}
+}
+
+func cloneServiceRouteHTTPMatch(in *structs.ServiceRouteHTTPMatch) *structs.ServiceRouteHTTPMatch {
+	if in == nil {
+		return nil
+	}
+	out := *in
+	out.Header = append([]structs.ServiceRouteHTTPMatchHeader(nil), in.Header...)
+	out.QueryParam = append([]structs.ServiceRouteHTTPMatchQueryParam(nil), in.QueryParam...)
+	out.Methods = append([]string(nil), in.Methods...)
+	return &out
+}
+
+func cloneServiceRouteDestination(in *structs.ServiceRouteDestination) *structs.ServiceRouteDestination {
+	if in == nil {
+		return nil
+	}
+	out := *in
+	out.RequestHeaders = cloneHeaderModifiers(in.RequestHeaders)
+	out.ResponseHeaders = cloneHeaderModifiers(in.ResponseHeaders)
+	return &out
+}
+
+func cloneHeaderModifiers(in *structs.HTTPHeaderModifiers) *structs.HTTPHeaderModifiers {
+	if in == nil {
+		return nil
+	}
+	out := &structs.HTTPHeaderModifiers{
+		Add:    make(map[string]string),
+		Set:    make(map[string]string),
+		Remove: append([]string(nil), in.Remove...),
+	}
+	for k, v := range in.Add {
+		out.Add[k] = v
+	}
+	for k, v := range in.Set {
+		out.Set[k] = v
+	}
+	return out
 }
 
 func httpRouteFiltersToDestinationPrefixRewrite(rewrite *structs.URLRewrite) string {

commit 59dae4b8c64978b4c3ea8ee5c31da878b51d2736

fix(apigw): resolve composed service-router destinations as http during gateway synthesis
An API gateway builds its xDS config from a synthesized discovery chain. As of #23294, gateway synthesis composes a backend service's own ServiceRouter rules into that synthetic chain, and a composed route can point at a different, entry-less downstream service that relies on the global proxy-defaults protocol fallback.

The synthetic entry set intentionally omits proxy-defaults, and a synthetic http service-default was only created for the directly-referenced backend, not for composed sub-destinations. So the synthetic chain resolves those destinations as tcp and fails with "uses inconsistent protocols" against the http gateway chain, discarding the gateway's entire config batch and causing intermittent 503s (most visibly in cluster-peering / multi-partition topologies).

Add a synthetic http service-default for composed service-router destinations, keyed by the destination's own partition/namespace so cross-partition targets match. Reachability through an http gateway listener already implies the destination is http, matching how directly-referenced destinations are treated.

Regression introduced by 4ae45d4 (#23294).

agent/consul/discoverychain/gateway_httproute.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/hashicorp/consul/acl"
 	"github.com/hashicorp/consul/agent/structs"
 )
 
@@ -43,6 +44,28 @@ func httpServiceDefault(entry structs.ConfigEntry, meta map[string]string) *stru
 		Meta:           meta,
 		EnterpriseMeta: *entry.GetEnterpriseMeta(),
 	}
+}
+
+// appendComposedHTTPDefault ensures a synthetic http service-defaults exists for
+// a composed service-router destination that may target a service other than the
+// one referenced directly by the HTTPRoute. Without it, the synthesized gateway
+// discovery chain would resolve an entry-less destination's protocol as tcp — the
+// synthetic entry set intentionally omits proxy-defaults, so the proxy-defaults
+// protocol fallback that the real (on-demand) compiler applies is unavailable
+// here — producing a spurious "uses inconsistent protocols" error against the
+// http gateway chain. Reachability through an http gateway listener already
+// implies the destination is http, matching how directly-referenced destinations
+// are handled above.
+func appendComposedHTTPDefault(defaults []*structs.ServiceConfigEntry, dest *structs.ServiceRouteDestination) []*structs.ServiceConfigEntry {
+	if dest == nil || dest.Service == "" {
+		return defaults
+	}
+	return append(defaults, &structs.ServiceConfigEntry{
+		Kind:           structs.ServiceDefaults,
+		Name:           dest.Service,
+		Protocol:       "http",
+		EnterpriseMeta: acl.NewEnterpriseMetaWithPartition(dest.Partition, dest.Namespace),
+	})
 }
 
 func synthesizeHTTPRouteDiscoveryChain(route structs.HTTPRouteConfigEntry, serviceRouters map[structs.ServiceName][]*structs.ServiceRoute) (structs.IngressService, *structs.ServiceRouterConfigEntry, []*structs.ServiceSplitterConfigEntry, []*structs.ServiceConfigEntry) {
@@ -151,6 +174,17 @@ func httpRouteToDiscoveryChain(route structs.HTTPRouteConfigEntry, serviceRouter
 							continue
 						}
 						mergedDest := mergeServiceRouteDestination(&destination, svcRoute.Destination)
+
+						// A composed service-router route may target a service other
+						// than the one referenced directly by the HTTPRoute (the
+						// backend router can route/redirect elsewhere). Ensure a
+						// synthetic service-defaults exists for that destination so
+						// the synthesized gateway chain resolves it as http rather
+						// than falling through to the tcp default: the synthetic
+						// entry set intentionally carries no proxy-defaults, so an
+						// entry-less destination would otherwise be recorded as tcp
+						// and fail the http chain with "inconsistent protocols".
+						defaults = appendComposedHTTPDefault(defaults, mergedDest)
 						router.Routes = append(router.Routes, structs.ServiceRoute{
 							Match:       mergedMatch,
 							Destination: mergedDest,

agent/consul/discoverychain/gateway_test.go
@@ -6,9 +6,10 @@ package discoverychain
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/hashicorp/consul/agent/configentry"
 	"github.com/hashicorp/consul/agent/structs"
-	"github.com/stretchr/testify/require"
 )
 
 func TestGatewayChainSynthesizer_AddTCPRoute(t *testing.T) {
@@ -1066,4 +1067,201 @@ func TestGatewayChainSynthesizer_ComplexChain(t *testing.T) {
 			require.Equal(t, tc.expectedDiscoveryChain, discoveryChains[0])
 		})
 	}
+}
+
+// TestSynthesizeHTTPRouteDiscoveryChain_ComposedDestinationProtocol is a
+// regression test for the protocol-resolution defect introduced by #12479.
+//
+// When an HTTPRoute's backend service has its own ServiceRouter, gateway
+// synthesis composes that router's routes into the synthetic gateway chain. A
+// composed route may point at a *different* downstream service than the one the
+// HTTPRoute named directly. That downstream service can be entry-less (no
+// service-defaults), relying on the global proxy-defaults protocol fallback.
+//
+// The synthetic entry set intentionally carries no proxy-defaults, so the
+// fallback the real compiler applies is unavailable during synthesis. Without a
+// synthetic http default for the composed destination, the synthesized chain
+// resolves it as tcp and fails with "uses inconsistent protocols" against the
+// http gateway chain — dropping the gateway's entire xDS config batch.
+func TestSynthesizeHTTPRouteDiscoveryChain_ComposedDestinationProtocol(t *testing.T) {
+	t.Parallel()
+
+	backend := structs.HTTPService{Name: "service-a"}
+
+	// service-a's own ServiceRouter routes to service-b, which has no
+	// service-defaults entry of its own.
+	serviceRouters := map[structs.ServiceName][]*structs.ServiceRoute{
+		backend.ServiceName(): {
+			{
+				Match: &structs.ServiceRouteMatch{
+					HTTP: &structs.ServiceRouteHTTPMatch{PathPrefix: "/b"},
+				},
+				Destination: &structs.ServiceRouteDestination{
+					Service: "service-b",
+				},
+			},
+		},
+	}
+
+	route := structs.HTTPRouteConfigEntry{
+		Kind: structs.HTTPRoute,
+		Name: "test-route",
+		Rules: []structs.HTTPRouteRule{{
+			Matches: []structs.HTTPMatch{{
+				Path: structs.HTTPPathMatch{Match: structs.HTTPPathMatchPrefix, Value: "/"},
+			}},
+			Services: []structs.HTTPService{backend},
+		}},
+	}
+
+	_, router, _, defaults := synthesizeHTTPRouteDiscoveryChain(route, serviceRouters)
+
+	// The composition must have produced a route to service-b.
+	var composedToB bool
+	for _, r := range router.Routes {
+		if r.Destination != nil && r.Destination.Service == "service-b" {
+			composedToB = true
+		}
+	}
+	require.True(t, composedToB, "expected a composed route to service-b")
+
+	protocols := make(map[string]string)
+	for _, d := range defaults {
+		protocols[d.Name] = d.Protocol
+	}
+
+	require.Equal(t, "http", protocols["service-a"], "direct destination should have an http default")
+	require.Equal(t, "http", protocols["service-b"],
+		"composed sub-destination must have a synthetic http default so the "+
+			"synthesized chain does not resolve it as tcp (regression #12479)")
+}
+
+// TestGatewaySynthesis_ProxyDefaultsFallback_StateFaithful reproduces the
+// customer's production regression the way proxycfg actually does it: the
+// backend service's discovery chain is compiled through the *real* compiler with
+// a full entry set (proxy-defaults present) — exactly what watchDiscoveryChain
+// stores in snap.APIGateway.DiscoveryChain — and *that* compiled chain is then
+// fed to the gateway synthesizer.
+//
+// This is the faithful model of:
+//
+//	API gateway (HTTP) --route--> service "x" (has a service-router w/ header
+//	routing) --router--> service "y" (entry-less, relies on proxy-defaults=http)
+//
+// It also answers the two questions raised by the field team's mitigation
+// evidence — does proxy-defaults clear the synthesis error? does service-defaults
+// on the router destination clear it? — by toggling those entries on the *real*
+// (backend) compile side and observing the synthesis outcome.
+func TestGatewaySynthesis_ProxyDefaultsFallback_StateFaithful(t *testing.T) {
+	t.Parallel()
+
+	proxyDefaultsHTTP := &structs.ProxyConfigEntry{
+		Kind:     structs.ProxyDefaults,
+		Name:     structs.ProxyConfigGlobal,
+		Protocol: "http",
+		Config:   map[string]interface{}{"protocol": "http"},
+	}
+
+	// service "x" has a real service-router doing header-based routing
+	// (X-Forwarded-For), whose destination is service "y".
+	xRouter := &structs.ServiceRouterConfigEntry{
+		Kind: structs.ServiceRouter,
+		Name: "x",
+		Routes: []structs.ServiceRoute{{
+			Match: &structs.ServiceRouteMatch{
+				HTTP: &structs.ServiceRouteHTTPMatch{
+					Header: []structs.ServiceRouteHTTPMatchHeader{{
+						Name:    "X-Forwarded-For",
+						Present: true,
+					}},
+				},
+			},
+			Destination: &structs.ServiceRouteDestination{Service: "y"},
+		}},
+	}
+
+	yServiceDefaultsHTTP := &structs.ServiceConfigEntry{
+		Kind:     structs.ServiceDefaults,
+		Name:     "y",
+		Protocol: "http",
+	}
+
+	// compileBackendChain mimics watchDiscoveryChain: compile x's chain via the
+	// real compiler against the given entry set.
+	compileBackendChain := func(t *testing.T, entries ...structs.ConfigEntry) (*structs.CompiledDiscoveryChain, error) {
+		t.Helper()
+		set := configentry.NewDiscoveryChainSet()
+		set.AddEntries(entries...)
+		return Compile(CompileRequest{
+			ServiceName:           "x",
+			EvaluateInNamespace:   "default",
+			EvaluateInPartition:   "default",
+			EvaluateInDatacenter:  "dc1",
+			EvaluateInTrustDomain: "domain",
+			Entries:               set,
+		})
+	}
+
+	synthesize := func(t *testing.T, backend *structs.CompiledDiscoveryChain) ([]*structs.CompiledDiscoveryChain, error) {
+		t.Helper()
+		synth := NewGatewayChainSynthesizer("dc1", "domain", "listener", &structs.APIGatewayConfigEntry{
+			Kind: structs.APIGateway,
+			Name: "gateway",
+		})
+		synth.SetHostname("*")
+		synth.AddHTTPRoute(structs.HTTPRouteConfigEntry{
+			Kind: structs.HTTPRoute,
+			Name: "route",
+			Rules: []structs.HTTPRouteRule{{
+				Matches: []structs.HTTPMatch{{
+					Path: structs.HTTPPathMatch{Match: structs.HTTPPathMatchPrefix, Value: "/"},
+				}},
+				Services: []structs.HTTPService{{Name: "x"}},
+			}},
+		})
+		_, chains, err := synth.Synthesize(backend)
+		return chains, err
+	}
+
+	// Sanity: with proxy-defaults=http, the REAL chain for x resolves http and
+	// compiles cleanly (y is http via the fallback) — i.e. /v1/discovery-chain
+	// would return http. This is the precondition that distinguishes the
+	// regression from a genuine protocol mismatch.
+	t.Run("real backend chain resolves http via proxy-defaults", func(t *testing.T) {
+		chain, err := compileBackendChain(t, proxyDefaultsHTTP, xRouter)
+		require.NoError(t, err)
+		require.Equal(t, "http", chain.Protocol,
+			"real compiler applies the proxy-defaults fallback for entry-less y")
+	})
+
+	// The regression itself: real chain is http, but gateway synthesis must not
+	// fail with "inconsistent protocols". With the fix in place this passes;
+	// before the fix this returned that error (the composed destination y had no
+	// synthetic http default). proxy-defaults alone does NOT prevent it — it is
+	// never part of the synthetic entry set — which is why the field team saw
+	// "proxy-defaults was set all along and it still failed".
+	t.Run("gateway synthesis succeeds (regression fixed)", func(t *testing.T) {
+		chain, err := compileBackendChain(t, proxyDefaultsHTTP, xRouter)
+		require.NoError(t, err)
+
+		chains, err := synthesize(t, chain)
+		require.NoError(t, err,
+			"gateway synthesis must not spuriously resolve y as tcp when the real chain is http")
+		require.NotEmpty(t, chains)
+	})
+
+	// Field-team mitigation check: adding service-defaults=http for y on the
+	// real/backend side. It keeps x's real chain http (already was) but note it
+	// does NOT feed the synthesizer's synthetic entry set — the synthesizer only
+	// receives x's compiled chain, not y's config. The fix (not the mitigation)
+	// is what makes synthesis pass here.
+	t.Run("service-defaults on y keeps real chain http", func(t *testing.T) {
+		chain, err := compileBackendChain(t, proxyDefaultsHTTP, xRouter, yServiceDefaultsHTTP)
+		require.NoError(t, err)
+		require.Equal(t, "http", chain.Protocol)
+
+		chains, err := synthesize(t, chain)
+		require.NoError(t, err)
+		require.NotEmpty(t, chains)
+	})
 }