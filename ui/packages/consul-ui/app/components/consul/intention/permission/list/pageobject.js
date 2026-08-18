405abd0ebb68056bddb78f6cfbd19f635601c325
Suresh Dunga <suresh.kumardunga@hashicorp.com>
2026-08-18 17:31:51 +0530

    Migrate intention permission list components to HDS

--- ui/packages/consul-ui/app/components/consul/intention/permission/list/pageobject.js
+++ ui/packages/consul-ui/app/components/consul/intention/permission/list/pageobject.js
@@ -3,11 +3,15 @@
  * SPDX-License-Identifier: BUSL-1.1
  */
 
-import { collection } from 'ember-cli-page-object';
+import { collection, clickable } from 'ember-cli-page-object';
 
 export default (scope = '.consul-intention-permission-list') => {
   return {
     scope: scope,
-    intentionPermissions: collection('[data-test-list-row]', {}),
+    intentionPermissions: collection('[data-test-list-row]', {
+      edit: clickable('[data-test-edit-action]'),
+      delete: clickable('[data-test-delete-action]'),
+    }),
+    confirmDelete: clickable('[data-test-confirm-action]'),
   };
 };
