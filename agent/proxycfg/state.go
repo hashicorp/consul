cbd985d322969a0ccaf407187b6767c0ed8be80c
mansi991999 <mansi.panchal@hashicorp.com>
2026-09-23 19:18:58 +0530

    reverting enable check

--- agent/proxycfg/state.go
+++ agent/proxycfg/state.go
@@ -333,7 +333,7 @@ func newConfigSnapshotFromServiceInstance(s serviceInstance, config stateConfig)
 		Locality:              GatewayKey{Datacenter: config.source.Datacenter, Partition: s.proxyID.PartitionOrDefault()},
 		ServerSNIFn:           config.serverSNIFn,
 		IntentionDefaultAllow: config.intentionDefaultAllow,
-		LocalizedDNSEnabled:   config.featureGate == nil || config.featureGate.Enabled(featuregate.LocalizedDNS),
+		LocalizedDNSEnabled:   config.featureGate != nil && config.featureGate.Enabled(featuregate.LocalizedDNS),
 	}
 }
 
